bba4d7c0d04aae2fcc7765d9af60a5b18b8a316a
acrefoot <acrefoot@alum.mit.edu>
2016-08-14 14:25:41 -0700

    Make Android work again (#14)
    * Update Android manifest
    https://github.com/facebook/react-native/issues/8215
    
    * Update Android-specific files
    The app now runs again

--- index.android.js
+++ index.android.js
@@ -1,51 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+import rootReducer from './src/reducers';
+import ZulipApp from './src/ZulipApp';
+
+// Set up middleware
+const middleware = [thunk];
+if (process.env.NODE_ENV === 'development') {
+  // Log states and actions to the console in dev mode
+  middleware.push(createLogger());
+}
+
+const store = applyMiddleware(...middleware)(createStore)(rootReducer);
 
 class ZulipNative extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        <ZulipApp />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ZulipNative', () => ZulipNative);
