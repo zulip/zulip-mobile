b59be797a0273526c42d9359e073a1c2e9f6dad0
Boris Yankov <borisyankov@gmail.com>
2016-09-27 01:27:39 +0300

    Log out plain objects even when immutable

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { persistStore, autoRehydrate } from 'redux-persist';
+import { Iterable } from 'immutable';
 
 import rootReducer from './reducers';
 
@@ -11,7 +12,15 @@ import rootReducer from './reducers';
 const middleware = [thunk];
 if (process.env.NODE_ENV === 'development') {
   // Log states and actions to the console in dev mode
-  middleware.push(createLogger());
+  middleware.push(createLogger({
+    stateTransformer: (state) => {
+      const newState = {};
+      for (const i of Object.keys(state)) {
+        newState[i] = Iterable.isIterable(state[i]) ? state[i].toJS() : state[i];
+      }
+      return newState;
+    },
+  }));
 }
 
 const store = compose(
@@ -20,7 +29,7 @@ const store = compose(
 )(createStore)(rootReducer);
 
 persistStore(store, {
-  config: { whitelist: 'user' },
+  config: { whitelist: 'auth' },
   storage: AsyncStorage,
 });
 
