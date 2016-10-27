fdcab7fc2462df9f2c20ceccd2c1cd7a9baff15d
Boris Yankov <borisyankov@gmail.com>
2016-10-27 05:59:31 +0300

    Better test descriptions for TextAvatar

--- src/common/__tests__/TextAvatar-test.js
+++ src/common/__tests__/TextAvatar-test.js
@@ -1,33 +1,37 @@
 import 'react-native';
 import { colorHashFromName, initialsFromName } from '../TextAvatar';
 
-test('colorHashFromName', () => {
-  const hash = colorHashFromName('John Doe');
-  expect(hash.length).toEqual(7);
+describe('colorHashFromName', () => {
+  test('returns a 6 digit hex number to use as a color ', () => {
+    const hash = colorHashFromName('John Doe');
+    expect(hash.length).toEqual(7);
+  });
+
+  test('produces the same output for the same input', () => {
+    const hash1 = colorHashFromName('John Doe');
+    const hash2 = colorHashFromName('John Doe');
+    expect(hash1).toEqual(hash2);
+  });
 });
 
-test('colorHashFromName', () => {
-  const hash1 = colorHashFromName('John Doe');
-  const hash2 = colorHashFromName('John Doe');
-  expect(hash1).toEqual(hash2);
-});
+describe('initialsFromName', () => {
+  test('a single name has a single letter initial', () => {
+    const initials = initialsFromName('John');
+    expect(initials).toEqual('J');
+  });
 
-test('initialsFromName', () => {
-  const initials = initialsFromName('John');
-  expect(initials).toEqual('J');
-});
+  test('two names result in two initials', () => {
+    const initials = initialsFromName('John Doe');
+    expect(initials).toEqual('JD');
+  });
 
-test('initialsFromName', () => {
-  const initials = initialsFromName('John Doe');
-  expect(initials).toEqual('JD');
-});
+  test('initials are always upper case', () => {
+    const initials = initialsFromName('small caps');
+    expect(initials).toEqual('SC');
+  });
 
-test('initialsFromName', () => {
-  const initials = initialsFromName('small caps');
-  expect(initials).toEqual('SC');
-});
-
-test('initialsFromName', () => {
-  const initials = initialsFromName('Jean-Pierre Doe');
-  expect(initials).toEqual('JD');
+  test('double names produce one initial', () => {
+    expect(initialsFromName('Jean-Pierre')).toEqual('J');
+    expect(initialsFromName('Mc\'Donald')).toEqual('M');
+  });
 });
