398a264192900d6eb5ba20b937c2460a857e2db6
Boris Yankov <borisyankov@gmail.com>
2016-10-30 17:06:20 +0200

    Autoscroll compose box, better button

--- src/compose/ComposeBox.js
+++ src/compose/ComposeBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   StyleSheet,
   View,
+  ScrollView,
   TextInput,
 } from 'react-native';
 
@@ -9,20 +10,22 @@ import SendButton from './SendButton';
 import { BORDER_COLOR } from '../common/styles';
 import ThreadBox from './ThreadBox';
 
+const MIN_HEIGHT = 38;
+const MAX_HEIGHT = 200;
+
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
+    flexDirection: 'row',
     borderTopWidth: 1,
     borderTopColor: BORDER_COLOR,
     backgroundColor: '#fff',
   },
   messageBox: {
     flex: 1,
-    flexDirection: 'row',
   },
   composeInput: {
     flex: 1,
-    padding: 8,
+    padding: 4,
     fontSize: 16,
   },
   sendButton: {
@@ -43,32 +46,37 @@ export default class ComposeBox extends React.Component {
     this.state = {
       editing: false,
       text: '',
+      contentHeight: MIN_HEIGHT,
     };
   }
 
   handleSend = () =>
     this.props.onSubmit(this.props.text);
 
+  handleContentSizeChange = (event) =>
+    this.setState({ contentHeight: event.nativeEvent.contentSize.height });
+
   handleChangeText = (text) =>
     this.setState({ text });
 
   render() {
-    const { text } = this.state;
-    const composeBoxHeight = { height: 44 };
+    const { contentHeight, text } = this.state;
+    const height = Math.min(Math.max(MIN_HEIGHT, contentHeight), MAX_HEIGHT);
 
     return (
       <View style={styles.container}>
         {this.state.editing && <ThreadBox />}
-        <View style={[styles.messageBox, composeBoxHeight]}>
+        <ScrollView style={[styles.messageBox, { height }]}>
           <TextInput
-            style={styles.composeInput}
+            style={[styles.composeInput]}
             multiline
+            height={contentHeight}
+            onContentSizeChange={this.handleContentSizeChange}
             onChangeText={this.handleChangeText}
             placeholder="Type a message here"
           />
-          {text.length > 0 &&
-            <SendButton onPress={this.handleSend} />}
-        </View>
+        </ScrollView>
+        <SendButton disabled={text.length === 0} onPress={this.handleSend} />
       </View>
     );
   }
