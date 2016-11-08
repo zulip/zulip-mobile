f2634c8a312db5905e362816250dd9f438baeb48
Boris Yankov <borisyankov@gmail.com>
2016-11-08 16:10:34 +0200

    Add numeric activity indicator to tabs

--- src/main/Tab.js
+++ src/main/Tab.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import {
   StyleSheet,
-  TouchableOpacity,
+  TouchableHighlight,
+  Text,
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import { BRAND_COLOR } from '../common/styles';
+import { HIGHLIGHT_COLOR } from '../common/styles';
 
 const styles = StyleSheet.create({
   tab: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: BRAND_COLOR,
   },
   activityCircle: {
     position: 'absolute',
-    top: 12,
-    left: 56,
-    backgroundColor: 'white',
-    borderRadius: 4,
-    width: 8,
-    height: 8,
+    top: 0,
+    left: 18,
+    backgroundColor: 'red',
+    borderRadius: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 14,
+    height: 14,
+  },
+  activityValue: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    fontSize: 8,
+    width: 16,
+    height: 10,
   },
 });
 
 export default class Tab extends React.Component {
 
   props: {
-    activity: boolean,
+    activity: number,
     index: number;
   }
 
@@ -40,17 +50,26 @@ export default class Tab extends React.Component {
     const { activity, icon } = this.props;
 
     return (
-      <TouchableOpacity
+      <TouchableHighlight
         style={styles.tab}
+        underlayColor={HIGHLIGHT_COLOR}
         onPress={this.handlePress}
       >
-        <Icon
-          name={icon}
-          size={22}
-          color="white"
-        />
-        {activity && <View style={styles.activityCircle} />}
-      </TouchableOpacity>
+        <View>
+          <Icon
+            name={icon}
+            size={22}
+            color="white"
+          />
+          {activity > 0 &&
+            <View style={styles.activityCircle}>
+              <Text style={styles.activityValue}>
+                {activity}
+              </Text>
+            </View>
+          }
+        </View>
+      </TouchableHighlight>
     );
   }
 }
