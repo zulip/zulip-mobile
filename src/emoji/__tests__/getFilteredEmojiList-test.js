0fcf0287f93a7befb95ba54ec94cd3627889a0f7
Boris Yankov <borisyankov@gmail.com>
2016-11-16 01:54:15 +0200

    Add emojis to autocomplete list

---
+++ src/emoji/__tests__/getFilteredEmojiList-test.js
@@ -0,0 +1,18 @@
+import getFilteredEmojiList from '../getFilteredEmojiList';
+
+describe('getFilteredEmojiList', () => {
+  test('empty query returns all emojis', () => {
+    const list = getFilteredEmojiList('');
+    expect(list.length).toEqual(1341);
+  });
+
+  test('non existing query returns empty list', () => {
+    const list = getFilteredEmojiList('qwerty');
+    expect(list.length).toEqual(0);
+  });
+
+  test('returns a sorted list of emojis starting with query', () => {
+    const list = getFilteredEmojiList('go');
+    expect(list).toEqual(['goat', 'golf', 'golfer']);
+  });
+});
