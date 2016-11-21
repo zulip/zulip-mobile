2af7c6331b443f1c0da8a5cf5c555a5c22499e38
Boris Yankov <borisyankov@gmail.com>
2016-11-21 19:22:45 +0200

    Replace InfiniteScrollView with WebView

--- src/stream/MessageList.js
+++ src/stream/MessageList.js
@@ -1,102 +1,87 @@
 import React from 'react';
 import {
   StyleSheet,
+  WebView,
 } from 'react-native';
 
-import InfiniteScrollView from './InfiniteScrollView';
-import StreamMessageHeader from '../message/headers/StreamMessageHeader';
-import PrivateMessageHeader from '../message/headers/PrivateMessageHeader';
-import MessageView from '../message/MessageView';
-import {
-  sameRecipient,
-  rewriteLink,
-} from '../utils/message';
-import { getAuthHeader } from '../api/apiFetch';
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#fff',
+  webview: {
+    borderWidth: 0,
   },
 });
 
 export default class MessageList extends React.PureComponent {
-
-  getHeader(item) {
-    if (item.type === 'stream') {
-      const subscription = this.props.subscriptions.find(x => x.get('name') === item.display_recipient);
-      return (
-        <StreamMessageHeader
-          key={`section_${item.id}`}
-          isPrivate={item.invite_only}
-          stream={item.display_recipient}
-          topic={item.subject}
-          color={subscription ? subscription.get('color') : '#ccc'}
-          item={item}
-          narrow={this.props.narrow}
-        />
-      );
-    } else if (item.type === 'private') {
-      return (
-        <PrivateMessageHeader
-          key={`section_${item.id}`}
-          recipients={item.display_recipient.filter(r =>
-            r.email !== this.props.auth.get('email')
-          )}
-          item={item}
-          narrow={this.props.narrow}
-        />
-      );
-    }
-    return undefined;
-  }
-
-  populateStream(items) {
-    const headerIndices = [];
-    const context = {
-      rewriteLink: (uri) => rewriteLink(
-        uri,
-        this.props.auth.get('realm'),
-        getAuthHeader(this.props.auth.get('email'), this.props.auth.get('apiKey')),
-      ),
-    };
-    let prevItem;
-    let totalIdx = 0;
-    for (const item of this.props.messages) {
-      if (!sameRecipient(prevItem, item)) {
-        items.push(this.getHeader(item));
-        headerIndices.push(totalIdx);
-        totalIdx++;
-      }
-      items.push(
-        <MessageView
-          key={item.id}
-          from={item.sender_full_name}
-          message={item.content}
-          timestamp={item.timestamp}
-          avatarUrl={item.avatar_url}
-          context={context}
-        />
-      );
-      totalIdx++;
-      prevItem = item;
-    }
-    return headerIndices;
-  }
-
   render() {
-    const stream = [];
-    const headerIndices = this.populateStream(stream);
+    console.log('MESSAGES', this.props.messages.toJS());
+    const { messages } = this.props;
+    const css = `
+<style>
+  a {
+    color: #08c;
+    text-decoration: none;
+  }
+  blockquote {
+    padding-left: 5px;
+    margin-left: 10px;
+    border-left-color: #ddd;
+  }
+  code {
+    font-size: .857em;
+    color: black;
+    background-color: #f7f7f9;
+    border-radius: 3px;
+    border: 1px solid #e1e1e8;
+    white-space: pre-wrap;
+    padding: 0 4px;
+    font-family: Monaco, Menlo, Consolas, "Courier New", monospace;
+  }
+  .message {
+    display: flex;
+    font-family: sans-serif;
+    word-wrap: break-word;
+  }
+  .avatar {
+    border-radius: 100px;
+    overflow: hidden;
+    margin-right: 10px;
+  }
+  .avatar, .avatar .img {
+    width: 35px;
+    height: 35px;
+  }
+  .content {
+    flex: 1;
+    background: 'red';
+  }
+  .sender_name {
+    font-weight: bold;
+  }
+  .user-mention {
+    background-color: #eee;
+    border-radius: 3px;
+    padding: 0 .2em;
+    box-shadow: 0 0 0 1px #ccc;
+  }
+</style>
+`;
+    const messagesHtml = messages.toJS().map(x => `
+<div class="message">
+  <div class="avatar">
+    <img src="${x.avatar_url}">
+  </div>
+  <div>
+    <div class="sender_name">
+      ${x.sender_full_name}
+    </div>
+    <div class="content">
+      ${x.content}
+    </div>
+  </div>
+</div>
+`);
+    const html = css + messagesHtml.join('\n');
     return (
-      <InfiniteScrollView
-        style={styles.scrollView}
-        automaticallyAdjustContentInset="false"
-        autoScrollToBottom={this.props.caughtUp}
-        stickyHeaderIndices={headerIndices}
-        onStartReached={this.props.fetchOlder}
-        onEndReached={this.props.fetchNewer}
-      >
-        {stream}
-      </InfiniteScrollView>
+      <WebView source={{ html }} bounces={false} style={styles.webview} />
     );
   }
 }
