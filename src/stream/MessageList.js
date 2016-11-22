67919cc93b6124e07881df0aeb3692c3f99baa21
Boris Yankov <borisyankov@gmail.com>
2016-11-22 20:54:46 +0200

    Refactor message headers

--- src/stream/MessageList.js
+++ src/stream/MessageList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import InfiniteScrollView from './InfiniteScrollView';
-import StreamMessageHeader from '../message/headers/StreamMessageHeader';
-import PrivateMessageHeader from '../message/headers/PrivateMessageHeader';
+import MessageHeader from '../message/headers/MessageHeader';
 import MessageContainer from '../message/MessageContainer';
 import {
   sameRecipient,
@@ -11,36 +10,6 @@ import {
 import { getAuthHeader } from '../api/apiFetch';
 
 export default class MessageList extends React.PureComponent {
-
-  getHeader(item) {
-    if (item.type === 'stream') {
-      const subscription = this.props.subscriptions.find(x => x.get('name') === item.display_recipient);
-      return (
-        <StreamMessageHeader
-          key={`section_${item.id}`}
-          isPrivate={item.invite_only}
-          stream={item.display_recipient}
-          topic={item.subject}
-          color={subscription ? subscription.get('color') : '#ccc'}
-          item={item}
-          narrow={this.props.narrow}
-        />
-      );
-    } else if (item.type === 'private') {
-      return (
-        <PrivateMessageHeader
-          key={`section_${item.id}`}
-          recipients={item.display_recipient.filter(r =>
-            r.email !== this.props.auth.get('email')
-          )}
-          item={item}
-          narrow={this.props.narrow}
-        />
-      );
-    }
-
-    return null;
-  }
 
   populateStream(items) {
     const headerIndices = [];
@@ -55,7 +24,15 @@ export default class MessageList extends React.PureComponent {
     let totalIdx = 0;
     for (const item of this.props.messages) {
       if (!sameRecipient(prevItem, item)) {
-        items.push(this.getHeader(item));
+        items.push(
+          <MessageHeader
+            key={`header${item.id}`}
+            item={item}
+            auth={this.props.auth}
+            subscriptions={this.props.subscriptions}
+            narrow={this.props.narrow}
+          />
+        );
         headerIndices.push(totalIdx);
         totalIdx++;
       }
