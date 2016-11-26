867341495949b6d2381557ed1524923e91fc4313
Boris Yankov <borisyankov@gmail.com>
2016-11-26 18:44:56 +0200

    Simplify MessageList, add tests for message utils

---
+++ src/utils/__tests__/message-test.js
@@ -0,0 +1,85 @@
+import 'react-native';
+import { normalizeRecipients, sameRecipient, rewriteLink } from '../message';
+
+describe('normalizeRecipients', () => {
+  test('joins emails from recipients, sorted, trimmed, not including missing ones', () => {
+    const recipients = [
+      { email: '' },
+      { email: 'abc@example.com' },
+      { email: 'xyz@example.com' },
+      { email: '  def@example.com  ' },
+    ];
+    const expectedResult = 'abc@example.com,def@example.com,xyz@example.com';
+
+    const normalized = normalizeRecipients(recipients);
+
+    expect(normalized).toEqual(expectedResult);
+  });
+});
+
+describe('sameRecipient', () => {
+  test('passing undefined as any of parameters means recipients are not the same', () => {
+    expect(sameRecipient(undefined, {})).toBe(false);
+    expect(sameRecipient({}, undefined)).toBe(false);
+    expect(sameRecipient(undefined, undefined)).toBe(false);
+  });
+
+  test('recipient types are compared first, if they differm recipients differ', () => {
+    expect(sameRecipient({ type: 'private' }, { type: 'stream' })).toBe(false);
+  });
+
+  test('recipient of unknown types are never the same', () => {
+    expect(sameRecipient({ type: 'someUnknown' }, { type: 'someUnknown' })).toBe(false);
+  });
+
+  test('recipients are same for private type if display_recipient match in any order', () => {
+    const msg1 = {
+      type: 'private',
+      display_recipient: [
+        { email: 'abc@example.com' },
+        { email: 'xyz@example.com' },
+      ],
+    };
+    const msg2 = {
+      type: 'private',
+      display_recipient: [
+        { email: 'xyz@example.com' },
+        { email: 'abc@example.com' },
+      ],
+    };
+    expect(sameRecipient(msg1, msg2)).toBe(true);
+  });
+
+  test('recipients are same for stream type if display_recipient and subject match', () => {
+    const msg1 = {
+      type: 'stream',
+      display_recipient: 'abc',
+      subject: 'def',
+    };
+    const msg2 = {
+      type: 'stream',
+      display_recipient: 'abc',
+      subject: 'def',
+    };
+    expect(sameRecipient(msg1, msg2)).toBe(true);
+  });
+});
+
+describe('rewriteLink', () => {
+  test('when uri contains domain, do not change', () => {
+    const expectedResult = { uri: 'https://example.com/img.gif' };
+    const context = rewriteLink('https://example.com/img.gif', '', '');
+    expect(context).toEqual(expectedResult);
+  });
+
+  test('when uri does not contain domain, append realm', () => {
+    const expectedResult = {
+      uri: 'https://example.com/img.gif',
+      headers: {
+        Authorization: 'someAuthHead',
+      },
+    };
+    const context = rewriteLink('/img.gif', 'https://example.com', 'someAuthHead');
+    expect(context).toEqual(expectedResult);
+  });
+});
