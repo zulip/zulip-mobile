1d212a976fb20d6a5577045fef840d5baf71d8a4
Boris Yankov <borisyankov@gmail.com>
2016-12-07 01:05:59 +0200

    Add more narrow types. Add isNarrow functions. Add tests

--- src/utils/narrow.js
+++ src/utils/narrow.js
@@ -1,47 +1,66 @@
-export const homeNarrow = {};
+export const homeNarrow = () => [];
 
-export const mentionedNarrow = [{
-  negated: false,
-  operator: 'is',
-  operand: 'mentioned',
+export const isHomeNarrow = (narrow: []) =>
+  narrow.length === 0;
+
+export const privateNarrow = (email: string) => [{
+  operator: 'pm-with',
+  operand: email,
 }];
 
-export const privateNarrow = (emails) => {
-  if (!emails) {
-    return [{
-      negated: false,
-      operator: 'is',
-      operand: 'private',
-    }];
-  }
-  return [{
-    negated: false,
-    operator: 'pm-with',
-    operand: emails.join(),
-  }];
-};
+export const isPrivateNarrow = (narrow: []) =>
+  narrow.length === 1 &&
+  narrow[0].operator === 'pm-with' &&
+  narrow[0].operand.indexOf(',') === -1;
 
-export const starredNarrow = [{
-  negated: false,
-  operator: 'is',
-  operand: 'starred',
+export const groupNarrow = (emails: string[]) => [{
+  operator: 'pm-with',
+  operand: emails.join(),
 }];
+
+export const isGroupNarrow = (narrow: []) =>
+  narrow.length === 1 &&
+  narrow[0].operator === 'pm-with' &&
+  narrow[0].operand.indexOf(',') >= 0;
+
+export const specialNarrow = (operand: string) => [{
+  operator: 'is',
+  operand,
+}];
+
+export const isSpecialNarrow = (narrow: []) =>
+  narrow.length === 1 &&
+  narrow[0].operator === 'is';
 
 export const streamNarrow = (stream) => [{
-  negated: false,
   operator: 'stream',
   operand: stream,
 }];
 
-export const topicNarrow = (stream, topic) => [
+export const isStreamNarrow = (narrow: []) =>
+  narrow.length === 1 &&
+  narrow[0].operator === 'stream';
+
+export const topicNarrow = (stream: string, topic: string) => [
   {
-    negated: false,
     operator: 'stream',
     operand: stream,
   },
   {
-    negated: false,
     operator: 'topic',
     operand: topic,
   },
 ];
+
+export const isTopicNarrow = (narrow: []) =>
+  narrow.length === 2 &&
+  narrow[1].operator === 'topic';
+
+export const searchNarrow = (query: string) => [{
+  operator: 'search',
+  operand: query,
+}];
+
+export const isSearchNarrow = (narrow: []) =>
+  narrow.length === 1 &&
+  narrow[0].operator === 'search';
