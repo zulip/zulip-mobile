a5fdf7ca144f86f14cb037b9e31fdbfb8ea20e6d
Boris Yankov <borisyankov@gmail.com>
2016-12-15 12:00:09 +0200

    Send userAgent string with api requests

--- src/utils/userAgent.js
+++ src/utils/userAgent.js
@@ -1,6 +1,8 @@
 import { NativeModules } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
-const userAgent = NativeModules.RNDeviceInfo ? `ZulipMobile ${DeviceInfo.getVersion()} (${DeviceInfo.getSystemName()} ${DeviceInfo.getSystemVersion()})` : '';
+const { getVersion, getSystemName, getSystemVersion } = DeviceInfo;
 
-export default userAgent;
+export default !NativeModules.RNDeviceInfo ?
+  '' :
+  `ZulipMobile ${getVersion()} (${getSystemName()} ${getSystemVersion()})`;
