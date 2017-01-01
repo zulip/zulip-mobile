c100c939b41a0ac074ff83d2e41503d39ade0bc4
Boris Yankov <borisyankov@gmail.com>
2017-01-01 22:18:33 +0200

    Configure google-signin for Android

--- android/app/src/main/java/com/zulipmobile/MainApplication.java
+++ android/app/src/main/java/com/zulipmobile/MainApplication.java
@@ -13,6 +13,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -25,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNDeviceInfo()
+          new RNDeviceInfo(),
+          new RNGoogleSigninPackage()
       );
     }
   };
