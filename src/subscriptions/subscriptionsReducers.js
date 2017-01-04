731ebefacf256f4f588de6003b6f93fbb5ad7ff3
Boris Yankov <borisyankov@gmail.com>
2017-01-04 20:34:58 +0200

    Understand stream update events

--- src/subscriptions/subscriptionsReducers.js
+++ src/subscriptions/subscriptionsReducers.js
@@ -1,5 +1,7 @@
 import {
   REALM_INIT,
+  EVENT_SUBSCRIPTION_ADD,
+  EVENT_SUBSCRIPTION_REMOVE,
 } from '../constants';
 
 const initialState = [];
@@ -8,6 +10,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case REALM_INIT:
       return action.data.subscriptions;
+    case EVENT_SUBSCRIPTION_ADD:
+      return state;
+    case EVENT_SUBSCRIPTION_REMOVE:
+      return state;
     default:
       return state;
   }
