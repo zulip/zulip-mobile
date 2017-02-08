2af42cffc084bc5162e33565834b37bfd673b805
Boris Yankov <borisyankov@gmail.com>
2017-02-08 11:12:21 +0200

    Fix links and image links (#285)
    Fixes #281

--- src/message/html/HtmlTagA.js
+++ src/message/html/HtmlTagA.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Linking, Text } from 'react-native';
 
+import { getFullUrl } from '../../utils/url';
 import renderHtmlChildren from './renderHtmlChildren';
 
 export default ({ href, auth, childrenNodes, cascadingStyle, onPress }) => (
-  <Text onPress={() => Linking.openURL(href)}>
+  <Text onPress={() => Linking.openURL(getFullUrl(href, auth.realm))}>
     {renderHtmlChildren({ childrenNodes, auth, cascadingStyle })}
   </Text>
 );
