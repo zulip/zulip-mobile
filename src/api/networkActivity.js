fde79245e4dc8fb3af2fb25ea1415a3c92240ad3
Boris Yankov <borisyankov@gmail.com>
2017-02-18 19:21:40 +0200

    Separate networkActivity code from apiFetch

---
+++ src/api/networkActivity.js
@@ -0,0 +1,23 @@
+import { StatusBar, Platform } from 'react-native';
+
+// Network activity indicators should be visible if *any* network activity is occurring
+
+let activityCounter = 0;
+
+export const networkActivityStart = (isSilent: boolean) => {
+  if (isSilent) return;
+
+  activityCounter++;
+  if (Platform.OS === 'ios') {
+    StatusBar.setNetworkActivityIndicatorVisible(true);
+  }
+};
+
+export const networkActivityStop = (isSilent: boolean) => {
+  if (isSilent) return;
+
+  activityCounter--;
+  if (activityCounter === 0 && Platform.OS === 'ios') {
+    StatusBar.setNetworkActivityIndicatorVisible(false);
+  }
+};
