0b639e42ed0ef87af96559496a6d2d914fa914ec
Boris Yankov <borisyankov@gmail.com>
2017-03-01 12:25:51 +0200

    Simplify middleware creation and JSC detection

---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import { REHYDRATE } from 'redux-persist/constants';
+import createActionBuffer from 'redux-action-buffer';
+
+const middleware = [
+  thunk,
+  createActionBuffer(REHYDRATE),
+];
+
+// Only enable remote debugging! It really slows down the iOS JSC
+// Checking for btoa() is not available in JSC
+const enableLogging = process.env.NODE_ENV === 'development' && !!global.btoa;
+
+if (enableLogging) {
+  middleware.push(createLogger());
+}
+
+export default middleware;
--- src/store.js
+++ src/store.js
@@ -1,25 +1,11 @@
 import { AsyncStorage } from 'react-native';
 import { applyMiddleware, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import { persistStore, autoRehydrate } from 'redux-persist';
-import { REHYDRATE } from 'redux-persist/constants';
-import createActionBuffer from 'redux-action-buffer';
 
 import rootReducer from './reducers';
+import middleware from './middleware';
 
 // AsyncStorage.clear(); // use to reset storage during development
-
-// Only enable remote debugging! It really slows down the iOS JSC
-// Checking for btoa() is not available in JSC
-const enableLogging = process.env.NODE_ENV === 'development' && global.btoa;
-
-// Set up middleware
-const middleware = [thunk, createActionBuffer(REHYDRATE)];
-
-if (enableLogging) {
-  middleware.push(createLogger());
-}
 
 const store = compose(
   autoRehydrate(),
