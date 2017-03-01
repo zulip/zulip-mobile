6c0e52a7fdf0beff1bba45af8475c063a63d936d
Boris Yankov <borisyankov@gmail.com>
2017-03-01 11:40:27 +0200

    Simplify check for JSC

--- src/store.js
+++ src/store.js
@@ -10,22 +10,15 @@ import rootReducer from './reducers';
 
 // AsyncStorage.clear(); // use to reset storage during development
 
+// Only enable remote debugging! It really slows down the iOS JSC
+// Checking for btoa() is not available in JSC
+const enableLogging = process.env.NODE_ENV === 'development' && global.btoa;
+
 // Set up middleware
 const middleware = [thunk, createActionBuffer(REHYDRATE)];
-if (process.env.NODE_ENV === 'development') {
-  // Log states and actions to the console in dev mode
-  // But only when remote debugging! It really slows down the iOS JSC
-  let runningInChrome = false;
-  try {
-    // btoa is not available in JSC so we can use it to figure out if we're
-    // running in Chrome remote debugging or not
-    runningInChrome = !!btoa;
-  } catch (err) {
-    // pass
-  }
-  if (runningInChrome) {
-    middleware.push(createLogger());
-  }
+
+if (enableLogging) {
+  middleware.push(createLogger());
 }
 
 const store = compose(
