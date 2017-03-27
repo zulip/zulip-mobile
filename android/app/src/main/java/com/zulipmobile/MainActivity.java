f8c0fe7b4e9e7db0fb6ace8dda55e040264f3a38
Vishwesh <vishwesh03071998@gmail.com>
2017-03-27 21:31:06 +0530

    Register Android app to receive text data.
    And send it to react component through props.

--- android/app/src/main/java/com/zulipmobile/MainActivity.java
+++ android/app/src/main/java/com/zulipmobile/MainActivity.java
@@ -1,6 +1,12 @@
 package com.zulipmobile;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import com.smixx.fabric.FabricPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 
@@ -14,4 +20,32 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "ZulipMobile";
     }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Nullable
+            @Override
+            protected Bundle getLaunchOptions() {
+                Bundle initialProps = new Bundle();
+                initialProps.putString("data", handleSendText(getIntent()));
+                return initialProps;
+            }
+        };
+    }
+
+    /**
+     *
+     * @param intent to this activity
+     * @return text which is send through intent
+     */
+    private String handleSendText(Intent intent) {
+        // Get the text from intent
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            return sharedText;
+        }
+        return  "";
+    }
+
 }
