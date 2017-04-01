3af94d3e25981298ba8ceaeb35500c11ee7fae11
vihanggodbole <vihang16091996@gmail.com>
2017-04-01 11:14:30 +0530

    create separate module for tutorial

--- src/nav/routingSelectors.js
+++ src/nav/routingSelectors.js
@@ -8,31 +8,32 @@ export const getInitialRoutes = (accounts: any[]): string => {
   }
 
   if (accounts.length > 1) return ['account'];
-  if (checkFirstLaunch() ==  true) return ['tutorial'];
+  if (checkFirstLaunch() === true) return ['tutorial'];
   return ['realm'];
 };
 
-export const getCurrentRoute = (state) =>
-  state.nav.routes[state.nav.index];
+export const getCurrentRoute = state => state.nav.routes[state.nav.index];
 
-checkFirstLaunch = () => {
-	let repo = new Realm({
-		schema: [{
-			name: 'FirstLaunch',
-			primaryKey: 'id',
-			properties: {
-				id: 'int',
-				value: {type: 'bool'},
-			}
-		}]
-	});
-	const firstLaunch = repo.objectForPrimaryKey('FirstLaunch', 1);
-	if(firstLaunch === undefined){
-		repo.write(() => {
-			repo.create('FirstLaunch', { id: 1, value: true, });
-  		});
-  	return true;
+const checkFirstLaunch = () => {
+  const repo = new Realm({
+    schema: [
+      {
+        name: 'FirstLaunch',
+        primaryKey: 'id',
+        properties: {
+          id: 'int',
+          value: { type: 'bool' }
+        }
+      }
+    ]
+  });
+  const firstLaunch = repo.objectForPrimaryKey('FirstLaunch', 1);
+  if (firstLaunch === undefined) {
+    repo.write(() => {
+      repo.create('FirstLaunch', { id: 1, value: true });
+    });
+    return true;
   } else {
-  	return false;
+    return false;
   }
 };
--- src/start/TutorialScreen.js
+++ src/start/TutorialScreen.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { View } from 'react-native';
 import AppIntro from 'react-native-app-intro';
 import boundActions from '../boundActions';
+import { tutorialPages } from '../tutorial/TutorialPages';
 
 class TutorialScreen extends React.Component {
 
@@ -22,51 +23,6 @@ class TutorialScreen extends React.Component {
   }
 
   render() {
-    const pageArray = [{
-      title: 'Zulip',
-      description: 'A powerful open source group chat app.',
-      backgroundColor: '#a4b602',
-      fontColor: '#fff',
-      level: 10,
-      img: require('../../assets/zulip_logo.png'),
-      imgStyle: {
-        height: 500 * 0.4,
-        width: 500 * 0.4,
-      },
-    }, {
-      title: 'Streams',
-      description: 'Chatrooms that you can join, leave or even make. Messages in Zulip go to a stream and have a topic.',
-      backgroundColor: '#76b600',
-      fontColor: '#fff',
-      level: 10,
-      img: require('../../assets/streams.png'),
-      imgStyle: {
-        height: 469 * 0.375,
-        width: 844 * 0.375,
-      },
-    }, {
-      title: 'Topics',
-      description: 'Every conversation in Zulip has a topic, so it’s easy to keep conversations straight. One or two words that describe what it\'s all about.',
-      backgroundColor: '#10b601',
-      fontColor: '#fff',
-      level: 10,
-      img: require('../../assets/topics.png'),
-      imgStyle: {
-        height: 518 * 0.375,
-        width: 844 * 0.375,
-      },
-    }, {
-      title: 'And everything else...',
-      description: 'Fast and powerful search, group private messages, audible notifications, missed-message emails, desktop apps and even emojis!',
-      backgroundColor: '#1f5133',
-      fontColor: '#fff',
-      level: 10,
-      img: require('../../assets/everything_else.png'),
-      imgStyle: {
-        height: 649 * 0.375,
-        width: 844 * 0.375,
-      },
-    }];
     return (
       <View>
         <AppIntro
@@ -74,7 +30,7 @@ class TutorialScreen extends React.Component {
           onDoneBtnClick={this.doneBtnHandle}
           onSkipBtnClick={this.onSkipBtnHandle}
           onSlideChange={this.onSlideChangeHandle}
-          pageArray={pageArray}
+          pageArray={tutorialPages}
         />
       </View>
     );
