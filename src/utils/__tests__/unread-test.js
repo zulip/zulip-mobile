7ed5712f8aad0d6bd2981ca84da3bbc56ef49d43
Boris Yankov <borisyankov@gmail.com>
2017-04-07 01:06:19 +0300

    Add function to count unread messages. Add tests

--- src/utils/__tests__/unread-test.js
+++ src/utils/__tests__/unread-test.js
@@ -1,4 +1,4 @@
-import { filterUnreadMessageIds } from '../unread';
+import { countUnread, filterUnreadMessageIds } from '../unread';
 
 describe('filterUnreadMessageIds', () => {
   test('empty message list has no unread messages', () => {
@@ -33,5 +33,64 @@ describe('filterUnreadMessageIds', () => {
     const actualUnread = filterUnreadMessageIds(messages, flags);
 
     expect(actualUnread).toEqual(expectedUnread);
+  });
+});
+
+describe('countUnread', () => {
+  test('for an empty message list the unread count is zero', () => {
+    const messages = [];
+    const flags = {};
+
+    const actualCount = countUnread(messages, flags);
+
+    expect(actualCount).toEqual(0);
+  });
+
+  test('when no read flags are set, every message is unread', () => {
+    const messages = [1, 2, 3];
+    const flags = {};
+
+    const actualCount = countUnread(messages, flags);
+
+    expect(actualCount).toEqual(3);
+  });
+
+  test('every message present in read flags is not counted', () => {
+    const messages = [1, 2, 3];
+    const flags = {
+      1: true,
+      2: true,
+    };
+
+    const actualCount = countUnread(messages, flags);
+
+    expect(actualCount).toEqual(1);
+  });
+
+  test('counts all unread from specified message id to the end', () => {
+    const messages = [1, 2, 3, 4, 5];
+    const flags = {};
+
+    const actualCount = countUnread(messages, flags, 3);
+
+    expect(actualCount).toEqual(3);
+  });
+
+  test('counts all unread from beginning to given message id', () => {
+    const messages = [1, 2, 3, 4, 5];
+    const flags = {};
+
+    const actualCount = countUnread(messages, flags, -1, 4);
+
+    expect(actualCount).toEqual(4);
+  });
+
+  test('counts all unread from given message id to another id', () => {
+    const messages = [1, 2, 3, 4, 5, 6, 7, 8];
+    const flags = {};
+
+    const actualCount = countUnread(messages, flags, 3, 6);
+
+    expect(actualCount).toEqual(4);
   });
 });
