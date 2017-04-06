592853ca49dfb39318b1b4737f891291e8648928
Boris Yankov <borisyankov@gmail.com>
2017-04-07 01:06:19 +0300

    Add function to count unread messages. Add tests

--- src/utils/unread.js
+++ src/utils/unread.js
@@ -2,3 +2,29 @@ import { Message } from '../types';
 
 export const filterUnreadMessageIds = (messageIds: number[], flags: Object): Message[] =>
   messageIds.filter(msgId => !flags || !flags.read || !flags.read[msgId]);
+
+export const countUnread = (
+  messageIds: number[],
+  readFlags: Object,
+  fromId: number = -1,
+  toId: number = -1,
+): number => {
+  let count = 0;
+  // count starts from the beginning if fromId === -1
+  // and continues to the end if toId === -1
+  let started = fromId === -1;
+
+  for (let i = 0; i < messageIds.length; i++) {
+    if (messageIds[i] === fromId) {
+      started = true;
+    }
+    if (started && !readFlags[messageIds[i]]) {
+      count++;
+    }
+    if (messageIds[i] === toId) {
+      break;
+    }
+  }
+
+  return count;
+};
