8e767fac4e8d3769a6546fb4ecca87bd889a4ab2
suplex.saket@gmail.com <saket.singh1410@gmail.com>
2017-04-16 03:24:10 +0530

    Entire button is selectable

--- src/common/Touchable.android.js
+++ src/common/Touchable.android.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableNativeFeedback, Platform } from 'react-native';
+import { TouchableNativeFeedback, Platform, TouchableOpacity } from 'react-native';
 import { HIGHLIGHT_COLOR } from './styles';
 
 const background = Platform.Version >= 21 ?
@@ -7,10 +7,10 @@ const background = Platform.Version >= 21 ?
   TouchableNativeFeedback.SelectableBackground();
 
 export default (props) => (
-  <TouchableNativeFeedback
+  <TouchableOpacity
     background={background}
     {...props}
   >
     {props.children}
-  </TouchableNativeFeedback>
+  </TouchableOpacity>
 );
