3f29b238ae594f27ee68770525f46c111fd254a9
Neeraj Wahi <neeraj.wahi@gmail.com>
2017-05-12 21:49:57 -0700

    Add OAuth functionality (iOS only).
    This diff enables OAuth based authentication with the Zulip server. A one time pad is generated and sent to the server. The server processes the OAuth request inside of an embedded browser view and returns control to the app via a custom URI scheme (zulip://). The response contains the authenticated API key XORed with the one time pad in order to prevent URI hijacking.

--- src/start/AuthScreen.js
+++ src/start/AuthScreen.js
@@ -1,24 +1,115 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { Linking, Platform, ScrollView, View } from 'react-native';
 import { connect } from 'react-redux';
+import DeviceInfo from 'react-native-device-info';
+
+import SafariView from 'react-native-safari-view';
+import parseURL from 'url-parse';
 
 import boundActions from '../boundActions';
 import styles from '../styles';
-import { RawLabel, Screen, ZulipButton } from '../common';
+import { RawLabel, Screen, ZulipButton, GoogleButton } from '../common';
+import { generateOtp, extractApiKey } from '../utils/encoding';
 import { getAuth } from '../account/accountSelectors';
-import { getCurrentRoute } from '../nav/routingSelectors';
 
 import PasswordAuthView from './PasswordAuthView';
 
-class AuthScreen extends React.Component {
+class AuthScreen extends React.PureComponent {
 
   props: {
     authBackends: string[],
   };
 
+  componentDidMount = () => {
+    // Add listeners for OAuth flow
+    Linking.addEventListener('url', this.endOAuthFlow);
+    this.safariViewDismissEvent = SafariView.addEventListener('onDismiss', () => {
+      this.otp = undefined;
+    });
+  }
+
+  componentWillUnmount = () => {
+    // Remove listeners for OAuth flow
+    Linking.removeEventListener('url', this.endOAuthFlow);
+    SafariView.removeEventListener('onDismiss', this.safariViewDismissEvent);
+  }
+
   handleTypeSelect = (authType: string) => {
-    this.props.setAuthType(authType);
-    this.props.pushRoute(authType);
+    const { realm } = this.props;
+
+    if (authType === 'google') {
+      // Google OAuth flow
+      this.beginOAuthFlow(`${realm}/accounts/login/google/`);
+    } else if (authType === 'github') {
+      // Github OAuth flow
+      this.beginOAuthFlow(`${realm}/accounts/login/social/github/`);
+    } else {
+      // Password auth flow
+      this.props.setAuthType(authType);
+      this.props.pushRoute(authType);
+    }
+  }
+
+  beginOAuthFlow = async (url) => {
+    // Generate a one time pad (OTP) to send up with the request
+    // The server XORs the API key with this OTP in its response to protect
+    // against malicious apps registering an identical URI scheme and trying
+    // to intercept credentials
+    this.otp = await generateOtp();
+    SafariView.show({ url: `${url}?mobile_flow_otp=${this.otp}` });
+  };
+
+  endOAuthFlow = (event) => {
+    const { realm, loginSuccess } = this.props;
+
+    SafariView.dismiss();
+
+    const url = parseURL(event.url, true);
+
+    // The OAuth callback should have the following format:
+    // zulip://login?realm={}&email={}&otp_encrypted_api_key={}
+    if (url.host === 'login') {
+      const query = url.query;
+
+      // Check for errors
+      if (query.realm !== realm) {
+        // eslint-disable-next-line no-console
+        console.error('Zulip realm does not match request.');
+        return;
+      }
+
+      if (!this.otp) {
+        // eslint-disable-next-line no-console
+        console.error('No one time pad stored. This auth redirect may not have been requested.');
+        return;
+      }
+
+      if (!query.email || !query.otp_encrypted_api_key) {
+        // eslint-disable-next-line no-console
+        console.error('No credentials returned in server response.');
+        return;
+      }
+
+      if (query.otp_encrypted_api_key.length !== this.otp.length) {
+        // eslint-disable-next-line no-console
+        console.error('API key in server response has the wrong length.');
+        return;
+      }
+
+      // If we've made it this far we have a valid API key
+      const apiKey = extractApiKey(query.otp_encrypted_api_key, this.otp);
+
+      // Reset stored one time pad
+      this.otp = undefined;
+
+      // Add the authenticated account
+      loginSuccess(realm, query.email, apiKey);
+    }
+  }
+
+  shouldShowOAuth = () => {
+    // OAuth flow only supports iOS 9+ right now
+    return Platform.OS === 'ios' && DeviceInfo.getSystemVersion() >= 9.0;
   }
 
   render() {
@@ -43,7 +134,11 @@ class AuthScreen extends React.Component {
               />
             }
             {authBackends.includes('password') && <PasswordAuthView />}
-
+            {authBackends.includes('google') && this.shouldShowOAuth() &&
+              <GoogleButton
+                onPress={() => this.handleTypeSelect('google')}
+              />
+            }
           </View>
         </ScrollView>
       </Screen>
@@ -53,7 +148,6 @@ class AuthScreen extends React.Component {
 
 export default connect(
   (state) => ({
-    authBackends: getCurrentRoute(state).data || [],
     realm: getAuth(state).realm,
   }),
   boundActions,
---
+++ src/utils/encoding.js
@@ -0,0 +1,43 @@
+import base64 from 'base-64';
+import { NativeModules } from 'react-native';
+
+export const xorHexStrings = (hex1, hex2) => {
+  if (hex1.length !== hex2.length) {
+    throw new Error('Both inputs must have the same length.');
+  }
+
+  return hex1.split('').map((char, i) =>
+    (parseInt(hex1[i], 16) ^ parseInt(hex2[i], 16)).toString(16)
+    ).join('').toUpperCase();
+};
+
+export const hexToAscii = (hex) => {
+  let ascii = '';
+  for (let i = 0; i < hex.length; i += 2) {
+    ascii += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return ascii;
+};
+
+export const asciiToHex = (ascii) =>
+  ascii.split('').map((char) =>
+    (`0${char.charCodeAt(0).toString(16)}`).slice(-2)
+  ).join('').toUpperCase();
+
+export const base64ToHex = (bytes) =>
+  asciiToHex(base64.decode(bytes));
+
+export const hexToBase64 = (hex) =>
+  base64.encode(hexToAscii(hex));
+
+// Returns a one time pad (OTP) as a hex string
+// Uses the native platform random number generator
+export const generateOtp = async () => {
+  const rand = await NativeModules.UtilManager.randomBase64(32);
+  return base64ToHex(rand);
+};
+
+// Extract an API key encoded as a hex string XOR'ed with a one time pad (OTP)
+// (this is used during the OAuth flow)
+export const extractApiKey = (encoded, otp) =>
+  hexToAscii(xorHexStrings(encoded, otp));
