768dba9372a25e6372db77f6bcbf556a812558c2
kunall17 <kunall.gupta17@gmail.com>
2017-05-23 16:30:39 -0700

    Add integrations tests in javascript

---
+++ __tests__/ui-tests.js
@@ -0,0 +1,44 @@
+import wd from 'wd';
+
+const port = 4723;
+const driver = wd.promiseChainRemote('localhost', port);
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+const caps = {
+  'platformName': 'iOS',
+  'app': `${process.cwd()}/ios/build/Build/Products/Release-iphonesimulator/ZulipMobile.app`,
+  platformVersion: '10.3',
+  deviceName: 'iPhone Simulator',
+};
+
+const options = {
+  validEmail: 'iago@zulip.com',
+  inValidEmail: 'invalid@zulip.com',
+  validServer: 'http://localhost:9991/',
+  inValidServer: 'http://invalid:9991'
+};
+
+describe('Integration testings', () => {
+  beforeAll(async () => await driver.init(caps));
+  afterAll(async () => await driver.quit());
+
+  afterEach(async () => {
+    await driver.closeApp();
+    await driver.launchApp();
+  });
+
+  it('Test to connect to server', async () => {
+    await driver.elementByClassName('XCUIElementTypeTextField').sendKeys(options.validServer);
+    await driver.elementByAccessibilityId('Enter').click();
+    expect(await driver.hasElementByAccessibilityId('Sign in')).toBe(true);
+  });
+
+  it('Test to sign in using dev server', async () => {
+    await driver.elementByClassName('XCUIElementTypeTextField').sendKeys(options.validServer);
+    await driver.elementByAccessibilityId('Enter').click();
+    await driver.elementByAccessibilityId('Sign in with dev account').click();
+    await driver.elementByAccessibilityId(options.validEmail).click();
+    expect(await driver.waitForElementByAccessibilityId('Home', 10000).text()).toEqual('Home');
+  });
+});
