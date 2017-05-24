bb20c2e525df68e883dfc69e316593bd52057164
Tomasz Kolek <tomasz-kolek@o2.pl>
2017-05-24 04:06:04 +0200

    Fix flow static typing for src/emoji.

--- src/emoji/Emoji.js
+++ src/emoji/Emoji.js
@@ -1,3 +1,4 @@
+/* @flow */
 import { createIconSet } from 'react-native-vector-icons';
 import emojiMap from './emojiMap';
 
--- src/emoji/getFilteredEmojiList.js
+++ src/emoji/getFilteredEmojiList.js
@@ -1,3 +1,4 @@
+/* @flow */
 import emojiMap from './emojiMap';
 
 export default (query: string) =>
