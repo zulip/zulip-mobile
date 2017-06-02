f1818be1d752c7220fc9dd0c2f4753eab8c7f587
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-06-02 17:03:18 +0530

    Apply bold, italic, links, strikethrough styles in messages.
    Fix:#642

--- src/html/HtmlNodeText.js
+++ src/html/HtmlNodeText.js
@@ -1,14 +1,17 @@
 /* @flow */
 import React from 'react';
 import entities from 'entities';
+import type { StyleObj } from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 import { RawLabel } from '../common';
 
 type Props = {
   data: string,
+  cascadingTextStyle: StyleObj,
 };
 
-export default ({ data }: Props) => (
+export default ({ data, cascadingTextStyle }: Props) => (
   <RawLabel
+    style={cascadingTextStyle}
     text={entities.decodeHTML(data).replace(/\n$/, '')}
   />
 );
--- src/html/tags/HtmlTagA.js
+++ src/html/tags/HtmlTagA.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import styles from '../HtmlStyles';
 import { Touchable } from '../../common';
 import renderHtmlChildren from '../renderHtmlChildren';
 
-export default ({ onPress, href, ...restProps }) => (
-  <Touchable onPress={() => onPress(href)}>
+export default ({ onPress, href, cascadingStyle, ...restProps }) => (
+  <Touchable style={[styles.a, cascadingStyle]} onPress={() => onPress(href)}>
     {renderHtmlChildren({ href, ...restProps })}
   </Touchable>
 );
