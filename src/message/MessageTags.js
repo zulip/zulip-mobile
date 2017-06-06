e892f7dc5cc125276092b5ae6f9e793ce40d527c
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-06-06 14:39:57 +0530

    Use separate component for 'edited' and 'starred'.
    - Used Label for 'edited' and 'starred'.
    - Used RawLabel for time.

--- src/message/MessageTags.js
+++ src/message/MessageTags.js
@@ -2,38 +2,42 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 
-import { Label } from '../common';
+import { Label, RawLabel } from '../common';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
+    paddingRight: 8,
   },
   text: {
     fontSize: 10,
-    paddingRight: 8,
     color: '#999'
   },
+  editedContainer: {
+    flexDirection: 'row',
+  }
 });
 
 export default class MessageTags extends React.PureComponent {
   render() {
     const { timestamp, starred } = this.props;
-    const { container, text } = styles;
+    const { container, text, editedContainer } = styles;
 
     if (timestamp === undefined && !starred) return null;
-    let tag = '';
-    if (timestamp !== undefined) {
-      tag = `edited ${distanceInWordsToNow(timestamp * 1000)},`;
-    }
-    if (starred) {
-      tag = `${tag} starred`;
-    }
+
     return (
       <View style={container}>
-        <Label style={text} text={tag} />
+        {timestamp &&
+          <View style={editedContainer}>
+            <Label style={text} text={'edited '} />
+            <RawLabel style={text} text={distanceInWordsToNow(timestamp * 1000)} />
+            {starred && <RawLabel style={text} text={', '} />}
+          </View>
+        }
+        {starred && <Label style={text} text={'starred'} />}
       </View>
     );
   }
