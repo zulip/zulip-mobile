4b23fd828505212a61e64dfce6124a553ad1074b
kunall17 <kunall.gupta17@gmail.com>
2017-06-14 22:16:09 +0530

    Hide other non working icons in ComposeOptions for ComposeText

--- src/compose/ComposeOptions.js
+++ src/compose/ComposeOptions.js
@@ -26,10 +26,10 @@ export default class ComposeOptions extends React.Component {
     return (
       <View style={styles.container}>
         <ComposeIcon name="md-text" isActive={selected === 0} onChange={() => onChange(0)} />
-        <ComposeIcon name="md-image" isActive={selected === 1} onChange={() => onChange(1)} />
-        <ComposeIcon name="md-camera" isActive={selected === 2} onChange={() => onChange(2)} />
-        <ComposeIcon name="md-videocam" isActive={selected === 3} onChange={() => onChange(3)} />
-        <ComposeIcon name="md-happy" isActive={selected === 4} onChange={() => onChange(4)} />
+        {/* <ComposeIcon name="md-image" isActive={selected === 1} onChange={() => onChange(1)} />
+          <ComposeIcon name="md-camera" isActive={selected === 2} onChange={() => onChange(2)} />
+          <ComposeIcon name="md-videocam" isActive={selected === 3} onChange={() => onChange(3)} />
+          <ComposeIcon name="md-happy" isActive={selected === 4} onChange={() => onChange(4)} />*/}
       </View>
     );
   }
