89c28c38d3ab48e5bd37b00e2812de4ba54ba3b3
saketkumar95 <saket.singh1410@gmail.com>
2017-06-15 23:35:50 +0530

    Changed loading screen view

--- src/start/LoadingScreen.js
+++ src/start/LoadingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, Image } from 'react-native';
 
 import styles, { BRAND_COLOR } from '../styles';
 
@@ -7,7 +7,9 @@ export default class LoadingScreen extends React.PureComponent {
   render() {
     return (
       <View style={styles.center}>
-        <ActivityIndicator color={BRAND_COLOR} size="large" />
+        <Image style={[styles.image]}
+          source={require('../../static/img/logo.png')}
+        />
       </View>
     );
   }
