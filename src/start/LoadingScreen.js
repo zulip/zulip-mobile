5d99ca411cbb4c118bc7756768a3f57325c8ea42
saketkumar95 <saket.singh1410@gmail.com>
2017-06-16 00:04:26 +0530

    fixes lint

--- src/start/LoadingScreen.js
+++ src/start/LoadingScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View, ActivityIndicator, Image } from 'react-native';
+import { View, Image } from 'react-native';
 
-import styles, { BRAND_COLOR } from '../styles';
+import styles from '../styles';
 
 export default class LoadingScreen extends React.PureComponent {
   render() {
     return (
       <View style={styles.center}>
-        <Image style={[styles.image]}
+        <Image
+          style={[styles.image]}
           source={require('../../static/img/logo.png')}
         />
       </View>
