96c4d3a0bb73e0856ef7062f4c8b76492318cb3f
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-06-16 18:42:27 +0530

    Replaced TextInput with Input in StreamBox.

--- src/compose/ModeViews/StreamBox.js
+++ src/compose/ModeViews/StreamBox.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
+import { Input } from '../../common';
 import { isTopicNarrow } from '../../utils/narrow';
+import styles from '../../styles';
 
-const styles = StyleSheet.create({
+const moreStyles = StyleSheet.create({
   streamInputWrapper: {
     flexDirection: 'row', alignItems: 'center', flex: 1
-  },
-  topicInput: {
-    flex: 0.8,
-    margin: 2
   }
 });
 
@@ -30,10 +28,10 @@ export default class StreamBox extends Component {
   render() {
     const { operator, setTopic } = this.props;
     return (
-      <View style={styles.streamInputWrapper}>
-        <TextInput
+      <View style={moreStyles.streamInputWrapper}>
+        <Input
           ref={component => { this.operandInput = component; }}
-          style={styles.topicInput}
+          style={styles.composeText}
           placeholder={'Topic'}
           onChange={(event) => setTopic(
             event.nativeEvent.text
