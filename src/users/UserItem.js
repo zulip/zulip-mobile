46215560898b32e02f3445fd18a2f2b9b528d817
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-06-20 10:12:55 +0530

    Show email in auto complete suggestions.

--- src/users/UserItem.js
+++ src/users/UserItem.js
@@ -15,13 +15,18 @@ const styles = StyleSheet.create({
     backgroundColor: BRAND_COLOR,
   },
   text: {
-    flex: 1,
+    flex: 3,
     marginLeft: 8,
     marginRight: 8,
   },
   selectedText: {
     color: 'white',
-  }
+  },
+  textEmail: {
+    flex: 2,
+    fontSize: 10,
+    color: '#999',
+  },
 });
 
 export default class UserItem extends Component {
@@ -40,7 +45,16 @@ export default class UserItem extends Component {
     this.props.onPress(this.props.email);
 
   render() {
-    const { fullName, avatarUrl, status, isSelected, unreadCount, realm } = this.props;
+    const {
+      fullName,
+      avatarUrl,
+      status,
+      isSelected,
+      unreadCount,
+      realm,
+      showEmail,
+      email
+    } = this.props;
 
     return (
       <Touchable onPress={this.handlePress}>
@@ -52,10 +66,17 @@ export default class UserItem extends Component {
             status={status}
             realm={realm}
           />
-          <RawLabel
-            style={[styles.text, isSelected && styles.selectedText]}
-            text={fullName}
-          />
+          <View style={styles.textWrapper} >
+            <RawLabel
+              style={[styles.text, isSelected && styles.selectedText]}
+              text={fullName}
+            />
+            {showEmail &&
+            <RawLabel
+              style={[styles.text, styles.textEmail, isSelected && styles.selectedText]}
+              text={email}
+            />}
+          </View>
           <UnreadCount count={unreadCount} />
         </View>
       </Touchable>
