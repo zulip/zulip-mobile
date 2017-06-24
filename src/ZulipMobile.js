387551f6544c27f1e214e264bea47ffa859e8060
Boris Yankov <borisyankov@gmail.com>
2017-06-24 22:42:47 +0300

    Add JS exception logging to Crashlytics

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-
+import crashlytics from 'react-native-fabric-crashlytics';
 import '../vendor/intl/intl';
 import store, { restore } from './store';
 import LoadingScreen from './start/LoadingScreen';
 import Providers from './Providers';
 
 require('./i18n/locale');
+
+crashlytics.init();
 
 export default class ZulipMobile extends Component {
 
