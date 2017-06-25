bdd9a256000547c97484e9dffc8e490632aae91c
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-06-25 21:18:51 +0530

    Use react-native-toast for toast messages.

--- src/utils/showToast.js
+++ src/utils/showToast.js
@@ -1,4 +1,4 @@
 /* @flow */
-import Toast from 'react-native-simple-toast';
+import Toast from '@remobile/react-native-toast';
 
 export default (message: string) => Toast.show(message, Toast.SHORT);
