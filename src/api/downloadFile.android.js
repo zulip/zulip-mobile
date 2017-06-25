f1b916c0ca5b753c42b8b574b1b1715ef4e9dac7
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-06-25 19:35:28 +0530

    Add support for download and share. (#660)

---
+++ src/api/downloadFile.android.js
@@ -0,0 +1,21 @@
+/* @flow */
+import RNFetchBlob from 'react-native-fetch-blob';
+
+import { getAuthHeader } from '../utils/url';
+import userAgent from '../utils/userAgent';
+import { Auth } from '../types';
+
+export default (url: string, auth: Auth) =>
+  RNFetchBlob.config({
+    addAndroidDownloads: {
+      useDownloadManager: true,
+      // Optional, but recommended since android DownloadManager will fail when
+      // the url does not contains a file extension, by default the mime type will be text/plain
+      mime: 'text/plain',
+      title: url.split('/').pop(),
+    },
+  }).fetch('GET', url, {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    'User-Agent': userAgent,
+    Authorization: getAuthHeader(auth.email, auth.apiKey),
+  });
---
+++ src/light-box/share.js
@@ -0,0 +1,12 @@
+/* @flow */
+import { Share } from 'react-native';
+
+import { BRAND_COLOR } from '../styles';
+
+export default (url: string) => {
+  const shareOptions = {
+    message: url,
+    title: 'Shared using Zulip!',
+  };
+  Share.share(shareOptions, { tintColor: BRAND_COLOR }).catch(err => {});
+};
