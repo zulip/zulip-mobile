4bf64e15ea8a640a9f847fe52c453086c3d5c793
kunall17 <kunall.gupta17@gmail.com>
2017-06-27 07:54:34 +0530

    Add a setting for enabling offline push notifications

--- src/settings/settingsReducers.js
+++ src/settings/settingsReducers.js
@@ -1,14 +1,20 @@
 /* @flow */
 import { SettingsState, Action } from '../types';
-import { SETTINGS_CHANGE } from '../actionConstants';
+import { SETTINGS_CHANGE, REALM_INIT } from '../actionConstants';
 
 const initialState: SettingsState = {
   locale: 'en',
   theme: 'default',
+  'offlinePushNotification': true,
 };
 
 export default (state: SettingsState = initialState, action: Action): SettingsState => {
   switch (action.type) {
+    case REALM_INIT:
+      return {
+        ...state,
+        offlinePushNotification: action.data.enable_offline_push_notifications
+      };
     case SETTINGS_CHANGE:
       return {
         ...state,
