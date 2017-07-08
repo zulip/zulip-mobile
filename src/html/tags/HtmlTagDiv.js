3c1601165bf95628b47d79ab1f7d0513407793a8
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-07-08 20:15:03 +0530

    fix flow errors.

--- src/html/tags/HtmlTagDiv.js
+++ src/html/tags/HtmlTagDiv.js
@@ -9,11 +9,12 @@ export default class HtmlTagDiv extends PureComponent {
   props: {
     style: StyleObj,
     actions: Actions,
+    className: string,
   };
 
   render() {
     const { style, className, ...restProps } = this.props;
-    const WrapperView = (className === 'codehilite') ? ScrollView : View;
+    const WrapperView = className === 'codehilite' ? ScrollView : View;
     return (
       <WrapperView style={style} horizontal>
         {renderHtmlChildren({ ...restProps })}
