d2a49e771c61d9ab68b9f4939c15c528327e2294
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-07-09 13:23:25 +0530

    Fix: @mention suggestions disappear after typing a space.
    - Created new method to get filters from text.
    - Added tests.
    
    Complete fix for #823.

---
+++ src/autocomplete/__tests__/getAutocompleteFilter-test.js
@@ -0,0 +1,34 @@
+import getAutocompleteFilter from '../getAutocompleteFilter';
+
+describe('getAutocompleteFilter', () => {
+  test('get False for empty text', () => {
+    expect(getAutocompleteFilter('')).toEqual(false);
+  });
+
+  test('get @users filters', () => {
+    expect(getAutocompleteFilter('@')).toEqual(false);
+    expect(getAutocompleteFilter('@ab')).toEqual({ filter: 'ab', lastWordPrefix: '@' });
+    expect(getAutocompleteFilter('@ab cd')).toEqual({ filter: 'ab cd', lastWordPrefix: '@' });
+  });
+
+  test('get #streams filters', () => {
+    expect(getAutocompleteFilter('#')).toEqual(false);
+    expect(getAutocompleteFilter('#ab')).toEqual({ filter: 'ab', lastWordPrefix: '#' });
+    expect(getAutocompleteFilter('#ab cd')).toEqual({ filter: 'ab cd', lastWordPrefix: '#' });
+  });
+
+  test('get :emoji filters', () => {
+    expect(getAutocompleteFilter(':')).toEqual(false);
+    expect(getAutocompleteFilter(':ab')).toEqual({ filter: 'ab', lastWordPrefix: ':' });
+    expect(getAutocompleteFilter(':ab cd')).toEqual({ filter: 'ab cd', lastWordPrefix: ':' });
+  });
+
+  test('get filter for more complicated text', () => {
+    expect(getAutocompleteFilter(':smile@ab')).toEqual({ filter: 'ab', lastWordPrefix: '@' });
+    expect(getAutocompleteFilter(':smile@ab cd')).toEqual({ filter: 'ab cd', lastWordPrefix: '@' });
+    expect(getAutocompleteFilter('@ab :cd #a b')).toEqual({ filter: 'a b', lastWordPrefix: '#' });
+    expect(getAutocompleteFilter(':#@smile')).toEqual({ filter: 'smile', lastWordPrefix: '@' });
+    expect(getAutocompleteFilter('@r@::@q@m p')).toEqual({ filter: 'm p', lastWordPrefix: '@' });
+    expect(getAutocompleteFilter('some @ab:cd #a b')).toEqual({ filter: 'a b', lastWordPrefix: '#' });
+  });
+});
