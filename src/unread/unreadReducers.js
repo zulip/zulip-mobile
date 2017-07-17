0a15a8575a2b321bbc0f47f81139c030a830d6e1
Boris Yankov <borisyankov@gmail.com>
2017-07-18 01:21:05 +0300

    Request and store unread_msgs data (#884)

---
+++ src/unread/unreadReducers.js
@@ -0,0 +1,22 @@
+/* @flow */
+import type { Action, UnreadState } from '../types';
+import { REALM_INIT, ACCOUNT_SWITCH } from '../actionConstants';
+
+const initialState: UnreadState = {
+  streams: [],
+  huddles: [],
+  pms: [],
+};
+
+export default (state: UnreadState = initialState, action: Action): UnreadState => {
+  switch (action.type) {
+    case REALM_INIT:
+      return action.data.unread_msgs;
+
+    case ACCOUNT_SWITCH:
+      return initialState;
+
+    default:
+      return state;
+  }
+};
