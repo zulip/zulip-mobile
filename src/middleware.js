7a1dca91d91b896de5622856032c109224705b6a
Boris Yankov <borisyankov@gmail.com>
2017-07-17 07:59:37 +0300

    Remove Crashlytics completely. Fix Sentry config

--- src/middleware.js
+++ src/middleware.js
@@ -4,13 +4,11 @@ import { createLogger } from 'redux-logger';
 import { REHYDRATE } from 'redux-persist/constants';
 import createActionBuffer from 'redux-action-buffer';
 
+import config from './config';
+
 const middleware = [thunk, createActionBuffer(REHYDRATE)];
 
-// Only enable remote debugging! It really slows down the iOS JSC
-// Checking for btoa() is not available in JSC
-const enableLogging = process.env.NODE_ENV === 'development' && !!global.btoa;
-
-if (enableLogging) {
+if (config.enableReduxLogging) {
   middleware.push(createLogger({ duration: true }));
 }
 
