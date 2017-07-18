7c2aab684b3a8a77769b832c0734cff8f0cf2883
Vishwesh Jainkuniya <catchvishwesh@gmail.com>
2017-07-18 17:47:45 +0530

    Removed Pods files.

--- ios/Pods/Target Support Files/KSCrash/KSCrash-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KSCrash : NSObject
-@end
-@implementation PodsDummy_KSCrash
-@end
--- ios/Pods/Target Support Files/SentryReactNative/SentryReactNative-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SentryReactNative : NSObject
-@end
-@implementation PodsDummy_SentryReactNative
-@end
