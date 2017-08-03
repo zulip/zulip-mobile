24db5bf5118868e1e01222ab8d372b1acf9f2b95
Boris Yankov <borisyankov@gmail.com>
2017-08-03 11:39:22 +0300

    Split SearchMessagesScreen in several components (#974)

---
+++ src/search/SearchMessagesCard.js
@@ -0,0 +1,96 @@
+/* @flow */
+import React, { PureComponent } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import throttle from 'lodash.throttle';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+
+import type { Actions, Auth } from '../types';
+import { Label } from '../common';
+import { BRAND_COLOR } from '../styles';
+import { searchNarrow } from '../utils/narrow';
+import MessageList from '../message/MessageList';
+import { getMessages } from '../api';
+
+const styles = StyleSheet.create({
+  empty: {
+    fontSize: 20,
+    padding: 8,
+    textAlign: 'center',
+  },
+  results: {
+    flex: 1,
+  },
+  activity: {
+    padding: 8,
+  },
+});
+
+export default class SearchMessagesCard extends PureComponent {
+  props: {
+    actions: Actions,
+    twentyFourHourTime: boolean,
+    subscriptions: any[],
+    query: string,
+    auth: Auth,
+    flags: Object,
+  };
+
+  state = {
+    messages: [],
+    isFetching: false,
+  };
+
+  handleQueryChange = async (query: string) => {
+    const { auth } = this.props;
+
+    throttle(async () => {
+      this.setState({ isFetching: true });
+      const messages = await getMessages(
+        auth,
+        Number.MAX_SAFE_INTEGER,
+        20,
+        0,
+        searchNarrow(query),
+        false,
+      );
+      this.setState({
+        messages,
+        isFetching: false,
+      });
+    }, 500).call(this);
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.query !== this.props.query) {
+      this.handleQueryChange(nextProps.query);
+    }
+  }
+
+  render() {
+    const { isFetching, messages, query } = this.state;
+    const { actions, auth, subscriptions, twentyFourHourTime, flags } = this.props;
+    const noResults = !!query && !isFetching && !messages.length;
+
+    return (
+      <View style={styles.results}>
+        {isFetching &&
+          <ActivityIndicator style={styles.activity} color={BRAND_COLOR} size="large" />}
+        {noResults && <Label style={styles.empty} text="No results" />}
+        <ActionSheetProvider>
+          <MessageList
+            actions={actions}
+            messages={messages}
+            caughtUp={{ older: true, newer: true }}
+            fetching={{ older: false, newer: isFetching }}
+            singleFetchProgress
+            narrow={[]}
+            twentyFourHourTime={twentyFourHourTime}
+            subscriptions={subscriptions}
+            auth={auth}
+            flags={flags}
+          />
+        </ActionSheetProvider>
+      </View>
+    );
+  }
+}
--- src/search/SearchMessagesScreen.js
+++ src/search/SearchMessagesScreen.js
@@ -1,111 +1,27 @@
 /* @flow */
 import React, { PureComponent } from 'react';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
-import { connect } from 'react-redux';
-import throttle from 'lodash.throttle';
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
-import type { Actions, Auth } from '../types';
-import boundActions from '../boundActions';
-import { Label } from '../common';
-import { getAuth } from '../selectors';
 import SearchScreen from './SearchScreen';
-import { BRAND_COLOR } from '../styles';
-import { searchNarrow } from '../utils/narrow';
-import MessageList from '../message/MessageList';
-import { getMessages } from '../api';
+import SearchMessagesContainer from './SearchMessagesContainer';
 
-const styles = StyleSheet.create({
-  empty: {
-    fontSize: 20,
-    padding: 8,
-    textAlign: 'center',
-  },
-  results: {
-    flex: 1,
-  },
-  activity: {
-    padding: 8,
-  },
-});
-
-class SearchMessagesScreen extends PureComponent {
-  query: string;
-
-  props: {
-    actions: Actions,
-    twentyFourHourTime: boolean,
-    subscriptions: any[],
-    auth: Auth,
-    flags: Object,
+export default class SearchMessagesScreen extends PureComponent {
+  state: {
+    query: string,
   };
 
   state = {
-    messages: [],
-    isFetching: false,
+    query: '',
   };
 
-  handleQueryChange = async (query: string) => {
-    const { auth } = this.props;
-    this.query = query;
-
-    throttle(async () => {
-      this.setState({ isFetching: true });
-      const messages = await getMessages(
-        auth,
-        Number.MAX_SAFE_INTEGER,
-        20,
-        0,
-        searchNarrow(query),
-        false,
-      );
-      this.setState({
-        messages,
-        isFetching: false,
-      });
-    }, 500).call(this);
-  };
+  handleQueryChange = (query: string) => this.setState({ query });
 
   render() {
-    const { isFetching, messages } = this.state;
-    const { actions, auth, subscriptions, twentyFourHourTime, flags } = this.props;
-    const noResults = !!this.query && !isFetching && !messages.length;
+    const { query } = this.state;
 
     return (
-      <SearchScreen title="Search" keyboardAvoiding searchBarOnChange={this.handleQueryChange}>
-        <View style={styles.results}>
-          {isFetching &&
-            <ActivityIndicator style={styles.activity} color={BRAND_COLOR} size="large" />}
-          {noResults && <Label style={styles.empty} text="No results" />}
-          <ActionSheetProvider>
-            <MessageList
-              actions={actions}
-              messages={messages}
-              caughtUp={{ older: true, newer: true }}
-              fetching={{ older: false, newer: isFetching }}
-              singleFetchProgress
-              narrow={[]}
-              twentyFourHourTime={twentyFourHourTime}
-              subscriptions={subscriptions}
-              auth={auth}
-              flags={flags}
-            />
-          </ActionSheetProvider>
-        </View>
+      <SearchScreen title="Search" searchBarOnChange={this.handleQueryChange}>
+        <SearchMessagesContainer query={query} />
       </SearchScreen>
     );
   }
 }
-
-export default connect(
-  state => ({
-    auth: getAuth(state),
-    isOnline: state.app.isOnline,
-    subscriptions: state.subscriptions,
-    narrow: state.chat.narrow,
-    startReached: state.chat.startReached,
-    streamlistOpened: state.nav.opened,
-    flags: state.flags,
-  }),
-  boundActions,
-)(SearchMessagesScreen);
