0b5dcba669fd96d86e2a6e43f51296bef17d0507
Boris Yankov <borisyankov@gmail.com>
2017-08-06 18:20:30 +0300

    Fix breaking change with createJSModules in RC 0.47 (#994)

--- android/app/src/main/java/com/zulipmobile/ZulipNativePackage.java
+++ android/app/src/main/java/com/zulipmobile/ZulipNativePackage.java
@@ -13,11 +13,6 @@ import java.util.List;
 public class ZulipNativePackage implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         List<ViewManager> managers = new ArrayList<>();
         managers.add(new AnchorScrollViewManager());
