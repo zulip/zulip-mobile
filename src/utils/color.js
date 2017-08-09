d3b44a3cbe8bfbb11ab8b62c196f0155c56b0b92
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-08-09 09:46:17 +0530

    Fix: status bar style in dark theme for private narrow.

--- src/utils/color.js
+++ src/utils/color.js
@@ -2,4 +2,4 @@
 import Color from 'color';
 
 export const foregroundColorFromBackground = (color: string) =>
-  color === 'transparent' || Color(color).luminosity() > 0.4 ? 'black' : 'white';
+  Color(color).luminosity() > 0.4 ? 'black' : 'white';
