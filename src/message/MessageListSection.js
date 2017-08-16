f0ea1c58d18aa3ac9cfb5260e56dc83da5ef3740
Boris Yankov <borisyankov@gmail.com>
2017-08-16 16:50:20 +0300

    Refactor MessageList prerendered data

---
+++ src/message/MessageListSection.js
@@ -0,0 +1,19 @@
+/* @flow */
+import React, { PureComponent } from 'react';
+
+import type { Message } from '../types';
+import MessageHeaderContainer from './headers/MessageHeaderContainer';
+
+export default class MessageListSection extends PureComponent {
+  props: {
+    message?: Message,
+  };
+
+  render() {
+    const { message } = this.props;
+
+    if (!message) return null;
+
+    return <MessageHeaderContainer message={message} />;
+  }
+}
