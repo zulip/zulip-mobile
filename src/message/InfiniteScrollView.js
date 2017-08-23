b9ddcf0b107bd591f6418e615ccbc9793ed383ae
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-08-24 00:40:00 +0530

    Fix: Message being requested in a loop on Android. (#1082)

--- src/message/InfiniteScrollView.js
+++ src/message/InfiniteScrollView.js
@@ -106,7 +106,7 @@ export default class InfiniteScrollView extends PureComponent {
         onLayout={this._onScrollViewLayout}
         onScroll={this._onScroll}
         scrollEventThrottle={config.scrollCallbackThrottle}
-        stickyHeaderIndices={this.props.stickyHeaderIndices}
+        stickyHeaderIndices={Platform.OS === 'ios' ? this.props.stickyHeaderIndices : undefined}
         autoScrollToBottom={this.props.autoScrollToBottom}
         removeClippedSubviews
         ref={component => {
