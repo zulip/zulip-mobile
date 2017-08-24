eff7dfe9568e882c76dc3eaf9eac61113bc2790e
Nash Vail <nishantve1@gmail.com>
2017-08-24 14:49:44 +0530

    Added flag to conditionally show unreads UI

--- src/nav/StreamSidebar.js
+++ src/nav/StreamSidebar.js
@@ -25,6 +25,8 @@ const componentStyles = StyleSheet.create({
   },
 });
 
+const SHOW_UNREAD_VIEW = true;
+
 class StreamSidebar extends PureComponent {
   static contextTypes = {
     styles: () => null,
@@ -68,7 +70,8 @@ class StreamSidebar extends PureComponent {
         <View style={componentStyles.iconList}>
           <NavButton name="md-home" onPress={() => this.narrowAndClose(homeNarrow)} />
           <NavButton name="md-mail" onPress={() => this.narrowAndClose(allPrivateNarrow)} />
-          <NavButton name="md-albums" onPress={actions.navigateToUnreadCards} />
+          {SHOW_UNREAD_VIEW &&
+            <NavButton name="md-albums" onPress={actions.navigateToUnreadCards} />}
           <NavButton name="md-star" onPress={() => this.narrowAndClose(specialNarrow('starred'))} />
           <NavButton name="md-at" onPress={() => this.narrowAndClose(specialNarrow('mentioned'))} />
           <NavButton name="md-search" onPress={() => this.navigateAndClose('search')} />
