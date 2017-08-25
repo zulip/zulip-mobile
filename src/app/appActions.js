ffcbb9b7567064ce2e98118f925bbc5cc21b7923
Boris Yankov <borisyankov@gmail.com>
2017-08-25 20:14:55 +0300

    Do not fire app actions when they do not change state

--- src/app/appActions.js
+++ src/app/appActions.js
@@ -11,20 +11,34 @@ import {
 import getSingleMessage from '../api/getSingleMessage';
 import { getAuth } from '../selectors';
 
-export const appOnline = (isOnline: boolean): Action => ({
-  type: APP_ONLINE,
-  isOnline,
-});
+export const appOnline = (isOnline: boolean): Action => (
+  dispatch: Dispatch,
+  getState: GetState,
+) => {
+  if (isOnline !== getState().app.isOnline) {
+    dispatch({
+      type: APP_ONLINE,
+      isOnline,
+    });
+  }
+};
 
 export const appState = (isActive: boolean): Action => ({
   type: APP_STATE,
   isActive,
 });
 
-export const appOrientation = (orientation: string): Action => ({
-  type: APP_ORIENTATION,
-  orientation,
-});
+export const appOrientation = (orientation: string): Action => (
+  dispatch: Dispatch,
+  getState: GetState,
+) => {
+  if (orientation !== getState().app.orientation) {
+    dispatch({
+      type: APP_ORIENTATION,
+      orientation,
+    });
+  }
+};
 
 export const toggleComposeTools = () => ({
   type: TOGGLE_COMPOSE_TOOLS,
