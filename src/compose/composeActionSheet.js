4f1f5b5cf94c5ed94b39205c801275ae000da948
kunall17 <kunall.gupta17@gmail.com>
2017-09-18 16:50:40 +0530

    Rework ComposeMenu to have menu construction like message action sheet

---
+++ src/compose/composeActionSheet.js
@@ -0,0 +1,40 @@
+/* @flow */
+import type { Actions } from '../types';
+import { isStreamNarrow } from '../utils/narrow';
+
+type ConstructActionButtonsType = {
+  narrow: [],
+};
+
+type ExecuteActionSheetParams = {
+  title: string,
+  actions: Actions,
+};
+
+const actionSheetButtons: ButtonType[] = [
+  {
+    title: 'Toggle compose tools',
+    onPress: ({ actions }) => actions.toggleComposeTools(),
+    onlyIf: isStreamNarrow,
+  },
+  {
+    title: 'Create group',
+    onPress: ({ actions }) => {
+      actions.navigateToCreateGroup();
+    },
+  },
+];
+
+export const constructActionButtons = ({ narrow }: ConstructActionButtonsType) => {
+  const buttons = actionSheetButtons.filter(x => !x.onlyIf || x.onlyIf(narrow)).map(x => x.title);
+
+  buttons.push('Cancel');
+  return buttons;
+};
+
+export const executeActionSheetAction = ({ title, ...props }: ExecuteActionSheetParams) => {
+  const button = actionSheetButtons.find(x => x.title === title);
+  if (button) {
+    button.onPress(props);
+  }
+};
