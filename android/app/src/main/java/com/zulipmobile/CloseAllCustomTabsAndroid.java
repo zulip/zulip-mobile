39ced1785ebb88fa16403891603d267e792e2b6c
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-09-30 15:15:45 +0530

    Added description for Java class.

--- android/app/src/main/java/com/zulipmobile/CloseAllCustomTabsAndroid.java
+++ android/app/src/main/java/com/zulipmobile/CloseAllCustomTabsAndroid.java
@@ -1,15 +1,13 @@
 package com.zulipmobile;
 
-import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 /**
- * Created by vishwesh on 30/09/17.
+ * Close all custom tabs.
  */
 
 public class CloseAllCustomTabsAndroid extends ReactContextBaseJavaModule {
