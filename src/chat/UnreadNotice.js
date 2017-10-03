52ba784977568694b007638bffdac792fc521aec
Boris Yankov <borisyankov@gmail.com>
2017-10-03 13:47:43 +0300

    Add 'Mark as read' button to UnreadNotice

--- src/chat/UnreadNotice.js
+++ src/chat/UnreadNotice.js
@@ -1,15 +1,20 @@
 /* @flow */
 import React, { PureComponent } from 'react';
-import { Animated, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { Label, RawLabel } from '../common';
 import { IconDownArrow } from '../common/Icons';
 import { unreadToLimitedCount } from '../utils/unread';
+import MarkUnreadButton from './MarkUnreadButton';
 
 const styles = StyleSheet.create({
   unreadContainer: {
     padding: 2,
     backgroundColor: '#96A3F9',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  unreadTextWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
   },
@@ -41,14 +46,17 @@ export default class UnreadNotice extends PureComponent {
     if (unreadCount === 0) return null;
 
     return (
-      <Animated.View style={styles.unreadContainer}>
-        <IconDownArrow style={styles.icon} />
-        <RawLabel
-          style={[styles.unreadText, styles.margin]}
-          text={unreadToLimitedCount(unreadCount)}
-        />
-        <Label style={styles.unreadText} text="unread messages" />
-      </Animated.View>
+      <View style={styles.unreadContainer}>
+        <View style={styles.unreadTextWrapper}>
+          <IconDownArrow style={styles.icon} />
+          <RawLabel
+            style={[styles.unreadText, styles.margin]}
+            text={unreadToLimitedCount(unreadCount)}
+          />
+          <Label style={styles.unreadText} text="unread messages" />
+        </View>
+        <MarkUnreadButton />
+      </View>
     );
   }
 }
