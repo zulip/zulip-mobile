6454ac716b10585afd8cd502713b277bc7b32f73
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-10-10 20:50:19 +0530

    Handle when custom tabs and browser is not installed. (#1285)

--- android/app/src/main/java/com/zulipmobile/CustomTabsAndroid.java
+++ android/app/src/main/java/com/zulipmobile/CustomTabsAndroid.java
@@ -1,17 +1,24 @@
 package com.zulipmobile;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
+import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.util.List;
 
 /**
  * Launches custom tabs.
  */
 
 public class CustomTabsAndroid extends ReactContextBaseJavaModule {
+
 
     public CustomTabsAndroid(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -28,6 +35,23 @@ public class CustomTabsAndroid extends ReactContextBaseJavaModule {
         builder.setStartAnimations(getReactApplicationContext().getCurrentActivity(), R.anim.slide_in_right, R.anim.slide_out_left);
         builder.setExitAnimations(getReactApplicationContext().getCurrentActivity(), R.anim.slide_in_left, R.anim.slide_out_right);
         CustomTabsIntent customTabsIntent = builder.build();
-        customTabsIntent.launchUrl(getReactApplicationContext().getCurrentActivity(), Uri.parse(url));
+
+        if (CustomTabsHelper.isChromeCustomTabsSupported(getReactApplicationContext())) {
+            customTabsIntent.launchUrl(getReactApplicationContext().getCurrentActivity(), Uri.parse(url));
+        } else {
+            //open in browser
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            //ensure browser is present
+            final List<ResolveInfo> customTabsApps = getReactApplicationContext()
+                    .getCurrentActivity().getPackageManager().queryIntentActivities(i, 0);
+
+            if (customTabsApps.size() > 0) {
+                getReactApplicationContext().startActivity(i);
+            } else {
+                // no browser
+                Toast.makeText(getReactApplicationContext(), R.string.no_browser_found, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ android/app/src/main/java/com/zulipmobile/CustomTabsHelper.java
@@ -0,0 +1,25 @@
+package com.zulipmobile;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+/**
+ * Contains helper methods for custom tabs.
+ */
+
+public class CustomTabsHelper {
+
+    private static final String SERVICE_ACTION = "android.support.customtabs.action.CustomTabsService";
+    private static final String CHROME_PACKAGE = "com.android.chrome";
+
+    public static boolean isChromeCustomTabsSupported(@NonNull final Context context) {
+        Intent serviceIntent = new Intent(SERVICE_ACTION);
+        serviceIntent.setPackage(CHROME_PACKAGE);
+        List<ResolveInfo> resolveInfos = context.getPackageManager().queryIntentServices(serviceIntent, 0);
+        return !(resolveInfos == null || resolveInfos.isEmpty());
+    }
+}
