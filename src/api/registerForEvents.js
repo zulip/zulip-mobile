786ce8bbd087f08636283e1d639db6df6615d249
Boris Yankov <borisyankov@gmail.com>
2017-10-19 20:01:02 +0300

    Extract diagnostics and events settings to config

--- src/api/registerForEvents.js
+++ src/api/registerForEvents.js
@@ -1,37 +1,12 @@
 /* @flow */
 import type { Auth } from '../types';
+import config from '../config';
 import { apiPost } from './apiFetch';
 
 export default (auth: Auth) =>
   apiPost(auth, 'register', res => res, {
     apply_markdown: true,
-    event_types: JSON.stringify([
-      'message',
-      'muted_topics',
-      'presence',
-      'reaction',
-      'realm_emoji',
-      'realm_filters',
-      'realm_user',
-      'stream',
-      'subscription',
-      'typing',
-      'update_message',
-      'update_message_flags',
-      'update_display_settings',
-      'update_global_notifications',
-    ]),
-    fetch_event_types: JSON.stringify([
-      'alert_words',
-      'message',
-      'muted_topics',
-      'presence',
-      'realm_emoji',
-      'realm_filters',
-      'subscription',
-      'update_display_settings',
-      'update_global_notifications',
-      'update_message_flags',
-    ]),
+    event_types: JSON.stringify(config.trackServerEvents),
+    fetch_event_types: JSON.stringify(config.serverDataOnStartup),
     include_subscribers: false,
   });
