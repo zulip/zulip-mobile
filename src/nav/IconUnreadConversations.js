805a360af837c280413915a9f09b36e5b5b63786
Boris Yankov <borisyankov@gmail.com>
2017-10-19 13:00:46 +0300

    Fix NavButtons sizing

--- src/nav/IconUnreadConversations.js
+++ src/nav/IconUnreadConversations.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 import connectWithActions from '../connectWithActions';
 import { getUnreadPmsTotal, getUnreadHuddlesTotal } from '../selectors';
 import { IconPeople } from '../common/Icons';
 import { ComponentWithOverlay, UnreadCount } from '../common';
 
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+  },
+});
+
 const IconUnreadConversations = ({ unreadHuddlesTotal, unreadPmsTotal, color }) => {
   const unreadCount = unreadHuddlesTotal + unreadPmsTotal;
 
   return (
     <ComponentWithOverlay
+      style={styles.button}
       overlaySize={15}
       showOverlay={unreadCount > 0}
       overlay={<UnreadCount count={unreadCount} />}
