c97360452ae912a1e7a8c7460e9f8b8714227639
Boris Yankov <borisyankov@gmail.com>
2017-10-19 18:09:51 +0300

    Add filterArray that does not create a new array if no change

--- src/utils/__tests__/immutability-test.js
+++ src/utils/__tests__/immutability-test.js
@@ -1,6 +1,6 @@
 import deepFreeze from 'deep-freeze';
 
-import { removeItemsFromArray } from '../immutability';
+import { removeItemsFromArray, filterArray } from '../immutability';
 
 describe('removeItemsFromArray', () => {
   test('return a new array with items removed, do not mutate input', () => {
@@ -20,5 +20,26 @@ describe('removeItemsFromArray', () => {
     const result = removeItemsFromArray(inputArray, itemsToRemove);
 
     expect(result).toBe(inputArray);
+  });
+});
+
+describe('filterArray', () => {
+  test('filters an array according to given predicate', () => {
+    const input = [1, 2, 3, 4, 5];
+    const predicate = x => x % 2 === 0;
+    const expectedResult = [2, 4];
+
+    const result = filterArray(input, predicate);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('do not return a new array if no items are filtered', () => {
+    const input = [2, 4, 6];
+    const predicate = x => x % 2 === 0;
+
+    const result = filterArray(input, predicate);
+
+    expect(result).toBe(input);
   });
 });
--- src/utils/immutability.js
+++ src/utils/immutability.js
@@ -8,3 +8,8 @@ export const addItemsToArray = (input: Object[], itemsToAdd: Object[]): Object[]
   const newItems = itemsToAdd.filter(item => !input.includes(item));
   return newItems.length > 0 ? [...input, ...itemsToAdd] : input;
 };
+
+export const filterArray = (input: any[], predicate: () => boolean): any[] => {
+  const filteredList = input.filter(predicate);
+  return filteredList.length === input.length ? input : filteredList;
+};
