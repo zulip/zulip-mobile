b2e3d367fa4caca6adecaf1dc3a9db3a17669a21
Boris Yankov <borisyankov@gmail.com>
2017-10-23 23:05:52 +0300

    Do not persist 'typing' state

--- src/boot/store.js
+++ src/boot/store.js
@@ -20,7 +20,7 @@ export const restore = (onFinished?: () => void) =>
   persistStore(
     store,
     {
-      blacklist: ['app', 'caughtUp', 'fetching', 'presence', 'users'],
+      blacklist: ['app', 'caughtUp', 'fetching', 'presence', 'typing', 'users'],
       storage: AsyncStorage,
     },
     onFinished,
