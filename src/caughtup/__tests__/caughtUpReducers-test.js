8b8f778a8c1345152b69e081d16ae0cec9608860
Boris Yankov <borisyankov@gmail.com>
2017-10-24 21:25:20 +0300

    Improve caught up logic. Fix and add more tests (#1357)

--- src/caughtup/__tests__/caughtUpReducers-test.js
+++ src/caughtup/__tests__/caughtUpReducers-test.js
@@ -2,54 +2,115 @@ import deepFreeze from 'deep-freeze';
 
 import caughtUpReducers from '../caughtUpReducers';
 import { MESSAGE_FETCH_START, MESSAGE_FETCH_COMPLETE } from '../../actionConstants';
+import { homeNarrow, homeNarrowStr } from '../../utils/narrow';
 
 describe('caughtUpReducers', () => {
   describe('MESSAGE_FETCH_START', () => {
-    test('when fetch starts, we consider it not caught up????', () => {
+    test('when fetch starts caught up does not change', () => {
       const initialState = deepFreeze({
-        older: true,
-        newer: true,
+        [homeNarrowStr]: {
+          older: true,
+          newer: true,
+        },
       });
 
       const action = deepFreeze({
         type: MESSAGE_FETCH_START,
-        narrow: [],
+        narrow: homeNarrow,
       });
-
-      const expectedState = {
-        older: false,
-        newer: false,
-      };
 
       const newState = caughtUpReducers(initialState, action);
 
-      expect(newState.messages).toEqual(expectedState.messages);
+      expect(newState).toBe(initialState);
     });
   });
 
   describe('MESSAGE_FETCH_COMPLETE', () => {
-    test('if we receive less messages than expected we consider it caught up', () => {
+    test('if messages received are less than requested then we are caught up', () => {
       const initialState = deepFreeze({
-        older: false,
-        newer: false,
+        [homeNarrowStr]: {
+          older: false,
+          newer: false,
+        },
       });
 
       const action = deepFreeze({
         type: MESSAGE_FETCH_COMPLETE,
         narrow: [],
+        anchor: 1,
         messages: [{ id: 1 }, { id: 2 }, { id: 3 }],
         numBefore: 5,
         numAfter: 5,
       });
 
       const expectedState = {
-        older: true,
-        newer: true,
+        [homeNarrowStr]: {
+          older: true,
+          newer: true,
+        },
       };
 
       const newState = caughtUpReducers(initialState, action);
 
-      expect(newState.messages).toEqual(expectedState.messages);
+      expect(newState).toEqual(expectedState);
     });
+  });
+
+  test('if messages received are requested amount we consider it not yet caught up', () => {
+    const initialState = deepFreeze({
+      [homeNarrowStr]: {
+        older: false,
+        newer: false,
+      },
+    });
+
+    const action = deepFreeze({
+      type: MESSAGE_FETCH_COMPLETE,
+      narrow: [],
+      anchor: 3,
+      messages: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+      numBefore: 2,
+      numAfter: 2,
+    });
+
+    const expectedState = {
+      [homeNarrowStr]: {
+        older: false,
+        newer: false,
+      },
+    };
+
+    const newState = caughtUpReducers(initialState, action);
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  test('new results reset previous state', () => {
+    const initialState = deepFreeze({
+      [homeNarrowStr]: {
+        older: true,
+        newer: true,
+      },
+    });
+
+    const action = deepFreeze({
+      type: MESSAGE_FETCH_COMPLETE,
+      narrow: [],
+      anchor: 3,
+      messages: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+      numBefore: 2,
+      numAfter: 2,
+    });
+
+    const expectedState = {
+      [homeNarrowStr]: {
+        older: false,
+        newer: false,
+      },
+    };
+
+    const newState = caughtUpReducers(initialState, action);
+
+    expect(newState).toEqual(expectedState);
   });
 });
