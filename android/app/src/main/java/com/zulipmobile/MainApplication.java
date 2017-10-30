515436a93deb25f22d96227da466db9483694605
Boris Yankov <borisyankov@gmail.com>
2017-10-31 00:17:51 +0200

    Add 'react-native-image-picker' (#1375)

--- android/app/src/main/java/com/zulipmobile/MainApplication.java
+++ android/app/src/main/java/com/zulipmobile/MainApplication.java
@@ -8,8 +8,9 @@ import android.util.Pair;
 
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactApplication;
-import com.github.yamill.orientation.OrientationPackage;
 import com.wix.reactnativenotifications.RNNotificationsPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.github.yamill.orientation.OrientationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -50,6 +51,8 @@ public class MainApplication extends Application implements ReactApplication, IN
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new RNNotificationsPackage(),
+                    new ImagePickerPackage(),
                     new OrientationPackage(),
                     new RNSentryPackage(MainApplication.this),
                     new PhotoViewPackage(),
