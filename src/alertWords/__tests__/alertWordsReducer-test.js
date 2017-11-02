5833fee88365fe7acf709de21af29158d5086d5f
Boris Yankov <borisyankov@gmail.com>
2017-11-02 18:42:49 +0200

    Fix alert_words exceptions. Add tests

--- src/alertWords/__tests__/alertWordsReducer-test.js
+++ src/alertWords/__tests__/alertWordsReducer-test.js
@@ -1,18 +1,18 @@
 import deepFreeze from 'deep-freeze';
 
 import alertWordsReducer from '../alertWordsReducer';
-import { INIT_ALERT_WORDS } from '../../actionConstants';
+import { REALM_INIT, INIT_ALERT_WORDS } from '../../actionConstants';
 
 describe('alertWordsReducer', () => {
-  describe('INIT_ALERT_WORDS', () => {
-    test('add new alert words', () => {
+  describe('REALM_INIT', () => {
+    test('when `alert_words` data is provided init state with it', () => {
       const initialState = deepFreeze([]);
-
       const action = deepFreeze({
-        type: INIT_ALERT_WORDS,
-        alertWords: ['word', '@mobile-core', 'alert'],
+        type: REALM_INIT,
+        data: {
+          alert_words: ['word', '@mobile-core', 'alert'],
+        },
       });
-
       const expectedState = ['word', '@mobile-core', 'alert'];
 
       const actualState = alertWordsReducer(initialState, action);
@@ -20,14 +20,39 @@ describe('alertWordsReducer', () => {
       expect(actualState).toEqual(expectedState);
     });
 
-    test('update alert words list', () => {
-      const initialState = deepFreeze(['word', '@mobile-core', 'alert']);
+    test('when no `alert_words` data is given do not mutate state', () => {
+      const initialState = deepFreeze(['word']);
+      const action = deepFreeze({
+        type: REALM_INIT,
+        data: {},
+      });
 
+      const actualState = alertWordsReducer(initialState, action);
+
+      expect(actualState).toBe(initialState);
+    });
+  });
+
+  describe('INIT_ALERT_WORDS', () => {
+    test('on first call adds new data', () => {
+      const initialState = deepFreeze([]);
+      const action = deepFreeze({
+        type: INIT_ALERT_WORDS,
+        alertWords: ['word', '@mobile-core', 'alert'],
+      });
+      const expectedState = ['word', '@mobile-core', 'alert'];
+
+      const actualState = alertWordsReducer(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+
+    test('subsequent calls replace existing data', () => {
+      const initialState = deepFreeze(['word', '@mobile-core', 'alert']);
       const action = deepFreeze({
         type: INIT_ALERT_WORDS,
         alertWords: ['word', '@mobile-core', 'new alert'],
       });
-
       const expectedState = ['word', '@mobile-core', 'new alert'];
 
       const actualState = alertWordsReducer(initialState, action);
