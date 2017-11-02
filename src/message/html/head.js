ab9afc412e7258e88ba6dd6477e5f85de6f81b60
Boris Yankov <borisyankov@gmail.com>
2017-11-03 00:51:55 +0200

    More native feel of the html messages (#1386)

--- src/message/html/head.js
+++ src/message/html/head.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
 
 export default `
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 `;
