eb70ee21e41215d3c7e6358042716144918472f2
Boris Yankov <borisyankov@gmail.com>
2017-11-15 19:33:29 +0200

    Add topics selectors and tests (#1425)

---
+++ src/topics/topicSelectors.js
@@ -0,0 +1,19 @@
+/* @flow */
+import { createSelector } from 'reselect';
+
+import { getActiveNarrow, getStreams, getTopics } from '../selectors';
+import { NULL_ARRAY } from '../nullObjects';
+import { isStreamNarrow } from '../utils/narrow';
+
+export const getTopicsInActiveNarrow = createSelector(
+  getActiveNarrow,
+  getTopics,
+  getStreams,
+  (narrow, topics, streams) => {
+    if (!isStreamNarrow(narrow)) {
+      return NULL_ARRAY;
+    }
+    const stream = streams.find(x => x.name === narrow[0].operand);
+    return (stream && topics[stream.stream_id]) || NULL_ARRAY;
+  },
+);
