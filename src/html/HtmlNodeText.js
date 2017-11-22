f459b07f8daee28a64497d5bd042c9eae1737c87
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-11-22 11:04:50 +0530

    message render: Don't join last word with space.

--- src/html/HtmlNodeText.js
+++ src/html/HtmlNodeText.js
@@ -11,21 +11,22 @@ type Props = {
   splitMessageText?: boolean,
 };
 
-export default ({ data, cascadingTextStyle, splitMessageText }: Props) =>
-  splitMessageText ? (
-    entities
+export default ({ data, cascadingTextStyle, splitMessageText }: Props) => {
+  if (splitMessageText) {
+    const textItems = entities
       .decodeHTML(data)
       .replace(/\n/, '')
-      .split(' ')
-      .map(text => (
-        <RawLabel
-          key={Math.random()
-            .toString(36)
-            .substring(7)}
-          style={cascadingTextStyle}
-          text={`${text} `}
-        />
-      ))
-  ) : (
-    <RawLabel style={cascadingTextStyle} text={entities.decodeHTML(data).replace(/\n/, '')} />
-  );
+      .split(' ');
+    const itemsCount = textItems.length;
+    return textItems.map((text, index) => (
+      <RawLabel
+        key={Math.random()
+          .toString(36)
+          .substring(7)}
+        style={cascadingTextStyle}
+        text={itemsCount === index + 1 ? text : `${text} `}
+      />
+    ));
+  }
+  return <RawLabel style={cascadingTextStyle} text={entities.decodeHTML(data).replace(/\n/, '')} />;
+};
