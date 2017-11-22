1c02e23ed2390db34ba4758ae5f99a731846db69
Boris Yankov <borisyankov@gmail.com>
2017-11-22 21:30:45 +0200

    Add selectors for users by status and tests (#1449)

--- src/nav/__tests__/navigationSelector-test.js
+++ src/nav/__tests__/navigationSelector-test.js
@@ -8,14 +8,14 @@ import {
 
 describe('getNavigationRoutes', () => {
   test('return routes in navigation stack', () => {
-    const state = {
+    const state = deepFreeze({
       nav: {
         index: 1,
         routes: [{ routeName: 'first' }, { routeName: 'second' }],
       },
-    };
-    deepFreeze(state);
+    });
     const expectedResult = [{ routeName: 'first' }, { routeName: 'second' }];
+
     const actualResult = getNavigationRoutes(state);
 
     expect(actualResult).toEqual(expectedResult);
@@ -24,23 +24,22 @@ describe('getNavigationRoutes', () => {
 
 describe('getNavigationIndex', () => {
   test('return index of the current route in navigation', () => {
-    const state = {
+    const state = deepFreeze({
       nav: {
         index: 1,
         routes: [{ routeName: 'first' }, { routeName: 'second' }],
       },
-    };
-    deepFreeze(state);
-    const expectedResult = 1;
+    });
+
     const actualResult = getNavigationIndex(state);
 
-    expect(actualResult).toEqual(expectedResult);
+    expect(actualResult).toEqual(1);
   });
 });
 
 describe('getCurrentRouteParams', () => {
   test('return params of the current route', () => {
-    const state = {
+    const state = deepFreeze({
       nav: {
         index: 1,
         routes: [
@@ -48,9 +47,9 @@ describe('getCurrentRouteParams', () => {
           { routeName: 'second', params: { email: 'b@a.com' } },
         ],
       },
-    };
-    deepFreeze(state);
+    });
     const expectedResult = { email: 'b@a.com' };
+
     const actualResult = getCurrentRouteParams(state);
 
     expect(actualResult).toEqual(expectedResult);
@@ -59,7 +58,7 @@ describe('getCurrentRouteParams', () => {
 
 describe('getCurrentRoute', () => {
   test('return name of the current route', () => {
-    const state = {
+    const state = deepFreeze({
       nav: {
         index: 1,
         routes: [
@@ -67,9 +66,9 @@ describe('getCurrentRoute', () => {
           { routeName: 'second', params: { email: 'b@a.com' } },
         ],
       },
-    };
-    deepFreeze(state);
+    });
     const expectedResult = 'second';
+
     const actualResult = getCurrentRoute(state);
 
     expect(actualResult).toEqual(expectedResult);
