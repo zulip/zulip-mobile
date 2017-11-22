2c741110666869bb1267a6a80e19bc4f9c61353e
Boris Yankov <borisyankov@gmail.com>
2017-11-22 13:23:09 +0200

    Refactor event polling (#1446)

--- src/events/eventActions.js
+++ src/events/eventActions.js
@@ -1,68 +1,75 @@
 /* @flow */
 import { batchActions } from 'redux-batched-actions';
 
-import type { Dispatch, GetState } from '../types';
+import type { Action, Dispatch, GetState, GlobalState } from '../types';
 import { pollForEvents } from '../api';
 import { appRefresh } from '../actions';
 import eventToAction from './eventToAction';
 import eventMiddleware from './eventMiddleware';
 import { getAuth } from '../selectors';
 
+const responseToActions = (state: GlobalState, response) => {
+  const actions = [];
+  for (const event of response.events) {
+    eventMiddleware(state, event);
+    const action = eventToAction(state, event);
+
+    if (action === 'ignore') continue;
+
+    if (!action || action === 'unknown') {
+      console.log('Can not handle event', event); // eslint-disable-line
+      continue;
+    }
+
+    if (!action.type) {
+      console.log('Can not handle event', event); // eslint-disable-line
+      continue;
+    }
+
+    actions.push({ ...action, eventId: event.id });
+  }
+  return actions;
+};
+
+const dispatchOrBatch = (dispatch: Dispatch, actions: Action[]) => {
+  if (actions.length > 1) {
+    dispatch(batchActions(actions));
+  } else if (actions.length === 1) {
+    dispatch(actions[0]);
+  }
+};
+
 export const startEventPolling = (queueId: number, eventId: number) => async (
   dispatch: Dispatch,
   getState: GetState,
 ) => {
+  const state = getState();
+
   let lastEventId = eventId;
 
-  // Event loop
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    let response;
+    const auth = getAuth(state);
     try {
       // eslint-disable-next-line no-await-in-loop
-      response = await pollForEvents(getAuth(getState()), queueId, lastEventId);
-    } catch (e) {
-      // Stop polling - user likely switched accounts or logged out
-      if (queueId !== getState().app.eventQueueId || getAuth(getState()).apiKey === '') {
+      const response = await pollForEvents(auth, queueId, lastEventId);
+
+      // User switched accounts or logged out
+      if (queueId !== state.app.eventQueueId || auth.apiKey === '') {
         break;
       }
 
-      // Force a refresh of the app if the event queue is too old
-      // or has been garbage collected
+      const actions = responseToActions(state, response);
+
+      dispatchOrBatch(dispatch, actions);
+
+      lastEventId = Math.max.apply(null, [lastEventId, ...response.events.map(x => x.id)]);
+    } catch (e) {
+      // The event queue is too old or has been garbage collected
       if (e.message.indexOf('too old') !== -1 || e.message.indexOf('Bad event queue id') !== -1) {
-        // Force a refresh (index 0 is the currently active account)
         dispatch(appRefresh());
         break;
       }
-
-      continue; // eslint-disable-line no-continue
-    }
-    // Stop polling - user likely switched accounts or logged out
-    if (queueId !== getState().app.eventQueueId) {
-      break;
-    }
-
-    const actions = [];
-    for (const event of response.events) {
-      lastEventId = Math.max(lastEventId, event.id);
-      eventMiddleware(getState(), event);
-      const action = eventToAction(getState(), event);
-
-      if (action !== 'ignore') {
-        if (action && action.type) {
-          actions.push({ ...action, eventId: event.id });
-        } else {
-          console.log('Can not handle event or operation', event); // eslint-disable-line
-        }
-      }
-    }
-
-    if (actions.length > 1) {
-      // Batch actions together to speed up rendering
-      // (especially when resuming from a suspended state)
-      dispatch(batchActions(actions));
-    } else if (actions.length === 1) {
-      dispatch(actions[0]);
     }
   }
 };
