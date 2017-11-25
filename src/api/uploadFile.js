035b4c72ec033bf1f161fd5c0522ed87180b5ef6
Boris Yankov <borisyankov@gmail.com>
2017-11-25 13:51:46 +0200

    Image upload fixes and improvements (#1457)

--- src/api/uploadFile.js
+++ src/api/uploadFile.js
@@ -5,8 +5,9 @@ import { getFileExtension, getMimeTypeFromFileExtension } from '../utils/url';
 
 export default (auth: Auth, uri: string, name: string) => {
   const formData = new FormData();
-  const type = getMimeTypeFromFileExtension(getFileExtension(name));
+  const extension = getFileExtension(name);
+  const type = getMimeTypeFromFileExtension(extension);
   // $FlowFixMe
-  formData.append('file', { uri, name, type });
+  formData.append('file', { uri, name, type, extension });
   return apiFile(auth, 'user_uploads', res => res.uri, formData);
 };
