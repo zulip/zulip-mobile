f5196bfab3ac919148642dd1f1628793eea466b0
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-11-27 19:09:27 +0530

    Use getNarrowFromMessage function.

--- src/chat/chatReducers.js
+++ src/chat/chatReducers.js
@@ -14,7 +14,7 @@ import {
   EVENT_REACTION_REMOVE,
   EVENT_UPDATE_MESSAGE,
 } from '../actionConstants';
-import { homeNarrow, isMessageInNarrow, topicNarrow, groupNarrow } from '../utils/narrow';
+import { homeNarrow, isMessageInNarrow, getNarrowFromMessage } from '../utils/narrow';
 import chatUpdater from './chatUpdater';
 import { getMessagesById } from '../selectors';
 import { NULL_ARRAY, NULL_OBJECT } from '../nullObjects';
@@ -109,22 +109,15 @@ export default (state: ChatState = initialState, action: Action) => {
       if (!stateChange) {
         // new message is in new narrow in which we don't have any message
         const { message } = action;
-        let key;
-        if (message.type === 'stream') {
-          key = JSON.stringify(topicNarrow(message.display_recipient, message.subject));
-        } else if (message.type === 'private') {
-          key = JSON.stringify(groupNarrow(message.display_recipient.map(user => user.email)));
-        }
-        if (key) {
-          stateChange = true;
-          newState = {
-            ...state,
-            messages: {
-              ...state.messages,
-              [key]: [action.message],
-            },
-          };
-        }
+        const key = JSON.stringify(getNarrowFromMessage(message));
+        stateChange = true;
+        newState = {
+          ...state,
+          messages: {
+            ...state.messages,
+            [key]: [action.message],
+          },
+        };
       }
       return stateChange ? newState : state;
     }
