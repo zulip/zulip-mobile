6cbe94784244a91db9473635b4d28dbaffe13b2b
Boris Yankov <borisyankov@gmail.com>
2017-11-30 00:22:05 +0200

    Fix crash related to keyboard adjustment

--- src/message/InfiniteScrollView.js
+++ src/message/InfiniteScrollView.js
@@ -80,19 +80,24 @@ export default class InfiniteScrollView extends PureComponent<Props, State> {
 
   _keyboardShow(e) {
     this._keyboardHeight = e.endCoordinates.height;
-    if (_scrollOffset !== undefined) {
+
+    if (!listComponent) {
+      return;
+    }
+
+    if (_scrollOffset === 0) {
+      listComponent.scrollToEnd();
+    } else {
       listComponent.scrollTo({
         x: 0,
         y: _scrollOffset + this._keyboardHeight,
         animated: true,
       });
-    } else if (_scrollOffset === 0) {
-      listComponent.scrollToEnd();
     }
   }
 
   _keyboardHide() {
-    if (_scrollOffset) {
+    if (_scrollOffset && listComponent) {
       listComponent.scrollTo({
         x: 0,
         y: _scrollOffset - this._keyboardHeight,
