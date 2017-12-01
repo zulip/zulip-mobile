228a7804219ee6a9d6aca2f0ee1f40acec705e1b
Kunal Gupta <kunall.gupta17@gmail.com>
2017-12-02 01:10:55 +0530

    Move the home, private, starred icons to root sidebar (#1480)

--- src/nav/StreamTabsCard.js
+++ src/nav/StreamTabsCard.js
@@ -2,24 +2,16 @@
 import React, { PureComponent } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import connectWithActions from '../connectWithActions';
-import type { Actions, Narrow } from '../types';
-import { homeNarrow, specialNarrow, allPrivateNarrow } from '../utils/narrow';
-import NavButton from './NavButton';
+import type { Narrow } from '../types';
 import StreamTabs from './StreamTabs';
 
 const componentStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  iconList: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
 });
 
 type Props = {
-  actions: Actions,
   doNarrowCloseDrawer: (narrow: Narrow) => void,
 };
 
@@ -27,24 +19,14 @@ class StreamTabsCard extends PureComponent<Props> {
   props: Props;
 
   render() {
-    const { actions, doNarrowCloseDrawer } = this.props;
+    const { doNarrowCloseDrawer } = this.props;
 
     return (
       <View style={componentStyles.container}>
-        <View style={componentStyles.iconList}>
-          <NavButton name="home" onPress={() => doNarrowCloseDrawer(homeNarrow)} />
-          <NavButton name="mail" onPress={() => doNarrowCloseDrawer(allPrivateNarrow)} />
-          <NavButton name="star" onPress={() => doNarrowCloseDrawer(specialNarrow('starred'))} />
-          <NavButton
-            name="at-sign"
-            onPress={() => doNarrowCloseDrawer(specialNarrow('mentioned'))}
-          />
-          <NavButton name="search" onPress={actions.navigateToSearch} />
-        </View>
         <StreamTabs screenProps={{ doNarrowCloseDrawer }} />
       </View>
     );
   }
 }
 
-export default connectWithActions(null)(StreamTabsCard);
+export default StreamTabsCard;
