0a22357da7b64d5d616107a7a7bca7191702f3ad
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-12-12 03:18:01 +0530

    Send logs to Sentry only if it is enabled. (#1526)

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -3,12 +3,16 @@ import { Sentry, SentrySeverity } from 'react-native-sentry';
 import config from '../config';
 
 export const logErrorRemotely = (e: Error, msg: string) => {
-  Sentry.captureException(e);
+  if (config.enableSentry) {
+    Sentry.captureException(e);
+  }
   if (config.enableErrorConsoleLogging) console.log(msg || '', e); // eslint-disable-line
 };
 
 export const logWarningToSentry = (msg: string) => {
-  Sentry.captureMessage(msg, {
-    level: SentrySeverity.Warning,
-  });
+  if (config.enableSentry) {
+    Sentry.captureMessage(msg, {
+      level: SentrySeverity.Warning,
+    });
+  }
 };
