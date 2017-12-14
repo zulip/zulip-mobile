7b8e7eebf80ead1b01dce8c9792642d3863ea9ed
Boris Yankov <borisyankov@gmail.com>
2017-12-14 23:17:00 +0200

    Add section separator components (#1543)

---
+++ src/common/SectionSeparator.js
@@ -0,0 +1,18 @@
+/* @flow */
+import React, { PureComponent } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: 'rgba(127, 127, 127, 0.75)',
+  },
+});
+
+export default class SectionSeparator extends PureComponent<{}> {
+  render() {
+    return <View style={styles.separator} />;
+  }
+}
