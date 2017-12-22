79484594cc8bef6af7edc739732abc4fd4967fc6
kunall17 <kunall.gupta17@gmail.com>
2017-12-22 08:59:51 +0530

    Enable flow in notifications.ios.js

--- src/realm/realmActions.js
+++ src/realm/realmActions.js
@@ -1,7 +1,6 @@
 /* @flow */
 import type { GetState, Dispatch, Action } from '../types';
 import { initializeNotifications } from '../utils/notifications';
-import { switchNarrow } from '../message/messagesActions';
 import { getAuth } from '../selectors';
 
 import { REALM_INIT, SAVE_TOKEN_PUSH, DELETE_TOKEN_PUSH } from '../actionConstants';
@@ -23,9 +22,7 @@ const saveTokenPush = (pushToken: string, result: string, msg: string) => ({
 });
 
 export const initNotifications = (): Action => (dispatch: Dispatch, getState: GetState) => {
-  initializeNotifications(
-    getAuth(getState()),
-    (token, msg, result) => dispatch(saveTokenPush(token, result, msg)),
-    switchNarrow,
+  initializeNotifications(getAuth(getState()), (token, msg, result) =>
+    dispatch(saveTokenPush(token, result, msg)),
   );
 };
