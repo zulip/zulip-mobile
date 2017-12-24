c39c715e8d8cdf80cdf1f9857067798d57a6e1de
Boris Yankov <borisyankov@gmail.com>
2017-12-25 00:50:48 +0200

    Better fetch and anchoring in web message list

--- src/message/html/renderMessagesAsHtml.js
+++ src/message/html/renderMessagesAsHtml.js
@@ -2,45 +2,41 @@ import { getFullUrl } from '../../utils/url';
 
 import messageAsHtml from './messageAsHtml';
 import messageHeaderAsHtml from './messageHeaderAsHtml';
-import messageLoadingListAsHtml from './messageLoadingListAsHtml';
 import timeRowAsHtml from './timeRowAsHtml';
 
-export default ({ auth, subscriptions, renderedMessages, isFetching, narrow, doNarrow }) =>
-  renderedMessages.reduce(
-    (list, section, index) => {
-      list.push(
-        messageHeaderAsHtml({
-          auth,
-          item: section.message,
-          subscriptions,
-          narrow,
-        }),
-      );
+export default ({ auth, subscriptions, renderedMessages, narrow, doNarrow }) =>
+  renderedMessages.reduce((list, section, index) => {
+    list.push(
+      messageHeaderAsHtml({
+        auth,
+        item: section.message,
+        subscriptions,
+        narrow,
+      }),
+    );
 
-      section.data.forEach((item, idx) => {
-        if (item.type === 'time') {
-          list.push(timeRowAsHtml(item.timestamp));
-        } else {
-          list.push(
-            messageAsHtml({
-              id: item.message.id,
-              isBrief: item.isBrief,
-              fromName: item.message.sender_full_name,
-              fromEmail: item.message.sender_email,
-              content: item.message.content,
-              timestamp: item.message.timestamp,
-              avatarUrl: getFullUrl(item.message.avatar_url, auth ? auth.realm : ''),
-              timeEdited: item.message.last_edit_timestamp,
-              isOutbox: item.message.isOutbox,
-              isStarred: (item.message.flags || []).indexOf('starred') > -1,
-              reactions: item.message.reactions,
-              ownEmail: auth.email,
-            }),
-          );
-        }
-      });
+    section.data.forEach((item, idx) => {
+      if (item.type === 'time') {
+        list.push(timeRowAsHtml(item.timestamp));
+      } else {
+        list.push(
+          messageAsHtml({
+            id: item.message.id,
+            isBrief: item.isBrief,
+            fromName: item.message.sender_full_name,
+            fromEmail: item.message.sender_email,
+            content: item.message.content,
+            timestamp: item.message.timestamp,
+            avatarUrl: getFullUrl(item.message.avatar_url, auth ? auth.realm : ''),
+            timeEdited: item.message.last_edit_timestamp,
+            isOutbox: item.message.isOutbox,
+            isStarred: (item.message.flags || []).indexOf('starred') > -1,
+            reactions: item.message.reactions,
+            ownEmail: auth.email,
+          }),
+        );
+      }
+    });
 
-      return list; // eslint-disable-next-line
-    },
-    [messageLoadingListAsHtml(isFetching)],
-  );
+    return list; // eslint-disable-next-line
+  }, []);
