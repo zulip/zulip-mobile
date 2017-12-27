2642a388dac78804ff9ffc2dc0df83a8bb60803f
Aswin Mohan <git.aswinmohan@gmail.com>
2017-12-27 22:18:50 +0000

    Fix AutoComplete from showing up when typing Emails (#1574)
    * Fix AutoComplete from showing up when typing Emails
    
    The AutoComplete View pops up when typing '@' to mention usernames, but when typing email addresses the same View pops up. This was fixed by checking whether the previous letter before the '@' is an empty space. This stops the view from showing as the letter before '@' would never be a space in an email.
    
    Fixes:#1553
    
    * Mention can follow a blank space, hash and a colon
    
    If we enforce that the @ should be preceded by a blank space, then ':@abc' would look for an emoji with @abc. To prevent that we add ':' and '#' with the space.
    
    * Fix Failing Tests

--- src/autocomplete/getAutocompleteFilter.js
+++ src/autocomplete/getAutocompleteFilter.js
@@ -10,8 +10,11 @@ export default (text: string, selection: InputSelectionType) => {
   const lastIndex: number = Math.max(
     text.lastIndexOf(':'),
     text.lastIndexOf('#'),
-    text.lastIndexOf('@'),
+    [' ', '#', ':'].includes(text[text.lastIndexOf('@') - 1]) || text.lastIndexOf('@') === 0
+      ? text.lastIndexOf('@')
+      : -1,
   );
+
   const lastWordPrefix: string = lastIndex !== -1 ? text[lastIndex] : '';
   const filter: string =
     text.length > lastIndex + 1 ? text.substring(lastIndex + 1, text.length) : '';
