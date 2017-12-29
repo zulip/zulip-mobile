476052839c0b939df80b27b107281f26acbae80e
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2017-12-30 01:17:56 +0530

    Fix:multiple hardware back button presses are required to navigate back.
    Fix: #1456

--- src/main/MainScreenWithDrawers.js
+++ src/main/MainScreenWithDrawers.js
@@ -1,9 +1,12 @@
 /* @TODO flow */
 import React, { PureComponent } from 'react';
+import { BackHandler } from 'react-native';
 import { DrawerNavigator } from 'react-navigation';
 
 import MainScreen from './MainScreen';
 import Sidebar from '../nav/Sidebar';
+import connectWithActions from '../connectWithActions';
+import { getCanGoBack } from '../selectors';
 
 export const Drawer = DrawerNavigator(
   {
@@ -21,8 +24,30 @@ export const Drawer = DrawerNavigator(
   },
 );
 
-export default class MainScreenWithDrawers extends PureComponent<{}> {
+class MainScreenWithDrawers extends PureComponent<{}> {
+  componentDidMount() {
+    // to ovveride with TabsNavigator
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonPress);
+  }
+
+  handleBackButtonPress = () => {
+    const { canGoBack, actions } = this.props;
+    if (canGoBack) {
+      actions.navigateBack();
+    }
+    return canGoBack;
+  };
+
   render() {
     return <Drawer />;
   }
 }
+
+export default connectWithActions(state => ({
+  nav: state.nav,
+  canGoBack: getCanGoBack(state),
+}))(MainScreenWithDrawers);
