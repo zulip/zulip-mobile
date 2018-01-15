f0f98a8f4e6778c47888d6e0188dccf735c67dd3
Boris Yankov <borisyankov@gmail.com>
2018-01-15 19:52:56 +0200

    Fix fetching display and message overflow (#1749)

--- src/render-html/webViewHandleUpdates.js
+++ src/render-html/webViewHandleUpdates.js
@@ -8,39 +8,51 @@ import messageTypingAsHtml from './messageTypingAsHtml';
 
 let previousContent = '';
 
+const updateContent = (prevProps: Props, nextProps: Props, sendMessage: any => void) => {
+  const content = htmlBody(renderMessagesAsHtml(nextProps), nextProps.showMessagePlaceholders);
+
+  if (content !== previousContent) {
+    previousContent = content;
+    sendMessage({
+      type: 'content',
+      anchor: nextProps.anchor,
+      sameNarrow: isEqual(prevProps.narrow, nextProps.narrow),
+      content,
+    });
+  }
+};
+
+const updateFetching = (prevProps: Props, nextProps: Props, sendMessage: any => void) => {
+  sendMessage({
+    type: 'fetching',
+    showMessagePlaceholders: nextProps.showMessagePlaceholders,
+    fetchingOlder: nextProps.fetching.older && !nextProps.showMessagePlaceholders,
+    fetchingNewer: nextProps.fetching.newer && !nextProps.showMessagePlaceholders,
+  });
+};
+
+const updateTyping = (prevProps: Props, nextProps: Props, sendMessage: any => void) => {
+  sendMessage({
+    type: 'typing',
+    content: nextProps.typingUsers
+      ? messageTypingAsHtml(nextProps.auth.realm, nextProps.typingUsers)
+      : '',
+  });
+};
+
 export default (prevProps: Props, nextProps: Props, sendMessage: any => void) => {
+  if (prevProps.renderedMessages !== nextProps.renderedMessages) {
+    updateContent(prevProps, nextProps, sendMessage);
+  }
+
   if (
     !isEqual(prevProps.fetching, nextProps.fetching) ||
     prevProps.showMessagePlaceholders !== nextProps.showMessagePlaceholders
   ) {
-    sendMessage({
-      type: 'fetching',
-      showMessagePlaceholders: nextProps.showMessagePlaceholders,
-      fetchingOlder: nextProps.fetching.older && !nextProps.showMessagePlaceholders,
-      fetchingNewer: nextProps.fetching.newer && !nextProps.showMessagePlaceholders,
-    });
-  }
-
-  if (prevProps.renderedMessages !== nextProps.renderedMessages) {
-    const content = htmlBody(renderMessagesAsHtml(nextProps), nextProps.showMessagePlaceholders);
-
-    if (content !== previousContent) {
-      previousContent = content;
-      sendMessage({
-        type: 'content',
-        anchor: nextProps.anchor,
-        sameNarrow: isEqual(prevProps.narrow, nextProps.narrow),
-        content,
-      });
-    }
+    updateFetching(prevProps, nextProps, sendMessage);
   }
 
   if (prevProps.typingUsers !== nextProps.typingUsers) {
-    sendMessage({
-      type: 'typing',
-      content: nextProps.typingUsers
-        ? messageTypingAsHtml(nextProps.auth.realm, nextProps.typingUsers)
-        : '',
-    });
+    updateTyping(prevProps, nextProps, sendMessage);
   }
 };
