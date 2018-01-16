4cb4bb75e30749ceadcecc282ce9202d96c5aec5
Boris Yankov <borisyankov@gmail.com>
2018-01-16 23:32:55 +0200

    Send unread message ids on scroll. Add tests

--- src/utils/__tests__/unread-test.js
+++ src/utils/__tests__/unread-test.js
@@ -1,4 +1,4 @@
-import { countUnread, filterUnreadMessageIds } from '../unread';
+import { countUnread, filterUnreadMessageIds, filterUnreadMessagesInRange } from '../unread';
 
 describe('filterUnreadMessageIds', () => {
   test('empty message list has no unread messages', () => {
@@ -31,6 +31,52 @@ describe('filterUnreadMessageIds', () => {
     const expectedUnread = [1];
 
     const actualUnread = filterUnreadMessageIds(messages, flags);
+
+    expect(actualUnread).toEqual(expectedUnread);
+  });
+});
+
+describe('filterUnreadMessagesInRange', () => {
+  test('if from or to ids are -1 result is empty', () => {
+    const messages = [{ id: 1 }];
+    const flags = {};
+    const expectedUnread = [];
+
+    const actualUnread = filterUnreadMessagesInRange(messages, flags, -1, -1);
+
+    expect(actualUnread).toEqual(expectedUnread);
+  });
+
+  test('empty message list has no unread messages', () => {
+    const messages = [];
+    const flags = {};
+    const expectedUnread = [];
+
+    const actualUnread = filterUnreadMessagesInRange(messages, flags, 1, 5);
+
+    expect(actualUnread).toEqual(expectedUnread);
+  });
+
+  test('messages with no flags or empty flag array are not read', () => {
+    const messages = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
+    const flags = {
+      read: {
+        3: true,
+      },
+    };
+    const expectedUnread = [2, 4, 5];
+
+    const actualUnread = filterUnreadMessagesInRange(messages, flags, 2, 5);
+
+    expect(actualUnread).toEqual(expectedUnread);
+  });
+
+  test('messages are not read if not in flags object, regardless of message property', () => {
+    const messages = [{ id: 1 }];
+    const flags = {};
+    const expectedUnread = [1];
+
+    const actualUnread = filterUnreadMessagesInRange(messages, flags, 1, 5);
 
     expect(actualUnread).toEqual(expectedUnread);
   });
