10a93b63201148a6989db935268eaee9ea858bd9
Boris Yankov <borisyankov@gmail.com>
2018-01-19 16:23:51 +0200

    Refactor and simplify message rendering

--- src/render-html/messageAsHtml.js
+++ src/render-html/messageAsHtml.js
@@ -10,14 +10,29 @@ const messageDiv = (id, msgClass, flags) =>
      ${flags.map(flag => `data-${flag}="true" `).join('')}
     >`;
 
+const messageSubheader = ({ fromName, timestamp, twentyFourHourTime }) => `
+<div class="subheader">
+  <div class="username">
+    ${fromName}
+  </div>
+  <div class="timestamp">
+    ${shortTime(timestamp * 1000, twentyFourHourTime)}
+  </div>
+</div>
+`;
+
+const messageBody = ({ content, flags, isOutbox, id, ownEmail, timeEdited, reactions }) => `
+<div class="msg-raw-content">
+  ${content}
+</div>
+${messageTagsAsHtml(flags, timeEdited, isOutbox)}
+${messageReactionListAsHtml(reactions, id, ownEmail)}
+`;
+
 const briefMessageAsHtml = ({ id, content, flags, timeEdited, isOutbox, reactions, ownEmail }) => `
 ${messageDiv(id, 'message-brief', flags)}
   <div class="content">
-    <div class="msg-raw-content">
-      ${content}
-    </div>
-    ${messageTagsAsHtml(flags, timeEdited, isOutbox)}
-    ${messageReactionListAsHtml(reactions, id, ownEmail)}
+    ${messageBody({ content, flags, isOutbox, id, ownEmail, timeEdited, reactions })}
   </div>
 </div>
 `;
@@ -43,19 +58,8 @@ ${messageDiv(id, 'message-full', flags)}
     <img src="${avatarUrl}" class="avatar-img" data-email="${fromEmail}">
   </div>
   <div class="content">
-    <div class="subheader">
-      <div class="username">
-        ${fromName}
-      </div>
-      <div class="timestamp">
-        ${shortTime(timestamp * 1000, twentyFourHourTime)}
-      </div>
-    </div>
-    <div class="msg-raw-content">
-      ${content}
-    </div>
-    ${messageTagsAsHtml(flags, timeEdited, isOutbox)}
-    ${messageReactionListAsHtml(reactions, id, ownEmail)}
+    ${messageSubheader({ fromName, timestamp, twentyFourHourTime })}
+    ${messageBody({ content, flags, isOutbox, id, ownEmail, timeEdited, reactions })}
   </div>
 </div>
 `;
