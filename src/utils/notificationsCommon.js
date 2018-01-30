661b7468d9f0bf506964f800e99d6ebe0f7037f1
Kunal Gupta <kunall.gupta17@gmail.com>
2018-01-31 01:15:17 +0530

    Call tryInitalNotification before REHYDRATE for proper notification narrow (#1839)

--- src/utils/notificationsCommon.js
+++ src/utils/notificationsCommon.js
@@ -1,15 +1,9 @@
 /* @flow */
-import type { Actions } from '../types';
 import config from '../config';
 import { topicNarrow, privateNarrow } from '../utils/narrow';
 
-export const handleNotification = (
-  data: Object,
-  doNarrow: Actions.doNarrow,
-  anchor: number,
-): void => {
+export const handleNotification = (data: Object, anchor: number): void => {
   if (!data || !data.recipient_type) return;
-
   if (data.recipient_type === 'stream') {
     config.startup.narrow = topicNarrow(data.stream, data.topic);
   } else if (data.recipient_type === 'private') {
