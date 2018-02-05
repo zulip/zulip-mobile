5798110a7c63337a07f1df804715dffe69311758
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-02-05 23:54:23 +0530

    animation: Fix out animation is broken in AnimatedScaleComponent.
    Update display style after animation have successfully ended.

--- src/animation/AnimatedScaleComponent.js
+++ src/animation/AnimatedScaleComponent.js
@@ -14,19 +14,27 @@ type Props = {
 export default class AnimatedScaleComponent extends PureComponent<Props> {
   props: Props;
 
+  state = {
+    visible: this.props.visible,
+  };
+
   animatedValue = new Animated.Value(this.props.visible ? 1 : 0);
 
   componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.visible) {
+      this.setState({ visible: true });
+    }
     Animated.timing(this.animatedValue, {
       toValue: nextProps.visible ? 1 : 0,
       duration: 300,
       useNativeDriver: true,
       easing: Easing.elastic(),
-    }).start();
+    }).start((finished: boolean) => finished && this.setState({ visible: nextProps.visible }));
   }
 
   render() {
-    const { children, visible, style } = this.props;
+    const { children, style } = this.props;
+    const { visible } = this.state;
     const animatedStyle = {
       transform: [{ scale: this.animatedValue }],
       opacity: this.animatedValue,
