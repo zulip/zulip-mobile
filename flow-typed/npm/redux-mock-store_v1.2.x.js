887ee207578c8073d7dd3cf9c51ed645caa47d27
Boris Yankov <borisyankov@gmail.com>
2018-02-10 19:19:38 +0200

    Upgrade to React Native 0.53 (#1874)

--- flow-typed/npm/redux-mock-store_v1.2.x.js
+++ flow-typed/npm/redux-mock-store_v1.2.x.js
@@ -1,7 +1,7 @@
-// flow-typed signature: 46cfe61426b707ae9e2e03f873d0eb95
-// flow-typed version: b43dff3e0e/redux-mock-store_v1.2.x/flow_>=v0.25.x
+// flow-typed signature: 9e2769d1002704a6dd0533f4362869d2
+// flow-typed version: 614bf49aa8/redux-mock-store_v1.2.x/flow_>=v0.25.x
 
-declare module 'redux-mock-store' {
+declare module "redux-mock-store" {
   /*
     S = State
     A = Action
@@ -19,13 +19,13 @@ declare module 'redux-mock-store' {
     replaceReducer(nextReducer: Function): void
   };
 
-  declare function exports(middlewares: ?Array<Function>): mockStore;
+  declare module.exports: (middlewares: ?Array<Function>) => mockStore;
 }
 
 // Filename aliases
-declare module 'redux-mock-store/src/index' {
-  declare module.exports: $Exports<'redux-mock-store'>;
+declare module "redux-mock-store/src/index" {
+  declare module.exports: $Exports<"redux-mock-store">;
 }
-declare module 'redux-mock-store/src/index.js' {
-  declare module.exports: $Exports<'redux-mock-store'>;
+declare module "redux-mock-store/src/index.js" {
+  declare module.exports: $Exports<"redux-mock-store">;
 }
