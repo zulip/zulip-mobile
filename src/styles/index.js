1e7be0733a8dfc3e4863dcc1575925b23ec59ea3
Aakar Sharma <2016ucs0066@iitjammu.ac.in>
2018-02-14 16:09:52 +0530

    Changed the value of tintColor of the Switch.

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,6 +3,7 @@
 export { CONTROL_SIZE, NAVBAR_SIZE, REACTION_HEIGHT, REACTION_SPINNER_OFFSET } from './platform';
 
 export const BRAND_COLOR = 'rgba(82, 194, 175, 1)';
+export const BRAND_OFF_COLOR = 'rgba(220, 220, 220, 1)';
 export const BORDER_COLOR = BRAND_COLOR;
 export const HIGHLIGHT_COLOR = 'rgba(86, 164, 174, 0.5)';
 export const HALF_COLOR = 'rgba(127, 127, 127, 0.5)';
