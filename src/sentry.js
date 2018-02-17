8140d0b513999a3ed3dff3e7e83ad1d2606269ed
Boris Yankov <borisyankov@gmail.com>
2018-02-17 18:48:00 +0200

    Rework Sentry config. Add error ignoring

---
+++ src/sentry.js
@@ -0,0 +1,11 @@
+/* @flow */
+import { Sentry } from 'react-native-sentry';
+
+import config from './config';
+
+if (config.enableSentry) {
+  Sentry.config(config.sentryKey, {
+    deactivateStacktraceMerging: true,
+    ignoreErrors: ['Network request failed'],
+  }).install();
+}
