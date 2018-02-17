443886aacb1dbe005744dbc3153aa24c6d2d2d29
Boris Yankov <borisyankov@gmail.com>
2018-02-17 18:55:54 +0200

    Rework Sentry config. Add error ignoring (#1941)

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -1,9 +1,7 @@
 /* @flow */
 import React from 'react';
-import { Sentry } from 'react-native-sentry';
 
 import '../vendor/intl/intl';
-import config from './config';
 import StoreProvider from './boot/StoreProvider';
 import TranslationProvider from './boot/TranslationProvider';
 import StylesProvider from './boot/StylesProvider';
@@ -13,12 +11,9 @@ import AppDataFetcher from './boot/AppDataFetcher';
 import AppWithNavigationState from './nav/AppWithNavigationState';
 
 require('./i18n/locale');
+require('./sentry');
 
 // console.disableYellowBox = true; // eslint-disable-line
-
-if (config.enableSentry) {
-  Sentry.config(config.sentryKey, { deactivateStacktraceMerging: true }).install();
-}
 
 export default () => (
   <CompatibilityChecker>
