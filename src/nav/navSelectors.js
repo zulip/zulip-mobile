9e7c393bc0ab45baae25eb4703c5ad8c015d37d3
Boris Yankov <borisyankov@gmail.com>
2018-02-18 17:30:28 +0200

    Calculate navigation if push notification on startup (#1945)

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -2,26 +2,39 @@
 import { createSelector } from 'reselect';
 
 import type { GlobalState } from '../types';
+import config from '../config';
 import { getNav, getAccounts } from '../directSelectors';
 import { getAuth } from '../account/accountSelectors';
 import AppNavigator from './AppNavigator';
 
 export const getCanGoBack = (state: GlobalState) => state.nav.index > 0;
 
-export const getStateForRoute = (route: string) =>
-  AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams(route));
+export const getStateForRoute = (route: string, params?: Object) =>
+  AppNavigator.router.getStateForAction(
+    AppNavigator.router.getActionForPathAndParams(route, params),
+  );
 
-export const getInitialRoute = createSelector(
+export const getInitialNavState = createSelector(
   getNav,
   getAccounts,
   getAuth,
   (nav, accounts, auth) => {
-    if (auth.apiKey) {
-      return nav.routes.length === 1 && nav.routes[0].routeName === 'loading'
-        ? getStateForRoute('main')
-        : nav;
+    if (!auth.apiKey) {
+      return getStateForRoute(accounts && accounts.length > 1 ? 'account' : 'realm');
     }
 
-    return getStateForRoute(accounts && accounts.length > 1 ? 'account' : 'realm');
+    const state =
+      nav.routes.length === 1 && nav.routes[0].routeName === 'loading'
+        ? getStateForRoute('main')
+        : nav;
+
+    if (config.startup.narrow) {
+      state.routes.push(
+        AppNavigator.router.getActionForPathAndParams('chat', { narrow: config.startup.narrow }),
+      );
+      state.index++;
+    }
+
+    return state;
   },
 );
