3df80c3aa5e21bd616ae8138b048d76142f4f913
Aswin Mohan <aswinmmohan@gmail.com>
2018-03-05 07:07:54 +0530

    Resize and Give More Space to the Image

--- src/common/UnreadMsgImg.js
+++ src/common/UnreadMsgImg.js
@@ -6,8 +6,9 @@ import unreadMsgImg from '../../static/img/unread-msg.png';
 
 const styles = StyleSheet.create({
   unreadMsg: {
-    width: 160,
-    height: 160,
+    width: 140,
+    height: 140,
+    margin: 20,
     alignSelf: 'center',
   },
 });
