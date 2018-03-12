34ce158d6a47e0b3c8d20445fae38ac73a2948d6
Boris Yankov <borisyankov@gmail.com>
2018-03-12 15:02:58 +0200

    Handle long stream/topic/user names inside webview message list

--- src/title/TitlePrivate.js
+++ src/title/TitlePrivate.js
@@ -7,6 +7,7 @@ import { Avatar } from '../common';
 
 const styles = StyleSheet.create({
   wrapper: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
   },
@@ -29,7 +30,9 @@ export default class TitlePrivate extends PureComponent<Props> {
     return (
       <View style={styles.wrapper}>
         <Avatar size={24} name={fullName} email={email} avatarUrl={avatarUrl} />
-        <Text style={[styles.title, { color }]}>{fullName}</Text>
+        <Text style={[styles.title, { color }]} numberOfLines={1} ellipsizeMode="tail">
+          {fullName}
+        </Text>
       </View>
     );
   }
