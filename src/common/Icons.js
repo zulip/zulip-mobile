952352f941852fa4871d1092183ed8dd8ee6d28d
harishnandan <harishshivam@gmail.com>
2018-03-14 13:32:07 +0530

    flow type added for Icons.js

--- src/common/Icons.js
+++ src/common/Icons.js
@@ -4,30 +4,36 @@ import Feather from 'react-native-vector-icons/Feather';
 import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-export const IconHome = (props: Object) => <Feather name="home" {...props} />;
-export const IconStar = (props: Object) => <Feather name="star" {...props} />;
-export const IconMention = (props: Object) => <Feather name="at-sign" {...props} />;
-export const IconSearch = (props: Object) => <Feather name="search" {...props} />;
-export const IconDone = (props: Object) => <Feather name="check" {...props} />;
-export const IconCancel = (props: Object) => <Feather name="slash" {...props} />;
-export const IconTrash = (props: Object) => <Feather name="trash-2" {...props} />;
-export const IconWarning = (props: Object) => <Feather name="alert-triangle" {...props} />;
-export const IconSend = (props: Object) => <MaterialIcon name="send" {...props} />;
-export const IconMute = (props: Object) => <MaterialIcon name="volume-off" {...props} />;
-export const IconStream = (props: Object) => <Feather name="hash" {...props} />;
-export const IconPrivate = (props: Object) => <Feather name="lock" {...props} />;
-export const IconPrivateChat = (props: Object) => <Feather name="mail" {...props} />;
-export const IconDownArrow = (props: Object) => <Feather name="chevron-down" {...props} />;
-export const IconGoogle = (props: Object) => <IoniconsIcon name="logo-google" {...props} />;
-export const IconGitHub = (props: Object) => <Feather name="github" {...props} />;
-export const IconCross = (props: Object) => <Feather name="x" {...props} />;
-export const IconSettings = (props: Object) => <Feather name="settings" {...props} />;
-export const IconRight = (props: Object) => <Feather name="chevron-right" {...props} />;
-export const IconPlus = (props: Object) => <Feather name="plus-circle" {...props} />;
-export const IconLeft = (props: Object) => <Feather name="chevron-left" {...props} />;
-export const IconPeople = (props: Object) => <Feather name="users" {...props} />;
-export const IconImage = (props: Object) => <Feather name="image" {...props} />;
-export const IconCamera = (props: Object) => <Feather name="camera" {...props} />;
-export const IconTerminal = (props: Object) => <Feather name="terminal" {...props} />;
+type Icontype = {
+  size?: number,
+  color?: string,
+  style?: StyleObj
+}
+
+export const IconHome = (props:Icontype) => <Feather name="home" {...props} />;
+export const IconStar = (props: Icontype) => <Feather name="star" {...props} />;
+export const IconMention = (props: Icontype) => <Feather name="at-sign" {...props} />;
+export const IconSearch = (props: Icontype) => <Feather name="search" {...props} />;
+export const IconDone = (props: Icontype) => <Feather name="check" {...props} />;
+export const IconCancel = (props: Icontype) => <Feather name="slash" {...props} />;
+export const IconTrash = (props: Icontype) => <Feather name="trash-2" {...props} />;
+export const IconWarning = (props: Icontype) => <Feather name="alert-triangle" {...props} />;
+export const IconSend = (props:Icontype) => <MaterialIcon name="send" {...props} />;
+export const IconMute = (props:Icontype) => <MaterialIcon name="volume-off" {...props} />;
+export const IconStream = (props: Icontype) => <Feather name="hash" {...props} />;
+export const IconPrivate = (props: Icontype) => <Feather name="lock" {...props} />;
+export const IconPrivateChat = (props: Icontype) => <Feather name="mail" {...props} />;
+export const IconDownArrow = (props: Icontype) => <Feather name="chevron-down" {...props} />;
+export const IconGoogle = (props: Icontype) => <IoniconsIcon name="logo-google" {...props} />;
+export const IconGitHub = (props: Icontype) => <Feather name="github" {...props} />;
+export const IconCross = (props: Icontype) => <Feather name="x" {...props} />;
+export const IconSettings = (props: Icontype) => <Feather name="settings" {...props} />;
+export const IconRight = (props: Icontype) => <Feather name="chevron-right" {...props} />;
+export const IconPlus = (props: Icontype) => <Feather name="plus-circle" {...props} />;
+export const IconLeft = (props: Icontype) => <Feather name="chevron-left" {...props} />;
+export const IconPeople = (props: Icontype) => <Feather name="users" {...props} />;
+export const IconImage = (props: Icontype) => <Feather name="image" {...props} />;
+export const IconCamera = (props: Icontype) => <Feather name="camera" {...props} />;
+export const IconTerminal = (props: Icontype) => <Feather name="terminal" {...props} />;
 
 export default Feather;
