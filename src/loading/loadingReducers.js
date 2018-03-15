734a7a409de572a3e992c95044370b8e2074432c
Boris Yankov <borisyankov@gmail.com>
2018-03-15 23:12:24 +0200

    More precise tracking of loading state (#2069)
    Add start loading event on initial fetching

--- src/loading/loadingReducers.js
+++ src/loading/loadingReducers.js
@@ -3,10 +3,11 @@ import type { Action, LoadingState } from '../types';
 import {
   APP_REFRESH,
   ACCOUNT_SWITCH,
+  INITIAL_FETCH_START,
+  INITIAL_FETCH_COMPLETE,
   INIT_STREAMS,
   INIT_SUBSCRIPTIONS,
   INIT_USERS,
-  REALM_INIT,
 } from '../actionConstants';
 
 const initialState: LoadingState = {
@@ -23,7 +24,16 @@ export default (state: LoadingState = initialState, action: Action): LoadingStat
     case ACCOUNT_SWITCH:
       return initialState;
 
-    case REALM_INIT:
+    case INITIAL_FETCH_START:
+      return {
+        ...state,
+        presence: true,
+        subscriptions: true,
+        unread: true,
+        users: true,
+      };
+
+    case INITIAL_FETCH_COMPLETE:
       return {
         ...state,
         presence: false,
