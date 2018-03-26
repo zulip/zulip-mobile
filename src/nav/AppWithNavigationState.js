ed52e237aff8416f23b3ae6536f4d1c4010ac007
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-03-26 12:09:52 +0530

    navigation: Implement BackHandler in AppWithNavigationstate.
    So that route can be popped out on back button from stack.

--- src/nav/AppWithNavigationState.js
+++ src/nav/AppWithNavigationState.js
@@ -1,18 +1,37 @@
 /* @noflow */
 import React, { PureComponent } from 'react';
+import { BackHandler } from 'react-native';
 import { addNavigationHelpers } from 'react-navigation';
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
 
 import connectWithActions from '../connectWithActions';
-import { getNav } from '../selectors';
+import { getCanGoBack, getNav } from '../selectors';
 import AppNavigator from './AppNavigator';
 import type { Actions } from '../types';
 
 type Props = {
   actions: Actions,
+  canGoBack: boolean,
 };
 
 class AppWithNavigation extends PureComponent<Props> {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonPress);
+  }
+
+  handleBackButtonPress = () => {
+    const { canGoBack, actions } = this.props;
+    if (canGoBack) {
+      actions.navigateBack();
+      return true;
+    }
+    return false;
+  };
+
   render() {
     const { dispatch, nav } = this.props;
     const addListener = createReduxBoundAddListener('root');
@@ -30,5 +49,6 @@ class AppWithNavigation extends PureComponent<Props> {
 }
 
 export default connectWithActions(state => ({
+  canGoBack: getCanGoBack(state),
   nav: getNav(state),
 }))(AppWithNavigation);
