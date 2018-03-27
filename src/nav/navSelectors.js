b64df7dc11941b8c030f8a491e8ac60c8bd7f84e
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-03-27 23:41:51 +0530

    navigation: Fix regression in getCanGoBack selector.
    If current route is lighbox then also we can go back.
    Created new selector isCurrentRouteIsLightbox, which
    can be used to distinguish such cases and back arrow
    button can be rendered accordingly.
    
    Fix: #2127.
    
    Also fix re-render of MainNavBar on navigating to/back
    from lightbox.

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -10,8 +10,7 @@ import { getAuth } from '../account/accountSelectors';
 import AppNavigator from './AppNavigator';
 import { getNarrowFromNotificationData } from '../utils/notificationsCommon';
 
-export const getCanGoBack = (state: GlobalState) =>
-  state.nav.index > 0 && state.nav.routes[state.nav.index].routeName !== 'lightbox';
+export const getCanGoBack = (state: GlobalState) => state.nav.index > 0;
 
 export const getSameRoutesCount = createSelector(getNav, nav => {
   let i = nav.routes.length - 1;
