95862725bff4d927bf5505c8d2b72f9acd5702c9
Harshul Sharma <harshulsharma000@outlook.com>
2018-03-30 19:13:05 +0530

    Moved ZulipStatusBar to AppNavigator

--- src/nav/AppWithNavigationState.js
+++ src/nav/AppWithNavigationState.js
@@ -1,11 +1,12 @@
 /* @noflow */
 import React, { PureComponent } from 'react';
-import { BackHandler } from 'react-native';
+import { BackHandler, View } from 'react-native';
 import { addNavigationHelpers } from 'react-navigation';
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
 
 import connectWithActions from '../connectWithActions';
 import { getCanGoBack, getNav } from '../selectors';
+import { ZulipStatusBar } from '../common';
 import AppNavigator from './AppNavigator';
 import type { Actions } from '../types';
 
@@ -36,13 +37,16 @@ class AppWithNavigation extends PureComponent<Props> {
     const addListener = createReduxBoundAddListener('root');
 
     return (
-      <AppNavigator
-        navigation={addNavigationHelpers({
-          state: nav,
-          dispatch,
-          addListener,
-        })}
-      />
+      <View style={{ flex:1 }}>
+        <ZulipStatusBar />
+        <AppNavigator
+          navigation={addNavigationHelpers({
+            state: nav,
+            dispatch,
+            addListener,
+          })}
+        />
+      </View>
     );
   }
 }
