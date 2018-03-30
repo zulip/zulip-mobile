53b0de5c8cdb2a2786ba257a6a0011f0132c66ad
Harshul Sharma <harshulsharma000@outlook.com>
2018-03-30 19:10:39 +0530

    Integrated reactotron into application with dev configuration, also did add console.logs()

--- src/boot/store.js
+++ src/boot/store.js
@@ -2,19 +2,20 @@
 import { AsyncStorage } from 'react-native';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { persistStore, autoRehydrate } from 'redux-persist';
+import Reactotron from 'reactotron-react-native';
 
 import rootReducer from './reducers';
 import middleware from './middleware';
 
 // AsyncStorage.clear(); // use to reset storage during development
 
-// uncomment the following lines to integrate reactotron with redux
-// const store = Reactotron.createStore(
-//   rootReducer,
-//   compose(autoRehydrate(), applyMiddleware(...middleware)),
-// );
+let store  = null; 
 
-const store = compose(applyMiddleware(...middleware), autoRehydrate())(createStore)(rootReducer);
+if(__DEV__) {
+  store = Reactotron.createStore(rootReducer, {},compose(applyMiddleware(...middleware), autoRehydrate()))
+} else {
+  store = compose(applyMiddleware(...middleware), autoRehydrate())(Reactotron.createStore)(rootReducer);
+}
 
 export const restore = (onFinished?: () => void) =>
   persistStore(
