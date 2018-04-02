023ba58ee47db02e5e8c71f7baa58277d6a4457c
Boris Yankov <borisyankov@gmail.com>
2018-04-03 02:46:28 +0300

    flow: Add types for Settings actions and reducers

--- src/settings/settingsActions.js
+++ src/settings/settingsActions.js
@@ -1,8 +1,8 @@
 /* @flow */
-import type { Actions } from '../types';
+import type { SettingsChangeAction } from '../types';
 import { SETTINGS_CHANGE } from '../actionConstants';
 
-export const settingsChange = (key: string, value: any): Actions => ({
+export const settingsChange = (key: string, value: any): SettingsChangeAction => ({
   type: SETTINGS_CHANGE,
   key,
   value,
--- src/settings/settingsReducers.js
+++ src/settings/settingsReducers.js
@@ -1,5 +1,11 @@
 /* @flow */
-import type { SettingsState, Action } from '../types';
+import type {
+  SettingsState,
+  SettingsAction,
+  RealmInitAction,
+  SettingsChangeAction,
+  EventUpdateGlobalNotificationsSettingsAction,
+} from '../types';
 import {
   SETTINGS_CHANGE,
   REALM_INIT,
@@ -15,32 +21,43 @@ const initialState: SettingsState = {
   streamNotification: false,
 };
 
-export default (state: SettingsState = initialState, action: Action): SettingsState => {
+const realmInit = (state: SettingsState, action: RealmInitAction): SettingsState => ({
+  ...state,
+  offlineNotification: action.data.enable_offline_push_notifications,
+  onlineNotification: action.data.enable_online_push_notifications,
+});
+
+const settingsChange = (state: SettingsState, action: SettingsChangeAction): SettingsState => ({
+  ...state,
+  [action.key]: action.value,
+});
+
+const eventUpdateGlobalNotificationsSettings = (
+  state: SettingsState,
+  action: EventUpdateGlobalNotificationsSettingsAction,
+): SettingsState => {
+  switch (action.notification_name) {
+    case 'enable_offline_push_notifications':
+      return { ...state, offlineNotification: action.setting };
+    case 'enable_online_push_notifications':
+      return { ...state, onlineNotification: action.setting };
+    case 'enable_stream_push_notifications':
+      return { ...state, streamNotification: action.setting };
+    default:
+      return state;
+  }
+};
+
+export default (state: SettingsState = initialState, action: SettingsAction): SettingsState => {
   switch (action.type) {
     case REALM_INIT:
-      return {
-        ...state,
-        offlineNotification: action.data.enable_offline_push_notifications,
-        onlineNotification: action.data.enable_online_push_notifications,
-      };
+      return realmInit(state, action);
 
     case SETTINGS_CHANGE:
-      return {
-        ...state,
-        [action.key]: action.value,
-      };
+      return settingsChange(state, action);
 
     case EVENT_UPDATE_GLOBAL_NOTIFICATIONS_SETTINGS:
-      switch (action.notification_name) {
-        case 'enable_offline_push_notifications':
-          return { ...state, offlineNotification: action.setting };
-        case 'enable_online_push_notifications':
-          return { ...state, onlineNotification: action.setting };
-        case 'enable_stream_push_notifications':
-          return { ...state, streamNotification: action.setting };
-        default:
-          return state;
-      }
+      return eventUpdateGlobalNotificationsSettings(state, action);
 
     default:
       return state;
