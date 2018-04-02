46cd960216b853ca5bb5d07baae1316ed766d4fe
Boris Yankov <borisyankov@gmail.com>
2018-04-02 10:52:13 +0300

    flow: Add types for realm actions

--- src/realm/realmActions.js
+++ src/realm/realmActions.js
@@ -1,20 +1,32 @@
 /* @flow */
-import type { GetState, Dispatch, Action, InitialRealmData } from '../types';
+import type {
+  GetState,
+  Dispatch,
+  Action,
+  InitialRealmData,
+  RealmInitAction,
+  DeleteTokenPushAction,
+  SaveTokenPushAction,
+} from '../types';
 import { initializeNotifications } from '../utils/notifications';
 import { getAuth } from '../selectors';
 
 import { REALM_INIT, SAVE_TOKEN_PUSH, DELETE_TOKEN_PUSH } from '../actionConstants';
 
-export const realmInit = (data: InitialRealmData): Action => ({
+export const realmInit = (data: InitialRealmData): RealmInitAction => ({
   type: REALM_INIT,
   data,
 });
 
-export const deleteTokenPush = (): Action => ({
+export const deleteTokenPush = (): DeleteTokenPushAction => ({
   type: DELETE_TOKEN_PUSH,
 });
 
-const saveTokenPush = (pushToken: string, result: string, msg: string) => ({
+export const saveTokenPush = (
+  pushToken: string,
+  result: string,
+  msg: string,
+): SaveTokenPushAction => ({
   type: SAVE_TOKEN_PUSH,
   pushToken,
   result,
