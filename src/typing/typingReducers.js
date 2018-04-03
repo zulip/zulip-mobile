fe792c30b1d0962d73e1913050dcacbc6fff29f7
Boris Yankov <borisyankov@gmail.com>
2018-04-03 23:09:17 +0300

    flow: Add types for typing actions and reducers
    Removed start typing and stop typing actions from typingActions.js
    They were not manually used; this actuion is produced in eventToAction.js

--- src/typing/typingReducers.js
+++ src/typing/typingReducers.js
@@ -1,9 +1,15 @@
 /* @flow */
-import type { Action, TypingState } from '../types';
+import type {
+  TypingAction,
+  ClearTypingAction,
+  StartTypingAction,
+  StopTypingAction,
+  TypingState,
+} from '../types';
 import {
   EVENT_TYPING_START,
   EVENT_TYPING_STOP,
-  CLEAR_TYPING_NOTIFICATIONS,
+  CLEAR_TYPING,
   LOGOUT,
   LOGIN_SUCCESS,
   ACCOUNT_SWITCH,
@@ -14,70 +20,79 @@ import { NULL_OBJECT } from '../nullObjects';
 
 const initialState: TypingState = NULL_OBJECT;
 
-export default (state: TypingState = initialState, action: Action): TypingState => {
+const eventTypingStart = (state: TypingState, action: StartTypingAction): TypingState => {
+  if (action.sender.email === action.ownEmail) {
+    // don't change state when self is typing
+    return state;
+  }
+
+  const normalizedRecipients = normalizeRecipientsSansMe(action.recipients, action.ownEmail);
+  const previousTypingUsers = state[normalizedRecipients] || { userIds: [] };
+
+  const isUserAlreadyTyping = previousTypingUsers.userIds.indexOf(action.sender.user_id);
+  if (isUserAlreadyTyping > -1) {
+    return {
+      ...state,
+      [normalizedRecipients]: {
+        userIds: [...previousTypingUsers.userIds],
+        time: action.time,
+      },
+    };
+  }
+
+  return {
+    ...state,
+    [normalizedRecipients]: {
+      userIds: [...previousTypingUsers.userIds, action.sender.user_id],
+      time: action.time,
+    },
+  };
+};
+
+const eventTypingStop = (state: TypingState, action: StopTypingAction): TypingState => {
+  const normalizedRecipients = normalizeRecipientsSansMe(action.recipients, action.ownEmail);
+  const previousTypingUsers = state[normalizedRecipients];
+
+  if (!previousTypingUsers) {
+    return state;
+  }
+
+  const newTypingUsers = state[normalizedRecipients].userIds.filter(
+    userId => userId !== action.sender.user_id,
+  );
+
+  if (newTypingUsers.length > 0) {
+    return {
+      ...state,
+      [normalizedRecipients]: {
+        time: action.time,
+        userIds: newTypingUsers,
+      },
+    };
+  }
+
+  // if key is empty now, remove the key
+  const newState = { ...state };
+  delete newState[normalizedRecipients];
+  return newState;
+};
+
+const clearTyping = (state: TypingState, action: ClearTypingAction): TypingState => {
+  const newState = { ...state };
+  action.outdatedNotifications.map(recipients => delete newState[recipients]);
+  return newState;
+};
+
+export default (state: TypingState = initialState, action: TypingAction): TypingState => {
   switch (action.type) {
-    case EVENT_TYPING_START: {
-      if (action.sender.email === action.ownEmail) {
-        // don't change state when self is typing
-        return state;
-      }
+    case EVENT_TYPING_START:
+      return eventTypingStart(state, action);
 
-      const normalizedRecipients = normalizeRecipientsSansMe(action.recipients, action.ownEmail);
-      const previousTypingUsers = state[normalizedRecipients] || { userIds: [] };
+    case EVENT_TYPING_STOP:
+      return eventTypingStop(state, action);
 
-      const isUserAlreadyTyping = previousTypingUsers.userIds.indexOf(action.sender.user_id);
-      if (isUserAlreadyTyping > -1) {
-        return {
-          ...state,
-          [normalizedRecipients]: {
-            userIds: [...previousTypingUsers.userIds],
-            time: action.time,
-          },
-        };
-      }
-
-      return {
-        ...state,
-        [normalizedRecipients]: {
-          userIds: [...previousTypingUsers.userIds, action.sender.user_id],
-          time: action.time,
-        },
-      };
-    }
-
-    case EVENT_TYPING_STOP: {
-      const normalizedRecipients = normalizeRecipientsSansMe(action.recipients, action.ownEmail);
-      const previousTypingUsers = state[normalizedRecipients];
-
-      if (!previousTypingUsers) {
-        return state;
-      }
-
-      const newTypingUsers = state[normalizedRecipients].userIds.filter(
-        userId => userId !== action.sender.user_id,
-      );
-
-      if (newTypingUsers.length > 0) {
-        return {
-          ...state,
-          [normalizedRecipients]: {
-            time: action.time,
-            userIds: newTypingUsers,
-          },
-        };
-      }
-
-      // if key is empty now, remove the key
-      const newState = { ...state };
-      delete newState[normalizedRecipients];
-      return newState;
-    }
-
-    case CLEAR_TYPING_NOTIFICATIONS: {
-      const newState = { ...state };
-      action.outdatedNotifications.map(recipients => delete newState[recipients]);
-      return newState;
-    }
+    case CLEAR_TYPING:
+      return clearTyping(state, action);
 
     case LOGOUT:
     case LOGIN_SUCCESS:
