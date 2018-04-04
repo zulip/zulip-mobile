zulip/zulip-mobile

commit 0c647303a266800355b8cabfdd4857c9fa300375

screen: Fix scrolling in ScrollView
The ScrollView wasn't scrolling in the Screen component due to a minor
syntax error.

src/common/Screen.js
@@ -75,8 +75,7 @@ class Screen extends PureComponent<Props> {
         >
           <ScrollView
             contentContainerStyle={[
-              componentStyles.childrenWrapper,
-              centerContent ? componentStyles.content : null,
+              componentStyles.childrenWrapper && centerContent ? componentStyles.content : null,
             ]}
           >
             {children}

commit 2a39a5ca7fc7c611a40001e95a1e4bf42cb3a7e6

fix: MessageList in Search Screen not visible
Revert bug introduced in #2184

src/common/Screen.js
@@ -75,7 +75,8 @@ class Screen extends PureComponent<Props> {
         >
           <ScrollView
             contentContainerStyle={[
-              componentStyles.childrenWrapper && centerContent ? componentStyles.content : null,
+              componentStyles.childrenWrapper,
+              centerContent && componentStyles.content,
             ]}
           >
             {children}