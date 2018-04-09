19e6adb94218bb1686cce09eef2d3aed8fecfc72
Boris Yankov <borisyankov@gmail.com>
2018-04-09 23:39:28 +0300

    flow: Add types for nav reducers

--- src/nav/navReducers.js
+++ src/nav/navReducers.js
@@ -1,7 +1,16 @@
 /* @flow */
 import { REHYDRATE } from 'redux-persist/constants';
 
-import type { NavigationState, Action } from '../types';
+import type {
+  NavigationState,
+  NavAction,
+  RehydrateAction,
+  AccountSwitchAction,
+  LoginSuccessAction,
+  LogoutAction,
+  SwitchNarrowAction,
+  InitialFetchCompleteAction,
+} from '../types';
 import { navigateToChat } from './navActions';
 import { getStateForRoute, getInitialNavState } from './navSelectors';
 import AppNavigator from './AppNavigator';
@@ -16,25 +25,46 @@ import {
 
 const initialState = getStateForRoute('loading') || NULL_NAV_STATE;
 
-export default (state: NavigationState = initialState, action: Action): NavigationState => {
+const rehydrate = (state: NavigationState, action: RehydrateAction): NavigationState =>
+  getInitialNavState(action.payload) || state;
+
+const accountSwitch = (state: NavigationState, action: AccountSwitchAction): NavigationState =>
+  getStateForRoute('loading') || state;
+
+const loginSuccess = (state: NavigationState, action: LoginSuccessAction): NavigationState =>
+  getStateForRoute('main') || state;
+
+const logout = (state: NavigationState, action: LogoutAction): NavigationState =>
+  getStateForRoute('account') || state;
+
+const switchNarrow = (state: NavigationState, action: SwitchNarrowAction): NavigationState =>
+  AppNavigator.router.getStateForAction(navigateToChat(action.narrow), state) || state;
+
+const initialFetchComplete = (
+  state: NavigationState,
+  action: InitialFetchCompleteAction,
+): NavigationState =>
+  state.routes[0].routeName === 'main' ? state : getStateForRoute('main') || state;
+
+export default (state: NavigationState = initialState, action: NavAction): NavigationState => {
   switch (action.type) {
     case REHYDRATE:
-      return getInitialNavState(action.payload) || state;
+      return rehydrate(state, action);
 
     case ACCOUNT_SWITCH:
-      return getStateForRoute('loading') || state;
+      return accountSwitch(state, action);
 
     case LOGIN_SUCCESS:
-      return getStateForRoute('main') || state;
+      return loginSuccess(state, action);
 
     case INITIAL_FETCH_COMPLETE:
-      return state.routes[0].routeName === 'main' ? state : getStateForRoute('main') || state;
+      return initialFetchComplete(state, action);
 
     case LOGOUT:
-      return getStateForRoute('account') || state;
+      return logout(state, action);
 
     case SWITCH_NARROW:
-      return AppNavigator.router.getStateForAction(navigateToChat(action.narrow), state) || state;
+      return switchNarrow(state, action);
 
     default:
       return AppNavigator.router.getStateForAction(action, state) || state;
