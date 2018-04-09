23d69ff7d9bcc1d1e9708bfa547aea1833179603
Boris Yankov <borisyankov@gmail.com>
2018-04-10 00:26:21 +0300

    refactor+flow: Make sure navReducers do not return null
    This reverts changes in commit 3ca0e78dd02cd7a5c81c755f818bc1d127a057ab
    getStateForRoute and getStateForAction can return null
    We do not want them to ever do that, and instead return the unmodified state

--- src/nav/navReducers.js
+++ src/nav/navReducers.js
@@ -5,6 +5,7 @@ import type { NavigationState, Action } from '../types';
 import { navigateToChat } from './navActions';
 import { getStateForRoute, getInitialNavState } from './navSelectors';
 import AppNavigator from './AppNavigator';
+import { NULL_NAV_STATE } from '../nullObjects';
 import {
   INITIAL_FETCH_COMPLETE,
   ACCOUNT_SWITCH,
@@ -13,31 +14,29 @@ import {
   SWITCH_NARROW,
 } from '../actionConstants';
 
-export default (
-  state: ?NavigationState = getStateForRoute('loading'),
-  action: Action,
-): ?NavigationState => {
+const initialState = getStateForRoute('loading') || NULL_NAV_STATE;
+
+export default (state: NavigationState = initialState, action: Action): NavigationState => {
   switch (action.type) {
     case REHYDRATE:
-      return getInitialNavState(action.payload);
+      return getInitialNavState(action.payload) || state;
 
     case ACCOUNT_SWITCH:
-      return getStateForRoute('loading');
+      return getStateForRoute('loading') || state;
 
     case LOGIN_SUCCESS:
-      return getStateForRoute('main');
+      return getStateForRoute('main') || state;
 
     case INITIAL_FETCH_COMPLETE:
-      return (state && state.routes[0].routeName === 'main') ?
-        state : getStateForRoute('main');
+      return state.routes[0].routeName === 'main' ? state : getStateForRoute('main') || state;
 
     case LOGOUT:
-      return getStateForRoute('account');
+      return getStateForRoute('account') || state;
 
     case SWITCH_NARROW:
-      return AppNavigator.router.getStateForAction(navigateToChat(action.narrow), state);
+      return AppNavigator.router.getStateForAction(navigateToChat(action.narrow), state) || state;
 
     default:
-      return AppNavigator.router.getStateForAction(action, state);
+      return AppNavigator.router.getStateForAction(action, state) || state;
   }
 };
