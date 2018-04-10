e07aa87cf4d26e57714467f06058103a70b87e33
Boris Yankov <borisyankov@gmail.com>
2018-04-10 20:41:14 +0300

    flow: Add types for caughtUp reducers

--- src/caughtup/caughtUpReducers.js
+++ src/caughtup/caughtUpReducers.js
@@ -1,5 +1,5 @@
 /* @flow */
-import type { CaughtUpState, Action } from '../types';
+import type { CaughtUpState, CaughtUpAction, MessageFetchCompleteAction } from '../types';
 import {
   APP_REFRESH,
   LOGOUT,
@@ -11,7 +11,51 @@ import { NULL_CAUGHTUP, NULL_OBJECT } from '../nullObjects';
 
 const initialState: CaughtUpState = NULL_OBJECT;
 
-export default (state: CaughtUpState = initialState, action: Action): CaughtUpState => {
+const messageFetchComplete = (
+  state: CaughtUpState,
+  action: MessageFetchCompleteAction,
+): CaughtUpState => {
+  const key = JSON.stringify(action.narrow);
+
+  if (action.anchor === Number.MAX_SAFE_INTEGER) {
+    return {
+      ...state,
+      [key]: {
+        older: action.numBefore > action.messages.length,
+        newer: true,
+      },
+    };
+  }
+
+  // Find the anchor in the results (or set it past the end of the list)
+  let anchorIdx = action.messages.findIndex(msg => msg.id === action.anchor);
+  if (anchorIdx === -1) {
+    anchorIdx = action.messages.length;
+  }
+  const totalMessagesRequested = action.numBefore + action.numAfter;
+  // If we're requesting messages before the anchor, the server
+  // returns one less than we expect (to avoid duplicating the anchor)
+  // only do adjustment if messages are more than expected
+  const adjustment =
+    action.messages.length > totalMessagesRequested && action.numBefore > 0
+      ? -(action.messages.length - totalMessagesRequested)
+      : 0;
+
+  const caughtUpOlder = anchorIdx + 1 < action.numBefore;
+  const caughtUpNewer = action.messages.length - anchorIdx + adjustment < action.numAfter;
+
+  const prevState = state[key] || NULL_CAUGHTUP;
+
+  return {
+    ...state,
+    [key]: {
+      older: prevState.older || caughtUpOlder,
+      newer: prevState.newer || caughtUpNewer,
+    },
+  };
+};
+
+export default (state: CaughtUpState = initialState, action: CaughtUpAction): CaughtUpState => {
   switch (action.type) {
     case APP_REFRESH:
     case LOGOUT:
@@ -19,46 +63,8 @@ export default (state: CaughtUpState = initialState, action: Action): CaughtUpSt
     case ACCOUNT_SWITCH:
       return initialState;
 
-    case MESSAGE_FETCH_COMPLETE: {
-      const key = JSON.stringify(action.narrow);
-
-      if (action.anchor === Number.MAX_SAFE_INTEGER) {
-        return {
-          ...state,
-          [key]: {
-            older: action.numBefore > action.messages.length,
-            newer: true,
-          },
-        };
-      }
-
-      // Find the anchor in the results (or set it past the end of the list)
-      let anchorIdx = action.messages.findIndex(msg => msg.id === action.anchor);
-      if (anchorIdx === -1) {
-        anchorIdx = action.messages.length;
-      }
-      const totalMessagesRequested = action.numBefore + action.numAfter;
-      // If we're requesting messages before the anchor, the server
-      // returns one less than we expect (to avoid duplicating the anchor)
-      // only do adjustment if messages are more than expected
-      const adjustment =
-        action.messages.length > totalMessagesRequested && action.numBefore > 0
-          ? -(action.messages.length - totalMessagesRequested)
-          : 0;
-
-      const caughtUpOlder = anchorIdx + 1 < action.numBefore;
-      const caughtUpNewer = action.messages.length - anchorIdx + adjustment < action.numAfter;
-
-      const prevState = state[key] || NULL_CAUGHTUP;
-
-      return {
-        ...state,
-        [key]: {
-          older: prevState.older || caughtUpOlder,
-          newer: prevState.newer || caughtUpNewer,
-        },
-      };
-    }
+    case MESSAGE_FETCH_COMPLETE:
+      return messageFetchComplete(state, action);
 
     default:
       return state;
