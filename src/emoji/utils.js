253cb36febce264ee00c3432cd99459a502c2ff9
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-04-10 11:58:02 +0530

    flow: Add method return type of emoji utils.

--- src/emoji/utils.js
+++ src/emoji/utils.js
@@ -1,7 +1,10 @@
 /* @flow */
+import type { RealmEmojiType, ZulipExtraEmojiType } from '../types';
 
-export const getRealmEmojiWithName = (realmEmoji: Object, emojiName: string) =>
+export const getRealmEmojiWithName = (realmEmoji: Object, emojiName: string): RealmEmojiType =>
   realmEmoji[Object.keys(realmEmoji).find(key => realmEmoji[key].name === emojiName)];
 
-export const getZulipExtraEmojiWithName = (zulipExtraEmojis: Object, emojiName: string) =>
-  zulipExtraEmojis[emojiName];
+export const getZulipExtraEmojiWithName = (
+  zulipExtraEmojis: Object,
+  emojiName: string,
+): ZulipExtraEmojiType => zulipExtraEmojis[emojiName];
