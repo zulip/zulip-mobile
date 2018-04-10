adca5e7dc005acaa1c30af7f60efd6e58c1f5f83
Boris Yankov <borisyankov@gmail.com>
2018-04-10 17:54:55 +0300

    flow: Add types for realm actions
    Merge realmEmojiActions.js and realmFilterActions.js into
    the main realmActions.js

--- src/realm/realmActions.js
+++ src/realm/realmActions.js
@@ -1,17 +1,27 @@
 /* @flow */
 import type {
+  Auth,
   GetState,
   Dispatch,
-  Action,
+  RealmFilter,
   InitialRealmData,
   RealmInitAction,
   DeleteTokenPushAction,
   SaveTokenPushAction,
+  InitRealmEmojiAction,
+  InitRealmFilterAction,
 } from '../types';
 import { initializeNotifications } from '../utils/notifications';
 import { getAuth } from '../selectors';
+import { getRealmEmojis, getRealmFilters } from '../api';
 
-import { REALM_INIT, SAVE_TOKEN_PUSH, DELETE_TOKEN_PUSH } from '../actionConstants';
+import {
+  REALM_INIT,
+  SAVE_TOKEN_PUSH,
+  DELETE_TOKEN_PUSH,
+  INIT_REALM_EMOJI,
+  INIT_REALM_FILTER,
+} from '../actionConstants';
 
 export const realmInit = (data: InitialRealmData): RealmInitAction => ({
   type: REALM_INIT,
@@ -33,8 +43,24 @@ export const saveTokenPush = (
   msg,
 });
 
-export const initNotifications = (): Action => (dispatch: Dispatch, getState: GetState) => {
+export const initNotifications = () => (dispatch: Dispatch, getState: GetState) => {
   initializeNotifications(getAuth(getState()), (token, msg, result) =>
     dispatch(saveTokenPush(token, result, msg)),
   );
 };
+
+export const initRealmEmojis = (emojis: Object): InitRealmEmojiAction => ({
+  type: INIT_REALM_EMOJI,
+  emojis,
+});
+
+export const fetchRealmEmojis = (auth: Auth) => async (dispatch: Dispatch) =>
+  dispatch(initRealmEmojis(await getRealmEmojis(auth)));
+
+export const initRealmFilters = (filters: RealmFilter[]): InitRealmFilterAction => ({
+  type: INIT_REALM_FILTER,
+  filters,
+});
+
+export const fetchRealmFilters = (auth: Auth) => async (dispatch: Dispatch) =>
+  dispatch(initRealmFilters(await getRealmFilters(auth)));
--- src/realm/realmReducers.js
+++ src/realm/realmReducers.js
@@ -1,5 +1,17 @@
 /* @flow */
-import type { RealmState, Action } from '../types';
+import type {
+  RealmState,
+  RealmAction,
+  RealmInitAction,
+  SaveTokenPushAction,
+  DeleteTokenPushAction,
+  LoginSuccessAction,
+  LogoutAction,
+  InitRealmEmojiAction,
+  InitRealmFilterAction,
+  EventRealmFilterUpdateAction,
+  EventRealmEmojiUpdateAction,
+} from '../types';
 import {
   APP_REFRESH,
   REALM_INIT,
@@ -23,71 +35,89 @@ const initialState = {
   filters: [],
 };
 
-export default (state: RealmState = initialState, action: Action): RealmState => {
-  switch (action.type) {
-    case REALM_INIT:
-      return {
-        ...state,
-        emoji: action.data.realm_emoji,
-        filters: action.data.realm_filters,
-        twentyFourHourTime: action.data.twenty_four_hour_time,
-      };
+const realmInit = (state: RealmState, action: RealmInitAction): RealmState => ({
+  ...state,
+  emoji: action.data.realm_emoji,
+  filters: action.data.realm_filters,
+  twentyFourHourTime: action.data.twenty_four_hour_time,
+});
 
+const saveTokenPush = (state: RealmState, action: SaveTokenPushAction): RealmState => ({
+  ...state,
+  pushToken: {
+    token: action.pushToken,
+    result: action.result,
+    msg: action.msg,
+  },
+});
+
+const deleteTokenPush = (state: RealmState, action: DeleteTokenPushAction): RealmState => ({
+  ...state,
+  pushToken: { token: '', result: '', msg: '' },
+});
+
+const loginChange = (state: RealmState, action: LoginSuccessAction | LogoutAction): RealmState => ({
+  ...state,
+  emoji: {},
+  pushToken: { token: '', result: '', msg: '' },
+});
+
+const initRealmEmoji = (state: RealmState, action: InitRealmEmojiAction): RealmState => ({
+  ...state,
+  emoji: action.emojis,
+});
+
+const initRealmFilter = (state: RealmState, action: InitRealmFilterAction): RealmState => ({
+  ...state,
+  filters: action.filters,
+});
+
+const eventRealmFilterUpdate = (
+  state: RealmState,
+  action: EventRealmFilterUpdateAction,
+): RealmState => ({
+  ...state,
+  filters: action.realm_filters,
+});
+
+const eventRealmEmojiUpdate = (
+  state: RealmState,
+  action: EventRealmEmojiUpdateAction,
+): RealmState => ({
+  ...state,
+  emoji: action.realm_emoji,
+});
+
+export default (state: RealmState = initialState, action: RealmAction): RealmState => {
+  switch (action.type) {
     case APP_REFRESH:
     case ACCOUNT_SWITCH:
       return initialState;
 
-    case SAVE_TOKEN_PUSH: {
-      return {
-        ...state,
-        pushToken: {
-          token: action.pushToken,
-          result: action.result,
-          msg: action.msg,
-        },
-      };
-    }
+    case REALM_INIT:
+      return realmInit(state, action);
 
-    case DELETE_TOKEN_PUSH: {
-      return {
-        ...state,
-        pushToken: { token: '', result: '', msg: '' },
-      };
-    }
+    case SAVE_TOKEN_PUSH:
+      return saveTokenPush(state, action);
+
+    case DELETE_TOKEN_PUSH:
+      return deleteTokenPush(state, action);
 
     case LOGOUT:
     case LOGIN_SUCCESS:
-      return {
-        ...state,
-        emoji: {},
-        pushToken: { token: '', result: '', msg: '' },
-      };
+      return loginChange(state, action);
 
     case INIT_REALM_EMOJI:
-      return {
-        ...state,
-        emoji: action.emojis,
-      };
+      return initRealmEmoji(state, action);
 
-    case INIT_REALM_FILTER: {
-      return {
-        ...state,
-        filters: action.filters,
-      };
-    }
+    case INIT_REALM_FILTER:
+      return initRealmFilter(state, action);
 
-    case EVENT_REALM_FILTER_UPDATE: {
-      return {
-        ...state,
-        filters: action.realm_filters,
-      };
-    }
+    case EVENT_REALM_FILTER_UPDATE:
+      return eventRealmFilterUpdate(state, action);
 
     case EVENT_REALM_EMOJI_UPDATE:
-      return {
-        ...state,
-        emoji: action.realm_emoji,
-      };
+      return eventRealmEmojiUpdate(state, action);
 
     case EVENT_UPDATE_DISPLAY_SETTINGS:
       switch (action.setting_name) {
