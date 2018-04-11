049e668031a5b7c5c36b5366fc5d7907aa7ffa02
Harshul Sharma <harshulsharma000@outlook.com>
2018-04-11 23:50:36 +0530

    WIP: Tried experimental workarounds to load images into chat webview
    The aim of this commit is only to enlist possible attempts and failures occured when I tried to load images from backend.
    Various workarounds were tried and all the present within this commit.
    Various sections of the code has been commented out many of which contain console.logs() calls which are log commands for reactotron and are placed at specific locations to get information about various sections of code.
    One more thing to note is, most of the code is static and is just written to illustrate the least and simplest working case in order to test if something truely works. Much of the code might require a little editing for user specifig senario kindly keep that in mind.
    Further note that I am not sure if my implementations are correct, so If I have tried something in a way that it is not meant to be done, kindly correct me.

--- src/render-html/messageAsHtml.js
+++ src/render-html/messageAsHtml.js
@@ -3,6 +3,8 @@ import type { FlagsState, ReactionType, RealmEmojiType } from '../types';
 import { shortTime } from '../utils/date';
 import messageTagsAsHtml from './messageTagsAsHtml';
 import messageReactionListAsHtml from './messageReactionListAsHtml';
+import base64 from 'base-64';
+import RNFetchBlob from 'react-native-fetch-blob';
 
 const messageDiv = (id: number, msgClass: string, flags: Object): string =>
   `<div
@@ -54,7 +56,45 @@ type Props = FullMessageProps & {
   isBrief: boolean,
 };
 
-const messageBody = ({
+// An async function but do remember it return a promise...
+async function getBase64(url, val) {
+  const headers = {
+    // "user-agent" : "ZulipMobile/1.0.13 (Android 6.0)",
+    // "content-type" : "application/x-www-form-urlencoded; charset=utf-8",
+    "Authorization": "Basic aGFyc2h1bHNoYXJtYTAwMEBnbWFpbC5jb206MjU0ZGNnaFl1Y3MyTG1vM0JJcHdYbDg5TzF1RlVGeEQ=",
+  };
+  // FAILED ATTEMPT with normal fetch api and base64 
+  // NOTE- PLEASE CHECK API RESPONSE BEFORE PROCEEDING
+  
+  // const response = await fetch(url,{method:'GET', headers, mode: 'cors', cache: 'default'});
+  // const res = await response.arrayBuffer();
+  // const encoded = base64.encode(res);
+
+  const fs = RNFetchBlob.fs;
+  let imagePath = null;
+  let resp = await RNFetchBlob
+              .config({ 
+                    fileCache : true 
+              })
+              .fetch('GET', url, headers  );
+    // the image is now dowloaded to device's storage
+    imagePath = resp.path();
+  let base64Data = await resp.readFile('base64');
+  console.logs(base64Data); // Check if base64 data is correct
+  fs.unlink(imagePath)
+  return base64Data;
+  
+  // Just an earlier attempt with .then()
+  // .then((base64Data) => { 
+    //     // here's base64 encoded image
+    //     console.logs(base64Data)
+    //     val = base64Data;
+    //     // remove the file from storage
+    //     return fs.unlink(imagePath)
+    // });
+};
+
+const messageBody =  ({
   content,
   flags,
   id,
@@ -72,12 +112,36 @@ const messageBody = ({
   reactions: ReactionType[],
   realmEmoji: ReactionType,
   timeEdited: Date,
-}) => `
+}) => {
+  let val = null; 
+  // Regex to scan out image URLs
+  const regex = /<img src="(.*)">/g; 
+  // A replacement string to convert relative to full URL manually and replace it within img tags
+  const substr = `<img src="https://hsinter0.zulipchat.com/static$1" >`; 
+  
+  // A very cheap and disgusting replacement style but it works!
+  // content = `${content}`.replace(regex,substr);
+  
+  // Get base64 for a given image(URL provided)
+  // val = await getBase64('https://hsinter0.zulipchat.com/user_uploads/2592/QdFY8LLU32q-pOh9MV37nzoQ/art.jpg');
+  
+  // I also planned to return image tags with embeded base64 strings but these opertions are aync
+  // And as we know our whole rendering process is synchronous
+  
+  // let funfunc =  async (val) => {
+  //   let valuuuu = await getBase64('https://hsinter0.zulipchat.com/user_uploads/2592/QdFY8LLU32q-pOh9MV37nzoQ/art.jpg');
+  //   return `<img src="data:image/jpeg;base64,${val}" >`;
+  // };
+  
+  content = `${content}`.replace(regex, substr);
+  // console.logs('bing bandlesk blaaahoooh');
+  // console.logs(content); //Finally replaced the img tags 
+  return `
 ${content}
 ${isOutbox ? '<div class="loading-spinner outbox-spinner"></div>' : ''}
 ${messageTagsAsHtml(flags, timeEdited)}
 ${messageReactionListAsHtml(reactions, id, ownEmail, realmEmoji)}
-`;
+`;};
 
 const briefMessageAsHtml = ({
   content,
