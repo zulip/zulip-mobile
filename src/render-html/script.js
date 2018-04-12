a59cfb3776e845821a20e6942ef9181898eb3b74
Boris Yankov <borisyankov@gmail.com>
2018-04-12 20:56:18 +0300

    webview: Move error handling inside `js.js`
    This is in preparation to add webview error propagation to Sentry.
    
    Also, enable the error reporting in-webview by default.

--- src/render-html/script.js
+++ src/render-html/script.js
@@ -3,20 +3,11 @@ import smoothScroll from './smoothScroll.min';
 import js from './generatedEs3';
 import config from '../config';
 
-const errorHandler = `
-window.onerror = function (message, source, line, column, error) {
-  var obj = JSON.stringify(error);
-  var errorStr = ['Message: ' + message + '<br>', 'Line: ' + line + ':' + column + '<br>', 'Error: ' + obj + '<br>'].join('');
-  document.getElementById('js-error').innerHTML = errorStr;
-  return false;
-};
-`;
-
 export default (anchor: number): string => `
 <script>
 window.__forceSmoothScrollPolyfill__ = true;
-${config.enableWebViewErrorDisplay ? errorHandler : ''}
 ${smoothScroll}
+window.enableWebViewErrorDisplay = ${config.enableWebViewErrorDisplay.toString()};
 document.addEventListener('DOMContentLoaded', function() {
   ${js}
   scrollToAnchor(${anchor});
