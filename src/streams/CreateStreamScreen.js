4081569e65e53dca473ff64ba4c4bef8a90d5727
Boris Yankov <borisyankov@gmail.com>
2018-04-13 06:59:36 +0300

    usability: Create and Edit sream screen one-tap on buttons
    Make sure we do not ignore the first tap on buttons in
    Create & Edit screens, activate on first tap.

--- src/streams/CreateStreamScreen.js
+++ src/streams/CreateStreamScreen.js
@@ -7,7 +7,7 @@ import EditStreamContainer from './EditStreamContainer';
 export default class CreateStreamScreen extends PureComponent<{}> {
   render() {
     return (
-      <Screen title="Create new stream" padding>
+      <Screen title="Create new stream" padding keyboardShouldPersistTaps="handled">
         <EditStreamContainer />
       </Screen>
     );
