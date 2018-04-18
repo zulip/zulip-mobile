bc84637bbd7006552aec131446b4a87407747138
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-04-18 11:04:49 +0530

    refactor: Remove color from unreadView streamItem.
    As we are already sending stream color via backgroundColor
    prop. And we want text, icon color to be decided from the
    background color.
    
    This removes the confusion in stream item that text and
    icon color should be taken either from color or backgroundColor.

--- src/streams/StreamItem.js
+++ src/streams/StreamItem.js
@@ -39,7 +39,7 @@ type Props = {
   isPrivate?: boolean,
   isSelected?: boolean,
   showSwitch?: boolean,
-  color: string,
+  color?: string,
   backgroundColor?: string,
   isSwitchedOn?: boolean,
   unreadCount?: number,
