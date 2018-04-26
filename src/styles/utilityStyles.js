0b0c6849ad741e752694c3670afe2cf884205aa5
Boris Yankov <borisyankov@gmail.com>
2018-04-26 18:28:47 +0300

    ui: Extract padding & margin constants
    * margin and padding based on a single constant
    * utility padding styles
    
    This is to unify the padding sizes in most of the places we use them.
    Sometimes we can use custom sizes but rarely it is required.
    This will help us make the spacing more consistent.

---
+++ src/styles/utilityStyles.js
@@ -0,0 +1,69 @@
+/* @flow */
+import { SPACING, HALF_SPACING } from './';
+
+export default {
+  // Padding
+  padding: {
+    padding: SPACING,
+  },
+  paddingTop: {
+    paddingTop: SPACING,
+  },
+  paddingBottom: {
+    paddingBottom: SPACING,
+  },
+  paddingLeft: {
+    paddingLeft: SPACING,
+  },
+  paddingRight: {
+    paddingRight: SPACING,
+  },
+  // Half padding
+  halfPadding: {
+    padding: HALF_SPACING,
+  },
+  halfPaddingTop: {
+    paddingTop: HALF_SPACING,
+  },
+  halfPaddingBottom: {
+    paddingBottom: HALF_SPACING,
+  },
+  halfPaddingLeft: {
+    paddingLeft: HALF_SPACING,
+  },
+  halfPaddingRight: {
+    paddingRight: HALF_SPACING,
+  },
+  // Margin
+  margin: {
+    margin: SPACING,
+  },
+  marginTop: {
+    marginTop: SPACING,
+  },
+  marginBottom: {
+    marginBottom: SPACING,
+  },
+  marginLeft: {
+    marginLeft: SPACING,
+  },
+  marginRight: {
+    marginRight: SPACING,
+  },
+  // Half margin
+  halfMargin: {
+    margin: HALF_SPACING,
+  },
+  halfMarginTop: {
+    marginTop: HALF_SPACING,
+  },
+  halfMarginBottom: {
+    marginBottom: HALF_SPACING,
+  },
+  halfMarginLeft: {
+    marginLeft: HALF_SPACING,
+  },
+  halfMarginRight: {
+    marginRight: HALF_SPACING,
+  },
+};
