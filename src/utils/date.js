02088d928ab7ee2fc85afbf7ece8301db7ec1808
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-05-01 23:20:03 +0530

    presnece: Show status as 'active' if timestamp < OFFLINE_THRESHOLD_SECS.
    OFFLINE_THRESHOLD_SECS is the time interval in which status ping
    is send to server.
    
    Move presenceToHumanTime to presence module.

--- src/utils/date.js
+++ src/utils/date.js
@@ -3,10 +3,6 @@ import format from 'date-fns/format';
 import isToday from 'date-fns/is_today';
 import isYesterday from 'date-fns/is_yesterday';
 import isSameYear from 'date-fns/is_same_year';
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
-import differenceInSeconds from 'date-fns/difference_in_seconds';
-
-import type { Presence } from '../types';
 
 export { default as isSameDay } from 'date-fns/is_same_day';
 
@@ -24,13 +20,4 @@ export const humanDate = (date: Date): string => {
   if (isYesterday(date)) return 'Yesterday';
 
   return isSameYear(new Date(date), new Date()) ? shortDate(date) : longDate(date);
-};
-
-export const presenceToHumanTime = (presence: Presence): string => {
-  if (!presence || !presence.aggregated) return 'never';
-
-  const lastTimeActive = new Date(presence.aggregated.timestamp * 1000);
-  return differenceInSeconds(Date.now(), lastTimeActive) < 60
-    ? 'now'
-    : `${distanceInWordsToNow(lastTimeActive)} ago`;
 };
