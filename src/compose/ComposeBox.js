696711f7e1d9a09b33c2934c3e41ea4aaf03d3c1
agzuniverse <aswinganesh666@gmail.com>
2018-05-02 21:49:10 +0530

    Move ComposeMenu to row above composeInput
    ComposeMenu now is in the top row along with topicInput.
    It will appear only when composeInput is focused.

--- src/compose/ComposeBox.js
+++ src/compose/ComposeBox.js
@@ -29,6 +29,9 @@ const componentStyles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-end',
   },
+  composeOptions: {
+    flexDirection: 'row',
+  },
   composeText: {
     flex: 1,
     justifyContent: 'center',
@@ -273,28 +276,28 @@ export default class ComposeBox extends PureComponent<Props, State> {
           onTopicAutocomplete={this.handleTopicChange}
         />
         <View style={styles.composeBox} onLayout={this.handleLayoutChange}>
-          <View style={componentStyles.bottom}>
-            <ComposeMenuContainer
-              narrow={narrow}
-              expanded={isMenuExpanded}
-              onExpandContract={this.handleComposeMenuToggle}
-            />
-          </View>
           <View style={componentStyles.composeText}>
             {canSelectTopic && (
-              <Input
-                style={styles.topicInput}
-                underlineColorAndroid="transparent"
-                placeholder="Topic"
-                selectTextOnFocus
-                textInputRef={component => {
-                  this.topicInput = component;
-                }}
-                onChangeText={this.handleTopicChange}
-                onFocus={this.handleTopicFocus}
-                onBlur={this.handleTopicBlur}
-                value={topic}
-              />
+              <View style={componentStyles.composeOptions}>
+                <ComposeMenuContainer
+                  narrow={narrow}
+                  expanded={isMenuExpanded}
+                  onExpandContract={this.handleComposeMenuToggle}
+                />
+                <Input
+                  style={styles.topicInput}
+                  underlineColorAndroid="transparent"
+                  placeholder="Topic"
+                  selectTextOnFocus
+                  textInputRef={component => {
+                    this.topicInput = component;
+                  }}
+                  onChangeText={this.handleTopicChange}
+                  onFocus={this.handleTopicFocus}
+                  onBlur={this.handleTopicBlur}
+                  value={topic}
+                />
+              </View>
             )}
             <MultilineInput
               style={styles.composeTextInput}
