2b83b2abed25f8159c3cd5088e91c92e212a3be1
Boris Yankov <borisyankov@gmail.com>
2018-05-07 20:11:38 +0300

    flow: Add types for `alert words`
    Also, do not require an auth parameter for the `initAlertWords` call.

--- src/api/alert_words/getAlertWords.js
+++ src/api/alert_words/getAlertWords.js
@@ -2,4 +2,5 @@
 import type { Auth } from '../../types';
 import { apiGet } from '../apiFetch';
 
-export default async (auth: Auth) => apiGet(auth, 'users/me/alert_words', res => res.alert_words);
+export default async (auth: Auth): Promise<string[]> =>
+  apiGet(auth, 'users/me/alert_words', res => res.alert_words);
