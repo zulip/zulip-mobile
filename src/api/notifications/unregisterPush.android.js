20941054816243191f7195ee80d1e6f3fa107747
Boris Yankov <borisyankov@gmail.com>
2018-05-08 13:23:05 +0300

    api: Return whole response from post/patch API calls
    We do not use any of the responses from the post/patch API calls
    currently. Yet we have specified (wrongly) some extraction
    functions. This is likely caused by copy-pasting, as all of these
    are wrong (but since unused not causing bugs).
    
    Return the whole response for all these calls. Upcoming Flow types
    for these calls will ensure proper use of these.

--- src/api/notifications/unregisterPush.android.js
+++ src/api/notifications/unregisterPush.android.js
@@ -3,4 +3,4 @@ import type { Auth } from '../../types';
 import { apiDelete } from '../apiFetch';
 
 export default (auth: Auth, token: string) =>
-  apiDelete(auth, 'users/me/android_gcm_reg_id', res => res.api_key, { token });
+  apiDelete(auth, 'users/me/android_gcm_reg_id', res => res, { token });
--- src/api/notifications/unregisterPush.ios.js
+++ src/api/notifications/unregisterPush.ios.js
@@ -3,4 +3,4 @@ import type { Auth } from '../../types';
 import { apiDelete } from '../apiFetch';
 
 export default (auth: Auth, token: string) =>
-  apiDelete(auth, 'users/me/apns_device_token', res => res.api_key, { token });
+  apiDelete(auth, 'users/me/apns_device_token', res => res, { token });
