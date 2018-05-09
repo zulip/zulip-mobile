54cf8bcc376e29682059854d18e00db8e451b14c
Boris Yankov <borisyankov@gmail.com>
2018-05-09 13:49:56 +0300

    maintenance: Fix prettier errors

--- src/start/WelcomeHelpScreen.js
+++ src/start/WelcomeHelpScreen.js
@@ -17,14 +17,12 @@ export default class WelcomeHelpScreen extends PureComponent<{}> {
         <Centerer>
           <RawLabel
             style={styles.helpText}
-            text={
-`Welcome to Zulip!
+            text={`Welcome to Zulip!
 
 You'll need to first create an account from your computer. If you're not sure where to start, go to zulipchat.com from your web browser.
 
 Hope to see you back here soon!
-`
-            }
+`}
           />
         </Centerer>
       </Screen>
