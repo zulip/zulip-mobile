cb1cb47e9f6e8eaf088962a071420d483f3b7f99
Greg Price <greg@zulipchat.com>
2018-05-08 17:56:17 -0700

    welcome: Format the source of the help prose a bit better.
    Template literals FTW.
    
    I'd like to keep all the lines to a reasonable length for reading the
    source... but unfortunately even single newlines get displayed
    literally.  Still, this is much closer.

--- src/start/WelcomeHelpScreen.js
+++ src/start/WelcomeHelpScreen.js
@@ -10,7 +10,14 @@ export default class WelcomeHelpScreen extends PureComponent<{}> {
         <Centerer>
           <RawLabel
             text={
-              "Zulip combines the immediacy of Slack with an email threading model. With Zulip, you can catch up on important conversations while ignoring irrelevant ones.\n\nNew to Zulip? You'll need to first create an account from your computer.\n\nIf you're not sure where to start, go to zulipchat.com from your web browser.\n\nHope to see you back here soon!"
+`Zulip combines the immediacy of Slack with an email threading model. With Zulip, you can catch up on important conversations while ignoring irrelevant ones.
+
+New to Zulip? You'll need to first create an account from your computer.
+
+If you're not sure where to start, go to zulipchat.com from your web browser.
+
+Hope to see you back here soon!
+`
             }
           />
         </Centerer>
