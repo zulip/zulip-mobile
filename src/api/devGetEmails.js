51baa597d7d310d682cf78a66fea2fe07c33d8c8
Boris Yankov <borisyankov@gmail.com>
2018-05-09 18:56:55 +0300

    flow: Add types for all 'get' API calls missing them

--- src/api/devGetEmails.js
+++ src/api/devGetEmails.js
@@ -2,5 +2,5 @@
 import type { Auth } from '../types';
 import { apiGet } from './apiFetch';
 
-export default (auth: Auth) =>
+export default (auth: Auth): Promise<[string[], string[]]> =>
   apiGet(auth, 'dev_get_emails', res => [res.direct_admins, res.direct_users]);
--- src/api/subscriptions/getSubscriptions.js
+++ src/api/subscriptions/getSubscriptions.js
@@ -1,6 +1,6 @@
 /* @flow */
-import type { Auth } from '../../types';
+import type { Auth, Subscription } from '../../types';
 import { apiGet } from '../apiFetch';
 
-export default async (auth: Auth) =>
+export default async (auth: Auth): Promise<Subscription[]> =>
   apiGet(auth, 'users/me/subscriptions', res => res.subscriptions);
