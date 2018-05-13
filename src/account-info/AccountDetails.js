5c2cdc08408df67284c0d97d0d83330ce90c1b31
Boris Yankov <borisyankov@gmail.com>
2018-05-13 16:58:00 +0300

    Simplify AccountDetails screen
    This was a not so successful experiment at providing different views
    for different screen orientations. While this works, the complication
    and extra code adds too much to our maintenance burden versus the
    benefit this code adds (I several times did not change stuff in this
    code because it was twice the work)
    
    As a first step, just merging the portrait code into AccountDetails and
    completely scraping the 'landscape' one.

--- src/account-info/AccountDetails.js
+++ src/account-info/AccountDetails.js
@@ -2,17 +2,22 @@
 import React, { PureComponent } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-import type { Auth, Actions, Orientation, Presence } from '../types';
+import type { Auth, Actions, Presence } from '../types';
 import { Avatar, ZulipButton } from '../common';
 import { IconPrivateChat } from '../common/Icons';
 import { BRAND_COLOR } from '../styles';
 import { privateNarrow } from '../utils/narrow';
 import UserStatusIndicator from '../common/UserStatusIndicator';
 import { getMediumAvatar } from '../utils/avatar';
-import LandscapeContent from './AccountDetailsContent.landscape';
-import PortraitContent from './AccountDetailsContent.portrait';
 
 const styles = StyleSheet.create({
+  containerPortrait: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  infoContainer: {
+    padding: 8,
+  },
   info: {
     textAlign: 'center',
     fontSize: 18,
@@ -42,7 +47,6 @@ type Props = {
   presence: Presence,
   avatarUrl: string,
   fullName: string,
-  orientation: Orientation,
 };
 
 export default class AccountDetails extends PureComponent<Props, void> {
@@ -54,39 +58,32 @@ export default class AccountDetails extends PureComponent<Props, void> {
   };
 
   render() {
-    const { avatarUrl, auth, email, fullName, presence, orientation } = this.props;
+    const { avatarUrl, auth, email, fullName, presence } = this.props;
     const screenWidth = Dimensions.get('window').width;
 
-    const ContentComponent = orientation === 'LANDSCAPE' ? LandscapeContent : PortraitContent;
-
     return (
-      <ContentComponent
-        screenWidth={screenWidth}
-        avatar={width => (
-          <Avatar
-            avatarUrl={getMediumAvatar(avatarUrl)}
-            name={fullName}
-            email={email}
-            size={width}
-            realm={auth.realm}
-            shape="square"
-          />
-        )}
-        userDetails={() => (
+      <View style={styles.containerPortrait}>
+        <Avatar
+          avatarUrl={getMediumAvatar(avatarUrl)}
+          name={fullName}
+          email={email}
+          size={screenWidth}
+          realm={auth.realm}
+          shape="square"
+        />
+        <View style={styles.infoContainer}>
           <View style={styles.details}>
             <UserStatusIndicator presence={presence} style={styles.statusIndicator} />
             <Text style={styles.info}>{email}</Text>
           </View>
-        )}
-        sendButton={() => (
           <ZulipButton
             style={styles.sendButton}
             text="Send private message"
             onPress={this.handleChatPress}
             Icon={IconPrivateChat}
           />
-        )}
-      />
+        </View>
+      </View>
     );
   }
 }
