57980805bf79a8bd5eb61b902b660149a9837323
Boris Yankov <borisyankov@gmail.com>
2018-05-17 23:40:43 +0300

    Initialize user group state and reset it
    * on RealmInit initialize with related data
    * on Logout/SwitchAccount/Login reset the data
    * adds a direct selector

--- src/user-groups/userGroupsReducers.js
+++ src/user-groups/userGroupsReducers.js
@@ -1,14 +1,26 @@
 /* @flow */
-import type { UserGroupsState, UserGroupsAction } from '../types';
+import type { UserGroupsState, UserGroupsAction, RealmInitAction } from '../types';
+import { LOGOUT, LOGIN_SUCCESS, ACCOUNT_SWITCH, REALM_INIT } from '../actionConstants';
 import { NULL_ARRAY } from '../nullObjects';
 
 const initialState: UserGroupsState = NULL_ARRAY;
+
+const realmInit = (state: UserGroupsState, action: RealmInitAction): UserGroupsState =>
+  action.data.realm_user_groups;
 
 export default (
   state: UserGroupsState = initialState,
   action: UserGroupsAction,
 ): UserGroupsState => {
   switch (action.type) {
+    case LOGOUT:
+    case LOGIN_SUCCESS:
+    case ACCOUNT_SWITCH:
+      return initialState;
+
+    case REALM_INIT:
+      return realmInit(state, action);
+
     default:
       return state;
   }
