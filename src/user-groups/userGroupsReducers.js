4965772a2e1970bf9e6c0701b2dda6e9e95d88b0
Boris Yankov <borisyankov@gmail.com>
2018-05-18 16:55:33 +0300

    Handle server events for new user groups created

--- src/user-groups/userGroupsReducers.js
+++ src/user-groups/userGroupsReducers.js
@@ -1,12 +1,28 @@
 /* @flow */
-import type { UserGroupsState, UserGroupsAction, RealmInitAction } from '../types';
-import { LOGOUT, LOGIN_SUCCESS, ACCOUNT_SWITCH, REALM_INIT } from '../actionConstants';
+import type {
+  UserGroupsState,
+  UserGroupsAction,
+  RealmInitAction,
+  EventUserGroupAddAction,
+} from '../types';
+import {
+  LOGOUT,
+  LOGIN_SUCCESS,
+  ACCOUNT_SWITCH,
+  REALM_INIT,
+  EVENT_USER_GROUP_ADD,
+} from '../actionConstants';
 import { NULL_ARRAY } from '../nullObjects';
 
 const initialState: UserGroupsState = NULL_ARRAY;
 
 const realmInit = (state: UserGroupsState, action: RealmInitAction): UserGroupsState =>
   action.data.realm_user_groups;
+
+const eventUserGroupAdd = (
+  state: UserGroupsState,
+  action: EventUserGroupAddAction,
+): UserGroupsState => [...state, action.group];
 
 export default (
   state: UserGroupsState = initialState,
@@ -20,6 +36,9 @@ export default (
 
     case REALM_INIT:
       return realmInit(state, action);
+
+    case EVENT_USER_GROUP_ADD:
+      return eventUserGroupAdd(state, action);
 
     default:
       return state;
