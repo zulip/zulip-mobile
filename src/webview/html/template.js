a182ca87efd0da5f45c19e5a05778de664300afa
Greg Price <greg@zulipchat.com>
2018-05-21 00:16:29 -0400

    webview: Make a template tag to automatically escape HTML.
    This makes the default the safe thing, and we provide a handy syntax
    (but with a nice loud '!' in it) for including a value raw into the
    HTML.  See doc comment for details.

---
+++ src/webview/html/template.js
@@ -0,0 +1,27 @@
+/* @flow */
+import escape from 'lodash.escape';
+
+// As an ES6 template tag, escapes HTML in interpolated values,
+// except where marked.
+//
+// To mark a value as HTML to be included raw, use '$!$' instead of '$':
+//   template`Hello ${&<world}` -> 'Hello &amp;&lt;world'
+// but
+//   template`Hello $!${&<world}` -> 'Hello &<world'
+// To include a literal '$!' before a value, write '$\!':
+//   template`Hello $\!${&<world}` -> 'Hello $!&amp;&lt;world'
+export default (strings: any, ...values: Array<string>) => {
+  // 'strings: any' because github.com/facebook/flow/issues/2616
+  const result = [];
+  values.forEach((value, i) => {
+    if (strings.raw[i].endsWith('$!')) {
+      result.push(strings[i].substring(0, strings[i].length - 2));
+      result.push(value);
+    } else {
+      result.push(strings[i]);
+      result.push(escape(value));
+    }
+  });
+  result.push(strings[strings.length - 1]);
+  return result.join('');
+};
