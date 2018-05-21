3e4d0c1b5aec09a815a9509ca0731f0165918cac
Greg Price <greg@zulipchat.com>
2018-05-21 00:16:29 -0400

    webview: Make a template tag to automatically escape HTML.
    This makes the default the safe thing, and we provide a handy syntax
    (but with a nice loud '!' in it) for including a value raw into the
    HTML.  See doc comment for details.

---
+++ src/webview/html/__tests__/template-test.js
@@ -0,0 +1,25 @@
+import escape from 'lodash.escape';
+import template from '../template';
+
+describe('template', () => {
+  const evil = '<alert />&amp;';
+  const escaped = escape(evil);
+
+  test('interpolates', () => {
+    expect(template``).toEqual('');
+    expect(template`a`).toEqual('a');
+    expect(template`a${'b'}c`).toEqual('abc');
+  });
+
+  test('escapes HTML', () => {
+    expect(template`a${evil}c`).toEqual(`a${escaped}c`);
+  });
+
+  test('optionally preserves HTML', () => {
+    expect(template`a$!${evil}c`).toEqual(`a${evil}c`);
+  });
+
+  test('has an escape for the option', () => {
+    expect(template`a$\!${evil}c`).toEqual(`a$!${escaped}c`);
+  });
+});
