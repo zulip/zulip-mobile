7813d792184ce58a0ce3c538291257aa9f3c9ea1
Boris Yankov <borisyankov@gmail.com>
2018-05-22 12:14:13 +0300

    Extract remaining styles into 'miscStyles'
    Now 'theme.js' just merges several styles and exports them.
    In future commits miscStyles will be split further.

---
+++ src/styles/miscStyles.js
@@ -0,0 +1,237 @@
+/* @flow */
+import { Platform } from 'react-native';
+
+import type { Style } from '../types';
+import { BORDER_COLOR, BRAND_COLOR, CONTROL_SIZE, SPACING, HALF_SPACING } from './';
+
+export type MiscStyles = {
+  text: Style,
+  largerText: Style,
+  color: Style,
+  backgroundColor: Style,
+  row: Style,
+  input: Style,
+  passwordInput: Style,
+  showPasswordButton: Style,
+  showPasswordButtonText: Style,
+  realmInput: Style,
+  realmPlaceholder: Style,
+  realmInputEmpty: Style,
+  background: Style,
+  label: Style,
+  icon: Style,
+  settingsIcon: Style,
+  item: Style,
+  listItem: Style,
+  screen: Style,
+  flexed: Style,
+  flexedLeftAlign: Style,
+  messageList: Style,
+  webview: Style,
+  rightItem: Style,
+  screenWrapper: Style,
+  link: Style,
+  divider: Style,
+  navigationCard: Style,
+  container: Style,
+  center: Style,
+  heading2: Style,
+  field: Style,
+  disabled: Style,
+  username: Style,
+  cardView: Style,
+  lineSeparator: Style,
+  floatingView: Style,
+  alignBottom: Style,
+};
+
+type Props = {
+  color: string,
+  backgroundColor: string,
+  borderColor: string,
+  cardColor: string,
+  dividerColor: string,
+};
+
+export default ({ color, backgroundColor, borderColor, cardColor, dividerColor }: Props) => ({
+  text: {
+    fontSize: 16,
+  },
+  largerText: {
+    fontSize: 20,
+  },
+  color: {
+    color,
+  },
+  backgroundColor: {
+    backgroundColor,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    color,
+    ...Platform.select({
+      ios: {
+        borderWidth: 1,
+        borderColor: BORDER_COLOR,
+        borderRadius: 2,
+        padding: 8,
+      },
+    }),
+  },
+  passwordInput: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  showPasswordButton: {
+    position: 'absolute',
+    right: 0,
+    alignItems: 'center',
+    padding: HALF_SPACING,
+  },
+  showPasswordButtonText: {
+    color: BRAND_COLOR,
+  },
+  realmInput: {
+    color,
+    padding: 0,
+    fontSize: 20,
+  },
+  realmPlaceholder: {
+    opacity: 0.75,
+  },
+  realmInputEmpty: {
+    width: 1,
+  },
+  background: {
+    backgroundColor,
+  },
+  label: {
+    color,
+    fontSize: 15,
+  },
+  icon: {
+    color,
+    width: 24,
+    height: 24,
+    margin: 8,
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  settingsIcon: {
+    marginLeft: 8,
+    marginRight: 16,
+  },
+  item: {
+    flex: 1,
+    flexBasis: CONTROL_SIZE,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: HALF_SPACING,
+    paddingBottom: HALF_SPACING,
+    paddingLeft: SPACING,
+    paddingRight: SPACING,
+  },
+  screen: {
+    flex: 1,
+    flexDirection: 'column',
+    // alignItems: 'stretch',
+    backgroundColor,
+  },
+  flexed: {
+    flex: 1,
+  },
+  flexedLeftAlign: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  messageList: {
+    flex: 1,
+  },
+  webview: {
+    borderWidth: 0,
+    backgroundColor,
+  },
+  rightItem: {
+    marginLeft: 'auto',
+  },
+  screenWrapper: {
+    flex: 1,
+    padding: 8,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  link: {
+    marginTop: 10,
+    fontSize: 15,
+    color: BRAND_COLOR,
+    textAlign: 'right',
+  },
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: dividerColor,
+  },
+  navigationCard: {
+    backgroundColor,
+    shadowColor: 'transparent',
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading2: {
+    fontSize: 20,
+  },
+  field: {
+    flex: 1,
+    flexDirection: 'row',
+    height: CONTROL_SIZE,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  disabled: {
+    backgroundColor: '#ddd',
+    color: '#333',
+  },
+  username: {
+    flex: 1,
+    fontWeight: 'bold',
+    fontSize: 16,
+    lineHeight: 16,
+  },
+  cardView: {
+    backgroundColor: cardColor,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+  },
+  lineSeparator: {
+    height: 1,
+    backgroundColor: cardColor,
+    margin: 4,
+  },
+  floatingView: {
+    position: 'absolute',
+    width: '100%',
+  },
+  alignBottom: {
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+});
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,10 +1,14 @@
 /* @flow */
-import { Platform } from 'react-native';
-
-import { BORDER_COLOR, BRAND_COLOR, CONTROL_SIZE, SPACING, HALF_SPACING } from './';
+import type { UtilityStyles } from './utilityStyles';
+import type { ComposeBoxStyles } from './composeBoxStyles';
+import type { NavStyles } from './navStyles';
+import type { MiscStyles } from './miscStyles';
 import utilityStyles from './utilityStyles';
 import composeBoxStyles from './composeBoxStyles';
 import navStyles from './navStyles';
+import miscStyles from './miscStyles';
+
+export type AppStyles = UtilityStyles & ComposeBoxStyles & NavStyles & MiscStyles;
 
 type Props = {
   color: string,
@@ -14,188 +18,9 @@ type Props = {
   dividerColor: string,
 };
 
-export default ({ color, backgroundColor, borderColor, cardColor, dividerColor }: Props) => ({
+export default (props: Props) => ({
   ...utilityStyles,
-  ...composeBoxStyles({ color, backgroundColor, borderColor }),
-  ...navStyles({ color, backgroundColor, borderColor }),
-  text: {
-    fontSize: 16,
-  },
-  largerText: {
-    fontSize: 20,
-  },
-  color: {
-    color,
-  },
-  backgroundColor: {
-    backgroundColor,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  input: {
-    color,
-    ...Platform.select({
-      ios: {
-        borderWidth: 1,
-        borderColor: BORDER_COLOR,
-        borderRadius: 2,
-        padding: 8,
-      },
-    }),
-  },
-  passwordInput: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  showPasswordButton: {
-    position: 'absolute',
-    right: 0,
-    alignItems: 'center',
-    padding: HALF_SPACING,
-  },
-  showPasswordButtonText: {
-    color: BRAND_COLOR,
-  },
-  realmInput: {
-    color,
-    padding: 0,
-    fontSize: 20,
-  },
-  realmPlaceholder: {
-    opacity: 0.75,
-  },
-  realmInputEmpty: {
-    width: 1,
-  },
-  background: {
-    backgroundColor,
-  },
-  label: {
-    color,
-    fontSize: 15,
-  },
-  icon: {
-    color,
-    width: 24,
-    height: 24,
-    margin: 8,
-    fontSize: 24,
-    textAlign: 'center',
-  },
-  settingsIcon: {
-    marginLeft: 8,
-    marginRight: 16,
-  },
-  item: {
-    flex: 1,
-    flexBasis: CONTROL_SIZE,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  listItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: HALF_SPACING,
-    paddingBottom: HALF_SPACING,
-    paddingLeft: SPACING,
-    paddingRight: SPACING,
-  },
-  screen: {
-    flex: 1,
-    flexDirection: 'column',
-    // alignItems: 'stretch',
-    backgroundColor,
-  },
-  flexed: {
-    flex: 1,
-  },
-  flexedLeftAlign: {
-    flex: 1,
-    justifyContent: 'flex-start',
-  },
-  messageList: {
-    flex: 1,
-  },
-  webview: {
-    borderWidth: 0,
-    backgroundColor,
-  },
-  rightItem: {
-    marginLeft: 'auto',
-  },
-  screenWrapper: {
-    flex: 1,
-    padding: 8,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-  },
-  link: {
-    marginTop: 10,
-    fontSize: 15,
-    color: BRAND_COLOR,
-    textAlign: 'right',
-  },
-  divider: {
-    borderBottomWidth: 1,
-    borderBottomColor: dividerColor,
-  },
-  navigationCard: {
-    backgroundColor,
-    shadowColor: 'transparent',
-  },
-  container: {
-    flex: 1,
-    padding: 16,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-  },
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  heading2: {
-    fontSize: 20,
-  },
-  field: {
-    flex: 1,
-    flexDirection: 'row',
-    height: CONTROL_SIZE,
-    marginTop: 5,
-    marginBottom: 5,
-  },
-  disabled: {
-    backgroundColor: '#ddd',
-    color: '#333',
-  },
-  username: {
-    flex: 1,
-    fontWeight: 'bold',
-    fontSize: 16,
-    lineHeight: 16,
-  },
-  cardView: {
-    backgroundColor: cardColor,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-  },
-  lineSeparator: {
-    height: 1,
-    backgroundColor: cardColor,
-    margin: 4,
-  },
-  floatingView: {
-    position: 'absolute',
-    width: '100%',
-  },
-  alignBottom: {
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-  },
+  ...composeBoxStyles(props),
+  ...navStyles(props),
+  ...miscStyles(props),
 });
