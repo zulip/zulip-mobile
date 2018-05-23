b74414a20a1374438177423c8e3f4bc1844d3032
Boris Yankov <borisyankov@gmail.com>
2018-05-23 20:29:35 +0300

    maintenance: Run 'prettier'

--- src/webview/html/__tests__/render-test.js
+++ src/webview/html/__tests__/render-test.js
@@ -2,9 +2,13 @@ import messageTypingAsHtml from '../messageTypingAsHtml';
 
 describe('typing', () => {
   it('escapes values', () => {
-    expect(messageTypingAsHtml('&<r', [{
-      avatar_url: '&<av',
-      email: '&<e',
-    }])).not.toContain('&<');
+    expect(
+      messageTypingAsHtml('&<r', [
+        {
+          avatar_url: '&<av',
+          email: '&<e',
+        },
+      ]),
+    ).not.toContain('&<');
   });
 });
