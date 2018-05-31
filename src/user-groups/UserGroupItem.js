2694d28531d6af2c4d310f68ca835b4753bfe7eb
Boris Yankov <borisyankov@gmail.com>
2018-05-31 23:24:52 +0300

    user groups: Add component `UserGroupItem`.
    Add a new component `UserGroupItem.js` to be used in the
    `PeopleAutocomplete` list.

---
+++ src/user-groups/UserGroupItem.js
@@ -0,0 +1,67 @@
+/* @flow */
+import React, { PureComponent } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import type { Context } from '../types';
+import { IconPeople } from '../common/Icons';
+import { RawLabel, Touchable } from '../common';
+
+const componentStyles = StyleSheet.create({
+  text: {
+    marginLeft: 8,
+  },
+  textEmail: {
+    fontSize: 10,
+    color: '#999',
+  },
+  textWrapper: {
+    flex: 1,
+  },
+});
+
+type Props = {
+  name: string,
+  description: string,
+  onPress: (name: string) => void,
+};
+
+export default class UserGroupItem extends PureComponent<Props> {
+  context: Context;
+  props: Props;
+
+  static contextTypes = {
+    styles: () => null,
+  };
+
+  handlePress = () => {
+    const { name, onPress } = this.props;
+    onPress(name);
+  };
+
+  render() {
+    const { styles } = this.context;
+    const { name, description } = this.props;
+
+    return (
+      <Touchable onPress={this.handlePress}>
+        <View style={styles.listItem}>
+          <IconPeople size={32} />
+          <View style={componentStyles.textWrapper}>
+            <RawLabel
+              style={[componentStyles.text]}
+              text={name}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            />
+            <RawLabel
+              style={[componentStyles.text, componentStyles.textEmail]}
+              text={description}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            />
+          </View>
+        </View>
+      </Touchable>
+    );
+  }
+}
