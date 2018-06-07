ce2e3333db16480736651357472ab415c54c616c
Boris Yankov <borisyankov@gmail.com>
2018-06-07 18:27:40 +0300

    compose box: Use 'controlled' inputs on iOS
    This reverts the `ComposeBox.js` file to its version before making
    it uncontrolled. Keeping it as a separate copy-pasted file for
    simplicity.
    
    This is a temporary, but needed because of a bug that does not allow
    us to reset an uncontrolled input's value.
    
    A likely fix, not yet merged is here:
    https://github.com/facebook/react-native/pull/18278

---
+++ src/compose/ComposeBox.ios.js
@@ -0,0 +1,353 @@
+/* @flow */
+import React, { PureComponent } from 'react';
+import { View, TextInput, findNodeHandle } from 'react-native';
+import { connect } from 'react-redux';
+import TextInputReset from 'react-native-text-input-reset';
+import isEqual from 'lodash.isequal';
+
+import type {
+  Auth,
+  Context,
+  Narrow,
+  EditMessage,
+  InputSelectionType,
+  User,
+  Dispatch,
+  Dimensions,
+  GlobalState,
+} from '../types';
+import {
+  addToOutbox,
+  cancelEditMessage,
+  draftAdd,
+  draftRemove,
+  fetchTopicsForActiveStream,
+  sendTypingEvent,
+} from '../actions';
+import { updateMessage } from '../api';
+import { FloatingActionButton, Input, MultilineInput } from '../common';
+import { showErrorAlert } from '../utils/info';
+import { IconDone, IconSend } from '../common/Icons';
+import { isStreamNarrow, topicNarrow } from '../utils/narrow';
+import ComposeMenu from './ComposeMenu';
+import AutocompleteViewWrapper from '../autocomplete/AutocompleteViewWrapper';
+import getComposeInputPlaceholder from './getComposeInputPlaceholder';
+import NotSubscribed from '../message/NotSubscribed';
+
+import {
+  getAuth,
+  getSession,
+  canSendToActiveNarrow,
+  getLastMessageTopic,
+  getUsers,
+  getShowMessagePlaceholders,
+} from '../selectors';
+import { getIsActiveStreamSubscribed } from '../subscriptions/subscriptionSelectors';
+import { getDraftForActiveNarrow } from '../drafts/draftsSelectors';
+
+type Props = {
+  auth: Auth,
+  canSend: boolean,
+  narrow: Narrow,
+  users: User[],
+  draft: string,
+  lastMessageTopic: string,
+  isSubscribed: boolean,
+  editMessage: EditMessage,
+  safeAreaInsets: Dimensions,
+  dispatch: Dispatch,
+  messageInputRef: (component: any) => void,
+};
+
+type State = {
+  isMessageFocused: boolean,
+  isTopicFocused: boolean,
+  isMenuExpanded: boolean,
+  topic: string,
+  message: string,
+  height: number,
+  selection: InputSelectionType,
+};
+
+class ComposeBox extends PureComponent<Props, State> {
+  context: Context;
+  props: Props;
+  state: State;
+
+  messageInput: TextInput = null;
+  topicInput: TextInput = null;
+
+  static contextTypes = {
+    styles: () => null,
+  };
+
+  state = {
+    isMessageFocused: false,
+    isTopicFocused: false,
+    isMenuExpanded: false,
+    height: 20,
+    topic: '',
+    message: this.props.draft,
+    selection: { start: 0, end: 0 },
+  };
+
+  handleComposeMenuToggle = () => {
+    this.setState(({ isMenuExpanded }) => ({
+      isMenuExpanded: !isMenuExpanded,
+    }));
+  };
+
+  handleLayoutChange = (event: Object) => {
+    this.setState({
+      height: event.nativeEvent.layout.height,
+    });
+  };
+
+  handleTopicChange = (topic: string) => {
+    this.setState({ topic, isMenuExpanded: false });
+  };
+
+  handleMessageChange = (message: string) => {
+    this.setState({ message, isMenuExpanded: false });
+    const { dispatch, narrow } = this.props;
+    dispatch(sendTypingEvent(narrow));
+  };
+
+  handleMessageSelectionChange = (event: Object) => {
+    const { selection } = event.nativeEvent;
+    this.setState({ selection });
+  };
+
+  handleMessageFocus = () => {
+    const { lastMessageTopic } = this.props;
+    this.setState(({ topic }) => ({
+      isMessageFocused: true,
+      isMenuExpanded: false,
+      topic: topic || lastMessageTopic,
+    }));
+  };
+
+  handleMessageBlur = () => {
+    setTimeout(() => {
+      this.setState({
+        isMessageFocused: false,
+        isMenuExpanded: false,
+      });
+    }, 200); // give a chance to the topic input to get the focus
+  };
+
+  handleTopicFocus = () => {
+    const { dispatch, narrow } = this.props;
+    this.setState({
+      isTopicFocused: true,
+      isMenuExpanded: false,
+    });
+    dispatch(fetchTopicsForActiveStream(narrow));
+  };
+
+  handleTopicBlur = () => {
+    setTimeout(() => {
+      this.setState({
+        isTopicFocused: false,
+        isMenuExpanded: false,
+      });
+    }, 200); // give a chance to the mesage input to get the focus
+  };
+
+  handleInputTouchStart = () => {
+    this.setState({ isMenuExpanded: false });
+  };
+
+  clearMessageInput = () => {
+    if (this.messageInput) {
+      this.messageInput.clear();
+      if (TextInputReset) {
+        TextInputReset.resetKeyboardInput(findNodeHandle(this.messageInput));
+      }
+    }
+
+    this.handleMessageChange('');
+  };
+
+  handleSend = () => {
+    const { dispatch, narrow } = this.props;
+    const { topic, message } = this.state;
+
+    const destinationNarrow = isStreamNarrow(narrow)
+      ? topicNarrow(narrow[0].operand, topic || '(no topic)')
+      : narrow;
+
+    dispatch(addToOutbox(destinationNarrow, message));
+    dispatch(draftRemove(narrow));
+
+    this.clearMessageInput();
+  };
+
+  handleEdit = () => {
+    const { auth, editMessage, dispatch } = this.props;
+    const { message, topic } = this.state;
+    const content = editMessage.content !== message ? message : undefined;
+    const subject = topic !== editMessage.topic ? topic : undefined;
+    if (content || subject) {
+      updateMessage(auth, { content, subject }, editMessage.id).catch(error => {
+        showErrorAlert(error.message, 'Failed to edit message');
+      });
+    }
+    dispatch(cancelEditMessage());
+  };
+
+  tryUpdateDraft = () => {
+    const { dispatch, draft, narrow } = this.props;
+    const { message } = this.state;
+
+    if (draft.trim() === message.trim()) {
+      return;
+    }
+
+    if (message.trim().length === 0) {
+      dispatch(draftRemove(narrow));
+    } else {
+      dispatch(draftAdd(narrow, message));
+    }
+  };
+
+  componentWillUnmount() {
+    this.tryUpdateDraft();
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.editMessage !== this.props.editMessage) {
+      const topic =
+        isStreamNarrow(nextProps.narrow) && nextProps.editMessage
+          ? nextProps.editMessage.topic
+          : '';
+      this.setState({
+        message: nextProps.editMessage ? nextProps.editMessage.content : '',
+        topic,
+      });
+      if (this.messageInput) {
+        this.messageInput.focus();
+      }
+    } else if (!isEqual(nextProps.narrow, this.props.narrow)) {
+      this.tryUpdateDraft();
+
+      if (nextProps.draft) {
+        this.setState({ message: nextProps.draft });
+      } else {
+        this.clearMessageInput();
+      }
+    }
+  }
+
+  render() {
+    const { styles } = this.context;
+    const {
+      isMessageFocused,
+      isTopicFocused,
+      isMenuExpanded,
+      height,
+      message,
+      topic,
+      selection,
+    } = this.state;
+    const {
+      auth,
+      canSend,
+      narrow,
+      users,
+      editMessage,
+      safeAreaInsets,
+      messageInputRef,
+      isSubscribed,
+    } = this.props;
+
+    if (!canSend) {
+      return null;
+    }
+
+    if (!isSubscribed) {
+      return <NotSubscribed narrow={narrow} />;
+    }
+
+    const canSelectTopic = (isMessageFocused || isTopicFocused) && isStreamNarrow(narrow);
+    const placeholder = getComposeInputPlaceholder(narrow, auth.email, users);
+
+    return (
+      <View style={{ marginBottom: safeAreaInsets.bottom }}>
+        <AutocompleteViewWrapper
+          composeText={message}
+          isTopicFocused={isTopicFocused}
+          marginBottom={height}
+          messageSelection={selection}
+          narrow={narrow}
+          topicText={topic}
+          onMessageAutocomplete={this.handleMessageChange}
+          onTopicAutocomplete={this.handleTopicChange}
+        />
+        <View style={styles.composeBox} onLayout={this.handleLayoutChange}>
+          <View style={styles.alignBottom}>
+            <ComposeMenu
+              narrow={narrow}
+              expanded={isMenuExpanded}
+              onExpandContract={this.handleComposeMenuToggle}
+            />
+          </View>
+          <View style={styles.composeText}>
+            {canSelectTopic && (
+              <Input
+                style={styles.topicInput}
+                underlineColorAndroid="transparent"
+                placeholder="Topic"
+                selectTextOnFocus
+                textInputRef={component => {
+                  this.topicInput = component;
+                }}
+                onChangeText={this.handleTopicChange}
+                onFocus={this.handleTopicFocus}
+                onBlur={this.handleTopicBlur}
+                onTouchStart={this.handleInputTouchStart}
+                value={topic}
+              />
+            )}
+            <MultilineInput
+              style={styles.composeTextInput}
+              placeholder={placeholder}
+              textInputRef={component => {
+                if (component) {
+                  this.messageInput = component;
+                  messageInputRef(component);
+                }
+              }}
+              value={message}
+              onBlur={this.handleMessageBlur}
+              onChange={this.handleMessageChange}
+              onFocus={this.handleMessageFocus}
+              onSelectionChange={this.handleMessageSelectionChange}
+              onTouchStart={this.handleInputTouchStart}
+            />
+          </View>
+          <View style={styles.alignBottom}>
+            <FloatingActionButton
+              style={styles.composeSendButton}
+              Icon={editMessage === null ? IconSend : IconDone}
+              size={32}
+              disabled={message.trim().length === 0}
+              onPress={editMessage === null ? this.handleSend : this.handleEdit}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default connect((state: GlobalState, props) => ({
+  auth: getAuth(state),
+  users: getUsers(state),
+  safeAreaInsets: getSession(state).safeAreaInsets,
+  isSubscribed: getIsActiveStreamSubscribed(props.narrow)(state),
+  canSend: canSendToActiveNarrow(props.narrow) && !getShowMessagePlaceholders(props.narrow)(state),
+  editMessage: getSession(state).editMessage,
+  draft: getDraftForActiveNarrow(props.narrow)(state),
+  lastMessageTopic: getLastMessageTopic(props.narrow)(state),
+}))(ComposeBox);
