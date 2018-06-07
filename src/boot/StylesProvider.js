2267150aec792fd5f39f3cc94076a46b8bee80d0
Robert Hönig <robhoenig@gmail.com>
2018-06-07 10:22:51 +0200

    styles provider: Use standard Redux dispatch.
    See #2628 for reference.

--- src/boot/StylesProvider.js
+++ src/boot/StylesProvider.js
@@ -1,9 +1,10 @@
 /* @flow */
+import { connect } from 'react-redux';
+
 import React, { PureComponent } from 'react';
 import { StyleSheet } from 'react-native';
 
 import type { ChildrenArray } from '../types';
-import connectWithActions from '../connectWithActions';
 import { getSettings } from '../directSelectors';
 import themeCreator from '../styles/theme';
 import themeDark from '../styles/themeDark';
@@ -18,6 +19,13 @@ const themeNameToObject = {
 const Dummy = props => props.children;
 
 type Props = {
+  // This errors because `string` is too generic:
+  // string [1] is incompatible with string enum [2].
+  // In reality, `theme`  is and should be marked as an enum of type
+  // `ThemeType`. However, doing so throws another error:
+  // undefined [1] is incompatible with string enum [2].
+  // We should figure out what causes this and fix it.
+  // $FlowFixMe
   theme: string,
   children?: ChildrenArray<*>,
 };
@@ -47,6 +55,6 @@ class StyleProvider extends PureComponent<Props> {
   }
 }
 
-export default connectWithActions(state => ({
+export default connect(state => ({
   theme: getSettings(state).theme,
 }))(StyleProvider);
