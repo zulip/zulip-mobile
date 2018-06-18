75f8618f9120fc44d1e2bbe87fb553d0c5d25db6
Boris Yankov <borisyankov@gmail.com>
2018-06-18 15:55:01 +0300

    webview: Add css for KaTeX
    This is the main `katex.less` file compiled to css
    https://github.com/Khan/KaTeX/blob/master/src/katex.less
    
    We do not yet include the custom fonts, but still the improvement
    in rendering LaTeX is significant.

--- src/webview/css/css.js
+++ src/webview/css/css.js
@@ -12,6 +12,7 @@ export default (highlightUnreadMessages: boolean): string => `
 <link type="text/css" href="${getWebviewResource('dark.css')}" rel="stylesheet" />
 <link type="text/css" href="${getWebviewResource('emoji.css')}" rel="stylesheet" />
 <link type="text/css" href="${getWebviewResource('code.css')}" rel="stylesheet" />
+<link type="text/css" href="${getWebviewResource('katex.css')}" rel="stylesheet" />
 <style>
 ${highlightUnreadMessages ? '.message:not([data-read="true"]) { background: red; }' : ''}
 </style>
