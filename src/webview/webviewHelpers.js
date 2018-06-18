3bd2f9c53bcfc800771feac8332f22bcaf2e45b5
Boris Yankov <borisyankov@gmail.com>
2018-06-18 03:49:48 +0300

    webview: Add `getWebviewResource` utility function
    Function is given a resource file name and returns the URL to be
    used by WebView to refer to a resource on that device.

--- src/webview/webviewHelpers.js
+++ src/webview/webviewHelpers.js
@@ -3,3 +3,6 @@
 import { Platform } from 'react-native';
 
 export const baseUrl = Platform.OS === 'ios' ? './' : 'file:///android_asset/webview/';
+
+export const getWebviewResource = (resource: string): string =>
+  Platform.OS === 'ios' ? `./${resource}` : `file:///android_asset/webview/${resource}`;
