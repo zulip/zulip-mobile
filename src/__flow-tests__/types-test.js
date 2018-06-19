564eeea0327ba01ee5dab21a638e8bf6e694270a
Robert Hönig <robhoenig@gmail.com>
2018-06-19 10:56:44 +0200

    flow: Add test suite for types.
    This allows us to verify that our Flow types work as intended.
    In particular, we can now assert type declarations that should
    error.
    
    Without additional configuration, Flow prints assertion errors
    as warnings.  Add `--max-warnings 0` to make all such warnings
    errors, which makes CI tests fail when our tests fail.
    
    Also, add a test for the `Action` type.
    
    Disable eslint for these tests, because we are not testing the
    code but the types, and unused variables and the like are
    acceptable for this.

---
+++ src/__flow-tests__/types-test.js
@@ -0,0 +1,9 @@
+/* @flow */
+import type { Action } from '../types';
+
+// Assert that Action does not allow arbitrary objects.
+{
+  const foo = { nonexistent_key: 'bar' };
+  // $FlowExpectedError
+  const bar: Action = foo;
+}
