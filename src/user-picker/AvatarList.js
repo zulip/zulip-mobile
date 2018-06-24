488b8b37a658cc4aab922d9d184579faa2a96bdf
agzuniverse <aswinganesh666@gmail.com>
2018-06-24 14:22:28 +0530

    Move action button to AvatarList
    The floating action button to create the group/invite users in UserPickerCard was aligned to be on the bottom of the scroll view.
    This caused problems when the list was large as the user had to scroll all the way to the bottom to find the button.
    The button is now moved to the AvatarList component which appears at the top and appears on the right end of this component.

--- src/user-picker/AvatarList.js
+++ src/user-picker/AvatarList.js
@@ -1,48 +1,63 @@
 /* @flow */
 import React, { PureComponent } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 
 import type { User } from '../types';
 import AvatarItem from './AvatarItem';
+import { FloatingActionButton } from '../common';
+import AnimatedScaleComponent from '../animation/AnimatedScaleComponent';
+import { IconDone } from '../common/Icons';
 
 const styles = StyleSheet.create({
   list: {},
+  wrapper: {
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 8,
+  },
 });
 
 type Props = {
   users: User[],
   listRef: (component: any) => void,
   onPress: (email: string) => void,
+  onButtonPress: () => void,
 };
 
 export default class AvatarList extends PureComponent<Props> {
   props: Props;
 
   render() {
-    const { listRef, users, onPress } = this.props;
+    const { listRef, users, onPress, onButtonPress } = this.props;
 
     return (
-      <FlatList
-        style={styles.list}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        initialNumToRender={20}
-        data={users}
-        ref={(component: any) => {
-          if (listRef) {
-            listRef(component);
-          }
-        }}
-        keyExtractor={item => item.email}
-        renderItem={({ item }) => (
-          <AvatarItem
-            email={item.email}
-            avatarUrl={item.avatar_url}
-            fullName={item.full_name}
-            onPress={onPress}
-          />
-        )}
-      />
+      <View style={styles.wrapper}>
+        <FlatList
+          style={styles.list}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          initialNumToRender={20}
+          data={users}
+          ref={(component: any) => {
+            if (listRef) {
+              listRef(component);
+            }
+          }}
+          keyExtractor={item => item.email}
+          renderItem={({ item }) => (
+            <AvatarItem
+              email={item.email}
+              avatarUrl={item.avatar_url}
+              fullName={item.full_name}
+              onPress={onPress}
+            />
+          )}
+        />
+        <AnimatedScaleComponent style={styles.button} visible>
+          <FloatingActionButton Icon={IconDone} size={50} onPress={onButtonPress} disabled={false} />
+        </AnimatedScaleComponent>
+      </View>
     );
   }
 }
