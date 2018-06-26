c868ceb78d6afd3beeed6fe2330eeac1cd34738c
Boris Yankov <borisyankov@gmail.com>
2018-06-27 00:39:40 +0200

    jsdoc: Document FloatingActionButton component

--- src/common/FloatingActionButton.js
+++ src/common/FloatingActionButton.js
@@ -23,6 +23,18 @@ type Props = {
   onPress: () => void,
 };
 
+/**
+ * A button component implementing a popular 'action button'
+ * UI pattern. The button is circular, has an icon, and usually
+ * ovlayed over the main UI in a prominent place.
+ *
+ * @prop [style] - Style applied to the wrapper component.
+ * @prop disabled - If 'true' component can't be pressed and
+ *   becomes visibly inactive.
+ * @prop size - Diameter of the component in pixels.
+ * @prop Icon - Icon component to render.
+ * @prop onPress - Event called on component press.
+ */
 export default class FloatingActionButton extends PureComponent<Props> {
   props: Props;
 
