9c4b5ed66677e3d79a2ce3b56b7ef6430fdb25aa
Boris Yankov <borisyankov@gmail.com>
2018-06-27 14:44:08 +0200

    jsdoc: Document ViewPlaceholder component.

--- src/common/ViewPlaceholder.js
+++ src/common/ViewPlaceholder.js
@@ -7,6 +7,14 @@ type Props = {
   height?: number,
 };
 
+/**
+ * An empty layout component used to simplify UI alignment.
+ * Use when it is easier to use this instead of setting component
+ * padding and margin.
+ *
+ * @prop [width] - Width of the component in pixels.
+ * @prop [height] - Height of the component in pixels.
+ */
 export default class ViewPlaceholder extends PureComponent<Props> {
   props: Props;
 
