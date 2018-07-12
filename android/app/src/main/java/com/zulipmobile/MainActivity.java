8930bd5bda299993e44843c0baa8753ae37f1d8d
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-07-12 19:51:39 +0530

    android: Create a entry point activity for app.
    Which just extends ZulipActvitiy, which also contains
    all the logic.
    
    Why this is required?
    We are pretending that our package name is `com.zulipmobile.debug`
    by adding a comment in the manifest (just for RN).
    (Note: Read previous commits for more info)
    
    If we are pretending that our package name is something different
    we want valid activity (entry point activity) which should be
    present at this pretending package.
    
    Thus extracting logic will help in avoiding logic duplicacy.

--- android/app/src/main/java/com/zulipmobile/MainActivity.java
+++ android/app/src/main/java/com/zulipmobile/MainActivity.java
@@ -1,26 +1,20 @@
 package com.zulipmobile;
 
-import android.content.Intent;
-import android.content.res.Configuration;
+/**
+ * Entry point activity for the APP
+ * <p>
+ * Which just extends ZulipActvitiy, which also contains all the logic.
+ * <p>
+ * We are pretending that our package name is `com.zulipmobile.debug`
+ * by adding a comment in the manifest (just for RN).
+ * <p>
+ * If we are pretending that our package name is something different
+ * we want valid activity (entry point activity) which should be
+ * present at this pretending package.
+ * <p>
+ * Thus extracting logic will help in avoiding logic duplicacy.
+ */
 
-import com.facebook.react.ReactActivity;
+public class MainActivity extends ZulipActivity {
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "ZulipMobile";
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        Intent intent = new Intent("onConfigurationChanged");
-        intent.putExtra("newConfig", newConfig);
-        this.sendBroadcast(intent);
-    }
 }
