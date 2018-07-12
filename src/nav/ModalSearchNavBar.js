2ecf4254987e8e77595c09fb7de06617e9955252
Aswin G <aswinganesh666@gmail.com>
2018-07-12 17:35:44 +0530

    ModalSearchNavBar: Remove redundant code
    Earlier ModelNavBar made search functionality available depending on
    props passed to it. Later this was spun off into two components,
    ModalNavBar and ModalSearchNavBar, with ModalSearchNavBar being used
    when search was required. There was some leftover logic from when one
    component provided the search functionality in ModalSearchNavBar,
    which has been removed in this commit.
    
    The value of isSearchActive is always true for ModelSearchNavBar,
    since the prop searchBar is never passed by anything in the code
    anymore. The only function that can set it to false,
    disableSearchActiveState() is never called because that requires
    clearSearchInput prop to be true, and that is never passed by anything
    as well. This causes some code to be never exectued.

--- src/nav/ModalSearchNavBar.js
+++ src/nav/ModalSearchNavBar.js
@@ -2,76 +2,33 @@
 import { connect } from 'react-redux';
 
 import React, { PureComponent } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 
 import type { Dispatch, Context, GlobalState } from '../types';
 import { getNav } from '../selectors';
-import { NAVBAR_SIZE } from '../styles';
-import { Label, SearchInput } from '../common';
+import { SearchInput } from '../common';
 import NavButton from './NavButton';
 import { connectPreserveOnBackOption } from '../utils/redux';
 import { navigateBack } from '../actions';
-
-const localStyles = StyleSheet.create({
-  buttonCancel: {
-    transform: [{ rotate: '45deg' }],
-  },
-});
 
 type Props = {
   nav: any,
   dispatch: Dispatch,
   autoFocus: boolean,
-  title: string,
-  searchBar: boolean,
   searchBarOnChange: () => void,
-  clearSearchInput?: () => void,
 };
 
-type State = {
-  isSearchActive: boolean,
-};
-
-class ModalSearchNavBar extends PureComponent<Props, State> {
+class ModalSearchNavBar extends PureComponent<Props> {
   context: Context;
   props: Props;
-  state: State = {
-    isSearchActive: false,
-  };
 
   static contextTypes = {
     styles: () => null,
   };
 
-  enableSearchActiveState = () => {
-    this.setState({
-      isSearchActive: true,
-    });
-  };
-
-  disableSearchActiveState = () => {
-    this.setState({
-      isSearchActive: false,
-    });
-  };
-
   render() {
     const { styles } = this.context;
-    const { isSearchActive } = this.state;
-    const {
-      dispatch,
-      autoFocus,
-      nav,
-      title,
-      searchBar,
-      searchBarOnChange,
-      clearSearchInput,
-    } = this.props;
-    const showSearchInput = isSearchActive || !searchBar;
-    const textStyle = [
-      styles.navTitle,
-      nav.index > 0 && showSearchInput && { marginRight: NAVBAR_SIZE },
-    ];
+    const { dispatch, autoFocus, nav, searchBarOnChange } = this.props;
 
     return (
       <View style={styles.navBar}>
@@ -83,23 +40,7 @@ class ModalSearchNavBar extends PureComponent<Props, State> {
             }}
           />
         )}
-        {showSearchInput ? (
-          <SearchInput autoFocus={autoFocus} onChange={searchBarOnChange} />
-        ) : (
-          <Label style={textStyle} text={title} />
-        )}
-        {!showSearchInput && <NavButton name="search" onPress={this.enableSearchActiveState} />}
-        {showSearchInput
-          && clearSearchInput && (
-            <NavButton
-              name="md-add"
-              style={localStyles.buttonCancel}
-              onPress={() => {
-                this.disableSearchActiveState();
-                clearSearchInput();
-              }}
-            />
-          )}
+        <SearchInput autoFocus={autoFocus} onChange={searchBarOnChange} />
       </View>
     );
   }
