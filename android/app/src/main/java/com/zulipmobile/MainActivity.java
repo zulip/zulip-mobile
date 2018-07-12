def609f02bfb3ce5cf23aa0cc0a010e75fb5c821
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-07-12 20:10:36 +0530

    android: Create entry point activity for debug build.
    We are pretending that our package name is
    `com.zulipmobile.debug` (by adding a comment
    in manifest (see previous commits)) to
    `react-native run-android`.
    
    So there should a laucher activity present
    at this pretended package name.
    
    This activity extends the ZulipActivity which
    contains all the logics.

--- android/app/src/main/java/com/zulipmobile/MainActivity.java
+++ android/app/src/main/java/com/zulipmobile/MainActivity.java
@@ -3,16 +3,10 @@ package com.zulipmobile;
 /**
  * Entry point activity for the APP
  * <p>
- * Which just extends ZulipActvitiy, which also contains all the logic.
- * <p>
  * We are pretending that our package name is `com.zulipmobile.debug`
- * by adding a comment in the manifest (just for RN).
- * <p>
- * If we are pretending that our package name is something different
- * we want valid activity (entry point activity) which should be
- * present at this pretending package.
- * <p>
- * Thus extracting logic will help in avoiding logic duplicacy.
+ * by adding a comment in the manifest (just for RN). But for others (release build)
+ * this is the entry point.
+ * So just extends ZulipActvitiy, which also contains all the logic.
  */
 
 public class MainActivity extends ZulipActivity {
