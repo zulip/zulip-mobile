08f6c64575334d5d6861294374919ac6f01d46c5
Boris Yankov <borisyankov@gmail.com>
2018-07-17 21:39:11 +0300

    flow: Move api-specific types to `apiTypes.js`.
    To make the API functions less coupled with the rest of the app,
    move all types the API calls depend on from `types.js` to
    `apiTypes.js`. All API types are already exported from `types.js`.

--- src/types.js
+++ src/types.js
@@ -3,6 +3,17 @@ import type { Dispatch as ReduxDispatch } from 'redux';
 import type { IntlShape } from 'react-intl';
 import type { InputSelector } from 'reselect';
 
+import type {
+  Auth,
+  Topic,
+  Message,
+  RealmEmojiState,
+  RealmFilter,
+  Narrow,
+  Stream,
+  Subscription,
+  User,
+} from './api/apiTypes';
 import type { AppStyles } from './styles/theme';
 
 export type { ChildrenArray } from 'react';
@@ -37,29 +48,12 @@ export type ObjectsMappedById = {
   [key: number]: ObjectWithId,
 };
 
-export type Auth = {
-  realm: string,
-  apiKey: string,
-  email: string,
-};
-
 export type InputSelectionType = {
   start: number,
   end: number,
 };
 
 export type Account = Auth;
-
-/**
- * An emoji reaction to a message, in minimal form.
- *
- * See also EventReaction, which carries additional properties computed from
- * these.
- */
-export type SlimEventReaction = {
-  emoji_name: string,
-  user: any,
-};
 
 /**
  * An emoji reaction to a message.
@@ -92,105 +86,8 @@ export type PmRecipientUser = {
   short_name: string,
 };
 
-export type MessageEdit = {
-  prev_content?: string,
-  prev_rendered_content?: string,
-  prev_rendered_content_version?: number,
-  prev_subject?: string,
-  timestamp: number,
-  user_id: number,
-};
-
-/**
- * A Zulip message.
- *
- * This type is mainly intended to represent the data the server sends as
- * the `message` property of an event of type `message`.  Caveat lector: we
- * pass these around to a lot of places, and do a lot of further munging, so
- * this type may not quite represent that.  Any differences should
- * definitely be commented, and perhaps refactored.
- *
- * The server's behavior here is undocumented and the source is very
- * complex; this is naturally a place where a large fraction of all the
- * features of Zulip have to appear.
- *
- * References include:
- *  * the two example events at https://zulipchat.com/api/get-events-from-queue
- *  * `process_message_event` in zerver/tornado/event_queue.py; the call
- *    `client.add_event(user_event)` makes the final determination of what
- *    goes into the event, so `message_dict` is the final value of `message`
- *  * `MessageDict.wide_dict` and its helpers in zerver/lib/message.py;
- *    via `do_send_messages` in `zerver/lib/actions.py`, these supply most
- *    of the data ultimately handled by `process_message_event`
- *  * the `Message` and `AbstractMessage` models in zerver/models.py, but
- *    with caution; many fields are adjusted between the DB row and the event
- *  * empirical study looking at Redux events logged [to the
- *    console](docs/howto/debugging.md).
- *
- * See also `Outbox`.
- */
-export type Message = {
-  /** Our own flag; if true, really type `Outbox`. */
-  isOutbox: false,
-
-  /**
-   * These don't appear in `message` events, but they appear in GET
-   * requests for messages in a narrow when a search is involved.
-   */
-  match_content?: string,
-  match_subject?: string,
-
-  /** Obsolete? Gone in server commit 1.6.0~1758 . */
-  sender_domain: string,
-
-  /** The rest are believed to really appear in `message` events. */
-  avatar_url: ?string,
-  client: string,
-  content: string,
-  content_type: 'text/html' | 'text/markdown',
-  display_recipient: $FlowFixMe, // `string` for type stream, else PmRecipientUser[].
-  edit_history: MessageEdit[],
-  flags: string[],
-  gravatar_hash: string,
-  id: number,
-  is_me_message: boolean,
-  last_edit_timestamp?: number,
-  reactions: SlimEventReaction[],
-  recipient_id: number,
-  sender_email: string,
-  sender_full_name: string,
-  sender_id: number,
-  sender_realm_str: string,
-  sender_short_name: string,
-  stream_id: number, // FixMe: actually only for type `stream`, else absent.
-  subject: string,
-  subject_links: string[],
-  submessages: Message[],
-  timestamp: number,
-  type: 'stream' | 'private',
-};
-
 export type NarrowsState = {
   [narrow: string]: Message[],
-};
-
-/**
- * A Zulip user.
- *
- * For details on the properties, see the Zulip API docs:
- *   https://zulipchat.com/api/get-all-users#response
- */
-export type User = {
-  avatar_url: ?string,
-  bot_type?: number,
-  bot_owner?: string,
-  email: string,
-  full_name: string,
-  is_admin: boolean,
-  is_bot: boolean,
-  profile_data?: Object,
-  timezone: string,
-  user_id: number,
 };
 
 export type UserIdMap = {
@@ -202,11 +99,6 @@ export type UserGroup = {
   id: number,
   members: number[],
   name: string,
-};
-
-export type DevUser = {
-  realm_uri: string,
-  email: string,
 };
 
 /** See ClientPresence, and the doc linked there. */
@@ -284,28 +176,7 @@ export type Fetching = {
   newer: boolean,
 };
 
-export type Stream = {
-  stream_id: number,
-  description: string,
-  name: string,
-  invite_only: boolean,
-  is_announcement_only: boolean,
-};
-
 export type StreamsState = Stream[];
-
-export type Subscription = Stream & {
-  color: string,
-  in_home_view: boolean,
-  pin_to_top: boolean,
-  audible_notifications: boolean,
-  desktop_notifications: boolean,
-  email_address: string,
-  push_notifications: boolean,
-  is_old_stream: boolean,
-  push_notifications: boolean,
-  stream_weekly_traffic: number,
-};
 
 export type SubscriptionsState = Subscription[];
 
@@ -335,24 +206,6 @@ export type UpdateMessageFlagsEvent = {
   messages: number[],
   operation: 'add' | '???',
 };
-
-export type NarrowOperator =
-  | 'is'
-  | 'in'
-  | 'near'
-  | 'id'
-  | 'stream'
-  | 'topic'
-  | 'sender'
-  | 'pm-with'
-  | 'search';
-
-export type NarrowElement = {
-  operand: string,
-  operator?: NarrowOperator, // TODO type: this shouldn't be absent.
-};
-
-export type Narrow = NarrowElement[];
 
 export type EditMessage = {
   id: number,
@@ -425,27 +278,6 @@ export type NavigationState = {
   }>, */,
 };
 
-export type RealmFilter = [string, string, number];
-
-export type RealmEmojiType = {
-  author: {
-    email: string,
-    full_name: string,
-    id: number,
-  },
-  deactivated: boolean,
-  id: number,
-  name: string,
-  source_url: string,
-  // This prevents accidentally using this type as a map.
-  // See https://github.com/facebook/flow/issues/4257#issuecomment-321951793
-  [any]: mixed,
-};
-
-export type RealmEmojiState = {
-  [id: string]: RealmEmojiType,
-};
-
 export type RealmBot = {
   email: string,
   full_name: string,
@@ -482,11 +314,6 @@ export type RealmState = {
   filters: RealmFilter[],
   emoji: RealmEmojiState,
   isAdmin: boolean,
-};
-
-export type Topic = {
-  name: string,
-  max_id: number,
 };
 
 export type TopicExtended = Topic & {
@@ -747,8 +574,6 @@ export type NamedUser = {
   email: string,
   full_name: string,
 };
-
-export type ResponseExtractionFunc = any;
 
 export type TabNavigationOptionsPropsType = {
   isFocussed: boolean,
