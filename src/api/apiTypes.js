060a7762e6883165c3999566068409b9ce61fe01
Boris Yankov <borisyankov@gmail.com>
2018-07-18 22:00:36 +0300

    api: Add type for server error response.
    The response often contains a program-readable `code` constant,
    but not always.
    
    In addition to `code`, `msg`, and `result`, the response may include
    different additional values for certain errors.  We might decide to
    define them in future.

--- src/api/apiTypes.js
+++ src/api/apiTypes.js
@@ -190,6 +190,30 @@ export type ApiResponse = {
   msg: string,
 };
 
+/** List of error codes at https://github.com/zulip/zulip/blob/master/zerver/lib/exceptions.py */
+
+export type ApiErrorCode =
+  | 'BAD_REQUEST'
+  | 'REQUEST_VARIABLE_MISSING'
+  | 'REQUEST_VARIABLE_INVALID'
+  | 'BAD_IMAGE'
+  | 'REALM_UPLOAD_QUOTA'
+  | 'BAD_NARROW'
+  | 'MISSING_HTTP_EVENT_HEADER'
+  | 'STREAM_DOES_NOT_EXIST'
+  | 'UNAUTHORIZED_PRINCIPAL'
+  | 'UNEXPECTED_WEBHOOK_EVENT_TYPE'
+  | 'BAD_EVENT_QUEUE_ID'
+  | 'CSRF_FAILED'
+  | 'INVITATION_FAILED'
+  | 'INVALID_ZULIP_SERVER';
+
+export type ApiResponseError = {
+  code?: ApiErrorCode,
+  msg: string,
+  result: 'error',
+};
+
 export type ResponseExtractionFunc = (response: Object) => any;
 
 export type ApiResponseWithPresence = ApiResponse & {
