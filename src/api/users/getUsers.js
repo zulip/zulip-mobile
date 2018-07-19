0c5926fc36f839169e5ce4bade928f1808baee49
Boris Yankov <borisyankov@gmail.com>
2018-07-19 10:12:15 +0300

    api: Make `getUsers` return full response
    Note: We are not using this API function at the moment (we do the
    request with the `register` call) so the different return type does
    not require code changes anywhere else.

--- src/api/users/getUsers.js
+++ src/api/users/getUsers.js
@@ -1,7 +1,12 @@
 /* @flow strict-local */
-import type { Auth, User } from '../apiTypes';
+import type { Auth, ApiResponseSuccess, User } from '../apiTypes';
 import { apiGet } from '../apiFetch';
 
+type ApiResponseUsers = {|
+  ...ApiResponseSuccess,
+  members: User[],
+|};
+
 /** See https://zulipchat.com/api/get-all-users */
-export default (auth: Auth): Promise<User[]> =>
-  apiGet(auth, 'users', res => res.members, { client_gravatar: true });
+export default (auth: Auth): Promise<ApiResponseUsers> =>
+  apiGet(auth, 'users', res => res, { client_gravatar: true });
