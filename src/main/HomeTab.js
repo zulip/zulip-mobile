443755659a3a0baed40921a134df87746942bbfc
Aswin G <aswinganesh666@gmail.com>
2018-07-21 00:58:09 +0530

    HomeTab: Remove top navigation buttons
    These buttons have been moved to the sidebar as part of the drawer navigator.

--- src/main/HomeTab.js
+++ src/main/HomeTab.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import type { Dispatch } from '../types';
 import { homeNarrow, specialNarrow } from '../utils/narrow';
 import NavButton from '../nav/NavButton';
 import UnreadCards from '../unread/UnreadCards';
@@ -15,52 +14,14 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  iconList: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-  },
 });
 
-type Props = {
-  dispatch: Dispatch,
-};
-
-class HomeTab extends PureComponent<Props> {
+export default class HomeTab extends PureComponent<Props> {
   render() {
-    const { dispatch } = this.props;
-
     return (
       <View style={styles.wrapper}>
-        <View style={styles.iconList}>
-          <NavButton
-            name="home"
-            onPress={() => {
-              dispatch(doNarrow(homeNarrow));
-            }}
-          />
-          <NavButton
-            name="star"
-            onPress={() => {
-              dispatch(doNarrow(specialNarrow('starred')));
-            }}
-          />
-          <NavButton
-            name="at-sign"
-            onPress={() => {
-              dispatch(doNarrow(specialNarrow('mentioned')));
-            }}
-          />
-          <NavButton
-            name="search"
-            onPress={() => {
-              dispatch(navigateToSearch());
-            }}
-          />
-        </View>
         <UnreadCards />
       </View>
     );
   }
 }
-
-export default connect()(HomeTab);
