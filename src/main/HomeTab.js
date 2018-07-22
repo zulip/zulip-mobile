efba35fbc243a120609766b8c898f91215854dc6
Aswin G <aswinganesh666@gmail.com>
2018-07-22 18:53:25 +0530

    HomeTab: Create a navbar on top
    The navbar has buttons to open the side drawer and retains the search function of the original navbar.

--- src/main/HomeTab.js
+++ src/main/HomeTab.js
@@ -4,24 +4,62 @@ import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import { homeNarrow, specialNarrow } from '../utils/narrow';
+import type { Context, Dispatch } from '../types';
 import NavButton from '../nav/NavButton';
+import { Label } from '../common';
 import UnreadCards from '../unread/UnreadCards';
-import { doNarrow, navigateToSearch } from '../actions';
+import { navigateToSearch } from '../actions';
 
-const styles = StyleSheet.create({
+const componentStyles = StyleSheet.create({
   wrapper: {
     flex: 1,
     flexDirection: 'column',
   },
 });
 
-export default class HomeTab extends PureComponent<Props> {
+type Props = {
+  dispatch: Dispatch,
+  navigation: Object,
+};
+
+class HomeTab extends PureComponent<Props> {
+  context: Context;
+  props: Props;
+
+  static contextTypes = {
+    styles: () => null,
+  };
+
   render() {
+    const { styles } = this.context;
+    const { dispatch, navigation } = this.props;
+
     return (
-      <View style={styles.wrapper}>
+      <View style={componentStyles.wrapper}>
+        <View style={styles.navBar}>
+          <NavButton
+            name="menu"
+            onPress={() => {
+              navigation.navigate('DrawerOpen');
+            }}
+          />
+          <Label
+            text="Zulip"
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={[styles.flexed, styles.navTitle]}
+          />
+          <NavButton
+            name="search"
+            onPress={() => {
+              dispatch(navigateToSearch());
+            }}
+          />
+        </View>
         <UnreadCards />
       </View>
     );
   }
 }
+
+export default connect()(HomeTab);
