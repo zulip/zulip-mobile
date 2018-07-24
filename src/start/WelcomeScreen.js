b0e2e9b79f3c8286314e80c695ea1eb6972476cf
Aswin G <aswinganesh666@gmail.com>
2018-07-24 18:32:49 +0530

    WelcomeScreen: Improve aesthetics
    Replace the screen with a simple view that has no navBar. Display
    "Welcome to Zulip" and the Zulip logo prominently.

--- src/start/WelcomeScreen.js
+++ src/start/WelcomeScreen.js
@@ -2,10 +2,23 @@
 import { connect } from 'react-redux';
 
 import React, { PureComponent } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import type { Dispatch } from '../types';
-import { Screen, ViewPlaceholder, ZulipButton } from '../common';
+import type { Context, Dispatch } from '../types';
+import { ViewPlaceholder, ZulipButton, Label, Logo } from '../common';
 import { navigateToAddNewAccount, navigateToWelcomeHelp } from '../actions';
+
+const componentStyles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    paddingVertical: 16,
+  },
+  sectionWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 type Props = {
   dispatch: Dispatch,
@@ -13,25 +26,38 @@ type Props = {
 
 class WelcomeScreen extends PureComponent<Props> {
   props: Props;
+  context: Context;
+
+  static contextTypes = {
+    styles: () => null,
+  };
 
   render() {
     const { dispatch } = this.props;
+    const { styles } = this.context;
+
     return (
-      <Screen title="Welcome!" centerContent padding>
-        <ZulipButton
-          text="I have a Zulip account"
-          onPress={() => {
-            dispatch(navigateToAddNewAccount(''));
-          }}
-        />
-        <ViewPlaceholder height={20} />
-        <ZulipButton
-          text="I am new to Zulip"
-          onPress={() => {
-            dispatch(navigateToWelcomeHelp());
-          }}
-        />
-      </Screen>
+      <View style={[styles.screen, styles.padding]}>
+        <View style={componentStyles.sectionWrapper}>
+          <Logo />
+          <Label text="Welcome to Zulip!" style={componentStyles.title} />
+        </View>
+        <View style={componentStyles.sectionWrapper}>
+          <ZulipButton
+            text="I have a Zulip account"
+            onPress={() => {
+              dispatch(navigateToAddNewAccount(''));
+            }}
+          />
+          <ViewPlaceholder height={20} />
+          <ZulipButton
+            text="I am new to Zulip"
+            onPress={() => {
+              dispatch(navigateToWelcomeHelp());
+            }}
+          />
+        </View>
+      </View>
     );
   }
 }
