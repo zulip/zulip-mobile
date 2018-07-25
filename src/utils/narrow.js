8f7afd38039ee7792f2bf20f588bf47502874286
Jack Zhang <15jzhang@gmail.com>
2018-07-24 19:44:27 -0700

    Update message state on EVENT_UPDATE_MESSAGE_FLAGS.
    Prior to this commit, dispatching the EVENT_UPDATE_MESSAGE_FLAGS
    action only updated the FlagsState, did not correspondingly update
    the flags field(s) in MessagesState.
    
    Fix #2676 by linearly searching through all messages in
    MessagesState, and adding or removing the action payload's flag
    from the flags field(s).
    
    Fix #2677 by adding, in chronological order, any newly starred
    messages to the 'is:starred' narrow or removing any newly
    unstarred messages to from the 'is:starred' narrow.
    
    Resolves #2627.

--- src/utils/narrow.js
+++ src/utils/narrow.js
@@ -157,5 +157,6 @@ export const isSameNarrow = (narrow1: Narrow, narrow2: Narrow): boolean =>
 export const parseNarrowString = (narrowStr: string): Narrow => JSON.parse(unescape(narrowStr));
 
 export const STARRED_NARROW = specialNarrow('starred');
+export const STARRED_NARROW_STR = JSON.stringify(STARRED_NARROW);
 
 export const MENTIONED_NARROW = specialNarrow('mentioned');
