59a71e86f1af3204c7af601e7f493885e677c6de
Aswin G <aswinganesh666@gmail.com>
2018-08-02 21:30:21 +0530

    SearchMessagesCard: Use new emoji selector
    Replace getAllRealmEmoji with getAllRealmEmojiByName. The code
    was already attempting to select the emoji by name, but the previous
    selector returned emoji by code, causing it to break.
    
    Also rename object 'realmEmoji' to 'allRealmEmojiByName'.

--- src/search/SearchMessagesCard.js
+++ src/search/SearchMessagesCard.js
@@ -13,8 +13,7 @@ import MessageList from '../message/MessageList';
 import { getMessages } from '../api';
 import renderMessages from '../message/renderMessages';
 import { NULL_ARRAY, NULL_FETCHING } from '../nullObjects';
-import { getAllRealmEmojiById, getAuth, getSubscriptions } from '../selectors';
-import { LAST_MESSAGE_ANCHOR } from '../constants';
+import { getAllRealmEmojiByName, getAuth, getSubscriptions } from '../selectors';
 
 const styles = StyleSheet.create({
   results: {
@@ -104,5 +103,11 @@ class SearchMessagesCard extends PureComponent<Props, State> {
 export default connect((state: GlobalState) => ({
   auth: getAuth(state),
   subscriptions: getSubscriptions(state),
+<<<<<<< HEAD
   realmEmoji: getAllRealmEmojiById(state),
+||||||| merged common ancestors
+  realmEmoji: getAllRealmEmoji(state),
+=======
+  allRealmEmojiByName: getAllRealmEmojiByName(state),
+>>>>>>> SearchMessagesCard: Use new emoji selector
 }))(SearchMessagesCard);
