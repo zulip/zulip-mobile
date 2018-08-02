ddc187f267cfdc3788293bc727562bcd96654e2f
Boris Yankov <borisyankov@gmail.com>
2018-08-02 22:52:46 +0300

    flow: Provide return type for `getFilteredEmojiList`
    With latest versions of Flow, providing this is mandatory.

--- src/emoji/getFilteredEmojiList.js
+++ src/emoji/getFilteredEmojiList.js
@@ -2,7 +2,7 @@
 import emojiMap from './emojiMap';
 import type { RealmEmojiType } from '../types';
 
-export default (query: string, realmEmoji: { [string]: RealmEmojiType }) =>
+export default (query: string, realmEmoji: { [string]: RealmEmojiType }): string[] =>
   // TODO: this doesn't actually handle realm emoji.  See our issue #2846.
   Array.from(
     new Set([
