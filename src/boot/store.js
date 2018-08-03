d4efb0b049c65f6cc8990754125f39b41a6de657
Greg Price <greg@zulipchat.com>
2018-08-02 22:52:38 -0700

    store: Reorder code to put "config" code at top.
    The config part here is going to get longer, particularly with the
    redux-persist v5 migration; so consolidate all the non-"config" code
    at the end so that it doesn't get spread widely apart as that happens.

--- src/boot/store.js
+++ src/boot/store.js
@@ -10,17 +10,17 @@ import ZulipAsyncStorage from './ZulipAsyncStorage';
 
 // AsyncStorage.clear(); // use to reset storage during development
 
-const store = createStore(
-  rootReducer,
-  undefined,
-  compose(applyMiddleware(...middleware), autoRehydrate()),
-);
-
 const reduxPersistConfig: Config = {
   whitelist: [...config.storeKeys, ...config.cacheKeys],
   // $FlowFixMe: https://github.com/rt2zz/redux-persist/issues/823
   storage: ZulipAsyncStorage,
 };
+
+const store = createStore(
+  rootReducer,
+  undefined,
+  compose(applyMiddleware(...middleware), autoRehydrate()),
+);
 
 export const restore = (onFinished?: () => void) =>
   persistStore(store, reduxPersistConfig, onFinished);
