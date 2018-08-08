2751309be9d600f09a4cb7fa9458654fbae7d5f7
Greg Price <greg@zulipchat.com>
2018-08-07 17:33:34 -0700

    emoji: Clarify what these "key"s are.
    These are keys, yes -- in particular, they're IDs of these realm
    emoji.  The latter is more informative.

--- src/emoji/emojiSelectors.js
+++ src/emoji/emojiSelectors.js
@@ -5,17 +5,17 @@ import { getAuth } from '../account/accountSelectors';
 import { getFullUrl } from '../utils/url';
 
 export const getAllRealmEmojiById = createSelector(getAuth, getRawRealmEmoji, (auth, emojis) =>
-  Object.keys(emojis).reduce((result, key) => {
-    result[key] = { ...emojis[key], source_url: getFullUrl(emojis[key].source_url, auth.realm) };
+  Object.keys(emojis).reduce((result, id) => {
+    result[id] = { ...emojis[id], source_url: getFullUrl(emojis[id].source_url, auth.realm) };
     return result;
   }, {}),
 );
 
 export const getActiveRealmEmojiById = createSelector(getAllRealmEmojiById, emojis =>
   Object.keys(emojis)
-    .filter(emoji => !emojis[emoji].deactivated)
-    .reduce((result, key) => {
-      result[key] = emojis[key];
+    .filter(id => !emojis[id].deactivated)
+    .reduce((result, id) => {
+      result[id] = emojis[id];
       return result;
     }, {}),
 );
