25c6c9bdd3183fb5685b7486e3517e7aba85993d
Greg Price <greg@zulipchat.com>
2018-08-07 17:23:11 -0700

    emoji: Fix misleading variable name `list` for non-lists.
    The `Array#reduce` method basically operates on a list... but
    these variables are referring to essentially the opposite thing!
    The array (or "list") acts as input, but this variable / this
    parameter to the inner function is the *output* we're in the
    middle of building.
    
    That makes this misnaming super confusing if you don't have the
    specifics of the `Array#reduce` signature solid in your head;
    and in any event, it's just wrong.  Fix it.

--- src/emoji/emojiSelectors.js
+++ src/emoji/emojiSelectors.js
@@ -5,17 +5,17 @@ import { getAuth } from '../account/accountSelectors';
 import { getFullUrl } from '../utils/url';
 
 export const getAllRealmEmojiById = createSelector(getAuth, getRawRealmEmoji, (auth, emojis) =>
-  Object.keys(emojis).reduce((list, key) => {
-    list[key] = { ...emojis[key], source_url: getFullUrl(emojis[key].source_url, auth.realm) };
-    return list;
+  Object.keys(emojis).reduce((result, key) => {
+    result[key] = { ...emojis[key], source_url: getFullUrl(emojis[key].source_url, auth.realm) };
+    return result;
   }, {}),
 );
 
 export const getActiveRealmEmojiById = createSelector(getAllRealmEmojiById, emojis =>
   Object.keys(emojis)
     .filter(emoji => !emojis[emoji].deactivated)
-    .reduce((list, key) => {
-      list[key] = emojis[key];
-      return list;
+    .reduce((result, key) => {
+      result[key] = emojis[key];
+      return result;
     }, {}),
 );
