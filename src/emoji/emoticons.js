f236aaff69b5974de1d4007a38cd4cd7ee244ca0
Greg Price <greg@zulipchat.com>
2018-08-14 16:05:10 -0700

    emoji: Cut disused emoticon-rewriting code.
    In 3da95e567 a few months ago, we removed all uses of this code
    because the same functionality now exists on the server, where
    it's uniform between devices and is configurable by the user.
    So, delete the actual code too.

--- src/emoji/emoticons.js
+++
@@ -1,5 +0,0 @@
-/* @flow */
-import emoticonMap from './emoticonMap';
-
-export const replaceEmoticonsWithEmoji = (text: string): string =>
-  text.replace(/[:;)(pP<3'-\\_xoO0cb|{‑%/]+/g, word => emoticonMap[word] || word);
