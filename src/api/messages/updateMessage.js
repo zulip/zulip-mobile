9758714a203e52a6a4279564c052d76d42182d25
Boris Yankov <borisyankov@gmail.com>
2018-08-15 20:18:11 +0300

    api: Remove unneeded `removeEmptyValues` from `updateMessage`
    Since we now call `removeEmptyValues` on all API calls, there is
    no need to explicitly call it for `updateMessage`.

--- src/api/messages/updateMessage.js
+++ src/api/messages/updateMessage.js
@@ -1,7 +1,6 @@
 /* @flow */
 import type { ApiResponse, Auth } from '../../types';
 import { apiPatch } from '../apiFetch';
-import { removeEmptyValues } from '../../utils/misc';
 
 export default async (auth: Auth, content: Object, id: number): Promise<ApiResponse> =>
-  apiPatch(auth, `messages/${id}`, res => res, removeEmptyValues(content));
+  apiPatch(auth, `messages/${id}`, res => res, content);
