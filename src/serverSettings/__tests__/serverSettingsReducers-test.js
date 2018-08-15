e11a01da340d5be5ce7323445124ec98c34c764f
Boris Yankov <borisyankov@gmail.com>
2018-08-15 18:05:54 +0300

    Update serverSettings with REALM_INIT data
    When the server responds to a `register` API call it includes a lot
    of the data that we have from the `serverSettings` call (but not all).
    Thus we get and update whatever props we are provided.

--- src/serverSettings/__tests__/serverSettingsReducers-test.js
+++ src/serverSettings/__tests__/serverSettingsReducers-test.js
@@ -1,6 +1,6 @@
 import deepFreeze from 'deep-freeze';
 
-import { NEW_SERVER_SETTINGS } from '../../actionConstants';
+import { NEW_SERVER_SETTINGS, REALM_INIT } from '../../actionConstants';
 import serverSettingsReducers from '../serverSettingsReducers';
 
 describe('serverSettingsReducers', () => {
@@ -44,6 +44,57 @@ describe('serverSettingsReducers', () => {
         'https://example.com': {
           realm_uri: 'https://example.com',
           realm_name: 'New name',
+        },
+      };
+
+      const newState = serverSettingsReducers(prevState, action);
+
+      expect(newState).toEqual(expectedState);
+    });
+  });
+
+  describe('REALM_INIT', () => {
+    test('if no entry exists creates and fills up with known data', () => {
+      const prevState = deepFreeze({});
+      const action = {
+        type: REALM_INIT,
+        data: {
+          realm_uri: 'https://example.com',
+          realm_name: 'Example',
+        },
+      };
+      const expectedState = {
+        'https://example.com': {
+          realm_uri: 'https://example.com',
+          realm_name: 'Example',
+        },
+      };
+
+      const newState = serverSettingsReducers(prevState, action);
+
+      expect(newState).toEqual(expectedState);
+    });
+
+    test('updates already stored data', () => {
+      const prevState = deepFreeze({
+        'https://example.com': {
+          realm_name: 'Old name',
+        },
+      });
+      const action = {
+        type: REALM_INIT,
+        data: {
+          realm_uri: 'https://example.com',
+          realm_name: 'New name',
+          realm_icon_url: 'https://example.com/icon.png',
+          extra_data: 'Not in server settings',
+        },
+      };
+      const expectedState = {
+        'https://example.com': {
+          realm_uri: 'https://example.com',
+          realm_name: 'New name',
+          realm_icon: 'https://example.com/icon.png',
         },
       };
 
--- src/serverSettings/serverSettingsReducers.js
+++ src/serverSettings/serverSettingsReducers.js
@@ -1,7 +1,12 @@
 /* @flow */
-import type { NewServerSettingsAction, ServerSettingsState, ServerSettingsAction } from '../types';
+import type {
+  NewServerSettingsAction,
+  RealmInitAction,
+  ServerSettingsState,
+  ServerSettingsAction,
+} from '../types';
 import { NULL_OBJECT } from '../nullObjects';
-import { NEW_SERVER_SETTINGS } from '../actionConstants';
+import { NEW_SERVER_SETTINGS, REALM_INIT } from '../actionConstants';
 
 const initialState: ServerSettingsState = NULL_OBJECT;
 
@@ -13,6 +18,18 @@ const newServerSettings = (
   [action.serverSettings.realm_uri]: action.serverSettings,
 });
 
+const realmInit = (state: ServerSettingsState, action: RealmInitAction): ServerSettingsState => ({
+  ...state,
+  [action.data.realm_uri]: {
+    ...state[action.data.realm_uri],
+    authentication_methods: action.data.realm_authentication_methods,
+    realm_description: action.data.realm_description,
+    realm_icon: action.data.realm_icon_url,
+    realm_name: action.data.realm_name,
+    realm_uri: action.data.realm_uri,
+  },
+});
+
 export default (
   state: ServerSettingsState = initialState,
   action: ServerSettingsAction,
@@ -20,6 +37,9 @@ export default (
   switch (action.type) {
     case NEW_SERVER_SETTINGS:
       return newServerSettings(state, action);
+
+    case REALM_INIT:
+      return realmInit(state, action);
 
     default:
       return state;
