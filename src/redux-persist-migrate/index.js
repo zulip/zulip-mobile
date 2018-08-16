401fc87359e136c197f1d58990bfc727d169406e
Robert Hönig <robhoenig@gmail.com>
2018-08-16 09:41:15 +0200

    persist: Format redux-persist-migrate with eslint and prettier.
    Also replace a use of `console.error` with our `logErrorRemotely`.

--- src/redux-persist-migrate/index.js
+++ src/redux-persist-migrate/index.js
@@ -1,58 +1,72 @@
-import { REHYDRATE } from 'redux-persist/constants'
+import { REHYDRATE } from 'redux-persist/constants';
 
-const processKey = (key) => {
-  let int = parseInt(key)
-  if (isNaN(int)) throw new Error('redux-persist-migrate: migrations must be keyed with integer values')
-  return int
-}
+import { logErrorRemotely } from '../utils/logging';
 
-export default function createMigration (manifest, versionSelector, versionSetter) {
+const processKey = key => {
+  const int = parseInt(key, 10);
+  if (Number.isNaN(int)) {
+    throw new Error('redux-persist-migrate: migrations must be keyed with integer values');
+  }
+  return int;
+};
+
+export default function createMigration(manifest, versionSelector, versionSetter) {
   if (typeof versionSelector === 'string') {
-    let reducerKey = versionSelector
-    versionSelector = (state) => state && state[reducerKey] && state[reducerKey].version
+    const reducerKey = versionSelector;
+    versionSelector = state => state && state[reducerKey] && state[reducerKey].version;
     versionSetter = (state, version) => {
       if (['undefined', 'object'].indexOf(typeof state[reducerKey]) === -1) {
-        console.error('redux-persist-migrate: state for versionSetter key must be an object or undefined')
-        return state
+        logErrorRemotely(
+          new Error(
+            'redux-persist-migrate: state for versionSetter key must be an object or undefined',
+          ),
+        );
+        return state;
       }
-      state[reducerKey] = state[reducerKey] || {}
-      state[reducerKey].version = version
-      return state
-    }
+      state[reducerKey] = state[reducerKey] || {};
+      state[reducerKey].version = version;
+      return state;
+    };
   }
 
-  const versionKeys = Object.keys(manifest).map(processKey).sort((a, b) => a - b)
-  let currentVersion = versionKeys[versionKeys.length - 1]
-  if (!currentVersion && currentVersion !== 0) currentVersion = -1
-
-  const migrationDispatch = (next) => (action) => {
-    if (action.type === REHYDRATE) {
-      const incomingState = action.payload
-      let incomingVersion = parseInt(versionSelector(incomingState))
-      if (isNaN(incomingVersion)) incomingVersion = null
-
-      if (incomingVersion !== currentVersion) {
-        const migratedState = migrate(incomingState, incomingVersion)
-        action.payload = migratedState
-      }
-    }
-    return next(action)
+  const versionKeys = Object.keys(manifest)
+    .map(processKey)
+    .sort((a, b) => a - b);
+  let currentVersion = versionKeys[versionKeys.length - 1];
+  if (!currentVersion && currentVersion !== 0) {
+    currentVersion = -1;
   }
 
   const migrate = (state, version) => {
-    versionKeys
-      .filter((v) => v > version || version === null)
-      .forEach((v) => { state = manifest[v](state) })
+    versionKeys.filter(v => v > version || version === null).forEach(v => {
+      state = manifest[v](state);
+    });
 
-    state = versionSetter(state, currentVersion)
-    return state
-  }
+    state = versionSetter(state, currentVersion);
+    return state;
+  };
 
-  return (next) => (reducer, initialState, enhancer) => {
-    var store = next(reducer, initialState, enhancer)
+  const migrationDispatch = next => action => {
+    if (action.type === REHYDRATE) {
+      const incomingState = action.payload;
+      let incomingVersion = parseInt(versionSelector(incomingState), 10);
+      if (Number.isNaN(incomingVersion)) {
+        incomingVersion = null;
+      }
+
+      if (incomingVersion !== currentVersion) {
+        const migratedState = migrate(incomingState, incomingVersion);
+        action.payload = migratedState;
+      }
+    }
+    return next(action);
+  };
+
+  return next => (reducer, initialState, enhancer) => {
+    const store = next(reducer, initialState, enhancer);
     return {
       ...store,
-      dispatch: migrationDispatch(store.dispatch)
-    }
-  }
+      dispatch: migrationDispatch(store.dispatch),
+    };
+  };
 }
