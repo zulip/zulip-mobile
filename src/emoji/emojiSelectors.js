5825573d2d6f907df67ca44698cb5eecc689d4f6
Greg Price <greg@zulipchat.com>
2018-08-21 20:02:07 -0700

    emoji: Add type annotations to selectors.
    This fully covers this (small) file with types.

--- src/emoji/emojiSelectors.js
+++ src/emoji/emojiSelectors.js
@@ -1,21 +1,27 @@
 /* @flow */
 import { createSelector } from 'reselect';
+import type { Selector, RealmEmojiState } from '../types';
 import { getRawRealmEmoji } from '../directSelectors';
 import { getAuth } from '../account/accountSelectors';
 import { getFullUrl } from '../utils/url';
 
-export const getAllRealmEmojiById = createSelector(getAuth, getRawRealmEmoji, (auth, emojis) =>
-  Object.keys(emojis).reduce((result, id) => {
-    result[id] = { ...emojis[id], source_url: getFullUrl(emojis[id].source_url, auth.realm) };
-    return result;
-  }, {}),
-);
-
-export const getActiveRealmEmojiById = createSelector(getAllRealmEmojiById, emojis =>
-  Object.keys(emojis)
-    .filter(id => !emojis[id].deactivated)
-    .reduce((result, id) => {
-      result[id] = emojis[id];
+export const getAllRealmEmojiById: Selector<RealmEmojiState> = createSelector(
+  getAuth,
+  getRawRealmEmoji,
+  (auth, emojis) =>
+    Object.keys(emojis).reduce((result, id) => {
+      result[id] = { ...emojis[id], source_url: getFullUrl(emojis[id].source_url, auth.realm) };
       return result;
     }, {}),
+);
+
+export const getActiveRealmEmojiById: Selector<RealmEmojiState> = createSelector(
+  getAllRealmEmojiById,
+  emojis =>
+    Object.keys(emojis)
+      .filter(id => !emojis[id].deactivated)
+      .reduce((result, id) => {
+        result[id] = emojis[id];
+        return result;
+      }, {}),
 );
