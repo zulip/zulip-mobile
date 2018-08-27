75240efbf8d0c347f71acc5405ff7250b2324575
Boris Yankov <borisyankov@gmail.com>
2018-08-27 21:00:37 +0300

    drafts: Factor "add or remove draft" logic out of ComposeBox.
    Add the code from the `tryUpdateDraft` function in ComposeBox as a
    separate redux action.  We don't yet actually replace the use of
    that function with this one; that'll be a subsequent commit.
    
    Does not include the `if (draft.trim() === message.trim())` line
    intentionally, as this is not a behavior we want (and may even
    be unintentional reading at the commit that introduced the code).

--- src/drafts/draftsActions.js
+++ src/drafts/draftsActions.js
@@ -1,5 +1,5 @@
 /* @flow */
-import type { Narrow, DraftAddAction, DraftRemoveAction } from '../types';
+import type { GetState, Narrow, DraftAddAction, DraftRemoveAction } from '../types';
 import { DRAFT_ADD, DRAFT_REMOVE } from '../actionConstants';
 
 export const draftAdd = (narrow: Narrow, content: string): DraftAddAction => ({
@@ -12,3 +12,14 @@ export const draftRemove = (narrow: Narrow): DraftRemoveAction => ({
   type: DRAFT_REMOVE,
   narrow,
 });
+
+export const updateDraft = (narrow: Narrow, message: string) => (
+  dispatch: Dispatch,
+  getState: GetState,
+) => {
+  if (message.trim().length === 0) {
+    dispatch(draftRemove(narrow));
+  } else {
+    dispatch(draftAdd(narrow, message));
+  }
+};
