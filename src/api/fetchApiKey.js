a21e8ab28281aa9260a64a6a97a989c4cd53c14c
Boris Yankov <borisyankov@gmail.com>
2018-09-01 00:42:35 +0300

    api: Add type for `fetchApiKey` response

--- src/api/fetchApiKey.js
+++ src/api/fetchApiKey.js
@@ -1,6 +1,11 @@
 /* @flow */
-import type { Auth } from './apiTypes';
+import type { Auth, ApiResponseSuccess } from './apiTypes';
 import { apiPost } from './apiFetch';
 
-export default (auth: Auth, email: string, password: string) =>
+type ApiResponseFetchApiKey = ApiResponseSuccess & {
+  email: string,
+  api_key: string,
+};
+
+export default (auth: Auth, email: string, password: string): Promise<ApiResponseFetchApiKey> =>
   apiPost(auth, 'fetch_api_key', res => res, { username: email, password });
