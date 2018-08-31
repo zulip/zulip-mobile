fe1ec0d234edc8bc093fe3fbce2aeb6a10b0c1b4
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-08-31 15:53:14 +0530

    flow: Add return type of methods in info module.

--- src/utils/info.js
+++ src/utils/info.js
@@ -2,14 +2,14 @@
 import { Alert } from 'react-native';
 import Toast from '@remobile/react-native-toast';
 
-export const showToast = (message: string) => {
+export const showToast = (message: string): void => {
   Toast.show(message, Toast.SHORT);
 };
 
-export const showErrorAlert = (message: string, title: string) =>
+export const showErrorAlert = (message: string, title: string): void =>
   Alert.alert(title, message, [{ text: 'OK', onPress: () => {} }], { cancelable: true });
 
-export const showErrorFromException = (error: Error, title: string) =>
+export const showErrorFromException = (error: Error, title: string): void =>
   // $FlowFixMe
   Alert.alert(title, error && error.data && error.data.msg, [{ text: 'OK', onPress: () => {} }], {
     cancelable: true,
