1acf7d96a1de5d151659f2cec0c05d767bd5c55e
Boris Yankov <borisyankov@gmail.com>
2018-09-01 20:57:09 +0300

    api: Add `deleteEventQueue` function
    Currently not used but we are trying to create a complete API
    accesing library that will likely get extracted from the project.

---
+++ src/api/deleteEventQueue.js
@@ -0,0 +1,9 @@
+/* @flow strict-local */
+import type { Auth, ApiResponseSuccess } from './apiTypes';
+import { apiDelete } from './apiFetch';
+
+/** See https://zulipchat.com/api/delete-queue */
+export default (auth: Auth, queueId: string): Promise<ApiResponseSuccess> =>
+  apiDelete(auth, 'events', res => res, {
+    queue_id: queueId,
+  });
