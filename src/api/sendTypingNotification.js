d05352d58e3699764bee012bd5e0800bb216cc88
Boris Yankov <borisyankov@gmail.com>
2018-09-01 17:13:35 +0300

    api: Remove unused sendTypingNotification.js .
    We already have the same API call in `api/typing.js`.
    Remove duplicate code.

--- src/api/sendTypingNotification.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-import type { Auth } from './apiTypes';
-import { apiPost } from './apiFetch';
-
-export default async (auth: Auth, to: string, op: 'start' | 'stop') =>
-  apiPost(auth, 'typing', res => res.messages, {
-    to,
-    op,
-  });
