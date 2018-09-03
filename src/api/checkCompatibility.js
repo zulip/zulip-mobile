7340eccbf0fc75aea40e52c11f4df16b8897597d
Boris Yankov <borisyankov@gmail.com>
2018-09-03 19:59:49 +0300

    api: Remove `config` dependency in `checkCompatibility`
    Doesn't make much sense to keep this constant in `config.js`.
    Hard-code the value, remove the dependency.

--- src/api/checkCompatibility.js
+++ src/api/checkCompatibility.js
@@ -1,11 +1,10 @@
 /* @flow */
-import config from '../config';
 import userAgent from '../utils/userAgent';
 
 // check with server if current mobile app is compatible with latest backend
 // compatibility fails only if server responds with 400 (but not with 200 or 404)
 export default () =>
-  fetch(config.compatibilityUrl, {
+  fetch('https://zulipchat.com/compatibility', {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
       'User-Agent': userAgent,
