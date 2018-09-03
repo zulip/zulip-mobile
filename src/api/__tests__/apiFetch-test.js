d887c13270bdf5cb923e60bbf843694aaab0b125
Boris Yankov <borisyankov@gmail.com>
2018-09-04 00:24:08 +0300

    api: Add tests for `getFetchParams`

---
+++ src/api/__tests__/apiFetch-test.js
@@ -0,0 +1,32 @@
+import { getFetchParams } from '../apiFetch';
+
+global.FormData = jest.fn();
+
+describe('getFetchParams', () => {
+  test('creates a `header` key with authorization data', () => {
+    const auth = {
+      email: 'john@example.com',
+      apiKey: 'some_key',
+    };
+    const params = {};
+
+    const actualResult = getFetchParams(auth, params);
+
+    expect(actualResult.headers).toBeTruthy();
+    expect(actualResult.headers.Authorization).toBeTruthy();
+  });
+
+  test('merges `headers` key and all given params into a new object', () => {
+    const auth = {
+      email: 'john@example.com',
+      apiKey: 'some_key',
+    };
+    const params = {
+      key: 'value',
+    };
+
+    const actualResult = getFetchParams(auth, params);
+
+    expect(actualResult.key).toBe('value');
+  });
+});
