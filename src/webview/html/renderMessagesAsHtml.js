d8d925b425cc5a6d10e8135646a9bad1b8e20b7f
Greg Price <greg@zulipchat.com>
2018-09-05 15:41:29 -0700

    webview render: Refactor to slightly tighten renderMessagesAsHtml.
    Since bb0523f18 a few weeks ago this wrapper at the end has been
    nearly empty.  Cut a layer of indirection by just squashing it
    with the thing it wraps.

--- src/webview/html/renderMessagesAsHtml.js
+++ src/webview/html/renderMessagesAsHtml.js
@@ -6,51 +6,51 @@ import messageHeaderAsHtml from './messageHeaderAsHtml';
 import timeRowAsHtml from './timeRowAsHtml';
 import { getGravatarFromEmail } from '../../utils/avatar';
 
-const renderMessages = ({
+export default ({
   auth,
   subscriptions,
   realmEmoji,
   renderedMessages,
   narrow,
   twentyFourHourTime,
-}: Props): string[] =>
-  renderedMessages.reduce((list, section, index) => {
-    list.push(
-      messageHeaderAsHtml({
-        auth,
-        item: section.message,
-        subscriptions,
-        narrow,
-      }),
-    );
+}: Props): string =>
+  renderedMessages
+    .reduce((list, section, index) => {
+      list.push(
+        messageHeaderAsHtml({
+          auth,
+          item: section.message,
+          subscriptions,
+          narrow,
+        }),
+      );
 
-    section.data.forEach((item, idx) => {
-      if (item.type === 'time') {
-        list.push(timeRowAsHtml(item.timestamp, item.firstMessage));
-      } else {
-        const { message } = item;
-        list.push(
-          messageAsHtml({
-            id: message.id,
-            isBrief: item.isBrief,
-            fromName: message.sender_full_name,
-            fromEmail: message.sender_email,
-            content: message.match_content || message.content,
-            flags: message.flags || [],
-            timestamp: message.timestamp,
-            avatarUrl: message.avatar_url || getGravatarFromEmail(message.sender_email),
-            timeEdited: message.last_edit_timestamp,
-            isOutbox: message.isOutbox,
-            reactions: message.reactions,
-            ownEmail: auth.email,
-            realmEmoji,
-            twentyFourHourTime,
-          }),
-        );
-      }
-    });
+      section.data.forEach((item, idx) => {
+        if (item.type === 'time') {
+          list.push(timeRowAsHtml(item.timestamp, item.firstMessage));
+        } else {
+          const { message } = item;
+          list.push(
+            messageAsHtml({
+              id: message.id,
+              isBrief: item.isBrief,
+              fromName: message.sender_full_name,
+              fromEmail: message.sender_email,
+              content: message.match_content || message.content,
+              flags: message.flags || [],
+              timestamp: message.timestamp,
+              avatarUrl: message.avatar_url || getGravatarFromEmail(message.sender_email),
+              timeEdited: message.last_edit_timestamp,
+              isOutbox: message.isOutbox,
+              reactions: message.reactions,
+              ownEmail: auth.email,
+              realmEmoji,
+              twentyFourHourTime,
+            }),
+          );
+        }
+      });
 
-    return list; // eslint-disable-next-line
-  }, []);
-
-export default (props: Props): string => renderMessages(props).join('');
+      return list; // eslint-disable-next-line
+    }, [])
+    .join('');
