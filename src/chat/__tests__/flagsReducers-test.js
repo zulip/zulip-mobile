37e26b1494f23a29706a19d737e49724093a2435
Boris Yankov <borisyankov@gmail.com>
2018-09-08 03:35:41 +0300

    state: Do not rehydrate 'flags' state
    Previous commit makes sure we store the `flags` state.
    
    This is better than trying to restore flags from `messages` as we
    do not mutate this data. So any state that was changed during the
    previous run of the app would not get restored properly.
    
    The simpler solution of storing the `flags` is far superior to this
    approach while not being a complete solution.

--- src/chat/__tests__/flagsReducers-test.js
+++ src/chat/__tests__/flagsReducers-test.js
@@ -2,7 +2,6 @@ import deepFreeze from 'deep-freeze';
 
 import flagsReducers from '../flagsReducers';
 import {
-  REHYDRATE,
   MESSAGE_FETCH_COMPLETE,
   EVENT_NEW_MESSAGE,
   EVENT_UPDATE_MESSAGE_FLAGS,
@@ -12,56 +11,6 @@ import {
 import { NULL_OBJECT } from '../../nullObjects';
 
 describe('flagsReducers', () => {
-  describe('REHYDRATE', () => {
-    test('handles no input data', () => {
-      const initialState = NULL_OBJECT;
-
-      const action = deepFreeze({
-        type: REHYDRATE,
-        payload: {},
-      });
-
-      const expectedState = {};
-
-      const actualState = flagsReducers(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
-    });
-
-    test('flags from all messages are extracted and stored by id', () => {
-      const initialState = NULL_OBJECT;
-
-      const action = deepFreeze({
-        type: REHYDRATE,
-        payload: {
-          accounts: [],
-          messages: {
-            1: { id: 1 },
-            2: { id: 2, flags: [] },
-            3: { id: 3, flags: ['read'] },
-            4: { id: 4, flags: ['starred'] },
-            5: { id: 5, flags: ['read', 'starred'] },
-          },
-        },
-      });
-
-      const expectedState = {
-        read: {
-          3: true,
-          5: true,
-        },
-        starred: {
-          4: true,
-          5: true,
-        },
-      };
-
-      const actualState = flagsReducers(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
-    });
-  });
-
   describe('MESSAGE_FETCH_COMPLETE', () => {
     test('flags from all messages are extracted and stored by id', () => {
       const initialState = NULL_OBJECT;
