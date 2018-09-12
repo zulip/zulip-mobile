76024be17a0ce7de6c7ceae084bc78ae739b39fe
Greg Price <greg@zulipchat.com>
2018-09-11 21:25:57 -0700

    null objects: Add comments explaining deprecation.

--- src/nullObjects.js
+++ src/nullObjects.js
@@ -16,12 +16,22 @@ export const NULL_OBJECT = Object.freeze({});
 
 export const NULL_ARRAY = Object.freeze([]);
 
+/*
+ * All the below objects are DEPRECATED; rather than using one, choose the
+ * appropriate fallback behavior explicitly.  See 25125db94 for more
+ * discussion, and 25125db94^..e22596c24 for a variety of examples of how.
+ *
+ * Further changes to eliminate the remaining uses of these would be great.
+ */
+
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_ACCOUNT: Account = {
   apiKey: '',
   email: '',
   realm: '',
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_USER: User = {
   avatar_url: '',
   email: '',
@@ -32,6 +42,7 @@ export const NULL_USER: User = {
   user_id: -1,
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_SUBSCRIPTION: Subscription = {
   audible_notifications: false,
   color: 'gray',
@@ -49,26 +60,31 @@ export const NULL_SUBSCRIPTION: Subscription = {
   is_announcement_only: false,
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_PRESENCE_AGGREGATED: PresenceAggregated = {
   client: '',
   status: 'offline',
   timestamp: 0,
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_PRESENCE: Presence = {
   aggregated: NULL_PRESENCE_AGGREGATED,
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_CAUGHTUP: CaughtUp = {
   older: false,
   newer: false,
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_FETCHING: Fetching = {
   older: false,
   newer: false,
 };
 
+/** DEPRECATED; don't add new uses.  See block comment above definition. */
 export const NULL_NAV_STATE: NavigationState = {
   index: -1,
   isTransitioning: false,
