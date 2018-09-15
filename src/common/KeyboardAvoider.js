2c52ef5c4283201813bd6292f45f20f9a0d46172
Boris Yankov <borisyankov@gmail.com>
2018-09-16 00:50:27 +0300

    KeyboardAvoider: Make more explicit for clearer types.
    Fixes two Flow errors (with Flow 0.75) that warn of unused props.
    For one platform we use `KeyboardAvoidingView` for the other
    we use a simple `View`. The latter does not have a `behavior`
    property, nor `contentContainerStyle`.

--- src/common/KeyboardAvoider.js
+++ src/common/KeyboardAvoider.js
@@ -1,4 +1,35 @@
 /* @flow */
+import React, { PureComponent } from 'react';
+import type { ChildrenArray } from 'react';
 import { KeyboardAvoidingView, Platform, View } from 'react-native';
 
-export default (Platform.OS === 'ios' ? KeyboardAvoidingView : View);
+import type { Style } from '../types';
+
+type Props = {
+  behavior?: ?('height' | 'position' | 'padding'),
+  children: ChildrenArray<*>,
+  style?: Style,
+  contentContainerStyle?: Style,
+};
+
+export default class KeyboardAvoider extends PureComponent<Props> {
+  props: Props;
+
+  render() {
+    const { behavior, children, style, contentContainerStyle } = this.props;
+
+    if (Platform.OS === 'android') {
+      return <View style={style}>{children}</View>;
+    }
+
+    return (
+      <KeyboardAvoidingView
+        behavior={behavior}
+        contentContainerStyle={contentContainerStyle}
+        style={style}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    );
+  }
+}
