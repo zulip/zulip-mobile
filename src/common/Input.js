d04d3e217e6cdbc7241d29ae311645ee1e13702d
Boris Yankov <borisyankov@gmail.com>
2018-09-16 01:47:12 +0300

    flow: Give a type to the containing function in `Input`
    Flow was not able to choose from two versions of the function:
     * ((...formattedMessage: Array<React$Node>) => React$Node)
     * (string => React$Node)
    
    Giving the concrete type fixes this confusion.

--- src/common/Input.js
+++ src/common/Input.js
@@ -81,7 +81,7 @@ export default class Input extends PureComponent<Props, State> {
         defaultMessage={placeholderMessage}
         values={placeholder.values}
       >
-        {text => (
+        {(text: string) => (
           <TextInput
             style={[styles.input, style]}
             placeholder={text}
