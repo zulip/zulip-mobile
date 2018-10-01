cff83b3f42d4923ae57d0e386fed654ed2f70a84
Boris Yankov <borisyankov@gmail.com>
2018-10-01 18:52:45 +0300

    tests: Verify no extra content is sent to WebView
    Add a test to verify that two props differing shallowly but equal
    deeply will not produce a WebView content update.

--- src/webview/__tests__/webViewHandleUpdates-test.js
+++ src/webview/__tests__/webViewHandleUpdates-test.js
@@ -82,6 +82,26 @@ describe('getInputMessages', () => {
     expect(messages[0].type).toEqual('content');
   });
 
+  test('when the rendered messages are equal deeply, do not return a new "content" message', () => {
+    const prevProps = {
+      auth: { realm: '' },
+      messages: [],
+      flags: { starred: {} },
+      renderedMessages: [
+        {
+          key: 0,
+          data: [{ key: 123, type: 'message', isBrief: false, message: { id: 0 } }],
+          message: {},
+        },
+      ],
+    };
+    const nextProps = { ...prevProps }; // a clone that differs shallowly
+
+    const messages = getInputMessages(prevProps, nextProps);
+
+    expect(messages).toHaveLength(0);
+  });
+
   test('there are several props that differ, return several messages', () => {
     const prevProps = {
       auth: {},
