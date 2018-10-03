7eb97b44026e4de8ef50a678ecc9969f381c56ae
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-10-03 23:54:51 +0530

    api/downloadFile: Combine `.android.js` and `.ios.js` files.
    No logical change, just code refactor. Refer #3018 to know the
    motivation behind this.
    
    Create mock `DocumentDir` method for `react-native-fetch-blob` to pass
    jest test cases
    (https://github.com/wkh237/react-native-fetch-blob/issues/383).

--- src/api/downloadFile.ios.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-import { CameraRoll } from 'react-native';
-
-import type { Auth } from './apiTypes';
-import { getFullUrl } from '../utils/url';
-
-export default (url: string, auth: Auth) =>
-  CameraRoll.saveToCameraRoll(getFullUrl(url, auth.realm));
