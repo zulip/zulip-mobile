223fa32a7b15ebccaf9f59c156053e108ce84de8
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-10-31 10:44:07 +0530

    api: Create api to get emojiNameToCodePoint map.

--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,7 @@ import registerForEvents from './registerForEvents';
 import uploadFile from './uploadFile';
 import emojiReactionAdd from './emoji/emojiReactionAdd';
 import emojiReactionRemove from './emoji/emojiReactionRemove';
+import getEmojiNameToCodePoint from './emoji/getEmojiNameToCodePoint';
 import markAllAsRead from './mark_as_read/markAllAsRead';
 import markStreamAsRead from './mark_as_read/markStreamAsRead';
 import markTopicAsRead from './mark_as_read/markTopicAsRead';
@@ -64,6 +65,7 @@ export {
   uploadFile,
   emojiReactionAdd,
   emojiReactionRemove,
+  getEmojiNameToCodePoint,
   markAllAsRead,
   markStreamAsRead,
   markTopicAsRead,
@@ -115,6 +117,7 @@ export default {
   uploadFile,
   emojiReactionAdd,
   emojiReactionRemove,
+  getEmojiNameToCodePoint,
   markAllAsRead,
   markStreamAsRead,
   markTopicAsRead,
