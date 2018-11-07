82fe73c2fd325001f47ad2b56d0587b4da7f6a09
Boris Yankov <borisyankov@gmail.com>
2018-11-06 16:52:00 -0800

    cleanup: Remove two unused functions from AppDelegate.m
    The functions were added in:
    https://github.com/zulip/zulip-mobile/commit/d7cfbe501e
    and intended to be used for OAuth on iOS.
    
    They were never used, so it is time for them to go!
    
    PS. We are using a component `SafariView` instead.

--- ios/ZulipMobile/AppDelegate.m
+++ ios/ZulipMobile/AppDelegate.m
@@ -46,20 +46,6 @@
   return YES;
 }
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
-{
-  return [RCTLinkingManager application:application openURL:url
-                      sourceApplication:sourceApplication annotation:annotation];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
- restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
-{
-  return [RCTLinkingManager application:application
-                   continueUserActivity:userActivity
-                     restorationHandler:restorationHandler];
-}
 // Required to register for notifications
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
 {
