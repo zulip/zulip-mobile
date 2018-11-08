52a4a079d126cd43e4e9bd7882b829d5f2255a67
Greg Price <gnprice@gmail.com>
2018-11-07 18:46:48 -0800

    wip ?. -> !.

--- ios/ZulipMobile/AppDelegate.swift
+++ ios/ZulipMobile/AppDelegate.swift
@@ -9,15 +9,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     let jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index.ios", fallbackResource: nil)
     let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "ZulipMobile", initialProperties: nil, launchOptions: launchOptions)
-
-    rootView?.backgroundColor = .white
-    window = UIWindow()
-
+    rootView!.backgroundColor = .white
     let rootViewController = UIViewController()
     rootViewController.view = rootView
     self.window = UIWindow(frame: UIScreen.main.bounds)
-    self.window?.rootViewController = rootViewController
-    self.window?.makeKeyAndVisible()
+    self.window!.rootViewController = rootViewController
+    self.window!.makeKeyAndVisible()
 
     return true
   }
