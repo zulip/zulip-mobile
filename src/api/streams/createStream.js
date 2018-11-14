8ebcd8c311d0abf3f45fda70de89dd2dfddfc550
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-11-14 18:48:41 +0530

    api, createStream: Add `stream_post_policy` parameter.
    Zulip supports multiple stream polices defining who can post to that
    stream, only admin or all users or only new members.
    
    It is a integer parameter:
    1 => Any user can post.
    2 => Only administrators can post.
    3 => Only new members can post.

--- src/api/streams/createStream.js
+++ src/api/streams/createStream.js
@@ -9,11 +9,13 @@ export default (
   description?: string = '',
   principals?: string[] = [],
   inviteOnly?: boolean = false,
+  streamPostPolicy?: number = 1,
   announce?: boolean = false,
 ): Promise<ApiResponse> =>
   apiPost(auth, 'users/me/subscriptions', {
     subscriptions: JSON.stringify([{ name, description }]),
     principals: JSON.stringify(principals),
     invite_only: inviteOnly,
+    stream_post_policy: streamPostPolicy,
     announce,
   });
