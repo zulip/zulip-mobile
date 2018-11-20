b078d27862bae3a49e33e6605227d5fe822595f5
Greg Price <greg@zulipchat.com>
2018-11-19 17:06:24 -0800

    search: Clean up unused props, and stop splatting props.
    All the props we're splatting here -- `auth`, `dispatch`,
    `realmEmoji`, `subscriptions` -- are simply ignored by MessageList,
    clobbered by its own `connect` call.  So stop passing them.
    
    With that, we don't need two of them at all, and we don't need to
    mention `dispatch`.

--- src/search/SearchMessagesCard.js
+++ src/search/SearchMessagesCard.js
@@ -6,14 +6,14 @@ import { StyleSheet, View } from 'react-native';
 import throttle from 'lodash.throttle';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
-import type { Auth, Dispatch, GlobalState, Message, Subscription } from '../types';
+import type { Auth, GlobalState, Message } from '../types';
 import { LoadingIndicator, SearchEmptyState } from '../common';
 import { HOME_NARROW, SEARCH_NARROW } from '../utils/narrow';
 import MessageList from '../message/MessageList';
 import { getMessages } from '../api';
 import renderMessages from '../message/renderMessages';
 import { NULL_ARRAY, NULL_FETCHING } from '../nullObjects';
-import { getAllRealmEmojiById, getAuth, getSubscriptions } from '../selectors';
+import { getAuth } from '../selectors';
 import { LAST_MESSAGE_ANCHOR } from '../constants';
 
 const styles = StyleSheet.create({
@@ -24,9 +24,7 @@ const styles = StyleSheet.create({
 
 type Props = {
   auth: Auth,
-  dispatch: Dispatch,
   query: string,
-  subscriptions: Subscription[],
 };
 
 type State = {
@@ -92,7 +90,6 @@ class SearchMessagesCard extends PureComponent<Props, State> {
             fetching={NULL_FETCHING}
             showMessagePlaceholders={false}
             typingUsers={NULL_ARRAY}
-            {...this.props}
           />
         </ActionSheetProvider>
       </View>
@@ -102,6 +99,4 @@ class SearchMessagesCard extends PureComponent<Props, State> {
 
 export default connect((state: GlobalState) => ({
   auth: getAuth(state),
-  subscriptions: getSubscriptions(state),
-  realmEmoji: getAllRealmEmojiById(state),
 }))(SearchMessagesCard);
