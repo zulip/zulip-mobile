26bc7e535adcfcdceed69fb413e1a7303ae7bcfa
Jack Zhang <15jzhang@gmail.com>
2018-11-20 16:48:57 -0800

    SwitchAccountButton: Remove unused prop `accounts`.

--- src/account-info/SwitchAccountButton.js
+++ src/account-info/SwitchAccountButton.js
@@ -6,7 +6,7 @@ import { StyleSheet } from 'react-native';
 
 import type { Auth, Dispatch, GlobalState } from '../types';
 import { ZulipButton } from '../common';
-import { getAuth, getAccounts, getPushToken } from '../selectors';
+import { getAuth, getPushToken } from '../selectors';
 import { unregisterPush } from '../api';
 import { logErrorRemotely } from '../utils/logging';
 import { deleteTokenPush, navigateToAccountPicker } from '../actions';
@@ -54,6 +54,5 @@ class SwitchAccountButton extends PureComponent<Props> {
 
 export default connect((state: GlobalState) => ({
   auth: getAuth(state),
-  accounts: getAccounts(state),
   pushToken: getPushToken(state),
 }))(SwitchAccountButton);
