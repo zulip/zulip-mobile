8adb3e2369b9912fbcdea5090210ea44e10ca7de
Boris Yankov <borisyankov@gmail.com>
2018-11-24 11:28:53 +0200

    routeConstants

--- src/nav/navActions.js
+++ src/nav/navActions.js
@@ -2,101 +2,102 @@
 import { NavigationActions } from 'react-navigation';
 
 import type { NavigationAction, Message, Narrow, ApiServerSettings } from '../types';
+import * as routes from './routeConstants';
 
 export const navigateBack = (): NavigationAction => NavigationActions.back();
 
 export const navigateToChat = (narrow: Narrow): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'chat', params: { narrow } });
+  NavigationActions.navigate({ routeName: routes.Chat, params: { narrow } });
 
 export const navigateToAllStreams = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'subscriptions' });
+  NavigationActions.navigate({ routeName: routes.Subscriptions });
 
 export const navigateToUsersScreen = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'users' });
+  NavigationActions.navigate({ routeName: routes.Users });
 
 export const navigateToSearch = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'search' });
+  NavigationActions.navigate({ routeName: routes.SearchMessages });
 
 export const navigateToSettings = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'settings' });
+  NavigationActions.navigate({ routeName: routes.Settings });
 
 export const navigateToEmojiPicker = (messageId: number): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'emoji-picker', params: { messageId } });
+  NavigationActions.navigate({ routeName: routes.EmojiPicker, params: { messageId } });
 
 export const navigateToAuth = (serverSettings: ApiServerSettings): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'auth', params: { serverSettings } });
+  NavigationActions.navigate({ routeName: routes.Auth, params: { serverSettings } });
 
 export const navigateToDev = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'dev' });
+  NavigationActions.navigate({ routeName: routes.DevAuth });
 
 export const navigateToPassword = (requireEmailFormat: boolean): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'password', params: { requireEmailFormat } });
+  NavigationActions.navigate({ routeName: routes.PasswordAuth, params: { requireEmailFormat } });
 
 export const navigateToAccountPicker = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'account' });
+  NavigationActions.navigate({ routeName: routes.AccountPick });
 
 export const navigateToAccountDetails = (email: string): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'account-details', params: { email } });
+  NavigationActions.navigate({ routeName: routes.AccountDetails, params: { email } });
 
 export const navigateToGroupDetails = (recipients: string[]): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'group-details', params: { recipients } });
+  NavigationActions.navigate({ routeName: routes.GroupDetails, params: { recipients } });
 
 export const navigateToRealmScreen = (realm?: string): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'realm', params: { realm } });
+  NavigationActions.navigate({ routeName: routes.Realm, params: { realm } });
 
 export const navigateToLightbox = (src: string, message: Message): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'lightbox', params: { src, message } });
+  NavigationActions.navigate({ routeName: routes.Lightbox, params: { src, message } });
 
 export const navigateToLoading = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'loading' });
+  NavigationActions.navigate({ routeName: routes.Loading });
 
 export const navigateToLanguage = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'language' });
+  NavigationActions.navigate({ routeName: routes.Language });
 
 export const navigateToCreateGroup = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'group' });
+  NavigationActions.navigate({ routeName: routes.CreateGroup });
 
 export const navigateToDiagnostics = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'diagnostics' });
+  NavigationActions.navigate({ routeName: routes.Diagnostics });
 
 export const navigateToWelcomeHelp = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'welcome-help' });
+  NavigationActions.navigate({ routeName: routes.WelcomeHelp });
 
 export const navigateToWelcomeScreen = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'welcome' });
+  NavigationActions.navigate({ routeName: routes.Welcome });
 
 export const navigateToVariables = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'variables' });
+  NavigationActions.navigate({ routeName: routes.Variables });
 
 export const navigateToTiming = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'timing' });
+  NavigationActions.navigate({ routeName: routes.Timing });
 
 export const navigateToStorage = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'storage' });
+  NavigationActions.navigate({ routeName: routes.Storage });
 
 export const navigateToDebug = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'debug' });
+  NavigationActions.navigate({ routeName: routes.Debug });
 
 export const navigateToNotifDiag = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'notifDiag' });
+  NavigationActions.navigate({ routeName: routes.NotificationDiag });
 
 export const navigateToStream = (streamId: number): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'stream', params: { streamId } });
+  NavigationActions.navigate({ routeName: routes.Stream, params: { streamId } });
 
 export const navigateToTopicList = (streamId: number): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'topics', params: { streamId } });
+  NavigationActions.navigate({ routeName: routes.TopicList, params: { streamId } });
 
 export const navigateToCreateStream = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'stream-create' });
+  NavigationActions.navigate({ routeName: routes.CreateStream });
 
 export const navigateToEditStream = (streamId: number): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'stream-edit', params: { streamId } });
+  NavigationActions.navigate({ routeName: routes.EditStream, params: { streamId } });
 
 export const navigateToStreamSubscribers = (streamId: number): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'invite-users', params: { streamId } });
+  NavigationActions.navigate({ routeName: routes.InviteUsers, params: { streamId } });
 
 export const navigateToNotifications = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'notifications' });
+  NavigationActions.navigate({ routeName: routes.Notifications });
 
 export const navigateToLegal = (): NavigationAction =>
-  NavigationActions.navigate({ routeName: 'legal' });
+  NavigationActions.navigate({ routeName: routes.Legal });
