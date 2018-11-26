fa16bc1fac58b5ed195849ebd1a4520c2493b8e4
Boris Yankov <borisyankov@gmail.com>
2018-11-26 15:52:08 +0200

    flow: Add types to `fetchingSelectors`

--- src/chat/fetchingSelectors.js
+++ src/chat/fetchingSelectors.js
@@ -1,14 +1,14 @@
 /* @flow strict-local */
 import { createSelector } from 'reselect';
 
-import type { Fetching, Narrow } from '../types';
+import type { Fetching, Selector, Narrow } from '../types';
 import { getFetching } from '../directSelectors';
 import { NULL_FETCHING } from '../nullObjects';
 
-export const getFetchingForActiveNarrow = (narrow: Narrow) =>
+export const getFetchingForActiveNarrow = (narrow: Narrow): Selector<Fetching> =>
   createSelector(getFetching, fetching => fetching[JSON.stringify(narrow)] || NULL_FETCHING);
 
-export const getIsFetching = (narrow: Narrow) =>
+export const getIsFetching = (narrow: Narrow): Selector<boolean> =>
   createSelector(
     getFetchingForActiveNarrow(narrow),
     (fetching: Fetching): boolean => fetching.older || fetching.newer,
