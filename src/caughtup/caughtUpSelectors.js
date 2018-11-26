39faa8e68dcfdbaa3be7e0cf809b2a4cadac76f2
Boris Yankov <borisyankov@gmail.com>
2018-11-26 15:51:56 +0200

    flow: Add types to `caughtUpSelectors`

--- src/caughtup/caughtUpSelectors.js
+++ src/caughtup/caughtUpSelectors.js
@@ -1,10 +1,10 @@
 /* @flow strict-local */
 import { createSelector } from 'reselect';
 
-import type { CaughtUpState, GlobalState, Narrow } from '../types';
+import type { CaughtUp, CaughtUpState, GlobalState, Narrow, Selector } from '../types';
 import { NULL_CAUGHTUP, NULL_OBJECT } from '../nullObjects';
 
 export const getCaughtUp = (state: GlobalState): CaughtUpState => state.caughtUp || NULL_OBJECT;
 
-export const getCaughtUpForActiveNarrow = (narrow: Narrow) =>
+export const getCaughtUpForActiveNarrow = (narrow: Narrow): Selector<CaughtUp> =>
   createSelector(getCaughtUp, caughtUp => caughtUp[JSON.stringify(narrow)] || NULL_CAUGHTUP);
