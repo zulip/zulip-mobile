99f55c1c2346010b74ba2df0ac76d697d896d706
Greg Price <greg@zulipchat.com>
2018-11-27 14:05:49 -0800

    emoji data: Small refactor to pull out the list of names in one place.

--- src/emoji/data.js
+++ src/emoji/data.js
@@ -4,12 +4,14 @@ import 'string.fromcodepoint'; // eslint-disable-line spellcheck/spell-checker
 import type { RealmEmojiType } from '../types';
 import { unicodeCodeByName, override } from './codePointMap';
 
-export const nameToEmojiMap = Object.keys(unicodeCodeByName).reduce((obj, name) => {
+const unicodeEmojiNames = Object.keys(unicodeCodeByName);
+
+export const nameToEmojiMap = unicodeEmojiNames.reduce((obj, name) => {
   obj[name] = String.fromCodePoint(parseInt(unicodeCodeByName[name], 16));
   return obj;
 }, ({}: { [string]: string }));
 
-export const codeToEmojiMap = Object.keys(unicodeCodeByName).reduce((obj, name) => {
+export const codeToEmojiMap = unicodeEmojiNames.reduce((obj, name) => {
   const code = unicodeCodeByName[name];
   const displayCode = override[code] || code;
   obj[code] = String.fromCodePoint(parseInt(displayCode, 16));
@@ -20,6 +22,6 @@ export const getFilteredEmojiNames = (
   query: string,
   activeRealmEmojiByName: { [string]: RealmEmojiType },
 ): string[] => {
-  const names = [...Object.keys(unicodeCodeByName), ...Object.keys(activeRealmEmojiByName)];
+  const names = [...unicodeEmojiNames, ...Object.keys(activeRealmEmojiByName)];
   return Array.from(new Set([...names.filter(x => x.indexOf(query) === 0).sort()]));
 };
