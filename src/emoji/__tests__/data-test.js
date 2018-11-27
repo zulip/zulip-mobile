51e650faf5a9d7845c795457192a3f2ea27b8de2
Greg Price <greg@zulipchat.com>
2018-11-27 14:43:26 -0800

    emoji: Diagnose #3129 as bug in nameToEmojiMap; add tests.

--- src/emoji/__tests__/data-test.js
+++ src/emoji/__tests__/data-test.js
@@ -1,4 +1,24 @@
-import { getFilteredEmojiNames } from '../data';
+import { getFilteredEmojiNames, nameToEmojiMap } from '../data';
+
+// Prettier disabled in .prettierignore ; it misparses this file, apparently
+// because of the emoji.  (Even if they're tucked away in comments, it still
+// gets it wrong.)
+
+/* eslint-disable dot-notation, spellcheck/spell-checker */
+describe('nameToEmojiMap', () => {
+  test('works for some single-codepoint emoji', () => {
+    expect(nameToEmojiMap['thumbs_up']).toEqual('👍').toEqual('');
+    expect(nameToEmojiMap['pride']).toEqual('🌈');
+    expect(nameToEmojiMap['rainbow']).toEqual('🌈');
+  });
+
+  // Skipped because of (part of?) #3129.
+  test.skip('works for some multi-codepoint emoji', () => {
+    expect(nameToEmojiMap['0']).toEqual('0⃣');
+    expect(nameToEmojiMap['asterisk']).toEqual('*⃣');
+    expect(nameToEmojiMap['hash']).toEqual('#⃣');
+  });
+});
 
 describe('getFilteredEmojiNames', () => {
   test('empty query returns all emojis', () => {
