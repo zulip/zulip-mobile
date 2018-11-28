559a13a77bee12d4e2fdc0d0ced3462f29d01e73
Boris Yankov <borisyankov@gmail.com>
2018-11-28 17:39:03 +0200

    flow: Add types to `navSelectors`

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -1,7 +1,7 @@
 /* @flow */
 import { createSelector } from 'reselect';
 
-import type { GlobalState, NavigationState } from '../types';
+import type { GlobalState, Narrow, NavigationState, Selector } from '../types';
 import config from '../config';
 import { navigateToChat } from './navActions';
 import { getUsersById } from '../users/userSelectors';
@@ -17,20 +17,20 @@ const getNavigationIndex = (state: GlobalState): number => state.nav.index;
 export const getCurrentRouteName = (state: GlobalState): string =>
   state.nav.routes[state.nav.index].routeName;
 
-export const getCurrentRouteParams = createSelector(
+export const getCurrentRouteParams: Selector<Object> = createSelector(
   getNavigationRoutes,
   getNavigationIndex,
   (routes, index) => routes && routes[index] && routes[index].params,
 );
 
-export const getChatScreenParams = createSelector(
+export const getChatScreenParams: Selector<{ narrow: Narrow }> = createSelector(
   getCurrentRouteParams,
   params => params || { narrow: undefined },
 );
 
-export const getCanGoBack = (state: GlobalState) => state.nav.index > 0;
+export const getCanGoBack = (state: GlobalState): boolean => state.nav.index > 0;
 
-export const getSameRoutesCount = createSelector(getNav, nav => {
+export const getSameRoutesCount: Selector<number> = createSelector(getNav, nav => {
   let i = nav.routes.length - 1;
   while (i >= 0) {
     if (nav.routes[i].routeName !== nav.routes[nav.routes.length - 1].routeName) {
@@ -41,21 +41,25 @@ export const getSameRoutesCount = createSelector(getNav, nav => {
   return nav.routes.length - i - 1;
 });
 
-export const getStateForRoute = (route: string, params?: Object) => {
+export const getStateForRoute = (route: string, params?: Object): ?NavigationState => {
   const action = AppNavigator.router.getActionForPathAndParams(route, params);
   return action != null ? AppNavigator.router.getStateForAction(action) : null;
 };
 
-export const getInitialNavState = createSelector(getNav, getUsersById, (nav, usersById) => {
-  const state =
-    !nav || (nav.routes.length === 1 && nav.routes[0].routeName === 'loading')
-      ? getStateForRoute('main')
-      : nav;
+export const getInitialNavState: Selector<?NavigationState> = createSelector(
+  getNav,
+  getUsersById,
+  (nav, usersById) => {
+    const state =
+      !nav || (nav.routes.length === 1 && nav.routes[0].routeName === 'loading')
+        ? getStateForRoute('main')
+        : nav;
 
-  if (!config.startup.notification) {
-    return state;
-  }
+    if (!config.startup.notification) {
+      return state;
+    }
 
-  const narrow = getNarrowFromNotificationData(config.startup.notification, usersById);
-  return AppNavigator.router.getStateForAction(navigateToChat(narrow), state);
-});
+    const narrow = getNarrowFromNotificationData(config.startup.notification, usersById);
+    return AppNavigator.router.getStateForAction(navigateToChat(narrow), state);
+  },
+);
