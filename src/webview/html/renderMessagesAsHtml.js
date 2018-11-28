e76434740e0166495e16c07090042f7e3ed49c13
Greg Price <greg@zulipchat.com>
2018-11-27 17:41:51 -0800

    message render: Tighten renderMessagesAsHtml.
    This is less functional-looking... but with all those `push` calls,
    the functional-ness of the `reduce` version was really an illusion
    anyway.  This version does exactly the same thing, and is a lot
    clearer about it.

--- src/webview/html/renderMessagesAsHtml.js
+++ src/webview/html/renderMessagesAsHtml.js
@@ -10,20 +10,17 @@ export default (
   backgroundData: BackgroundData,
   narrow: Narrow,
   renderedMessages: RenderedSectionDescriptor[],
-): string =>
-  renderedMessages
-    .reduce((list, section, index) => {
-      list.push(messageHeaderAsHtml(backgroundData, narrow, section.message));
-
-      section.data.forEach((item, idx) => {
-        if (item.type === 'time') {
-          list.push(timeRowAsHtml(item.timestamp, item.firstMessage));
-        } else {
-          const { message } = item;
-          list.push(messageAsHtml(backgroundData, message, item.isBrief));
-        }
-      });
-
-      return list;
-    }, [])
-    .join('');
+): string => {
+  const pieces = [];
+  renderedMessages.forEach(section => {
+    pieces.push(messageHeaderAsHtml(backgroundData, narrow, section.message));
+    section.data.forEach(item => {
+      if (item.type === 'time') {
+        pieces.push(timeRowAsHtml(item.timestamp, item.firstMessage));
+      } else {
+        pieces.push(messageAsHtml(backgroundData, item.message, item.isBrief));
+      }
+    });
+  });
+  return pieces.join('');
+};
