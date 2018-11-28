2dd991edd9b55a898f4cf281a5f7e600d81e6fb4
Greg Price <greg@zulipchat.com>
2018-11-27 16:08:40 -0800

    message list: Fix extra space before count in realm-emoji reactions.

--- src/webview/html/messageReactionAsHtml.js
+++ src/webview/html/messageReactionAsHtml.js
@@ -4,8 +4,7 @@ import { codeToEmojiMap } from '../../emoji/data';
 import template from './template';
 
 const getRealmEmojiHtml = (realmEmoji: RealmEmojiType): string =>
-  template`<img src="${realmEmoji.source_url}"/>
-  `;
+  template`<img src="${realmEmoji.source_url}"/>`;
 
 export default (
   messageId: number,
