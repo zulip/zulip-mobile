f7f573ea7f5ffa4b937e43c098e0181720f25b37
Greg Price <greg@zulipchat.com>
2018-11-27 16:10:09 -0800

    message list: Fold tiny helper getRealmEmojiHtml into caller.
    This just adds layers when reading.

--- src/webview/html/messageReactionAsHtml.js
+++ src/webview/html/messageReactionAsHtml.js
@@ -3,9 +3,6 @@ import type { AggregatedReaction, RealmEmojiType } from '../../types';
 import { codeToEmojiMap } from '../../emoji/data';
 import template from './template';
 
-const getRealmEmojiHtml = (realmEmoji: RealmEmojiType): string =>
-  template`<img src="${realmEmoji.source_url}"/>`;
-
 export default (
   messageId: number,
   reaction: AggregatedReaction,
@@ -16,7 +13,7 @@ export default (
         data-code="${reaction.code}"
         data-type="${reaction.type}">$!${
     allRealmEmojiById[reaction.code]
-      ? getRealmEmojiHtml(allRealmEmojiById[reaction.code])
+      ? template`<img src="${allRealmEmojiById[reaction.code].source_url}"/>`
       : codeToEmojiMap[reaction.code]
   }&nbsp;${reaction.count}
 </span>`;
