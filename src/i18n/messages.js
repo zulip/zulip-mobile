13386b14d4266135458d5a1d5a1f294bd219868a
Greg Price <greg@zulipchat.com>
2018-12-03 13:16:21 -0800

    i18n/messages: Re-enable lint, fixing extensions in imports.
    ESLint was showing a pair of errors on each import, like these
    (edited slightly for readability):
    
      error  Unable to resolve path to module './translations/messages_ar'
        import/no-unresolved
      error  Missing file extension for "./translations/messages_ar"
        import/extensions
    
    Tell the `import` plugin that `.json` files are importable,
    eliminating the `import/no-unresolved` errors; and take the
    airbnb style's advice on the other errors by writing the
    extensions explicitly.  I remember this pattern being pretty
    confusing the first couple of times I saw it, so explicitly
    saying it's a JSON file may help.
    
    Leave the spellchecker disabled so we don't have to add a bunch of
    "words" for these language codes.

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -1,30 +1,30 @@
-/* eslint-disable */
+/* eslint-disable spellcheck/spell-checker */
 /* @flow */
 
-import ar from './translations/messages_ar';
-import bg from './translations/messages_bg';
-import ca from './translations/messages_ca';
-import cs from './translations/messages_cs';
-import de from './translations/messages_de';
-import en from './translations/messages_en';
-import es from './translations/messages_es';
-import fr from './translations/messages_fr';
-import hi from './translations/messages_hi';
-import hu from './translations/messages_hu';
-import id from './translations/messages_id_ID';
-import it from './translations/messages_it';
-import ja from './translations/messages_ja';
-import ko from './translations/messages_ko';
-import ml from './translations/messages_ml';
-import nl from './translations/messages_nl';
-import pl from './translations/messages_pl';
-import pt from './translations/messages_pt';
-import ru from './translations/messages_ru';
-import sr from './translations/messages_sr';
-import sv from './translations/messages_sv';
-import ta from './translations/messages_ta';
-import tr from './translations/messages_tr';
-import zh from './translations/messages_zh-Hans';
+import ar from './translations/messages_ar.json';
+import bg from './translations/messages_bg.json';
+import ca from './translations/messages_ca.json';
+import cs from './translations/messages_cs.json';
+import de from './translations/messages_de.json';
+import en from './translations/messages_en.json';
+import es from './translations/messages_es.json';
+import fr from './translations/messages_fr.json';
+import hi from './translations/messages_hi.json';
+import hu from './translations/messages_hu.json';
+import id from './translations/messages_id_ID.json';
+import it from './translations/messages_it.json';
+import ja from './translations/messages_ja.json';
+import ko from './translations/messages_ko.json';
+import ml from './translations/messages_ml.json';
+import nl from './translations/messages_nl.json';
+import pl from './translations/messages_pl.json';
+import pt from './translations/messages_pt.json';
+import ru from './translations/messages_ru.json';
+import sr from './translations/messages_sr.json';
+import sv from './translations/messages_sv.json';
+import ta from './translations/messages_ta.json';
+import tr from './translations/messages_tr.json';
+import zh from './translations/messages_zh-Hans.json';
 
 export default {
   ar,
