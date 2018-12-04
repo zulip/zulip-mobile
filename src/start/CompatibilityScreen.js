5cefd7e171779f49b074c1e197ff16eac99411db
Jack Zhang <15jzhang@gmail.com>
2018-12-03 16:39:09 -0800

    compatibility: Link to the respective app stores for update.

--- src/start/CompatibilityScreen.js
+++ src/start/CompatibilityScreen.js
@@ -1,10 +1,23 @@
 /* @flow strict-local */
 import React, { PureComponent } from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Image, Text, View, Platform, Linking } from 'react-native';
 
+import { Touchable } from '../common';
 import { BRAND_COLOR } from '../styles';
+import appStoreBadgePNG from '../../static/img/app-store-badge.png';
+import googlePlayBadgePNG from '../../static/img/google-play-badge.png';
 
 const styles = StyleSheet.create({
+  appStoreBadge: {
+    width: 180,
+    height: 60,
+    alignSelf: 'center',
+  },
+  googlePlayBadge: {
+    height: 80,
+    width: 210,
+    alignSelf: 'center',
+  },
   screen: {
     flex: 1,
     justifyContent: 'center',
@@ -16,18 +29,40 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 8,
   },
+  badgeContainer: {
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
 });
 
+const AppStoreBadge = () => (
+  <Image style={styles.appStoreBadge} source={appStoreBadgePNG} resizeMode="contain" />
+);
+
+const GooglePlayBadge = () => (
+  <Image style={styles.googlePlayBadge} source={googlePlayBadgePNG} resizeMode="contain" />
+);
+
 export default class CompatibilityScreen extends PureComponent<{}> {
+  storeURL = Platform.OS === 'ios'
+    ? 'https://itunes.apple.com/app/zulip/id1203036395'
+    : 'https://play.google.com/store/apps/details?id=com.zulipmobile';
+
+  openStoreURL = () => {
+    Linking.openURL(this.storeURL);
+  };
+
   render() {
-    const pleaseDownloadMsg =
-      Platform.OS === 'ios'
-        ? 'Please download the latest version from the App Store.'
-        : 'Please download the latest version from the Play Store.';
     return (
       <View style={styles.screen}>
         <Text style={styles.text}>This app is too old!</Text>
-        <Text style={styles.text}>{pleaseDownloadMsg}</Text>
+        <Text style={styles.text}>Please update to the latest version.</Text>
+        <View style={styles.badgeContainer}>
+          <Touchable onPress={this.openStoreURL}>
+            {Platform.OS === 'ios' ? <AppStoreBadge /> : <GooglePlayBadge />}
+          </Touchable>
+        </View>
       </View>
     );
   }
