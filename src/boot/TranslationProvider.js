c22dfee9b976c1e4ad46587ce08241b34512daaa
Greg Price <greg@zulipchat.com>
2018-12-06 15:22:05 -0800

    i18n: Add comment explaining why our use of legacy-context works.

--- src/boot/TranslationProvider.js
+++ src/boot/TranslationProvider.js
@@ -24,6 +24,20 @@ class TranslationProvider extends PureComponent<Props> {
     const { locale, children } = this.props;
 
     return (
+      /* `IntlProvider` uses React's "legacy context API", deprecated since
+       * React 16.3, of which the docs say:
+       *
+       *   ## Updating Context
+       *
+       *   Don't do it.
+       *
+       *   React has an API to update context, but it is fundamentally
+       *   broken and you should not use it.
+       *
+       * To work around that, we set `key={locale}` to force the whole tree
+       * to rerender if the locale changes.  Not cheap, but the locale
+       * changing is rare.
+       */
       <IntlProvider key={locale} locale={locale} textComponent={Text} messages={messages[locale]}>
         {children}
       </IntlProvider>
