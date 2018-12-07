0871eaa14c6d097e83c3e19bab9ed846669fa5e9
Greg Price <greg@zulipchat.com>
2018-12-06 17:45:22 -0800

    showToast: Don't bother returning meaningless value.
    This is probably `undefined` aka type `void` anyway... but we don't
    have a libdef for this library, so it's ending up treated as `any`.
    We can get a bit more code covered by just settling the question by
    not returning it; now our function returns `void` for sure.

--- src/utils/info.js
+++ src/utils/info.js
@@ -2,7 +2,9 @@
 import { Alert } from 'react-native';
 import Toast from '@remobile/react-native-toast';
 
-export const showToast = (message: string) => Toast.show(message, Toast.SHORT);
+export const showToast = (message: string) => {
+  Toast.show(message, Toast.SHORT);
+};
 
 export const showErrorAlert = (message: string, title: string) =>
   Alert.alert(title, message, [{ text: 'OK', onPress: () => {} }], { cancelable: true });
