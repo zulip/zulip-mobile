95fd7d4830555c00f020b13b8c225036a6f6ddfb
Greg Price <greg@zulipchat.com>
2018-12-13 12:56:19 -0800

    api/reportPresence: Link to subsystem doc.

--- src/api/reportPresence.js
+++ src/api/reportPresence.js
@@ -2,6 +2,7 @@
 import type { ApiResponseWithPresence, Auth } from './apiTypes';
 import { apiPost } from './apiFetch';
 
+/** See https://zulip.readthedocs.io/en/latest/subsystems/presence.html . */
 export default (
   auth: Auth,
   hasFocus: boolean = true,
