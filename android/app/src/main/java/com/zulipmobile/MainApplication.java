6d3382f495ec41b6824bfeb5908229ea44d45fa0
Greg Price <greg@zulipchat.com>
2018-12-18 05:48:09 -0800

    android: Fix last Android Studio warning in MainApplication, too.

--- android/app/src/main/java/com/zulipmobile/MainApplication.java
+++ android/app/src/main/java/com/zulipmobile/MainApplication.java
@@ -39,7 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
+            return Arrays.asList(
                     new MainReactPackage(),
                     new RNTextInputResetPackage(),
                     new ImagePickerPackage(),
