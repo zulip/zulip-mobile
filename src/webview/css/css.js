fe037e2d32150bc3772da07d5afd63687c04e9d6
Boris Yankov <borisyankov@gmail.com>
2018-12-20 19:13:29 +0200

    webview: WIP: subheader alignment

--- src/webview/css/css.js
+++ src/webview/css/css.js
@@ -74,7 +74,7 @@ hr {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin-bottom: 4px;
+  height: 1.4rem;
 }
 .timerow {
   text-align: center;
@@ -95,10 +95,18 @@ hr {
 .timerow-right {
   background: -webkit-linear-gradient(left, #999 0%, transparent 90%);
 }
+.username,
+.timestamp {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  line-height: 1;
+}
+.username {
+  font-weight: bold;
+}
 .timestamp {
   color: #999;
   font-size: 0.9rem;
-  white-space: nowrap;
 }
 .message,
 .loading {
@@ -126,12 +134,6 @@ hr {
 .content {
   width: 100%;
   max-width: 100%;
-  overflow: hidden;
-}
-.username {
-  font-weight: bold;
-  text-overflow: ellipsis;
-  white-space: nowrap;
   overflow: hidden;
 }
 .user-group-mention,
