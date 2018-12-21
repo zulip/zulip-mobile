a5cc17e02f3be78f215637c91f9280766d20530c
anhhuynh <anhhuynh98@gmail.com>
2018-12-21 18:54:00 +0700

    settings: Identify each language with its own name for itself, too.
    The names are from those used by Wikipedia in its own mobile UIs.
    Wikipedia is a very conscientiously international and intercultural
    project with a lot of effort going into it by speakers of many
    languages, making it a useful gold standard for this.
    
    One reference directly available on the Web is the list of Wikipedias:
      https://meta.wikimedia.org/wiki/List_of_Wikipedias
    
    One option not found on that list is "Chinese (Simplified)" -- there's
    just one Chinese Wikipedia for both simplified and traditional scripts:
      https://en.wikipedia.org/wiki/Chinese_Wikipedia
    The translation we use does appear, though, in the "Language" box in
    preferences in the Wikipedia webapp:
      https://en.wikipedia.org/wiki/Special:Preferences
    
    [greg: added discussion in commit message]

--- src/settings/LanguagePickerItem.js
+++ src/settings/LanguagePickerItem.js
@@ -7,8 +7,13 @@ import { BRAND_COLOR } from '../styles';
 import { IconDone } from '../common/Icons';
 
 const componentStyles = StyleSheet.create({
-  language: {
+  languageWrapper: {
     flex: 1,
+    flexDirection: 'column',
+  },
+  name: {
+    fontWeight: '300',
+    fontSize: 13,
   },
   listItem: {
     flexDirection: 'row',
@@ -23,19 +28,23 @@ const componentStyles = StyleSheet.create({
 type Props = {|
   locale: string,
   name: string,
+  nativeName: string,
   selected: boolean,
   onValueChange: (locale: string) => void,
 |};
 
 export default class LanguagePickerItem extends PureComponent<Props> {
   render() {
-    const { locale, name, selected, onValueChange } = this.props;
+    const { locale, name, nativeName, selected, onValueChange } = this.props;
 
     return (
       <Touchable onPress={() => onValueChange(locale)}>
         <View style={componentStyles.listItem}>
-          <RawLabel style={componentStyles.language} text={name} />
-          {selected && <IconDone size={16} color={BRAND_COLOR} />}
+          <View style={componentStyles.languageWrapper}>
+            <RawLabel text={nativeName} />
+            <RawLabel text={name} style={componentStyles.name} />
+          </View>
+          <View>{selected && <IconDone size={16} color={BRAND_COLOR} />}</View>
         </View>
       </Touchable>
     );
--- src/settings/languages.js
+++ src/settings/languages.js
@@ -4,85 +4,106 @@ export default [
   {
     locale: 'en',
     name: 'English',
+    nativeName: 'English',
   },
   {
     locale: 'bg',
     name: 'Bulgarian',
+    nativeName: 'Български',
   },
   {
     locale: 'ca',
     name: 'Catalan',
+    nativeName: 'Català',
   },
   {
     locale: 'zh',
     name: 'Chinese (Simplified)',
+    nativeName: '中文 (简体)',
   },
   {
     locale: 'cs',
     name: 'Czech',
+    nativeName: 'Čeština',
   },
   {
     locale: 'nl',
     name: 'Dutch',
+    nativeName: 'Nederlands',
   },
   {
     locale: 'fr',
     name: 'French',
+    nativeName: 'Français',
   },
   {
     locale: 'de',
     name: 'German',
+    nativeName: 'Deutsch',
   },
   {
     locale: 'hi',
     name: 'Hindi',
+    nativeName: 'हिन्दी',
   },
   {
     locale: 'hu',
     name: 'Hungarian',
+    nativeName: 'Magyar',
   },
   {
     locale: 'id',
     name: 'Indonesian',
+    nativeName: 'Bahasa Indonesia',
   },
   {
     locale: 'it',
     name: 'Italian',
+    nativeName: 'Italiano',
   },
   {
     locale: 'ja',
     name: 'Japanese',
+    nativeName: '日本語',
   },
   {
     locale: 'ml',
     name: 'Malayalam',
+    nativeName: 'മലയാളം',
   },
   {
     locale: 'pl',
     name: 'Polish',
+    nativeName: 'Polski',
   },
   {
     locale: 'pt',
     name: 'Portuguese',
+    nativeName: 'Português',
   },
   {
     locale: 'ru',
     name: 'Russian',
+    nativeName: 'Русский',
   },
   {
     locale: 'sr',
     name: 'Serbian',
+    nativeName: 'Српски',
   },
   {
     locale: 'es',
     name: 'Spanish',
+    nativeName: 'Español',
   },
   {
     locale: 'ta',
     name: 'Tamil',
+    nativeName: 'தமிழ்',
   },
   {
     locale: 'tr',
     name: 'Turkish',
+    nativeName: 'Türkçe',
   },
 ];
