894cfa846a3ed6345fd7a9a719ffaf64ed423f8d
Greg Price <greg@zulipchat.com>
2018-12-28 17:51:01 -0800

    account: Organize selector tests a bit.
    Very small changes other than indentation.

--- src/account/__tests__/accountsSelectors-test.js
+++ src/account/__tests__/accountsSelectors-test.js
@@ -3,26 +3,28 @@ import deepFreeze from 'deep-freeze';
 import { getAuth } from '../accountsSelectors';
 import { NULL_ACCOUNT } from '../../nullObjects';
 
-test('getAuth returns an empty object when no accounts', () => {
-  const state = deepFreeze({
-    accounts: [],
+describe('getAuth', () => {
+  test('returns an empty object when no accounts', () => {
+    const state = deepFreeze({
+      accounts: [],
+    });
+
+    const auth = getAuth(state);
+
+    expect(auth).toBe(NULL_ACCOUNT);
   });
 
-  const auth = getAuth(state);
+  test('returns the auth information from the first account', () => {
+    const state = deepFreeze({
+      accounts: [{ realm: 'https://realm1.com' }, { realm: 'https://realm2.com' }],
+    });
 
-  expect(auth).toBe(NULL_ACCOUNT);
-});
+    const auth = getAuth(state);
 
-test('getAuth returns the auth information from the first account', () => {
-  const state = deepFreeze({
-    accounts: [{ realm: 'https://realm1.com' }, { realm: 'https://realm2.com' }],
-  });
-
-  const auth = getAuth(state);
-
-  expect(auth).toEqual({
-    realm: 'https://realm1.com',
-    email: undefined,
-    apiKey: undefined,
+    expect(auth).toEqual({
+      realm: 'https://realm1.com',
+      email: undefined,
+      apiKey: undefined,
+    });
   });
 });
