249254f81fa245ecbe3fd2b03f6711714e4632e9
Boris Yankov <borisyankov@gmail.com>
2019-01-05 15:13:31 +0200

    tryUntilSuccessful: Do not retry if error is 4xx (Client Error).
    Initially, this code was checking only for 401 (Unauthorized) error.
    But according to Tim we get either a 400 or 401 if realm is deactivated.
    
    In the case where the API key is invalid, the server recently changed
    from returning 400 to returning 401, in commit zulip/zulip@2adb0734d .
    
    The first check ensures that the exception is thrown by our API
    functions by checking for existence of httpError field.
    To keep this code compatible with different server versions and also
    because it makes sense, do not retry any 4xx error.  Since this error
    is a "Client Error", just retrying it should never succeed.
    
    Also add jsdoc.

--- src/utils/async.js
+++ src/utils/async.js
@@ -9,10 +9,21 @@ export function delay<T>(callback: () => T): Promise<T> {
 export const sleep = (ms: number = 0): Promise<void> =>
   new Promise(resolve => setTimeout(resolve, ms));
 
+/**
+ * Calls an async function and if unsuccessful retries the call.
+ *
+ * If the function is an API call and the response has HTTP status code 4xx
+ * the error is considered unrecoverable and the exception is rethrown, to be
+ * handled further up in the call stack.
+ */
 export async function tryUntilSuccessful<T>(func: () => Promise<T>): Promise<T> {
   try {
     return await func();
   } catch (e) {
+    if (e.httpStatus !== undefined && e.httpStatus >= 400 && e.httpStatus <= 499) {
+      // do not retry if error is 4xx (Client Error)
+      throw e;
+    }
     await progressiveTimeout();
   }
   return tryUntilSuccessful(func);
