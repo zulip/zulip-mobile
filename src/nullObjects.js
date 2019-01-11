fe65e7d151c23a48f1eb785d2ad8efc15be0d0cc
Greg Price <greg@zulipchat.com>
2019-01-10 17:13:23 -0800

    presence: Remove disused NULL_PRESENCE and ..._AGGREGATED.
    These have been unused since c68c5bb68 last April; cut them!

--- src/nullObjects.js
+++ src/nullObjects.js
@@ -1,13 +1,5 @@
 /* @flow strict-local */
-import type {
-  User,
-  Subscription,
-  UserPresence,
-  PresenceAggregated,
-  CaughtUp,
-  Fetching,
-  NavigationState,
-} from './types';
+import type { User, Subscription, CaughtUp, Fetching, NavigationState } from './types';
 
 export const nullFunction = () => {};
 
@@ -51,18 +43,6 @@ export const NULL_SUBSCRIPTION: Subscription = {
   is_old_stream: false,
   is_announcement_only: false,
   history_public_to_subscribers: false,
-};
-
-/** DEPRECATED; don't add new uses.  See block comment above definition. */
-export const NULL_PRESENCE_AGGREGATED: PresenceAggregated = {
-  client: '',
-  status: 'offline',
-  timestamp: 0,
-};
-
-/** DEPRECATED; don't add new uses.  See block comment above definition. */
-export const NULL_PRESENCE: UserPresence = {
-  aggregated: NULL_PRESENCE_AGGREGATED,
 };
 
 /** DEPRECATED; don't add new uses.  See block comment above definition. */
