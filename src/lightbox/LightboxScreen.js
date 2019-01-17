c7f15a465977906a54ed3a48cb71d600d3091a6b
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2019-01-17 23:34:36 +0530

    ios, lightbox: Fix `SafeAreaInsets` padding is not applied.
    Wrap screen content with `SafeAreaView` from `react-native` instead of
    `View`.
    
    In all other screens we are manually handeling safe area by applying
    margins/padding as we are using `Screen`. In 4 screens we aren't
    using `Screen`:
    
    $ git ls-files src/ | grep Screen | xargs grep -wL Screen
    - src/chat/ChatScreen.js
    We have custom handeling here, see ComposeBox and ChatScreen.
    
    - src/lightbox/LightboxScreen.js
    This change will take care for this as well.
    
    - src/main/MainScreenWithTabs.js
    `TabBarBottom` is taking care for this
    see https://github.com/react-navigation/react-navigation/blob/v1.5.11/src/views/TabView/TabBarBottom.js#L234
    
    - src/start/CompatibilityScreen.js
    - src/start/LoadingScreen.js
    For both the above screens, content is very less and aligned vertically
     center, so no scope for content touching top/bottom.
    
    All This custom handeling is dependent on third party
    dependency to calculate offsets. Now RN also comes up with this new
    component `SafeAreaView`. Plan is to use it instead of handeling
    manually across the app, and remove external dependency.
    
    This issue of safe area is only for iOS, becuase by defualt it allows us
    to paint whole screen including notch area and bottom.
    
    On Android by defualt we are not allowed to paint statusbar (notch area)
    so this is not a issue there.
    
    I have tested on Android by simulating all types of display cutout
    present in the developer option, and works fine even without this
    change.
    
    References: https://facebook.github.io/react-native/docs/safeareaview
    https://developer.android.com/guide/topics/display-cutout#default_behavior
    
    Fixes: #3291

--- src/lightbox/LightboxScreen.js
+++ src/lightbox/LightboxScreen.js
@@ -1,6 +1,6 @@
 /* @flow strict-local */
 import React, { PureComponent } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import type { NavigationScreenProp } from 'react-navigation';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
@@ -25,12 +25,12 @@ export default class LightboxScreen extends PureComponent<Props> {
   render() {
     const { src, message } = this.props.navigation.state.params;
     return (
-      <View style={styles.screen}>
+      <SafeAreaView style={styles.screen}>
         <ZulipStatusBar hidden backgroundColor="black" />
         <ActionSheetProvider>
           <Lightbox src={src} message={message} />
         </ActionSheetProvider>
-      </View>
+      </SafeAreaView>
     );
   }
 }
