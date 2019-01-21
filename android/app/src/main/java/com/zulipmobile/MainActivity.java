8843fe36edd68ca2f5846b280cdda36d002d6ca0
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2019-01-22 00:43:42 +0530

    android, mainActivity: Put share/implicit intent data in `AppSession`.
    part of #117

--- android/app/src/main/java/com/zulipmobile/MainActivity.java
+++ android/app/src/main/java/com/zulipmobile/MainActivity.java
@@ -2,10 +2,19 @@ package com.zulipmobile;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // check for data in Implicit Intent
+        // https://developer.android.com/guide/components/intents-filters#Receiving
+        checkForImplicitIntentData();
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -22,5 +31,26 @@ public class MainActivity extends ReactActivity {
         Intent intent = new Intent("onConfigurationChanged");
         intent.putExtra("newConfig", newConfig);
         this.sendBroadcast(intent);
+    }
+
+    private void checkForImplicitIntentData() {
+        String receivedAction = getIntent().getAction();
+        String receivedType = getIntent().getType();
+
+        if (receivedAction == null || !receivedAction.equals(Intent.ACTION_SEND)
+                || receivedType == null) {
+            return;
+        }
+
+        // check data type
+        if (receivedType.startsWith("text/")) {
+            String text = getIntent()
+                    .getStringExtra(Intent.EXTRA_TEXT);
+            if (text != null) {
+                AppSession appSession = AppSession.getAppSession();
+                appSession.setShareDataType("text");
+                appSession.setShareData(text);
+            }
+        }
     }
 }
