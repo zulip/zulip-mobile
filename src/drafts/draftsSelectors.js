c93b320d65e5c6840f0b2d3f3212c18999c9b88e
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2019-01-22 19:54:55 +0530

    selector: Create `getDefaultTextForComposeBox`.
    This selector will return the text which needs to be set to the
    compose box just after narrowing like draft, share data.
    
    //TODO add tests.

--- src/drafts/draftsSelectors.js
+++ src/drafts/draftsSelectors.js
@@ -1,8 +1,20 @@
 /* @flow strict-local */
 import { createSelector } from 'reselect';
 
-import { getDrafts } from '../directSelectors';
-import type { Narrow, Selector } from '../types';
+import { getDrafts, getShareData } from '../directSelectors';
+import type { Narrow, Selector, ShareDataType } from '../types';
 
 export const getDraftForActiveNarrow = (narrow: Narrow): Selector<string> =>
   createSelector(getDrafts, drafts => drafts[JSON.stringify(narrow)] || '');
+
+export const getDefaultTextForComposeBox = (narrow: Narrow): Selector<string> =>
+  createSelector(
+    getShareData,
+    getDraftForActiveNarrow(narrow),
+    (shareData: ?ShareDataType, draft: string) => {
+      if (shareData && shareData.type === 'text') {
+        return shareData.data;
+      }
+      return draft;
+    },
+  );
