c619962dfa07172375f45489bf09d7a0cf39417e
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2019-01-27 17:47:13 +0530

    safariView: Replace `\` with `/`.
    While opening links consisting of `\`, `SafariView` was throwing
    error. So replace it with `/`.
    
    Fixes: #3315

--- src/utils/openLink.js
+++ src/utils/openLink.js
@@ -4,6 +4,7 @@ import SafariView from 'react-native-safari-view';
 
 export default (url: string): void => {
   if (Platform.OS === 'ios') {
+    url.replace(/\\/g, '/'); // Fix for #3315
     SafariView.show({ url });
   } else {
     NativeModules.CustomTabsAndroid.openURL(url);
