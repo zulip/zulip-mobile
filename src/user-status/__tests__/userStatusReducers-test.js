5d9512712ab2ccdc26ffdc7250f2f260f15325f5
Boris Yankov <borisyankov@gmail.com>
2019-01-31 04:06:31 +0200

    user status: Handle 'EVENT_USER_STATUS_UPDATE' action in reducer.
    When a `EVENT_USER_STATUS_UPDATE` action is dispatched, handle it
    by updating the relevant user key or add a new key.
    
    Currently the web app does update the `away` value separately from
    the `status_text` string and that is the immediate plan for mobile
    too. It is possible though that both values are changed at once.
    
     * if an event contains `away: false` this has the meaning of 'clear
       away overriding'
     * if an event contains `status_text: ''` this means 'clear status text'
    
    There is value in keeping our internal state simpler and consistent
    with what the back-end gives us as values. To achieve this we make
    sure if we get `away: false` and `status_text: ''` we remove the
    corresponding keys.

--- src/user-status/__tests__/userStatusReducers-test.js
+++ src/user-status/__tests__/userStatusReducers-test.js
@@ -1,6 +1,6 @@
 import deepFreeze from 'deep-freeze';
 
-import { ACCOUNT_SWITCH, REALM_INIT } from '../../actionConstants';
+import { ACCOUNT_SWITCH, REALM_INIT, EVENT_USER_STATUS_UPDATE } from '../../actionConstants';
 import userStatusReducers from '../userStatusReducers';
 
 describe('userStatusReducers', () => {
@@ -48,13 +48,144 @@ describe('userStatusReducers', () => {
       expect(actualState).toEqual(testUserStatusData);
     });
 
-    test('handles older back-ends that do not have `user_status` by resetting the state', () => {
+    test('handles older back-ends that do not have `user_status` data by resetting the state', () => {
       const initialState = deepFreeze(testUserStatusData);
       const action = deepFreeze({
         type: REALM_INIT,
         data: {},
       });
       const expectedState = {};
+
+      const actualState = userStatusReducers(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+  });
+
+  describe('EVENT_USER_STATUS_UPDATE', () => {
+    test('when the user does not already have entry add a key by their `user_id`', () => {
+      const initialState = deepFreeze({
+        2: {
+          away: true,
+        },
+      });
+      const action = {
+        type: EVENT_USER_STATUS_UPDATE,
+        user_id: 1,
+        away: true,
+      };
+      const expectedState = {
+        1: {
+          away: true,
+        },
+        2: {
+          away: true,
+        },
+      };
+
+      const actualState = userStatusReducers(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+
+    test('if the user already has user status stored update their key', () => {
+      const initialState = deepFreeze({
+        1: {
+          away: false,
+        },
+      });
+      const action = deepFreeze({
+        type: EVENT_USER_STATUS_UPDATE,
+        user_id: 1,
+        away: true,
+      });
+      const expectedState = {
+        1: {
+          away: true,
+        },
+      };
+
+      const actualState = userStatusReducers(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+
+    test('when the user_ status text is updated this is reflected in the state', () => {
+      const initialState = deepFreeze({});
+      const action = deepFreeze({
+        type: EVENT_USER_STATUS_UPDATE,
+        user_id: 1,
+        status_text: 'Hello, world!',
+      });
+      const expectedState = {
+        1: {
+          status_text: 'Hello, world!',
+        },
+      };
+
+      const actualState = userStatusReducers(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+
+    test('both `away` and `status_text` values can be updated in one event', () => {
+      const initialState = deepFreeze({
+        1: {
+          away: false,
+        },
+      });
+      const action = deepFreeze({
+        type: EVENT_USER_STATUS_UPDATE,
+        user_id: 1,
+        away: true,
+        status_text: 'Hello, world!',
+      });
+      const expectedState = {
+        1: {
+          away: true,
+          status_text: 'Hello, world!',
+        },
+      };
+
+      const actualState = userStatusReducers(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+
+    test('if `away` status is removed delete the key from the object', () => {
+      const initialState = deepFreeze({
+        1: {
+          away: true,
+        },
+      });
+      const action = deepFreeze({
+        type: EVENT_USER_STATUS_UPDATE,
+        user_id: 1,
+        away: false,
+      });
+      const expectedState = {
+        1: {},
+      };
+
+      const actualState = userStatusReducers(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+
+    test('if `status_text` is removed delete the key from the object', () => {
+      const initialState = deepFreeze({
+        1: {
+          status_text: 'Hello, world!',
+        },
+      });
+      const action = deepFreeze({
+        type: EVENT_USER_STATUS_UPDATE,
+        user_id: 1,
+        status_text: '',
+      });
+      const expectedState = {
+        1: {},
+      };
 
       const actualState = userStatusReducers(initialState, action);
 
--- src/user-status/userStatusReducers.js
+++ src/user-status/userStatusReducers.js
@@ -1,6 +1,12 @@
 /* @flow strict-local */
 import type { UserStatusState, Action } from '../types';
-import { LOGOUT, LOGIN_SUCCESS, ACCOUNT_SWITCH, REALM_INIT } from '../actionConstants';
+import {
+  LOGOUT,
+  LOGIN_SUCCESS,
+  ACCOUNT_SWITCH,
+  REALM_INIT,
+  EVENT_USER_STATUS_UPDATE,
+} from '../actionConstants';
 import { NULL_OBJECT } from '../nullObjects';
 
 const initialState: UserStatusState = NULL_OBJECT;
@@ -14,6 +20,28 @@ export default (state: UserStatusState = initialState, action: Action): UserStat
 
     case REALM_INIT:
       return action.data.user_status || initialState;
+
+    case EVENT_USER_STATUS_UPDATE: {
+      const newUserStatus = { ...state[action.user_id] };
+      if (action.away !== undefined) {
+        if (action.away === true) {
+          newUserStatus.away = action.away;
+        } else {
+          delete newUserStatus.away;
+        }
+      }
+      if (action.status_text !== undefined) {
+        if (action.status_text.length > 0) {
+          newUserStatus.status_text = action.status_text;
+        } else {
+          delete newUserStatus.status_text;
+        }
+      }
+      return {
+        ...state,
+        [action.user_id]: newUserStatus,
+      };
+    }
 
     default:
       return state;
