4e30f737bfc3de2699df59e551de0c5d22fcf66d
Greg Price <greg@zulipchat.com>
2019-02-01 14:26:02 -0800

    Avatar: Handle optionality of onPress more idiomatically.
    We already made this change for `ImageAvatar` in 6ac0bf2b6.

--- src/common/Avatar.js
+++ src/common/Avatar.js
@@ -5,7 +5,6 @@ import React, { PureComponent } from 'react';
 import { StyleSheet } from 'react-native';
 
 import type { GlobalState, UserPresence } from '../types';
-import { nullFunction } from '../nullObjects';
 import { getCurrentRealm } from '../selectors';
 import ImageAvatar from './ImageAvatar';
 import TextAvatar from './TextAvatar';
@@ -52,7 +51,6 @@ class Avatar extends PureComponent<Props> {
     size: 32,
     realm: '',
     shape: 'rounded',
-    onPress: nullFunction,
   };
 
   render() {
