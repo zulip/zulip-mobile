2f5ec763792b2992857b5b779008d1df8c9314d8
Greg Price <greg@zulipchat.com>
2019-02-15 19:13:03 -0800

    ComposeMenu: Clarify a vague type, and mark strict-local.
    Still not entirely clear, but at least the unclarity is localized to
    this particular function.
    
    The change in the function's body is a pure refactor, done to allow
    the fixme to be isolated to the sketchy falsiness check and not some
    other type issue.

--- src/compose/ComposeMenu.js
+++ src/compose/ComposeMenu.js
@@ -1,4 +1,4 @@
-/* @flow */
+/* @flow strict-local */
 import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 // $FlowFixMe
@@ -47,13 +47,22 @@ export const chooseUploadImageFilename = (uri: string, fileName: string): string
 };
 
 class ComposeMenu extends PureComponent<Props> {
-  handleImagePickerResponse = (response: Object) => {
+  handleImagePickerResponse = (response: {
+    didCancel: boolean,
+    // Upstream docs are vague:
+    // https://github.com/react-native-community/react-native-image-picker/blob/master/docs/Reference.md
+    error?: string | void | null | false,
+    uri: string,
+    fileName: string,
+  }) => {
     if (response.didCancel) {
       return;
     }
 
-    if (response.error) {
-      showErrorAlert(response.error, 'Error');
+    // $FlowFixMe sketchy falsiness check, because upstream API is unclear
+    const error: string | null = response.error || null;
+    if (error !== null) {
+      showErrorAlert(error, 'Error');
       return;
     }
 
