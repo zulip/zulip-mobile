aec2ff255c64f4eedd11ad2c6819f9c8ef570081
Boris Yankov <borisyankov@gmail.com>
2019-02-22 18:37:41 +0200

    avatar: Use `getMediumAvatar` if larger avatar needed.
    This change takes into account the size passed to the `getAvatarFrom*`
    functions.  If a size larger than `100` is needed, use the 'medium'
    avatar.
    
    We should stop using `getMediumAvatar` directly from now on.

--- src/utils/avatar.js
+++ src/utils/avatar.js
@@ -4,6 +4,15 @@ import md5 from 'blueimp-md5';
 import type { Message, Outbox, User } from '../types';
 import { getFullUrl } from './url';
 
+/**
+ * When selecting the size of a gravatar we can pick any arbitrary
+ * size we want. For server-uploaded avatars this is not the case.
+ * We have only:
+ *  * default - image is 100x100
+ *  * medium - image is 500x500
+ *
+ * This function converts a normal avatar to medium-sized one.
+ */
 export const getMediumAvatar = (avatarUrl: string): string => {
   const matches = new RegExp(/(\w+)\.png/g).exec(avatarUrl);
 
@@ -18,8 +27,15 @@ export const getAvatarUrl = (
   email: string,
   realm: string,
   size: number = 80,
-): string =>
-  typeof avatarUrl === 'string' ? getFullUrl(avatarUrl, realm) : getGravatarFromEmail(email, size);
+): string => {
+  if (typeof avatarUrl !== 'string') {
+    return getGravatarFromEmail(email, size);
+  }
+
+  const fullUrl = getFullUrl(avatarUrl, realm);
+
+  return size > 100 ? getMediumAvatar(fullUrl) : fullUrl;
+};
 
 export const getAvatarFromUser = (user: User, realm: string, size?: number): string =>
   getAvatarUrl(user.avatar_url, user.email, realm, size);
