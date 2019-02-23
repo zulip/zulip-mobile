zulip/zulip-mobile

commit ef209dbb8c9399a9250ea51f496faefa9905aed6

avatar: Fix sketchy null-checks on `avatar_url`.
Flow points these out when we try marking these files as strict-local.

src/account-info/AccountDetails.js
@@ -1,4 +1,4 @@
-/* @flow */
+/* @flow strict-local */
 import React, { PureComponent } from 'react';
 import { View, Dimensions } from 'react-native';
 
@@ -39,7 +39,7 @@ export default class AccountDetails extends PureComponent<Props, void> {
     return (
       <View>
         <Avatar
-          avatarUrl={user.avatar_url ? getMediumAvatar(user.avatar_url) : null}
+          avatarUrl={typeof user.avatar_url === 'string' ? getMediumAvatar(user.avatar_url) : null}
           name={user.full_name}
           email={user.email}
           size={screenWidth}

src/common/Avatar.js
@@ -1,4 +1,4 @@
-/* @flow */
+/* @flow strict-local */
 import { connect } from 'react-redux';
 
 import React, { PureComponent } from 'react';
@@ -57,7 +57,8 @@ class Avatar extends PureComponent<Props> {
 
   render() {
     const { avatarUrl, email, name, size, presence, onPress, realm, shape } = this.props;
-    const fullAvatarUrl = avatarUrl ? getFullUrl(avatarUrl, realm) : getGravatarFromEmail(email);
+    const fullAvatarUrl =
+      typeof avatarUrl === 'string' ? getFullUrl(avatarUrl, realm) : getGravatarFromEmail(email);
     const AvatarComponent = fullAvatarUrl ? ImageAvatar : TextAvatar;
 
     return (

src/webview/html/messageTypingAsHtml.js
@@ -1,4 +1,4 @@
-/* @flow */
+/* @flow strict-local */
 import template from './template';
 import type { User } from '../../types';
 import { getFullUrl } from '../../utils/url';
@@ -10,7 +10,9 @@ const typingAvatar = (realm: string, user: User): string => template`
     class="avatar-img"
     data-email="${user.email}"
     src="${
-      user.avatar_url ? getFullUrl(user.avatar_url, realm) : getGravatarFromEmail(user.email)
+      typeof user.avatar_url === 'string'
+        ? getFullUrl(user.avatar_url, realm)
+        : getGravatarFromEmail(user.email)
     }">
 </div>
 `;

commit 3dab340cf8b457aa597777457ca76c676a7e3bd5

Avatar: Do not use `TextAvatar`, and simplify.
`TextAvatar` was used as a fallback if we can not find an avatar
for the user.

That was possibly effective very long time ago but right now we
always use as a fallback the gravatar of a user or the auto-generated
gravatar shape (there is always one!).

Changes in the recent commit ef209dbb8 inadvertently changed the
avatarUrl checks for '' into `avatarUrl === 'string'`, which is
never true.

This makes our use of `TextAvatar` not necessary (and a mess!)

We also don't need the `name` prop, so it is removed from the
component and all components that use `Avatar`.

src/account-info/AccountDetails.js
@@ -33,7 +33,6 @@ export default class AccountDetails extends PureComponent<Props, void> {
       <View>
         <Avatar
           avatarUrl={typeof user.avatar_url === 'string' ? getMediumAvatar(user.avatar_url) : null}
-          name={user.full_name}
           email={user.email}
           size={screenWidth}
           shape="square"

src/common/Avatar.js
@@ -7,7 +7,6 @@ import { StyleSheet } from 'react-native';
 import type { GlobalState } from '../types';
 import { getCurrentRealm } from '../selectors';
 import ImageAvatar from './ImageAvatar';
-import TextAvatar from './TextAvatar';
 import { getFullUrl } from '../utils/url';
 import { getGravatarFromEmail } from '../utils/avatar';
 import PresenceStatusIndicator from './PresenceStatusIndicator';
@@ -23,7 +22,6 @@ const componentStyles = StyleSheet.create({
 type Props = {|
   avatarUrl: ?string,
   email: string,
-  name: string,
   size: number,
   realm: string,
   shape: 'square' | 'rounded' | 'circle',
@@ -35,7 +33,6 @@ type Props = {|
  *
  * @prop [avatarUrl] - Absolute or relative url to an avatar image.
  * @prop [email] - User's' email address, to calculate Gravatar URL if not given `avatarUrl`.
- * @prop [name] - User's full name.
  * @prop [size] - Sets width and height in pixels.
  * @prop [realm] - Current realm url, used if avatarUrl is relative.
  * @prop [shape] - One of 'square', 'rounded', 'circle'.
@@ -45,28 +42,20 @@ class Avatar extends PureComponent<Props> {
   static defaultProps = {
     avatarUrl: '',
     email: '',
-    name: '',
     size: 32,
     realm: '',
     shape: 'rounded',
   };
 
   render() {
-    const { avatarUrl, email, name, size, onPress, realm, shape } = this.props;
+    const { avatarUrl, email, size, onPress, realm, shape } = this.props;
     const fullAvatarUrl =
       typeof avatarUrl === 'string' ? getFullUrl(avatarUrl, realm) : getGravatarFromEmail(email);
-    const AvatarComponent = fullAvatarUrl ? ImageAvatar : TextAvatar;
 
     return (
-      <AvatarComponent
-        name={name}
-        avatarUrl={fullAvatarUrl}
-        size={size}
-        onPress={onPress}
-        shape={shape}
-      >
+      <ImageAvatar avatarUrl={fullAvatarUrl} size={size} onPress={onPress} shape={shape}>
         <PresenceStatusIndicator style={componentStyles.status} email={email} hideIfOffline />
-      </AvatarComponent>
+      </ImageAvatar>
     );
   }
 }

src/title/TitleGroup.js
@@ -21,12 +21,7 @@ class TitleGroup extends PureComponent<Props> {
       <View style={styles.navWrapper}>
         {recipients.map((user, index) => (
           <View key={user.email} style={styles.titleAvatar}>
-            <Avatar
-              size={32}
-              name={user.full_name}
-              avatarUrl={user.avatar_url}
-              email={user.email}
-            />
+            <Avatar size={32} avatarUrl={user.avatar_url} email={user.email} />
           </View>
         ))}
       </View>

src/title/TitlePrivate.js
@@ -22,7 +22,7 @@ class TitlePrivate extends PureComponent<Props> {
     const avatarUrl: string | void | null = user.avatar_url;
     return (
       <View style={styles.navWrapper}>
-        <Avatar size={32} name={user.full_name} email={user.email} avatarUrl={avatarUrl} />
+        <Avatar size={32} email={user.email} avatarUrl={avatarUrl} />
         <ViewPlaceholder width={8} />
         <View>
           <Text style={[styles.navTitle, { color }]} numberOfLines={1} ellipsizeMode="tail">

src/user-picker/AvatarItem.js
@@ -74,7 +74,6 @@ export default class AvatarItem extends PureComponent<Props> {
             size={50}
             avatarUrl={avatarUrl}
             email={email}
-            name={fullName}
             onPress={this.handlePress}
           />
         </ComponentWithOverlay>

src/users/UserItem.js
@@ -53,13 +53,7 @@ export default class UserItem extends PureComponent<Props> {
     return (
       <Touchable onPress={this.handlePress}>
         <View style={[styles.listItem, isSelected && componentStyles.selectedRow]}>
-          <Avatar
-            size={32}
-            avatarUrl={avatarUrl}
-            email={email}
-            name={fullName}
-            onPress={this.handlePress}
-          />
+          <Avatar size={32} avatarUrl={avatarUrl} email={email} onPress={this.handlePress} />
           <View style={componentStyles.textWrapper}>
             <RawLabel
               style={[componentStyles.text, isSelected && componentStyles.selectedText]}