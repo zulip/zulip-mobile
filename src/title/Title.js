7a4c9408dd7b3b9bb76cd7eb1ccfaece50d60999
Greg Price <greg@zulipchat.com>
2019-02-28 17:50:11 -0800

    title: Simplify out single-prop `...props` in Title.
    Not sure why I wrote this indirection in the first place.  Perhaps
    left over from a previous iteration of the change that introduced it?

--- src/title/Title.js
+++ src/title/Title.js
@@ -22,19 +22,18 @@ type Props = {|
 class Title extends PureComponent<Props> {
   render() {
     const { narrow, color, editMessage } = this.props;
-    const props = { color };
     if (editMessage != null) {
-      return <TitlePlain text="Edit message" {...props} />;
+      return <TitlePlain text="Edit message" color={color} />;
     }
     return caseNarrow(narrow, {
       home: () => <TitleSpecial code="home" color={color} />,
-      starred: () => <TitleSpecial code="starred" {...props} />,
-      mentioned: () => <TitleSpecial code="mentioned" {...props} />,
-      allPrivate: () => <TitleSpecial code="private" {...props} />,
-      stream: () => <TitleStream narrow={narrow} {...props} />,
-      topic: () => <TitleStream narrow={narrow} {...props} />,
-      pm: email => <TitlePrivate email={email} {...props} />,
-      groupPm: () => <TitleGroup narrow={narrow} {...props} />,
+      starred: () => <TitleSpecial code="starred" color={color} />,
+      mentioned: () => <TitleSpecial code="mentioned" color={color} />,
+      allPrivate: () => <TitleSpecial code="private" color={color} />,
+      stream: () => <TitleStream narrow={narrow} color={color} />,
+      topic: () => <TitleStream narrow={narrow} color={color} />,
+      pm: email => <TitlePrivate email={email} color={color} />,
+      groupPm: () => <TitleGroup narrow={narrow} color={color} />,
       search: () => null,
     });
   }
