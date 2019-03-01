9b6b37642fc08c8d350a0e4583cd4d4bcf9bf4bb
Greg Price <greg@zulipchat.com>
2019-03-01 12:21:13 -0800

    api types: Pull "structure of a request" types into own file.
    This will help us make explicit some distinction between layers:
     * here, the basic structure of a request and response
     * then basic vocabulary: user, stream, message, etc.
     * then the unending catalog of specific routes, events, etc.

--- src/api/apiTypes.js
+++ src/api/apiTypes.js
@@ -1,5 +1,6 @@
 /* @flow strict-local */
 
+export type * from './transportTypes';
 export type * from './eventTypes';
 export type * from './initialDataTypes';
 
@@ -7,12 +8,6 @@ export type {
   AuthenticationMethods,
   ApiResponseServerSettings,
 } from './settings/getServerSettings';
-
-export type Auth = {|
-  realm: string,
-  apiKey: string,
-  email: string,
-|};
 
 export type DevUser = {|
   realm_uri: string,
@@ -295,38 +290,4 @@ export type UserStatus = {|
 
 export type UserStatusMapObject = {|
   [userId: number]: UserStatus,
-|};
-
-export type ApiResponse = {|
-  result: string,
-  msg: string,
-|};
-
-export type ApiResponseSuccess = {|
-  result: 'success',
-  msg: '',
-|};
-
-/** List of error codes at https://github.com/zulip/zulip/blob/master/zerver/lib/exceptions.py */
-
-export type ApiErrorCode =
-  | 'BAD_REQUEST'
-  | 'REQUEST_VARIABLE_MISSING'
-  | 'REQUEST_VARIABLE_INVALID'
-  | 'BAD_IMAGE'
-  | 'REALM_UPLOAD_QUOTA'
-  | 'BAD_NARROW'
-  | 'MISSING_HTTP_EVENT_HEADER'
-  | 'STREAM_DOES_NOT_EXIST'
-  | 'UNAUTHORIZED_PRINCIPAL'
-  | 'UNEXPECTED_WEBHOOK_EVENT_TYPE'
-  | 'BAD_EVENT_QUEUE_ID'
-  | 'CSRF_FAILED'
-  | 'INVITATION_FAILED'
-  | 'INVALID_ZULIP_SERVER';
-
-export type ApiResponseError = {|
-  code?: ApiErrorCode,
-  msg: string,
-  result: 'error',
 |};
---
+++ src/api/transportTypes.js
@@ -0,0 +1,40 @@
+/* @flow strict-local */
+
+export type Auth = {|
+  realm: string,
+  apiKey: string,
+  email: string,
+|};
+
+export type ApiResponse = {|
+  result: string,
+  msg: string,
+|};
+
+export type ApiResponseSuccess = {|
+  result: 'success',
+  msg: '',
+|};
+
+/** List of error codes at https://github.com/zulip/zulip/blob/master/zerver/lib/exceptions.py */
+export type ApiErrorCode =
+  | 'BAD_REQUEST'
+  | 'REQUEST_VARIABLE_MISSING'
+  | 'REQUEST_VARIABLE_INVALID'
+  | 'BAD_IMAGE'
+  | 'REALM_UPLOAD_QUOTA'
+  | 'BAD_NARROW'
+  | 'MISSING_HTTP_EVENT_HEADER'
+  | 'STREAM_DOES_NOT_EXIST'
+  | 'UNAUTHORIZED_PRINCIPAL'
+  | 'UNEXPECTED_WEBHOOK_EVENT_TYPE'
+  | 'BAD_EVENT_QUEUE_ID'
+  | 'CSRF_FAILED'
+  | 'INVITATION_FAILED'
+  | 'INVALID_ZULIP_SERVER';
+
+export type ApiResponseError = {|
+  code?: ApiErrorCode,
+  msg: string,
+  result: 'error',
+|};
