1f4ca99f372cbde9b0195df07983cb553f782c58
Greg Price <greg@zulipchat.com>
2019-03-01 12:08:05 -0800

    api types: Move response type for `server_settings` to that file.
    Much like the previous two commits.
    
    This also looks not quite right -- there's surely the usual
    `ApiResponseSuccess` properties in there.  Will fix that separately.

--- src/api/apiTypes.js
+++ src/api/apiTypes.js
@@ -3,6 +3,8 @@
 export type * from './eventTypes';
 export type * from './initialDataTypes';
 
+export type { AuthenticationMethods, ApiServerSettings } from './settings/getServerSettings';
+
 export type Auth = {|
   realm: string,
   apiKey: string,
@@ -324,26 +326,4 @@ export type ApiResponseError = {|
   code?: ApiErrorCode,
   msg: string,
   result: 'error',
-|};
-
-export type AuthenticationMethods = {|
-  dev: boolean,
-  github: boolean,
-  google: boolean,
-  ldap: boolean,
-  password: boolean,
-  remoteuser: boolean,
-|};
-
-export type ApiServerSettings = {|
-  authentication_methods: AuthenticationMethods,
-  email_auth_enabled: boolean,
-  msg: string,
-  push_notifications_enabled: boolean,
-  realm_description: string,
-  realm_icon: string,
-  realm_name: string,
-  realm_uri: string,
-  require_email_format_usernames: boolean,
-  zulip_version: string,
 |};
--- src/api/settings/getServerSettings.js
+++ src/api/settings/getServerSettings.js
@@ -1,6 +1,27 @@
 /* @flow strict-local */
-import type { ApiServerSettings } from '../apiTypes';
 import { apiGet } from '../apiFetch';
+
+export type AuthenticationMethods = {|
+  dev: boolean,
+  github: boolean,
+  google: boolean,
+  ldap: boolean,
+  password: boolean,
+  remoteuser: boolean,
+|};
+
+export type ApiServerSettings = {|
+  authentication_methods: AuthenticationMethods,
+  email_auth_enabled: boolean,
+  msg: string,
+  push_notifications_enabled: boolean,
+  realm_description: string,
+  realm_icon: string,
+  realm_name: string,
+  realm_uri: string,
+  require_email_format_usernames: boolean,
+  zulip_version: string,
+|};
 
 /** See https://zulipchat.com/api/server-settings */
 export default async (realm: string): Promise<ApiServerSettings> =>
