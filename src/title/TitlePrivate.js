zulip/zulip-mobile

commit 8f99a62220a16182a89896548cddb93356e138e7

nav: Make a user's avatar/name in PM app bar lead to their profile.
This is a natural thing to try when looking for another user's
profile/account-details screen, and seen in other messaging/chat apps.

src/title/TitlePrivate.js
@@ -4,24 +4,31 @@ import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import { Text, View } from 'react-native';
 
-import type { RealmBot, User } from '../types';
-import { UserAvatarWithPresence, ViewPlaceholder } from '../common';
+import type { Dispatch, RealmBot, User } from '../types';
+import { Touchable, UserAvatarWithPresence, ViewPlaceholder } from '../common';
 import ActivityText from './ActivityText';
 import { getAllUsersByEmail } from '../users/userSelectors';
 import styles from '../styles';
+import { navigateToAccountDetails } from '../nav/navActions';
 
 type Props = {
+  dispatch: Dispatch,
   user: User | RealmBot,
   color: string,
 };
 
 class TitlePrivate extends PureComponent<Props> {
+  handlePress = () => {
+    const { dispatch, user } = this.props;
+    dispatch(navigateToAccountDetails(user.email));
+  };
+
   render() {
     const { user, color } = this.props;
     // $FlowFixMe: sort out RealmBot
     const avatarUrl: string | null = user.avatar_url;
     return (
-      <View style={styles.navWrapper}>
+      <Touchable onPress={this.handlePress} style={styles.navWrapper}>
         <UserAvatarWithPresence size={32} email={user.email} avatarUrl={avatarUrl} />
         <ViewPlaceholder width={8} />
         <View>
@@ -30,7 +37,7 @@ class TitlePrivate extends PureComponent<Props> {
           </Text>
           <ActivityText style={styles.navSubtitle} color={color} email={user.email} />
         </View>
-      </View>
+      </Touchable>
     );
   }
 }

commit 73c72e09f9927bf5748a84fc416553a08c3d9343

titlePrivate: Apply `flexDireaction: 'row'` to view inside Touchable.
`Touchable` can only contain one view inside it, so applying this row
style to it doesn't make any sense. So layout issue was introduced in
8f99a62, where we replaced `View` with `Touchable`.

Now create a new view inside it and apply style to it. Other styles of
`navWrapper` are now taken care by `navContent` (which got added in
previous commit).

src/title/TitlePrivate.js
@@ -28,14 +28,16 @@ class TitlePrivate extends PureComponent<Props> {
     // $FlowFixMe: sort out CrossRealmBot
     const avatarUrl: string | null = user.avatar_url;
     return (
-      <Touchable onPress={this.handlePress} style={styles.navWrapper}>
-        <UserAvatarWithPresence size={32} email={user.email} avatarUrl={avatarUrl} />
-        <ViewPlaceholder width={8} />
-        <View>
-          <Text style={[styles.navTitle, { color }]} numberOfLines={1} ellipsizeMode="tail">
-            {user.full_name}
-          </Text>
-          <ActivityText style={styles.navSubtitle} color={color} email={user.email} />
+      <Touchable onPress={this.handlePress}>
+        <View style={styles.row}>
+          <UserAvatarWithPresence size={32} email={user.email} avatarUrl={avatarUrl} />
+          <ViewPlaceholder width={8} />
+          <View>
+            <Text style={[styles.navTitle, { color }]} numberOfLines={1} ellipsizeMode="tail">
+              {user.full_name}
+            </Text>
+            <ActivityText style={styles.navSubtitle} color={color} email={user.email} />
+          </View>
         </View>
       </Touchable>
     );