06f702d8a86311c6f2838157da258e508d55e715
Boris Yankov <borisyankov@gmail.com>
2019-03-20 19:12:38 +0200

    Babel and Metro config files

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@
 //   https://babeljs.io/docs/en/config-files
 
 module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
   env: {
     production: {
       plugins: ['transform-remove-console'],
