ee90f508827047ee5abbff67600c3883a57ac323
Greg Price <greg@zulipchat.com>
2019-04-04 14:57:48 -0700

    i18n: Generate libdefs for messages files.
    This lets us mark the file where we import these as strict-local.

---
+++ flow-typed/translations.js
@@ -0,0 +1,141 @@
+/*
+ * Crudely @generated like so:
+ *   $ ls static/translations/messages_*.json | perl -lne '
+           print "\
+declare module '\''../../$_'\'' {
+  declare export default {| [string]: string |};
+}";
+         ' >>flow-typed/translations.js
+ */
+
+declare module '../../static/translations/messages_ar.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_bg.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ca.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_cs.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_da.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_de.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_en.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_es.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_fa.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_fi.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_fr.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_gl.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_hi.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_hr.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_hu.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_id_ID.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_it.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ja.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ko.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ml.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_nl.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_pl.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_pt.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ro.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ru.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_sr.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_sv.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_ta.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_tr.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_uk.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_uz.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_zh-Hans.json' {
+  declare export default {| [string]: string |};
+}
+
+declare module '../../static/translations/messages_zh-Hant.json' {
+  declare export default {| [string]: string |};
+}
