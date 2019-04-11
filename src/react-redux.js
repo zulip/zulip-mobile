fb3f71b4e38d2d6196d2f157f30fd99188a29184
Greg Price <greg@zulipchat.com>
2019-04-11 13:08:08 -0700

    connect: Add a "fixme" version, `connectFlowFixMe`.

--- src/react-redux.js
+++ src/react-redux.js
@@ -14,3 +14,19 @@ export const connect: <
 >(
   mapStateToProps?: (GlobalState, SP) => RSP,
 ) => C => ComponentType<CP & SP> = mapStateToProps => connectInner(mapStateToProps);
+
+/**
+ * DEPRECATED.  Don't add new uses; and PRs to remove existing uses are welcome.
+ *
+ * This is exactly like `connect` except with type-checking disabled.
+ * Any place we use it, it's because there's a type error there.
+ * To fix: change the call site to use `connect`; see what error Flow
+ * reports; and fix the error.
+ *
+ * (Backstory: for a long time `connect` had a type that partly defeated
+ * type-checking, so we accumulated a number of type errors that that hid.
+ * This untyped version lets us fix those errors one by one, while using the
+ * new, well-typed `connect` everywhere else.)
+ */
+export const connectFlowFixMe = (mapStateToProps: $FlowFixMe) => (c: $FlowFixMe) =>
+  connect(mapStateToProps)(c);
