b5890a6e88c08c829bce9df6c2f98456af3503e6
Boris Yankov <borisyankov@gmail.com>
2019-04-15 23:06:37 +0300

    store: Add 'diagnosticNicknameChange' action creator
    Adds a 'diagnosticNicknameChange' action creator that changes
    the 'diagnosticNickname' property of 'setttings' and then calls
    the Sentry utility function that sets the username field to be
    reported by Sentry.

--- src/settings/settingsActions.js
+++ src/settings/settingsActions.js
@@ -1,8 +1,17 @@
 /* @flow strict-local */
-import type { Action, SettingsState } from '../types';
+import type { Action, Dispatch, GetState, SettingsState } from '../types';
 import { SETTINGS_CHANGE } from '../actionConstants';
+import { sentrySetUsername } from '../sentry';
 
 export const settingsChange = (update: $Shape<SettingsState>): Action => ({
   type: SETTINGS_CHANGE,
   update,
 });
+
+export const diagnosticNicknameChange = (diagnosticNickname: string) => (
+  dispatch: Dispatch,
+  getState: GetState,
+) => {
+  dispatch(settingsChange({ diagnosticNickname }));
+  sentrySetUsername(diagnosticNickname);
+};
