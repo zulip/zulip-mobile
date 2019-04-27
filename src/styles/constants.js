3efc7f3faf29105f938a84bc0df58cce946da988
Paul-Sebastian Ungureanu <ungureanupaulsebastian@gmail.com>
2019-04-27 16:38:04 +0300

    input: Display red underline below input field in case of error.
    The red underline is available only on Android. It is visibile
    when "error" property is true.

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -7,4 +7,5 @@ export const BRAND_COLOR = 'rgba(82, 194, 175, 1)';
 export const BORDER_COLOR = BRAND_COLOR;
 export const HIGHLIGHT_COLOR = 'rgba(86, 164, 174, 0.5)';
 export const HALF_COLOR = 'rgba(127, 127, 127, 0.5)';
+export const ERROR_COLOR = 'rgba(176, 0, 32, 1)';
 export const QUARTER_COLOR = 'rgba(127, 127, 127, 0.25)';
