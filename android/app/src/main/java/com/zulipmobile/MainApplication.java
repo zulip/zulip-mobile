d9bbb5e5f047a6de17eef1870fbad17c8debf5f9
Isham Mahajan <mahajan.isham@gmail.com>
2019-05-21 18:40:47 +0530

    Add dependency react-native-document-picker, on Android.
    `react-native-document-picker` is a library used to pick documents
    from the device memory or the cloud services connected to the device.
    This commit adds the dependency for this package, and links it.
    
    Based on the guide at:
      https://github.com/Elyx0/react-native-document-picker/issues/94
    
    [greg: cut Xcode config; ran `yarn update-libdefs` to pick up
     an autogenerated libdef in flow-typed/.]

--- android/app/src/main/java/com/zulipmobile/MainApplication.java
+++ android/app/src/main/java/com/zulipmobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.zulipmobile;
 import android.app.Application;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactApplication;
+import io.github.elyx0.reactnativedocumentpicker.DocumentPickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.asList(
                     new MainReactPackage(),
+                    new DocumentPickerPackage(),
                     new RNCWebViewPackage(),
                     new RNTextInputResetPackage(),
                     new ImagePickerPackage(),
