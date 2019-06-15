381d7788570ca4d13d35a34122f9eaf0eb5a1124
Greg Price <greg@zulipchat.com>
2019-06-14 19:17:55 -0700

    convert more, directly

--- src/utils/__tests__/message-test.js
+++ src/utils/__tests__/message-test.js
@@ -46,57 +46,39 @@ describe('shouldBeMuted', () => {
 
 describe('isMessageRead', () => {
   test('message with no flags entry is considered not read', () => {
-    const message = { id: 0, display_recipient: 'testStream', type: 'stream' };
-    const flags = { read: {} };
-    const subscriptions = [{ name: 'testStream', in_home_view: true }];
+    const message = eg.streamMessage();
+    const flags = eg.baseReduxState.flags;
+    const subscriptions = [eg.subscription];
 
-    const result = isMessageRead(message, flags, subscriptions);
+    const result = isMessageRead(message, flags, subscriptions, []);
 
     expect(result).toEqual(false);
   });
 
   test('message with flags entry is considered read', () => {
-    const message = { id: 123 };
-    const flags = { read: { 123: true } };
+    const message = eg.streamMessage();
+    const flags = { ...eg.baseReduxState.flags, read: { [123]: true } }; // eslint-disable-line no-useless-computed-key
 
-    const result = isMessageRead(message, flags);
+    const result = isMessageRead(message, flags, [], []);
 
     expect(result).toEqual(true);
   });
 
   test('a message in a muted stream is considered read', () => {
-    const message = {
-      id: 0,
-      display_recipient: 'muted stream',
-      subject: 'topic',
-    };
-    const flags = { read: {} };
-    const subscriptions = [
-      {
-        name: 'muted stream',
-        in_home_view: false,
-      },
-    ];
+    const message = eg.streamMessage();
+    const flags = eg.baseReduxState.flags;
+    const subscriptions = [{ ...eg.subscription, in_home_view: false }];
 
-    const result = isMessageRead(message, flags, subscriptions);
+    const result = isMessageRead(message, flags, subscriptions, []);
 
     expect(result).toEqual(true);
   });
 
   test('a message in a muted topic is considered read', () => {
-    const message = {
-      id: 0,
-      display_recipient: 'stream',
-      subject: 'muted topic',
-    };
-    const flags = { read: {} };
-    const subscriptions = [
-      {
-        name: 'stream',
-        in_home_view: false,
-      },
-    ];
-    const mute = [['stream', 'muted topic']];
+    const message = eg.streamMessage();
+    const flags = eg.baseReduxState.flags;
+    const subscriptions = [eg.subscription];
+    const mute = [[eg.stream.name, message.subject]];
 
     const result = isMessageRead(message, flags, subscriptions, mute);
 
