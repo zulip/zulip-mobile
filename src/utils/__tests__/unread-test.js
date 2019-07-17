dd31d12ffddcbaadf02f585758c0b583d7c6941e
Greg Price <greg@zulipchat.com>
2019-07-16 19:18:47 -0700

    unread: Cut obsolete `countUnread`.
    In the parent commit, we removed the one call site this had.
    It's just as obsolete as that call site was.  Cut it out.

--- src/utils/__tests__/unread-test.js
+++ src/utils/__tests__/unread-test.js
@@ -1,4 +1,4 @@
-import { countUnread, filterUnreadMessageIds, filterUnreadMessagesInRange } from '../unread';
+import { filterUnreadMessageIds, filterUnreadMessagesInRange } from '../unread';
 
 describe('filterUnreadMessageIds', () => {
   test('empty message list has no unread messages', () => {
@@ -99,64 +99,5 @@ describe('filterUnreadMessagesInRange', () => {
     const actualUnread = filterUnreadMessagesInRange(messages, flags, 1, 5);
 
     expect(actualUnread).toEqual(expectedUnread);
-  });
-});
-
-describe('countUnread', () => {
-  test('for an empty message list the unread count is zero', () => {
-    const messages = [];
-    const flags = {};
-
-    const actualCount = countUnread(messages, flags);
-
-    expect(actualCount).toEqual(0);
-  });
-
-  test('when no read flags are set, every message is unread', () => {
-    const messages = [1, 2, 3];
-    const flags = {};
-
-    const actualCount = countUnread(messages, flags);
-
-    expect(actualCount).toEqual(3);
-  });
-
-  test('every message present in read flags is not counted', () => {
-    const messages = [1, 2, 3];
-    const flags = {
-      1: true,
-      2: true,
-    };
-
-    const actualCount = countUnread(messages, flags);
-
-    expect(actualCount).toEqual(1);
-  });
-
-  test('counts all unread from specified message id to the end', () => {
-    const messages = [1, 2, 3, 4, 5];
-    const flags = {};
-
-    const actualCount = countUnread(messages, flags, 3);
-
-    expect(actualCount).toEqual(3);
-  });
-
-  test('counts all unread from beginning to given message id', () => {
-    const messages = [1, 2, 3, 4, 5];
-    const flags = {};
-
-    const actualCount = countUnread(messages, flags, -1, 4);
-
-    expect(actualCount).toEqual(4);
-  });
-
-  test('counts all unread from given message id to another id', () => {
-    const messages = [1, 2, 3, 4, 5, 6, 7, 8];
-    const flags = {};
-
-    const actualCount = countUnread(messages, flags, 3, 6);
-
-    expect(actualCount).toEqual(4);
   });
 });
--- src/utils/unread.js
+++ src/utils/unread.js
@@ -16,31 +16,5 @@ export const filterUnreadMessagesInRange = (
   return filterUnreadMessageIds(messagesInRange.map(x => x.id), flags);
 };
 
-export const countUnread = (
-  messageIds: number[],
-  readFlags: { [messageId: number]: boolean },
-  fromId: number = -1,
-  toId: number = -1,
-): number => {
-  let count = 0;
-  // count starts from the beginning if fromId === -1
-  // and continues to the end if toId === -1
-  let started = fromId === -1;
-
-  for (let i = 0; i < messageIds.length; i++) {
-    if (messageIds[i] === fromId) {
-      started = true;
-    }
-    if (started && !readFlags[messageIds[i]]) {
-      count++;
-    }
-    if (messageIds[i] === toId) {
-      break;
-    }
-  }
-
-  return count;
-};
-
 export const unreadToLimitedCount = (count: number): string =>
   count < 100 ? count.toString() : '99+';
