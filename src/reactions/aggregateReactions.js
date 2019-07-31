e0e420ebbd51d8b4830d2bfdd68ada163277ba90
Greg Price <greg@zulipchat.com>
2019-07-30 18:23:47 -0700

    loops: Rewrite false `reduce` with `forEach`, in aggregateReactions.
    As in previous commits, this is a maximally boring and mechanical
    rewrite.
    
    The diff can look misleadingly big because the indentation changed.
    To read it, try ignoring whitespace, e.g. with `git log -p -b`.

--- src/reactions/aggregateReactions.js
+++ src/reactions/aggregateReactions.js
@@ -4,36 +4,33 @@ import type { Reaction, AggregatedReaction } from '../types';
 export default (
   reactions: $ReadOnlyArray<Reaction>,
   ownEmail: string,
-): $ReadOnlyArray<AggregatedReaction> =>
-  Array.from(
-    reactions
-      .reduce((reactionMap, x) => {
-        if (!reactionMap.has(x.emoji_name)) {
-          reactionMap.set(x.emoji_name, {
-            code: x.emoji_code,
-            count: 1,
-            name: x.emoji_name,
-            type: x.reaction_type,
-            selfReacted: false,
-          });
-        } else {
-          const prevReaction = reactionMap.get(x.emoji_name);
-          if (prevReaction) {
-            reactionMap.set(x.emoji_name, {
-              ...prevReaction,
-              count: prevReaction.count + 1,
-            });
-          }
-        }
+): $ReadOnlyArray<AggregatedReaction> => {
+  const reactionMap = new Map();
+  reactions.forEach(x => {
+    if (!reactionMap.has(x.emoji_name)) {
+      reactionMap.set(x.emoji_name, {
+        code: x.emoji_code,
+        count: 1,
+        name: x.emoji_name,
+        type: x.reaction_type,
+        selfReacted: false,
+      });
+    } else {
+      const prevReaction = reactionMap.get(x.emoji_name);
+      if (prevReaction) {
+        reactionMap.set(x.emoji_name, {
+          ...prevReaction,
+          count: prevReaction.count + 1,
+        });
+      }
+    }
 
-        if (x.user && x.user.email === ownEmail) {
-          reactionMap.set(x.emoji_name, {
-            ...reactionMap.get(x.emoji_name),
-            selfReacted: true,
-          });
-        }
-
-        return reactionMap;
-      }, new Map())
-      .values(),
-  );
+    if (x.user && x.user.email === ownEmail) {
+      reactionMap.set(x.emoji_name, {
+        ...reactionMap.get(x.emoji_name),
+        selfReacted: true,
+      });
+    }
+  });
+  return Array.from(reactionMap.values());
+};
