8e83e98a2f93eb5c87b19700111f35ae7334950e
Isham Mahajan <mahajan.isham@gmail.com>
2019-08-06 19:10:26 +0530

    Make `pmConversationsSelectors-test.js` `flow strict`.
    The tests for `pmConversationsSelectors` are now `@flow
    strict-local`.
    
    To aid this process, we make use of the 'new' way of making tests,
    which is using `src/__tests__/exampleData.js`. A few examples of
    such tests include the unit tests at
    `src/users/__tests__/userSelectors-test.js` and
    `src/webview/html/__tests__/messageHeaderAsHtml-test.js`.
    
    Note that this is just a translation, and not a behavorial change.

--- src/pm-conversations/__tests__/pmConversationsSelectors-test.js
+++ src/pm-conversations/__tests__/pmConversationsSelectors-test.js
@@ -1,161 +1,155 @@
-import deepFreeze from 'deep-freeze';
+/* @flow strict-local */
 
 import { getRecentConversations } from '../pmConversationsSelectors';
+import * as eg from '../../__tests__/lib/exampleData';
 
 describe('getRecentConversations', () => {
   test('when no recent conversations, return no conversations', () => {
-    const state = deepFreeze({
-      realm: { email: 'me@example.com' },
-      recentPrivateConversations: [],
-      users: [{ user_id: 0, email: 'me@example.com' }],
-      unread: {
-        pms: [],
-        huddles: [],
-      },
+    const state = eg.reduxState({
+      realm: eg.realmState({ email: eg.selfUser.email }),
+      users: [eg.selfUser],
     });
 
-    const actual = getRecentConversations(state);
-
-    expect(actual).toEqual([]);
+    expect(getRecentConversations(state)).toEqual([]);
   });
 
   test('returns unique list of recipients, includes conversations with self', () => {
-    const state = deepFreeze({
-      realm: { email: 'me@example.com' },
-      recentPrivateConversations: [
-        { max_message_id: 4, user_ids: [] },
-        { max_message_id: 3, user_ids: [1] },
-        { max_message_id: 2, user_ids: [2] },
-        { max_message_id: 0, user_ids: [1, 2] },
+    const users = [eg.selfUser, eg.makeUser({ name: 'john' }), eg.makeUser({ name: 'mark' })];
+    const recentPrivateConversations = [
+      { max_message_id: 4, user_ids: [] },
+      { max_message_id: 3, user_ids: [users[1].user_id] },
+      { max_message_id: 2, user_ids: [users[2].user_id] },
+      { max_message_id: 0, user_ids: [users[1].user_id, users[2].user_id] },
+    ];
+    const unread = {
+      streams: [],
+      huddles: [
+        {
+          user_ids_string: [eg.selfUser.user_id, users[1].user_id, users[2].user_id]
+            .sort((a, b) => a - b)
+            .join(),
+          unread_message_ids: [5],
+        },
       ],
-      users: [
-        { user_id: 0, email: 'me@example.com' },
-        { user_id: 1, email: 'john@example.com' },
-        { user_id: 2, email: 'mark@example.com' },
+      pms: [
+        {
+          sender_id: eg.selfUser.user_id,
+          unread_message_ids: [4],
+        },
+        {
+          sender_id: users[1].user_id,
+          unread_message_ids: [1, 3],
+        },
+        {
+          sender_id: users[2].user_id,
+          unread_message_ids: [2],
+        },
       ],
-      unread: {
-        pms: [
-          {
-            sender_id: 0,
-            unread_message_ids: [4],
-          },
-          {
-            sender_id: 1,
-            unread_message_ids: [1, 3],
-          },
-          {
-            sender_id: 2,
-            unread_message_ids: [2],
-          },
-        ],
-        huddles: [
-          {
-            user_ids_string: '0,1,2',
-            unread_message_ids: [5],
-          },
-        ],
-      },
+      mentions: [],
+    };
+
+    const state = eg.reduxState({
+      realm: eg.realmState({ email: eg.selfUser.email }),
+      users,
+      recentPrivateConversations,
+      unread,
     });
 
-    const expectedResult = [
+    expect(getRecentConversations(state)).toEqual([
       {
-        ids: '0',
-        recipients: 'me@example.com',
+        ids: eg.selfUser.user_id.toString(),
+        recipients: eg.selfUser.email,
         msgId: 4,
         unread: 1,
       },
       {
-        ids: '1',
-        recipients: 'john@example.com',
+        ids: users[1].user_id.toString(),
+        recipients: users[1].email,
         msgId: 3,
         unread: 2,
       },
       {
-        ids: '2',
-        recipients: 'mark@example.com',
+        ids: users[2].user_id.toString(),
+        recipients: users[2].email,
         msgId: 2,
         unread: 1,
       },
       {
-        ids: '0,1,2',
-        recipients: 'john@example.com,mark@example.com',
+        ids: [eg.selfUser.user_id, users[1].user_id, users[2].user_id].sort((a, b) => a - b).join(),
+        recipients: [users[1].email, users[2].email].join(),
         msgId: 0,
         unread: 1,
       },
-    ];
-
-    const actual = getRecentConversations(state);
-
-    expect(actual).toEqual(expectedResult);
+    ]);
   });
 
   test('returns recipients sorted by last activity', () => {
-    const state = deepFreeze({
-      realm: { email: 'me@example.com' },
-      recentPrivateConversations: [
-        { max_message_id: 6, user_ids: [] },
-        { max_message_id: 5, user_ids: [1, 2] },
-        { max_message_id: 4, user_ids: [1] },
-        { max_message_id: 3, user_ids: [2] },
+    const users = [eg.selfUser, eg.makeUser({ name: 'john' }), eg.makeUser({ name: 'mark' })];
+    const recentPrivateConversations = [
+      { max_message_id: 6, user_ids: [] },
+      { max_message_id: 5, user_ids: [users[1].user_id, users[2].user_id] },
+      { max_message_id: 4, user_ids: [users[1].user_id] },
+      { max_message_id: 3, user_ids: [users[2].user_id] },
+    ];
+    const unread = {
+      streams: [],
+      huddles: [
+        {
+          user_ids_string: [eg.selfUser.user_id, users[1].user_id, users[2].user_id]
+            .sort((a, b) => a - b)
+            .join(),
+          unread_message_ids: [5],
+        },
       ],
-      users: [
-        { user_id: 0, email: 'me@example.com' },
-        { user_id: 1, email: 'john@example.com' },
-        { user_id: 2, email: 'mark@example.com' },
+      pms: [
+        {
+          sender_id: eg.selfUser.user_id,
+          unread_message_ids: [4],
+        },
+        {
+          sender_id: users[1].user_id,
+          unread_message_ids: [1, 3],
+        },
+        {
+          sender_id: users[2].user_id,
+          unread_message_ids: [2],
+        },
       ],
-      unread: {
-        pms: [
-          {
-            sender_id: 0,
-            unread_message_ids: [4],
-          },
-          {
-            sender_id: 1,
-            unread_message_ids: [1, 3],
-          },
-          {
-            sender_id: 2,
-            unread_message_ids: [2],
-          },
-        ],
-        huddles: [
-          {
-            user_ids_string: '0,1,2',
-            unread_message_ids: [5],
-          },
-        ],
-      },
+      mentions: [],
+    };
+
+    const state = eg.reduxState({
+      realm: eg.realmState({ email: eg.selfUser.email }),
+      users,
+      recentPrivateConversations,
+      unread,
     });
 
-    const expectedResult = [
+    expect(getRecentConversations(state)).toEqual([
       {
-        ids: '0',
-        recipients: 'me@example.com',
+        ids: eg.selfUser.user_id.toString(),
+        recipients: eg.selfUser.email,
         msgId: 6,
         unread: 1,
       },
       {
-        ids: '0,1,2',
-        recipients: 'john@example.com,mark@example.com',
+        ids: [eg.selfUser.user_id, users[1].user_id, users[2].user_id].sort((a, b) => a - b).join(),
+        recipients: [users[1].email, users[2].email].join(),
         msgId: 5,
         unread: 1,
       },
       {
-        ids: '1',
-        recipients: 'john@example.com',
+        ids: users[1].user_id.toString(),
+        recipients: users[1].email,
         msgId: 4,
         unread: 2,
       },
       {
-        ids: '2',
-        recipients: 'mark@example.com',
+        ids: users[2].user_id.toString(),
+        recipients: users[2].email,
         msgId: 3,
         unread: 1,
       },
-    ];
-
-    const actual = getRecentConversations(state);
-
-    expect(actual).toEqual(expectedResult);
+    ]);
   });
 });
