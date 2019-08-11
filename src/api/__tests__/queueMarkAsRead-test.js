189b04da2950bb6736b67e18d302dcd7ae79ff07
jainkuniya <jainkuniya@gmail.com>
2019-08-11 14:00:25 +0530

    api.queueMarkAsRead: Set timeout to follow up on a debounced request.
    If there are multiple requests to mark messages as read with interval
    less than 2s, and if there is no further request soon after the 2s
    mark, then the later requests remain stuck in the queue and aren't
    sent to the server.
    
    One symptom of this would be that after reaching the end of the
    message list, the unread banner might still be visible with some
    unread count.
    
    So now set timeout to send read-message flag to server.
    
    Fixes: #3509

--- src/api/__tests__/queueMarkAsRead-test.js
+++ src/api/__tests__/queueMarkAsRead-test.js
@@ -6,6 +6,8 @@ import * as eg from '../../__tests__/exampleData';
 // $FlowFixMe Make flow understand about mocking
 messagesFlags.default = jest.fn(() => {});
 
+jest.useFakeTimers();
+
 describe('queueMarkAsRead', () => {
   beforeEach(() => {
     resetAll();
@@ -13,12 +15,13 @@ describe('queueMarkAsRead', () => {
     jest.clearAllTimers();
   });
 
-  test('should not call messagesFlags on consecutive calls of queueMarkAsRead', () => {
+  test('should not call messagesFlags on consecutive calls of queueMarkAsRead,  setTimout on further calls', () => {
     queueMarkAsRead(eg.selfAuth, [1, 2, 3]);
     queueMarkAsRead(eg.selfAuth, [4, 5, 6]);
     queueMarkAsRead(eg.selfAuth, [7, 8, 9]);
     queueMarkAsRead(eg.selfAuth, [10, 11, 12]);
 
+    expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(messagesFlags.default).toHaveBeenCalledTimes(1);
   });
 
@@ -34,6 +37,22 @@ describe('queueMarkAsRead', () => {
     Date.now = jest.fn().mockReturnValue(secondCall);
 
     queueMarkAsRead(eg.selfAuth, [16, 17, 18]);
+
+    expect(messagesFlags.default).toHaveBeenCalledTimes(2);
+  });
+
+  test('should call messagesFlags after 2s to clear queue', async () => {
+    const currentDate = Date.now();
+    const secondCall = currentDate + 2100;
+    // $FlowFixMe Make flow understand about mocking
+    Date.now = jest.fn().mockReturnValue(currentDate);
+
+    queueMarkAsRead(eg.selfAuth, [1, 2, 3]);
+    queueMarkAsRead(eg.selfAuth, [4, 5, 6]);
+
+    // $FlowFixMe Make flow understand about mocking
+    Date.now = jest.fn().mockReturnValue(secondCall);
+    jest.runOnlyPendingTimers();
 
     expect(messagesFlags.default).toHaveBeenCalledTimes(2);
   });
--- src/api/queueMarkAsRead.js
+++ src/api/queueMarkAsRead.js
@@ -2,8 +2,10 @@
 import type { Auth } from './transportTypes';
 import messagesFlags from './messages/messagesFlags';
 
+const TIME_INTERVAL_BETWEEN_CONSECUTIVE_CALLS_MS = 2000;
 let unsentMessageIds = [];
 let lastSentTime = 0;
+let timeout = null;
 
 /**
  * Exported so that it can be used in test
@@ -12,13 +14,19 @@ let lastSentTime = 0;
 export const resetAll = () => {
   unsentMessageIds = [];
   lastSentTime = 0;
+  timeout = null;
 };
 
 const processQueue = (auth: Auth) => {
-  if (Date.now() - lastSentTime > 2000) {
+  if (Date.now() - lastSentTime > TIME_INTERVAL_BETWEEN_CONSECUTIVE_CALLS_MS) {
     messagesFlags(auth, unsentMessageIds, 'add', 'read');
     unsentMessageIds = [];
     lastSentTime = Date.now();
+  } else if (timeout === null) {
+    timeout = setTimeout(() => {
+      timeout = null;
+      processQueue(auth);
+    }, TIME_INTERVAL_BETWEEN_CONSECUTIVE_CALLS_MS);
   }
 };
 
