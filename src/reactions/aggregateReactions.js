43d094c94617fe3ab091cfea7256b3cf61e62d38
Greg Price <greg@zulipchat.com>
2019-10-09 11:19:26 -0700

    reactions [nfc]: Simplify loop in aggregateReactions.
    Two cheers for mutation!
    
    Well, really: three cheers for the nice simple syntax traditionally
    associated with mutation.  In this instance, `item` is always an
    object we created within this very loop and which no other code can
    have seen yet -- essentially, it's not even properly initialized until
    this loop is done -- so it's safe to mutate it.  And writing the code
    that way saves a good bit of goo and makes the logic cleaner to read.

--- src/reactions/aggregateReactions.js
+++ src/reactions/aggregateReactions.js
@@ -7,29 +7,21 @@ export default (
 ): $ReadOnlyArray<AggregatedReaction> => {
   const reactionMap = new Map();
   reactions.forEach(x => {
-    if (!reactionMap.has(x.emoji_name)) {
-      reactionMap.set(x.emoji_name, {
-        code: x.emoji_code,
-        count: 1,
+    let item = reactionMap.get(x.emoji_name);
+    if (!item) {
+      item = {
         name: x.emoji_name,
         type: x.reaction_type,
+        code: x.emoji_code,
+        count: 0,
         selfReacted: false,
-      });
-    } else {
-      const prevReaction = reactionMap.get(x.emoji_name);
-      if (prevReaction) {
-        reactionMap.set(x.emoji_name, {
-          ...prevReaction,
-          count: prevReaction.count + 1,
-        });
-      }
+      };
+      reactionMap.set(x.emoji_name, item);
     }
 
+    item.count += 1;
     if (x.user_id === ownUserId) {
-      reactionMap.set(x.emoji_name, {
-        ...reactionMap.get(x.emoji_name),
-        selfReacted: true,
-      });
+      item.selfReacted = true;
     }
   });
   return Array.from(reactionMap.values());
