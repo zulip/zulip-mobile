b25bca0a1d6512db0500bcd0f938cafbc2f3c1d1
Ray Kraesig <rkraesig@zulipchat.com>
2019-10-21 11:37:43 -0700

    event handlers: update netinfo API use
    Update calls into `react-native-netinfo` to use the new API.
    
    react-native-netinfo now uses a Boost `scoped_connection`- or .NET
    `IObservable`-style "return your disconnector" pattern, instead of
    using the hitherto-standard pair of `{add,remove}EventListener`.
    
    This is conceptually cleaner in some ways, and integrates much better
    with React's new "Hooks" mechanism... but Hooks were never intended to
    integrate well with classes.
    
    Fixes #3602.

--- src/boot/AppEventHandlers.js
+++ src/boot/AppEventHandlers.js
@@ -20,16 +20,41 @@ import {
 } from '../actions';
 
 /**
- * Part of the type of upstream's `NetInfo`.
- *
- * Based on docs: https://facebook.github.io/react-native/docs/netinfo
- *
- * TODO move this to a libdef, and/or get an explicit type into upstream.
+ * Part of the interface from react-native-netinfo.
+ * https://github.com/react-native-community/react-native-netinfo/tree/v3.2.1
  */
-type ConnectionInfo = {|
-  type: 'none' | 'wifi' | 'cellular' | 'unknown' | 'bluetooth' | 'ethernet' | 'wimax',
-  effectiveType: '2g' | '3g' | '4g' | 'unknown',
-|};
+// TODO: upgrade to 4.x.x so that we can use the `flow-typed` versions.
+// Requires RN 0.60+.
+type NetInfoStateType =
+  | 'unknown'
+  | 'none'
+  | 'cellular'
+  | 'wifi'
+  | 'bluetooth'
+  | 'ethernet'
+  | 'wimax'
+  | 'vpn'
+  | 'other';
+
+type NetInfoConnectedDetails = {
+  isConnectionExpensive: boolean,
+};
+
+type NetInfoState = {
+  /** The type of the current connection. */
+  type: NetInfoStateType,
+
+  /** Whether there is an active network connection. Note that this DOES NOT
+      mean that the Internet is reachable. */
+  isConnected: boolean,
+
+  /**
+   * This actually has a more complicated type whose exact shape is dependent on
+   * the value of `type`, above. (Flow could describe it, but we don't have a
+   * use for it yet.)
+   */
+  details: null | NetInfoConnectedDetails,
+};
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -46,15 +71,19 @@ type Props = {|
 |};
 
 class AppEventHandlers extends PureComponent<Props> {
+  /** NetInfo disconnection callback. */
+  netInfoDisconnectCallback: (() => void) | null = null;
+
   handleOrientationChange = (orientation: OrientationT) => {
     const { dispatch } = this.props;
     dispatch(appOrientation(orientation));
   };
 
-  /** https://facebook.github.io/react-native/docs/netinfo */
-  handleConnectivityChange = (connectionInfo: ConnectionInfo) => {
+  // https://github.com/react-native-community/react-native-netinfo/tree/v3.2.1
+  handleConnectivityChange = (netInfoState: NetInfoState) => {
     const { dispatch } = this.props;
-    const isConnected = connectionInfo.type !== 'none' && connectionInfo.type !== 'unknown';
+    const { type: connectionType } = netInfoState;
+    const isConnected = connectionType !== 'none' && connectionType !== 'unknown';
     dispatch(appOnline(isConnected));
   };
 
@@ -78,7 +107,7 @@ class AppEventHandlers extends PureComponent<Props> {
     const { dispatch } = this.props;
     handleInitialNotification(dispatch);
 
-    NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);
+    this.netInfoDisconnectCallback = NetInfo.addEventListener(this.handleConnectivityChange);
     AppState.addEventListener('change', this.handleAppStateChange);
     AppState.addEventListener('memoryWarning', this.handleMemoryWarning);
     SafeArea.getSafeAreaInsetsForRootView().then(params =>
@@ -90,7 +119,10 @@ class AppEventHandlers extends PureComponent<Props> {
   }
 
   componentWillUnmount() {
-    NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+    if (this.netInfoDisconnectCallback) {
+      this.netInfoDisconnectCallback();
+      this.netInfoDisconnectCallback = null;
+    }
     AppState.removeEventListener('change', this.handleAppStateChange);
     AppState.removeEventListener('memoryWarning', this.handleMemoryWarning);
     // $FlowFixMe: libdef wrongly says callback's parameter is optional
