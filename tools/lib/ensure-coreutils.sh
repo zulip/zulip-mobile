d20b50640a30ed4bad18d12717c325343509d0ff
Greg Price <greg@zulipchat.com>
2019-10-25 12:09:36 -0700

    ensure-coreutils: Make suitable to use in normal dev scripts.
    A need for `fmt` may be fairly niche, but `readlink -f` is awfully
    basic and widely useful in shell scripting.  Fortunately, with the
    insight that on Windows none of this is an issue, we can press ahead
    more widely with a clear conscience.
    
    Mostly this expands the comments; also adds a `brew install` mention
    to the error/help message.
    
    Also make the test a bit pickier, checking `readlink` too.
    
    A draft version prefixed `fmt` and `readlink` with `command`, to avoid
    seeing aliases or shell functions... but this apparently triggered a
    bug in the Bash 3.2.57 on a Mac:
      $ bash -ec 'command false || echo f; echo done'
        # no output
    Ah well.
    
    Suggested-by: Ray Kraesig <rkraesig@zulipchat.com>

--- tools/lib/ensure-coreutils.sh
+++ tools/lib/ensure-coreutils.sh
@@ -4,17 +4,34 @@
 # Ensures GNU coreutils are available in PATH.
 # May add to PATH, or exit with a message to stderr.
 #
-# We don't yet have good docs for handling failure here if on
-# Windows or macOS, so until then this isn't suitable for scripts
-# used in normal development -- only for maintainer-facing scripts.
+# The GNU coreutils are a basic piece of infrastructure for having
+# a reasonable 21st-century shell scripting environment, so we
+# freely invoke this in any of our scripts that need `readlink -f`
+# or other features not always found without them.
+#
+# On any GNU/Linux system this is of course a non-issue.  Likewise
+# on Windows: we inevitably require Git, and Git for Windows comes
+# with a GNU environment called "Git BASH", based on MSYS2.
+#
+# So this is really all about macOS.  Fortunately it's easy to get
+# coreutils installed there too... plus, many people already have
+# it installed but just not in their PATH.  We write our scripts
+# for a GNU environment, so we bring it into the PATH.
 
 check_coreutils() {
+    # Check a couple of commands for GNU-style --help and --version,
+    # which macOS's default BSD-based implementations don't understand.
     fmt --help >/dev/null 2>&1
+    readlink --version 2>&1 | grep -q GNU
 }
 
 ensure_coreutils() {
+    # If we already have it, then great.
     check_coreutils && return
 
+    # Homebrew provides names like `greadlink` on the PATH,
+    # but also provides the standard names in this directory.
+    # Try that.
     homebrew_gnubin=/usr/local/opt/coreutils/libexec/gnubin
     if [ -d "$homebrew_gnubin" ]; then
         export PATH="$homebrew_gnubin":"$PATH"
@@ -24,8 +41,10 @@ ensure_coreutils() {
     cat >&2 <<EOF
 This script requires GNU coreutils.
 
-Install from your favorite package manager, or from:
+Install from upstream:
   https://www.gnu.org/software/coreutils/
+or from your favorite package manager; for example:
+  brew install coreutils
 EOF
     return 2
 }
