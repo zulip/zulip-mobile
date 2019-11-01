78db91d177e0f5f5fb0ca226e6f53c51c5fe6e3d
Greg Price <greg@zulipchat.com>
2019-11-01 15:49:42 -0700

    webAuth: Add some tests for authFromCallbackUrl.

---
+++ src/start/__tests__/webAuth-test.js
@@ -0,0 +1,25 @@
+/* @flow strict-local */
+import { authFromCallbackUrl } from '../webAuth';
+
+describe('authFromCallbackUrl', () => {
+  const otp = '13579bdf';
+  const realm = 'https://chat.example';
+
+  test('success', () => {
+    const url = `zulip://login?realm=${realm}&email=a@b&otp_encrypted_api_key=2636fdeb`;
+    expect(authFromCallbackUrl(url, otp, realm)).toEqual({ realm, email: 'a@b', apiKey: '5af4' });
+  });
+
+  test('wrong realm', () => {
+    const url =
+      'zulip://login?realm=https://other.example.org&email=a@b&otp_encrypted_api_key=2636fdeb';
+    expect(authFromCallbackUrl(url, otp, realm)).toEqual(null);
+  });
+
+  test('not login', () => {
+    // Hypothetical link that isn't a login... but somehow with all the same
+    // query params, for extra confusion for good measure.
+    const url = `zulip://message?realm=${realm}&email=a@b&otp_encrypted_api_key=2636fdeb`;
+    expect(authFromCallbackUrl(url, otp, realm)).toEqual(null);
+  });
+});
