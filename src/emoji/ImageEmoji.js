0403001ada3622d6b87eeb29b88acae96e039fc1
Greg Price <greg@zulipchat.com>
2019-11-04 17:36:17 -0800

    ImageEmoji [nfc]: Move component styles inside class.
    This makes it a bit more encapsulated... which will be helpful
    shortly when consolidating it with closely-related code into a
    somewhat less tiny module.

--- src/emoji/ImageEmoji.js
+++ src/emoji/ImageEmoji.js
@@ -5,20 +5,20 @@ import { StyleSheet, Image } from 'react-native';
 
 import type { ImageEmojiType } from '../types';
 
-const styles = StyleSheet.create({
-  image: {
-    width: 20,
-    height: 20,
-  },
-});
-
 type Props = {|
   emoji: ImageEmojiType,
 |};
 
 export default class ImageEmoji extends PureComponent<Props> {
+  styles = StyleSheet.create({
+    image: {
+      width: 20,
+      height: 20,
+    },
+  });
+
   render() {
     const { emoji } = this.props;
-    return <Image style={styles.image} source={{ uri: emoji.source_url }} />;
+    return <Image style={this.styles.image} source={{ uri: emoji.source_url }} />;
   }
 }
