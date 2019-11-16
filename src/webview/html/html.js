4432f65e7b0c3feb886069ef50587e72435dbe66
Ray Kraesig <rkraesig@zulipchat.com>
2019-11-15 17:28:19 -0800

    webview: work around WebKit bug involving CSS transitions
    Fixes #3689.

--- src/webview/html/html.js
+++ src/webview/html/html.js
@@ -11,6 +11,34 @@ type InitOptionsType = {|
   showMessagePlaceholders: boolean,
 |};
 
+/**
+ * Workaround for a WebKit CSS transitions bug affecting both iOS and Android.
+ *
+ * The timestamp pills (and possibly other elements) have CSS transitions
+ * defined for properties whose values are different from the default user-agent
+ * specified values.
+ *
+ * When the relevant CSS rules were moved into a separate file in commit
+ * 8fdb93b, this triggered the bug: the WebView started initially using its
+ * default values, and only switching to the stylesheet-specified values once
+ * they were loaded. Since this switch included the addition of transition rules,
+ * the WebView applied the transition. This led to all the timestamp pills
+ * starting (visually) in the flyout state.
+ *
+ * There are many possible fixes to this, including:
+ *  - reworking the transitions such that their start states are all equivalent
+ *    to `initial`;
+ *  - moving (at least) the CSS rules with transitions into inline stylesheets
+ *    within the generated HTML; and/or
+ *  - the following bizarre, but minimally intrusive, hack.
+ *
+ *
+ * See:
+ *  - https://bugs.chromium.org/p/chromium/issues/detail?id=167083
+ *  - https://bugs.chromium.org/p/chromium/issues/detail?id=332189
+ */
+const webkitBugWorkaround: string = '<script> </script>';
+
 export default (content: string, theme: ThemeName, initOptions: InitOptionsType) => template`
 $!${script(initOptions.anchor, initOptions.auth)}
 $!${css(theme)}
@@ -18,5 +46,6 @@ $!${css(theme)}
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <body style="overflow-x: hidden;">
 $!${htmlBody(content, initOptions.showMessagePlaceholders)}
+$!${webkitBugWorkaround}
 </body>
 `;
