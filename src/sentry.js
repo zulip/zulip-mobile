db2b1edc00140c589a2195611d33051098d56756
Ray Kraesig <rkraesig@zulipchat.com>
2019-11-20 14:49:33 -0800

    sentry: check for, and guard against, accidental initialization
    Under no circumstances should Jest ever attempt to initialize Sentry.
    (This may change if we start using Jest for some sort of integration
    tests, but until then, just yell.)
    
    Trying to initialize, but not being able to, is normal for debug mode
    (as the existing comment notes), but trying and potentially
    *succeeding* means something is probably configured incorrectly.

--- src/sentry.js
+++ src/sentry.js
@@ -1,9 +1,39 @@
 /* @flow strict-local */
 import * as Sentry from '@sentry/react-native';
+import DeviceInfo from 'react-native-device-info';
 import config from './config';
+
+const preventNoise = (): void => {
+  /* Sentry should not normally be used in debug mode. (For one thing, the
+     debug-mode build process doesn't ordinarily create bundles or .map files,
+     so you'll probably get nonsensical stack traces.) */
+  if (process.env.NODE_ENV === 'development' && config.sentryKey !== null) {
+    /* If you have some reason to initialize Sentry in debug mode anyway, please
+       change the app's version name (currently specified in `ios/Info.plist`
+       and/or `android/app/build.gradle`) to something that doesn't look like a
+       normal version number -- preferably with your name and/or Github ID in
+       it. This will allow events produced by these debug builds to be easily
+       identified in the Sentry console. */
+    if (DeviceInfo.getVersion().match(/^\d+\.\d+\.\d+$/)) {
+      throw new Error('Sentry should not be initialized in debug builds');
+    }
+  }
+
+  /* Jest has no reason to even _try_ to initialize Sentry, even if `sentryKey`
+     is `null`. */
+  // (See the following links concerning detecting Jest.)
+  //   [1] https://jestjs.io/docs/en/24.0/getting-started.html#using-babel
+  //   [2] https://stackoverflow.com/a/52231746
+  if (process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID !== undefined) {
+    throw new Error('Sentry must not be initialized during testing!');
+  }
+};
 
 /** Initialize Sentry into its default configuration. */
 const initSentry = () => {
+  // Check to make sure it's safe to run Sentry. Abort if not.
+  preventNoise();
+
   const key = config.sentryKey;
   if (key !== null) {
     // The DSN is formatted as an `https:` URL. Omit the scheme.
