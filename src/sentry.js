d19687b469bf7fa382568cc6a9e05b873a780839
Ray Kraesig <rkraesig@zulipchat.com>
2019-11-25 11:33:08 -0800

    sentry: refactor initialization sequence
    ... mostly to add comments; but also to add a `console.log`, to
    confirm that Sentry initialization has taken place. No functional
    changes otherwise.
    
    As one of the comments notes, calling Sentry functions without
    initializing Sentry "has worked [...] so far". This is hardly
    reassuring, but will be bolstered by tests in an upcoming commit.

--- src/sentry.js
+++ src/sentry.js
@@ -1,14 +1,31 @@
 /* @flow strict-local */
 import * as Sentry from '@sentry/react-native';
-
 import config from './config';
 
-if (config.sentryKey !== null) {
-  Sentry.init({
-    dsn: config.sentryKey,
-    ignoreErrors: ['Network request failed'],
-  });
-} else {
-  // eslint-disable-next-line no-console
-  console.log('skipping Sentry initialization');
-}
+/** Initialize Sentry into its default configuration. */
+const initSentry = () => {
+  const key = config.sentryKey;
+  if (key !== null) {
+    // The DSN is formatted as an `https:` URL. Omit the scheme.
+    const displayKey = `${key.slice(8, 12)}......`;
+    // eslint-disable-next-line no-console
+    console.log(`Sentry key ${displayKey} provided; initializing`);
+
+    Sentry.init({
+      dsn: key,
+      ignoreErrors: ['Network request failed'],
+    });
+  } else {
+    // This is normal behavior when running locally; only published release
+    // builds will have a Sentry key.
+
+    // (It's not documented anywhere what happens when functions on Sentry are
+    // called without first initializing Sentry. Fortunately, it seems to have
+    // worked without warnings, so far.)
+
+    // eslint-disable-next-line no-console
+    console.log('no Sentry key provided; skipping initialization');
+  }
+};
+
+initSentry();
