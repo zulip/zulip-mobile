6c5f752e08e08f1a63d8180f1732b3ec540e1a26
Greg Price <greg@zulipchat.com>
2019-12-04 15:37:55 -0800

    session reducer [nfc]: More-compact formatting for tests.

--- src/session/__tests__/sessionReducer-test.js
+++ src/session/__tests__/sessionReducer-test.js
@@ -26,7 +26,6 @@ describe('sessionReducer', () => {
   test('ACCOUNT_SWITCH', () => {
     const state = deepFreeze({ ...baseState, lastNarrow: [], needsInitialFetch: false });
     const newState = sessionReducer(state, eg.action.account_switch);
-
     expect(newState).toEqual({ ...baseState, lastNarrow: null, needsInitialFetch: true });
   });
 
@@ -59,14 +58,12 @@ describe('sessionReducer', () => {
   test('DEAD_QUEUE', () => {
     const state = deepFreeze({ ...baseState, needsInitialFetch: false });
     const newState = sessionReducer(state, deepFreeze({ type: DEAD_QUEUE }));
-
     expect(newState).toEqual({ ...baseState, needsInitialFetch: true });
   });
 
   test('LOGOUT', () => {
     const state = deepFreeze({ ...baseState, lastNarrow: [], needsInitialFetch: true });
     const newState = sessionReducer(state, deepFreeze({ type: LOGOUT }));
-
     expect(newState).toEqual({ ...baseState, lastNarrow: null, needsInitialFetch: false });
   });
 
@@ -76,7 +73,6 @@ describe('sessionReducer', () => {
       data: { ...eg.action.realm_init.data, queue_id: 100 },
     });
     const newState = sessionReducer(baseState, action);
-
     expect(newState).toEqual({ ...baseState, eventQueueId: 100 });
   });
 
@@ -84,7 +80,6 @@ describe('sessionReducer', () => {
     const state = deepFreeze({ ...baseState, lastNarrow: [] });
     const action = deepFreeze({ type: DO_NARROW, narrow: privateNarrow('a@a.com') });
     const newState = sessionReducer(state, action);
-
     expect(newState).toEqual({ ...baseState, lastNarrow: privateNarrow('a@a.com') });
   });
 
@@ -92,7 +87,6 @@ describe('sessionReducer', () => {
     const state = deepFreeze({ ...baseState, isOnline: false });
     const action = deepFreeze({ type: APP_ONLINE, isOnline: true });
     const newState = sessionReducer(state, action);
-
     expect(newState).toEqual({ ...baseState, isOnline: true });
   });
 
@@ -100,59 +94,44 @@ describe('sessionReducer', () => {
     const state = deepFreeze({ ...baseState, isActive: false });
     const action = deepFreeze({ type: APP_STATE, isActive: true });
     const newState = sessionReducer(state, action);
-
     expect(newState).toEqual({ ...baseState, isActive: true });
   });
 
   test('INITIAL_FETCH_COMPLETE', () => {
     const state = deepFreeze({ ...baseState, needsInitialFetch: true });
     const newState = sessionReducer(state, deepFreeze({ type: INITIAL_FETCH_COMPLETE }));
-
     expect(newState).toEqual({ ...baseState, needsInitialFetch: false });
   });
 
   test('INIT_SAFE_AREA_INSETS', () => {
     const safeAreaInsets = { top: 1, bottom: 2, right: 3, left: 0 };
     const action = deepFreeze({ type: INIT_SAFE_AREA_INSETS, safeAreaInsets });
-    const newState = sessionReducer(baseState, action);
-
-    expect(newState).toEqual({ ...baseState, safeAreaInsets });
+    expect(sessionReducer(baseState, action)).toEqual({ ...baseState, safeAreaInsets });
   });
 
   test('APP_ORIENTATION', () => {
     const state = deepFreeze({ ...baseState, orientation: 'PORTRAIT' });
     const orientation = 'LANDSCAPE';
     const action = deepFreeze({ type: APP_ORIENTATION, orientation });
-    const newState = sessionReducer(state, action);
-
-    expect(newState).toEqual({ ...baseState, orientation });
+    expect(sessionReducer(state, action)).toEqual({ ...baseState, orientation });
   });
 
   test('GOT_PUSH_TOKEN', () => {
     const pushToken = 'pushToken';
     const action = deepFreeze({ type: GOT_PUSH_TOKEN, pushToken });
-    const newState = sessionReducer(baseState, action);
-
-    expect(newState).toEqual({ ...baseState, pushToken });
+    expect(sessionReducer(baseState, action)).toEqual({ ...baseState, pushToken });
   });
 
   test('TOGGLE_OUTBOX_SENDING', () => {
     const state = deepFreeze({ ...baseState, outboxSending: false });
-    const newState = sessionReducer(
-      state,
-      deepFreeze({ type: TOGGLE_OUTBOX_SENDING, sending: true }),
-    );
-
-    expect(newState).toEqual({ ...baseState, outboxSending: true });
+    expect(
+      sessionReducer(state, deepFreeze({ type: TOGGLE_OUTBOX_SENDING, sending: true })),
+    ).toEqual({ ...baseState, outboxSending: true });
   });
 
   test('DEBUG_FLAG_TOGGLE', () => {
-    const newState = sessionReducer(
-      baseState,
-      deepFreeze({ type: DEBUG_FLAG_TOGGLE, key: 'someKey', value: true }),
-    );
-
-    expect(newState).toEqual({
+    const action = deepFreeze({ type: DEBUG_FLAG_TOGGLE, key: 'someKey', value: true });
+    expect(sessionReducer(baseState, action)).toEqual({
       ...baseState,
       debug: { doNotMarkMessagesAsRead: false, someKey: true },
     });
