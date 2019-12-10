0637187876ddd5f9393578868e3b423d1f8f9df4
Greg Price <greg@zulipchat.com>
2019-12-09 17:19:58 -0800

    tests: Make small fixes to Pirlo test code.
    * Fix the usage message to stay accurate.
    * Make some tweaks to shell-script style.
    * Add a gitignore entry for the downloaded Pirlo CLI binary.

--- tools/pirlo-tests.sh
+++ tools/pirlo-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 set -e
-echo "Downloading pirlo-cli tool"
-curl https://pirlo-downloads.s3-us-west-1.amazonaws.com/pirlo-cli/pirlo-cli -o pirlo-cli
 
+echo "Downloading pirlo-cli tool"
+curl -s -o pirlo-cli \
+  https://pirlo-downloads.s3-us-west-1.amazonaws.com/pirlo-cli/pirlo-cli
 chmod a+x pirlo-cli
 
 echo "Initiating a new automated test run."
-
-./pirlo-cli run -k jNmWe3iFhWjvSy_SCjlvLsCWgfw=  android/app/build/outputs/apk/release/app-release.apk
+./pirlo-cli run -k jNmWe3iFhWjvSy_SCjlvLsCWgfw= \
+  android/app/build/outputs/apk/release/app-release.apk
