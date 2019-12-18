f0ee02e047740585b6f77431890fe6fb1c6da1f7
Ray Kraesig <rkraesig@zulipchat.com>
2019-12-18 13:04:40 -0800

    selectors: add logging to getPrivateMessages
    Sentry reports indicate that `getPrivateMessages` is sometimes
    returning a list containing `undefined`. Prevent these from being
    returned, and log IDs that would have caused them to Sentry.
    
    This ameliorates the known symptoms of issue #3749, but does not
    actually fix it.
    
    It would be convenient, and perhaps not entirely wrong, to lay blame
    for this error on the unsoundness of indexer-element types [1]... but
    it actually should be the case that all indices in `narrows` are also
    found in `messages`.
    
    [1] Specifically, the fact that
    
        (a: {[key: string]: T})["string"]
    
    is inferred to be of type `T`, but may actually be `undefined`. (This
    unsoundness is, sadly, shared by TypeScript and Flow.)

--- src/message/messageSelectors.js
+++ src/message/messageSelectors.js
@@ -9,16 +9,56 @@ import {
   getMute,
   getSubscriptions,
 } from '../directSelectors';
+import * as logging from '../utils/logging';
 import { getShownMessagesForNarrow } from '../chat/narrowsSelectors';
 import renderMessages from './renderMessages';
 import { findAnchor } from '../utils/message';
 import { ALL_PRIVATE_NARROW_STR } from '../utils/narrow';
 import { NULL_ARRAY } from '../nullObjects';
 
+/**
+ * Truncate a potentially-very-long array for logging and/or reporting purposes.
+ * Returns something which may or may not be an array, but is at least JSONable
+ * and human-readable.
+ */
+function truncateForLogging<T>(arr: Array<T>, len = 10) {
+  if (arr.length <= 2 * len) {
+    return arr;
+  }
+  return {
+    length: arr.length,
+    start: arr.slice(0, len),
+    end: arr.slice(-len),
+  };
+}
+
 export const getPrivateMessages: Selector<Message[]> = createSelector(
   getAllNarrows,
   getMessages,
-  (narrows, messages) => (narrows[ALL_PRIVATE_NARROW_STR] || NULL_ARRAY).map(id => messages[id]),
+  (narrows, messages) => {
+    const privateMessages: Message[] = [];
+    const unknownIds: number[] = [];
+
+    const pmIds = narrows[ALL_PRIVATE_NARROW_STR] || NULL_ARRAY;
+    pmIds.forEach(id => {
+      const msg = messages[id];
+      if (msg !== undefined) {
+        privateMessages.push(msg);
+      } else {
+        unknownIds.push(id);
+      }
+    });
+
+    // BUG (#3749): all messages in `narrows` _should_ also be in `messages`.
+    // Error reports indicate that, somehow, this isn't always so.
+    if (unknownIds.length > 0) {
+      logging.error('narrow IDs not found in state.messages', {
+        all_ids: truncateForLogging(pmIds),
+        unknown_ids: unknownIds,
+      });
+    }
+    return privateMessages;
+  },
 );
 
 export const getRenderedMessages = (narrow: Narrow): Selector<RenderedSectionDescriptor[]> =>
