6a4d5d0a48b0c1a243b6528276141c29ca93d47d
Ray Kraesig <rkraesig@zulipchat.com>
2019-12-18 15:42:09 -0800

    logging [nfc]: coalesce implementations of `error` and `warn`
    Unify `error` and `warn`.
    
    Rather than factoring them out in the usual way (by delegating to a
    shared-implementation function when called), we create a factory
    function that returns new logging functions. This avoids the creation
    of additional content-free call stack framesin the Sentry event.

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -4,26 +4,6 @@ import { getCurrentHub, Severity } from '@sentry/react-native';
 
 import config from '../config';
 
-// Notes on the behavior of `console.error`, `console.warn`, `console.log`:
-//
-// * When debugging JS: all display in the Chrome dev console, and not in
-//   the Android system log.
-//   * `error` and `warn` produce a nice expando-hidden stack trace, in
-//     addition to formatting their arguments.
-//   * `log` just formats the arguments.
-//   * An `Error` argument formats with its embedded stack trace.  (As just
-//     text, with no spiffy expando.)
-//
-// * In a debug build, whether debugging JS or not:
-//   * `error` pops up the RN error red-screen.
-//   * `warn` could pop up an RN yellow-box, but we've set
-//     `console.disableYellowBox` so it doesn't.
-//
-// * On Android, on both debug and release builds, except when debugging JS:
-//   * All of `error`, `warn`, `log` go to the system log (for `adb logcat`).
-//   * An `Error` argument formats with (the name + message and) just the
-//     single source location where it was created, no stack trace.
-
 /**
  * Log an event (a string or Error) at some arbitrary severity.
  *
@@ -65,6 +45,46 @@ const logToSentry = (event: string | Error, level: SeverityType): string => {
   return getCurrentHub().captureMessage(message, level, hint);
 };
 
+type LogParams = {|
+  consoleMethod: mixed => void,
+  severity: SeverityType,
+|};
+type LogFunction = (event: string | Error) => void;
+
+const makeLogFunction = ({ consoleMethod, severity }: LogParams): LogFunction => {
+  const toConsole = consoleMethod.bind(console);
+
+  return (event: string | Error) => {
+    logToSentry(event, severity);
+
+    if (config.enableErrorConsoleLogging) {
+      toConsole(event);
+    }
+  };
+};
+
+/* eslint-disable no-console */
+
+// Notes on the behavior of `console.error`, `console.warn`, `console.log`:
+//
+// * When debugging JS: all display in the Chrome dev console, and not in
+//   the Android system log.
+//   * `error` and `warn` produce a nice expando-hidden stack trace, in
+//     addition to formatting their arguments.
+//   * `log` just formats the arguments.
+//   * An `Error` argument formats with its embedded stack trace.  (As just
+//     text, with no spiffy expando.)
+//
+// * In a debug build, whether debugging JS or not:
+//   * `error` pops up the RN error red-screen.
+//   * `warn` could pop up an RN yellow-box, but we've set
+//     `console.disableYellowBox` so it doesn't.
+//
+// * On Android, on both debug and release builds, except when debugging JS:
+//   * All of `error`, `warn`, `log` go to the system log (for `adb logcat`).
+//   * An `Error` argument formats with (the name + message and) just the
+//     single source location where it was created, no stack trace.
+
 /**
  * Log an error at "error" severity.
  *
@@ -75,17 +95,17 @@ const logToSentry = (event: string | Error, level: SeverityType): string => {
  * represents a bug.  For conditions that can happen without a bug (e.g. a
  * failure to reach the server), consider `logging.warn`.
  *
+ * @param event A string describing the nature of the event to be logged, or an
+ *   exception whose `.message` is such a string. Related events should have
+ *   identical such strings, when feasible.
+ *
  * See also:
  *  * `logging.warn` for logging at lower severity
  */
-export const error = (err: string | Error) => {
-  logToSentry(err, Severity.Error);
-
-  if (config.enableErrorConsoleLogging) {
-    // See toplevel comment about behavior of `console` methods.
-    console.error(err); // eslint-disable-line
-  }
-};
+export const error: (event: string | Error) => void = makeLogFunction({
+  consoleMethod: console.error,
+  severity: Severity.Error,
+});
 
 /**
  * Log an event at "warning" severity.
@@ -97,14 +117,14 @@ export const error = (err: string | Error) => {
  * which have an inevitable background rate.  For conditions which
  * definitely represent a bug in the app, consider `logging.error` instead.
  *
+ * @param event A string describing the nature of the event to be logged, or an
+ *   exception whose `.message` is such a string. Related events should have
+ *   identical such strings, when feasible.
+ *
  * See also:
  *  * `logging.error` for logging at higher severity
  */
-export const warn = (event: string | Error) => {
-  logToSentry(event, Severity.Warning);
-
-  if (config.enableErrorConsoleLogging) {
-    // See toplevel comment about behavior of `console` methods.
-    console.warn(event); // eslint-disable-line
-  }
-};
+export const warn: (event: string | Error) => void = makeLogFunction({
+  consoleMethod: console.warn,
+  severity: Severity.Warning,
+});
