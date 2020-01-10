84a60063e946b2c185b651ef0c424b1c0ec1edd3
Chris Bobbe <cbobbe@zulipchat.com>
2020-01-10 10:25:22 -0800

    TitlePrivate: Fix connectFlowFixMe, render null if no user found.
    The connectFlowFixMe was hiding a missing prop `email`. Add it, with
    the type the single caller actually provides.
    
    Also, the connectFlowFixMe was hiding a plausible but unproven
    assumption that getAllUsersByEmail will always return a user here,
    so, update the user prop's type to UserOrBot | void and render null
    if !user. Also log the unexpected behavior in componentDidUpdate.

--- src/title/TitlePrivate.js
+++ src/title/TitlePrivate.js
@@ -4,18 +4,20 @@ import React, { PureComponent } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import type { Dispatch, UserOrBot } from '../types';
-import { connectFlowFixMe } from '../react-redux';
+import { connect } from '../react-redux';
 import { Touchable, UserAvatarWithPresence, ViewPlaceholder } from '../common';
 import ActivityText from './ActivityText';
 import { getAllUsersByEmail } from '../users/userSelectors';
 import styles from '../styles';
 import { navigateToAccountDetails } from '../nav/navActions';
+import * as logging from '../utils/logging';
 
 type SelectorProps = $ReadOnly<{|
-  user: UserOrBot,
+  user: UserOrBot | void,
 |}>;
 
 type Props = $ReadOnly<{
+  email: string,
   color: string,
 
   dispatch: Dispatch,
@@ -25,6 +27,9 @@ type Props = $ReadOnly<{
 class TitlePrivate extends PureComponent<Props> {
   handlePress = () => {
     const { dispatch, user } = this.props;
+    if (!user) {
+      return;
+    }
     dispatch(navigateToAccountDetails(user.email));
   };
 
@@ -33,8 +38,19 @@ class TitlePrivate extends PureComponent<Props> {
     inner: { flexDirection: 'row', alignItems: 'center' },
   });
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.user && !this.props.user) {
+      logging.warn('`user` prop disappeared in TitlePrivate.', {
+        email: prevProps.user.email.replace(/\w/g, 'x'),
+      });
+    }
+  }
+
   render() {
     const { user, color } = this.props;
+    if (!user) {
+      return null;
+    }
     return (
       <Touchable onPress={this.handlePress} style={this.styles.outer}>
         <View style={this.styles.inner}>
@@ -52,6 +68,7 @@ class TitlePrivate extends PureComponent<Props> {
   }
 }
 
-export default connectFlowFixMe((state, props) => ({
+export default connect<SelectorProps, _, _>((state, props) => ({
+  // TODO: use user_id, not email (https://github.com/zulip/zulip-mobile/issues/3764)
   user: getAllUsersByEmail(state).get(props.email),
 }))(TitlePrivate);
