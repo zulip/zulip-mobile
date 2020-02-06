3c0a42614afe9ff1313949de92139b1d1cacf154
Greg Price <greg@zulipchat.com>
2020-02-05 20:04:28 -0800

    compose [nfc]: Unpack narrow structurally in getComposeInputPlaceholder.
    Instead of using `narrow[0].operand` and hoping the code correctly
    lines up its meaning with the conditionals, use the `caseNarrow`
    family to unpack the components of the narrow in a structured way.

--- src/compose/getComposeInputPlaceholder.js
+++ src/compose/getComposeInputPlaceholder.js
@@ -1,42 +1,40 @@
 /* @flow strict-local */
 import type { Narrow, UserOrBot, LocalizableText } from '../types';
-import { isStreamNarrow, isTopicNarrow, isGroupPmNarrow, is1to1PmNarrow } from '../utils/narrow';
+import { caseNarrowDefault } from '../utils/narrow';
 
 export default (
   narrow: Narrow,
   ownEmail: string,
   usersByEmail: Map<string, UserOrBot>,
-): LocalizableText => {
-  if (isGroupPmNarrow(narrow)) {
-    return { text: 'Message group' };
-  }
+): LocalizableText =>
+  caseNarrowDefault(
+    narrow,
+    {
+      pm: emails => {
+        if (emails.length > 1) {
+          return { text: 'Message group' };
+        }
+        const email = emails[0];
 
-  if (is1to1PmNarrow(narrow)) {
-    if (ownEmail && narrow[0].operand === ownEmail) {
-      return { text: 'Jot down something' };
-    }
+        if (ownEmail && email === ownEmail) {
+          return { text: 'Jot down something' };
+        }
 
-    if (!usersByEmail) {
-      return { text: 'Type a message' };
-    }
+        if (!usersByEmail) {
+          return { text: 'Type a message' };
+        }
+        const user = usersByEmail.get(email) || {};
 
-    const user = usersByEmail.get(narrow[0].operand) || {};
-    return {
-      text: 'Message {recipient}',
-      values: { recipient: `@${user.full_name}` },
-    };
-  }
-
-  if (isStreamNarrow(narrow)) {
-    return {
-      text: 'Message {recipient}',
-      values: { recipient: `#${narrow[0].operand}` },
-    };
-  }
-
-  if (isTopicNarrow(narrow)) {
-    return { text: 'Reply' };
-  }
-
-  return { text: 'Type a message' };
-};
+        return {
+          text: 'Message {recipient}',
+          values: { recipient: `@${user.full_name}` },
+        };
+      },
+      stream: name => ({
+        text: 'Message {recipient}',
+        values: { recipient: `#${name}` },
+      }),
+      topic: () => ({ text: 'Reply' }),
+    },
+    () => ({ text: 'Type a message' }),
+  );
