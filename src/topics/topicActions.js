db79bb75b7800a59bcf15af9a01f7bd01017d82a
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-02-13 22:18:56 +0530

    topic actions: Create action 'deleteMessagesForTopic'.
    'deleteMessagesForTopic' deletes all messages (including those in
    outbox) for a given topic in a stream.

--- src/topics/topicActions.js
+++ src/topics/topicActions.js
@@ -1,9 +1,11 @@
 /* @flow strict-local */
-import type { GetState, Dispatch, Narrow, Topic, Action } from '../types';
+import type { GetState, Dispatch, Message, Narrow, Topic, Action, Outbox, Stream } from '../types';
 import * as api from '../api';
 import { INIT_TOPICS } from '../actionConstants';
 import { isStreamNarrow } from '../utils/narrow';
 import { getAuth, getStreams } from '../selectors';
+import { deleteOutboxMessage } from '../actions';
+import { getOutbox } from '../directSelectors';
 
 export const initTopics = (topics: Topic[], streamId: number): Action => ({
   type: INIT_TOPICS,
@@ -33,4 +35,26 @@ export const fetchTopicsForActiveStream = (narrow: Narrow) => async (
     return;
   }
   dispatch(fetchTopics(stream.stream_id));
+};
+
+export const deleteMessagesForTopic = (narrow: Narrow, message: Message | Outbox) => async (
+  dispatch: Dispatch,
+  getState: GetState,
+) => {
+  const state = getState();
+  const outbox = getOutbox(state);
+  outbox.forEach((outboxMessage: Outbox) => {
+    if (
+      outboxMessage.display_recipient === narrow[0].operand
+      && outboxMessage.subject === narrow[1].operand
+    ) {
+      dispatch(deleteOutboxMessage(outboxMessage.id));
+    }
+  });
+  const currentStream: Stream | void = getStreams(state).find(
+    (stream: Stream) => stream.name === message.display_recipient,
+  );
+  if (currentStream) {
+    await api.deleteTopic(getAuth(state), currentStream.stream_id, narrow[1].operand);
+  }
 };
