7269c9eaa6554085aff305a16d80cd7710d51ee8
sumukhah <sumukha214@gmail.com>
2020-02-14 00:00:50 +0530

    Add new account: Fix scrolling behaviour of Account Pick screen
    "Add new account" button becomes invisible as the number of user accountList
    grows.So make entire AccountPickScreen scrollable.
    
    Fixes: 3903

--- src/common/Screen.js
+++ src/common/Screen.js
@@ -64,6 +64,7 @@ type Props = $ReadOnly<{|
  * @prop [search] - If 'true' show a search box in place of the title.
  * @prop [autoFocus] - If search bar enabled, should it be focused initially.
  * @prop [searchBarOnChange] - Event called on search query change.
+
  * @prop [canGoBack] - If true (the default), show UI for "navigate back".
  * @prop [title] - Text shown as the title of the screen.
  *                 Required unless `search` is true.
