a6e1c02ad5161d7c1689634f9ada122650231dd2
sumukhah <sumukha214@gmail.com>
2020-02-14 00:30:48 +0530

    Add new account: Fix scrolling behaviour of Account Pick screen
    The verticle scroll bar in Account Pick Screen overlays on
    "Add new Account" button, this doesnot look good, So hide
    vericle scrollbar in Account Pick screen.

--- src/common/Screen.js
+++ src/common/Screen.js
@@ -38,6 +38,7 @@ type Props = $ReadOnly<{|
   keyboardShouldPersistTaps: 'never' | 'always' | 'handled',
   padding: boolean,
   scrollEnabled: boolean,
+  verticalScrollIndicatorEnabled: boolean,
   style?: ViewStyleProp,
 
   search: boolean,
@@ -59,6 +60,7 @@ type Props = $ReadOnly<{|
  * @prop [keyboardShouldPersistTaps] - Passed through to ScrollView.
  * @prop [padding] - Should padding be added to the contents of the screen.
  * @prop [scrollEnabled] - Passed through to ScrollView.
+ * @prop [verticalScrollIndicatorEnabled] - Passed through to ScrollView
  * @prop [style] - Additional style for the ScrollView.
  *
  * @prop [search] - If 'true' show a search box in place of the title.
@@ -81,6 +83,7 @@ class Screen extends PureComponent<Props> {
     keyboardShouldPersistTaps: 'handled',
     padding: false,
     scrollEnabled: true,
+    verticalScrollIndicatorEnabled: true,
 
     search: false,
     autoFocus: false,
@@ -100,6 +103,7 @@ class Screen extends PureComponent<Props> {
       padding,
       safeAreaInsets,
       scrollEnabled,
+      verticalScrollIndicatorEnabled,
       search,
       searchBarOnChange,
       style,
@@ -126,6 +130,7 @@ class Screen extends PureComponent<Props> {
             style={componentStyles.childrenWrapper}
             keyboardShouldPersistTaps={keyboardShouldPersistTaps}
             scrollEnabled={scrollEnabled}
+            showsVerticalScrollIndicator={verticalScrollIndicatorEnabled}
           >
             {children}
           </ScrollView>
