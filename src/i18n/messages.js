539f1152a3330b5401db9004d7065014409130b9
Greg Price <greg@zulipchat.com>
2020-02-13 16:28:13 -0800

    i18n [nfc]: Make the semi-mechanical changes needed for new languages.
    For the libdef in flow-typed/, I reran the command in the comment
    at the top.
    
    For the other two, I copied changes from a draft branch of Ray's (now
    PR #3915) which includes some scripts to fully automate generating
    all these files.  I dropped parts of that branch's changes where it
    fixed naming so that we can, e.g., offer both zh-Hans and zh-Hant --
    we should do that, but it'll need a migration for existing users who
    have `zh` in their settings, so it's not as completely trivial as the
    rest of these changes.
    
    Co-authored-by: Ray Kraesig <rkraesig@zulipchat.com>

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -5,25 +5,36 @@ import ar from '../../static/translations/messages_ar.json';
 import bg from '../../static/translations/messages_bg.json';
 import ca from '../../static/translations/messages_ca.json';
 import cs from '../../static/translations/messages_cs.json';
+import da from '../../static/translations/messages_da.json';
 import de from '../../static/translations/messages_de.json';
+import el from '../../static/translations/messages_el.json';
 import en from '../../static/translations/messages_en.json';
 import es from '../../static/translations/messages_es.json';
+import fa from '../../static/translations/messages_fa.json';
+import fi from '../../static/translations/messages_fi.json';
 import fr from '../../static/translations/messages_fr.json';
+import gl from '../../static/translations/messages_gl.json';
 import hi from '../../static/translations/messages_hi.json';
+import hr from '../../static/translations/messages_hr.json';
 import hu from '../../static/translations/messages_hu.json';
 import id from '../../static/translations/messages_id_ID.json';
 import it from '../../static/translations/messages_it.json';
 import ja from '../../static/translations/messages_ja.json';
 import ko from '../../static/translations/messages_ko.json';
+import lt from '../../static/translations/messages_lt.json';
 import ml from '../../static/translations/messages_ml.json';
 import nl from '../../static/translations/messages_nl.json';
 import pl from '../../static/translations/messages_pl.json';
 import pt from '../../static/translations/messages_pt.json';
+import ro from '../../static/translations/messages_ro.json';
 import ru from '../../static/translations/messages_ru.json';
 import sr from '../../static/translations/messages_sr.json';
 import sv from '../../static/translations/messages_sv.json';
 import ta from '../../static/translations/messages_ta.json';
 import tr from '../../static/translations/messages_tr.json';
+import uk from '../../static/translations/messages_uk.json';
+import uz from '../../static/translations/messages_uz.json';
+import vi from '../../static/translations/messages_vi.json';
 import zh from '../../static/translations/messages_zh-Hans.json';
 
 export default {
@@ -31,24 +42,35 @@ export default {
   bg,
   ca,
   cs,
+  da,
   de,
+  el,
   en,
   es,
+  fa,
+  fi,
   fr,
+  gl,
   hi,
+  hr,
   hu,
   id,
   it,
   ja,
   ko,
+  lt,
   ml,
   nl,
   pl,
   pt,
+  ro,
   ru,
   sr,
   sv,
   ta,
   tr,
+  uk,
+  uz,
+  vi,
   zh,
 };
