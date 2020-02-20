0b47a11a319f821fefe11dc88cd1673364a8061f
Ray Kraesig <rkraesig@zulipchat.com>
2020-02-20 14:13:26 -0800

    notifications [nfc]: Update `Notification` type's JSDoc.
    It's now the case that Notification objects are always exact on `iOS`.
    Remove the old warning about the type's potential inexactness.

--- src/notification/types.js
+++ src/notification/types.js
@@ -3,21 +3,14 @@
 /**
  * The data we need in JS/React code for acting on a notification.
  *
- * The actual objects we describe with these types may have a bunch more
- * fields.  So, properly, these object types aren't really exact.  But
- * pretending they are seems to be the least unpleasant way to get Flow to
- * recognize the effect of refinements like `data.pm_users === undefined`.
- *
  * Currently:
+ *  * On iOS, these objects are reconstructed in React Native from the APNs
+ *    payload; the exactness of these type definitions is checked by Flow. (See
+ *    `fromAPNs` in `src/notifications/extract.js`.)
  *
- *  * On iOS, these objects are translated directly, field by field, from
- *    the blobs of key/value pairs sent by the Zulip server in the
- *    "payload".  The set of fields therefore varies by server version.
- *
- *  * On Android, our platform-native code constructs the exact data to
- *    send; see `MessageFcmMessage#dataForOpen` in `FcmMessage.kt`.
- *    That should be kept in sync with this type definition, in which case
- *    these types really are exact.
+ *  * On Android, these objects are constructed by casting JSON objects from our
+ *    platform-native code; they are exact iff that code is kept in sync. (See
+ *    `MessageFcmMessage#dataForOpen` in `FcmMessage.kt`.)
  */
 export type Notification =
   | {| recipient_type: 'stream', stream: string, topic: string, realm_uri?: string |}
