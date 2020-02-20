2e2355cde37aede63141b192cba761b9f08d0233
Ray Kraesig <rkraesig@zulipchat.com>
2020-02-20 13:59:11 -0800

    types [nfc]: Define JSONableInput and JSONableInputDict.
    ... as a workaround for Flow's unsoundness where indexer-property
    accesses are concerned. (See interior comments.)

--- src/utils/jsonable.js
+++ src/utils/jsonable.js
@@ -45,3 +45,56 @@ export type JSONableDict = { +[string]: JSONable };
 //
 // ... which is consistent with the definition of inexact types, if
 // inconvenient.
+
+/**
+ * Approximate type of a JSONable value received as input.
+ *
+ * Unlike the definition of `JSONableDict`, this permits the possibility of
+ * `void` elements as dictionary members. This is a workaround for Flow's
+ * by-design soundness hole involving indexer elements:
+ *
+ * ```flow
+ *   const obj: { [string]: string } = { a: "a" };
+ *   const value: string = obj['b'];
+ *   value.trim();  // kaboom!
+ * ```
+ *
+ * ... that is, Flow will assume **all subscript accesses to indexer-property-
+ * typed values are valid**, and will not mark values extracted therefrom as
+ * potentially undefined. (For references indicating that this is, indeed, by
+ * design, see [0] and [1].)
+ *
+ * Adding `void` as a possible dictionary entry type to JSONable would solve
+ * this problem -- but only at the cost of introducing others:
+ *   * For output, it is possible to feed a `void`-containing structure to a
+ *     serializing function, even though that structure cannot be reversibly
+ *     JSON- serialized.
+ *   * When processing input, `Object.values` (and related functions) will yield
+ *     a type which contains `void`, even though no value in a just-parsed JSON
+ *     object can actually be `undefined`.
+ *
+ * We can mostly avoid these problems by creating a secondary JSONable-like
+ * type, `JSONableInput`, used only for validation and transformation of JSON
+ * input data.
+ *
+ * As JSONable <: JSONableInput, but not vice versa, JSON values should
+ * generally be passed around as `JSONable` until and unless they're actually
+ * being validated; they can then be locally cast to `JSONableInput`.
+ *
+ * - [0] https://flow.org/en/docs/types/objects/#toc-objects-as-maps
+ * - [1] https://github.com/facebook/flow/issues/6568
+ */
+export type JSONableInput =
+  | null
+  | string
+  | number
+  | boolean
+  | { +[string]: JSONableInput | void }
+  | $ReadOnlyArray<JSONableInput>;
+
+/**
+ * Type of a dictionary from JSON input.
+ *
+ * See documentation of `JSONable` and `JSONableInput` for caveats.
+ */
+export type JSONableInputDict = { +[string]: JSONableInput | void };
