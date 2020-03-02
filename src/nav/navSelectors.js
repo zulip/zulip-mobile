9e676e9fd1fc88608c3b9307cd3e65ff7503de33
sumukhah <sumukha214@gmail.com>
2020-03-02 20:05:20 +0530

    navigation: Fix back navigation from screens
    at present navigations are recorded inside a stack, as the result
    whenever user visits a screen will be pushed to stack, when user
    presses back, top nav  will be popped. But when 2 screens are inter-
    linked, in that case if user tried to navigate sequencially
    (mainScreen => a => b => a => b), it becomes hard to navigate back.
    He has to come by reverse order to reach back
    (b => a => b => a => mainScreen).This looks very hard for use.
    
    So pop untill the first existence of a navigation Screen.
    
    Fixes: #3941

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -45,13 +45,13 @@ export const getCanGoBack = (state: GlobalState) => state.nav.index > 0;
 export const getSameRoutesCount: Selector<number> = createSelector(
   getNav,
   nav => {
-    let i = nav.routes.length - 1;
-    while (i >= 0) {
-      if (nav.routes[i].routeName !== nav.routes[nav.routes.length - 1].routeName) {
-        break;
+    let i = 0;
+    while (i < nav.routes.length) {
+      if (nav.routes[i].routeName === nav.routes[nav.routes.length - 1].routeName) {
+        return nav.routes.length - i;
       }
-      i--;
+      i++;
     }
-    return nav.routes.length - i - 1;
+    return 0;
   },
 );
