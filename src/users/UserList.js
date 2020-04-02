6ea66eb51e4767e3c29fc8537d5276e8f7ae30c4
sumukhah <sumukha214@gmail.com>
2020-04-02 13:18:37 +0530

    UserList: Show Email address of every user in UserList
    Identifying a user by using only User-name makes difficult to
    search for a user.So display email address of users in a list
    along with the user-name.

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -57,6 +57,7 @@ export default class UserList extends PureComponent<Props> {
             email={item.email}
             onPress={onPress}
             isSelected={!!selected.find(user => user.user_id === item.user_id)}
+            showEmail
           />
         )}
         renderSectionHeader={({ section }) =>
