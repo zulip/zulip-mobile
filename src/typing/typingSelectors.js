f31fde7396b1a5349932ec9a3767f26e1f2cce99
Rohit <rohitmpaul@gmail.com>
2020-04-25 16:32:56 +0530

    typing status: Fix crash when viewing PMs with Welcome Bot.
    Issue was caused because `getUsersByEmail` couldn't determine
    the user-id of Welcome Bot. This is fixed in the PR by making use
    of `getAllUsersByEmail` instead.
    
    Fixes: #4067
    
    [ray: Rewrote summary; minor copyedits to commit message.]

--- src/typing/typingSelectors.js
+++ src/typing/typingSelectors.js
@@ -6,20 +6,20 @@ import { getTyping } from '../directSelectors';
 import { isPrivateOrGroupNarrow } from '../utils/narrow';
 import { normalizeRecipientsAsUserIds } from '../utils/recipient';
 import { NULL_ARRAY, NULL_USER } from '../nullObjects';
-import { getUsersById, getUsersByEmail } from '../users/userSelectors';
+import { getUsersById, getAllUsersByEmail } from '../users/userSelectors';
 
 export const getCurrentTypingUsers: Selector<$ReadOnlyArray<User>, Narrow> = createSelector(
   (state, narrow) => narrow,
   state => getTyping(state),
   state => getUsersById(state),
-  state => getUsersByEmail(state),
-  (narrow, typing, usersById, usersByEmail): User[] => {
+  state => getAllUsersByEmail(state),
+  (narrow, typing, usersById, allUsersByEmail): User[] => {
     if (!isPrivateOrGroupNarrow(narrow)) {
       return NULL_ARRAY;
     }
 
     const recipients = narrow[0].operand.split(',').map(email => {
-      const userId = usersByEmail.get(email)?.user_id;
+      const userId = allUsersByEmail.get(email)?.user_id;
       if (userId === undefined) {
         throw new Error(`Narrow contains email '${email}' that does not map to any user.`);
       }
