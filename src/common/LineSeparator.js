zulip/zulip-mobile

commit 51dd1b3b2ee04093ac6842e5871dcd04c53ac0f6

styles: Pass core theme data down as new-style context.
And demonstrate with one example component and style object,
`LineSeparator` and `styles.lineSeparator`.

To combine the theme data with details like height and margin that are
about the component and not the theme, use the same pattern introduced
in a2bfcb41b on ComposeBox: a class initializer.

src/common/LineSeparator.js
@@ -2,16 +2,22 @@
 import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 
-import type { Context } from '../types';
+import type { ThemeColors } from '../styles';
+import { ThemeContext } from '../styles';
 
 export default class LineSeparator extends PureComponent<{}> {
-  context: Context;
+  static contextType = ThemeContext;
+  context: ThemeColors;
 
-  static contextTypes = {
-    styles: () => null,
+  styles = {
+    lineSeparator: {
+      height: 1,
+      backgroundColor: this.context.cardColor,
+      margin: 4,
+    },
   };
 
   render() {
-    return <View style={this.context.styles.lineSeparator} />;
+    return <View style={this.styles.lineSeparator} />;
   }
 }

src/boot/StylesProvider.js
@@ -5,7 +5,7 @@ import React, { PureComponent } from 'react';
 
 import type { ChildrenArray, GlobalState, ThemeName } from '../types';
 import { getSettings } from '../directSelectors';
-import { stylesFromTheme } from '../styles/theme';
+import { stylesFromTheme, themeColors, ThemeContext } from '../styles/theme';
 
 const Dummy = props => props.children;
 
@@ -32,7 +32,11 @@ class StyleProvider extends PureComponent<Props> {
   render() {
     const { children, theme } = this.props;
 
-    return <Dummy key={theme}>{children}</Dummy>;
+    return (
+      <ThemeContext.Provider value={themeColors[theme]}>
+        <Dummy key={theme}>{children}</Dummy>
+      </ThemeContext.Provider>
+    );
   }
 }
 

src/styles/index.js
@@ -7,6 +7,8 @@ import { statics as navStyles } from './navStyles';
 import utilityStyles from './utilityStyles';
 
 export * from './constants';
+export type { ThemeColors } from './theme';
+export { ThemeContext } from './theme';
 
 export default StyleSheet.create({
   ...composeBoxStyles,

src/styles/miscStyles.js
@@ -58,7 +58,7 @@ export const statics = {
   },
 };
 
-export default ({ color, backgroundColor, cardColor, dividerColor }: ThemeColors) => ({
+export default ({ color, backgroundColor, dividerColor }: ThemeColors) => ({
   color: {
     color,
   },
@@ -104,10 +104,5 @@ export default ({ color, backgroundColor, cardColor, dividerColor }: ThemeColors
   divider: {
     borderBottomWidth: 1,
     borderBottomColor: dividerColor,
-  },
-  lineSeparator: {
-    height: 1,
-    backgroundColor: cardColor,
-    margin: 4,
   },
 });

src/styles/theme.js
@@ -1,4 +1,5 @@
 /* @flow strict-local */
+import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import type { ThemeName } from '../types';
@@ -17,7 +18,7 @@ export type AppStyles = $ReadOnly<{|
   ...$Call<typeof miscStyles, ThemeColors>,
 |}>;
 
-const themeColors: { [string]: ThemeColors } = {
+export const themeColors: { [string]: ThemeColors } = {
   night: {
     color: '#d5d9dd',
     backgroundColor: '#212D3B',
@@ -36,6 +37,8 @@ const themeColors: { [string]: ThemeColors } = {
   },
 };
 themeColors.default = themeColors.light;
+
+export const ThemeContext = React.createContext(themeColors.default);
 
 export const stylesFromTheme = (name: ThemeName) => {
   const colors = themeColors[name];

commit dfd9cda09bd4c08d69067b4b76c6aa1c856b2ebb

theme: Fix latent bug with colors not updating on rerender.
Compute theme-dependent styles in the render method.

As foreshadowed in a previous commit that touched
src/common/Screen.js, fix the remaining latent bugs where the
placement of theme-dependent styles in a class initializer, instead
of the `render` method, meant that these styles don't live-update
when the theme changes, unless the component is unmounted and a new
instance is created to replace it.

The `key={theme}` hack accomplished this replacement, so this bug
hasn't been visible. But that hack will disappear in an upcoming
commit, so this change needs to be made before then.

These bugs were introduced in

51dd1b3b2: LineSeparator.js
a4e0f23b3: OptionButton.js and OptionRow.js
f6ddc2dba: OptionDivider.js

src/common/LineSeparator.js
@@ -12,12 +12,13 @@ export default class LineSeparator extends PureComponent<{||}> {
   styles = {
     lineSeparator: {
       height: 1,
-      backgroundColor: this.context.cardColor,
       margin: 4,
     },
   };
 
   render() {
-    return <View style={this.styles.lineSeparator} />;
+    return (
+      <View style={[this.styles.lineSeparator, { backgroundColor: this.context.cardColor }]} />
+    );
   }
 }

src/common/OptionButton.js
@@ -20,10 +20,7 @@ export default class OptionButton extends PureComponent<Props> {
   context: ThemeColors;
 
   styles = {
-    icon: {
-      ...styles.settingsIcon,
-      color: this.context.color,
-    },
+    icon: styles.settingsIcon,
   };
 
   render() {
@@ -32,10 +29,10 @@ export default class OptionButton extends PureComponent<Props> {
     return (
       <Touchable onPress={onPress}>
         <View style={styles.listItem}>
-          {!!Icon && <Icon size={18} style={this.styles.icon} />}
+          {!!Icon && <Icon size={18} style={[this.styles.icon, { color: this.context.color }]} />}
           <Label text={label} />
           <View style={styles.rightItem}>
-            <IconRight size={18} style={this.styles.icon} />
+            <IconRight size={18} style={[this.styles.icon, { color: this.context.color }]} />
           </View>
         </View>
       </Touchable>

src/common/OptionDivider.js
@@ -12,11 +12,10 @@ export default class OptionDivider extends PureComponent<{||}> {
   styles = {
     divider: {
       borderBottomWidth: 1,
-      borderBottomColor: this.context.dividerColor,
     },
   };
 
   render() {
-    return <View style={this.styles.divider} />;
+    return <View style={[this.styles.divider, { borderBottomColor: this.context.dividerColor }]} />;
   }
 }

src/common/OptionRow.js
@@ -22,10 +22,7 @@ export default class OptionRow extends PureComponent<Props> {
   context: ThemeColors;
 
   styles = {
-    icon: {
-      ...styles.settingsIcon,
-      color: this.context.color,
-    },
+    icon: styles.settingsIcon,
   };
 
   render() {
@@ -33,7 +30,7 @@ export default class OptionRow extends PureComponent<Props> {
 
     return (
       <View style={[styles.listItem, style]}>
-        {!!Icon && <Icon size={18} style={this.styles.icon} />}
+        {!!Icon && <Icon size={18} style={[this.styles.icon, { color: this.context.color }]} />}
         <Label text={label} style={styles.flexed} />
         <View style={styles.rightItem}>
           <ZulipSwitch value={value} onValueChange={onValueChange} />