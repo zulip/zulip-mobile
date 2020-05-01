751366b4e9990eab2f9e5434c6f8ed3bd3e40339
sumukhah <sumukha214@gmail.com>
2020-05-01 23:37:17 +0530

    Alert words: Use AlertWordsState type for alert_words
    AlertWordsState is meant to be the type of alert_words. So use
    it in type instead of creating own type.

--- src/api/alert_words/getAlertWords.js
+++ src/api/alert_words/getAlertWords.js
@@ -1,10 +1,11 @@
 /* @flow strict-local */
 import type { Auth, ApiResponseSuccess } from '../transportTypes';
 import { apiGet } from '../apiFetch';
+import type { AlertWordsState } from '../../types';
 
 type ApiResponseAlertWords = {|
   ...ApiResponseSuccess,
-  alert_words: string[],
+  alert_words: AlertWordsState,
 |};
 
 export default async (auth: Auth): Promise<ApiResponseAlertWords> =>
