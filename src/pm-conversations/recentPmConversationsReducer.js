fcc3b538689f63e6a92ea0c814adf58ac65c056b
Ray Kraesig <rkraesig@zulip.com>
2020-05-08 18:13:46 -0700

    recent convs reducer: Sort and retain user IDs received from server.
    Code to sort the `user_id`s in `recent_private_conversations` was not
    added until 2.2-dev-53-g405a529340 (zulip/zulip@405a529340). This was
    backported to the 2.1 branch as 2.1.1-50-gd452ad31e0
    (zulip/zulip@d452ad31e0).
    
    Since we can't easily access the current server version here (yet?),
    just unconditionally sort the data.
    
    This is sufficient for us to begin storing the server data.

--- src/pm-conversations/recentPmConversationsReducer.js
+++ src/pm-conversations/recentPmConversationsReducer.js
@@ -5,13 +5,28 @@ import { NULL_ARRAY } from '../nullObjects';
 
 const initialState: RecentPrivateConversationsState = NULL_ARRAY;
 
+const realmInit = (state, action) => {
+  // If this is a pre-2.1 server, we'll have to get by without.
+  if (action.data.recent_private_conversations === undefined) {
+    return initialState;
+  }
+
+  // `user_id`s are not guaranteed to be sorted prior to v2.1.2.
+  // (Take care not to mutate the input.)
+  return action.data.recent_private_conversations.map(({ user_ids, ...rest }) => ({
+    ...rest,
+    user_ids: [...user_ids].sort((a, b) => a - b),
+  }));
+};
+
 export default (
   state: RecentPrivateConversationsState = initialState,
   action: Action,
 ): RecentPrivateConversationsState => {
   switch (action.type) {
     case REALM_INIT:
-      return /* action.data.recent_private_conversations || */ initialState;
+      return realmInit(state, action);
+
     default:
       return state;
   }
