d36e1c06fe11288c5947bebc16b24b8b1d8e38dc
Ray Kraesig <rkraesig@zulip.com>
2020-05-08 19:43:47 -0700

    selectors [nfc]: Extract fragments of `getRecentConversations`.
    We're about to add an alternate implementation of this function, but
    we'll need to keep the existing version to avoid loss of functionality
    on pre-2.1 servers.
    
    Preemptively factor some fragments of the existing implementation out,
    as we'll need to use them in more and/or different places.

--- src/pm-conversations/pmConversationsSelectors.js
+++ src/pm-conversations/pmConversationsSelectors.js
@@ -7,36 +7,46 @@ import { getOwnUser } from '../users/userSelectors';
 import { getUnreadByPms, getUnreadByHuddles } from '../unread/unreadSelectors';
 import { normalizeRecipientsSansMe, pmUnreadsKeyFromMessage } from '../utils/recipient';
 
-export const getRecentConversations: Selector<PmConversationData[]> = createSelector(
-  getOwnUser,
-  getPrivateMessages,
+/**
+ * Given a list of PmConversationPartial or PmConversationData, trim it down to
+ * contain only the most recent message from any conversation, and return them
+ * sorted by recency.
+ */
+const collateByRecipient = <T: { recipients: string, msgId: number }>(
+  items: $ReadOnlyArray<T>,
+): T[] => {
+  const latestByRecipient = new Map();
+  items.forEach(item => {
+    const prev = latestByRecipient.get(item.recipients);
+    if (!prev || item.msgId > prev.msgId) {
+      latestByRecipient.set(item.recipients, item);
+    }
+  });
+
+  const sortedByMostRecent = Array.from(latestByRecipient.values()).sort(
+    (a, b) => +b.msgId - +a.msgId,
+  );
+
+  return sortedByMostRecent;
+};
+
+type PmConversationPartial = $Diff<PmConversationData, {| unread: mixed |}>;
+type UnreadAttacher = ($ReadOnlyArray<PmConversationPartial>) => PmConversationData[];
+
+/**
+ * Auxiliary function: fragment of the selector(s) for PmConversationData.
+ * Transforms PmConversationPartial[] to PmConversationData[].
+ *
+ * Note that, since this will only ever be called by other selectors, the inner
+ * function doesn't need to do any memoization of its own.
+ */
+const getAttachUnread: Selector<UnreadAttacher> = createSelector(
   getUnreadByPms,
   getUnreadByHuddles,
-  (
-    ownUser: User,
-    messages: Message[],
-    unreadPms: { [number]: number },
-    unreadHuddles: { [string]: number },
-  ): PmConversationData[] => {
-    const items = messages.map(msg => ({
-      ids: pmUnreadsKeyFromMessage(msg, ownUser.user_id),
-      recipients: normalizeRecipientsSansMe(msg.display_recipient, ownUser.email),
-      msgId: msg.id,
-    }));
-
-    const latestByRecipient = new Map();
-    items.forEach(item => {
-      const prev = latestByRecipient.get(item.recipients);
-      if (!prev || item.msgId > prev.msgId) {
-        latestByRecipient.set(item.recipients, item);
-      }
-    });
-
-    const sortedByMostRecent = Array.from(latestByRecipient.values()).sort(
-      (a, b) => +b.msgId - +a.msgId,
-    );
-
-    return sortedByMostRecent.map(recipient => ({
+  (unreadPms: { [number]: number }, unreadHuddles: { [string]: number }) => (
+    partials: $ReadOnlyArray<PmConversationPartial>,
+  ) =>
+    partials.map(recipient => ({
       ...recipient,
       unread:
         // This business of looking in one place and then the other is kind
@@ -47,7 +57,23 @@ export const getRecentConversations: Selector<PmConversationData[]> = createSele
          end up converted to strings, so this access with string keys works.  We should probably use
          a Map for this and similar maps. */
         unreadPms[recipient.ids] || unreadHuddles[recipient.ids],
+    })),
+);
+
+export const getRecentConversations: Selector<PmConversationData[]> = createSelector(
+  getOwnUser,
+  getPrivateMessages,
+  getAttachUnread,
+  (ownUser: User, messages: Message[], attachUnread: UnreadAttacher): PmConversationData[] => {
+    const items = messages.map(msg => ({
+      ids: pmUnreadsKeyFromMessage(msg, ownUser.user_id),
+      recipients: normalizeRecipientsSansMe(msg.display_recipient, ownUser.email),
+      msgId: msg.id,
     }));
+
+    const sortedByMostRecent = collateByRecipient(items);
+
+    return attachUnread(sortedByMostRecent);
   },
 );
 
