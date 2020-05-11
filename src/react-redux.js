be2663f239aa560513c89901588ab210e5d29c5e
Greg Price <greg@zulipchat.com>
2020-05-11 14:28:50 -0700

    connect [nfc]: Expand jsdoc on our `connect` type-wrapper.
    The caveat where no-argument `connect` needs an explicit type
    parameter or else it doesn't get properly type-checked is one
    that should definitely get written down.
    
    The `SelectorProps` pattern wasn't documented either, so take
    this as a prompt to record that too.

--- src/react-redux.js
+++ src/react-redux.js
@@ -56,10 +56,34 @@ export type OwnProps<-C, -SP> = $Diff<
 /**
  * Exactly like the `connect` in `react-redux` upstream, but more typed.
  *
+ * NOTE:
+ *  * This version works great with a `mapStateToProps` that takes only
+ *    the state and not the outer props.
+ *
+ *  * For two-argument `mapStateToProps`, Flow needs a little help: write
+ *
+ *        connect<SelectorProps, _, _>((state, props) => …)(MyComponent)
+ *
+ *    where `SelectorProps` describes the return value of `mapStateToProps`.
+ *    This type will get fully checked; Flow just can't infer it.
+ *    See our commit fcde7005e, and previous discussion at f8c981028.
+ *
+ *  * For no-argument `connect`, Flow also needs help: write
+ *
+ *        connect<{||}, _, _>()(MyComponent)
+ *
+ *    Awkwardly, in this case Flow will actually infer the wrong thing if
+ *    the explicit `{||}` is omitted, and fail to catch errors.  See our
+ *    commit 1e0006e5b.
+ *    TODO: fix this, perhaps with an overload.
+ *
  * The upstream function, and its libdef in `flow-typed`, has an extremely
  * complex type involving many overloads, and it's difficult to get useful
  * results with it from the type-checker.  This trivial wrapper leaves out a
  * bunch of features we don't use, and then gives a more specific type.
+ *
+ * This version also takes advantage of being in our codebase, rather than a
+ * general libdef, by encoding once and for all the type of our Redux state.
  *
  * (Most of those features probably *could* be supported with a more-helpful
  * type like this if desired, just with a moderate amount of further work.
