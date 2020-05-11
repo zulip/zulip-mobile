de6c66cebcc01f516b52ad9a5c70b246a668d450
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-05-11 13:28:43 +0530

    compose: Create component to warn on unsubscribed @-mentionee.
    Creates a component 'MentionedUserNotSubscribed' that can be used to
    shows a warning when an @-mentioned user is not subscribed to a stream
    they are mentioned in, with a button to subscribe them.

---
+++ src/message/MentionedUserNotSubscribed.js
@@ -0,0 +1,84 @@
+/* @flow strict-local */
+
+import React, { PureComponent } from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+
+import type { Auth, Stream, Dispatch, UserOrBot, Subscription } from '../types';
+import { connect } from '../react-redux';
+import * as api from '../api';
+import { ZulipButton, Label } from '../common';
+import { getAuth } from '../selectors';
+
+type SelectorProps = {|
+  auth: Auth,
+|};
+
+type Props = $ReadOnly<{|
+  user: UserOrBot,
+  stream: Subscription | {| ...Stream, in_home_view: boolean |},
+  onDismiss: (user: UserOrBot) => void,
+
+  dispatch: Dispatch,
+  ...SelectorProps,
+|}>;
+
+const styles = StyleSheet.create({
+  mentionedUserNotSubscribed: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'hsl(40, 100%, 60%)', // Material warning-color
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderTopWidth: 1,
+    borderTopColor: 'orange',
+  },
+  mentionedUserNotSubscribedText: {
+    flex: 1,
+    color: 'white',
+  },
+  mentionedUserNotSubscribedButton: {
+    backgroundColor: 'orange',
+    padding: 6,
+  },
+});
+
+class MentionedUserNotSubscribed extends PureComponent<Props> {
+  subscribeToStream = () => {
+    const { auth, stream, user } = this.props;
+    api.subscriptionAdd(auth, [{ name: stream.name }], [user.email]);
+    this.handleDismiss();
+  };
+
+  handleDismiss = () => {
+    const { user, onDismiss } = this.props;
+    onDismiss(user);
+  };
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <View>
+        <TouchableOpacity onPress={this.handleDismiss} style={styles.mentionedUserNotSubscribed}>
+          <Label
+            text={{
+              text: '{username} will not be notified unless you subscribe them to this stream.',
+              values: { username: user.full_name },
+            }}
+            style={styles.mentionedUserNotSubscribedText}
+          />
+          <ZulipButton
+            style={styles.mentionedUserNotSubscribedButton}
+            text="Subscribe"
+            onPress={this.subscribeToStream}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default connect<SelectorProps, _, _>((state, props) => ({
+  auth: getAuth(state),
+}))(MentionedUserNotSubscribed);
