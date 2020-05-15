3cdb8a248831ef7c70fefa72e0f3c2aa03acf715
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-05-15 21:10:01 +0530

    api: Create helper tryGetFileDownloadUrl.js
    Using the new endpoint `/user_uploads/{realm_id_str}/{filename}`
    it generates a temporary URL that does not require auth to access.
    
    This allows the user to open the file in the browser without being
    logged in to the realm, or to directly download it without
    redirects to S3 or needing to send auth headers. If generation of
    the url fails, it returns `null`.

---
+++ src/api/tryGetFileDownloadUrl.js
@@ -0,0 +1,21 @@
+/* @flow strict-local */
+import type { Auth } from './apiTypes';
+import getTemporaryFileUrl from './messages/getTemporaryFileUrl';
+import { getFullUrl } from '../utils/url';
+
+/**
+ * Get the complete authless URL to a realm uploaded file, returns null if that
+ * fails.
+ *
+ * See aslo jsdoc on getTemporaryFileUrl.
+ *
+ * @param href URL path to the resource.
+ * @param auth Current user's auth information.
+ */
+export default async (href: string, auth: Auth): Promise<string | null> => {
+  try {
+    return getFullUrl((await getTemporaryFileUrl(auth, href)).url, auth.realm);
+  } catch (err) {
+    return null;
+  }
+};
