1f2657d3ddacea21bd9b4787c94d3056ba605d67
Chris Bobbe <cbobbe@zulip.com>
2020-05-29 18:12:04 -0700

    prettier [nfc]: Don't take some Prettier config in the RN 60 upgrade.
    This commit marks part of the RN v0.59 -> v0.60 changes to the
    template app [1] that we don't take. This commit may freely be
    positioned before the upgrade.
    
    Corresponds to facebook/react-native@f4d5e8c23, released in RN
    v0.60.5.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,14 @@
 module.exports = {
+  /**
+   * These two were added to the RN template file in
+   * `facebook/react-native@f4d5e8c23` (released in 0.60.5) because of
+   * conflicts with `eslint-config-react-native-community`. We haven't
+   * activated that config (we might, it's #4119), and we haven't
+   * otherwise found a use for these rules; we don't follow them.
+   */
+  // bracketSpacing: false,
+  // jsxBracketSameLine: true,
+
   printWidth: 100,
   parser: 'babel-flow',
   singleQuote: true,
