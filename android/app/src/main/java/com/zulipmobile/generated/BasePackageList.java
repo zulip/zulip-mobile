e116c3f9d707b147af4919dc3d2b0e1680486fd0
Chris Bobbe <cbobbe@zulip.com>
2020-06-11 14:58:15 -0700

    deps: Upgrade `react-native-unimodules` to ^0.9.1, the latest.
    This is possible, now that the RN v0.60 -> v0.61 upgrade is
    complete! [1]
    
    Change the `@unimodules/core` version, following a valid instruction
    in bc27f2df2:
    
    """
    So, declare @unimodules/core in our own "dependencies", with the
    version specified by react-native-unimodules in its active version.
    This matching should be done each time we change versions of
    react-native-unimodules.
    """
    
    Also, remove some now-unnecessary code in `android/build.gradle`, as
    foreshadowed in cb87f9062:
    
    """
    Second, specify a new dependency for
    `unimodules-react-native-adapter` in our own `android/build.gradle`,
    in a fix that is necessary because we're locked on version 0.6.0 of
    react-native-unimodules. Filed as
    https://github.com/unimodules/react-native-unimodules/issues/130.
    """
    
    There are some reasonable, automatic changes to the
    version-controlled files that ensure that unimodules are
    automatically linked [2]: `ios/Podfile.lock` and
    `android/app/src/main/java/com/zulipmobile/generated/BasePackageList.java`.
    These look like some modules that are part of
    `react-native-unimodules`' core; these additions are caused by the
    upgrade and don't stem from running something like `yarn add
    expo-apple-authentication` to get a particular Unimodule.
    
    Also, run `yarn yarn-deduplicate && yarn` as prompted by
    `tools/test deps`.
    
    [1]: See discussion of the various obstacles at
         https://github.com/unimodules/react-native-unimodules/issues/97#issuecomment-637714639
         and
         https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/Podfile.20dependency.20of.20a.20dependency/near/892373.
    [2]: This is similar to but distinct from `react-native`'s
         "autolinking" feature that we activated in f46046027 and
         a9a9ac7b3. See
         https://github.com/unimodules/react-native-unimodules/issues/75#issuecomment-536517253
         for a comment on that.
    
    Fixes: #4091

--- android/app/src/main/java/com/zulipmobile/generated/BasePackageList.java
+++ android/app/src/main/java/com/zulipmobile/generated/BasePackageList.java
@@ -10,6 +10,7 @@ public class BasePackageList {
         new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.screenorientation.ScreenOrientationPackage()
     );
