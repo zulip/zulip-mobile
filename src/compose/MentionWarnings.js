a428d55aff4f12f78c3efb7f036961cc48308284
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-06-15 17:34:26 +0530

    compose: Create component to process unsubscribed mentions.
    Creates a component, `MentionWarnings`, that processes and renders
    warnings when a user that's not subscribed to a stream is
    mentioned. This is an uncontrolled component, and will be
    accessed using refs. Therefore, passing ` { withRef: true }`
    option in the `connect` function is required.

---
+++ src/compose/MentionWarnings.js
@@ -0,0 +1,186 @@
+/* @flow strict-local */
+
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+import type { Auth, Stream, Dispatch, Narrow, UserOrBot, Subscription, GetText } from '../types';
+import { TranslationContext } from '../boot/TranslationProvider';
+import { getActiveUsersById, getAuth } from '../selectors';
+import { isPrivateNarrow } from '../utils/narrow';
+import * as api from '../api';
+import { showToast } from '../utils/info';
+
+import AnimatedScaleComponent from '../animation/AnimatedScaleComponent';
+import MentionedUserNotSubscribed from '../message/MentionedUserNotSubscribed';
+
+type State = {|
+  unsubscribedMentions: Array<number>,
+|};
+
+type SelectorProps = {|
+  auth: Auth,
+  usersById: Map<number, UserOrBot>,
+|};
+
+type Props = $ReadOnly<{|
+  narrow: Narrow,
+  stream: Subscription | {| ...Stream, in_home_view: boolean |},
+
+  dispatch: Dispatch,
+  ...SelectorProps,
+|}>;
+
+class MentionWarnings extends PureComponent<Props, State> {
+  static contextType = TranslationContext;
+  context: GetText;
+
+  state = {
+    unsubscribedMentions: [],
+  };
+
+  /**
+   * Tries to parse a user object from an @-mention.
+   *
+   * @param completion The autocomplete option chosend by the user.
+      See JSDoc for AutoCompleteView for details.
+   */
+  getUserFromMention = (completion: string): UserOrBot | void => {
+    const { usersById } = this.props;
+
+    const unformattedMessage = completion.split('**')[1];
+    const [userFullName, userIdRaw] = unformattedMessage.split('|');
+
+    // We skip user groups, for which autocompletes are of the form
+    // `*<user_group_name>*`, and therefore, message.split('**')[1]
+    // is undefined.
+    if (unformattedMessage === undefined) {
+      return undefined;
+    }
+
+    if (userIdRaw !== undefined) {
+      const userId = Number.parseInt(userIdRaw, 10);
+      return usersById.get(userId);
+    }
+
+    for (const user of usersById.values()) {
+      if (user.full_name === userFullName) {
+        return user;
+      }
+    }
+
+    return undefined;
+  };
+
+  showSubscriptionStatusLoadError = (mentionedUser: UserOrBot) => {
+    const _ = this.context;
+
+    const alertTitle = _.intl.formatMessage(
+      {
+        id: "Couldn't load information about {fullName}",
+        defaultMessage: "Couldn't load information about {fullName}",
+      },
+      { fullName: mentionedUser.full_name },
+    );
+    showToast(alertTitle);
+  };
+
+  /**
+   * Check whether the message text entered by the user contains
+   * an @-mention to a user unsubscribed to the current stream, and if
+   * so, shows a warning.
+   *
+   * This function is expected to be called by `ComposeBox` using a ref
+   * to this component.
+   *
+   * @param completion The autocomplete option chosend by the user.
+      See JSDoc for AutoCompleteView for details.
+   */
+  handleMentionSubscribedCheck = async (completion: string) => {
+    const { narrow, auth, stream } = this.props;
+    const { unsubscribedMentions } = this.state;
+
+    if (isPrivateNarrow(narrow)) {
+      return;
+    }
+    const mentionedUser = this.getUserFromMention(completion);
+    if (mentionedUser === undefined || unsubscribedMentions.includes(mentionedUser.user_id)) {
+      return;
+    }
+
+    let isSubscribed: boolean;
+    try {
+      isSubscribed = (await api.getSubscriptionToStream(
+        auth,
+        mentionedUser.user_id,
+        stream.stream_id,
+      )).is_subscribed;
+    } catch (err) {
+      this.showSubscriptionStatusLoadError(mentionedUser);
+      return;
+    }
+
+    if (!isSubscribed) {
+      this.setState(prevState => ({
+        unsubscribedMentions: [...prevState.unsubscribedMentions, mentionedUser.user_id],
+      }));
+    }
+  };
+
+  handleMentionWarningDismiss = (user: UserOrBot) => {
+    this.setState(prevState => ({
+      unsubscribedMentions: prevState.unsubscribedMentions.filter(
+        (x: number) => x !== user.user_id,
+      ),
+    }));
+  };
+
+  clearMentionWarnings = () => {
+    this.setState({
+      unsubscribedMentions: [],
+    });
+  };
+
+  render() {
+    const { unsubscribedMentions } = this.state;
+    const { stream, narrow, usersById } = this.props;
+
+    if (isPrivateNarrow(narrow)) {
+      return null;
+    }
+
+    const mentionWarnings = [];
+    for (const userId of unsubscribedMentions) {
+      const user = usersById.get(userId);
+
+      if (user === undefined) {
+        continue;
+      }
+
+      mentionWarnings.push(
+        <MentionedUserNotSubscribed
+          stream={stream}
+          user={user}
+          onDismiss={this.handleMentionWarningDismiss}
+          key={user.user_id}
+        />,
+      );
+    }
+
+    return (
+      <AnimatedScaleComponent visible={mentionWarnings.length !== 0}>
+        {mentionWarnings}
+      </AnimatedScaleComponent>
+    );
+  }
+}
+
+// $FlowFixMe. TODO: Use a type checked connect call.
+export default connect(
+  state => ({
+    auth: getAuth(state),
+    usersById: getActiveUsersById(state),
+  }),
+  null,
+  null,
+  { withRef: true },
+)(MentionWarnings);
