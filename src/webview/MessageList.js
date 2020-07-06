73808aac47074a5743a2726ba5ecd49762e138b1
Chris Bobbe <cbobbe@zulip.com>
2020-07-06 14:00:56 -0700

    msglist: Use absolute URL for webview assets on iOS.
    In a recent commit, we exposed this from the native side of the
    React Native bridge. Use it, make `baseUrl` a URL object, and remove
    various hacks that are now unnecessary.
    
    We also rename `assetsPath` to `webviewAssetsUrl`: there's no
    question that it's a URL now, and it specifically refers to the
    `webview/` directory in the platform-specific assets folder.
    
    The platform-specific assets folder URL is extracted as `assetsUrl`.

--- src/webview/MessageList.js
+++ src/webview/MessageList.js
@@ -1,9 +1,10 @@
 /* @flow strict-local */
 import React, { Component } from 'react';
-import { Platform } from 'react-native';
+import { Platform, NativeModules } from 'react-native';
 import { WebView } from 'react-native-webview';
 import type { WebViewNavigation } from 'react-native-webview';
 import { connectActionSheet } from '@expo/react-native-action-sheet';
+import { URL as WhatwgURL } from 'react-native-url-polyfill';
 
 import type {
   AlertWordsState,
@@ -54,6 +55,7 @@ import { getUpdateEvents } from './webViewHandleUpdates';
 import { handleMessageListEvent } from './webViewEventHandlers';
 import { base64Utf8Encode } from '../utils/encoding';
 import * as logging from '../utils/logging';
+import { tryParseUrl } from '../utils/url';
 
 // ESLint doesn't notice how `this.props` escapes, and complains about some
 // props not being used here.
@@ -113,45 +115,33 @@ export type Props = $ReadOnly<{|
 |}>;
 
 /**
- * A URL-like magic string denoting the webview-assets folder.
+ * The URL of the platform-specific assets folder.
  *
- * Not suitable for reuse or export.
+ * - On iOS: We can't easily hardcode this because it includes UUIDs.
+ *   So we bring it over the React Native bridge in ZLPConstants.m.
+ *
+ * - On Android: Different apps' WebViews see different (virtual) root
+ *   directories as `file:///`, and in particular the WebView provides
+ *   the APK's `assets/` directory as `file:///android_asset/`. [1]
+ *   We can easily hardcode that, so we do.
+ *
+ * [1] Oddly, this essential feature doesn't seem to be documented!  It's
+ *     widely described in how-tos across the web and StackOverflow answers.
+ *     It's assumed in some related docs which mention it in passing, and
+ *     treated matter-of-factly in some Chromium bug threads.  Details at:
+ *     https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/android.20filesystem/near/796440
  */
-const assetsPath = Platform.OS === 'ios' ? './webview' : 'file:///android_asset/webview';
-// What the value above probably _should_ be, semantically, is an absolute
-// `file:`-scheme URL pointing to the webview-assets folder. [1]
-//
-// * On Android, that's exactly what it is. Different apps' WebViews see
-//   different (virtual) root directories as `file:///`, and in particular
-//   the WebView provides the APK's `assets/` directory as
-//   `file:///android_asset/`. [2]  We can easily hardcode that, so we do.
-//
-// * On iOS, it's not: it's a relative path. (Or relative URL, if you prefer.)
-//   We can't make it absolute here, because neither React Native itself nor any
-//   of our current dependencies directly expose the Foundation API that would
-//   tell us the absolute path that our bundle is located at [3].
-//
-//   Instead, for now, we exploit the fact that (the iOS version of)
-//   react-native-webview will have React Native resolve it with respect to
-//   the bundle's absolute path. [4]  This fact is not known to be
-//   documented, however, and should not be taken for granted indefinitely.
-//
-// [1] See `tools/build-webview` for more information on what folder that is.
-//
-// [2] Oddly, this essential feature doesn't seem to be documented!  It's
-//     widely described in how-tos across the web and StackOverflow answers.
-//     It's assumed in some related docs which mention it in passing, and
-//     treated matter-of-factly in some Chromium bug threads.  Details at:
-//     https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/android.20filesystem/near/796440
-//
-// [3] Specifically, `Bundle.main.bundleURL` (aka `[[NSBundle mainbundle]
-//     bundleURL]`).  Alternatively, `resourceURL` has a meaning similar to
-//     the `assets/` directory in an Android app; for iOS app bundles, it
-//     has the same value as `bundleURL`.
-//     https://github.com/zulip/zulip-mobile/pull/3677#discussion_r344423032
-//
-// [4] https://github.com/react-native-community/react-native-webview/blob/v5.12.1/ios/RNCWKWebView.m#L376
-//     https://github.com/facebook/react-native/blob/v0.59.10/React/Base/RCTConvert.m#L85
+const assetsUrl =
+  Platform.OS === 'ios'
+    ? new WhatwgURL(NativeModules.ZLPConstants.resourceURL)
+    : new WhatwgURL('file:///android_asset/');
+
+/**
+ * The URL of the webview-assets folder.
+ *
+ * This is the folder populated at build time by `tools/build-webview`.
+ */
+const webviewAssetsUrl = new WhatwgURL('webview/', assetsUrl);
 
 class MessageList extends Component<Props> {
   static contextType = ThemeContext;
@@ -254,12 +244,14 @@ class MessageList extends Component<Props> {
     /**
      * Effective URL of the MessageList webview.
      *
+     * It points to `index.html` in the webview-assets folder.
+     *
      * We provide all HTML at creation (or refresh) time; the document named
      * here is not loaded, and doesn't even need to exist. It serves only as a
      * placeholder, so that relative URLs and cross-domain security restrictions
      * have somewhere to believe that this document originates from.
      */
-    const baseUrl = `${assetsPath}/index.html`;
+    const baseUrl = new WhatwgURL('index.html', webviewAssetsUrl);
 
     // Paranoia^WSecurity: only load `baseUrl`, and only load it once. Any other
     // requests should be handed off to the OS, not loaded inside the WebView.
@@ -275,23 +267,11 @@ class MessageList extends Component<Props> {
         // Otherwise (for iOS), return a closure that evaluates to `true` _exactly
         // once_, and even then only if the URL looks like what we're expecting.
         let loaded_once = false;
-        // The baseUrl, with its relative portion (if any) stripped.
-        const baseUrlTail = baseUrl.replace(/^\.\//, '');
-        // Disallow such monstrosities as `evilsite.com/?./webview/index.html`.
-        const unsafeUrlRegex = /[&?]/;
         return (url: string) => {
-          if (!loaded_once) {
-            // The exact URL that will be loaded could be determined statically on
-            // Android. On iOS, though, it involves some unpredictable UUIDs which
-            // RN provides no good interface to. (`react-native-fs` is awful.)
-            if (
-              url.startsWith('file://')
-              && url.endsWith(baseUrlTail)
-              && !unsafeUrlRegex.test(url)
-            ) {
-              loaded_once = true;
-              return true;
-            }
+          const parsedUrl = tryParseUrl(url);
+          if (!loaded_once && parsedUrl && parsedUrl.toString() === baseUrl.toString()) {
+            loaded_once = true;
+            return true;
           }
           return false;
         };
@@ -303,7 +283,7 @@ class MessageList extends Component<Props> {
         if (!ok) {
           logging.warn('webview: rejected navigation event', {
             navigation_event: { ...event },
-            expected_url: baseUrl,
+            expected_url: baseUrl.toString(),
           });
         }
         return ok;
@@ -323,7 +303,7 @@ class MessageList extends Component<Props> {
     // https://github.com/react-native-community/react-native-webview/pull/697
     return (
       <WebView
-        source={{ baseUrl, html }}
+        source={{ baseUrl: (baseUrl.toString(): string), html }}
         originWhitelist={['file://']}
         onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
         style={{ backgroundColor: 'transparent' }}
