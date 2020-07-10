03637c10b2a4b9b259bf02a35b97bce9f40755e5
Chris Bobbe <cbobbe@zulip.com>
2020-07-10 10:39:57 -0700

    messageActions tests: Start type-checking, use example data.

--- src/message/__tests__/messageActions-test.js
+++ src/message/__tests__/messageActions-test.js
@@ -1,9 +1,13 @@
+/* @flow strict-local */
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
 import { doNarrow } from '../messagesActions';
 import { streamNarrow, HOME_NARROW, privateNarrow } from '../../utils/narrow';
 import { navStateWithNarrow } from '../../utils/testHelpers';
+import * as eg from '../../__tests__/lib/exampleData';
+import type { GlobalState } from '../../reduxTypes';
+import type { Action } from '../../actionTypes';
 
 const mockStore = configureStore([thunk]);
 
@@ -13,54 +17,51 @@ const streamNarrowStr = JSON.stringify(streamNarrowObj);
 describe('messageActions', () => {
   describe('doNarrow', () => {
     test('when no messages in new narrow and caughtUp is false, actions to fetch messages and switch narrow are dispatched', () => {
-      const store = mockStore({
-        realm: {},
-        session: { isHydrated: true },
-        caughtUp: {
-          [streamNarrowStr]: {
-            newer: false,
-            older: true,
+      const store = mockStore<GlobalState, Action>(
+        eg.reduxState({
+          session: { ...eg.baseReduxState.session, isHydrated: true },
+          caughtUp: {
+            [streamNarrowStr]: {
+              newer: false,
+              older: true,
+            },
           },
-        },
-        ...navStateWithNarrow(HOME_NARROW),
-        narrows: {},
-        streams: [
-          {
-            name: 'some stream',
-          },
-        ],
-      });
+          ...navStateWithNarrow(HOME_NARROW),
+          narrows: {},
+          streams: [eg.makeStream({ name: 'some stream' })],
+        }),
+      );
 
       store.dispatch(doNarrow(streamNarrowObj));
       const actions = store.getActions();
 
       expect(actions).toHaveLength(3);
-      expect(actions[0].type).toBe('DO_NARROW');
-      expect(actions[1].type).toBe('MESSAGE_FETCH_START');
-      expect(actions[1].narrow).toEqual(streamNarrowObj);
-      expect(actions[2].type).toBe('Navigation/PUSH');
+      const [action0, action1, action2] = actions;
+      expect(action0.type).toBe('DO_NARROW');
+      expect(action1.type).toBe('MESSAGE_FETCH_START');
+      if (action1.type === 'MESSAGE_FETCH_START') {
+        expect(action1.narrow).toEqual(streamNarrowObj);
+      }
+      expect(action2.type).toBe('Navigation/PUSH');
     });
 
     test('when no messages in new narrow but caught up, only switch to narrow, do not fetch', () => {
-      const store = mockStore({
-        realm: {},
-        session: { isHydrated: true },
-        caughtUp: {
-          [streamNarrowStr]: {
-            newer: true,
-            older: true,
+      const store = mockStore<GlobalState, Action>(
+        eg.reduxState({
+          session: { ...eg.baseReduxState.session, isHydrated: true },
+          caughtUp: {
+            [streamNarrowStr]: {
+              newer: true,
+              older: true,
+            },
           },
-        },
-        ...navStateWithNarrow(HOME_NARROW),
-        narrows: {
-          [streamNarrowStr]: [],
-        },
-        streams: [
-          {
-            name: 'some stream',
+          ...navStateWithNarrow(HOME_NARROW),
+          narrows: {
+            [streamNarrowStr]: [],
           },
-        ],
-      });
+          streams: [eg.makeStream({ name: 'some stream' })],
+        }),
+      );
 
       store.dispatch(doNarrow(streamNarrowObj));
       const actions = store.getActions();
@@ -76,52 +77,49 @@ describe('messageActions', () => {
     });
 
     test('when messages in new narrow are too few and not caught up, fetch more messages', () => {
-      const store = mockStore({
-        realm: {},
-        session: { isHydrated: true },
-        caughtUp: {},
-        ...navStateWithNarrow(HOME_NARROW),
-        narrows: {
-          [streamNarrowStr]: [1],
-        },
-        messages: {
-          1: {},
-        },
-        streams: [
-          {
-            name: 'some stream',
+      const store = mockStore<GlobalState, Action>(
+        eg.reduxState({
+          session: { ...eg.baseReduxState.session, isHydrated: true },
+          caughtUp: {},
+          ...navStateWithNarrow(HOME_NARROW),
+          narrows: {
+            [streamNarrowStr]: [1],
           },
-        ],
-      });
+          messages: {
+            [1]: eg.streamMessage() /* eslint-disable-line no-useless-computed-key */,
+          },
+          streams: [eg.makeStream({ name: 'some stream' })],
+        }),
+      );
 
       store.dispatch(doNarrow(streamNarrowObj));
       const actions = store.getActions();
 
       expect(actions).toHaveLength(3);
-      expect(actions[0].type).toBe('DO_NARROW');
-      expect(actions[1].type).toBe('MESSAGE_FETCH_START');
-      expect(actions[1].narrow).toEqual(streamNarrowObj);
-      expect(actions[2].type).toBe('Navigation/PUSH');
+      const [action0, action1, action2] = actions;
+      expect(action0.type).toBe('DO_NARROW');
+      expect(action1.type).toBe('MESSAGE_FETCH_START');
+      if (action1.type === 'MESSAGE_FETCH_START') {
+        expect(action1.narrow).toEqual(streamNarrowObj);
+      }
+      expect(action2.type).toBe('Navigation/PUSH');
     });
 
     test('if new narrow stream is not valid, do nothing', () => {
-      const store = mockStore({
-        realm: {},
-        session: { isHydrated: true },
-        caughtUp: {},
-        ...navStateWithNarrow(HOME_NARROW),
-        narrows: {
-          [streamNarrowStr]: [1],
-        },
-        messages: {
-          1: {},
-        },
-        streams: [
-          {
-            name: 'some updated stream',
+      const store = mockStore<GlobalState, Action>(
+        eg.reduxState({
+          session: { ...eg.baseReduxState.session, isHydrated: true },
+          caughtUp: {},
+          ...navStateWithNarrow(HOME_NARROW),
+          narrows: {
+            [streamNarrowStr]: [1],
           },
-        ],
-      });
+          messages: {
+            [1]: eg.streamMessage({ id: 1 }) /* eslint-disable-line no-useless-computed-key */,
+          },
+          streams: [eg.makeStream({ name: 'some updated stream' })],
+        }),
+      );
 
       store.dispatch(doNarrow(streamNarrowObj));
       const actions = store.getActions();
@@ -130,28 +128,28 @@ describe('messageActions', () => {
     });
 
     test('if new narrow user is deactivated, do nothing', () => {
-      const store = mockStore({
-        realm: {
-          crossRealmBots: [],
-          nonActiveUsers: [],
-        },
-        session: { isHydrated: true },
-        caughtUp: {},
-        ...navStateWithNarrow(HOME_NARROW),
-        narrows: {
-          [streamNarrowStr]: [1],
-        },
-        messages: {
-          1: {},
-        },
-        users: [
-          {
-            email: 'ab@a.com',
-          },
-        ],
-      });
+      const inactiveUser = eg.makeUser({ name: 'inactive', is_active: false });
 
-      store.dispatch(doNarrow(privateNarrow('a@a.com')));
+      const store = mockStore(
+        eg.reduxState({
+          realm: eg.realmState({
+            crossRealmBots: [],
+            nonActiveUsers: [], // TODO: Shouldn't `inactiveUser` go here?
+          }),
+          session: { ...eg.baseReduxState.session, isHydrated: true },
+          caughtUp: {},
+          ...navStateWithNarrow(HOME_NARROW),
+          narrows: {
+            [streamNarrowStr]: [1],
+          },
+          messages: {
+            [1]: eg.streamMessage({ id: 1 }) /* eslint-disable-line no-useless-computed-key */,
+          },
+          users: [eg.makeUser({ name: 'bob', is_active: true })],
+        }),
+      );
+
+      store.dispatch(doNarrow(privateNarrow(inactiveUser.email)));
       const actions = store.getActions();
 
       expect(actions).toHaveLength(0);
