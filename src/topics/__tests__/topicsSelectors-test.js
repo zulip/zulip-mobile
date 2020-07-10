7718c951b1ec202f36090ca648a5188a1499bc9b
Chris Bobbe <cbobbe@zulip.com>
2020-07-10 12:07:40 -0700

    topicsSelectors tests: Start typechecking, use example data.

--- src/topics/__tests__/topicsSelectors-test.js
+++ src/topics/__tests__/topicsSelectors-test.js
@@ -1,11 +1,11 @@
-import deepFreeze from 'deep-freeze';
-
+/* @flow strict-local */
 import { getTopicsForNarrow, getLastMessageTopic, getTopicsForStream } from '../topicSelectors';
 import { HOME_NARROW, streamNarrow } from '../../utils/narrow';
+import * as eg from '../../__tests__/lib/exampleData';
 
 describe('getTopicsForNarrow', () => {
   test('when no topics return an empty list', () => {
-    const state = deepFreeze({});
+    const state = eg.reduxState();
 
     const topics = getTopicsForNarrow(state, HOME_NARROW);
 
@@ -13,14 +13,15 @@ describe('getTopicsForNarrow', () => {
   });
 
   test('when there are topics in the active narrow, return them as string array', () => {
-    const state = deepFreeze({
-      streams: [{ stream_id: 123, name: 'hello' }],
+    const stream = { ...eg.makeStream({ name: 'hello' }), stream_id: 123 };
+    const state = eg.reduxState({
+      streams: [stream],
       topics: {
-        123: [{ name: 'hi' }, { name: 'wow' }],
+        [stream.stream_id]: [{ name: 'hi', max_id: 123 }, { name: 'wow', max_id: 234 }],
       },
     });
 
-    const topics = getTopicsForNarrow(state, streamNarrow('hello'));
+    const topics = getTopicsForNarrow(state, streamNarrow(stream.name));
 
     expect(topics).toEqual(['hi', 'wow']);
   });
@@ -28,7 +29,7 @@ describe('getTopicsForNarrow', () => {
 
 describe('getLastMessageTopic', () => {
   test('when no messages in narrow return an empty string', () => {
-    const state = deepFreeze({
+    const state = eg.reduxState({
       narrows: {},
     });
 
@@ -39,32 +40,32 @@ describe('getLastMessageTopic', () => {
 
   test('when one or more messages return the topic of the last one', () => {
     const narrow = streamNarrow('hello');
-    const state = deepFreeze({
-      flags: {
-        mentioned: {},
-      },
+    const message1 = eg.streamMessage({ id: 1 });
+    const message2 = eg.streamMessage({ id: 2, subject: 'some topic' });
+    const state = eg.reduxState({
       narrows: {
         [JSON.stringify(narrow)]: [1, 2],
       },
       messages: {
-        1: { id: 1 },
-        2: { id: 2, subject: 'some topic' },
+        [message1.id]: message1,
+        [message2.id]: message2,
       },
     });
 
     const topic = getLastMessageTopic(state, narrow);
 
-    expect(topic).toEqual('some topic');
+    expect(topic).toEqual(message2.subject);
   });
 });
 
 describe('getTopicsForStream', () => {
   test('when no topics loaded for given stream return undefined', () => {
-    const state = deepFreeze({
+    const state = eg.reduxState({
       streams: [],
       topics: {},
       mute: [],
       unread: {
+        ...eg.baseReduxState.unread,
         streams: [],
       },
     });
@@ -75,27 +76,31 @@ describe('getTopicsForStream', () => {
   });
 
   test('when topics loaded for given stream return them', () => {
-    const state = deepFreeze({
-      streams: [{ stream_id: 123, name: 'stream 123' }],
+    const stream = { ...eg.makeStream({ name: 'stream 123' }), stream_id: 123 };
+    const state = eg.reduxState({
+      streams: [stream],
       topics: {
-        123: [{ name: 'topic', max_id: 456 }],
+        [stream.stream_id]: [{ name: 'topic', max_id: 456 }],
       },
       mute: [],
       unread: {
+        ...eg.baseReduxState.unread,
         streams: [],
       },
     });
 
-    const topics = getTopicsForStream(state, 123);
+    const topics = getTopicsForStream(state, stream.stream_id);
 
     expect(topics).toEqual([{ name: 'topic', max_id: 456, isMuted: false, unreadCount: 0 }]);
   });
 
   test('Return list of topic object with isMuted, unreadCount, topic name and max id in it.', () => {
-    const state = deepFreeze({
-      streams: [{ stream_id: 1, name: 'stream 1' }],
+    const stream = { ...eg.makeStream({ name: 'stream 1' }), stream_id: 1 };
+
+    const state = eg.reduxState({
+      streams: [stream],
       topics: {
-        1: [
+        [stream.stream_id]: [
           { name: 'topic 1', max_id: 5 },
           { name: 'topic 2', max_id: 6 },
           { name: 'topic 3', max_id: 7 },
@@ -105,6 +110,7 @@ describe('getTopicsForStream', () => {
       },
       mute: [['stream 1', 'topic 1'], ['stream 1', 'topic 3'], ['stream 2', 'topic 2']],
       unread: {
+        ...eg.baseReduxState.unread,
         streams: [
           {
             stream_id: 1,
