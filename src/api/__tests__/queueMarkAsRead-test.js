88cb8d0f6f064962fabc9124558e5440c560b944
Chris Bobbe <cbobbe@zulip.com>
2020-07-14 10:44:42 -0700

    queueMarkAsRead tests: Remove Lolex.
    Use Jest's "modern" fake timers instead of our Lolex wrapper.

--- src/api/__tests__/queueMarkAsRead-test.js
+++ src/api/__tests__/queueMarkAsRead-test.js
@@ -2,7 +2,6 @@
 import queueMarkAsRead, { resetAll } from '../queueMarkAsRead';
 import * as messagesFlags from '../messages/messagesFlags';
 import * as eg from '../../__tests__/lib/exampleData';
-import { Lolex } from '../../__tests__/lib/lolex';
 
 // $FlowFixMe[cannot-write] Make flow understand about mocking
 messagesFlags.default = jest.fn(
@@ -13,19 +12,13 @@ messagesFlags.default = jest.fn(
 );
 
 describe('queueMarkAsRead', () => {
-  let lolex: Lolex;
-
-  beforeAll(() => {
-    lolex = new Lolex();
-  });
-
-  afterAll(() => {
-    lolex.dispose();
-  });
+  jest.useFakeTimers('modern');
+  // Will throw if not actually using the "modern" implementation
+  jest.getRealSystemTime();
 
   afterEach(() => {
     jest.clearAllMocks();
-    lolex.clearAllTimers();
+    jest.clearAllTimers();
     resetAll();
   });
 
@@ -35,13 +28,13 @@ describe('queueMarkAsRead', () => {
     queueMarkAsRead(eg.selfAuth, [7, 8, 9]);
     queueMarkAsRead(eg.selfAuth, [10, 11, 12]);
 
-    expect(lolex.getTimerCount()).toBe(1);
+    expect(jest.getTimerCount()).toBe(1);
     expect(messagesFlags.default).toHaveBeenCalledTimes(1);
   });
 
   test('should call messagesFlags, if calls to queueMarkAsRead are 2s apart', () => {
     queueMarkAsRead(eg.selfAuth, [13, 14, 15]);
-    lolex.advanceTimersByTime(2100);
+    jest.advanceTimersByTime(2100);
     queueMarkAsRead(eg.selfAuth, [16, 17, 18]);
 
     expect(messagesFlags.default).toHaveBeenCalledTimes(2);
@@ -50,19 +43,19 @@ describe('queueMarkAsRead', () => {
   test('should set timeout for time remaining for next API call to clear queue', () => {
     queueMarkAsRead(eg.selfAuth, [1, 2, 3]);
 
-    lolex.advanceTimersByTime(1900);
+    jest.advanceTimersByTime(1900);
     queueMarkAsRead(eg.selfAuth, [4, 5, 6]);
 
-    lolex.runOnlyPendingTimers();
+    jest.runOnlyPendingTimers();
     expect(messagesFlags.default).toHaveBeenCalledTimes(2);
   });
 
   test('empty array should not cause anything to happen', () => {
     queueMarkAsRead(eg.selfAuth, []);
 
-    lolex.advanceTimersByTime(2500);
+    jest.advanceTimersByTime(2500);
 
-    lolex.runOnlyPendingTimers();
+    jest.runOnlyPendingTimers();
     expect(messagesFlags.default).toHaveBeenCalledTimes(0);
   });
 });
