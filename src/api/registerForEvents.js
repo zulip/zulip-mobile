f177462cbf95c8e862ceace1f89f638ee62daefc
Chris Bobbe <cbobbe@zulip.com>
2020-07-29 17:46:24 -0700

    register response [nfc]: Prepare to transform users and bots.
    This is setup for when we'll soon be converting `avatar_url` on
    objects in `realm_users` and `cross_realm_bots` on the register
    response. For motivation, see a recent commit where we added the
    `AvatarURL` abstract class.

--- src/api/registerForEvents.js
+++ src/api/registerForEvents.js
@@ -1,7 +1,8 @@
 /* @flow strict-local */
-import type { InitialData } from './initialDataTypes';
+import type { RawInitialData, InitialData } from './initialDataTypes';
 import type { Auth } from './transportTypes';
 import type { Narrow } from './apiTypes';
+import type { CrossRealmBot, User } from './modelTypes';
 import { apiPost } from './apiFetch';
 
 type RegisterForEventsParams = {|
@@ -19,14 +20,39 @@ type RegisterForEventsParams = {|
   |},
 |};
 
+const transformUser = (rawUser: {| ...User, avatar_url: string | null |}, realm: URL): User =>
+  // In an upcoming commit, we'll convert the raw-data `avatar_url`
+  // field to an AvatarURL instance.
+  rawUser;
+
+const transformCrossRealmBot = (
+  rawCrossRealmBot: {| ...CrossRealmBot, avatar_url: string | void | null |},
+  realm: URL,
+): CrossRealmBot =>
+  // In an upcoming commit, we'll convert the raw-data `avatar_url`
+  // field to an AvatarURL instance.
+  rawCrossRealmBot;
+
+const transform = (rawInitialData: RawInitialData, auth: Auth): InitialData => ({
+  ...rawInitialData,
+  realm_users: rawInitialData.realm_users.map(rawUser => transformUser(rawUser, auth.realm)),
+  realm_non_active_users: rawInitialData.realm_non_active_users.map(rawNonActiveUser =>
+    transformUser(rawNonActiveUser, auth.realm),
+  ),
+  cross_realm_bots: rawInitialData.cross_realm_bots.map(rawCrossRealmBot =>
+    transformCrossRealmBot(rawCrossRealmBot, auth.realm),
+  ),
+});
+
 /** See https://zulip.com/api/register-queue */
 export default async (auth: Auth, params: RegisterForEventsParams): Promise<InitialData> => {
   const { narrow, event_types, fetch_event_types, client_capabilities } = params;
-  return apiPost(auth, 'register', {
+  const rawInitialData = await apiPost(auth, 'register', {
     ...params,
     narrow: JSON.stringify(narrow),
     event_types: JSON.stringify(event_types),
     fetch_event_types: JSON.stringify(fetch_event_types),
     client_capabilities: JSON.stringify(client_capabilities),
   });
+  return transform(rawInitialData, auth);
 };
