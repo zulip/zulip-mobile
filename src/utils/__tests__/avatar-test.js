c0d8a0496ddaeea48938ccfe3e39b08994608ca5
Chris Bobbe <cbobbe@zulip.com>
2020-08-14 13:34:12 -0700

    avatar: Add `AvatarURL` abstract class, and two subclasses.
    We'll soon start instantiating these from raw data at the edge, in
    the "crunchy shell" pattern [1]. This will help us clean up lots of
    confusing UI logic in the "soft center" of the app that's in charge
    of displaying avatars, and we'll be able to uproot several helper
    functions and simplify some props (often ambiguously `string`-typed)
    of several React components.
    
    This doesn't yet add support for the
    `user_avatar_url_field_optional` client capability, but it will make
    it much more straightforward to do so in an upcoming commit.
    
    Some of the boilerplate in the subclasses is due to a performance
    optimization [2] based on an observed ~1s added to the rehydrate
    time on CZO when URL objects are constructed, vs. about 200ms (and
    maybe a bit more in the upper tail) when they aren't. During
    rehydration, we don't need to expensively construct URL objects to
    validate raw URL strings, as long as we do so at the edge when we
    receive them from the network.
    
    See discussion [3].
    
    [1] https://github.com/zulip/zulip-mobile/blob/master/docs/architecture/crunchy-shell.md
    [2] https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/user_avatar_url_field_optional/near/993660
    [3] https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/user_avatar_url_field_optional/near/908946

--- src/utils/__tests__/avatar-test.js
+++ src/utils/__tests__/avatar-test.js
@@ -1,5 +1,160 @@
 /* @flow strict-local */
-import { getMediumAvatar, getGravatarFromEmail } from '../avatar';
+import md5 from 'blueimp-md5';
+
+import {
+  AvatarURL,
+  GravatarURL,
+  UploadedAvatarURL,
+  getMediumAvatar,
+  getGravatarFromEmail,
+} from '../avatar';
+import * as eg from '../../__tests__/lib/exampleData';
+
+describe('AvatarURL', () => {
+  describe('fromUserOrBotData', () => {
+    const user = eg.makeUser();
+    const { email } = user;
+    const realm = eg.realm;
+
+    test('gives a `GravatarURL` if `rawAvatarURL` is null', () => {
+      const rawAvatarUrl = null;
+      expect(AvatarURL.fromUserOrBotData({ rawAvatarUrl, email, realm })).toBeInstanceOf(
+        GravatarURL,
+      );
+    });
+
+    test('gives a `GravatarURL` if `rawAvatarURL` is a URL string on Gravatar origin', () => {
+      const rawAvatarUrl =
+        'https://secure.gravatar.com/avatar/2efaec12efd9bea8a089299208117786?d=identicon&version=3';
+      expect(AvatarURL.fromUserOrBotData({ rawAvatarUrl, email, realm })).toBeInstanceOf(
+        GravatarURL,
+      );
+    });
+
+    test('gives an `UploadedAvatarURL` if `rawAvatarURL` is a non-Gravatar absolute URL string', () => {
+      const rawAvatarUrl =
+        'https://zulip-avatars.s3.amazonaws.com/13/430713047f2cffed661f84e139a64f864f17f286?x=x&version=5';
+      expect(AvatarURL.fromUserOrBotData({ rawAvatarUrl, email, realm })).toBeInstanceOf(
+        UploadedAvatarURL,
+      );
+    });
+
+    test('gives an `UploadedAvatarURL` if `rawAvatarURL` is a relative URL string', () => {
+      const rawAvatarUrl =
+        '/user_avatars/2/08fb6d007eb10a56efee1d64760fbeb6111c4352.png?x=x&version=2';
+      expect(AvatarURL.fromUserOrBotData({ rawAvatarUrl, email, realm })).toBeInstanceOf(
+        UploadedAvatarURL,
+      );
+    });
+  });
+});
+
+const SIZES_TO_TEST = [24, 32, 48, 80, 200];
+
+describe('GravatarURL', () => {
+  test('serializes/deserializes correctly', () => {
+    const instance = GravatarURL.validateAndConstructInstance({ email: eg.selfUser.email });
+
+    const roundTripped = GravatarURL.deserialize(GravatarURL.serialize(instance));
+
+    SIZES_TO_TEST.forEach(size => {
+      expect(instance.get(size).toString()).toEqual(roundTripped.get(size).toString());
+    });
+  });
+
+  test('lowercases email address before hashing', () => {
+    const email = 'uNuSuAlCaPs@example.com';
+    const instance = GravatarURL.validateAndConstructInstance({ email });
+    SIZES_TO_TEST.forEach(size => {
+      expect(instance.get(size).toString()).toContain(md5('unusualcaps@example.com'));
+    });
+  });
+
+  test('uses hash from server, if provided', () => {
+    const email = 'user13313@chat.zulip.org';
+    const hash = md5('cbobbe@zulip.com');
+    const instance = GravatarURL.validateAndConstructInstance({ email, hash });
+    SIZES_TO_TEST.forEach(size => {
+      expect(instance.get(size).toString()).toContain(hash);
+    });
+  });
+
+  test('produces corresponding URLs for all sizes', () => {
+    const instance = GravatarURL.validateAndConstructInstance({ email: eg.selfUser.email });
+
+    SIZES_TO_TEST.forEach(size => {
+      expect(instance.get(size).toString()).toContain(`s=${size.toString()}`);
+    });
+  });
+});
+
+describe('UploadedAvatarURL', () => {
+  test('serializes/deserializes correctly', () => {
+    const instance = UploadedAvatarURL.validateAndConstructInstance({
+      realm: eg.realm,
+      absoluteOrRelativeUrl:
+        'https://zulip-avatars.s3.amazonaws.com/13/430713047f2cffed661f84e139a64f864f17f286?x=x&version=5',
+    });
+
+    const roundTripped = UploadedAvatarURL.deserialize(UploadedAvatarURL.serialize(instance));
+
+    SIZES_TO_TEST.forEach(size => {
+      expect(instance.get(size).toString()).toEqual(roundTripped.get(size).toString());
+    });
+  });
+
+  test('if a relative URL, gives a URL on the given realm', () => {
+    const instance = UploadedAvatarURL.validateAndConstructInstance({
+      realm: new URL('https://chat.zulip.org'),
+      absoluteOrRelativeUrl:
+        '/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae.png?x=x&version=2',
+    });
+
+    SIZES_TO_TEST.forEach(size => {
+      const result = instance.get(size).toString();
+      expect(
+        result
+          === 'https://chat.zulip.org/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae.png?x=x&version=2'
+          || result
+            === 'https://chat.zulip.org/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae-medium.png?x=x&version=2',
+      ).toBeTrue();
+    });
+  });
+
+  test('if an absolute URL, just use it', () => {
+    const instance = UploadedAvatarURL.validateAndConstructInstance({
+      realm: new URL('https://chat.zulip.org'),
+      absoluteOrRelativeUrl:
+        'https://zulip-avatars.s3.amazonaws.com/13/430713047f2cffed661f84e139a64f864f17f286?x=x&version=5',
+    });
+    SIZES_TO_TEST.forEach(size => {
+      expect(instance.get(size).toString()).toMatch(
+        'https://zulip-avatars.s3.amazonaws.com/13/430713047f2cffed661f84e139a64f864f17f286?x=x&version=5',
+      );
+    });
+  });
+
+  test('converts *.png to *-medium.png for sizes over 100', () => {
+    const realm = new URL('https://chat.zulip.org');
+    const instance = UploadedAvatarURL.validateAndConstructInstance({
+      realm,
+      absoluteOrRelativeUrl:
+        '/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae.png?x=x&version=2',
+    });
+    const sizesOver100 = SIZES_TO_TEST.filter(s => s > 100);
+    const sizesAtMost100 = SIZES_TO_TEST.filter(s => s <= 100);
+    sizesOver100.forEach(size => {
+      expect(instance.get(size).toString()).toEqual(
+        'https://chat.zulip.org/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae-medium.png?x=x&version=2',
+      );
+    });
+    sizesAtMost100.forEach(size => {
+      expect(instance.get(size).toString()).toEqual(
+        'https://chat.zulip.org/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae.png?x=x&version=2',
+      );
+    });
+  });
+});
 
 // avatarUrl can be converted to retrieve medium sized avatars(mediumAvatarUrl) if and only if
 // avatarUrl contains avatar image name with a .png extension (e.g. AVATAR_IMAGE_NAME.png).
--- src/utils/avatar.js
+++ src/utils/avatar.js
@@ -1,9 +1,257 @@
 /* @flow strict-local */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-use-before-define */
 import md5 from 'blueimp-md5';
 
 import type { Message, Outbox, UserOrBot } from '../types';
 import { tryParseUrl } from './url';
 import * as logging from './logging';
+import { ensureUnreachable } from '../types';
+
+/**
+ * A way to get a standard avatar URL, or a sized one if available
+ *
+ * This class is abstract. Only instantiate its subclasses.
+ */
+export class AvatarURL {
+  /**
+   * From info on a user or bot, make the right subclass instance.
+   */
+  static fromUserOrBotData(args: {|
+    rawAvatarUrl: string | void | null,
+    email: string,
+    realm: URL,
+  |}): AvatarURL {
+    const { rawAvatarUrl, email, realm } = args;
+    if (rawAvatarUrl === undefined) {
+      // `rawAvatarUrl` will be undefined for cross-realm bots from
+      // servers prior to 58ee3fa8c (1.9.0). Fall back to Gravatar for
+      // this case; it should be pretty rare.
+      return GravatarURL.validateAndConstructInstance({ email });
+    } else if (rawAvatarUrl === null) {
+      // If we announce `client_gravatar`, which we sometimes do,
+      // `rawAvatarUrl` might be null. In that case, we take
+      // responsibility for computing a hash for the user's email and
+      // using it to form a URL for an avatar served by Gravatar.
+      return GravatarURL.validateAndConstructInstance({ email });
+    } else if (typeof rawAvatarUrl === 'string') {
+      // If we don't announce `client_gravatar` (which we sometimes
+      // do), or if the server doesn't have
+      // EMAIL_ADDRESS_VISIBILITY_EVERYONE set, then `rawAvatarUrl`
+      // will be the absolute Gravatar URL string.
+      //
+      // (In the latter case, we won't have real email addresses with
+      // which to generate the correct hash; see
+      // https://github.com/zulip/zulip/issues/15287. Implemented at
+      // `do_events_register` in zerver/lib/events.py on the server.)
+      if (tryParseUrl(rawAvatarUrl)?.origin === GravatarURL.ORIGIN) {
+        const hashMatch = /[0-9a-fA-F]{32}$/.exec(new URL(rawAvatarUrl).pathname);
+        if (hashMatch === null) {
+          const msg = 'Unexpected Gravatar URL shape from server.';
+          logging.error(msg, { value: rawAvatarUrl });
+          throw new Error(msg);
+        }
+        return GravatarURL.validateAndConstructInstance({ email, hash: hashMatch[0] });
+      }
+
+      // Otherwise, it's a realm-uploaded avatar, either absolute or
+      // relative, depending on how uploads are stored.
+      return UploadedAvatarURL.validateAndConstructInstance({
+        realm,
+        absoluteOrRelativeUrl: rawAvatarUrl,
+      });
+    } else {
+      ensureUnreachable(rawAvatarUrl);
+      const msg = 'Unexpected value for `rawAvatarUrl` in `AvatarURL.fromUserOrBotData`';
+      logging.error(msg, { value: rawAvatarUrl });
+      throw new Error(msg);
+    }
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  get(sizePhysicalPx: number): URL {
+    throw new Error('unimplemented');
+  }
+}
+
+/**
+ * A Gravatar URL with a hash we compute from an email address.
+ *
+ * See http://secure.gravatar.com/site/implement/images/, which covers
+ * the size options.
+ */
+export class GravatarURL extends AvatarURL {
+  /**
+   * Serialize to a special string; reversible with `deserialize`.
+   */
+  static serialize(instance: GravatarURL): string {
+    return instance._standardUrl instanceof URL
+      ? instance._standardUrl.toString()
+      : instance._standardUrl;
+  }
+
+  /**
+   * Use a special string from `serialize` to make a new instance.
+   */
+  static deserialize(serialized: string): GravatarURL {
+    return new GravatarURL(serialized);
+  }
+
+  /**
+   * Construct from raw server data, or throw an error.
+   *
+   * Pass the hash if the server provides it, to avoid computing it
+   * unnecessarily.
+   */
+  static validateAndConstructInstance(args: {| email: string, hash?: string |}): GravatarURL {
+    const { email, hash = md5(email.toLowerCase()) } = args;
+
+    const standardSizeUrl = new URL(`/avatar/${hash}`, GravatarURL.ORIGIN);
+    standardSizeUrl.searchParams.set('d', 'identicon');
+
+    return new GravatarURL(standardSizeUrl);
+  }
+
+  static ORIGIN = 'https://secure.gravatar.com';
+
+  /**
+   * Standard URL from which to generate others. PRIVATE.
+   *
+   * May be a string if the instance was constructed at rehydrate
+   * time, when URL validation is unnecessary.
+   */
+  _standardUrl: string | URL;
+
+  /**
+   * PRIVATE: Make an instance from already-validated data.
+   *
+   * Not part of the public interface; use the static methods instead.
+   *
+   * It's private because we need a path to constructing an instance
+   * without constructing URL objects, which takes more time than is
+   * acceptable when we can avoid it, e.g., during rehydration.
+   * Constructing URL objects is a necessary part of validating data
+   * from the server, but we only need to validate the data once, when
+   * it's first received.
+   */
+  constructor(standardUrl: string | URL) {
+    super();
+    this._standardUrl = standardUrl;
+  }
+
+  /**
+   * Get a URL object for the given size.
+   *
+   * `sizePhysicalPx` must be an integer. (Gravatar doesn't advertise
+   * the ability to specify noninteger values for the size.)
+   */
+  get(sizePhysicalPx: number): URL {
+    // `this._standardUrl` may have begun its life as a string, to
+    // avoid computing a URL object during rehydration
+    if (typeof this._standardUrl === 'string') {
+      this._standardUrl = new URL(this._standardUrl);
+    }
+
+    // Make a new URL to mutate
+    // $FlowFixMe - https://github.com/zulip/zulip-mobile/pull/4230#discussion_r512351202
+    const result: URL = new URL(this._standardUrl);
+    result.searchParams.set('s', sizePhysicalPx.toString());
+    return result;
+  }
+}
+
+/**
+ * An avatar that was uploaded to the Zulip server.
+ *
+ * There are two size options; if `sizePhysicalPx` is greater than
+ * 100, medium is chosen:
+ *  * default: 100x100
+ *  * medium: 500x500
+ */
+export class UploadedAvatarURL extends AvatarURL {
+  /**
+   * Serialize to a special string; reversible with `deserialize`.
+   */
+  static serialize(instance: UploadedAvatarURL): string {
+    return instance._standardUrl instanceof URL
+      ? instance._standardUrl.toString()
+      : instance._standardUrl;
+  }
+
+  /**
+   * Use a special string from `serialize` to make a new instance.
+   */
+  static deserialize(serialized: string): UploadedAvatarURL {
+    return new UploadedAvatarURL(serialized);
+  }
+
+  /**
+   * Construct from raw server data, or throw an error.
+   *
+   * Expects a relative URL plus the realm for a local upload;
+   * otherwise, an absolute URL of the avatar on the S3 backend.
+   */
+  static validateAndConstructInstance(args: {|
+    realm: URL,
+    absoluteOrRelativeUrl: string,
+  |}): UploadedAvatarURL {
+    const { realm, absoluteOrRelativeUrl } = args;
+    // If `absoluteOrRelativeUrl` is absolute, the second argument
+    // is ignored.
+    return new UploadedAvatarURL(new URL(absoluteOrRelativeUrl, realm));
+  }
+
+  /**
+   * Standard URL from which to generate others. PRIVATE.
+   *
+   * May be a string if the instance was constructed at rehydrate
+   * time, when URL validation is unnecessary.
+   */
+  _standardUrl: string | URL;
+
+  /**
+   * PRIVATE: Make an instance from already-validated data.
+   *
+   * Not part of the public interface; use the static methods instead.
+   *
+   * It's private because we need a path to constructing an instance
+   * without constructing URL objects, which takes more time than is
+   * acceptable when we can avoid it, e.g., during rehydration.
+   * Constructing URL objects is a necessary part of validating data
+   * from the server, but we only need to validate the data once, when
+   * it's first received.
+   */
+  constructor(standardUrl: string | URL) {
+    super();
+    this._standardUrl = standardUrl;
+  }
+
+  /**
+   * Get a URL object for the given size.
+   *
+   * `sizePhysicalPx` should be an integer.
+   */
+  get(sizePhysicalPx: number): URL {
+    // `this._standardUrl` may have begun its life as a string, to
+    // avoid computing a URL object during rehydration
+    if (typeof this._standardUrl === 'string') {
+      this._standardUrl = new URL(this._standardUrl);
+    }
+
+    let result: URL = this._standardUrl;
+    if (sizePhysicalPx > 100) {
+      // Make a new URL to mutate, instead of mutating this._standardUrl
+      // $FlowFixMe - https://github.com/zulip/zulip-mobile/pull/4230#discussion_r512351202
+      result = new URL(this._standardUrl);
+
+      const match = new RegExp(/(\w+)\.png/g).exec(result.pathname);
+      if (match !== null) {
+        result.pathname = result.pathname.replace(match[0], `${match[1]}-medium.png`);
+      }
+    }
+    return result;
+  }
+}
 
 /**
  * When selecting the size of a gravatar we can pick any arbitrary
