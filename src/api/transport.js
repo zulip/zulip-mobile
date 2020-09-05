535b2d7d964d9d2bc5b9653147236dc2ee9d82ba
Chris Bobbe <cbobbe@zulip.com>
2020-09-04 17:00:11 -0700

    transport types: Make `getAuthHeaders` return type more precise.
    It's not a mystery which auth headers we might want to get from this
    function; there's no reason we have to use `{ [string]: string }`
    here.
    
    We just have to do something a bit silly in the fix, like we did in
    d141be763, and make a note of the corresponding Flow issue in a
    comment.

--- src/api/transport.js
+++ src/api/transport.js
@@ -3,5 +3,9 @@ import base64 from 'base-64';
 
 import type { Auth } from './transportTypes';
 
-export const getAuthHeaders = (auth: Auth): { [string]: string } =>
-  auth.apiKey ? { Authorization: `Basic ${base64.encode(`${auth.email}:${auth.apiKey}`)}` } : {};
+export const getAuthHeaders = (auth: Auth): {| Authorization?: string |} =>
+  // The `Object.freeze`` in the `:` case avoids a Flow issue:
+  // https://github.com/facebook/flow/issues/2386#issuecomment-695064325
+  auth.apiKey
+    ? { Authorization: `Basic ${base64.encode(`${auth.email}:${auth.apiKey}`)}` }
+    : Object.freeze({});
