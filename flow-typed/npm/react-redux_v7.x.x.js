fe88d872d31f81a22f0d86dd51065de16eaf22c4
Chris Bobbe <cbobbe@zulip.com>
2020-09-17 16:52:39 -0700

    deps: Upgrade react-redux to v7.
    From the release notes [1]:
    
    """
    This release has undergone extensive performance benchmarking, and
    we're confident that it's the fastest version of React-Redux yet!
    """
    
    Cool! It doesn't necessarily mean a dramatic improvement all across
    our app (or anywhere); we'd need to do our own testing to establish
    that. But this is encouraging.
    
    This version now uses React Hooks in the `connect` implementation,
    so they announce that we need to be using React 16.8.4 or later.
    That's no problem; we're well past that and have been for a while.
    
    In fact, the corresponding React peer-dep change is announced as the
    only breaking public API change.
    
    There's a FlowTyped libdef for this version, so, grab that; there
    are no new Flow errors.
    
    [1] https://github.com/reduxjs/react-redux/releases/tag/v7.0.1

--- flow-typed/npm/react-redux_v7.x.x.js
+++ flow-typed/npm/react-redux_v7.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: a09118d2093dc5b4eee045cd2a7dbf5a
-// flow-typed version: c6154227d1/react-redux_v6.x.x/flow_>=v0.104.x
+// flow-typed signature: 8da1e134b3de1d6f6bf9ba1cc7e2dc7e
+// flow-typed version: 387a235736/react-redux_v7.x.x/flow_>=v0.104.x
 
 /**
 The order of type arguments for connect() is as follows:
@@ -27,6 +27,7 @@ Decrypting the abbreviations:
   RMP = Returned merge props
   CP = Props for returned component
   Com = React Component
+  SS = Selected state
   ST = Static properties of Com
   EFO = Extra factory options (used only in connectAdvanced)
 */
@@ -199,6 +200,19 @@ declare module "react-redux" {
   ): Connector<P, OP, P>;
 
   // ------------------------------------------------------------
+  // Typings for Hooks
+  // ------------------------------------------------------------
+
+  declare export function useDispatch<D>(): D;
+
+  declare export function useSelector<S, SS>(
+    selector: (state: S) => SS,
+    equalityFn?: (a: SS, b: SS) => boolean,
+  ): SS;
+
+  declare export function useStore<Store>(): Store;
+
+  // ------------------------------------------------------------
   // Typings for Provider
   // ------------------------------------------------------------
 
@@ -270,11 +284,17 @@ declare module "react-redux" {
     connectAdvancedOptions: ?(ConnectAdvancedOptions & EFO),
   ): (component: Com) => React$ComponentType<OP> & $Shape<ST>;
 
+  declare export function batch(() => void): void
+
   declare export default {
     Provider: typeof Provider,
     createProvider: typeof createProvider,
     connect: typeof connect,
     connectAdvanced: typeof connectAdvanced,
+    useDispatch: typeof useDispatch,
+    useSelector: typeof useSelector,
+    useStore: typeof useStore,
+    batch: typeof batch,
     ...
   };
 }
