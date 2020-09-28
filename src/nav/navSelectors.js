dd03f3b94bcb1bf84b30ee9b993cbd86a5af71dd
Chris Bobbe <cbobbe@zulip.com>
2020-09-28 10:56:06 -0700

    navSelectors: Stop using `createSelector` that isn't caching any real work.
    `createSelector` from `reselect` can be useful when we're dealing
    with a lot more data than we are here, e.g., users, messages, etc.

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -1,7 +1,5 @@
 /* @flow strict-local */
-import { createSelector } from 'reselect';
-
-import type { GlobalState, NavigationRouteState, NavigationState, Selector } from '../types';
+import type { GlobalState, NavigationRouteState, NavigationState } from '../types';
 import type { Narrow } from '../api/apiTypes';
 
 export const getNav = (state: GlobalState): NavigationState => state.nav;
@@ -16,28 +14,22 @@ const getCurrentRoute = (state: GlobalState): void | NavigationRouteState =>
 export const getCurrentRouteName = (state: GlobalState): void | string =>
   getCurrentRoute(state)?.routeName;
 
-export const getCurrentRouteParams: Selector<void | { narrow?: Narrow }> = createSelector(
-  getCurrentRoute,
-  currentRoute => currentRoute?.params,
-);
+export const getCurrentRouteParams = (state: GlobalState): void | { narrow?: Narrow } =>
+  getCurrentRoute(state)?.params;
 
-export const getChatScreenParams: Selector<{ narrow?: Narrow }> = createSelector(
-  getCurrentRouteParams,
-  params => params || { narrow: undefined },
-);
+export const getChatScreenParams = (state: GlobalState): { narrow?: Narrow } =>
+  getCurrentRouteParams(state) || { narrow: undefined };
 
 export const getCanGoBack = (state: GlobalState) => state.nav.index > 0;
 
-export const getSameRoutesCount: Selector<number> = createSelector(
-  getNav,
-  nav => {
-    let i = nav.routes.length - 1;
-    while (i >= 0) {
-      if (nav.routes[i].routeName !== nav.routes[nav.routes.length - 1].routeName) {
-        break;
-      }
-      i--;
+export const getSameRoutesCount = (state: GlobalState): number => {
+  const nav = getNav(state);
+  let i = nav.routes.length - 1;
+  while (i >= 0) {
+    if (nav.routes[i].routeName !== nav.routes[nav.routes.length - 1].routeName) {
+      break;
     }
-    return nav.routes.length - i - 1;
-  },
-);
+    i--;
+  }
+  return nav.routes.length - i - 1;
+};
