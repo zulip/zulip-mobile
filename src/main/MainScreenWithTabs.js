570db381dd518c37dba61f715ee35b4e3a89c35b
Greg Price <greg@zulipchat.com>
2020-10-07 09:33:17 -0700

    main screen: Render nothing if no server data.
    Since the upgrade to react-navigation v4 (f3b6c1f9e), there's
    a crash which seems to be caused by the main screen getting
    re-`render`ed while we transition away on logout.  At this
    point we've dropped the server data, which much of our main UI
    relies on; and there also seems to be a react-navigation bug
    where it's already dropped the child navigator (our MainTabs)
    from its own data structures, and crashes at the re-render.
    
    Fix that by just rendering nothing in that case.
    
    Fixes: #4275

--- src/main/MainScreenWithTabs.js
+++ src/main/MainScreenWithTabs.js
@@ -3,6 +3,9 @@ import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 import type { NavigationStackProp, NavigationStateRoute } from 'react-navigation-stack';
 
+import type { Dispatch } from '../types';
+import { connect } from '../react-redux';
+import { getHaveServerData } from '../selectors';
 import type { ThemeData } from '../styles';
 import styles, { ThemeContext } from '../styles';
 import { OfflineNotice, ZulipStatusBar } from '../common';
@@ -14,15 +17,29 @@ type Props = $ReadOnly<{|
   // don't invoke it anywhere else at all), we know it gets the
   // `navigation` prop for free, with the stack-nav shape.
   navigation: NavigationStackProp<NavigationStateRoute>,
+
+  dispatch: Dispatch,
+  haveServerData: boolean,
 |}>;
 
-export default class MainScreenWithTabs extends PureComponent<Props> {
+class MainScreenWithTabs extends PureComponent<Props> {
   static router = MainTabs.router;
 
   static contextType = ThemeContext;
   context: ThemeData;
 
   render() {
+    if (!this.props.haveServerData) {
+      // This can happen if the user has just logged out; this screen
+      // is still visible for the duration of the nav transition, and
+      // it's legitimate for its `render` to get called again.
+      // See our #4275.
+      //
+      // Avoid rendering any of our main UI in this case, to maintain
+      // the guarantee that it can all rely on server data existing.
+      return null;
+    }
+
     return (
       <View style={[styles.flexed, { backgroundColor: this.context.backgroundColor }]}>
         <ZulipStatusBar />
@@ -32,3 +49,7 @@ export default class MainScreenWithTabs extends PureComponent<Props> {
     );
   }
 }
+
+export default connect(state => ({
+  haveServerData: getHaveServerData(state),
+}))(MainScreenWithTabs);
