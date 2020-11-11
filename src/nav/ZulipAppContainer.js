59ed9266275e307b91f0a7debc287cc88c082d86
Chris Bobbe <cbobbe@zulip.com>
2020-11-11 11:56:12 -0800

    ZulipAppContainer: Pass `theme` prop to `AppContainer`.
    Both React Navigation v4 and v5 have interfaces for setting the
    theme between light and dark, but they're quite different.
    
    One kind of unfortunate difference is that, in v5, using that
    interface is effectively required, even though we have a so-far
    quite adequate way of managing the theme. React Navigation v5 widely
    applies some styling overrides to its provided components (such as
    the background of a scene in a bottom-tab navigator) even when we
    don't pass a `theme` prop. I'm not sure if it applies them based on
    its own arbitrary default, or if it's listening to the system-wide
    theme setting. But we're left with a choice between papering over
    each of those overridden styles, or taking up their theme interface
    and doing something with it.
    
    We take the latter approach because it's not piecemeal. (It would be
    tricky to find all the forced styles we'd need to override, and it's
    not obvious that we'll always be able to paper over them. One such
    fix is to set a `backgroundColor` within `sceneContainerStyle` in
    `MainTabs` -- but this wouldn't have been possible even a few months
    ago, before @react-navigation/bottom-tabs@5.10.0.)
    
    To smoothen the change toward using the `theme` prop in v5, start
    doing so now, while we're still on v4. Theme support is much more
    rudimentary in v4 (i.e., the `theme` prop here just takes "dark" or
    "light"), but I don't see anything particularly disruptive by
    starting to use it now [1], so, might as well.
    
    [1] https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/React.20Navigation.20v5.3A.20theme.20colors/near/1061811

--- src/nav/ZulipAppContainer.js
+++ src/nav/ZulipAppContainer.js
@@ -8,13 +8,16 @@ import {
 } from 'react-navigation';
 
 import { connect } from '../react-redux';
+import type { ThemeData } from '../styles';
+import { ThemeContext } from '../styles';
 import * as NavigationService from './NavigationService';
 import getInitialRouteInfo from './getInitialRouteInfo';
-import type { Dispatch, Account } from '../types';
-import { hasAuth as getHasAuth, getAccounts, getHaveServerData } from '../selectors';
+import type { Dispatch, Account, ThemeName } from '../types';
+import { hasAuth as getHasAuth, getAccounts, getHaveServerData, getSettings } from '../selectors';
 import { createAppNavigator } from './AppNavigator';
 
 type SelectorProps = $ReadOnly<{|
+  theme: ThemeName,
   hasAuth: boolean,
   accounts: Account[],
   haveServerData: boolean,
@@ -36,6 +39,9 @@ type Props = $ReadOnly<{|
  *   and `initialRouteParams` which we get from data in Redux.
  */
 class ZulipAppContainer extends PureComponent<Props> {
+  static contextType = ThemeContext;
+  context: ThemeData;
+
   // (odd spacing choices)
   // eslint-disable-next-line
   AppContainer: NavigationContainer<
@@ -54,11 +60,23 @@ class ZulipAppContainer extends PureComponent<Props> {
 
   render() {
     const { AppContainer } = this;
-    return <AppContainer ref={NavigationService.appContainerRef} />;
+    const { theme } = this.props;
+
+    return (
+      // The `theme` prop is documented, but apparently not included
+      // in the type we're using:
+      // https://reactnavigation.org/docs/4.x/themes/
+      // $FlowFixMe
+      <AppContainer
+        ref={NavigationService.appContainerRef}
+        theme={theme === 'default' ? 'light' : 'dark'}
+      />
+    );
   }
 }
 
 export default connect(state => ({
+  theme: getSettings(state).theme,
   hasAuth: getHasAuth(state),
   accounts: getAccounts(state),
   haveServerData: getHaveServerData(state),
