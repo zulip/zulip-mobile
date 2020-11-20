b902744345c559b6f80fea1e94ad96ec7e073788
Chris Bobbe <cbobbe@zulip.com>
2020-11-20 15:34:26 -0500

    avatar [nfc]: Make `getAvatarUrl` use the AvatarURL class.
    At this point, we switch out the old string-heavy, no-classes
    implementation of "compute the actual URL from the pattern the
    server provides in `avatar_url`" for the new one we've just
    introduced.
    
    We expect this to be NFC; we've combed through the possible things
    we could get from the server and ironed out various wrinkles [1].
    
    [1] https://github.com/zulip/zulip-mobile/pull/4230#discussion_r513075744

--- src/utils/__tests__/avatar-test.js
+++ src/utils/__tests__/avatar-test.js
@@ -1,13 +1,7 @@
 /* @flow strict-local */
 import md5 from 'blueimp-md5';
 
-import {
-  AvatarURL,
-  GravatarURL,
-  UploadedAvatarURL,
-  getMediumAvatar,
-  getGravatarFromEmail,
-} from '../avatar';
+import { AvatarURL, GravatarURL, UploadedAvatarURL } from '../avatar';
 import * as eg from '../../__tests__/lib/exampleData';
 
 describe('AvatarURL', () => {
@@ -153,41 +147,5 @@ describe('UploadedAvatarURL', () => {
         'https://chat.zulip.org/user_avatars/2/e35cdbc4771c5e4b94e705bf6ff7cca7fa1efcae.png?x=x&version=2',
       );
     });
-  });
-});
-
-// avatarUrl can be converted to retrieve medium sized avatars(mediumAvatarUrl) if and only if
-// avatarUrl contains avatar image name with a .png extension (e.g. AVATAR_IMAGE_NAME.png).
-
-describe('getMediumAvatar', () => {
-  test('if avatarUrl can be converted into mediumAvatarUrl, return mediumAvatarUrl', () => {
-    const avatarUrl = '/user_avatars/AVATAR_IMAGE_NAME.png/';
-    const mediumAvatarUrl = '/user_avatars/AVATAR_IMAGE_NAME-medium.png/';
-
-    const resultUrl = getMediumAvatar(avatarUrl);
-
-    expect(resultUrl).toEqual(mediumAvatarUrl);
-  });
-
-  test('if avatarUrl cannot be converted into mediumAvatarUrl, return avatarUrl itself', () => {
-    const avatarUrl = '/avatar/AVATAR_IMAGE_NAME/';
-
-    const resultUrl = getMediumAvatar(avatarUrl);
-
-    expect(resultUrl).toEqual(avatarUrl);
-  });
-});
-
-describe('getGravatarFromEmail', () => {
-  test('given an email return gravatar url', () => {
-    expect(getGravatarFromEmail('test@example.com', 80)).toEqual(
-      'https://secure.gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0?d=identicon&s=80',
-    );
-  });
-
-  test('given a case-sensitive email canonize and return gravatar url', () => {
-    expect(getGravatarFromEmail('Test@example.com', 80)).toEqual(
-      'https://secure.gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0?d=identicon&s=80',
-    );
   });
 });
--- src/utils/avatar.js
+++ src/utils/avatar.js
@@ -253,59 +253,15 @@ export class UploadedAvatarURL extends AvatarURL {
   }
 }
 
-/**
- * When selecting the size of a gravatar we can pick any arbitrary
- * size we want. For server-uploaded avatars this is not the case.
- * We have only:
- *  * default - image is 100x100
- *  * medium - image is 500x500
- *
- * This function converts a normal avatar to medium-sized one.
- */
-export const getMediumAvatar = (avatarUrl: string): string => {
-  const matches = new RegExp(/(\w+)\.png/g).exec(avatarUrl);
-
-  return matches ? avatarUrl.replace(matches[0], `${matches[1]}-medium.png`) : avatarUrl;
-};
-
-export const getGravatarFromEmail = (email: string = '', sizePhysicalPx: number): string =>
-  `https://secure.gravatar.com/avatar/${md5(email.toLowerCase())}?d=identicon&s=${sizePhysicalPx}`;
-
 export const getAvatarUrl = (
   avatarUrl: ?string,
   email: string,
   realm: URL,
   sizePhysicalPx: number,
-): string => {
-  if (typeof avatarUrl !== 'string') {
-    // It's our job to compute the Gravatar hash.
-    return getGravatarFromEmail(email, sizePhysicalPx);
-  }
-
-  // If we don't announce `client_gravatar` to the server (we
-  // sometimes don't), or if the server doesn't have
-  // `EMAIL_ADDRESS_VISIBILITY_EVERYONE` set, `avatarUrl` will be a
-  // Gravatar URL, and we should size it correctly with the `size`
-  // parameter.
-  const GRAVATAR_ORIGIN = 'https://secure.gravatar.com';
-  if (tryParseUrl(avatarUrl)?.origin === GRAVATAR_ORIGIN) {
-    const hashMatch = /[0-9a-fA-F]{32}$/.exec(new URL(avatarUrl).pathname);
-    if (hashMatch === null) {
-      const msg = 'Unexpected Gravatar URL shape from server.';
-      logging.error(msg, { value: avatarUrl });
-      throw new Error(msg);
-    }
-
-    const url = new URL(`/avatar/${hashMatch[0]}`, GRAVATAR_ORIGIN);
-    url.searchParams.set('d', 'identicon');
-    url.searchParams.set('s', sizePhysicalPx.toString());
-    return url.toString();
-  }
-
-  const fullUrl = new URL(avatarUrl, realm).toString();
-
-  return sizePhysicalPx > 100 ? getMediumAvatar(fullUrl) : fullUrl;
-};
+): string =>
+  AvatarURL.fromUserOrBotData({ rawAvatarUrl: avatarUrl, email, realm })
+    .get(sizePhysicalPx)
+    .toString();
 
 export const getAvatarFromUser = (user: UserOrBot, realm: URL, sizePhysicalPx: number): string =>
   getAvatarUrl(user.avatar_url, user.email, realm, sizePhysicalPx);
