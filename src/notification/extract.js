5d698d2e5d205d3190428f46fd29e865e3e6f780
Greg Price <greg@zulipchat.com>
2020-12-08 14:38:31 -0800

    notif: Sort user IDs correctly in pm_users.
    This fell into a classic JS pitfall: when sorting an array of numbers,
    the default comparison function is by string, and you need to pass an
    explicit comparison function to sort the actual numbers.
    
    This means that in b022e9c1d we actually broke this invariant which
    was probably previously always true in practice, oops.  Fortunately we
    generally avoid actually relying on that invariant; see comment on
    pmNarrowFromEmails (previously groupNarrow) in src/utils/narrow.js.

--- src/notification/extract.js
+++ src/notification/extract.js
@@ -186,7 +186,11 @@ export const fromAPNsImpl = (rawData: JSONableDict): Notification | void => {
       if (ids.some(id => Number.isNaN(id))) {
         throw err('invalid');
       }
-      return { recipient_type: 'private', pm_users: ids.sort().join(','), ...realm_uri_obj };
+      return {
+        recipient_type: 'private',
+        pm_users: ids.sort((a, b) => a - b).join(','),
+        ...realm_uri_obj,
+      };
     }
 
     if (typeof sender_email !== 'string') {
