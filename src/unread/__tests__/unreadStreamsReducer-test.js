0aa7705f2d3f406b8239a3fa74c56c50494b9d0e
Chris Bobbe <cbobbe@zulip.com>
2020-12-10 15:04:14 -0500

    unreadStreamsReducer tests: Start type-checking and use example data.
    And tweak a few tests to be a bit more straightforward.

--- src/unread/__tests__/unreadStreamsReducer-test.js
+++ src/unread/__tests__/unreadStreamsReducer-test.js
@@ -1,13 +1,10 @@
+/* @flow strict-local */
 import deepFreeze from 'deep-freeze';
 
 import unreadStreamsReducer from '../unreadStreamsReducer';
-import {
-  REALM_INIT,
-  ACCOUNT_SWITCH,
-  EVENT_NEW_MESSAGE,
-  EVENT_UPDATE_MESSAGE_FLAGS,
-} from '../../actionConstants';
+import { ACCOUNT_SWITCH, EVENT_UPDATE_MESSAGE_FLAGS } from '../../actionConstants';
 import { NULL_ARRAY } from '../../nullObjects';
+import * as eg from '../../__tests__/lib/exampleData';
 
 describe('unreadStreamsReducer', () => {
   describe('ACCOUNT_SWITCH', () => {
@@ -22,6 +19,7 @@ describe('unreadStreamsReducer', () => {
 
       const action = deepFreeze({
         type: ACCOUNT_SWITCH,
+        index: 1,
       });
 
       const expectedState = [];
@@ -36,29 +34,33 @@ describe('unreadStreamsReducer', () => {
     test('received data from "unread_msgs.streams" key replaces the current state ', () => {
       const initialState = deepFreeze([]);
 
+      const message1 = eg.streamMessage({ id: 1 });
+
       const action = deepFreeze({
-        type: REALM_INIT,
+        ...eg.action.realm_init,
         data: {
+          ...eg.action.realm_init.data,
           unread_msgs: {
+            ...eg.action.realm_init.data.unread_msgs,
             streams: [
               {
-                stream_id: 1,
-                topic: 'some topic',
-                unread_message_ids: [1, 2],
+                stream_id: message1.stream_id,
+                topic: message1.subject,
+                unread_message_ids: [message1.id, 2],
               },
             ],
-            huddles: [{}, {}, {}],
-            pms: [{}, {}, {}],
-            mentions: [1, 2, 3],
+            huddles: [],
+            pms: [],
+            mentions: [message1.id, 2, 3],
           },
         },
       });
 
       const expectedState = [
         {
-          stream_id: 1,
-          topic: 'some topic',
-          unread_message_ids: [1, 2],
+          stream_id: message1.stream_id,
+          topic: message1.subject,
+          unread_message_ids: [message1.id, 2],
         },
       ];
 
@@ -70,22 +72,18 @@ describe('unreadStreamsReducer', () => {
 
   describe('EVENT_NEW_MESSAGE', () => {
     test('if message id already exists, do not mutate state', () => {
+      const message1 = eg.streamMessage({ id: 1 });
       const initialState = deepFreeze([
         {
-          stream_id: 1,
-          topic: 'some topic',
-          unread_message_ids: [1, 2],
+          stream_id: message1.stream_id,
+          topic: message1.subject,
+          unread_message_ids: [message1.id],
         },
       ]);
 
       const action = deepFreeze({
-        type: EVENT_NEW_MESSAGE,
-        message: {
-          id: 1,
-          type: 'stream',
-          stream_id: 1,
-          subject: 'some topic',
-        },
+        ...eg.eventNewMessageActionBase,
+        message: message1,
       });
 
       const actualState = unreadStreamsReducer(initialState, action);
@@ -94,20 +92,18 @@ describe('unreadStreamsReducer', () => {
     });
 
     test('if message is not stream, return original state', () => {
+      const message4 = eg.pmMessage({ id: 4 });
       const initialState = deepFreeze([
         {
-          sender_id: 1,
+          stream_id: message4.stream_id,
+          topic: message4.subject,
           unread_message_ids: [1, 2, 3],
         },
       ]);
 
       const action = deepFreeze({
-        type: EVENT_NEW_MESSAGE,
-        message: {
-          id: 4,
-          type: 'private',
-          sender_id: 1,
-        },
+        ...eg.eventNewMessageActionBase,
+        message: message4,
       });
 
       const actualState = unreadStreamsReducer(initialState, action);
@@ -117,17 +113,12 @@ describe('unreadStreamsReducer', () => {
 
     test('if message is sent by self, do not mutate state', () => {
       const initialState = deepFreeze([]);
+      const message1 = eg.streamMessage({ sender: eg.selfUser });
 
       const action = deepFreeze({
-        type: EVENT_NEW_MESSAGE,
-        message: {
-          id: 1,
-          type: 'stream',
-          stream_id: 2,
-          subject: 'another topic',
-          sender_email: 'me@example.com',
-        },
-        ownEmail: 'me@example.com',
+        ...eg.eventNewMessageActionBase,
+        message: message1,
+        ownEmail: eg.selfUser.email,
       });
 
       const actualState = unreadStreamsReducer(initialState, action);
@@ -136,29 +127,26 @@ describe('unreadStreamsReducer', () => {
     });
 
     test('if message id does not exist, append to state', () => {
+      const message4 = eg.streamMessage({ id: 4 });
+
       const initialState = deepFreeze([
         {
-          stream_id: 1,
-          topic: 'some topic',
+          stream_id: message4.stream_id,
+          topic: message4.subject,
           unread_message_ids: [1, 2, 3],
         },
       ]);
 
       const action = deepFreeze({
-        type: EVENT_NEW_MESSAGE,
-        message: {
-          id: 4,
-          type: 'stream',
-          stream_id: 1,
-          subject: 'some topic',
-        },
+        ...eg.eventNewMessageActionBase,
+        message: message4,
       });
 
       const expectedState = [
         {
-          stream_id: 1,
-          topic: 'some topic',
-          unread_message_ids: [1, 2, 3, 4],
+          stream_id: message4.stream_id,
+          topic: message4.subject,
+          unread_message_ids: [1, 2, 3, message4.id],
         },
       ];
 
@@ -168,6 +156,8 @@ describe('unreadStreamsReducer', () => {
     });
 
     test('if stream with topic does not exist, append to state', () => {
+      const message4 = eg.streamMessage({ id: 4, stream_id: 2, subject: 'another topic' });
+
       const initialState = deepFreeze([
         {
           stream_id: 1,
@@ -177,13 +167,8 @@ describe('unreadStreamsReducer', () => {
       ]);
 
       const action = deepFreeze({
-        type: EVENT_NEW_MESSAGE,
-        message: {
-          id: 4,
-          type: 'stream',
-          stream_id: 2,
-          subject: 'another topic',
-        },
+        ...eg.eventNewMessageActionBase,
+        message: message4,
       });
 
       const expectedState = [
@@ -193,9 +178,9 @@ describe('unreadStreamsReducer', () => {
           unread_message_ids: [1, 2, 3],
         },
         {
-          stream_id: 2,
-          topic: 'another topic',
-          unread_message_ids: [4],
+          stream_id: message4.stream_id,
+          topic: message4.subject,
+          unread_message_ids: [message4.id],
         },
       ];
 
@@ -210,7 +195,10 @@ describe('unreadStreamsReducer', () => {
       const initialState = deepFreeze([]);
 
       const action = {
+        id: 1,
         type: EVENT_UPDATE_MESSAGE_FLAGS,
+        all: false,
+        allMessages: {},
         messages: [1, 2, 3],
         flag: 'star',
         operation: 'add',
@@ -236,7 +224,10 @@ describe('unreadStreamsReducer', () => {
       ]);
 
       const action = deepFreeze({
+        id: 1,
         type: EVENT_UPDATE_MESSAGE_FLAGS,
+        all: false,
+        allMessages: {},
         messages: [6, 7],
         flag: 'read',
         operation: 'add',
@@ -262,7 +253,10 @@ describe('unreadStreamsReducer', () => {
       ]);
 
       const action = deepFreeze({
+        id: 1,
         type: EVENT_UPDATE_MESSAGE_FLAGS,
+        all: false,
+        allMessages: {},
         messages: [3, 4, 5, 6],
         flag: 'read',
         operation: 'add',
@@ -291,7 +285,10 @@ describe('unreadStreamsReducer', () => {
       ]);
 
       const action = deepFreeze({
+        id: 1,
         type: EVENT_UPDATE_MESSAGE_FLAGS,
+        all: false,
+        allMessages: {},
         messages: [1, 2],
         flag: 'read',
         operation: 'remove',
@@ -312,11 +309,13 @@ describe('unreadStreamsReducer', () => {
       ]);
 
       const action = deepFreeze({
+        id: 1,
         type: EVENT_UPDATE_MESSAGE_FLAGS,
+        all: true,
+        allMessages: {},
         messages: [],
         flag: 'read',
         operation: 'add',
-        all: true,
       });
 
       const actualState = unreadStreamsReducer(initialState, action);
