6410d722ab645d2953a0ccde593a4da0b31f04f6
Greg Price <greg@zulipchat.com>
2020-12-11 23:49:06 -0800

    title buttons [nfc]: Use a more straightforward React idiom, and caseNarrow.

--- src/title-buttons/titleButtonFromNarrow.js
+++ src/title-buttons/titleButtonFromNarrow.js
@@ -3,14 +3,7 @@ import React from 'react';
 import type { ComponentType } from 'react';
 
 import type { Narrow } from '../types';
-import {
-  isHomeNarrow,
-  is1to1PmNarrow,
-  isGroupPmNarrow,
-  isSpecialNarrow,
-  isStreamNarrow,
-  isTopicNarrow,
-} from '../utils/narrow';
+import { caseNarrowDefault } from '../utils/narrow';
 import InfoNavButtonStream from './InfoNavButtonStream';
 import InfoNavButtonPrivate from './InfoNavButtonPrivate';
 import InfoNavButtonGroup from './InfoNavButtonGroup';
@@ -18,36 +11,25 @@ import ExtraNavButtonStream from './ExtraNavButtonStream';
 import ExtraNavButtonTopic from './ExtraNavButtonTopic';
 
 type Props = $ReadOnly<{| color: string, narrow: Narrow |}>;
-type NarrowNavButton = ComponentType<Props>;
-type NarrowNavButtonCandidate = {|
-  isFunc: Narrow => boolean,
-  ButtonComponent: NarrowNavButton | null,
-|};
 
-const infoButtonHandlers: NarrowNavButtonCandidate[] = [
-  { isFunc: isHomeNarrow, ButtonComponent: null },
-  { isFunc: isSpecialNarrow, ButtonComponent: null },
-  { isFunc: isStreamNarrow, ButtonComponent: InfoNavButtonStream },
-  { isFunc: isTopicNarrow, ButtonComponent: InfoNavButtonStream },
-  { isFunc: is1to1PmNarrow, ButtonComponent: InfoNavButtonPrivate },
-  { isFunc: isGroupPmNarrow, ButtonComponent: InfoNavButtonGroup },
-];
+export const InfoButton: ComponentType<Props> = props =>
+  caseNarrowDefault(
+    props.narrow,
+    {
+      stream: streamName => <InfoNavButtonStream {...props} />,
+      topic: (streamName, topic) => <InfoNavButtonStream {...props} />,
+      pm: (emails, ids) =>
+        ids.length === 1 ? <InfoNavButtonPrivate {...props} /> : <InfoNavButtonGroup {...props} />,
+    },
+    () => false,
+  );
 
-const extraButtonHandlers: NarrowNavButtonCandidate[] = [
-  { isFunc: isHomeNarrow, ButtonComponent: null },
-  { isFunc: isSpecialNarrow, ButtonComponent: null },
-  { isFunc: isStreamNarrow, ButtonComponent: ExtraNavButtonStream },
-  { isFunc: isTopicNarrow, ButtonComponent: ExtraNavButtonTopic },
-  { isFunc: is1to1PmNarrow, ButtonComponent: null },
-  { isFunc: isGroupPmNarrow, ButtonComponent: null },
-];
-
-const makeButton = (handlers): NarrowNavButton => props => {
-  const handler = handlers.find(x => x.isFunc(props.narrow)) || null;
-  const SpecificButton = handler && handler.ButtonComponent;
-  return !!SpecificButton && <SpecificButton {...props} />;
-};
-
-export const InfoButton = makeButton(infoButtonHandlers);
-
-export const ExtraButton = makeButton(extraButtonHandlers);
+export const ExtraButton: ComponentType<Props> = props =>
+  caseNarrowDefault(
+    props.narrow,
+    {
+      stream: streamName => <ExtraNavButtonStream {...props} />,
+      topic: (streamName, topic) => <ExtraNavButtonTopic {...props} />,
+    },
+    () => false,
+  );
