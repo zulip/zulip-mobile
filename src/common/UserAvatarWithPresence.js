fbb7153d8e37f5f42116f07afea4913f0bc64d62
Greg Price <greg@zulipchat.com>
2020-12-11 23:35:15 -0800

    user: Add "...ById" variant for UserAvatarWithPresence.
    We'll temporarily have two variants of this component -- an
    implementation in common but with slightly different interfaces -- as a
    way to smoothly migrate from callers using one interface to the other.
    
    The new interface keeps callers from having to know or care about
    emails, which we're moving away from.  It also encapsulates the need
    to find the user's avatar URL; the caller only has to be concerned
    with identifying which user it wants represented, by user ID.  This
    improves react-redux hygiene: it means that when that user's avatar
    changes, we no longer have to rerender the calling component, which
    fundamentally shouldn't care about the specific avatar URL.

--- src/common/UserAvatarWithPresence.js
+++ src/common/UserAvatarWithPresence.js
@@ -1,11 +1,13 @@
 /* @flow strict-local */
-
-import React, { PureComponent } from 'react';
+import React, { type ComponentType, PureComponent } from 'react';
 
 import { createStyleSheet } from '../styles';
+import type { Dispatch } from '../types';
 import UserAvatar from './UserAvatar';
 import PresenceStatusIndicator from './PresenceStatusIndicator';
 import { AvatarURL } from '../utils/avatar';
+import { getUserForId } from '../users/userSelectors';
+import { connect } from '../react-redux';
 
 const styles = createStyleSheet({
   status: {
@@ -17,20 +19,28 @@ const styles = createStyleSheet({
 
 type Props = $ReadOnly<{|
   avatarUrl: AvatarURL,
-  email: string,
   size: number,
   onPress?: () => void,
+  email: string,
 |}>;
 
 /**
- * Renders a user avatar with a PresenceStatusIndicator in the corner
+ * A user avatar with a PresenceStatusIndicator in the corner.
+ *
+ * Prefer `UserAvatarWithPresenceById` over this component: it does the same
+ * thing but avoids an email in the component's interface.  Once all callers
+ * have migrated to that version, it'll replace this one.
  *
  * @prop [avatarUrl]
  * @prop [email] - Sender's / user's email address, for the presence dot.
  * @prop [size] - Sets width and height in logical pixels.
  * @prop [onPress] - Event fired on pressing the component.
  */
-export default class UserAvatarWithPresence extends PureComponent<Props> {
+// The underlying class gets an inexact Props type to express that it's fine
+// for it to be passed extra props by the implementation of …ById.
+// We don't export it with that type, because we want exact Props types to
+// get the most effective type-checking.
+class UserAvatarWithPresence extends PureComponent<$ReadOnly<{ ...Props, ... }>> {
   render() {
     const { avatarUrl, email, size, onPress } = this.props;
 
@@ -41,3 +51,33 @@ export default class UserAvatarWithPresence extends PureComponent<Props> {
     );
   }
 }
+
+// Export the class with a tighter constraint on acceptable props, namely
+// that the type is an exact object type as usual.
+export default (UserAvatarWithPresence: ComponentType<Props>);
+
+/**
+ * A user avatar with a PresenceStatusIndicator in the corner.
+ *
+ * Use this in preference to the default export `UserAvatarWithPresence`.
+ * We're migrating from that one to this in order to avoid using emails.
+ *
+ * @prop [userId]
+ * @prop [size]
+ * @prop [onPress]
+ */
+export const UserAvatarWithPresenceById = connect<{| avatarUrl: AvatarURL, email: string |}, _, _>(
+  (state, props) => {
+    const user = getUserForId(state, props.userId);
+    return { avatarUrl: user.avatar_url, email: user.email };
+  },
+)(
+  // The type inference embedded in our `connect` type relies on seeing the
+  // exact Props type for the underlying component, complete with all the
+  // expected props.  Normally that's just how our Props types are in the
+  // first place, but here we have to provide it explicitly.
+  /* eslint-disable flowtype/generic-spacing */
+  (UserAvatarWithPresence: ComponentType<
+    $ReadOnly<{| ...Props, dispatch: Dispatch, userId: number |}>,
+  >),
+);
