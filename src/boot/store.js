25f147fc16ff04080a0df61a845afb4046ab408d
Chris Bobbe <cbobbe@zulip.com>
2020-12-18 14:49:45 -0500

    store [nfc]: Inline remotedev-serialize (15/x).
    Like we did in the previous commit for `reviver`, consolidate the
    conditions in `replacer` so they look consistent. Since we're
    inlining remotedev-serialize, there's no meaningful distinction
    between `customReviver` and `defaultReviver` anymore.

--- src/boot/store.js
+++ src/boot/store.js
@@ -371,7 +371,6 @@ const jsanOptions = {
 };
 
 const replacer = function replacer(key, value) {
-  // customReplacer, previously
   if (value instanceof ZulipVersion) {
     return { data: value.raw(), [SERIALIZED_TYPE_FIELD_NAME]: 'ZulipVersion' };
   } else if (value instanceof URL) {
@@ -388,13 +387,9 @@ const replacer = function replacer(key, value) {
       data: FallbackAvatarURL.serialize(value),
       [SERIALIZED_TYPE_FIELD_NAME]: 'FallbackAvatarURL',
     };
-  }
-
-  // defaultReplacer, previously
-  if (Immutable.Map.isMap(value)) {
+  } else if (Immutable.Map.isMap(value)) {
     return mark(value, 'ImmutableMap', 'toObject');
-  }
-  if (typeof value === 'object' && value !== null && SERIALIZED_TYPE_FIELD_NAME in value) {
+  } else if (typeof value === 'object' && value !== null && SERIALIZED_TYPE_FIELD_NAME in value) {
     const copy = { ...value };
     delete copy[SERIALIZED_TYPE_FIELD_NAME];
     return {
