59a356a0af0d162299259b150e447b18c63a3afe
Greg Price <greg@zulipchat.com>
2020-12-18 21:49:13 -0800

    redux [nfc]: Inline combineReducers.
    We'll use this to give ourselves some more flexibility in the
    structure of our overall reducer function.
    
    The upstream combineReducers has a number of dev-only checks to make
    sure that the reducers object has the right shape and the individual
    sub-reducers have the right behavior.  These checks aren't helpful
    for us (they're largely subsumed by type-checking, and they're mostly
    helpful when first figuring out how to use Redux in any case), so
    we'll delete them shortly.  This version just comments out the key
    bits of them, to save us from having to copy all the detailed helper
    functions for those checks only to delete them.

--- src/boot/reducers.js
+++ src/boot/reducers.js
@@ -1,5 +1,4 @@
 /* @flow strict-local */
-import { combineReducers } from 'redux';
 import type { CombinedReducer } from 'redux';
 import { enableBatching } from 'redux-batched-actions';
 
@@ -61,7 +60,78 @@ const reducers = {
 
 export const ALL_KEYS: string[] = Object.keys(reducers);
 
-const combinedReducer: CombinedReducer<GlobalState, Action> = combineReducers(
+// Inlined just now from Redux upstream.
+// We'll clean this up in the next few commits.
+/* eslint-disable semi */
+/* eslint-disable no-shadow */
+// prettier-ignore
+const combinedReducer: CombinedReducer<GlobalState, Action> = (reducers => {
+  const reducerKeys = Object.keys(reducers)
+  const finalReducers = {}
+  for (let i = 0; i < reducerKeys.length; i++) {
+    const key = reducerKeys[i]
+
+    if (process.env.NODE_ENV !== 'production') {
+      if (typeof reducers[key] === 'undefined') {
+        // warning(`No reducer provided for key "${key}"`)
+      }
+    }
+
+    if (typeof reducers[key] === 'function') {
+      finalReducers[key] = reducers[key]
+    }
+  }
+  const finalReducerKeys = Object.keys(finalReducers)
+
+  // This is used to make sure we don't warn about the same
+  // keys multiple times.
+  let unexpectedKeyCache
+  if (process.env.NODE_ENV !== 'production') {
+    unexpectedKeyCache = {} /* eslint-disable-line no-unused-vars */
+  }
+
+  let shapeAssertionError
+  try {
+    // assertReducerShape(finalReducers)
+  } catch (e) {
+    shapeAssertionError = e
+  }
+
+  return function combination(state = {}, action) {
+    if (shapeAssertionError) {
+      throw shapeAssertionError
+    }
+
+    if (process.env.NODE_ENV !== 'production') {
+      const warningMessage = undefined; /* getUnexpectedStateShapeWarningMessage(
+        state,
+        finalReducers,
+        action,
+        unexpectedKeyCache
+      ) */
+      if (warningMessage) {
+        // warning(warningMessage)
+      }
+    }
+
+    let hasChanged = false
+    const nextState = {}
+    for (let i = 0; i < finalReducerKeys.length; i++) {
+      const key = finalReducerKeys[i]
+      const reducer = finalReducers[key]
+      const previousStateForKey = state[key]
+      const nextStateForKey = reducer(previousStateForKey, action)
+      if (typeof nextStateForKey === 'undefined') {
+        const errorMessage = 'error' // getUndefinedStateErrorMessage(key, action)
+        throw new Error(errorMessage)
+      }
+      nextState[key] = nextStateForKey
+      hasChanged = hasChanged || nextStateForKey !== previousStateForKey
+    }
+    // $FlowFixMe -- errors from those empty object literals
+    return hasChanged ? nextState : state
+  }
+})(
   config.enableReduxSlowReducerWarnings ? logSlowReducers(reducers) : reducers,
 );
 
