b9f063245c44d909d49096212d97f3d4c4e10887
Greg Price <greg@zulipchat.com>
2020-12-18 21:54:26 -0800

    redux [nfc]: Simplify combined reducer (1/n): cut dev checks.
    As mentioned in the previous commit, we don't really need these
    checks, and so we disabled them when inlining this code in the
    first place.

--- src/boot/reducers.js
+++ src/boot/reducers.js
@@ -67,64 +67,17 @@ export const ALL_KEYS: string[] = Object.keys(reducers);
 // prettier-ignore
 const combinedReducer: CombinedReducer<GlobalState, Action> = (reducers => {
   const reducerKeys = Object.keys(reducers)
-  const finalReducers = {}
-  for (let i = 0; i < reducerKeys.length; i++) {
-    const key = reducerKeys[i]
-
-    if (process.env.NODE_ENV !== 'production') {
-      if (typeof reducers[key] === 'undefined') {
-        // warning(`No reducer provided for key "${key}"`)
-      }
-    }
-
-    if (typeof reducers[key] === 'function') {
-      finalReducers[key] = reducers[key]
-    }
-  }
-  const finalReducerKeys = Object.keys(finalReducers)
-
-  // This is used to make sure we don't warn about the same
-  // keys multiple times.
-  let unexpectedKeyCache
-  if (process.env.NODE_ENV !== 'production') {
-    unexpectedKeyCache = {} /* eslint-disable-line no-unused-vars */
-  }
-
-  let shapeAssertionError
-  try {
-    // assertReducerShape(finalReducers)
-  } catch (e) {
-    shapeAssertionError = e
-  }
 
   return function combination(state = {}, action) {
-    if (shapeAssertionError) {
-      throw shapeAssertionError
-    }
-
-    if (process.env.NODE_ENV !== 'production') {
-      const warningMessage = undefined; /* getUnexpectedStateShapeWarningMessage(
-        state,
-        finalReducers,
-        action,
-        unexpectedKeyCache
-      ) */
-      if (warningMessage) {
-        // warning(warningMessage)
-      }
-    }
-
     let hasChanged = false
     const nextState = {}
-    for (let i = 0; i < finalReducerKeys.length; i++) {
-      const key = finalReducerKeys[i]
-      const reducer = finalReducers[key]
+    for (let i = 0; i < reducerKeys.length; i++) {
+      const key = reducerKeys[i]
+      const reducer = reducers[key]
+      // $FlowFixMe
       const previousStateForKey = state[key]
+      // $FlowFixMe
       const nextStateForKey = reducer(previousStateForKey, action)
-      if (typeof nextStateForKey === 'undefined') {
-        const errorMessage = 'error' // getUndefinedStateErrorMessage(key, action)
-        throw new Error(errorMessage)
-      }
       nextState[key] = nextStateForKey
       hasChanged = hasChanged || nextStateForKey !== previousStateForKey
     }
