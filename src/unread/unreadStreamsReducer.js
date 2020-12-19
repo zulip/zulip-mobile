f712a72cab27c51fcaf03ef8191c5a3328467fd3
Greg Price <greg@zulipchat.com>
2020-12-18 22:37:06 -0800

    unread [nfc]: Use global state in unreadStreamsReducer, in a small way.
    This is one of the places where we've already been using information
    from elsewhere in the state, and have done so via a hack where that
    information gets stuck into the action as an extra property.
    
    Instead, use our new more-direct mechanism for getting at such data
    in a reducer, where it's simply passed down as an argument.
    
    This is a step toward cleaning up that `ownUserId` hack.  It also
    serves as a demonstration of the new mechanism, by being the first
    place we substantively use it.

--- src/unread/unreadStreamsReducer.js
+++ src/unread/unreadStreamsReducer.js
@@ -1,5 +1,5 @@
 /* @flow strict-local */
-import type { Action } from '../types';
+import type { Action, GlobalState } from '../types';
 import type { UnreadStreamsState } from './unreadModelTypes';
 import {
   REALM_INIT,
@@ -11,15 +11,16 @@ import {
 } from '../actionConstants';
 import { addItemsToStreamArray, removeItemsDeeply } from './unreadHelpers';
 import { NULL_ARRAY } from '../nullObjects';
+import { getOwnUserId } from '../users/userSelectors';
 
 const initialState: UnreadStreamsState = NULL_ARRAY;
 
-const eventNewMessage = (state, action) => {
+const eventNewMessage = (state, action, globalState) => {
   if (action.message.type !== 'stream') {
     return state;
   }
 
-  if (action.ownUserId === action.message.sender_id) {
+  if (getOwnUserId(globalState) === action.message.sender_id) {
     return state;
   }
 
@@ -49,7 +50,11 @@ const eventUpdateMessageFlags = (state, action) => {
   return state;
 };
 
-export default (state: UnreadStreamsState = initialState, action: Action): UnreadStreamsState => {
+export default (
+  state: UnreadStreamsState = initialState,
+  action: Action,
+  globalState: GlobalState,
+): UnreadStreamsState => {
   switch (action.type) {
     case LOGOUT:
     case ACCOUNT_SWITCH:
@@ -59,7 +64,7 @@ export default (state: UnreadStreamsState = initialState, action: Action): Unrea
       return (action.data.unread_msgs && action.data.unread_msgs.streams) || initialState;
 
     case EVENT_NEW_MESSAGE:
-      return eventNewMessage(state, action);
+      return eventNewMessage(state, action, globalState);
 
     case EVENT_MESSAGE_DELETE:
       return removeItemsDeeply(state, action.messageIds);
