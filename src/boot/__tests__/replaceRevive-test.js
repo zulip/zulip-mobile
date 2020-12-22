69834ac34b96dd64658af3bc36ce4d9131cb2eb4
Chris Bobbe <cbobbe@zulip.com>
2020-12-22 14:46:41 -0500

    replaceRevive tests [nfc]: Bring closer to our style.
    Remove the `.prettierignore` entry and the universal
    `eslint-disable`, reformat the file, and add in some
    `eslint-disable`s for specific rules.

--- src/boot/__tests__/replaceRevive-test.js
+++ src/boot/__tests__/replaceRevive-test.js
@@ -1,116 +1,122 @@
-/* eslint-disable */
+/* eslint-disable no-shadow */
+/* eslint-disable id-match */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/no-conditional-expect */
+/* eslint-disable new-cap */
 
-var Immutable = require('immutable');
-var Serialize = require('remotedev-serialize').immutable;
-var serialize = Serialize(Immutable);
-var stringify = serialize.stringify;
-var parse = serialize.parse;
+import Immutable from 'immutable';
+import { immutable as Serialize } from 'remotedev-serialize';
 
-var data = {
+const serialize = Serialize(Immutable);
+const stringify = serialize.stringify;
+const parse = serialize.parse;
+
+const data = {
   map: Immutable.Map({ a: 1, b: 2, c: 3, d: 4 }),
   orderedMap: Immutable.OrderedMap({ b: 2, a: 1, c: 3, d: 4 }),
-  list: Immutable.List([1,2,3,4,5,6,7,8,9,10]),
-  range: Immutable.Range(0,7),
+  list: Immutable.List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+  range: Immutable.Range(0, 7),
   repeat: Immutable.Repeat('hi', 100),
-  set: Immutable.Set([10,9,8,7,6,5,4,3,2,1]),
-  orderedSet: Immutable.OrderedSet([10,9,8,7,6,5,4,3,2,1]),
-  seq: Immutable.Seq.Indexed.of(1,2,3,4,5,6,7,8),
+  set: Immutable.Set([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]),
+  orderedSet: Immutable.OrderedSet([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]),
+  seq: Immutable.Seq.Indexed.of(1, 2, 3, 4, 5, 6, 7, 8),
   stack: Immutable.Stack.of('a', 'b', 'c'),
   withTypeKey: {
     a: 1,
     __serializedType__: {
       b: [2],
-      __serializedType__: {c: [3]},
+      __serializedType__: { c: [3] },
     },
-  }
+  },
 };
 
-describe('Immutable', function () {
-  var stringified = {};
-  describe('Stringify', function () {
-    Object.keys(data).forEach(function (key) {
-      it(key, function () {
+describe('Immutable', () => {
+  const stringified = {};
+  describe('Stringify', () => {
+    Object.keys(data).forEach(key => {
+      it(key, () => {
         stringified[key] = stringify(data[key]);
         expect(stringified[key]).toMatchSnapshot();
       });
     });
   });
 
-  describe('Parse', function () {
-    Object.keys(data).forEach(function(key) {
-      it(key, function() {
+  describe('Parse', () => {
+    Object.keys(data).forEach(key => {
+      it(key, () => {
         expect(parse(stringified[key])).toEqual(data[key]);
       });
     });
   });
 
-  describe('Record', function () {
-    var ABRecord = Immutable.Record({ a:1, b:2 });
-    var myRecord = new ABRecord({ b:3 });
+  describe('Record', () => {
+    const ABRecord = Immutable.Record({ a: 1, b: 2 });
+    const myRecord = new ABRecord({ b: 3 });
 
-    var serialize = Serialize(Immutable, [ABRecord]);
-    var stringify = serialize.stringify;
-    var parse = serialize.parse;
-    var stringifiedRecord;
+    const serialize = Serialize(Immutable, [ABRecord]);
+    const stringify = serialize.stringify;
+    const parse = serialize.parse;
+    let stringifiedRecord;
 
-    it('stringify', function() {
+    it('stringify', () => {
       stringifiedRecord = stringify(myRecord);
       expect(stringifiedRecord).toMatchSnapshot();
     });
 
-    it('parse', function() {
+    it('parse', () => {
       expect(parse(stringifiedRecord)).toEqual(myRecord);
     });
   });
 
-  describe('Nested', function () {
-    var ABRecord = Immutable.Record({
+  describe('Nested', () => {
+    const ABRecord = Immutable.Record({
       map: Immutable.OrderedMap({ seq: data.seq, stack: data.stack }),
-      repeat: data.repeat
+      repeat: data.repeat,
     });
-    var nestedData = Immutable.Set(ABRecord(), data.orderedSet, data.range);
+    const nestedData = Immutable.Set(ABRecord(), data.orderedSet, data.range);
 
-    var serialize = Serialize(Immutable, [ABRecord]);
-    var stringify = serialize.stringify;
-    var parse = serialize.parse;
-    var stringifiedNested;
+    const serialize = Serialize(Immutable, [ABRecord]);
+    const stringify = serialize.stringify;
+    const parse = serialize.parse;
+    let stringifiedNested;
 
-    it('stringify', function() {
+    it('stringify', () => {
       stringifiedNested = stringify(nestedData);
       expect(stringifiedNested).toMatchSnapshot();
     });
 
-    it('parse', function() {
+    it('parse', () => {
       expect(parse(stringifiedNested)).toEqual(nestedData);
     });
   });
-  describe('With references', function() {
-    it('serializes and deserializes', function() {
-      var sharedValue = [];
-      var record = Immutable.Record({
-        prop: sharedValue
+  describe('With references', () => {
+    it('serializes and deserializes', () => {
+      const sharedValue = [];
+      const record = Immutable.Record({
+        prop: sharedValue,
       });
 
-      var refs = [record];
+      const refs = [record];
 
-      var obj = Immutable.Map({
+      const obj = Immutable.Map({
         fst: new record(),
-        scnd: new record()
+        scnd: new record(),
       });
 
-      var serialized = stringify(obj, Serialize(Immutable, refs).replacer, null, true);
-      var parsed = JSON.parse(serialized);
+      const serialized = stringify(obj, Serialize(Immutable, refs).replacer, null, true);
+      const parsed = JSON.parse(serialized);
 
-      var fstProp = parsed.data.fst.data.prop;
-      var scndProp = parsed.data.scnd.data.prop;
+      const fstProp = parsed.data.fst.data.prop;
+      const scndProp = parsed.data.scnd.data.prop;
 
       expect(fstProp).toEqual(scndProp);
       expect(Array.isArray(obj.get('fst').get('prop')));
     });
   });
 
-  describe('Custom replacer and reviver functions', function() {
-    var customOneRepresentation = 'one';
+  describe('Custom replacer and reviver functions', () => {
+    const customOneRepresentation = 'one';
 
     function customReplacer(key, value, defaultReplacer) {
       if (value === 1) {
@@ -120,26 +126,28 @@ describe('Immutable', function () {
     }
 
     function customReviver(key, value, defaultReviver) {
-      if (typeof value === 'object'
-      && value.__serializedType__ === 'number'
-      && value.data === customOneRepresentation) {
+      if (
+        typeof value === 'object'
+        && value.__serializedType__ === 'number'
+        && value.data === customOneRepresentation
+      ) {
         return 1;
       }
       return defaultReviver(key, value);
     }
 
-    var serializeCustom = Serialize(Immutable, null, customReplacer, customReviver);
+    const serializeCustom = Serialize(Immutable, null, customReplacer, customReviver);
 
-    Object.keys(data).forEach(function(key) {
-      var stringified = serializeCustom.stringify(data[key]);
-      it(key, function() {
-        var deserialized = serializeCustom.parse(stringified);
+    Object.keys(data).forEach(key => {
+      const stringified = serializeCustom.stringify(data[key]);
+      it(key, () => {
+        const deserialized = serializeCustom.parse(stringified);
         expect(deserialized).toEqual(data[key]);
         if (key === 'map' || key === 'orderedMap') {
-          var deserializedDefault = parse(stringified);
+          const deserializedDefault = parse(stringified);
           expect(deserializedDefault.get('a')).toEqual(customOneRepresentation);
         }
       });
     });
-  })
+  });
 });
