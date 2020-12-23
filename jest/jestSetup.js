8049c8d8aef308ee7b5de9472576f61438c17f82
Greg Price <greg@zulipchat.com>
2020-12-22 22:50:10 -0800

    tests: Use the same `URL` polyfill as we do in the app.
    This avoids confusion from using the polyfill in the app and Node's
    implementation in tests -- the polyfill has some features explicitly
    unimplemented (to reduce its size), and each has its own set of bugs.

--- jest/jestSetup.js
+++ jest/jestSetup.js
@@ -1,6 +1,21 @@
 import * as ReactNative from 'react-native';
+import { polyfillGlobal } from 'react-native/Libraries/Utilities/PolyfillFunctions';
+import { URL, URLSearchParams } from 'react-native-url-polyfill';
 
 import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+// Use the same `URL` polyfill we do in the app.
+//
+// In the app we let `react-native-url-polyfill` handle doing this, by
+// importing `react-native-url-polyfill/auto`.  But in Jest, that produces
+// warnings apparently due to the lazy `require(…)` calls in the getters
+// provided for the polyfill; perhaps from them getting invoked by the Jest
+// framework after the test environment has been torn down.
+//   https://github.com/zulip/zulip-mobile/pull/4350#issuecomment-749247080
+// So we mimic the library's way of doing the polyfill, except we do the
+// imports eagerly so there's no dynamic `require`.
+polyfillGlobal('URL', () => URL);
+polyfillGlobal('URLSearchParams', () => URLSearchParams);
 
 // Mock `react-native` ourselves, following upstream advice [1] [2].
 //
