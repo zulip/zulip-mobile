8f569641f0aa6d2dcd8cb329822fbbe2dc27b223
Chris Bobbe <cbobbe@zulip.com>
2021-01-06 18:04:21 -0500

    deps: Make a type-wrapper for a thing from react-native-safe-area-context.
    As Greg points out [1], `react-native-safe-area-context` isn't doing
    the right thing with their type, and we lose type-checking on the
    wrapped component's props.
    
    So, make a small type-wrapper that works.
    
    [1] https://github.com/zulip/zulip-mobile/pull/4315#discussion_r552375309

---
+++ src/react-native-safe-area-context.js
@@ -0,0 +1,23 @@
+/* @flow strict-local */
+import type { ComponentType, ElementConfig } from 'react';
+import {
+  withSafeAreaInsets as withSafeAreaInsetsInner,
+  type EdgeInsets,
+} from 'react-native-safe-area-context';
+
+import type { BoundedDiff } from './generics';
+
+/* eslint-disable flowtype/generic-spacing */
+
+/**
+ * Exactly like `withSafeAreaInsets` upstream, but properly typed.
+ *
+ * The correction is made here, instead of in the libdef, so that we
+ * can use the handy `BoundedDiff` type, as we do in our type-wrapper
+ * for react-redux's `connect`.
+ */
+export function withSafeAreaInsets<P, C: ComponentType<P>>(
+  WrappedComponent: C,
+): ComponentType<$ReadOnly<BoundedDiff<$Exact<ElementConfig<C>>, {| insets: EdgeInsets |}>>> {
+  return withSafeAreaInsetsInner(WrappedComponent);
+}
