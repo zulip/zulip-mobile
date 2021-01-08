bec9005e5eb0fbfa3517b496642b9c31efb217eb
Chris Bobbe <cbobbe@zulip.com>
2021-01-08 17:32:11 -0500

    ZulipMobile: Set initial route declaratively; remove "initial navigation".
    In a recent commit, we ensured that none of React Navigation's
    components get mounted until the store has rehydrated. The data from
    rehydration is just what we need to tell React Navigation what the
    "initial route" is. With that data, we can remove the awkward
    `InitialNavigationDispatcher` logic, which would be difficult to
    coordinate with the `NavigationService`'s initialization schedule in
    React Navigation 5 [1], which we hope to upgrade to soon.
    
    The mechanics of initializing the root navigator will be simpler
    with React Navigation v5's component-based API. The initial route
    name and params will just be passed as props to the appropriate
    components; there isn't the kind of static configuration necessary
    in React Navigation 4 via `createStackNavigator`.
    
    [1] https://reactnavigation.org/docs/navigating-without-navigation-prop/#handling-initialization

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -12,7 +12,7 @@ import CompatibilityChecker from './boot/CompatibilityChecker';
 import AppEventHandlers from './boot/AppEventHandlers';
 import AppDataFetcher from './boot/AppDataFetcher';
 import BackNavigationHandler from './nav/BackNavigationHandler';
-import { ZulipAppContainer, InitialNavigationDispatcher } from './nav/ZulipAppContainer';
+import ZulipAppContainer from './nav/ZulipAppContainer';
 import { initializeSentry } from './sentry';
 import LoadingScreen from './start/LoadingScreen';
 
@@ -36,11 +36,9 @@ export default (): React$Node => (
             <AppDataFetcher>
               <TranslationProvider>
                 <ThemeProvider>
-                  <InitialNavigationDispatcher>
-                    <BackNavigationHandler>
-                      <ZulipAppContainer />
-                    </BackNavigationHandler>
-                  </InitialNavigationDispatcher>
+                  <BackNavigationHandler>
+                    <ZulipAppContainer />
+                  </BackNavigationHandler>
                 </ThemeProvider>
               </TranslationProvider>
             </AppDataFetcher>
--- src/nav/ZulipAppContainer.js
+++ src/nav/ZulipAppContainer.js
@@ -1,9 +1,6 @@
 /* @flow strict-local */
-import type { Node as React$Node } from 'react';
 import React, { PureComponent } from 'react';
 import {
-  StackActions,
-  NavigationActions,
   createAppContainer,
   type NavigationState,
   type NavigationContainerProps,
@@ -24,70 +21,21 @@ type SelectorProps = $ReadOnly<{|
 |}>;
 
 type Props = $ReadOnly<{|
-  children: React$Node,
-
   dispatch: Dispatch,
   ...SelectorProps,
 |}>;
 
-class InitialNavigationDispatcherInner extends PureComponent<Props> {
-  componentDidMount() {
-    // `NavigationService` will be ready by the time this is run: a
-    // `ref` is set in the `ref`fed component's
-    // `componentDidMount` [1], and a parent's `componentDidMount`
-    // is run after a child's `componentDidMount` [2].
-    //
-    // [1] https://reactjs.org/docs/refs-and-the-dom.html#adding-a-ref-to-a-dom-element
-    // [2] https://reactnavigation.org/docs/navigating-without-navigation-prop/#handling-initialization
-    this.doInitialNavigation();
-  }
-
-  /**
-   * Data has been loaded, so open the app to the right screen.
-   *
-   * Not to be called before the REHYDRATE action or before
-   * `NavigationService` is ready, and not to be called more than
-   * once.
-   */
-  doInitialNavigation = () => {
-    const { hasAuth, accounts, haveServerData } = this.props;
-
-    const { initialRouteName, initialRouteParams } = getInitialRouteInfo({
-      accounts,
-      hasAuth,
-      haveServerData,
-    });
-
-    if (initialRouteName === 'loading') {
-      // We're already on the loading screen -- see `initialRouteName`
-      // in `AppNavigator`.
-      return;
-    }
-
-    NavigationService.dispatch(
-      StackActions.reset({
-        index: 0,
-        actions: [
-          NavigationActions.navigate({
-            routeName: initialRouteName,
-            params: initialRouteParams,
-          }),
-        ],
-      }),
-    );
-  };
-
-  render() {
-    return this.props.children;
-  }
-}
-
-export const InitialNavigationDispatcher = connect(state => ({
-  hasAuth: getHasAuth(state),
-  accounts: getAccounts(state),
-  haveServerData: getHaveServerData(state),
-}))(InitialNavigationDispatcherInner);
-export class ZulipAppContainer extends PureComponent<{||}> {
+/**
+ * Wrapper for React Nav's component given by `createAppContainer`.
+ *
+ * Must be constructed after the store has been rehydrated.
+ *
+ * - Set `NavigationService`.
+ *
+ * - Call `createAppContainer` with the appropriate `initialRouteName`
+ *   and `initialRouteParams` which we get from data in Redux.
+ */
+class ZulipAppContainer extends PureComponent<Props> {
   // (odd spacing choices)
   // eslint-disable-next-line
   AppContainer: NavigationContainer<
@@ -96,10 +44,11 @@ export class ZulipAppContainer extends PureComponent<{||}> {
     NavigationContainerProps<{ ... }, NavigationState>,
   >;
 
-  constructor(props: {||}) {
+  constructor(props: Props) {
     super(props);
+    const { hasAuth, accounts, haveServerData } = this.props;
     this.AppContainer = createAppContainer<NavigationState, { ... }>(
-      createAppNavigator({ initialRouteName: 'loading' }),
+      createAppNavigator(getInitialRouteInfo({ hasAuth, accounts, haveServerData })),
     );
   }
 
@@ -108,3 +57,9 @@ export class ZulipAppContainer extends PureComponent<{||}> {
     return <AppContainer ref={NavigationService.appContainerRef} />;
   }
 }
+
+export default connect(state => ({
+  hasAuth: getHasAuth(state),
+  accounts: getAccounts(state),
+  haveServerData: getHaveServerData(state),
+}))(ZulipAppContainer);
