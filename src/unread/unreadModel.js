1c2225babb55fa728e96143b679ad70cc591e7e5
Greg Price <greg@zulipchat.com>
2021-01-11 20:27:10 -0800

    unread: Add new getUnreadStreams selector.
    This provides a data structure that's just like the one we're going
    to switch to maintaining as the state internally.  We'll migrate
    consumers to use this one, and then we can change out the state and
    reducer underneath it.
    
    As we migrate consumers to this selector, it may initially cause a
    certain performance regression: when the "unread" data structure gets
    updated, we'll promptly be making this (reformatted) copy of it, as
    the selectors that use it go to look for the new data.  But that extra
    work will then get eliminated when we switch to this being the data
    structure that we maintain in the first place.

--- src/unread/unreadModel.js
+++ src/unread/unreadModel.js
@@ -1,4 +1,6 @@
 /* @flow strict-local */
+import Immutable from 'immutable';
+import { createSelector } from 'reselect';
 
 import type { Action } from '../actionTypes';
 import type {
@@ -9,6 +11,7 @@ import type {
   UnreadStreamsState,
 } from './unreadModelTypes';
 import type { GlobalState } from '../reduxTypes';
+import type { Selector } from '../types';
 import unreadStreamsReducer from './unreadStreamsReducer';
 import unreadPmsReducer from './unreadPmsReducer';
 import unreadHuddlesReducer from './unreadHuddlesReducer';
@@ -16,6 +19,26 @@ import unreadMentionsReducer from './unreadMentionsReducer';
 
 export const getUnreadStreamsLegacy = (state: GlobalState): UnreadStreamsState =>
   state.unread.streams;
+
+export const getUnreadStreams: Selector<
+  Immutable.Map<number, Immutable.Map<string, Immutable.List<number>>>,
+> = createSelector(
+  getUnreadStreamsLegacy,
+  data => {
+    // prettier-ignore
+    // This is an example of the style-guide rule "Always provide a type
+    // when writing an empty `Immutable` value".  Without the explicit type,
+    // `result` gets inferred as `Immutable.Map<number, empty>`, and then
+    // e.g. the `setIn` call could be completely wrong and the type-checker
+    // wouldn't notice.
+    const result: Immutable.Map<number, Immutable.Map<string, Immutable.List<number>>> =
+      Immutable.Map().asMutable();
+    for (const { stream_id, topic, unread_message_ids } of data) {
+      result.setIn([stream_id, topic], Immutable.List(unread_message_ids));
+    }
+    return result.asImmutable();
+  },
+);
 
 export const getUnreadPms = (state: GlobalState): UnreadPmsState => state.unread.pms;
 
