6cbdced3d68f8b6f7f49466969978e1f00b44357
Chris Bobbe <cbobbe@zulip.com>
2021-01-21 19:58:58 -0500

    MainScreenWithTabs [nfc]: Convert to use Hooks (1/2).

--- src/main/MainScreenWithTabs.js
+++ src/main/MainScreenWithTabs.js
@@ -1,12 +1,11 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 
 import type { AppNavigationProp, AppNavigationRouteProp } from '../nav/AppNavigator';
 import type { Dispatch } from '../types';
 import { connect } from '../react-redux';
 import { getHaveServerData } from '../selectors';
-import type { ThemeData } from '../styles';
 import styles, { ThemeContext } from '../styles';
 import { OfflineNotice, ZulipStatusBar } from '../common';
 import MainTabs from './MainTabs';
@@ -19,30 +18,27 @@ type Props = $ReadOnly<{|
   haveServerData: boolean,
 |}>;
 
-class MainScreenWithTabs extends PureComponent<Props> {
-  static contextType = ThemeContext;
-  context: ThemeData;
+function MainScreenWithTabs(props: Props) {
+  const { backgroundColor } = useContext(ThemeContext);
 
-  render() {
-    if (!this.props.haveServerData) {
-      // This can happen if the user has just logged out; this screen
-      // is still visible for the duration of the nav transition, and
-      // it's legitimate for its `render` to get called again.
-      // See our #4275.
-      //
-      // Avoid rendering any of our main UI in this case, to maintain
-      // the guarantee that it can all rely on server data existing.
-      return null;
-    }
-
-    return (
-      <View style={[styles.flexed, { backgroundColor: this.context.backgroundColor }]}>
-        <ZulipStatusBar />
-        <OfflineNotice />
-        <MainTabs />
-      </View>
-    );
+  if (!props.haveServerData) {
+    // This can happen if the user has just logged out; this screen
+    // is still visible for the duration of the nav transition, and
+    // it's legitimate for its `render` to get called again.
+    // See our #4275.
+    //
+    // Avoid rendering any of our main UI in this case, to maintain
+    // the guarantee that it can all rely on server data existing.
+    return null;
   }
+
+  return (
+    <View style={[styles.flexed, { backgroundColor }]}>
+      <ZulipStatusBar />
+      <OfflineNotice />
+      <MainTabs />
+    </View>
+  );
 }
 
 export default connect(state => ({
