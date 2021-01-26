38ef7749884a4e940d84c6c5caf8881bae4d8a4d
Chris Bobbe <cbobbe@zulip.com>
2021-01-26 15:02:00 -0500

    ChatNavBar [nfc]: Convert to use Hooks.

--- src/nav/ChatNavBar.js
+++ src/nav/ChatNavBar.js
@@ -1,12 +1,12 @@
 /* @flow strict-local */
 
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import Color from 'color';
 
-import type { Dispatch, Narrow, EditMessage } from '../types';
+import type { Narrow, EditMessage } from '../types';
 import { LoadingBanner } from '../common';
-import { connect } from '../react-redux';
+import { useSelector } from '../react-redux';
 import { BRAND_COLOR, NAVBAR_SIZE } from '../styles';
 import Title from '../title/Title';
 import NavBarBackButton from './NavBarBackButton';
@@ -14,65 +14,53 @@ import { DEFAULT_TITLE_BACKGROUND_COLOR, getTitleBackgroundColor } from '../titl
 import { foregroundColorFromBackground } from '../utils/color';
 import { ExtraButton, InfoButton } from '../title-buttons/titleButtonFromNarrow';
 
-type SelectorProps = {|
-  backgroundColor: string,
-|};
-
 type Props = $ReadOnly<{|
   narrow: Narrow,
   editMessage: EditMessage | null,
-
-  dispatch: Dispatch,
-  ...SelectorProps,
 |}>;
 
-class ChatNavBar extends PureComponent<Props> {
-  render() {
-    const { backgroundColor, narrow, editMessage } = this.props;
-    const color =
-      backgroundColor === DEFAULT_TITLE_BACKGROUND_COLOR
-        ? BRAND_COLOR
-        : foregroundColorFromBackground(backgroundColor);
-    const spinnerColor =
-      backgroundColor === DEFAULT_TITLE_BACKGROUND_COLOR
-        ? 'default'
-        : foregroundColorFromBackground(backgroundColor);
+export default function ChatNavBar(props: Props) {
+  const { narrow, editMessage } = props;
+  const backgroundColor = useSelector(state => getTitleBackgroundColor(state, narrow));
+  const color =
+    backgroundColor === DEFAULT_TITLE_BACKGROUND_COLOR
+      ? BRAND_COLOR
+      : foregroundColorFromBackground(backgroundColor);
+  const spinnerColor =
+    backgroundColor === DEFAULT_TITLE_BACKGROUND_COLOR
+      ? 'default'
+      : foregroundColorFromBackground(backgroundColor);
 
-    return (
+  return (
+    <View
+      style={{
+        borderColor:
+          backgroundColor === 'transparent'
+            ? 'hsla(0, 0%, 50%, 0.25)'
+            : Color(backgroundColor).darken(0.1),
+        borderBottomWidth: 1,
+      }}
+    >
       <View
-        style={{
-          borderColor:
-            backgroundColor === 'transparent'
-              ? 'hsla(0, 0%, 50%, 0.25)'
-              : Color(backgroundColor).darken(0.1),
-          borderBottomWidth: 1,
-        }}
+        style={[
+          {
+            flexDirection: 'row',
+            height: NAVBAR_SIZE,
+            alignItems: 'center',
+          },
+          { backgroundColor },
+        ]}
       >
-        <View
-          style={[
-            {
-              flexDirection: 'row',
-              height: NAVBAR_SIZE,
-              alignItems: 'center',
-            },
-            { backgroundColor },
-          ]}
-        >
-          <NavBarBackButton color={color} />
-          <Title color={color} narrow={narrow} editMessage={editMessage} />
-          <ExtraButton color={color} narrow={narrow} />
-          <InfoButton color={color} narrow={narrow} />
-        </View>
-        <LoadingBanner
-          spinnerColor={spinnerColor}
-          backgroundColor={backgroundColor}
-          textColor={color}
-        />
+        <NavBarBackButton color={color} />
+        <Title color={color} narrow={narrow} editMessage={editMessage} />
+        <ExtraButton color={color} narrow={narrow} />
+        <InfoButton color={color} narrow={narrow} />
       </View>
-    );
-  }
+      <LoadingBanner
+        spinnerColor={spinnerColor}
+        backgroundColor={backgroundColor}
+        textColor={color}
+      />
+    </View>
+  );
 }
-
-export default connect<SelectorProps, _, _>((state, props) => ({
-  backgroundColor: getTitleBackgroundColor(state, props.narrow),
-}))(ChatNavBar);
