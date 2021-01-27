d1fd30dd3e00ea268c9592225ccd6fb52d140719
Chris Bobbe <cbobbe@zulip.com>
2021-01-26 21:26:03 -0500

    ModalSearchNavBar: Better integrate safe-area handling (2/x).

--- src/nav/ModalSearchNavBar.js
+++ src/nav/ModalSearchNavBar.js
@@ -1,7 +1,6 @@
 /* @flow strict-local */
 import React, { useContext } from 'react';
-import { View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ThemeContext } from '../styles';
 import SearchInput from '../common/SearchInput';
@@ -16,20 +15,20 @@ type Props = $ReadOnly<{|
 export default function ModalSearchNavBar(props: Props) {
   const { autoFocus, searchBarOnChange, canGoBack = true } = props;
   const { backgroundColor } = useContext(ThemeContext);
-  const insets = useSafeAreaInsets();
   return (
-    <View
+    <SafeAreaView
+      mode="padding"
+      edges={['top']}
       style={{
         borderColor: 'hsla(0, 0%, 50%, 0.25)',
         flexDirection: 'row',
         alignItems: 'center',
         borderBottomWidth: 1,
         backgroundColor,
-        paddingTop: insets.top,
       }}
     >
       {canGoBack && <NavBarBackButton />}
       <SearchInput autoFocus={autoFocus} onChangeText={searchBarOnChange} />
-    </View>
+    </SafeAreaView>
   );
 }
