d019f98120534fe7731f97490ee214454404e6ff
Chris Bobbe <cbobbe@zulip.com>
2021-01-26 20:51:23 -0500

    ModalSearchNavBar [nfc]: Convert to use Hooks.

--- src/nav/ModalSearchNavBar.js
+++ src/nav/ModalSearchNavBar.js
@@ -1,8 +1,7 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 
-import type { ThemeData } from '../styles';
 import { ThemeContext, NAVBAR_SIZE } from '../styles';
 import SearchInput from '../common/SearchInput';
 import NavBarBackButton from './NavBarBackButton';
@@ -10,35 +9,25 @@ import NavBarBackButton from './NavBarBackButton';
 type Props = $ReadOnly<{|
   autoFocus: boolean,
   searchBarOnChange: (text: string) => void,
-  canGoBack: boolean,
+  canGoBack?: boolean,
 |}>;
 
-class ModalSearchNavBar extends PureComponent<Props> {
-  static contextType = ThemeContext;
-  context: ThemeData;
-
-  static defaultProps = {
-    canGoBack: true,
-  };
-
-  render() {
-    const { autoFocus, searchBarOnChange, canGoBack } = this.props;
-    return (
-      <View
-        style={{
-          borderColor: 'hsla(0, 0%, 50%, 0.25)',
-          flexDirection: 'row',
-          height: NAVBAR_SIZE,
-          alignItems: 'center',
-          borderBottomWidth: 1,
-          backgroundColor: this.context.backgroundColor,
-        }}
-      >
-        {canGoBack && <NavBarBackButton />}
-        <SearchInput autoFocus={autoFocus} onChangeText={searchBarOnChange} />
-      </View>
-    );
-  }
+export default function ModalSearchNavBar(props: Props) {
+  const { autoFocus, searchBarOnChange, canGoBack = true } = props;
+  const { backgroundColor } = useContext(ThemeContext);
+  return (
+    <View
+      style={{
+        borderColor: 'hsla(0, 0%, 50%, 0.25)',
+        flexDirection: 'row',
+        height: NAVBAR_SIZE,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        backgroundColor,
+      }}
+    >
+      {canGoBack && <NavBarBackButton />}
+      <SearchInput autoFocus={autoFocus} onChangeText={searchBarOnChange} />
+    </View>
+  );
 }
-
-export default ModalSearchNavBar;
