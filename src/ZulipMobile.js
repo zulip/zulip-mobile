39d977cf9111769989a0b297b6630a4285367438
Chris Bobbe <cbobbe@zulip.com>
2021-01-28 14:26:26 -0500

    ZulipMobile: Enable `LayoutAnimation` on Android.
    We'll use this soon to redo the header and footer animations in
    `Lightbox`.
    
    Greg points out [1] that, while a code comment in React Native says
    that activating this 'experimental' code will one day be
    unnecessary, we should be skeptical that that'll actually happen,
    given React Native's record. :)
    
    [1] https://github.com/zulip/zulip-mobile/pull/4442#discussion_r566495689

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -1,5 +1,6 @@
 /* @flow strict-local */
 import React from 'react';
+import { Platform, UIManager } from 'react-native';
 import 'react-native-url-polyfill/auto';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
@@ -20,6 +21,18 @@ initializeSentry();
 
 // $FlowFixMe[prop-missing]
 console.disableYellowBox = true; // eslint-disable-line
+
+// Enable `LayoutAnimation` on Android. Already enabled on iOS.
+// https://reactnative.dev/docs/layoutanimation
+if (Platform.OS === 'android') {
+  // In the future, layout animation may be enabled by default. If
+  // that happens, this method will probably be removed:
+  // https://github.com/facebook/react-native/blob/v0.63.4/ReactAndroid/src/main/java/com/facebook/react/uimanager/UIManagerModule.java#L741-L755.
+  //
+  // In the meantime, we should be on the lookout for any issues with
+  // this feature on Android.
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 export default (): React$Node => (
   <CompatibilityChecker>
