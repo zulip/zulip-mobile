7bb4141c94670953b200ea7836425bb0e33f309d
Gautam Arora <gautamarora6248@gmail.com>
2021-01-30 00:29:07 +0530

    a11y: Add captions to icons on navbar of HomeScreen
    Add accessibilityLabel prop to various icons on navbar of HomeScreen.
    Internationalized the value of various accessibilityLabel
    
    Fixes part of #4394

--- src/nav/IconUnreadMentions.js
+++ src/nav/IconUnreadMentions.js
@@ -3,7 +3,9 @@
 import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 
-import type { Dispatch } from '../types';
+import type { Dispatch, GetText } from '../types';
+import { TranslationContext } from '../boot/TranslationProvider';
+
 import { connect } from '../react-redux';
 import { getUnreadMentionsTotal } from '../selectors';
 import { IconMention } from '../common/Icons';
@@ -16,13 +18,16 @@ type Props = $ReadOnly<{|
 |}>;
 
 class IconUnreadMentions extends PureComponent<Props> {
+  static contextType = TranslationContext;
+  context: GetText;
+
   render() {
     const { unreadMentionsTotal, color } = this.props;
-
+    const _ = this.context;
     return (
       <View>
         <CountOverlay unreadCount={unreadMentionsTotal}>
-          <IconMention size={24} color={color} />
+          <IconMention size={24} color={color} accessibilityLabel={_('Mentions')} />
         </CountOverlay>
       </View>
     );
