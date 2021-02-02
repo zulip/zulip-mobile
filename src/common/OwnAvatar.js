641cb161b85ed09653abe34347a7462f3f8f3893
Chris Bobbe <cbobbe@zulip.com>
2021-02-01 21:15:38 -0500

    OwnAvatar: Use `useSelector` hook.

--- src/common/OwnAvatar.js
+++ src/common/OwnAvatar.js
@@ -1,14 +1,11 @@
 /* @flow strict-local */
 import React from 'react';
 
-import type { User, Dispatch } from '../types';
-import { connect } from '../react-redux';
+import { useSelector } from '../react-redux';
 import UserAvatar from './UserAvatar';
 import { getOwnUser } from '../users/userSelectors';
 
 type Props = $ReadOnly<{|
-  dispatch: Dispatch,
-  user: User,
   size: number,
 |}>;
 
@@ -17,11 +14,8 @@ type Props = $ReadOnly<{|
  *
  * @prop size - Sets width and height in logical pixels.
  */
-function OwnAvatar(props: Props) {
-  const { user, size } = props;
+export default function OwnAvatar(props: Props) {
+  const { size } = props;
+  const user = useSelector(getOwnUser);
   return <UserAvatar avatarUrl={user.avatar_url} size={size} />;
 }
-
-export default connect(state => ({
-  user: getOwnUser(state),
-}))(OwnAvatar);
