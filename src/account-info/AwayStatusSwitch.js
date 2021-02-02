18b2c87712f59ae597cfd35678216a77a057897f
Chris Bobbe <cbobbe@zulip.com>
2021-02-01 21:12:38 -0500

    AwayStatusSwitch: Use `useSelector` hook.

--- src/account-info/AwayStatusSwitch.js
+++ src/account-info/AwayStatusSwitch.js
@@ -1,37 +1,29 @@
 /* @flow strict-local */
 import React from 'react';
 
-import type { Dispatch } from '../types';
-import { connect } from '../react-redux';
+import { useSelector, useDispatch } from '../react-redux';
 import { OptionRow } from '../common';
 import { getSelfUserAwayStatus } from '../selectors';
 import { updateUserAwayStatus } from '../user-status/userStatusActions';
 
-type Props = $ReadOnly<{|
-  awayStatus: boolean,
-  dispatch: Dispatch,
-|}>;
+type Props = $ReadOnly<{||}>;
 
 /**
  * This is a stand-alone component that:
  *  * retrieves the current user's `user status` data and presents it
  *  * allows by switching it to control the `away` status
  */
-function AwayStatusSwitch(props: Props) {
-  const { awayStatus } = props;
+export default function AwayStatusSwitch(props: Props) {
+  const awayStatus = useSelector(getSelfUserAwayStatus);
+  const dispatch = useDispatch();
 
   return (
     <OptionRow
       label="Set yourself to away"
       value={awayStatus}
       onValueChange={(away: boolean) => {
-        const { dispatch } = props;
         dispatch(updateUserAwayStatus(away));
       }}
     />
   );
 }
-
-export default connect(state => ({
-  awayStatus: getSelfUserAwayStatus(state),
-}))(AwayStatusSwitch);
