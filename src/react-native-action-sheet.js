932173a895f7a62306b095f4d92b83b771515226
Chris Bobbe <cbobbe@zulip.com>
2021-02-03 11:27:32 -0500

    action sheet: Add type wrapper for `connectActionSheet`.
    `connectActionSheet`'s poor typing is one thing stopping
    `MessageList`'s props from being type checked.
    
    Like we did for react-native-safe-area-context in 8f569641f, use a
    type-wrapper instead of a libdef so that we can use our handy
    `BoundedDiff` type.

---
+++ src/react-native-action-sheet.js
@@ -0,0 +1,29 @@
+/* @flow strict-local */
+import type { ComponentType, ElementConfig } from 'react';
+import { connectActionSheet as connectActionSheetInner } from '@expo/react-native-action-sheet';
+
+import type { BoundedDiff } from './generics';
+
+/* eslint-disable flowtype/generic-spacing */
+
+export type ShowActionSheetWithOptions = (
+  { options: string[], cancelButtonIndex: number },
+  (number) => void,
+) => void;
+
+/**
+ * Exactly like the `connectActionSheet` in
+ *   `react-native-action-sheet` upstream, but more typed.
+ */
+export function connectActionSheet<P, C: ComponentType<P>>(
+  WrappedComponent: C,
+): ComponentType<
+  $ReadOnly<
+    BoundedDiff<
+      $Exact<ElementConfig<C>>,
+      {| showActionSheetWithOptions: ShowActionSheetWithOptions |},
+    >,
+  >,
+> {
+  return connectActionSheetInner(WrappedComponent);
+}
