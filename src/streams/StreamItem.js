7bf6463fb599bd9d09fde591e5ad50ae80f87225
Chris Bobbe <cbobbe@zulip.com>
2021-02-04 13:54:26 -0500

    StreamItem [nfc]: Convert to use Hooks (2/2).

--- src/streams/StreamItem.js
+++ src/streams/StreamItem.js
@@ -1,8 +1,7 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 
-import type { ThemeData } from '../styles';
 import styles, { createStyleSheet, ThemeContext } from '../styles';
 import { RawLabel, Touchable, UnreadCount, ZulipSwitch } from '../common';
 import { foregroundColorFromBackground } from '../utils/color';
@@ -26,15 +25,15 @@ const componentStyles = createStyleSheet({
 type Props = $ReadOnly<{|
   name: string,
   description?: string,
-  isMuted: boolean,
-  isPrivate: boolean,
+  isMuted?: boolean,
+  isPrivate?: boolean,
   color?: string,
   backgroundColor?: string,
 
   unreadCount?: number,
   iconSize: number,
-  showSwitch: boolean,
-  isSwitchedOn: boolean,
+  showSwitch?: boolean,
+  isSwitchedOn?: boolean,
   onPress: (name: string) => void,
   onSwitch?: (name: string, newValue: boolean) => void,
 |}>;
@@ -59,79 +58,69 @@ type Props = $ReadOnly<{|
  * @prop onPress - press handler for the item; receives the stream name
  * @prop onSwitch - if switch exists; receives stream name and new value
  */
-export default class StreamItem extends PureComponent<Props> {
-  static contextType = ThemeContext;
-  context: ThemeData;
+export default function StreamItem(props: Props) {
+  const {
+    name,
+    description,
+    color,
+    backgroundColor,
+    isPrivate = false,
+    isMuted = false,
+    iconSize,
+    showSwitch = false,
+    isSwitchedOn = false,
+    unreadCount,
+    onPress,
+    onSwitch,
+  } = props;
 
-  static defaultProps = {
-    isMuted: false,
-    isPrivate: false,
-    showSwitch: false,
-    isSwitchedOn: false,
-  };
+  const { backgroundColor: themeBackgroundColor, color: themeColor } = useContext(ThemeContext);
 
-  render() {
-    const {
-      name,
-      description,
-      color,
-      backgroundColor,
-      isPrivate,
-      isMuted,
-      iconSize,
-      showSwitch,
-      isSwitchedOn,
-      unreadCount,
-      onPress,
-      onSwitch,
-    } = this.props;
+  const wrapperStyle = [styles.listItem, { backgroundColor }, isMuted && componentStyles.muted];
+  const iconColor =
+    color !== undefined
+      ? color
+      : foregroundColorFromBackground(
+          backgroundColor !== undefined ? backgroundColor : themeBackgroundColor,
+        );
+  const textColor =
+    backgroundColor !== undefined
+      ? (foregroundColorFromBackground(backgroundColor): string)
+      : themeColor;
 
-    const wrapperStyle = [styles.listItem, { backgroundColor }, isMuted && componentStyles.muted];
-    const iconColor =
-      color !== undefined
-        ? color
-        : foregroundColorFromBackground(
-            backgroundColor !== undefined ? backgroundColor : this.context.backgroundColor,
-          );
-    const textColor =
-      backgroundColor !== undefined
-        ? (foregroundColorFromBackground(backgroundColor): string)
-        : this.context.color;
-
-    return (
-      <Touchable onPress={() => onPress(this.props.name)}>
-        <View style={wrapperStyle}>
-          <StreamIcon size={iconSize} color={iconColor} isMuted={isMuted} isPrivate={isPrivate} />
-          <View style={componentStyles.text}>
+  return (
+    <Touchable onPress={() => onPress(name)}>
+      <View style={wrapperStyle}>
+        <StreamIcon size={iconSize} color={iconColor} isMuted={isMuted} isPrivate={isPrivate} />
+        <View style={componentStyles.text}>
+          <RawLabel
+            numberOfLines={1}
+            style={{ color: textColor }}
+            text={name}
+            ellipsizeMode="tail"
+          />
+          {description !== undefined && description !== '' && (
             <RawLabel
               numberOfLines={1}
-              style={{ color: textColor }}
-              text={name}
+              style={componentStyles.description}
+              text={description}
               ellipsizeMode="tail"
-            />
-            {description !== undefined && description !== '' && (
-              <RawLabel
-                numberOfLines={1}
-                style={componentStyles.description}
-                text={description}
-                ellipsizeMode="tail"
-              />
-            )}
-          </View>
-          <UnreadCount color={iconColor} count={unreadCount} />
-          {showSwitch && (
-            <ZulipSwitch
-              value={!!isSwitchedOn}
-              onValueChange={(newValue: boolean) => {
-                if (onSwitch) {
-                  onSwitch(name, newValue);
-                }
-              }}
-              disabled={!isSwitchedOn && isPrivate}
             />
           )}
         </View>
-      </Touchable>
-    );
-  }
+        <UnreadCount color={iconColor} count={unreadCount} />
+        {showSwitch && (
+          <ZulipSwitch
+            value={!!isSwitchedOn}
+            onValueChange={(newValue: boolean) => {
+              if (onSwitch) {
+                onSwitch(name, newValue);
+              }
+            }}
+            disabled={!isSwitchedOn && isPrivate}
+          />
+        )}
+      </View>
+    </Touchable>
+  );
 }
