43a75fefa4a83ee9e684f52bef5a975097e67a0b
Chris Bobbe <cbobbe@zulip.com>
2021-02-22 12:04:00 -0500

    redux: Add `observeStore`.
    On the pattern Dan Abramov provides at
      https://github.com/reduxjs/redux/issues/303#issuecomment-125184409,
    which a Redux doc [1] links to.
    
    We'll use this to listen for changes in the server version, so we
    can update the global Sentry scope and make sure the right tags are
    sent with every Sentry event.
    
    [1] https://redux.js.org/api/store#subscribelistener

---
+++ src/redux.js
@@ -0,0 +1,36 @@
+/* @flow strict-local */
+import type { Store } from 'redux';
+
+import type { Selector, GlobalState, Action } from './reduxTypes';
+
+/**
+ * Call a function whenever a selected piece of state changes.
+ *
+ * When the return value of `select` isn't equal (===) to the previous
+ * value, `onChange` gets called, with the old and new value.
+ * `onChange` is called once, immediately and unconditionally, at the
+ * beginning.
+ *
+ * Returns a teardown function.
+ */
+export function observeStore<T>(
+  store: Store<GlobalState, Action>,
+  select: Selector<T>,
+  onChange: (state: T) => void,
+) {
+  // Start as a nonce object, so the initial `prevState !== state`
+  // must return true.
+  let prevState = {};
+
+  function handleChange() {
+    const state = select(store.getState());
+    if (prevState !== state) {
+      onChange(state);
+      prevState = state;
+    }
+  }
+
+  const unsubscribe = store.subscribe(handleChange);
+  handleChange();
+  return unsubscribe;
+}
