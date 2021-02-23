a1e6ca6bc9a0b2cb0539254cceb9de6f8f43cb93
Chris Bobbe <cbobbe@zulip.com>
2021-02-22 20:38:34 -0500

    AccountList [nfc]: Convert to use Hooks.

--- src/account/AccountList.js
+++ src/account/AccountList.js
@@ -1,5 +1,5 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { View, FlatList } from 'react-native';
 
 import type { AccountStatus } from './accountsSelectors';
@@ -12,28 +12,26 @@ type Props = $ReadOnly<{|
   onAccountRemove: number => void,
 |}>;
 
-export default class AccountList extends PureComponent<Props> {
-  render() {
-    const { accounts, onAccountSelect, onAccountRemove } = this.props;
+export default function AccountList(props: Props) {
+  const { accounts, onAccountSelect, onAccountRemove } = props;
 
-    return (
-      <View>
-        <FlatList
-          data={accounts}
-          keyExtractor={item => `${item.email}${item.realm.toString()}`}
-          ItemSeparatorComponent={() => <ViewPlaceholder height={8} />}
-          renderItem={({ item, index }) => (
-            <AccountItem
-              index={index}
-              showDoneIcon={index === 0 && item.isLoggedIn}
-              email={item.email}
-              realm={item.realm}
-              onSelect={onAccountSelect}
-              onRemove={onAccountRemove}
-            />
-          )}
-        />
-      </View>
-    );
-  }
+  return (
+    <View>
+      <FlatList
+        data={accounts}
+        keyExtractor={item => `${item.email}${item.realm.toString()}`}
+        ItemSeparatorComponent={() => <ViewPlaceholder height={8} />}
+        renderItem={({ item, index }) => (
+          <AccountItem
+            index={index}
+            showDoneIcon={index === 0 && item.isLoggedIn}
+            email={item.email}
+            realm={item.realm}
+            onSelect={onAccountSelect}
+            onRemove={onAccountRemove}
+          />
+        )}
+      />
+    </View>
+  );
 }
