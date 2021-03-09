47ca6e753db3ad29ed0e20b1c8a9aabbf9d60fb2
im-adithya <imadithyavardhan@gmail.com>
2021-03-09 13:46:47 +0530

    isInternalLink [nfc]: Use URL object instead of string.
    Changes isInternalLink to accept a URL object instead
    of a string and moves the work of new URL(url, realm)
    out of the same as a first step to change
    isInternalLinks.js to use URL object.
    
    Related to #4292

--- src/utils/internalLinks.js
+++ src/utils/internalLinks.js
@@ -34,15 +34,11 @@ const getPathsFromUrl = (url: string = '', realm: URL) => {
  * millisecond.  Avoid using in a context where it might be called more than
  * 10 or 100 times per user action.
  */
-export const isInternalLink = (url: string, realm: URL): boolean => {
-  const resolved = new URL(url, realm);
-  return (
-    resolved.origin === realm.origin
-    && resolved.pathname === '/'
-    && resolved.search === ''
-    && /^#narrow\//i.test(resolved.hash)
-  );
-};
+export const isInternalLink = (url: URL, realm: URL): boolean =>
+  url.origin === realm.origin
+  && url.pathname === '/'
+  && url.search === ''
+  && /^#narrow\//i.test(url.hash);
 
 /**
  * PRIVATE -- exported only for tests.
@@ -53,8 +49,10 @@ export const isInternalLink = (url: string, realm: URL): boolean => {
  */
 // TODO: Work out what this does, write a jsdoc for its interface, and
 // reimplement using URL object (not just for the realm)
-export const isMessageLink = (url: string, realm: URL): boolean =>
-  isInternalLink(url, realm) && url.includes('near');
+export const isMessageLink = (url: string, realm: URL): boolean => {
+  const resolved = new URL(url, realm);
+  return isInternalLink(resolved, realm) && url.includes('near');
+};
 
 type LinkType = 'external' | 'home' | 'pm' | 'topic' | 'stream' | 'special';
 
@@ -68,7 +66,8 @@ type LinkType = 'external' | 'home' | 'pm' | 'topic' | 'stream' | 'special';
 // TODO: Work out what this does, write a jsdoc for its interface, and
 // reimplement using URL object (not just for the realm)
 export const getLinkType = (url: string, realm: URL): LinkType => {
-  if (!isInternalLink(url, realm)) {
+  const resolved = new URL(url, realm);
+  if (!isInternalLink(resolved, realm)) {
     return 'external';
   }
 
