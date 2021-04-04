82dab79a18df21892905bc87bc29e097e58010e5
im-adithya <imadithyavardhan@gmail.com>
2021-04-04 12:45:53 +0530

    styles: Update BRAND_COLOR.
    This updates the BRAND_COLOR to use hsl(222, 99%, 69%) which is the
    midpoint of the logo's gradient as per a report by Anders Kaseorg.
    
    This commit also updates the colors where the BRAND_COLOR value is
    used directly as specified in src/styles/constants.js.

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -5,7 +5,10 @@ export const CONTROL_SIZE = 44;
 export const NAVBAR_SIZE = 58;
 
 /* This color is also used directly in several places in our WebView's CSS. */
-export const BRAND_COLOR = 'hsl(170, 48%, 54%)';
+/* The BRAND_COLOR is set based on a report by Anders Kaseorg which says that
+   rgb(100, 146, 253.5) is the sRGB midpoint of our logo's gradient. Refer to:
+   https://github.com/zulip/zulip-mobile/pull/4467#issue-567898180 */
+export const BRAND_COLOR = 'hsl(222, 99%, 69%)';
 export const BORDER_COLOR = BRAND_COLOR;
 export const HIGHLIGHT_COLOR = Color(BRAND_COLOR)
   .fade(0.5)
