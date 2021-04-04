d0e63b7e929aca687961da95f4c17f524817028a
im-adithya <imadithyavardhan@gmail.com>
2021-04-04 11:32:46 +0530

    styles: Update HIGHLIGHT_COLOR.
    This replaces the HSL values chosen for the HIGHLIGHT_COLOR
    with the same HSL values as BRAND_COLOR but with different
    alpha value as the previous values are arbitrarily chosen and
    are not documented.

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -6,6 +6,6 @@ export const NAVBAR_SIZE = 58;
 /* This color is also used directly in several places in our WebView's CSS. */
 export const BRAND_COLOR = 'hsl(170, 48%, 54%)';
 export const BORDER_COLOR = BRAND_COLOR;
-export const HIGHLIGHT_COLOR = 'hsla(187, 35%, 51%, 0.5)';
+export const HIGHLIGHT_COLOR = 'hsla(170, 48%, 54%, 0.5)';
 export const HALF_COLOR = 'hsla(0, 0%, 50%, 0.5)';
 export const QUARTER_COLOR = 'hsla(0, 0%, 50%, 0.25)';
