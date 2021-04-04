5bc0af1a6d5274405c182d3238ff000763fd7d70
im-adithya <imadithyavardhan@gmail.com>
2021-04-04 11:54:29 +0530

    styles [nfc]: Connect HIGHLIGHT_COLOR with BRAND_COLOR explicitly.
    Now that the HIGHLIGHT_COLOR uses the same HSL values as BRAND_COLOR,
    we are using the `color` package to connect the HIGHLIGHT_COLOR with the
    BRAND_COLOR so that we don't have to change HIGHLIGHT_COLOR each time
    BRAND_COLOR changes.

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -1,4 +1,5 @@
 /* @flow strict-local */
+import Color from 'color';
 
 export const CONTROL_SIZE = 44;
 export const NAVBAR_SIZE = 58;
@@ -6,6 +7,8 @@ export const NAVBAR_SIZE = 58;
 /* This color is also used directly in several places in our WebView's CSS. */
 export const BRAND_COLOR = 'hsl(170, 48%, 54%)';
 export const BORDER_COLOR = BRAND_COLOR;
-export const HIGHLIGHT_COLOR = 'hsla(170, 48%, 54%, 0.5)';
+export const HIGHLIGHT_COLOR = Color(BRAND_COLOR)
+  .fade(0.5)
+  .toString();
 export const HALF_COLOR = 'hsla(0, 0%, 50%, 0.5)';
 export const QUARTER_COLOR = 'hsla(0, 0%, 50%, 0.25)';
