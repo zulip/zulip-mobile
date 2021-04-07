08db713e512d6e26f7c9fc6e578073d97d6f0313
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-04-07 02:02:18 +0200

    Setup VideoPlayer
    Following the same implementation of Lightbox and using LightboxHeader.
    
    The `Video` component accept several props, most important is the
    `source`, which take the video uri and the headers -if exists-.
    
    Wrapping the `Video` with `Touchable` to handle show/hide header,and
    because unlike `PhotoView` the `Video` doesn't have `onTap` props.
    
    Handle video error by show a message on the screen

--- src/video/VideoPlayer.js
+++ src/video/VideoPlayer.js
@@ -1,8 +1,44 @@
 /* @flow strict-local */
 
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { View, Dimensions, LayoutAnimation, Text } from 'react-native';
 import Video from 'react-native-video';
 import type { Message } from '../types';
+import * as NavigationService from '../nav/NavigationService';
+import { useSelector } from '../react-redux';
+import LightboxHeader from '../lightbox/LightboxHeader';
+import { getAuth, getSession } from '../selectors';
+import { getResource } from '../utils/url';
+import { navigateBack } from '../actions';
+import { createStyleSheet } from '../styles';
+import { Touchable } from '../common';
+
+const styles = createStyleSheet({
+  video: {
+    flex: 1,
+  },
+  header: {},
+  overlay: {
+    backgroundColor: 'black',
+    opacity: 0.8,
+    position: 'absolute',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  textError: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  textErrorContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 type Props = $ReadOnly<{|
   src: string,
@@ -10,9 +46,73 @@ type Props = $ReadOnly<{|
 |}>;
 
 export default function VideoPlayer(props: Props) {
-  // eslint-disable-next-line no-unused-vars
+  const [headerFooterVisible, setHeaderFooterVisible] = useState<boolean>(true);
+  const [videoError, setVideoError] = useState<boolean>(false);
+  const auth = useSelector(getAuth);
   const { src, message } = props;
+
+  const resource = getResource(src, auth);
+  const videoUri = resource.uri;
+  const videoHeaders = resource.headers;
+
+  // Since we're using `Dimensions.get` (below), we'll want a rerender
+  // when the orientation changes. No need to store the value.
+  useSelector(state => getSession(state).orientation);
+
+  const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
+  const onErrorHandle = err => {
+    setVideoError(true);
+  };
+
+  const handleVideoPress = useCallback(() => {
+    LayoutAnimation.configureNext({
+      ...LayoutAnimation.Presets.easeInEaseOut,
+      duration: 100, // from 300
+    });
+    setHeaderFooterVisible(m => !m);
+  }, [setHeaderFooterVisible]);
+
   return (
-    <Video />
+    <View style={styles.container}>
+      {videoError ? (
+        <View style={styles.textErrorContainer}>
+          <Text style={styles.textError}>Something wrong with this file!</Text>
+        </View>
+      ) : (
+        <Touchable onPress={handleVideoPress} style={styles.video}>
+          <Video
+            source={{
+              uri: videoUri,
+              headers: { Authorization: videoHeaders ? videoHeaders.Authorization : null },
+            }}
+            rate={1}
+            volume={1}
+            controls
+            resizeMode="contain"
+            onError={onErrorHandle}
+            style={styles.video}
+          />
+        </Touchable>
+      )}
+
+      <View
+        style={[
+          styles.overlay,
+          styles.header,
+          { width: windowWidth },
+          headerFooterVisible ? { top: 0 } : { bottom: windowHeight },
+        ]}
+      >
+        <LightboxHeader
+          onPressBack={() => {
+            NavigationService.dispatch(navigateBack());
+          }}
+          timestamp={message.timestamp}
+          avatarUrl={message.avatar_url}
+          senderName={message.sender_full_name}
+          senderEmail={message.sender_email}
+        />
+      </View>
+    </View>
   );
 }
