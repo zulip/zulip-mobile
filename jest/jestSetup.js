5fd977ae983017dfe7064f83179eadf334e4a0b8
Chris Bobbe <cbobbe@zulip.com>
2021-04-26 14:22:42 -0400

    jestSetup [nfc]: Remove blank line between third-party imports.

--- jest/jestSetup.js
+++ jest/jestSetup.js
@@ -3,7 +3,6 @@
 import * as ReactNative from 'react-native';
 import { polyfillGlobal } from 'react-native/Libraries/Utilities/PolyfillFunctions';
 import { URL, URLSearchParams } from 'react-native-url-polyfill';
-
 import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
 
 // Use the same `URL` polyfill we do in the app.
