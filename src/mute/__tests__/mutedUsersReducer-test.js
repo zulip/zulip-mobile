c8ed0b08fa3eb593876f263e4eff590f4698cb7a
Greg Price <greg@zulipchat.com>
2021-04-26 20:28:18 -0700

    muted users tests: Use eg.otherUser and eg.thirdUser.
    This helps make it explicit that there's nothing special about these
    particular user IDs, other than the fact that one appears in certain
    places in these tests and the other appears in certain other places.
    
    It also means that if in the future we were to have any code that
    expects these user IDs to be the IDs of users that we actually know
    about -- which is an invariant that it'd be quite reasonable for code
    to rely on -- these tests will be readily compatible with that because
    there are already full User objects for these.

--- src/mute/__tests__/mutedUsersReducer-test.js
+++ src/mute/__tests__/mutedUsersReducer-test.js
@@ -3,11 +3,10 @@ import Immutable from 'immutable';
 import deepFreeze from 'deep-freeze';
 
 import mutedUsersReducer from '../mutedUsersReducer';
-import { makeUserId } from '../../api/idTypes';
 import * as eg from '../../__tests__/lib/exampleData';
 
 describe('mutedUsersReducer', () => {
-  const baseState = Immutable.Map([[makeUserId(42), 1618822632]]);
+  const baseState = Immutable.Map([[eg.otherUser.user_id, 1618822632]]);
 
   describe('REALM_INIT', () => {
     test('when `muted_users` data is provided init state with it', () => {
@@ -15,11 +14,11 @@ describe('mutedUsersReducer', () => {
         ...eg.action.realm_init,
         data: {
           ...eg.action.realm_init.data,
-          muted_users: [{ id: makeUserId(42), timestamp: 1618822632 }],
+          muted_users: [{ id: eg.otherUser.user_id, timestamp: 1618822632 }],
         },
       });
       expect(mutedUsersReducer(eg.baseReduxState.mutedUsers, action)).toEqual(
-        Immutable.Map([[makeUserId(42), 1618822632]]),
+        Immutable.Map([[eg.otherUser.user_id, 1618822632]]),
       );
     });
 
@@ -39,15 +38,15 @@ describe('mutedUsersReducer', () => {
       const action = deepFreeze({
         ...eg.eventMutedUsersActionBase,
         muted_users: [
-          { id: makeUserId(42), timestamp: 1618822632 },
-          { id: makeUserId(1234), timestamp: 1618822635 },
+          { id: eg.otherUser.user_id, timestamp: 1618822632 },
+          { id: eg.thirdUser.user_id, timestamp: 1618822635 },
         ],
       });
 
       // prettier-ignore
       expect(mutedUsersReducer(baseState, action)).toEqual(Immutable.Map([
-        [makeUserId(42), 1618822632],
-        [makeUserId(1234), 1618822635],
+        [eg.otherUser.user_id, 1618822632],
+        [eg.thirdUser.user_id, 1618822635],
       ]));
     });
   });
