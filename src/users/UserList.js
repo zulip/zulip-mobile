b351de24e9b98fbacb411716e99da4b5200fbd50
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-04-28 19:59:17 +0800

    UserList [nfc]: Convert to functional component.

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -1,5 +1,5 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { SectionList } from 'react-native';
 
 import type { PresenceState, UserOrBot } from '../types';
@@ -17,53 +17,47 @@ const styles = createStyleSheet({
 type Props = $ReadOnly<{|
   filter: string,
   users: $ReadOnlyArray<UserOrBot>,
-  selected: $ReadOnlyArray<UserOrBot>,
+  selected?: $ReadOnlyArray<UserOrBot>,
   presences: PresenceState,
   onPress: (user: UserOrBot) => void,
 |}>;
 
-export default class UserList extends PureComponent<Props> {
-  static defaultProps = {
-    selected: [],
-  };
+export default function UserList(props: Props) {
+  const { filter, users, presences, onPress, selected = [] } = props;
+  const shownUsers = sortUserList(filterUserList(users, filter), presences);
 
-  render() {
-    const { filter, users, presences, onPress, selected } = this.props;
-    const shownUsers = sortUserList(filterUserList(users, filter), presences);
-
-    if (shownUsers.length === 0) {
-      return <SearchEmptyState text="No users found" />;
-    }
-
-    const groupedUsers = groupUsersByStatus(shownUsers, presences);
-    const sections = Object.keys(groupedUsers).map(key => ({
-      key: `${key.charAt(0).toUpperCase()}${key.slice(1)}`,
-      data: groupedUsers[key].map(u => u.user_id),
-    }));
-
-    return (
-      <SectionList
-        style={styles.list}
-        stickySectionHeadersEnabled
-        keyboardShouldPersistTaps="always"
-        initialNumToRender={20}
-        sections={sections}
-        keyExtractor={item => item}
-        renderItem={({ item }) => (
-          <UserItem
-            key={item}
-            userId={item}
-            onPress={onPress}
-            isSelected={!!selected.find(user => user.user_id === item)}
-          />
-        )}
-        renderSectionHeader={({ section }) =>
-          section.data.length === 0 ? null : (
-            // $FlowFixMe[incompatible-type]
-            <SectionHeader text={section.key} />
-          )
-        }
-      />
-    );
+  if (shownUsers.length === 0) {
+    return <SearchEmptyState text="No users found" />;
   }
+
+  const groupedUsers = groupUsersByStatus(shownUsers, presences);
+  const sections = Object.keys(groupedUsers).map(key => ({
+    key: `${key.charAt(0).toUpperCase()}${key.slice(1)}`,
+    data: groupedUsers[key].map(u => u.user_id),
+  }));
+
+  return (
+    <SectionList
+      style={styles.list}
+      stickySectionHeadersEnabled
+      keyboardShouldPersistTaps="always"
+      initialNumToRender={20}
+      sections={sections}
+      keyExtractor={item => item}
+      renderItem={({ item }) => (
+        <UserItem
+          key={item}
+          userId={item}
+          onPress={onPress}
+          isSelected={!!selected.find(user => user.user_id === item)}
+        />
+      )}
+      renderSectionHeader={({ section }) =>
+        section.data.length === 0 ? null : (
+          // $FlowFixMe[incompatible-type]
+          <SectionHeader text={section.key} />
+        )
+      }
+    />
+  );
 }
