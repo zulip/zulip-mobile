70bed8dd1e5a840050b6d0765a622a01331ab8ab
Greg Price <greg@zulipchat.com>
2021-04-28 14:58:35 -0700

    styles [nfc]: Tweak comment on BRAND_COLOR.
    Saying what this value is meant to correspond to is important, but the
    citation to back up the fact that that's the midpoint can be left to a
    commit message.  (The link in the deleted comment works fine, or one
    could refer directly to commit 4734533c9.)
    
    A small formatting thing is that when there's a block comment,
    anything above the `/*` doesn't look like it's attached to that code.
    So a single block comment, or a block of `//` comments, is preferable.

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -4,14 +4,15 @@ import Color from 'color';
 export const CONTROL_SIZE = 44;
 export const NAVBAR_SIZE = 58;
 
-/* This color is also used directly in several places in our WebView's CSS. */
-/* The BRAND_COLOR is set based on a report by Anders Kaseorg which says that
-   rgb(100, 146, 253.5) is the sRGB midpoint of our logo's gradient. Refer to:
-   https://github.com/zulip/zulip-mobile/pull/4467#issue-567898180 */
+// The value `hsl(222, 99%, 69%)` is chosen to match `rgb(100, 146, 253.5)`,
+// which is the sRGB midpoint of the Zulip logo's gradient.
+//
+// Note this color is also used directly in several places in our WebView's CSS.
 export const BRAND_COLOR = 'hsl(222, 99%, 69%)';
 export const BORDER_COLOR = BRAND_COLOR;
 export const HIGHLIGHT_COLOR = Color(BRAND_COLOR)
   .fade(0.5)
   .toString();
+
 export const HALF_COLOR = 'hsla(0, 0%, 50%, 0.5)';
 export const QUARTER_COLOR = 'hsla(0, 0%, 50%, 0.25)';
