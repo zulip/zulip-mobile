f2c7a793d63b62dc74514f3cc183e6f2cc660348
Gautam Arora <gautamarora6248@gmail.com>
2021-04-30 01:28:11 +0530

    ThemeProvider [nfc]: Convert to functional component.
    Convert to functional component. Make use of 'useSelector` hook
    instead of 'connect'. Removed Dispatch and the theme props since
    they are not required now.

--- src/boot/ThemeProvider.js
+++ src/boot/ThemeProvider.js
@@ -1,36 +1,27 @@
 /* @flow strict-local */
 
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import type { Node as React$Node } from 'react';
-import type { ThemeName, Dispatch } from '../types';
-import { connect } from '../react-redux';
+import { useSelector } from '../react-redux';
 import { getSettings } from '../directSelectors';
 import { themeData, ThemeContext } from '../styles/theme';
 import { ZulipStatusBar } from '../common';
 
 type Props = $ReadOnly<{|
-  dispatch: Dispatch,
-  theme: ThemeName,
   children: React$Node,
 |}>;
 
-class ThemeProvider extends PureComponent<Props> {
-  static defaultProps = {
-    theme: 'default',
-  };
+function ThemeProvider(props: Props) {
+  const theme = useSelector(state => getSettings(state).theme);
+  const { children } = props;
 
-  render() {
-    const { children, theme } = this.props;
-    return (
-      <ThemeContext.Provider value={themeData[theme]}>
-        <ZulipStatusBar />
-        {children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={themeData[theme]}>
+      <ZulipStatusBar />
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-export default connect(state => ({
-  theme: getSettings(state).theme,
-}))(ThemeProvider);
+export default ThemeProvider;
