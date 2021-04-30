e91d9a8b4422b2fa8e5be9363dfe53f78aa62418
Chris Bobbe <cbobbe@zulip.com>
2021-04-30 16:14:56 -0400

    ZulipAsyncStorage tests: Add tests for `getItem`.

--- src/boot/__tests__/ZulipAsyncStorage-test.js
+++ src/boot/__tests__/ZulipAsyncStorage-test.js
@@ -2,6 +2,7 @@
 import { Platform, NativeModules } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import ZulipAsyncStorage from '../ZulipAsyncStorage';
+import * as logging from '../../utils/logging';
 
 describe('setItem', () => {
   const key = 'foo!';
@@ -77,4 +78,109 @@ describe('setItem', () => {
       expect(callback).toHaveBeenCalledWith(expect.any(Error));
     });
   });
+});
+
+describe('getItem', () => {
+  const key = 'foo!';
+  const value = '123!';
+  const callback = jest.fn();
+  beforeEach(() => callback.mockClear());
+
+  // For checking that AsyncStorage.getItem is called in ways we expect.
+  const asyncStorageGetItemSpy = jest.spyOn(AsyncStorage, 'getItem');
+  beforeEach(() => asyncStorageGetItemSpy.mockClear());
+
+  beforeAll(async () => {
+    // `AsyncStorage` mocks storage by writing to a variable instead
+    // of to the disk. Put something there for our
+    // `ZulipAsyncStorage.getItem` to retrieve.
+    await AsyncStorage.setItem(
+      key,
+      Platform.OS === 'ios' ? value : await NativeModules.TextCompressionModule.compress(value),
+    );
+
+    // suppress `logging.error` output
+    // $FlowFixMe - teach Flow about mocks
+    logging.error.mockReturnValue();
+  });
+
+  const run = async () => ZulipAsyncStorage.getItem(key, callback);
+
+  describe('success', () => {
+    // AsyncStorage provides its own mock for `.getItem`, which gives
+    // success every time. So, no need to mock that behavior
+    // ourselves.
+
+    test('calls AsyncStorage.getItem as we expect it to', async () => {
+      await run();
+      expect(asyncStorageGetItemSpy).toHaveBeenCalledTimes(1);
+      expect(asyncStorageGetItemSpy).toHaveBeenCalledWith(key);
+    });
+
+    test('resolves correctly', async () => {
+      await expect(run()).resolves.toBe(value);
+    });
+
+    test('callback called correctly', async () => {
+      await run();
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith(null, value);
+    });
+  });
+
+  describe('failure', () => {
+    // AsyncStorage provides its own mock for `.getItem`, but it's
+    // not set up to simulate failure. So, mock that behavior
+    // ourselves.
+    const globalMock = AsyncStorage.getItem;
+    beforeEach(() => {
+      // $FlowFixMe[cannot-write] Make Flow understand about mocking.
+      AsyncStorage.getItem = jest.fn(
+        async (k: string, cb?: ?(e: ?Error, r: string | null) => void): Promise<string | null> => {
+          const error = new Error();
+          if (cb) {
+            cb(error, null);
+          }
+          throw error;
+        },
+      );
+    });
+    afterAll(() => {
+      // $FlowFixMe[cannot-write] Make Flow understand about mocking.
+      AsyncStorage.getItem = globalMock;
+    });
+
+    test('rejects correctly', async () => {
+      await expect(run()).rejects.toThrow(Error);
+    });
+
+    test('callback called correctly', async () => {
+      try {
+        await run();
+      } catch (e) {
+        // "rejects correctly" covers this
+      }
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith(expect.any(Error), null);
+    });
+  });
+
+  if (Platform.OS === 'android') {
+    describe('android', () => {
+      test('panics when value is in unknown compression format', async () => {
+        const unknownHeader = 'z|mock-unknown-format|';
+
+        await AsyncStorage.setItem(
+          `${key}-unknown`,
+          `${unknownHeader}${Buffer.from('123!').toString('hex')}`,
+        );
+
+        await expect(ZulipAsyncStorage.getItem(`${key}-unknown`, callback)).rejects.toThrow(
+          `No decompression module found for format ${unknownHeader}`,
+        );
+        expect(callback).toHaveBeenCalledTimes(1);
+        expect(callback).toHaveBeenCalledWith(expect.any(Error), null);
+      });
+    });
+  }
 });
