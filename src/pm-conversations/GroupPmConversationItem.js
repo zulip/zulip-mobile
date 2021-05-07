3cf8c3c070b15afbbdf4bbe8e50c747c8b1dcdab
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-05-08 03:44:53 +0800

    pm-conversations: Show "Muted User" placeholder in group PMs.

--- src/pm-conversations/GroupPmConversationItem.js
+++ src/pm-conversations/GroupPmConversationItem.js
@@ -1,10 +1,13 @@
 /* @flow strict-local */
-import React from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
+import { useSelector } from '../react-redux';
 
 import type { UserOrBot } from '../types';
 import styles, { createStyleSheet } from '../styles';
 import { GroupAvatar, RawLabel, Touchable, UnreadCount } from '../common';
+import { getMutedUsers } from '../selectors';
+import { TranslationContext } from '../boot/TranslationProvider';
 
 const componentStyles = createStyleSheet({
   text: {
@@ -30,7 +33,11 @@ export default function GroupPmConversationItem<U: $ReadOnlyArray<UserOrBot>>(pr
   };
 
   const { users, unreadCount } = props;
-  const names = users.map(user => user.full_name);
+  const _ = useContext(TranslationContext);
+  const mutedUsers = useSelector(getMutedUsers);
+  const names = users.map(user =>
+    mutedUsers.has(user.user_id) ? _('Muted user') : user.full_name,
+  );
 
   return (
     <Touchable onPress={handlePress}>
