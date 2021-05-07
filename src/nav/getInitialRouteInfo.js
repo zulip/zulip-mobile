52ab45539885a6ee133d85fbc425508cf888b2d8
Chris Bobbe <cbobbe@zulip.com>
2021-05-07 14:12:10 -0400

    getInitialRouteInfo: Give 'account-pick' when just one, logged-out account.
    Instead of 'realm-input'.
    
    The entire `!hasAuth` condition may become impossible, in a world
    where we don't keep around accounts that aren't logged in [1]. So,
    even if it's not totally clear that the new behavior from this
    commit is an improvement, at least we know that it might not be
    permanent. (I do think it's an improvement, for a reason I describe
    in a comment.)
    
    This removes the second of the two cases of #4604; see the comment
    in the removed `.length === 1` case describing the
    `RealmInputScreen` flash as the expected behavior.
    
    [1] https://chat.zulip.org/#narrow/stream/48-mobile/topic/logged-out.20state/near/1174840
    
    Fixes: #4604

--- src/nav/getInitialRouteInfo.js
+++ src/nav/getInitialRouteInfo.js
@@ -13,21 +13,12 @@ export default (args: {|
   // If the active account is not logged in, bring the user as close
   // as we can to AuthScreen, the place where they can log in.
   if (!hasAuth) {
-    if (accounts.length > 1) {
-      // We can't guess which account, of multiple, the user wants
-      // to use. Let them pick one.
+    if (accounts.length > 0) {
+      // Let the user pick the account. If there's just one, it'll be
+      // logged out (since `!hasAuth`); therefore it seems right to
+      // let the user choose between that account and some new
+      // account.
       return { initialRouteName: 'account-pick' };
-    } else if (accounts.length === 1) {
-      // We already know the realm, so give that to the realm
-      // screen. If that screen finds that the realm is valid, it'll
-      // send the user along to AuthScreen for that realm right
-      // away. If this means you're on the AuthScreen when you don't
-      // want to be (i.e., you want to choose a different realm),
-      // you can always go back to RealmInputScreen.
-      return {
-        initialRouteName: 'realm-input',
-        initialRouteParams: { initial: true, realm: accounts[0].realm },
-      };
     } else {
       // Just go to the realm screen and have the user type out the
       // realm.
