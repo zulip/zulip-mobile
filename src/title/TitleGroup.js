1d161a000dd7ea0117d810ab5adbdc84b6b6d1d9
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-06-02 11:51:48 -0700

    TitleGroup: Hide avatars for muted users.

--- src/title/TitleGroup.js
+++ src/title/TitleGroup.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { useSelector } from '../react-redux';
+import { getMutedUsers } from '../selectors';
 import type { UserId } from '../types';
 import * as NavigationService from '../nav/NavigationService';
 import styles, { createStyleSheet } from '../styles';
@@ -21,6 +23,8 @@ const componentStyles = createStyleSheet({
 
 export default function TitleGroup(props: Props) {
   const { userIds } = props;
+  const mutedUsers = useSelector(getMutedUsers);
+
   return (
     <View style={styles.navWrapper}>
       {userIds.map(userId => (
@@ -31,6 +35,7 @@ export default function TitleGroup(props: Props) {
             }}
             size={32}
             userId={userId}
+            isMuted={mutedUsers.has(userId)}
           />
         </View>
       ))}
