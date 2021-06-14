2d809c1f69d315f104b685428d854356eb76532e
Greg Price <greg@zulipchat.com>
2021-06-14 15:02:29 -0700

    StreamList [nfc]: Explain PseudoSubscription, and extend to newly-typed code.
    Before c37e5b914, the accesses to these properties were in code that
    wasn't effectively type-checked, so Flow didn't spot that we hadn't
    documented them as being potentially present.

--- src/streams/StreamList.js
+++ src/streams/StreamList.js
@@ -15,7 +15,27 @@ const styles = createStyleSheet({
   },
 });
 
-type PseudoSubscription = Subscription | {| ...Stream, subscribed: boolean, pin_to_top?: void |};
+// This component's callers have a legacy hack where they pass ad-hoc sets
+// of properties that are different between the two callers.
+//
+// This drives some visible differences in behavior, some of which are
+// desired.
+//
+// We should clean this up: #3767.  Until then, this type describes the
+// existing data flow.
+type PseudoSubscription =
+  // The `foo?: void` properties are a way of saying: "this property isn't
+  // here, but when I read it just say it gets `undefined` and don't worry
+  // about it."  The code below reads some properties that exist in only one
+  // branch of the union, and relies on getting `undefined` in the other branch.
+  | $ReadOnly<{| ...Subscription, subscribed?: void |}>
+  | $ReadOnly<{|
+      ...Stream,
+      subscribed: boolean,
+      pin_to_top?: void,
+      color?: void,
+      in_home_view?: void,
+    |}>;
 
 type Props = $ReadOnly<{|
   showDescriptions: boolean,
@@ -62,21 +82,16 @@ export default class StreamList extends PureComponent<Props> {
         extraData={unreadByStream}
         initialNumToRender={20}
         keyExtractor={item => item.stream_id}
-        // See https://github.com/zulip/zulip-mobile/pull/4718#discussion_r627779508
-        // for details on the FlowFixMe comments below.
         renderItem={({ item }: { item: PseudoSubscription }) => (
           <StreamItem
             name={item.name}
             iconSize={16}
             isPrivate={item.invite_only}
             description={showDescriptions ? item.description : ''}
-            // $FlowFixMe[prop-missing]
             color={item.color}
             unreadCount={unreadByStream[item.stream_id]}
-            // $FlowFixMe[prop-missing]
             isMuted={item.in_home_view === false} // if 'undefined' is not muted
             showSwitch={showSwitch}
-            // $FlowFixMe[prop-missing]
             isSubscribed={item.subscribed}
             onPress={onPress}
             onSwitch={onSwitch}
