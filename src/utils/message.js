89473dc2d9a15e3413ed0d15c405197b5e018e80
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-06-16 14:12:10 -0700

    messages: Don't hide muted messages in mentioned narrow.
    Currently, we don't show muted messages in the mentioned narrow. This is
    confusing and inconsistent with the webapp. This commit fixes that.
    
    We should do a more thorough audit of this code, and compare it to the
    webapp version. I haven't attempted to do that right now, since this
    small tactical fix seems important and high-priority.
    
    Related: #3472

--- src/utils/message.js
+++ src/utils/message.js
@@ -1,6 +1,6 @@
 /* @flow strict-local */
 import type { Narrow, Message, MuteState, Outbox, Subscription } from '../types';
-import { isHomeNarrow, isTopicNarrow } from './narrow';
+import { isHomeNarrow, isTopicNarrow, isMentionedNarrow } from './narrow';
 import { streamNameOfStreamMessage } from './recipient';
 
 export const isTopicMuted = (stream: string, topic: string, mute: MuteState = []): boolean =>
@@ -18,6 +18,16 @@ export const shouldBeMuted = (
 
   if (isTopicNarrow(narrow)) {
     return false; // never hide a message when narrowed to topic
+  }
+
+  // This logic isn't quite right - we want to make sure we never hide a
+  // message that has a mention, even if we aren't in the "Mentioned"
+  // narrow. (#3472)  However, it's more complex to do that, and this code
+  // fixes the largest problem we'd had with muted mentioned messages, which
+  // is that they show up in the count for the "Mentions" tab, but without
+  // this conditional they wouldn't in the actual narrow.
+  if (isMentionedNarrow(narrow)) {
+    return false;
   }
 
   const streamName = streamNameOfStreamMessage(message);
