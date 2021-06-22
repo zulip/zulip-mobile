6ac73d5372966425b278e3ea77e7ea5c3c6ca903
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-06-22 14:17:28 -0700

    mute tests: Add types.

--- src/utils/__tests__/message-test.js
+++ src/utils/__tests__/message-test.js
@@ -1,24 +1,29 @@
+/* @flow strict-local */
 import { shouldBeMuted } from '../message';
 import { HOME_NARROW, MENTIONED_NARROW, topicNarrow } from '../narrow';
+import * as eg from '../../__tests__/lib/exampleData';
 
 describe('shouldBeMuted', () => {
-  test('private messages are never muted', () => {
-    const message = {
-      display_recipient: [],
-      type: 'private',
-    };
+  const stream = eg.makeStream({
+    name: 'stream',
+  });
 
-    const isMuted = shouldBeMuted(message, HOME_NARROW, []);
+  const subscription = eg.makeSubscription({ stream });
+
+  const message = eg.streamMessage({
+    stream,
+    subject: 'some topic',
+  });
+
+  test('private messages are never muted', () => {
+    const pmMessage = eg.pmMessage();
+    const isMuted = shouldBeMuted(pmMessage, HOME_NARROW, []);
 
     expect(isMuted).toBe(false);
   });
 
   test('messages when narrowed to a topic are never muted', () => {
-    const message = {
-      display_recipient: 'stream',
-      subject: 'some topic',
-    };
-    const narrow = topicNarrow('some topic');
+    const narrow = topicNarrow('stream', 'some topic');
     const mutes = [['stream', 'some topic']];
 
     const isMuted = shouldBeMuted(message, narrow, [], mutes);
@@ -27,64 +32,32 @@ describe('shouldBeMuted', () => {
   });
 
   test('message in a stream is muted if stream is not in mute list', () => {
-    const message = {
-      type: 'stream',
-      display_recipient: 'stream',
-    };
-
     const isMuted = shouldBeMuted(message, HOME_NARROW, []);
 
     expect(isMuted).toBe(true);
   });
 
   test('message in a stream is muted if the stream is muted', () => {
-    const message = {
-      type: 'stream',
-      display_recipient: 'stream',
-    };
-    const subscriptions = [
-      {
-        name: 'stream',
-        in_home_view: false,
-      },
-    ];
-    const isMuted = shouldBeMuted(message, HOME_NARROW, subscriptions);
+    const mutedSubscription = eg.makeSubscription({
+      stream,
+      in_home_view: false,
+    });
+
+    const isMuted = shouldBeMuted(message, HOME_NARROW, [mutedSubscription]);
 
     expect(isMuted).toBe(true);
   });
 
   test('message in a stream is muted if the topic is muted and topic matches', () => {
-    const message = {
-      type: 'stream',
-      display_recipient: 'stream',
-      subject: 'topic',
-    };
-    const subscriptions = [
-      {
-        name: 'stream',
-        in_home_view: true,
-      },
-    ];
-    const mutes = [['stream', 'topic']];
-    const isMuted = shouldBeMuted(message, HOME_NARROW, subscriptions, mutes);
+    const mutes = [['stream', 'some topic']];
+    const isMuted = shouldBeMuted(message, HOME_NARROW, [subscription], mutes);
 
     expect(isMuted).toBe(true);
   });
 
   test('mention narrow messages are never muted', () => {
-    const message = {
-      type: 'stream',
-      display_recipient: 'stream',
-      subject: 'topic',
-    };
-    const subscriptions = [
-      {
-        name: 'stream',
-        in_home_view: true,
-      },
-    ];
-    const mutes = [['stream', 'topic']];
-    const isMuted = shouldBeMuted(message, MENTIONED_NARROW, subscriptions, mutes);
+    const mutes = [['stream', 'some topic']];
+    const isMuted = shouldBeMuted(message, MENTIONED_NARROW, [subscription], mutes);
 
     expect(isMuted).toBe(false);
   });
