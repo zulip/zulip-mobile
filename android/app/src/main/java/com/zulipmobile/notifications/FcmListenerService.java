c2087fa33ef528e7692e3eea87a502853126c8ff
Akash <akash.d0407@gmail.com>
2021-06-28 22:44:27 +0530

    android notif [nfc]: Remove `InstanceIDListenerService.java`.
    This is deprecated [1][2] in favor of overriding
    `FirebaseMessagingService#onNewToken`.
    
    While not causing any noticable bugs, its good to have up to date
    code. One benefit of this specific commit is that it removes some
    code.
    
    Something related but not done in this commit is to update
    firebase-messaging itself, which as of now stands at 22.0.0 .
    
    [1] https://firebase.google.com/docs/reference/android/com/google/firebase/iid/FirebaseInstanceIdService
    [2] https://firebase.google.com/support/release-notes/android#firebase_instance_id_version_1620

--- android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.java
+++ android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.java
@@ -1,6 +1,8 @@
 package com.zulipmobile.notifications;
 
 import android.content.Context;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.bridge.ReactContext;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -16,5 +18,16 @@ public class FcmListenerService extends FirebaseMessagingService {
         final ConversationMap conversations =
                 ((MainApplication) applicationContext).getConversations();
         FCMPushNotifications.onReceived(this, conversations, message.getData());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        final ReactContext reactContext =
+                ((ReactApplication) getApplication())
+                        .getReactNativeHost()
+                        .getReactInstanceManager()
+                        .getCurrentReactContext();
+        NotificationsModule.emitToken(reactContext, token);
     }
 }
