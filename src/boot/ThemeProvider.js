8ebb8053163764264f6fc538a85468ec9cc94881
Chris Bobbe <cbobbe@zulip.com>
2021-06-30 15:08:38 -0400

    ThemeProvider [nfc]: Use `useSelector` instead of `connect`.
    An instance of #4837.

--- src/boot/ThemeProvider.js
+++ src/boot/ThemeProvider.js
@@ -3,20 +3,18 @@
 import React from 'react';
 
 import type { Node as React$Node } from 'react';
-import type { ThemeName, Dispatch } from '../types';
-import { connect } from '../react-redux';
+import { useSelector } from '../react-redux';
 import { getSettings } from '../directSelectors';
 import { themeData, ThemeContext } from '../styles/theme';
 import { ZulipStatusBar } from '../common';
 
 type Props = $ReadOnly<{|
-  dispatch: Dispatch,
-  theme: ThemeName,
   children: React$Node,
 |}>;
 
-function ThemeProvider(props: Props) {
-  const { children, theme } = props;
+export default function ThemeProvider(props: Props) {
+  const { children } = props;
+  const theme = useSelector(state => getSettings(state).theme);
   return (
     <ThemeContext.Provider value={themeData[theme]}>
       <ZulipStatusBar />
@@ -24,7 +22,3 @@ function ThemeProvider(props: Props) {
     </ThemeContext.Provider>
   );
 }
-
-export default connect(state => ({
-  theme: getSettings(state).theme,
-}))(ThemeProvider);
