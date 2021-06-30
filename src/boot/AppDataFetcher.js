75f5d727109a57b702372131967e0c89ce3188c0
Chris Bobbe <cbobbe@zulip.com>
2021-06-30 14:57:47 -0400

    AppDataFetcher [nfc]: Use `useSelector` instead of `connect`.
    An instance of #4837.

--- src/boot/AppDataFetcher.js
+++ src/boot/AppDataFetcher.js
@@ -3,19 +3,17 @@
 import React from 'react';
 
 import type { Node as React$Node } from 'react';
-import type { Dispatch } from '../types';
-import { connect } from '../react-redux';
+import { useSelector, useDispatch } from '../react-redux';
 import { getSession } from '../directSelectors';
 import { doInitialFetch } from '../actions';
 
 type Props = $ReadOnly<{|
-  needsInitialFetch: boolean,
-  dispatch: Dispatch,
   children: React$Node,
 |}>;
 
-function AppDataFetcher(props: Props) {
-  const { needsInitialFetch, dispatch } = props;
+export default function AppDataFetcher(props: Props) {
+  const needsInitialFetch = useSelector(state => getSession(state).needsInitialFetch);
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
     if (needsInitialFetch) {
@@ -25,7 +23,3 @@ function AppDataFetcher(props: Props) {
 
   return props.children;
 }
-
-export default connect(state => ({
-  needsInitialFetch: getSession(state).needsInitialFetch,
-}))(AppDataFetcher);
