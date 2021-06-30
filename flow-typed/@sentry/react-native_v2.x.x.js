8b2b64a6c6975de498600b087e0ebb64cbb34b1f
Chris Bobbe <cbobbe@zulip.com>
2021-06-29 23:15:59 -0400

    @sentry/react-native libdef: Make object types exact or explicitly inexact.
    An instance of #3452.

--- flow-typed/@sentry/react-native_v2.x.x.js
+++ flow-typed/@sentry/react-native_v2.x.x.js
@@ -38,7 +38,7 @@ declare module '@sentry/react-native' {
   //
   // Flow doesn't support true enums, so we split the TypeScript `enum Severity`
   // into a separate variable `Severity` and type `SeverityType`.
-  declare export var Severity: $ReadOnly<{
+  declare export var Severity: $ReadOnly<{|
     Fatal: 'fatal',
     Error: 'error',
     Warning: 'warning',
@@ -46,7 +46,7 @@ declare module '@sentry/react-native' {
     Info: 'info',
     Debug: 'debug',
     Critical: 'critical',
-  }>;
+  |}>;
   declare export type SeverityType = $Values<typeof Severity>;
 
   // Taken from @sentry/types/src/event.ts.
@@ -110,6 +110,7 @@ declare module '@sentry/react-native' {
 
     captureException(exception: mixed, hint?: EventHint): string,
     captureMessage(message: string, level?: SeverityType, hint?: EventHint): string,
+    ...
   };
 
   // Taken from @sentry/{minimal,types}/src/scope.ts.
@@ -147,6 +148,7 @@ declare module '@sentry/react-native' {
   // Adapted from @sentry/types/src/client.ts, with some specialization.
   declare export type Client = {
     getOptions(): Options,
+    ...
   };
 
   // Adapted from @sentry/react-native/src/sdk.ts.
