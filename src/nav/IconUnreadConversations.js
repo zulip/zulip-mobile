7ddc1c3fd53d2cda666e07eb0fce60abb0c120fa
Chris Bobbe <cbobbe@zulip.com>
2021-07-06 20:17:27 -0400

    IconUnreadConversations [nfc]: Convert to a function component.

--- src/nav/IconUnreadConversations.js
+++ src/nav/IconUnreadConversations.js
@@ -1,6 +1,6 @@
 /* @flow strict-local */
 
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 
 import type { Dispatch } from '../types';
@@ -16,19 +16,17 @@ type Props = $ReadOnly<{|
   color: string,
 |}>;
 
-class IconUnreadConversations extends PureComponent<Props> {
-  render() {
-    const { unreadHuddlesTotal, unreadPmsTotal, color } = this.props;
-    const unreadCount = unreadHuddlesTotal + unreadPmsTotal;
+function IconUnreadConversations(props: Props) {
+  const { unreadHuddlesTotal, unreadPmsTotal, color } = props;
+  const unreadCount = unreadHuddlesTotal + unreadPmsTotal;
 
-    return (
-      <View>
-        <CountOverlay unreadCount={unreadCount}>
-          <IconPeople size={24} color={color} />
-        </CountOverlay>
-      </View>
-    );
-  }
+  return (
+    <View>
+      <CountOverlay unreadCount={unreadCount}>
+        <IconPeople size={24} color={color} />
+      </CountOverlay>
+    </View>
+  );
 }
 
 export default connect(state => ({
