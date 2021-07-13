ec3f51a70c0daeaf038b3a12a6f2be1bad3db8d4
Chris Bobbe <cbobbe@zulip.com>
2021-07-13 15:18:35 -0400

    UnreadNotice: Handle left and right insets.
    We'll tidy this up a bit in the next commit by removing the inner
    `View` (the one that uses the `unreadTextWrapper` styles).

--- src/chat/UnreadNotice.js
+++ src/chat/UnreadNotice.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import type { Narrow } from '../types';
 import { createStyleSheet } from '../styles';
@@ -16,9 +17,11 @@ const styles = createStyleSheet({
     paddingHorizontal: 8,
     paddingVertical: 4,
     backgroundColor: 'hsl(232, 89%, 78%)',
+    overflow: 'hidden',
+  },
+  safeAreaWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    overflow: 'hidden',
   },
   unreadTextWrapper: {
     flexDirection: 'row',
@@ -34,26 +37,33 @@ type Props = $ReadOnly<{|
   narrow: Narrow,
 |}>;
 
+/**
+ * Says how many unread messages are in the narrow.
+ *
+ * Pads the left and right insets with its background.
+ */
 export default function UnreadNotice(props: Props) {
   const { narrow } = props;
   const unreadCount = useSelector(state => getUnreadCountForNarrow(state, narrow));
 
   return (
     <AnimatedScaleComponent visible={unreadCount > 0} style={styles.unreadContainer}>
-      <View style={styles.unreadTextWrapper}>
-        <Label
-          style={styles.unreadText}
-          text={{
-            text: `{unreadCount, plural,
+      <SafeAreaView mode="padding" edges={['right', 'left']} style={styles.safeAreaWrapper}>
+        <View style={styles.unreadTextWrapper}>
+          <Label
+            style={styles.unreadText}
+            text={{
+              text: `{unreadCount, plural,
   =0 {No unread messages}
   =1 {# unread message}
   other {# unread messages}
 }`,
-            values: { unreadCount },
-          }}
-        />
-      </View>
-      <MarkAsReadButton narrow={narrow} />
+              values: { unreadCount },
+            }}
+          />
+        </View>
+        <MarkAsReadButton narrow={narrow} />
+      </SafeAreaView>
     </AnimatedScaleComponent>
   );
 }
