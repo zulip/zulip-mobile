2c7caa67081bc6faea4bd84c69bb41c84227c26d
Chris Bobbe <cbobbe@zulip.com>
2021-07-12 20:01:46 -0400

    navSelectors [nfc]: Inline getNavigationRoutes.

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -1,13 +1,8 @@
 /* @flow strict-local */
-import type { Route } from '@react-navigation/native';
-
 import * as NavigationService from './NavigationService';
 
-export const getNavigationRoutes = (): $ReadOnlyArray<Route<string>> =>
-  NavigationService.getState().routes;
-
 export const getSameRoutesCount = (): number => {
-  const routes = getNavigationRoutes();
+  const routes = NavigationService.getState().routes;
   let i = routes.length - 1;
   while (i >= 0) {
     if (routes[i].name !== routes[routes.length - 1].name) {
