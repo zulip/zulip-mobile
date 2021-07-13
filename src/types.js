ce48ef5065a2f646cb39688da0d9ed80649ebefc
Akash <akash.d0407@gmail.com>
2021-07-14 03:44:27 +0530

    sharing [nfc]: Add doc about synchronising native and js data type.

--- src/types.js
+++ src/types.js
@@ -379,6 +379,15 @@ export type PmConversationData = {|
   unread: number,
 |};
 
+/**
+ * The data we need in JS/React code representing shared content.
+ *
+ * On Android, these objects are sent to JS from our platform-native code,
+ * constructed there by `getParamsFromIntent` in `SharingHelper.kt`.
+ * The correspondence of that code with this type isn't type-checked.
+ *
+ * Note: Keep in sync with platform-native code.
+ */
 export type SharedText = {|
   type: 'text',
   sharedText: string,
