b7bf634262970f73c3f5b5782c2e7ee077d00ee6
Greg Price <greg@zulipchat.com>
2021-07-19 16:12:28 -0700

    mute [nfc]: Make isTopicMuted's `mute` parameter required.
    It wouldn't make any sense to call this without providing
    the actual data here.

--- src/mute/muteModel.js
+++ src/mute/muteModel.js
@@ -1,5 +1,5 @@
 /* @flow strict-local */
 import type { MuteState } from '../types';
 
-export const isTopicMuted = (stream: string, topic: string, mute: MuteState = []): boolean =>
+export const isTopicMuted = (stream: string, topic: string, mute: MuteState): boolean =>
   mute.some(x => x[0] === stream && x[1] === topic);
