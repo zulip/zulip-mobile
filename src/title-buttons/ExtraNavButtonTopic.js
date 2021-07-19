0c124382eb6e833ff1665a4123ec9b6ca55f37b6
Chris Bobbe <cbobbe@zulip.com>
2021-07-19 13:20:41 -0400

    ExtraNavButtonTopic [nfc]: Use `useSelector` instead of `connect`.
    An instance of #4837.

--- src/title-buttons/ExtraNavButtonTopic.js
+++ src/title-buttons/ExtraNavButtonTopic.js
@@ -2,22 +2,22 @@
 
 import React, { useCallback } from 'react';
 
-import type { Dispatch, Narrow, Stream } from '../types';
-import { connect } from '../react-redux';
+import type { Narrow } from '../types';
+import { useSelector, useDispatch } from '../react-redux';
 import { getStreams } from '../selectors';
 import { streamNameOfNarrow, streamNarrow } from '../utils/narrow';
 import NavButton from '../nav/NavButton';
 import { doNarrow } from '../actions';
 
 type Props = $ReadOnly<{|
-  dispatch: Dispatch,
   narrow: Narrow,
   color: string,
-  streams: Stream[],
 |}>;
 
-function ExtraNavButtonTopic(props: Props) {
-  const { dispatch, narrow, streams, color } = props;
+export default function ExtraNavButtonTopic(props: Props) {
+  const { narrow, color } = props;
+  const dispatch = useDispatch();
+  const streams = useSelector(getStreams);
 
   const handlePress = useCallback(() => {
     const streamName = streamNameOfNarrow(narrow);
@@ -29,7 +29,3 @@ function ExtraNavButtonTopic(props: Props) {
 
   return <NavButton name="arrow-up" color={color} onPress={handlePress} />;
 }
-
-export default connect(state => ({
-  streams: getStreams(state),
-}))(ExtraNavButtonTopic);
