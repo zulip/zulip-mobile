091eb7cc18ab93bf6fc18bebc4d91885736689d9
Greg Price <greg@zulipchat.com>
2021-07-19 14:39:49 -0700

    mute tests [nfc]: Compact further by inlining actuals.
    This follows up on adding types and using exampleData, to further
    make it easier to scan through these tests and see what scenarios
    they cover and don't cover.

--- src/utils/__tests__/message-test.js
+++ src/utils/__tests__/message-test.js
@@ -17,24 +17,17 @@ describe('shouldBeMuted', () => {
 
   test('private messages are never muted', () => {
     const pmMessage = eg.pmMessage();
-    const isMuted = shouldBeMuted(pmMessage, HOME_NARROW, []);
-
-    expect(isMuted).toBe(false);
+    expect(shouldBeMuted(pmMessage, HOME_NARROW, [])).toBe(false);
   });
 
   test('messages when narrowed to a topic are never muted', () => {
     const narrow = topicNarrow('stream', 'some topic');
     const mutes = [['stream', 'some topic']];
-
-    const isMuted = shouldBeMuted(message, narrow, [], mutes);
-
-    expect(isMuted).toBe(false);
+    expect(shouldBeMuted(message, narrow, [], mutes)).toBe(false);
   });
 
   test('message in a stream is muted if stream is not in mute list', () => {
-    const isMuted = shouldBeMuted(message, HOME_NARROW, []);
-
-    expect(isMuted).toBe(true);
+    expect(shouldBeMuted(message, HOME_NARROW, [])).toBe(true);
   });
 
   test('message in a stream is muted if the stream is muted', () => {
@@ -42,23 +35,16 @@ describe('shouldBeMuted', () => {
       stream,
       in_home_view: false,
     });
-
-    const isMuted = shouldBeMuted(message, HOME_NARROW, [mutedSubscription]);
-
-    expect(isMuted).toBe(true);
+    expect(shouldBeMuted(message, HOME_NARROW, [mutedSubscription])).toBe(true);
   });
 
   test('message in a stream is muted if the topic is muted and topic matches', () => {
     const mutes = [['stream', 'some topic']];
-    const isMuted = shouldBeMuted(message, HOME_NARROW, [subscription], mutes);
-
-    expect(isMuted).toBe(true);
+    expect(shouldBeMuted(message, HOME_NARROW, [subscription], mutes)).toBe(true);
   });
 
   test('mention narrow messages are never muted', () => {
     const mutes = [['stream', 'some topic']];
-    const isMuted = shouldBeMuted(message, MENTIONED_NARROW, [subscription], mutes);
-
-    expect(isMuted).toBe(false);
+    expect(shouldBeMuted(message, MENTIONED_NARROW, [subscription], mutes)).toBe(false);
   });
 });
