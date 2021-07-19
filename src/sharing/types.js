79b1f3d17cf005c774beb3dfadfcd0f51247f20c
Greg Price <greg@zulipchat.com>
2021-07-19 11:53:29 -0700

    sharing types [nfc]: Condense SharedData union to one definition.
    This lets us not give names to the individual branches of this
    union, which frees up namespace so that in particular we can use
    a name like `SharedFile` for each file's data when we add support
    for multiple files in `SharedData`.
    
    This also makes it easier to take in the whole definition at once,
    since it's short enough that it fits so compactly this way.

--- src/sharing/types.js
+++ src/sharing/types.js
@@ -1,7 +1,7 @@
 /* @flow strict-local */
 
 /**
- * The data we need in JS/React code representing shared content.
+ * The data we get when the user "shares" to Zulip from another app.
  *
  * On Android, these objects are sent to JS from our platform-native code,
  * constructed there by `getParamsFromIntent` in `SharingHelper.kt`.
@@ -9,24 +9,7 @@
  *
  * Note: Keep in sync with platform-native code.
  */
-type SharedText = {|
-  type: 'text',
-  sharedText: string,
-|};
-
-type SharedImage = {|
-  type: 'image',
-  sharedImageUrl: string,
-  fileName: string,
-|};
-
-type SharedFile = {|
-  type: 'file',
-  sharedFileUrl: string,
-  fileName: string,
-|};
-
-/**
- * The data we get when the user "shares" to Zulip from another app.
- */
-export type SharedData = SharedText | SharedImage | SharedFile;
+export type SharedData =
+  | {| type: 'text', sharedText: string |}
+  | {| type: 'image', sharedImageUrl: string, fileName: string |}
+  | {| type: 'file', sharedFileUrl: string, fileName: string |};
