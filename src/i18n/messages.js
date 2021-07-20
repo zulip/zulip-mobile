ff6636d7a4d5563abdc1a7b603f939673ff02508
Chris Bobbe <cbobbe@zulip.com>
2021-07-19 20:36:37 -0400

    i18n [nfc]: Use inline requires, instead of `import`s at the top.
    So we don't have to assign valid JavaScript identifiers to these
    things, which causes confusion.
    
    As Anders points out [1], `-` and `_` have separate meanings. So
    it's best not to choose `_` simply to make a valid JS identifier.
    
    [1] https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/.60_.60.20vs.20.60-.60.20in.20locales/near/1234566

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -1,101 +1,53 @@
 /* @flow strict-local */
-
-import ar from '../../static/translations/messages_ar.json';
-import bg from '../../static/translations/messages_bg.json';
-import bn from '../../static/translations/messages_bn.json';
-import ca from '../../static/translations/messages_ca.json';
-import cs from '../../static/translations/messages_cs.json';
-import da from '../../static/translations/messages_da.json';
-import de from '../../static/translations/messages_de.json';
-import el from '../../static/translations/messages_el.json';
-import en from '../../static/translations/messages_en.json';
-import eo from '../../static/translations/messages_eo.json';
-import es from '../../static/translations/messages_es.json';
-import fa from '../../static/translations/messages_fa.json';
-import fi from '../../static/translations/messages_fi.json';
-import fr from '../../static/translations/messages_fr.json';
-import fr_FR from '../../static/translations/messages_fr_FR.json';
-import gl from '../../static/translations/messages_gl.json';
-import gu from '../../static/translations/messages_gu.json';
-import hi from '../../static/translations/messages_hi.json';
-import hr from '../../static/translations/messages_hr.json';
-import hr_HR from '../../static/translations/messages_hr_HR.json';
-import hu from '../../static/translations/messages_hu.json';
-import id from '../../static/translations/messages_id.json';
-import it from '../../static/translations/messages_it.json';
-import ja from '../../static/translations/messages_ja.json';
-import ko from '../../static/translations/messages_ko.json';
-import lt from '../../static/translations/messages_lt.json';
-import ml from '../../static/translations/messages_ml.json';
-import nl from '../../static/translations/messages_nl.json';
-import no from '../../static/translations/messages_no.json';
-import pa from '../../static/translations/messages_pa.json';
-import pl from '../../static/translations/messages_pl.json';
-import pt from '../../static/translations/messages_pt.json';
-import pt_BR from '../../static/translations/messages_pt_BR.json';
-import pt_PT from '../../static/translations/messages_pt_PT.json';
-import ro from '../../static/translations/messages_ro.json';
-import ru from '../../static/translations/messages_ru.json';
-import sk from '../../static/translations/messages_sk.json';
-import sr from '../../static/translations/messages_sr.json';
-import sv from '../../static/translations/messages_sv.json';
-import ta from '../../static/translations/messages_ta.json';
-import te from '../../static/translations/messages_te.json';
-import tl from '../../static/translations/messages_tl.json';
-import tr from '../../static/translations/messages_tr.json';
-import uk from '../../static/translations/messages_uk.json';
-import uz from '../../static/translations/messages_uz.json';
-import vi from '../../static/translations/messages_vi.json';
-import zh_Hans from '../../static/translations/messages_zh-Hans.json';
-import zh_Hant from '../../static/translations/messages_zh-Hant.json';
+/* eslint-disable global-require */
 
 export default {
-  ar,
-  bg,
-  bn,
-  ca,
-  cs,
-  da,
-  de,
-  el,
-  en,
-  eo,
-  es,
-  fa,
-  fi,
-  fr,
-  'fr-FR': fr_FR,
-  gl,
-  gu,
-  hi,
-  hr,
-  'hr-HR': hr_HR,
-  hu,
-  id,
-  it,
-  ja,
-  ko,
-  lt,
-  ml,
-  nl,
-  no,
-  pa,
-  pl,
-  pt,
-  'pt-BR': pt_BR,
-  'pt-PT': pt_PT,
-  ro,
-  ru,
-  sk,
-  sr,
-  sv,
-  ta,
-  te,
-  tl,
-  tr,
-  uk,
-  uz,
-  vi,
-  'zh-Hans': zh_Hans,
-  'zh-Hant': zh_Hant,
+  ar: require('../../static/translations/messages_ar.json'),
+  bg: require('../../static/translations/messages_bg.json'),
+  bn: require('../../static/translations/messages_bn.json'),
+  ca: require('../../static/translations/messages_ca.json'),
+  cs: require('../../static/translations/messages_cs.json'),
+  da: require('../../static/translations/messages_da.json'),
+  de: require('../../static/translations/messages_de.json'),
+  el: require('../../static/translations/messages_el.json'),
+  en: require('../../static/translations/messages_en.json'),
+  eo: require('../../static/translations/messages_eo.json'),
+  es: require('../../static/translations/messages_es.json'),
+  fa: require('../../static/translations/messages_fa.json'),
+  fi: require('../../static/translations/messages_fi.json'),
+  fr: require('../../static/translations/messages_fr.json'),
+  'fr-FR': require('../../static/translations/messages_fr_FR.json'),
+  gl: require('../../static/translations/messages_gl.json'),
+  gu: require('../../static/translations/messages_gu.json'),
+  hi: require('../../static/translations/messages_hi.json'),
+  hr: require('../../static/translations/messages_hr.json'),
+  'hr-HR': require('../../static/translations/messages_hr_HR.json'),
+  hu: require('../../static/translations/messages_hu.json'),
+  id: require('../../static/translations/messages_id.json'),
+  it: require('../../static/translations/messages_it.json'),
+  ja: require('../../static/translations/messages_ja.json'),
+  ko: require('../../static/translations/messages_ko.json'),
+  lt: require('../../static/translations/messages_lt.json'),
+  ml: require('../../static/translations/messages_ml.json'),
+  nl: require('../../static/translations/messages_nl.json'),
+  no: require('../../static/translations/messages_no.json'),
+  pa: require('../../static/translations/messages_pa.json'),
+  pl: require('../../static/translations/messages_pl.json'),
+  pt: require('../../static/translations/messages_pt.json'),
+  'pt-BR': require('../../static/translations/messages_pt_BR.json'),
+  'pt-PT': require('../../static/translations/messages_pt_PT.json'),
+  ro: require('../../static/translations/messages_ro.json'),
+  ru: require('../../static/translations/messages_ru.json'),
+  sk: require('../../static/translations/messages_sk.json'),
+  sr: require('../../static/translations/messages_sr.json'),
+  sv: require('../../static/translations/messages_sv.json'),
+  ta: require('../../static/translations/messages_ta.json'),
+  te: require('../../static/translations/messages_te.json'),
+  tl: require('../../static/translations/messages_tl.json'),
+  tr: require('../../static/translations/messages_tr.json'),
+  uk: require('../../static/translations/messages_uk.json'),
+  uz: require('../../static/translations/messages_uz.json'),
+  vi: require('../../static/translations/messages_vi.json'),
+  'zh-Hans': require('../../static/translations/messages_zh-Hans.json'),
+  'zh-Hant': require('../../static/translations/messages_zh-Hant.json'),
 };
