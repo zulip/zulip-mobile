7d5bcc370dd385644057edd67eaef74b73d4b90e
Chris Bobbe <cbobbe@zulip.com>
2021-07-20 17:31:40 -0400

    CreateGroupScreen [nfc]: Use `useSelector` instead of `connect`.
    An instance of #4837.

--- src/user-groups/CreateGroupScreen.js
+++ src/user-groups/CreateGroupScreen.js
@@ -4,8 +4,8 @@ import React, { useState, useCallback } from 'react';
 import type { RouteProp } from '../react-navigation';
 import type { AppNavigationProp } from '../nav/AppNavigator';
 import * as NavigationService from '../nav/NavigationService';
-import type { Dispatch, UserId, UserOrBot } from '../types';
-import { connect } from '../react-redux';
+import type { UserOrBot } from '../types';
+import { useSelector, useDispatch } from '../react-redux';
 import { Screen } from '../common';
 import { doNarrow, navigateBack } from '../actions';
 import { pmNarrowFromUsers } from '../utils/narrow';
@@ -13,20 +13,14 @@ import { pmKeyRecipientsFromUsers } from '../utils/recipient';
 import UserPickerCard from '../user-picker/UserPickerCard';
 import { getOwnUserId } from '../users/userSelectors';
 
-type SelectorProps = {|
-  +ownUserId: UserId,
-|};
-
 type Props = $ReadOnly<{|
   navigation: AppNavigationProp<'create-group'>,
   route: RouteProp<'create-group', void>,
-
-  dispatch: Dispatch,
-  ...SelectorProps,
 |}>;
 
-function CreateGroupScreen(props: Props) {
-  const { dispatch, ownUserId } = props;
+export default function CreateGroupScreen(props: Props) {
+  const dispatch = useDispatch();
+  const ownUserId = useSelector(getOwnUserId);
 
   const [filter, setFilter] = useState<string>('');
 
@@ -46,7 +40,3 @@ function CreateGroupScreen(props: Props) {
     </Screen>
   );
 }
-
-export default connect<SelectorProps, _, _>(state => ({
-  ownUserId: getOwnUserId(state),
-}))(CreateGroupScreen);
