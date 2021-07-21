ea65f051fa153b98e891fbecb7f0455b5ed54b43
Chris Bobbe <cbobbe@zulip.com>
2021-07-21 11:52:33 -0400

    OptionDivider [nfc]: Pull out `componentStyles`.

--- src/common/OptionDivider.js
+++ src/common/OptionDivider.js
@@ -3,19 +3,21 @@ import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 
 import type { ThemeData } from '../styles';
-import { ThemeContext } from '../styles';
+import { ThemeContext, createStyleSheet } from '../styles';
+
+const componentStyles = createStyleSheet({
+  divider: {
+    borderBottomWidth: 1,
+  },
+});
 
 export default class OptionDivider extends PureComponent<{||}> {
   static contextType = ThemeContext;
   context: ThemeData;
 
-  styles = {
-    divider: {
-      borderBottomWidth: 1,
-    },
-  };
-
   render() {
-    return <View style={[this.styles.divider, { borderBottomColor: this.context.dividerColor }]} />;
+    return (
+      <View style={[componentStyles.divider, { borderBottomColor: this.context.dividerColor }]} />
+    );
   }
 }
