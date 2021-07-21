c1a3c516e87d358f423be87af1b3016a586cd473
Chris Bobbe <cbobbe@zulip.com>
2021-07-21 11:41:26 -0400

    LineSeparator [nfc]: Pull out `componentStyles`.

--- src/common/LineSeparator.js
+++ src/common/LineSeparator.js
@@ -3,22 +3,22 @@ import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 
 import type { ThemeData } from '../styles';
-import { ThemeContext } from '../styles';
+import { ThemeContext, createStyleSheet } from '../styles';
+
+const componentStyles = createStyleSheet({
+  lineSeparator: {
+    height: 1,
+    margin: 4,
+  },
+});
 
 export default class LineSeparator extends PureComponent<{||}> {
   static contextType = ThemeContext;
   context: ThemeData;
 
-  styles = {
-    lineSeparator: {
-      height: 1,
-      margin: 4,
-    },
-  };
-
   render() {
     return (
-      <View style={[this.styles.lineSeparator, { backgroundColor: this.context.cardColor }]} />
+      <View style={[componentStyles.lineSeparator, { backgroundColor: this.context.cardColor }]} />
     );
   }
 }
