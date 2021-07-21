34eb383a368b9ae5b35d5494cd20649a50cfe546
Chris Bobbe <cbobbe@zulip.com>
2021-07-21 13:23:18 -0400

    SwitchRow [nfc]: Convert to function component.

--- src/common/SwitchRow.js
+++ src/common/SwitchRow.js
@@ -1,12 +1,11 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
 
 import type { SpecificIconType } from './Icons';
 import Label from './Label';
 import ZulipSwitch from './ZulipSwitch';
-import type { ThemeData } from '../styles';
 import styles, { ThemeContext, createStyleSheet } from '../styles';
 
 type Props = $ReadOnly<{|
@@ -26,21 +25,18 @@ const componentStyles = createStyleSheet({
 /**
  * A row with a label and a switch component.
  */
-export default class SwitchRow extends PureComponent<Props> {
-  static contextType = ThemeContext;
-  context: ThemeData;
+export default function SwitchRow(props: Props) {
+  const { label, value, onValueChange, style, Icon } = props;
 
-  render() {
-    const { label, value, onValueChange, style, Icon } = this.props;
+  const themeContext = useContext(ThemeContext);
 
-    return (
-      <View style={[componentStyles.container, styles.listItem, style]}>
-        {!!Icon && <Icon size={24} style={[styles.settingsIcon, { color: this.context.color }]} />}
-        <Label text={label} style={styles.flexed} />
-        <View style={styles.rightItem}>
-          <ZulipSwitch value={value} onValueChange={onValueChange} />
-        </View>
+  return (
+    <View style={[componentStyles.container, styles.listItem, style]}>
+      {!!Icon && <Icon size={24} style={[styles.settingsIcon, { color: themeContext.color }]} />}
+      <Label text={label} style={styles.flexed} />
+      <View style={styles.rightItem}>
+        <ZulipSwitch value={value} onValueChange={onValueChange} />
       </View>
-    );
-  }
+    </View>
+  );
 }
