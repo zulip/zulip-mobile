74557b428443abc30db87886972e780c6ad172ee
Chris Bobbe <cbobbe@zulip.com>
2021-07-26 16:49:06 -0400

    ZulipNavigationContainer [nfc]: Convert to function component.

--- src/nav/ZulipNavigationContainer.js
+++ src/nav/ZulipNavigationContainer.js
@@ -1,9 +1,8 @@
 /* @flow strict-local */
-import React, { PureComponent } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 
 import { connect } from '../react-redux';
-import type { ThemeData } from '../styles';
 import { ThemeContext } from '../styles';
 import * as NavigationService from './NavigationService';
 import type { Dispatch, ThemeName } from '../types';
@@ -29,43 +28,46 @@ type Props = $ReadOnly<{|
  * - Call `createAppContainer` with the appropriate `initialRouteName`
  *   and `initialRouteParams` which we get from data in Redux.
  */
-class ZulipAppContainer extends PureComponent<Props> {
-  static contextType = ThemeContext;
-  context: ThemeData;
+function ZulipAppContainer(props: Props) {
+  const { theme: themeName } = props;
 
-  componentWillUnmount() {
-    NavigationService.isReadyRef.current = false;
-  }
-
-  render() {
-    const { theme: themeName } = this.props;
-
-    const BaseTheme = themeName === 'night' ? DarkTheme : DefaultTheme;
-
-    const theme = {
-      ...BaseTheme,
-      dark: themeName === 'night',
-      colors: {
-        ...BaseTheme.colors,
-        primary: this.context.color,
-        background: this.context.backgroundColor,
-        card: this.context.cardColor,
-        border: this.context.dividerColor,
+  useEffect(
+    () =>
+      // return a cleanup function:
+      //   https://reactjs.org/docs/hooks-effect.html#example-using-hooks-1
+      () => {
+        NavigationService.isReadyRef.current = false;
       },
-    };
+    [],
+  );
 
-    return (
-      <NavigationContainer
-        ref={NavigationService.navigationContainerRef}
-        onReady={() => {
-          NavigationService.isReadyRef.current = true;
-        }}
-        theme={theme}
-      >
-        <AppNavigator />
-      </NavigationContainer>
-    );
-  }
+  const themeContext = useContext(ThemeContext);
+
+  const BaseTheme = themeName === 'night' ? DarkTheme : DefaultTheme;
+
+  const theme = {
+    ...BaseTheme,
+    dark: themeName === 'night',
+    colors: {
+      ...BaseTheme.colors,
+      primary: themeContext.color,
+      background: themeContext.backgroundColor,
+      card: themeContext.cardColor,
+      border: themeContext.dividerColor,
+    },
+  };
+
+  return (
+    <NavigationContainer
+      ref={NavigationService.navigationContainerRef}
+      onReady={() => {
+        NavigationService.isReadyRef.current = true;
+      }}
+      theme={theme}
+    >
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
 
 export default connect(state => ({
