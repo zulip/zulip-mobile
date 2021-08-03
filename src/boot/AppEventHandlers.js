0df08eb8a1ff75b61ccf0570200718b4437fc299
Chris Bobbe <cbobbe@zulip.com>
2021-08-03 13:50:47 -0400

    netinfo libdef: Add and use, instead of writing types in AppEventHandlers.
    The version we're on now (5.9.10) is much later than 3.2.1, which it
    looks like was what informed the types written down in
    AppEventHandlers.
    
    One nice-looking thing we get is `isInternetReachable`, which should
    actually tell us whether the Internet is reachable (not just whether
    there's an active network connection).

--- src/boot/AppEventHandlers.js
+++ src/boot/AppEventHandlers.js
@@ -3,7 +3,6 @@
 import React, { PureComponent } from 'react';
 import type { Node, ComponentType } from 'react';
 import { AppState, View, Platform, NativeModules } from 'react-native';
-// $FlowFixMe[untyped-import]
 import NetInfo from '@react-native-community/netinfo';
 import * as ScreenOrientation from 'expo-screen-orientation';
 
@@ -15,45 +14,6 @@ import { handleInitialNotification, NotificationListener } from '../notification
 import { ShareReceivedListener, handleInitialShare } from '../sharing';
 import { appOnline, appOrientation } from '../actions';
 import PresenceHeartbeat from '../presence/PresenceHeartbeat';
-
-/**
- * Part of the interface from react-native-netinfo.
- * https://github.com/react-native-community/react-native-netinfo/tree/v3.2.1
- */
-// TODO: upgrade to 4.x.x so that we can use the `flow-typed` versions.
-// Requires RN 0.60+.
-type NetInfoStateType =
-  | 'unknown'
-  | 'none'
-  | 'cellular'
-  | 'wifi'
-  | 'bluetooth'
-  | 'ethernet'
-  | 'wimax'
-  | 'vpn'
-  | 'other';
-
-type NetInfoConnectedDetails = {
-  isConnectionExpensive: boolean,
-  ...
-};
-
-type NetInfoState = {
-  /** The type of the current connection. */
-  type: NetInfoStateType,
-
-  /** Whether there is an active network connection. Note that this DOES NOT
-      mean that the Internet is reachable. */
-  isConnected: boolean,
-
-  /**
-   * This actually has a more complicated type whose exact shape is dependent on
-   * the value of `type`, above. (Flow could describe it, but we don't have a
-   * use for it yet.)
-   */
-  details: null | NetInfoConnectedDetails,
-  ...
-};
 
 const styles = createStyleSheet({
   wrapper: {
@@ -103,8 +63,7 @@ class AppEventHandlersInner extends PureComponent<Props> {
     dispatch(appOrientation(orientationLookup[orientation]));
   };
 
-  // https://github.com/react-native-community/react-native-netinfo/tree/v3.2.1
-  handleConnectivityChange = (netInfoState: NetInfoState) => {
+  handleConnectivityChange = netInfoState => {
     const { dispatch } = this.props;
     const { type: connectionType } = netInfoState;
     const isConnected = connectionType !== 'none' && connectionType !== 'unknown';
