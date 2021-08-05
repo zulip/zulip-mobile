cf91b28eee5badf5da64de9323817383c8e68ab3
Akash Dhiman <akash.d0407@gmail.com>
2021-08-05 14:01:54 +0530

    notif [nfc]: prepare taskData for headless reply.
    This involves adding reply to taskData.

--- android/app/src/main/java/com/zulipmobile/notifications/ReplyService.kt
+++ android/app/src/main/java/com/zulipmobile/notifications/ReplyService.kt
@@ -1,15 +1,20 @@
 package com.zulipmobile.notifications
 
 import android.content.Intent
+import androidx.core.app.RemoteInput
 import com.facebook.react.HeadlessJsTaskService
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.jstasks.HeadlessJsTaskConfig
 
 class ReplyService : HeadlessJsTaskService() {
     override fun getTaskConfig(intent: Intent): HeadlessJsTaskConfig {
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        val taskData = intent.extras
+        val reply = remoteInput.getCharSequence(REPLY).toString()
+        taskData!!.putString("reply", reply)
         return HeadlessJsTaskConfig(
             "reply",
-            Arguments.fromBundle(intent.extras),
+            Arguments.fromBundle(taskData),
             5000
         )
     }
