f2412d0ce2d103d19b8ddf23410f5b2b22634d03
Akash Dhiman <akash.d0407@gmail.com>
2021-08-12 12:58:29 +0530

    wip: Configure headless fetch task.
    reference:
    - https://reactnative.dev/docs/headless-js-android

--- android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.java
+++ android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.java
@@ -1,11 +1,14 @@
 package com.zulipmobile.notifications;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.bridge.ReactContext;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import com.zulipmobile.HeadlessFetchService;
 import com.zulipmobile.MainApplication;
 
 public class FcmListenerService extends FirebaseMessagingService {
@@ -14,6 +17,15 @@ public class FcmListenerService extends FirebaseMessagingService {
         final Context applicationContext = getApplicationContext();
         if (!(applicationContext instanceof MainApplication)) {
             return;
+        }
+        {
+            Intent service = new Intent(applicationContext, HeadlessFetchService.class);
+            Bundle bundle = new Bundle();
+
+            bundle.putString("foo", "bar");
+            service.putExtras(bundle);
+
+            getApplicationContext().startService(service);
         }
         final ConversationMap conversations =
                 ((MainApplication) applicationContext).getConversations();
---
+++ src/headless/FetchTask.js
@@ -0,0 +1,8 @@
+/* @flow strict-local */
+
+const fetchTask = async (taskData: { ... }) => {
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(taskData));
+};
+
+export default fetchTask;
