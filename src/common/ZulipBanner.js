a71f1645654a633ea788b7d95f3b59c0826e524a
Chris Bobbe <cbobbe@zulip.com>
2021-08-17 17:16:38 -0400

    ZulipBanner: Better support single-row layout (3/3).
    This isn't pixel-perfect, as noted in a comment. But it's close, and
    it'll save a lot of space when the text and the buttons have room to
    share a row, and that's an important feature of how the doc says
    banners should work.

--- src/common/ZulipBanner.js
+++ src/common/ZulipBanner.js
@@ -10,8 +10,24 @@ import type { LocalizableText } from '../types';
 import Label from './Label';
 import ZulipTextButton from './ZulipTextButton';
 
+// `textRow` and `buttonsRow` are named for the more common case where
+// there's not enough room for the text and the button(s) to share a single
+// row.
+//
+// But we do support having the text and button(s) share a row if there's
+// room, i.e., if the entities' combined widths are less than or equal to
+// one row width. The spec gives an example of this in an illustration where
+// the text just takes one line and there's only one action button.
+//
+// TODO(?): The vertical centering logic for when the text and buttons share
+//   a row isn't pixel-perfect; it depends on explicit padding/margin values
+//   instead of declaring "center" somewhere. This is an intentional
+//   compromise to reduce complexity while still supporting the two-row
+//   layout. If we find an elegant solution, we should use it.
 const styles = createStyleSheet({
   wrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     backgroundColor: HALF_COLOR,
     paddingLeft: 16,
     paddingRight: 8,
@@ -19,6 +35,7 @@ const styles = createStyleSheet({
     paddingTop: 10,
   },
   textRow: {
+    flexGrow: 1,
     flexDirection: 'row',
     marginBottom: 12,
   },
@@ -27,6 +44,7 @@ const styles = createStyleSheet({
     lineHeight: 20,
   },
   buttonsRow: {
+    flexGrow: 1,
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
