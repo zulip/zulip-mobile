04bbee3cd3792e5b46ee70ad6515230b15cc6a2d
Chris Bobbe <cbobbe@zulip.com>
2021-08-27 16:23:26 -0700

    navSelectors types [nfc]: Add a quick annotation, for types-first.
    Related: #4907

--- src/nav/navSelectors.js
+++ src/nav/navSelectors.js
@@ -1,5 +1,5 @@
 /* @flow strict-local */
-import type { NavigationState, Route } from '@react-navigation/native';
+import type { NavigationState, Route, ScreenParams } from '@react-navigation/native';
 
 import * as NavigationService from './NavigationService';
 
@@ -11,13 +11,14 @@ const getNavigationIndex = () => getNavState().index;
 
 const getCurrentRoute = (): void | Route<string> => getNavigationRoutes()[getNavigationIndex()];
 
-export const getCurrentRouteName = () => getCurrentRoute()?.name;
+export const getCurrentRouteName = (): void | string => getCurrentRoute()?.name;
 
-export const getCurrentRouteParams = () => getCurrentRoute()?.params;
+export const getCurrentRouteParams = (): void | ScreenParams => getCurrentRoute()?.params;
 
-export const getChatScreenParams = () => getCurrentRouteParams() ?? { narrow: undefined };
+export const getChatScreenParams = (): ScreenParams | {| narrow: void |} =>
+  getCurrentRouteParams() ?? { narrow: undefined };
 
-export const getSameRoutesCount = () => {
+export const getSameRoutesCount = (): number => {
   const routes = getNavigationRoutes();
   let i = routes.length - 1;
   while (i >= 0) {
