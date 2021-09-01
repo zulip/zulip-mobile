181d7f55072ab2191b2b2907ba520e6ddc6f1593
Chris Bobbe <cbobbe@zulip.com>
2021-08-31 17:46:04 -0700

    flow: Add missing error codes in some new suppressions.

--- src/__flow-tests__/reactUtils-test.js
+++ src/__flow-tests__/reactUtils-test.js
@@ -11,26 +11,26 @@ function test_usePrevious() {
     // In particular, make sure the type knows the return value can be void.
     // (This is the case that would break with one natural-but-wrong way to
     // write the definition.)
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-cast]
     (usePrevious(b): boolean);
 
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-cast]
     (usePrevious(b): void);
   }
 
   // With initial value, returns union type:
   {
     (usePrevious(b, null): boolean | null);
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-cast]
     (usePrevious(b, null): boolean);
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-cast]
     (usePrevious(b, null): null);
   }
 
   // With initial value of type T, returns T:
   {
     (usePrevious(b, false): boolean);
-    // $FlowExpectedError
+    // $FlowExpectedError[incompatible-cast]
     (usePrevious(b, false): false);
   }
 }
