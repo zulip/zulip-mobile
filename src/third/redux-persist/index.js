7ef8c5ba839cea107482f382078287cf2792f305
Greg Price <greg@zulipchat.com>
2021-10-04 21:21:26 -0700

    persist [nfc]: Drop unused re-exports of getStoredState, purgeStoredState
    These are used internally to the implementation of this subsystem,
    but don't really belong to its external API.

--- src/third/redux-persist/index.js
+++ src/third/redux-persist/index.js
@@ -2,10 +2,8 @@
 
 import autoRehydrate from './autoRehydrate';
 import createPersistor from './createPersistor';
-import getStoredState from './getStoredState';
 import persistStore from './persistStore';
-import purgeStoredState from './purgeStoredState';
 
 export type { Persistor, Config } from './types';
 
-export { autoRehydrate, createPersistor, getStoredState, persistStore, purgeStoredState };
+export { autoRehydrate, createPersistor, persistStore };
