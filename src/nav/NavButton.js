cb3bf790a05cb866cc04809d6f6bde4c3098c7bd
Greg Price <greg@zulip.com>
2021-10-06 15:05:08 -0700

    app bar [nfc]: Consolidate NavButton and NavButtonGeneral
    These are really best understood as two entry points to the same
    little subsystem.  In particular, NavButtonGeneral implicitly assumes
    that the children you pass it are very much like what the NavButton
    implementation passes.

--- src/nav/NavButton.js
+++ src/nav/NavButton.js
@@ -1,18 +1,41 @@
 /* @flow strict-local */
 import React from 'react';
 import type { Node } from 'react';
+import { View } from 'react-native';
 import type { TextStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
 
-import { BRAND_COLOR, createStyleSheet } from '../styles';
+import { BRAND_COLOR, createStyleSheet, NAVBAR_SIZE } from '../styles';
 import { Icon } from '../common/Icons';
 import type { IconNames } from '../common/Icons';
-import NavButtonGeneral from './NavButtonGeneral';
+import { Touchable } from '../common';
 
 const componentStyles = createStyleSheet({
+  navButtonFrame: {
+    width: NAVBAR_SIZE,
+    height: NAVBAR_SIZE,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   navButtonIcon: {
     textAlign: 'center',
   },
 });
+
+export function NavButtonGeneral(
+  props: $ReadOnly<{|
+    children: Node,
+    onPress: () => void,
+    accessibilityLabel?: string,
+  |}>,
+): Node {
+  const { children, onPress, accessibilityLabel } = props;
+
+  return (
+    <Touchable onPress={onPress} accessibilityLabel={accessibilityLabel}>
+      <View style={componentStyles.navButtonFrame}>{children}</View>
+    </Touchable>
+  );
+}
 
 export default function NavButton(
   props: $ReadOnly<{|
