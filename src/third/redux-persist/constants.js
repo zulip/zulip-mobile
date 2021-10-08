bad62664ee0a394f47c0308fa785f652c4526e5e
Chris Bobbe <cbobbe@zulip.com>
2021-10-08 16:11:42 -0700

    persist types: Annotate exports of redux-persist/constants

--- src/third/redux-persist/constants.js
+++ src/third/redux-persist/constants.js
@@ -1,2 +1,4 @@
-export const KEY_PREFIX = 'reduxPersist:';
-export const REHYDRATE = 'persist/REHYDRATE';
+/* @flow strict-local */
+
+export const KEY_PREFIX: string = 'reduxPersist:';
+export const REHYDRATE: string = 'persist/REHYDRATE';
