d0964f132abead523ad3adebaf4c25cd2e9a220d
Greg Price <greg@zulipchat.com>
2021-10-12 14:53:23 -0700

    typeaheads:Make typeaheads more visible by adding white overlay

--- src/common/Popup.js
+++ src/common/Popup.js
@@ -4,6 +4,8 @@ import type { Node } from 'react';
 import { View } from 'react-native';
 
 import { ThemeContext, createStyleSheet } from '../styles';
+import { useSelector } from '../react-redux';
+import { getGlobalSettings } from '../directSelectors';
 
 const styles = createStyleSheet({
   popup: {
@@ -12,8 +14,18 @@ const styles = createStyleSheet({
     borderRadius: 5,
     shadowOpacity: 0.5,
     shadowRadius: 16,
-    elevation: 3,
+    elevation: 8,
     maxHeight: 250,
+    overflow: 'hidden',
+  },
+
+  overlay: {
+    /*
+     * Adding background color white with 12% overlay as specified in Material.io
+     * Reference https://material.io/design/color/dark-theme.html#properties.
+     * For more information
+     */
+    backgroundColor: 'hsla(0,0%,100%, 0.12)',
   },
 });
 
@@ -23,9 +35,11 @@ type Props = $ReadOnly<{|
 
 export default function Popup(props: Props): Node {
   const themeContext = useContext(ThemeContext);
+  // Getting the active theme to set, the overlay only to the night mode.
+  const theme = useSelector(state => getGlobalSettings(state).theme);
   return (
     <View style={[{ backgroundColor: themeContext.backgroundColor }, styles.popup]}>
-      {props.children}
+      <View style={theme !== 'default' && styles.overlay}>{props.children}</View>
     </View>
   );
 }
