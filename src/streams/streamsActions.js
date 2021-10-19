8871d60718ee4e3e44c3549aab52a0a1f475016b
Greg Price <greg@zulipchat.com>
2021-10-18 21:15:34 -0700

    api: Adapt to API change in update-stream, conditionally
    Until server feature level 64 earlier this year, the server API
    expected these two string parameters to be JSON-encodings of the
    desired strings, rather than simply the desired strings.  So the
    `JSON.stringify` calls here were required for correct behavior
    (even though the comments' reasoning, about "unsafe characters",
    does not inspire confidence.)
    
    But then the API was cleaned up to simply take the strings
    directly, and broke compatibility as a result of accidentally
    not correctly checking whether the mobile app was using this
    feature.  This causes an edited stream name or description
    to get the JSON encoding of the desired value (in particular,
    to always get enclosed in double-quotes "…"), instead of the
    actual desired value: #4747.
    
    Fix that, conditioning on the server's feature level so that we
    continue to behave correctly on older servers as well as new ones.
    
    This includes the changes from #4748, which switched unconditionally
    to the new API.
    
    Includes-changes-by: Gautam Arora <gautamarora6248@gmail.com>
    Fixes: #4747

--- src/streams/streamsActions.js
+++ src/streams/streamsActions.js
@@ -1,7 +1,7 @@
 /* @flow strict-local */
 import type { Stream, ThunkAction } from '../types';
 import * as api from '../api';
-import { getAuth } from '../selectors';
+import { getAuth, getZulipFeatureLevel } from '../selectors';
 
 export const createNewStream = (
   name: string,
@@ -17,20 +17,23 @@ export const updateExistingStream = (
   initialValues: Stream,
   newValues: {| name: string, description: string, isPrivate: boolean |},
 ): ThunkAction<Promise<void>> => async (dispatch, getState) => {
+  const state = getState();
+
+  const maybeEncode = (value: string): string =>
+    // Adapt to a server API change that was accidentally incompatible:
+    //   https://github.com/zulip/zulip-mobile/pull/4748#issuecomment-852254404
+    //   https://github.com/zulip/zulip-mobile/issues/4747#issuecomment-946362729
+    // TODO(#4659): Ideally this belongs inside `api.updateStream`.
+    getZulipFeatureLevel(state) >= 64 ? value : JSON.stringify(value);
+
+  const auth = getAuth(state);
   if (initialValues.name !== newValues.name) {
-    // Stream names might contain unsafe characters so we must encode it first.
-    await api.updateStream(getAuth(getState()), id, 'new_name', JSON.stringify(newValues.name));
+    await api.updateStream(auth, id, 'new_name', maybeEncode(newValues.name));
   }
   if (initialValues.description !== newValues.description) {
-    // Description might contain unsafe characters so we must encode it first.
-    await api.updateStream(
-      getAuth(getState()),
-      id,
-      'description',
-      JSON.stringify(newValues.description),
-    );
+    await api.updateStream(auth, id, 'description', maybeEncode(newValues.description));
   }
   if (initialValues.invite_only !== newValues.isPrivate) {
-    await api.updateStream(getAuth(getState()), id, 'is_private', newValues.isPrivate);
+    await api.updateStream(auth, id, 'is_private', newValues.isPrivate);
   }
 };
