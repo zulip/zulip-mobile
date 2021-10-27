6180479493ebaa0c03cf0b222ba9e1c435036d64
Chris Bobbe <cbobbe@zulip.com>
2021-10-26 19:24:29 -0700

    deleteMessage [nfc]: Add TODO for #4701

--- src/api/messages/deleteMessage.js
+++ src/api/messages/deleteMessage.js
@@ -2,6 +2,12 @@
 import type { ApiResponse, Auth } from '../transportTypes';
 import { apiPatch } from '../apiFetch';
 
+// TODO(#4701): Make an API method for DELETE /messages/{id}. Use it to
+// implement the permanent message deletion capability:
+//   https://zulip.com/help/configure-message-editing-and-deletion
+// To do so, we'll need input from realm_delete_own_message_policy,
+// realm_message_content_delete_limit_seconds, the user's role, the current
+// time, and maybe more; we'll want #3898 if we can.
 export default async (auth: Auth, id: number): Promise<ApiResponse> =>
   apiPatch(auth, `messages/${id}`, {
     content: '',
