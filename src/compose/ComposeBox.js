2c3f9a23eb4cfa8e0a3dc97ca5352b4c625a6f28
Chris Bobbe <cbobbe@zulip.com>
2021-11-02 16:50:34 -0700

    compose: Stop suggesting sentence case for topic input

--- src/compose/ComposeBox.js
+++ src/compose/ComposeBox.js
@@ -571,6 +571,7 @@ class ComposeBoxInner extends PureComponent<Props, State> {
                 // work.
                 !this.getCanSelectTopic() && { position: 'absolute', transform: [{ scale: 0 }] },
               ]}
+              autoCapitalize="none"
               underlineColorAndroid="transparent"
               placeholder="Topic"
               defaultValue={topic}
