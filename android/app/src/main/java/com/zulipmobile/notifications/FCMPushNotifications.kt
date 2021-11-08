ee5d1e097f3cbf4ac50f4e88f2cdfd545af61b18
Akash Dhiman <akash.d0407@gmail.com>
2021-11-09 01:15:02 +0530

    android [nfc]: Add `brandColor` to `color.xml`.

--- android/app/src/main/java/com/zulipmobile/notifications/FCMPushNotifications.kt
+++ android/app/src/main/java/com/zulipmobile/notifications/FCMPushNotifications.kt
@@ -9,7 +9,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -158,8 +157,7 @@ private fun getNotificationBuilder(
         builder.setSmallIcon(R.drawable.zulip_notification)
     }
 
-    // This should agree with `BRAND_COLOR` in the JS code.
-    builder.setColor(Color.rgb(100, 146, 254))
+    builder.color = context.getColor(R.color.brandColor)
 
     val nameList = extractNames(conversations)
 
