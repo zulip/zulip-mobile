476f3a79fcedf04c06d48c0334139a5a8eca6c8d
Chris Bobbe <cbobbe@zulip.com>
2021-11-15 15:00:46 -0800

    android notif [nfc]: Remove a redundant `let`
    Looks like this got left behind in 9620629a7, as Greg explains at
      https://github.com/zulip/zulip-mobile/pull/5108/files#r744036460.

--- android/app/src/main/java/com/zulipmobile/notifications/FcmMessage.kt
+++ android/app/src/main/java/com/zulipmobile/notifications/FcmMessage.kt
@@ -111,7 +111,7 @@ data class MessageFcmMessage(
      */
     fun dataForOpen(): Bundle = Bundle().apply {
         // NOTE: Keep the JS-side type definition in sync with this code.
-        identity.realmUri.let { putString("realm_uri", it.toString()) }
+        putString("realm_uri", identity.realmUri.toString())
         when (recipient) {
             is Recipient.Stream -> {
                 putString("recipient_type", "stream")
