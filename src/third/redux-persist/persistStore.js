19c0b4b44b7a856d9b200de71b99f1323f3be3d3
Greg Price <greg@zulipchat.com>
2021-11-29 16:29:54 -0800

    persist [nfc]: Drop skipRestore option, long deprecated
    We've never used this option ourselves at all.

--- src/third/redux-persist/persistStore.js
+++ src/third/redux-persist/persistStore.js
@@ -1,4 +1,3 @@
-import * as logging from '../../utils/logging';
 import { cacheKeys } from '../../boot/store';
 
 import { REHYDRATE } from './constants';
@@ -6,15 +5,6 @@ import getStoredState from './getStoredState';
 import createPersistor from './createPersistor';
 
 export default function persistStore(store, config = {}, onComplete) {
-  // defaults
-  // @TODO remove shouldRestore
-  const shouldRestore = !config.skipRestore;
-  if (config.skipRestore) {
-    logging.warn(
-      'redux-persist: config.skipRestore has been deprecated. If you want to skip restoration use `createPersistor` instead',
-    );
-  }
-
   let purgeKeys = null;
 
   // create and pause persistor
@@ -22,94 +12,90 @@ export default function persistStore(store, config = {}, onComplete) {
   persistor.pause();
 
   // restore
-  if (shouldRestore) {
-    setImmediate(() => {
-      getStoredState(config, async (err, restoredState) => {
-        if (err) {
-          complete(err);
-          return;
+  setImmediate(() => {
+    getStoredState(config, async (err, restoredState) => {
+      if (err) {
+        complete(err);
+        return;
+      }
+      // do not persist state for purgeKeys
+      if (purgeKeys) {
+        if (purgeKeys === '*') {
+          restoredState = {};
+        } else {
+          purgeKeys.forEach(key => delete restoredState[key]);
         }
-        // do not persist state for purgeKeys
-        if (purgeKeys) {
-          if (purgeKeys === '*') {
-            restoredState = {};
-          } else {
-            purgeKeys.forEach(key => delete restoredState[key]);
-          }
+      }
+      try {
+        // The version (in redux-persist-migrate's terms) that was
+        // current in the previous session.
+        //
+        // Caution: this same expression would give a different
+        // value if it were run after the `store.dispatch` line,
+        // because redux-persist-migrate's store enhancer
+        // `createMigration` mutates `migrations.version` in the
+        // action's payload (see `realVersionSetter` in
+        // redux-persist-migrate).
+        const prevVersion = restoredState.migrations?.version;
+
+        if (prevVersion == null || prevVersion < 24) {
+          // Super-powered `dropCache` to clear out corrupted
+          // (because not-fully-migrated) data from #4458. TODO: A
+          // proper, non-hacky fix, as Greg describes at
+          //   https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/.23M4458.3A.20.22t.2Eget.20is.20not.20a.20function.22.20on.20state.2Enarrows.20at.20sta.2E.2E.2E/near/1119541.
+          await persistor.purge(cacheKeys);
         }
-        try {
-          // The version (in redux-persist-migrate's terms) that was
-          // current in the previous session.
+
+        store.dispatch(rehydrateAction(restoredState, err));
+
+        // The version (in redux-persist-migrate's terms) that is
+        // current now, after rehydration.
+        const currentVersion = store.getState().migrations.version;
+
+        if (prevVersion !== undefined && prevVersion === currentVersion) {
+          // Don't persist `REHYDRATE`'s payload unnecessarily.
           //
-          // Caution: this same expression would give a different
-          // value if it were run after the `store.dispatch` line,
-          // because redux-persist-migrate's store enhancer
-          // `createMigration` mutates `migrations.version` in the
-          // action's payload (see `realVersionSetter` in
-          // redux-persist-migrate).
-          const prevVersion = restoredState.migrations?.version;
-
-          if (prevVersion == null || prevVersion < 24) {
-            // Super-powered `dropCache` to clear out corrupted
-            // (because not-fully-migrated) data from #4458. TODO: A
-            // proper, non-hacky fix, as Greg describes at
-            //   https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/.23M4458.3A.20.22t.2Eget.20is.20not.20a.20function.22.20on.20state.2Enarrows.20at.20sta.2E.2E.2E/near/1119541.
-            await persistor.purge(cacheKeys);
-          }
-
-          store.dispatch(rehydrateAction(restoredState, err));
-
-          // The version (in redux-persist-migrate's terms) that is
-          // current now, after rehydration.
-          const currentVersion = store.getState().migrations.version;
-
-          if (prevVersion !== undefined && prevVersion === currentVersion) {
-            // Don't persist `REHYDRATE`'s payload unnecessarily.
-            //
-            // The state in memory now (after `REHYDRATE` has fired)
-            // contains no useful information beyond what has already
-            // been saved to storage, so we can skip saving it back to
-            // storage. That's because:
-            //
-            // (a) The state in memory was empty before `REHYDRATE`
-            //     fired. There wasn't anything interesting there that
-            //     was merged with `REHYDRATE`'s payload. And,
-            //
-            // (b) The `REHYDRATE` payload itself came straight (via
-            //     our reviver) from what was saved to storage. It
-            //     would only have changed if at least one migration
-            //     had run, and it hasn't. In this conditional, we
-            //     know that it hasn't because the previous version
-            //     (in redux-persist-migrate's terms) is the same as
-            //     the current version.
-            //
-            // Part of the work for preventing the save is already
-            // done: `.pause()` is called on `persistor` above, and
-            // `.resume()` is called after. This does mean that
-            // persisting `REHYDRATE`'s payload isn't triggered
-            // directly on `REHYDRATE`. However, it is triggered on a
-            // *subsequent* action, because, upon each action, the
-            // persistor compares a piece of `lastState` to the
-            // corresponding piece of `state` to check whether that
-            // piece needs to be persisted -- and, on an action just
-            // after `REHYDRATE`, `lastState` is stale, containing the
-            // pre-`REHYDRATE` state. That's because `lastState`
-            // doesn't naturally update when the persistor is paused.
-            //
-            // So, fix that by still resetting `lastState` with the
-            // result of `REHYDRATE` when the persistor is paused; we
-            // can do that because we've exposed `_resetLastWrittenState` on
-            // the persistor.
-            persistor._resetLastWrittenState(); // eslint-disable-line no-underscore-dangle
-          }
-        } finally {
-          complete(err, restoredState);
+          // The state in memory now (after `REHYDRATE` has fired)
+          // contains no useful information beyond what has already
+          // been saved to storage, so we can skip saving it back to
+          // storage. That's because:
+          //
+          // (a) The state in memory was empty before `REHYDRATE`
+          //     fired. There wasn't anything interesting there that
+          //     was merged with `REHYDRATE`'s payload. And,
+          //
+          // (b) The `REHYDRATE` payload itself came straight (via
+          //     our reviver) from what was saved to storage. It
+          //     would only have changed if at least one migration
+          //     had run, and it hasn't. In this conditional, we
+          //     know that it hasn't because the previous version
+          //     (in redux-persist-migrate's terms) is the same as
+          //     the current version.
+          //
+          // Part of the work for preventing the save is already
+          // done: `.pause()` is called on `persistor` above, and
+          // `.resume()` is called after. This does mean that
+          // persisting `REHYDRATE`'s payload isn't triggered
+          // directly on `REHYDRATE`. However, it is triggered on a
+          // *subsequent* action, because, upon each action, the
+          // persistor compares a piece of `lastState` to the
+          // corresponding piece of `state` to check whether that
+          // piece needs to be persisted -- and, on an action just
+          // after `REHYDRATE`, `lastState` is stale, containing the
+          // pre-`REHYDRATE` state. That's because `lastState`
+          // doesn't naturally update when the persistor is paused.
+          //
+          // So, fix that by still resetting `lastState` with the
+          // result of `REHYDRATE` when the persistor is paused; we
+          // can do that because we've exposed `_resetLastWrittenState` on
+          // the persistor.
+          persistor._resetLastWrittenState(); // eslint-disable-line no-underscore-dangle
         }
-      });
+      } finally {
+        complete(err, restoredState);
+      }
     });
-  } else {
-    setImmediate(complete);
-  }
+  });
 
   function complete(err, restoredState) {
     persistor.resume();
