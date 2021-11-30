124dea44ce7a19d2c5a46d81b1c04979dcfd014f
Greg Price <greg@zulipchat.com>
2021-11-29 19:14:52 -0800

    persist types: Cut .js.flow; type-check implementation directly!

--- src/third/redux-persist/index.js
+++ src/third/redux-persist/index.js
@@ -1,7 +1,11 @@
+// @flow strict-local
+
 import autoRehydrate from './autoRehydrate';
 import createPersistor from './createPersistor';
 import getStoredState from './getStoredState';
 import persistStore from './persistStore';
 import purgeStoredState from './purgeStoredState';
+
+export type { Persistor, Config } from './types';
 
 export { autoRehydrate, createPersistor, getStoredState, persistStore, purgeStoredState };
