76bd8119945e5c016d795d969da2b077214d6b2d
Greg Price <greg@zulipchat.com>
2021-12-01 13:28:03 -0800

    persist [nfc]: Rearrange persistStore a bit
    This reorders the source code, but preserves the order things
    actually happen in.  Hopefully it makes that control flow easier
    to see: we create the persistor, set an immediate task to restore
    to it, and promptly return.  All that other code runs later.
    
    Also add a couple of blank lines to avoid making things look
    grouped together that logically aren't.

--- src/third/redux-persist/persistStore.js
+++ src/third/redux-persist/persistStore.js
@@ -21,12 +21,20 @@ export default function persistStore<
 ): Persistor {
   let purgeKeys = null;
 
-  // create and pause persistor
   const persistor = createPersistor(store, config);
   persistor.pause();
 
-  // restore
-  setImmediate(async () => {
+  setImmediate(restore);
+
+  return {
+    ...persistor,
+    purge: keys => {
+      purgeKeys = keys || '*';
+      return persistor.purge(keys);
+    },
+  };
+
+  async function restore() {
     try {
       let restoredState: { ... } = await getStoredState(config);
 
@@ -38,6 +46,7 @@ export default function persistStore<
           purgeKeys.forEach(key => delete restoredState[key]);
         }
       }
+
       // The version (in redux-persist-migrate's terms) that was
       // current in the previous session.
       //
@@ -53,8 +62,8 @@ export default function persistStore<
 
       if (typeof prevVersion !== 'number' || prevVersion < 24) {
         // Super-powered `dropCache` to clear out corrupted
-        // (because not-fully-migrated) data from #4458. TODO: A
-        // proper, non-hacky fix, as Greg describes at
+        // (because not-fully-migrated) data from #4458.
+        // TODO: A proper, non-hacky fix, as Greg describes at
         //   https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/.23M4458.3A.20.22t.2Eget.20is.20not.20a.20function.22.20on.20state.2Enarrows.20at.20sta.2E.2E.2E/near/1119541.
         await persistor.purge(cacheKeys);
       }
@@ -107,48 +116,37 @@ export default function persistStore<
         persistor._resetLastWrittenState(); // eslint-disable-line no-underscore-dangle
       }
     } finally {
-      complete();
-    }
-  });
+      persistor.resume();
 
-  function complete() {
-    persistor.resume();
+      // As mentioned in a comment above, the current strategy of using
+      // `.pause()` and `.resume()` means that the time at which the
+      // `REHYDRATE` payload is persisted (if we've asked it to be
+      // persisted; i.e., if a migration has been done) is not during
+      // the processing of `REHYDRATE` itself, but rather during the
+      // processing of the action that immediately follows it. Rather
+      // than postponing the important task of persisting a migration's
+      // results until some arbitrary next action fired somewhere in the
+      // app, fire a next action right here, right now.
+      //
+      // TODO: Find a cleaner way of handling this.
+      store.dispatch(
+        ({
+          // Include a random string, to be sure no other action type
+          // matches this one. Like Redux does for the initial action.
+          type: `PERSIST_DUMMY/${Math.floor(Math.random() * 2 ** 54).toString(36)}`,
 
-    // As mentioned in a comment above, the current strategy of using
-    // `.pause()` and `.resume()` means that the time at which the
-    // `REHYDRATE` payload is persisted (if we've asked it to be
-    // persisted; i.e., if a migration has been done) is not during
-    // the processing of `REHYDRATE` itself, but rather during the
-    // processing of the action that immediately follows it. Rather
-    // than postponing the important task of persisting a migration's
-    // results until some arbitrary next action fired somewhere in the
-    // app, fire a next action right here, right now.
-    //
-    // TODO: Find a cleaner way of handling this.
-    store.dispatch(
-      ({
-        // Include a random string, to be sure no other action type
-        // matches this one. Like Redux does for the initial action.
-        type: `PERSIST_DUMMY/${Math.floor(Math.random() * 2 ** 54).toString(36)}`,
+          // The intended param type for `dispatch` would be a union where one
+          // branch is "any other `type`".  There isn't a way to specify that
+          // without losing the information of what the type looks like for
+          // known `type`.  So just ignore that case.
+        }: $FlowFixMe),
+      );
 
-        // The intended param type for `dispatch` would be a union where one
-        // branch is "any other `type`".  There isn't a way to specify that
-        // without losing the information of what the type looks like for
-        // known `type`.  So just ignore that case.
-      }: $FlowFixMe),
-    );
-    if (onComplete) {
-      onComplete();
+      if (onComplete) {
+        onComplete();
+      }
     }
   }
-
-  return {
-    ...persistor,
-    purge: keys => {
-      purgeKeys = keys || '*';
-      return persistor.purge(keys);
-    },
-  };
 }
 
 function rehydrateAction(payload) {
