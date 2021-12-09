165b8d03e63c9d6e2df16427d1fc73190088065c
Greg Price <greg@zulipchat.com>
2021-12-08 21:58:55 -0800

    complete a reasonable minimal AsyncStorage test suite

--- src/storage/__tests__/AsyncStorage-test.js
+++ src/storage/__tests__/AsyncStorage-test.js
@@ -31,14 +31,51 @@ describe('expo-sqlite', () => {
 });
 
 describe('AsyncStorage', () => {
-  test('getItem settles', async () => {
-    await AsyncStorage.getItem('a');
-    expect(true).toBeTruthy();
+  afterEach(async () => {
+    await AsyncStorage.clear();
   });
 
-  test('set/get round-trips', async () => {
-    await AsyncStorage.setItem('a', 'b');
-    const result = await AsyncStorage.getItem('a');
-    expect(result).toEqual('b');
+  test('getItem / setItem', async () => {
+    expect(await AsyncStorage.getItem('a')).toEqual(null);
+
+    await AsyncStorage.setItem('a', '1');
+    expect(await AsyncStorage.getItem('a')).toEqual('1');
+
+    await AsyncStorage.setItem('a', '2');
+    expect(await AsyncStorage.getItem('a')).toEqual('2');
+
+    expect(await AsyncStorage.getItem('b')).toEqual(null);
+  });
+
+  test('multiSet', async () => {
+    await AsyncStorage.multiSet([
+      ['a', '1'],
+      ['b', '2'],
+    ]);
+    expect(await AsyncStorage.getItem('a')).toEqual('1');
+    expect(await AsyncStorage.getItem('b')).toEqual('2');
+  });
+
+  test('removeItem', async () => {
+    await AsyncStorage.setItem('a', '1');
+    expect(await AsyncStorage.getItem('a')).toEqual('1');
+    await AsyncStorage.removeItem('a');
+    expect(await AsyncStorage.getItem('a')).toEqual(null);
+    await AsyncStorage.removeItem('a');
+    expect(await AsyncStorage.getItem('a')).toEqual(null);
+  });
+
+  test('getAllKeys', async () => {
+    await AsyncStorage.setItem('a', '1');
+    await AsyncStorage.setItem('b', '2');
+    expect(await AsyncStorage.getAllKeys()).toEqual(['a', 'b']);
+  });
+
+  test('clear', async () => {
+    await AsyncStorage.setItem('a', '1');
+    await AsyncStorage.setItem('b', '2');
+    expect(await AsyncStorage.getAllKeys()).toEqual(['a', 'b']);
+    await AsyncStorage.clear();
+    expect(await AsyncStorage.getAllKeys()).toEqual([]);
   });
 });
