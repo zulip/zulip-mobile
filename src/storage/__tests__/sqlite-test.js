6634190f8d32dc5c7a9d48bc4fdd044b78a1e85a
Greg Price <greg@zulipchat.com>
2021-12-08 17:08:20 -0800

    sqlite3: Write a quick smoke-test to show it's working in Jest

---
+++ src/storage/__tests__/sqlite-test.js
@@ -0,0 +1,13 @@
+// @flow strict-local
+
+import sqlite3 from 'sqlite3';
+
+describe('sqlite3', () => {
+  test('smoke', async () => {
+    const db = new sqlite3.Database(':memory:');
+    const result = await new Promise((resolve, reject) => {
+      db.get('SELECT 42 as n', (err, row) => (err ? reject(err) : resolve(row)));
+    });
+    expect(result).toEqual({ n: 42 });
+  });
+});
