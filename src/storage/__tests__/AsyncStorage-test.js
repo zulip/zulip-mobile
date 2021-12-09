41e6a42e9b8b0fcb47cbf52c234b6d5c1a5b7f07
Greg Price <greg@zulipchat.com>
2021-12-08 21:38:20 -0800

    reorder tests

--- src/storage/__tests__/AsyncStorage-test.js
+++ src/storage/__tests__/AsyncStorage-test.js
@@ -14,19 +14,6 @@ describe('sqlite3', () => {
   });
 });
 
-describe('AsyncStorage', () => {
-  test('getItem settles', async () => {
-    await AsyncStorage.getItem('a');
-    expect(true).toBeTruthy();
-  });
-
-  test('set/get round-trips', async () => {
-    await AsyncStorage.setItem('a', 'b');
-    const result = await AsyncStorage.getItem('a');
-    expect(result).toEqual('b');
-  });
-});
-
 describe('expo-sqlite', () => {
   test('smoke', async () => {
     const db = openDatabase('test.db');
@@ -40,5 +27,18 @@ describe('expo-sqlite', () => {
       );
     });
     expect(result.rows._array).toEqual([{ n: 42 }]);
+  });
+});
+
+describe('AsyncStorage', () => {
+  test('getItem settles', async () => {
+    await AsyncStorage.getItem('a');
+    expect(true).toBeTruthy();
+  });
+
+  test('set/get round-trips', async () => {
+    await AsyncStorage.setItem('a', 'b');
+    const result = await AsyncStorage.getItem('a');
+    expect(result).toEqual('b');
   });
 });
