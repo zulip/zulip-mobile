bf77f3c02eb081c970fee871de506938e18e2c6f
Greg Price <greg@zulipchat.com>
2021-12-08 16:00:10 -0800

    wip test expo-sqlite directly

--- src/storage/__tests__/AsyncStorage-test.js
+++ src/storage/__tests__/AsyncStorage-test.js
@@ -1,11 +1,33 @@
 // @flow strict-local
 
+import { openDatabase } from 'expo-sqlite';
 import { AsyncStorage } from '../AsyncStorage';
 
 describe('AsyncStorage', () => {
-  test('set/get round-trips', async () => {
-    await AsyncStorage.setItem('a', 'b');
-    const result = await AsyncStorage.getItem('a');
-    expect(result).toEqual('b');
+  test('getItem settles', async () => {
+    // await AsyncStorage.getItem('a');
+    expect(true).toBeTruthy();
+  });
+
+  //   test('set/get round-trips', async () => {
+  //     await AsyncStorage.setItem('a', 'b');
+  //     const result = await AsyncStorage.getItem('a');
+  //     expect(result).toEqual('b');
+  //   });
+});
+
+describe('expo-sqlite', () => {
+  test('smoke', async () => {
+    const db = openDatabase('test.db');
+    await new Promise((resolve, reject) => {
+      db.readTransaction(
+        tx => {
+          tx.executeSql('SELECT 1');
+        },
+        reject,
+        resolve,
+      );
+    });
+    expect(true).toBeTruthy();
   });
 });
