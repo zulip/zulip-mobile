f6033f1d0116101c94c7ffb6064b74fc84d5748c
Chris Bobbe <cbobbe@zulip.com>
2021-12-14 14:49:39 -0800

    openLink [nfc]: Add TODOs for functions to take URL objects, not strings

--- src/utils/openLink.js
+++ src/utils/openLink.js
@@ -5,6 +5,7 @@ import * as WebBrowser from 'expo-web-browser';
 import type { BrowserPreference, GlobalSettingsState } from '../types';
 
 /** Open a URL in the in-app browser. */
+// TODO(#4146): Take a URL object, not a string.
 export function openLinkEmbedded(url: string): void {
   if (Platform.OS === 'ios') {
     WebBrowser.openBrowserAsync(encodeURI(url));
@@ -14,6 +15,7 @@ export function openLinkEmbedded(url: string): void {
 }
 
 /** Open a URL in the user's default browser app. */
+// TODO(#4146): Take a URL object, not a string.
 export function openLinkExternal(url: string): void {
   Linking.openURL(url);
 }
@@ -27,6 +29,7 @@ export function shouldUseInAppBrowser(browser: BrowserPreference): boolean {
 }
 
 /** Open a URL using whichever browser the user has configured in the Zulip settings. */
+// TODO(#4146): Take a URL object, not a string.
 export function openLinkWithUserPreference(url: string, settings: GlobalSettingsState): void {
   if (shouldUseInAppBrowser(settings.browser)) {
     openLinkEmbedded(url);
