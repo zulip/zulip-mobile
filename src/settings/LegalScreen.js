084cce8b7ee1ac1fdb0fa3b3a8ba6edbc97b9d71
Chris Bobbe <cbobbe@zulip.com>
2021-12-14 18:46:18 -0800

    legal: Link to Zulip's terms and terms for the active realm

--- src/settings/LegalScreen.js
+++ src/settings/LegalScreen.js
@@ -6,9 +6,9 @@ import type { Node } from 'react';
 import type { RouteProp } from '../react-navigation';
 import type { AppNavigationProp } from '../nav/AppNavigator';
 import { useSelector } from '../react-redux';
-import { Screen, NestedNavRow } from '../common';
+import { Screen, NestedNavRow, RawLabel } from '../common';
 import { openLinkEmbedded } from '../utils/openLink';
-import { getRealmUrl } from '../selectors';
+import { getRealmUrl, getRealmName } from '../selectors';
 
 type Props = $ReadOnly<{|
   navigation: AppNavigationProp<'legal'>,
@@ -18,19 +18,36 @@ type Props = $ReadOnly<{|
 /** (NB this is a per-account screen: it leads to this realm's policies.) */
 export default function LegalScreen(props: Props): Node {
   const realm = useSelector(getRealmUrl);
+  const realmName = useSelector(getRealmName);
 
-  const openTermsOfService = useCallback(() => {
-    openLinkEmbedded(new URL('/terms/?nav=no', realm).toString());
-  }, [realm]);
+  const openZulipPolicies = useCallback(() => {
+    openLinkEmbedded('https://zulip.com/policies/?nav=no');
+  }, []);
 
-  const openPrivacyPolicy = useCallback(() => {
-    openLinkEmbedded(new URL('/privacy/?nav=no', realm).toString());
+  const openRealmPolicies = useCallback(() => {
+    openLinkEmbedded(new URL('/policies/?nav=no', realm).toString());
   }, [realm]);
 
   return (
     <Screen title="Legal">
-      <NestedNavRow label="Terms of service" onPress={openTermsOfService} />
-      <NestedNavRow label="Privacy policy" onPress={openPrivacyPolicy} />
+      <NestedNavRow label="Zulip terms" onPress={openZulipPolicies} />
+      <NestedNavRow
+        // These are really terms set by the server admin responsible for
+        // hosting the org, and that server admin may or may not represent
+        // the org itself, as this text might be read to imply. (E.g.,
+        // on Zulip Cloud they don't.) But:
+        // - We don't want to complicate the wording. Not everyone knows
+        //   what a server is.
+        // - These terms will often differ from Zulip's own terms (the ones
+        //   at the other link).
+        // - These terms will apply to all users in the org, in all cases.
+        //   We should link to them.
+        label={{
+          text: 'Terms for {realmName}',
+          values: { realmName: <RawLabel style={{ fontWeight: 'bold' }} text={realmName} /> },
+        }}
+        onPress={openRealmPolicies}
+      />
     </Screen>
   );
 }
