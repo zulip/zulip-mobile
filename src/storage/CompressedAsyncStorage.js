f3564bfb465e7c86af9b014e11b8916386dbb0b7
Greg Price <greg@zulipchat.com>
2021-12-20 20:52:08 -0800

    storage [nfc]: Factor out de/compression from getItem/setItem/etc
    This will let us reuse these in providing them to migrations.

--- src/storage/CompressedAsyncStorage.js
+++ src/storage/CompressedAsyncStorage.js
@@ -22,6 +22,80 @@ function assertPlausiblyJSONEncoded(value: string) {
   invariant(/^[ntf\-0-9"[{]/.test(value), 'value must be JSON-encoded');
 }
 
+async function decode(item: string | null): Promise<string | null> {
+  // It's possible that getItem() is called on uncompressed state, for
+  // example when a user updates their app from a version without
+  // compression to a version with compression.  So we need to detect that.
+  //
+  // We can detect compressed states by inspecting the first few
+  // characters of `result`.  First, a leading 'z' indicates a
+  // "Zulip"-compressed string; otherwise, the string is the only other
+  // format we've ever stored, namely uncompressed JSON (which,
+  // conveniently, never starts with a 'z').
+  //
+  // Then, a Zulip-compressed string looks like `z|TRANSFORMS|DATA`, where
+  // TRANSFORMS is a space-separated list of the transformations that we
+  // applied, in order, to the data to produce DATA and now need to undo.
+  // E.g., `zlib base64` means DATA is a base64 encoding of a zlib
+  // encoding of the underlying data.  We call the "z|TRANSFORMS|" part
+  // the "header" of the string.
+  if (item !== null && item.startsWith('z')) {
+    // In this block, `item` is compressed state.
+    const header = item.substring(0, item.indexOf('|', item.indexOf('|') + 1) + 1);
+    if (
+      NativeModules.TextCompressionModule
+      && header === NativeModules.TextCompressionModule.header
+    ) {
+      // TODO: It'd be real nice to do this decompression on the native
+      //   side, so that we pass the data one way native->JS instead of
+      //   three ways native->JS->native->JS.
+      return NativeModules.TextCompressionModule.decompress(item);
+    } else {
+      // Panic! If we are confronted with an unknown format, there is
+      // nothing we can do to save the situation. Log an error and ignore
+      // the data.  This error should not happen unless a user downgrades
+      // their version of the app.
+      const err = new Error(`No decompression module found for format ${header}`);
+      logging.error(err);
+      throw err;
+    }
+  }
+
+  // Uncompressed state
+  return item;
+}
+
+async function encode(value: string): Promise<string> {
+  if (NODE_ENV !== 'production') {
+    assertPlausiblyJSONEncoded(value);
+  }
+
+  return NativeModules.TextCompressionModule
+    ? NativeModules.TextCompressionModule.compress(value)
+    : value;
+}
+
+async function encodeValues(keyValuePairs: string[][]): Promise<string[][]> {
+  if (NODE_ENV !== 'production') {
+    // eslint-disable-next-line no-unused-vars
+    for (const [_, value] of keyValuePairs) {
+      assertPlausiblyJSONEncoded(value);
+    }
+  }
+
+  return NativeModules.TextCompressionModule
+    ? Promise.all(
+        keyValuePairs.map(async ([key, value]) => [
+          key,
+          // TODO: It'd be real nice to do this compression on the native
+          //   side, so that we pass the data one way native->JS instead of
+          //   three ways native->JS->native->JS.
+          await NativeModules.TextCompressionModule.compress(value),
+        ]),
+      )
+    : keyValuePairs;
+}
+
 class CompressedAsyncStorageImpl {
   storage: BaseAsyncStorage;
 
@@ -30,90 +104,21 @@ class CompressedAsyncStorageImpl {
   }
 
   async getItem(key: string): Promise<string | null> {
-    const item = await this.storage.getItem(key);
-
-    // It's possible that getItem() is called on uncompressed state, for
-    // example when a user updates their app from a version without
-    // compression to a version with compression.  So we need to detect that.
-    //
-    // We can detect compressed states by inspecting the first few
-    // characters of `result`.  First, a leading 'z' indicates a
-    // "Zulip"-compressed string; otherwise, the string is the only other
-    // format we've ever stored, namely uncompressed JSON (which,
-    // conveniently, never starts with a 'z').
-    //
-    // Then, a Zulip-compressed string looks like `z|TRANSFORMS|DATA`, where
-    // TRANSFORMS is a space-separated list of the transformations that we
-    // applied, in order, to the data to produce DATA and now need to undo.
-    // E.g., `zlib base64` means DATA is a base64 encoding of a zlib
-    // encoding of the underlying data.  We call the "z|TRANSFORMS|" part
-    // the "header" of the string.
-    if (item !== null && item.startsWith('z')) {
-      // In this block, `item` is compressed state.
-      const header = item.substring(0, item.indexOf('|', item.indexOf('|') + 1) + 1);
-      if (
-        NativeModules.TextCompressionModule
-        && header === NativeModules.TextCompressionModule.header
-      ) {
-        // TODO: It'd be real nice to do this decompression on the native
-        //   side, so that we pass the data one way native->JS instead of
-        //   three ways native->JS->native->JS.
-        return NativeModules.TextCompressionModule.decompress(item);
-      } else {
-        // Panic! If we are confronted with an unknown format, there is
-        // nothing we can do to save the situation. Log an error and ignore
-        // the data.  This error should not happen unless a user downgrades
-        // their version of the app.
-        const err = new Error(`No decompression module found for format ${header}`);
-        logging.error(err);
-        throw err;
-      }
-    }
-
-    // Uncompressed state
-    return item;
+    return decode(await this.storage.getItem(key));
   }
 
   /** (The value must be a result of `JSON.stringify`.) */
   // The invariant that the value is JSON is relied on by `getItem`, to
   // guarantee that our compression header can't appear in uncompressed data.
   async setItem(key: string, value: string): Promise<mixed> {
-    if (NODE_ENV !== 'production') {
-      assertPlausiblyJSONEncoded(value);
-    }
-
-    return this.storage.setItem(
-      key,
-      NativeModules.TextCompressionModule
-        ? await NativeModules.TextCompressionModule.compress(value)
-        : value,
-    );
+    return this.storage.setItem(key, await encode(value));
   }
 
   /** (Each value must be a result of `JSON.stringify`.) */
   // The invariant that the value is JSON is relied on by `getItem`, to
   // guarantee that our compression header can't appear in uncompressed data.
   async multiSet(keyValuePairs: Array<Array<string>>): Promise<mixed> {
-    if (NODE_ENV !== 'production') {
-      // eslint-disable-next-line no-unused-vars
-      for (const [_, value] of keyValuePairs) {
-        assertPlausiblyJSONEncoded(value);
-      }
-    }
-
-    return this.storage.multiSet(
-      NativeModules.TextCompressionModule
-        ? await Promise.all(
-            keyValuePairs.map(async ([key, value]) => [
-              key,
-              // TODO: It'd be real nice to do this compression on the native
-              //   side, so that we pass the data one way native->JS instead of
-              //   three ways native->JS->native->JS.
-              await NativeModules.TextCompressionModule.compress(value),
-            ]),
-          )
-        : keyValuePairs,
-    );
+    return this.storage.multiSet(await encodeValues(keyValuePairs));
   }
 
   removeItem: typeof AsyncStorage.removeItem = key => this.storage.removeItem(key);
