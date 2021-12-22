3f3aee27b9d94ebb50536295c0006a18dd141449
Greg Price <greg@zulipchat.com>
2021-12-22 13:47:43 -0800

    wip sqlite add tests -- one failing, on transaction
    We could work around this for the case when the await is actually of a
    query result -- rework everything into callback style, so the next bit
    of the transaction runs immediately in that result callback, before
    the transaction engine checks its queue again.  Ugly but it'd work.
    
    But then we also have cases where something else is async, like
    compression.  I think the underlying expo-sqlite (and really the
    websql implementation underneath it) may not make it possible to
    handle that.
    
    Then on top of that, and rather unforgivably: the failure is
    completely silent!  We go and call `tx.executeSql` on a transaction
    that's already decided it's complete... and there's no error.  It just
    pushes the new task on the queue, which it never again intends to
    check.  This would be so easy to fix, too: just a check for
    `self._complete` at the top of `executeSql` (in
    `lib/websql/WebSQLTransaction.js` in `@expo/websql`), throwing an
    error when that's true.

--- src/storage/__tests__/sqlite-test.js
+++ src/storage/__tests__/sqlite-test.js
@@ -3,6 +3,8 @@ import { openDatabase } from 'expo-sqlite';
 // $FlowFixMe[untyped-import]
 import sqlite3 from 'sqlite3';
 
+import { SQLDatabase } from '../sqlite';
+
 /* eslint-disable no-underscore-dangle */
 
 describe('sqlite3', () => {
@@ -28,5 +30,86 @@ describe('expo-sqlite', () => {
       );
     });
     expect(result.rows._array).toEqual([{ n: 42 }]);
+  });
+
+  test('transaction with no internal await', async () => {
+    const db = openDatabase('test2.db');
+    await new Promise((resolve, reject) =>
+      db.transaction(
+        tx => {
+          tx.executeSql('CREATE TABLE foo (x INT)');
+          tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1]);
+          tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]);
+        },
+        reject,
+        resolve,
+      ),
+    );
+    const result = await new Promise((resolve, reject) => {
+      db.readTransaction(
+        tx => {
+          tx.executeSql('SELECT x FROM foo', [], (t, r) => resolve(r));
+        },
+        reject,
+        resolve,
+      );
+    });
+    expect(result.rows._array).toEqual([{ x: 1 }, { x: 2 }]);
+  });
+
+  test('transaction with internal await^W callback ', async () => {
+    const db = openDatabase('test3.db');
+    await new Promise((resolve, reject) =>
+      db.transaction(
+        tx => {
+          tx.executeSql('CREATE TABLE foo (x INT)');
+          tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1], () =>
+            tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]),
+          );
+        },
+        reject,
+        resolve,
+      ),
+    );
+    const result = await new Promise((resolve, reject) => {
+      db.readTransaction(
+        tx => {
+          tx.executeSql('SELECT x FROM foo', [], (t, r) => resolve(r));
+        },
+        reject,
+        resolve,
+      );
+    });
+    expect(result.rows._array).toEqual([{ x: 1 }, { x: 2 }]);
+  });
+});
+
+describe('our promisified sqlite', () => {
+  test('smoke', async () => {
+    const db = new SQLDatabase('test4.db');
+    const rows = await db.query<{ n: number }>('SELECT 42 AS n', []);
+    expect(rows).toEqual([{ n: 42 }]);
+  });
+
+  test('transaction with no internal await', async () => {
+    const db = new SQLDatabase('test5.db');
+    await db.transaction(tx => {
+      tx.executeSql('CREATE TABLE foo (x INT)');
+      tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1]);
+      tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]);
+    });
+    const rows = await db.query<{ x: number }>('SELECT x FROM foo', []);
+    expect(rows).toEqual([{ x: 1 }, { x: 2 }]);
+  });
+
+  test('transaction with internal await', async () => {
+    const db = new SQLDatabase('test6.db');
+    await db.transaction(async tx => {
+      tx.executeSql('CREATE TABLE foo (x INT)');
+      await tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1]);
+      tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]);
+    });
+    const rows = await db.query<{ x: number }>('SELECT x FROM foo', []);
+    expect(rows).toEqual([{ x: 1 }, { x: 2 }]); // FAILS -- no 2
   });
 });
