57b6dce5d5bdd5a82df85c566b30604c01a66b52
Greg Price <greg@zulipchat.com>
2021-12-23 14:24:41 -0800

    wip migrations tests [nfc]: Reorganize toward testing future migrations

--- src/storage/__tests__/migrations-test.js
+++ src/storage/__tests__/migrations-test.js
@@ -9,14 +9,14 @@ import { migrationLegacyRollup } from '../migrations';
 import { objectFromEntries } from '../../jsBackport';
 import { ZulipVersion } from '../../utils/zulipVersion';
 
-// These are copied from the implementation.
-const reduxPersistKeyPrefix = 'reduxPersist:';
-const encodeKey = k => `${reduxPersistKeyPrefix}${k}`;
-const decodeKey = k => k.slice(reduxPersistKeyPrefix.length);
-const deserializer = parse;
-const serializer = stringify;
+describe('migrations where one top-level subtree is still exactly one key', () => {
+  // These are copied from the implementation.
+  const reduxPersistKeyPrefix = 'reduxPersist:';
+  const encodeKey = k => `${reduxPersistKeyPrefix}${k}`;
+  const decodeKey = k => k.slice(reduxPersistKeyPrefix.length);
+  const deserializer = parse;
+  const serializer = stringify;
 
-describe('migrationLegacyRollup', () => {
   const baseStorage = new CompressedAsyncStorageImpl(1, [new Migration(0, 1, async () => {})]);
 
   beforeAll(() => baseStorage.devWipe());
@@ -44,7 +44,7 @@ describe('migrationLegacyRollup', () => {
   }
 
   // A plausible-ish state from before all surviving migrations.
-  const base = {
+  const base3 = {
     // Include something non-empty for each of the storeKeys.
     migrations: { version: 3 },
     accounts: [{ email: 'me@example.com', api_key: '1234', realm: 'https://chat.example' }],
@@ -64,14 +64,14 @@ describe('migrationLegacyRollup', () => {
     foo: 1,
   };
 
-  // What `base` becomes after migrations up through 9.
+  // What `base3` becomes after legacy migrations up through 9.
   const base9 = {
-    ...base,
+    ...base3,
     migrations: { version: 9 },
-    accounts: [{ ...base.accounts[0], ackedPushToken: null }],
+    accounts: [{ ...base3.accounts[0], ackedPushToken: null }],
   };
 
-  // What `base` becomes after migrations up through 15.
+  // What `base3` becomes after legacy migrations up through 15.
   const base15 = {
     ...base9,
     migrations: { version: 15 },
@@ -85,7 +85,7 @@ describe('migrationLegacyRollup', () => {
     ],
   };
 
-  // What `base` becomes after all migrations.
+  // What `base3` becomes after all legacy migrations.
   const endBase = {
     migrations: { version: 37 },
     accounts: [
@@ -110,118 +110,130 @@ describe('migrationLegacyRollup', () => {
     },
   };
 
-  for (const [desc, before, after] of [
-    // Test the behavior with no migration state, which doesn't apply any
-    // of the specific migrations.
-    ['empty state -> just store version', {}, { migrations: { version: 37 } }],
-    [
-      'no migration state -> just clear and store version',
-      { nonsense: [1, 2, 3] },
-      { migrations: { version: 37 } },
-    ],
+  describe('migrationLegacyRollup', () => {
+    for (const [desc, before, after] of [
+      // Test the behavior with no migration state, which doesn't apply any
+      // of the specific migrations.
+      ['empty state -> just store version', {}, { migrations: { version: 37 } }],
+      [
+        'no migration state -> just clear and store version',
+        { nonsense: [1, 2, 3] },
+        { migrations: { version: 37 } },
+      ],
 
-    // Test the whole sequence all together.  This covers many of the
-    // individual migrations.  (This might not be a good design if we were
-    // going to be adding more migrations in this sequence; but we aren't.)
-    ['whole sequence', base, endBase],
+      // Test the whole sequence all together.  This covers many of the
+      // individual migrations.  (This might not be a good design if we were
+      // going to be adding more migrations in this sequence; but we aren't.)
+      ['whole sequence', base3, endBase],
 
-    //
-    // Now test individual migrations further, where needed.
+      //
+      // Now test individual migrations further, where needed.
 
-    // 6 is redundant with 9
-    // 9 covered by whole
-    [
-      'check 10 with locale zh',
-      // with locale id, 10 gets reverted by 26
-      { ...base9, settings: { ...base9.settings, locale: 'zh' } },
-      { ...endBase, settings: { ...endBase.settings, language: 'zh-Hans' } },
-    ],
-    // 12 covered by whole
-    [
-      'check 13',
-      {
-        ...base9,
-        migrations: { version: 12 },
-        accounts: [{ ...base9.accounts[0], zulipVersion: '1.2.3' }],
-      },
-      {
-        ...endBase,
-        accounts: [{ ...endBase.accounts[0], zulipVersion: new ZulipVersion('1.2.3') }],
-      },
-    ],
-    // 14 covered by whole
-    // 15 covered by whole
-    // 21 covered by whole
-    [
-      'check 22',
-      { ...base15, migrations: { version: 21 }, drafts: { 'pm:d:12:other@example.com': 'text' } },
-      // Should be this:
-      //   { ...endBase, drafts: { 'pm:12': 'text' } }, // FAILS
-      // But this migration is buggy!  Should have written tests in the first place.
-      // Instead we get:
-      { ...endBase, drafts: {} }, // WRONG
-      // TODO reflect that in the migration.  (Too late for fixing it to be
-      //   of much use.)
-    ],
-    [
-      'check 26',
-      { ...base15, settings: { ...base15.settings, locale: 'id-ID' } },
-      { ...endBase, settings: { ...endBase.settings, language: 'id' } },
-    ],
-    [
-      'check 27',
-      { ...base15, accounts: [{ ...base15.accounts[0], email: '' }] },
-      { ...endBase, accounts: [] },
-    ],
-    // 28 covered by whole
-    [
-      'check 29 with outbox-message that does have sender_id',
-      { ...base15, outbox: [{ ...base15.outbox[0], sender_id: 345 }] },
-      { ...endBase, outbox: [{ ...base15.outbox[0], sender_id: 345 }] },
-    ],
-    [
-      'check 30',
-      { ...base15, settings: { ...base15.settings, locale: 'pt_PT' } },
-      { ...endBase, settings: { ...endBase.settings, language: 'pt-PT' } },
-    ],
-    // 31 covered by whole
-    [
-      'check 32',
-      { ...base15, settings: { ...base15.settings, locale: 'zh-Hant' } },
-      { ...endBase, settings: { ...endBase.settings, language: 'zh-TW' } },
-    ],
-    // 33 covered by whole
-    [
-      'check 35 with missing stream_id',
-      { ...base15, outbox: [{ ...base15.outbox[0], type: 'stream', sender_id: 345 }] },
-      { ...endBase, outbox: [] },
-    ],
-    [
-      'check 35 with stream_id present',
-      {
-        ...base15,
-        outbox: [{ ...base15.outbox[0], type: 'stream', sender_id: 345, stream_id: 17 }],
-      },
-      {
-        ...endBase,
-        outbox: [{ ...base15.outbox[0], type: 'stream', sender_id: 345, stream_id: 17 }],
-      },
-    ],
-    // 36 covered by whole
-    [
-      'check 37 with setting already false',
-      { ...base15, settings: { ...base15.settings, doNotMarkMessagesAsRead: false } },
-      { ...endBase, settings: { ...endBase.settings, doNotMarkMessagesAsRead: false } },
-    ],
-    [
-      'check 37 with setting already true',
-      { ...base15, settings: { ...base15.settings, doNotMarkMessagesAsRead: true } },
-      { ...endBase, settings: { ...endBase.settings, doNotMarkMessagesAsRead: true } },
-    ],
-  ]) {
-    test(desc, async () => {
-      await prep(before);
-      expect(await fetch()).toEqual(after);
-    });
-  }
+      // 6 is redundant with 9
+      // 9 covered by whole
+      [
+        'check 10 with locale zh',
+        // with locale id, 10 gets reverted by 26
+        { ...base9, settings: { ...base9.settings, locale: 'zh' } },
+        { ...endBase, settings: { ...endBase.settings, language: 'zh-Hans' } },
+      ],
+      // 12 covered by whole
+      [
+        'check 13',
+        {
+          ...base9,
+          migrations: { version: 12 },
+          accounts: [{ ...base9.accounts[0], zulipVersion: '1.2.3' }],
+        },
+        {
+          ...endBase,
+          accounts: [{ ...endBase.accounts[0], zulipVersion: new ZulipVersion('1.2.3') }],
+        },
+      ],
+      // 14 covered by whole
+      // 15 covered by whole
+      // 21 covered by whole
+      [
+        'check 22',
+        { ...base15, migrations: { version: 21 }, drafts: { 'pm:d:12:other@example.com': 'text' } },
+        // Should be this:
+        //   { ...endBase, drafts: { 'pm:12': 'text' } }, // FAILS
+        // But this migration is buggy!  Should have written tests in the first place.
+        // Instead we get:
+        { ...endBase, drafts: {} }, // WRONG
+        // TODO reflect that in the migration.  (Too late for fixing it to be
+        //   of much use.)
+      ],
+      [
+        'check 26',
+        { ...base15, settings: { ...base15.settings, locale: 'id-ID' } },
+        { ...endBase, settings: { ...endBase.settings, language: 'id' } },
+      ],
+      [
+        'check 27',
+        { ...base15, accounts: [{ ...base15.accounts[0], email: '' }] },
+        { ...endBase, accounts: [] },
+      ],
+      // 28 covered by whole
+      [
+        'check 29 with outbox-message that does have sender_id',
+        { ...base15, outbox: [{ ...base15.outbox[0], sender_id: 345 }] },
+        { ...endBase, outbox: [{ ...base15.outbox[0], sender_id: 345 }] },
+      ],
+      [
+        'check 30',
+        { ...base15, settings: { ...base15.settings, locale: 'pt_PT' } },
+        { ...endBase, settings: { ...endBase.settings, language: 'pt-PT' } },
+      ],
+      // 31 covered by whole
+      [
+        'check 32',
+        { ...base15, settings: { ...base15.settings, locale: 'zh-Hant' } },
+        { ...endBase, settings: { ...endBase.settings, language: 'zh-TW' } },
+      ],
+      // 33 covered by whole
+      [
+        'check 35 with missing stream_id',
+        { ...base15, outbox: [{ ...base15.outbox[0], type: 'stream', sender_id: 345 }] },
+        { ...endBase, outbox: [] },
+      ],
+      [
+        'check 35 with stream_id present',
+        {
+          ...base15,
+          outbox: [{ ...base15.outbox[0], type: 'stream', sender_id: 345, stream_id: 17 }],
+        },
+        {
+          ...endBase,
+          outbox: [{ ...base15.outbox[0], type: 'stream', sender_id: 345, stream_id: 17 }],
+        },
+      ],
+      // 36 covered by whole
+      [
+        'check 37 with setting already false',
+        { ...base15, settings: { ...base15.settings, doNotMarkMessagesAsRead: false } },
+        { ...endBase, settings: { ...endBase.settings, doNotMarkMessagesAsRead: false } },
+      ],
+      [
+        'check 37 with setting already true',
+        { ...base15, settings: { ...base15.settings, doNotMarkMessagesAsRead: true } },
+        { ...endBase, settings: { ...endBase.settings, doNotMarkMessagesAsRead: true } },
+      ],
+    ]) {
+      test(desc, async () => {
+        await prep(before);
+        expect(await fetch()).toEqual(after);
+      });
+    }
+  });
+
+  //   const base = endBase;
+
+  //   describe('migrationAccountId', () => {
+  //     test('smoke', async () => {
+  //       // TODO fix prep/fetch
+  //       await prep(base);
+  //       expect(await fetch()).toEqual({ ...base, accounts: [{ ...base.accounts[0], accountId: 1 }] });
+  //     });
+  //   });
 });
