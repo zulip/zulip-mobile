102c6daea0ad77e8328dda862c2bbb649d4a3038
Greg Price <greg@zulipchat.com>
2021-12-22 20:09:44 -0800

    expo-sqlite tests: Demo the broken asynchrony handling
    This test shows that if using expo-sqlite you try to make a
    transaction that involves some asynchronous work between SQL queries,
    the transaction gets committed in a half-done state.
    
    This is a limitation that stems from Web SQL Database, the API (an
    old proposed standard, implemented in Chromium-based browsers but no
    others) that expo-sqlite mostly provides: that API doesn't provide
    any way for the implementation to know when you're done with any
    async work, so effectively it assumes you don't try to do any.
    
    Moreover there's an implementation bug, which isn't due to the API:
    when you do try to do this, the subsequent statements you try to add
    to the transaction get silently ignored, with no error.
    
    We'll work around this issue in the promisifying wrapper we'll add
    atop expo-sqlite.  Meanwhile, this test serves to demonstrate what
    the issue is in the first place.

--- src/storage/__tests__/sqlite-test.js
+++ src/storage/__tests__/sqlite-test.js
@@ -4,7 +4,10 @@ import sqlite3 from 'sqlite3';
 // $FlowFixMe[missing-export] -- present in test version of module
 import { openDatabase, deleteDatabase } from 'expo-sqlite';
 
+import { objectFromEntries } from '../../jsBackport';
+
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable no-return-assign */
 
 describe('sqlite3', () => {
   test('smoke', async () => {
@@ -69,5 +72,68 @@ describe('expo-sqlite', () => {
     });
     const rows = await select(db, 'SELECT x FROM foo');
     expect(rows).toEqual([{ x: 1 }, { x: 2 }]);
+  });
+
+  test('BROKEN: transaction with internal asynchrony other than executeSql', async () => {
+    // This test shows that if using expo-sqlite you try to make a
+    // transaction that involves some asynchronous work between SQL queries,
+    // the transaction gets committed in a half-done state.  Moreover
+    // the subsequent statements get silently ignored, with no error.
+    //
+    // This failure mode gets triggered very easily if you try to use
+    // Promises.  But if you actually have some asynchronous work to do,
+    // then this hits even if you express the flow in pure callback terms.
+    // So we'll demonstrate it that way here.
+    //
+    // (In our promisifying wrapper, we'll work around this issue.)
+
+    const db = openDatabase(dbName);
+    let txEnded = false;
+    await promiseTxn(db, tx => {
+      tx.executeSql('CREATE TABLE foo (name TEXT, value INT)');
+
+      // Get some data from SQL.
+      tx.executeSql('SELECT 2 + 2 AS total', [], (t, result) => {
+        const { total } = result.rows._array[0];
+
+        // Use that data to compute something synchronously, and store it.
+        const double = 2 * total;
+        tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['double', double]);
+        // So far, all's well.
+
+        // Now do some *asynchronous* computation, using the data we
+        // read earlier in the transaction.
+        const asyncSquare = (x, cb) => setTimeout(() => cb(x * x), 0);
+        asyncSquare(total, square => {
+          // By the time we get here, the transaction will have found its
+          // queue empty and decided it's complete.
+
+          // So when we try to write this result, nothing will happen.
+          tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['square', square]);
+
+          // The new statement goes into a queue which will never again
+          // get read.  We don't even get an exception -- the next line
+          // runs fine (as we confirm below):
+          txEnded = true;
+        });
+      });
+    });
+
+    // Let that asynchronous computation complete, and confirm the callback
+    // completes without an exception.
+    jest.runAllTimers();
+    while (!txEnded) {
+      await null;
+    }
+
+    // Now read what data got written.
+    const rows = await select(db, 'SELECT name, value FROM foo');
+    const data = objectFromEntries(rows.map(({ name, value }) => [name, value]));
+
+    // This would be a good answer:
+    // expect(data).toEqual({ double: 8, square: 16 }); // FAILS
+
+    // Instead, we get:
+    expect(data).toEqual({ double: 8 }); // bad: missing the second INSERT
   });
 });
