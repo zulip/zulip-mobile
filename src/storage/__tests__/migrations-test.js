632554cd29d04c0415ce40c1850cd6e5571154a2
Greg Price <greg@zulipchat.com>
2021-12-23 13:23:48 -0800

    migrations tests: Start on some substantive tests

--- src/storage/__tests__/migrations-test.js
+++ src/storage/__tests__/migrations-test.js
@@ -52,8 +52,73 @@ describe('migrationLegacyRollup', () => {
     expect(await fetch()).toEqual({ migrations: { version: 37 } });
   });
 
-  test('0 -> ??', async () => {
-    await prep({ migrations: { version: 0 }, foo: 1 });
-    expect(await fetch()).toEqual({ migrations: { version: 37 } });
+  // A plausible-ish state from before all surviving migrations.
+  const fixture3 = {
+    // Include something non-empty for each of the storeKeys.
+    migrations: { version: 3 },
+    accounts: [{ email: 'me@example.com', api_key: '1234', realm: 'https://chat.example' }],
+    drafts: { '[]': 'draft text' },
+    // Real Outbox values have more properties, but fudge that.
+    outbox: [{ isOutbox: true, isSent: false, type: 'private' }],
+    // Settings from their reducer's initial value at this version.
+    settings: {
+      locale: 'en',
+      theme: 'default',
+      offlineNotification: true,
+      onlineNotification: true,
+      experimentalFeaturesEnabled: false,
+      streamNotification: false,
+    },
+    // Include something extraneous, too.
+    foo: 1,
+  };
+
+  const fixture3_37 = {
+    migrations: { version: 37 },
+    accounts: [
+      {
+        email: 'me@example.com',
+        api_key: '1234',
+        // realm converted
+        realm: new URL('https://chat.example'),
+        // added:
+        ackedPushToken: null,
+        lastDismissedServerPushSetupNotice: null,
+        userId: null,
+        zulipFeatureLevel: null,
+        zulipVersion: null,
+      },
+    ],
+    drafts: {}, // cleared
+    outbox: [], // cleared of lacking sender_id
+    settings: {
+      language: 'en', // renamed from locale
+      theme: 'default',
+      offlineNotification: true,
+      onlineNotification: true,
+      experimentalFeaturesEnabled: false,
+      streamNotification: false,
+      // added:
+      browser: 'default',
+      doNotMarkMessagesAsRead: false,
+    },
+  };
+
+  test('3 -> end', async () => {
+    await prep(fixture3);
+    expect(await fetch()).toEqual(fixture3_37);
+  });
+
+  // Exercises migration 10.
+  const fixture3_zh = { ...fixture3, settings: { ...fixture3.settings, locale: 'zh' } };
+
+  const fixture3_zh_37 = {
+    ...fixture3_37,
+    settings: { ...fixture3_37.settings, language: 'zh-Hans' },
+  };
+
+  test('3 -> end, locale zh', async () => {
+    await prep(fixture3_zh);
+    expect(await fetch()).toEqual(fixture3_zh_37);
   });
 });
