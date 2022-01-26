db7a18f29ab1c85e417267a25948010f8ff51f64
Chris Bobbe <cbobbe@zulip.com>
2022-01-25 16:42:40 -0800

    deps: Run catch-all `yarn upgrade`
    Note: On iOS, if the app gets to the "Connecting..." stage and
    mostly hangs, not responding when you tap one of the bottom tabs,
    try stopping and starting Metro a.k.a. `yarn start`.
    
    - Ask `jest-expo` to resolve `react-test-renderer` to the right
      version for `react`/`react-native`.
    
    - Limit `react-native-reanimated` to <2.3.0. I encountered an error
      like the one reported at
        https://github.com/software-mansion/react-native-reanimated/issues/2702
      . I don't know what's causing it, but there's reportedly some
      interaction with expo@43.x, which we recently added. See
        https://github.com/software-mansion/react-native-reanimated/issues/2702#issuecomment-988743559
      . We don't need 2.3.0 or later, for now. We can investigate more
      later, when we do need it. At that time, we may be on a later
      version of `expo`.
    
    - For a simple way to make sure we're not affected by CVE-2022-0235,
      we wanted to avoid bringing in node-fetch at one of the affected
      versions. According to
        https://github.com/advisories/GHSA-r683-j2x4-v87g
      those are
    
        >= 3.0.0, < 3.1.1
        < 2.6.7
    
      Unfortunately, there's still one chain, through `expo`, that's
      bringing in node-fetch@1.7.3; see
        https://github.com/expo/expo/pull/16023#issuecomment-1021732956
    
    - Looks like some sub-dependency at its updated version wants to
      cause different output for `generate-webview-js`. Examine that
      output (looks OK), and commit.

--- src/webview/js/generatedEs3.js
+++ src/webview/js/generatedEs3.js
@@ -19,14 +19,9 @@ var compiledWebviewJs = (function (exports) {
 
     if (Object.getOwnPropertySymbols) {
       var symbols = Object.getOwnPropertySymbols(object);
-
-      if (enumerableOnly) {
-        symbols = symbols.filter(function (sym) {
-          return Object.getOwnPropertyDescriptor(object, sym).enumerable;
-        });
-      }
-
-      keys.push.apply(keys, symbols);
+      enumerableOnly && (symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+      })), keys.push.apply(keys, symbols);
     }
 
     return keys;
@@ -34,19 +29,12 @@ var compiledWebviewJs = (function (exports) {
 
   function _objectSpread2(target) {
     for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i] != null ? arguments[i] : {};
-
-      if (i % 2) {
-        ownKeys(Object(source), true).forEach(function (key) {
-          _defineProperty(target, key, source[key]);
-        });
-      } else if (Object.getOwnPropertyDescriptors) {
-        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
-      } else {
-        ownKeys(Object(source)).forEach(function (key) {
-          Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
-        });
-      }
+      var source = null != arguments[i] ? arguments[i] : {};
+      i % 2 ? ownKeys(Object(source), !0).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
     }
 
     return target;
@@ -1034,6 +1022,6 @@ var compiledWebviewJs = (function (exports) {
 
   return exports;
 
-}({}));
+})({});
 
 `;
