8c8841370162b4134c21efbb5c16d1fa8709efd0
Chris Bobbe <cbobbe@zulip.com>
2022-02-04 18:49:32 -0800

    user status tests [nfc]: Start type-checking; use example data
    And make each test tighter, removing unnecessary variable
    declarations, whitespace, etc.

--- src/user-status/__tests__/userStatusReducer-test.js
+++ src/user-status/__tests__/userStatusReducer-test.js
@@ -1,154 +1,126 @@
-import deepFreeze from 'deep-freeze';
+/* @flow strict-local */
 import Immutable from 'immutable';
 
-import { ACCOUNT_SWITCH, REGISTER_COMPLETE, EVENT_USER_STATUS_UPDATE } from '../../actionConstants';
+import { makeUserId } from '../../api/idTypes';
+import type { UserStatusState } from '../../types';
+import * as eg from '../../__tests__/lib/exampleData';
+import { EVENT_USER_STATUS_UPDATE, DISMISS_SERVER_COMPAT_NOTICE } from '../../actionConstants';
 import userStatusReducer from '../userStatusReducer';
 
 describe('userStatusReducer', () => {
-  const testUserStatusState = Immutable.Map([
-    [1, { away: true }],
-    [2, { status_text: 'Hello, world' }],
+  const testUserStatusState: UserStatusState = Immutable.Map([
+    [makeUserId(1), { away: true }],
+    [makeUserId(2), { status_text: 'Hello, world' }],
   ]);
 
   test('handles unknown action by returning initial state', () => {
     const initialState = Immutable.Map();
-    const newState = userStatusReducer(initialState, {});
+    // "unknown", meaning not one this reducer wants to act on
+    const newState = userStatusReducer(initialState, { type: DISMISS_SERVER_COMPAT_NOTICE });
     expect(newState).toBeDefined();
   });
 
   describe('ACCOUNT_SWITCH', () => {
     test('resets state to initial state', () => {
-      const initialState = testUserStatusState;
-      const action = deepFreeze({
-        type: ACCOUNT_SWITCH,
-      });
-      const expectedState = Immutable.Map();
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(userStatusReducer(testUserStatusState, eg.action.account_switch)).toEqual(
+        Immutable.Map(),
+      );
     });
   });
 
   describe('REGISTER_COMPLETE', () => {
     test('when `user_status` data is provided init state with it', () => {
-      const initialState = Immutable.Map();
-      const action = {
-        type: REGISTER_COMPLETE,
-        data: {
-          user_status: deepFreeze({
-            1: { away: true },
-            2: { status_text: 'Hello, world' },
+      expect(
+        userStatusReducer(
+          Immutable.Map(),
+          eg.mkActionRegisterComplete({
+            user_status: { '1': { away: true }, '2': { status_text: 'Hello, world' } },
           }),
-        },
-      };
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(testUserStatusState);
+        ),
+      ).toEqual(testUserStatusState);
     });
 
     test('handles older back-ends that do not have `user_status` data by resetting the state', () => {
-      const initialState = testUserStatusState;
-      const action = deepFreeze({
-        type: REGISTER_COMPLETE,
-        data: {},
-      });
-      const expectedState = Immutable.Map();
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(
+          testUserStatusState,
+          eg.mkActionRegisterComplete({ user_status: undefined }),
+        ),
+      ).toEqual(Immutable.Map());
     });
   });
 
   describe('EVENT_USER_STATUS_UPDATE', () => {
     test('when the user does not already have entry add a key by their `user_id`', () => {
-      const initialState = Immutable.Map([[2, { away: true }]]);
-      const action = {
-        type: EVENT_USER_STATUS_UPDATE,
-        user_id: 1,
-        away: true,
-      };
-      const expectedState = Immutable.Map([
-        [1, { away: true }],
-        [2, { away: true }],
-      ]);
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(Immutable.Map([[makeUserId(2), { away: true }]]), {
+          id: 0,
+          type: EVENT_USER_STATUS_UPDATE,
+          user_id: makeUserId(1),
+          away: true,
+        }),
+      ).toEqual(
+        Immutable.Map([
+          [1, { away: true }],
+          [2, { away: true }],
+        ]),
+      );
     });
 
     test('if the user already has user status stored update their key', () => {
-      const initialState = Immutable.Map([[1, { status_text: 'foo' }]]);
-      const action = deepFreeze({
-        type: EVENT_USER_STATUS_UPDATE,
-        user_id: 1,
-        status_text: 'bar',
-      });
-      const expectedState = Immutable.Map([[1, { status_text: 'bar' }]]);
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(Immutable.Map([[makeUserId(1), { status_text: 'foo' }]]), {
+          id: 0,
+          type: EVENT_USER_STATUS_UPDATE,
+          user_id: makeUserId(1),
+          status_text: 'bar',
+        }),
+      ).toEqual(Immutable.Map([[1, { status_text: 'bar' }]]));
     });
 
     test('when the user_ status text is updated this is reflected in the state', () => {
-      const initialState = Immutable.Map();
-      const action = deepFreeze({
-        type: EVENT_USER_STATUS_UPDATE,
-        user_id: 1,
-        status_text: 'Hello, world!',
-      });
-      const expectedState = Immutable.Map([[1, { status_text: 'Hello, world!' }]]);
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(Immutable.Map(), {
+          id: 0,
+          type: EVENT_USER_STATUS_UPDATE,
+          user_id: makeUserId(1),
+          status_text: 'Hello, world!',
+        }),
+      ).toEqual(Immutable.Map([[1, { status_text: 'Hello, world!' }]]));
     });
 
     test('both `away` and `status_text` values can be updated in one event', () => {
-      const initialState = Immutable.Map([[1, {}]]);
-      const action = deepFreeze({
-        type: EVENT_USER_STATUS_UPDATE,
-        user_id: 1,
-        away: true,
-        status_text: 'Hello, world!',
-      });
-      const expectedState = Immutable.Map([[1, { away: true, status_text: 'Hello, world!' }]]);
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(Immutable.Map([[makeUserId(1), {}]]), {
+          id: 0,
+          type: EVENT_USER_STATUS_UPDATE,
+          user_id: makeUserId(1),
+          away: true,
+          status_text: 'Hello, world!',
+        }),
+      ).toEqual(Immutable.Map([[1, { away: true, status_text: 'Hello, world!' }]]));
     });
 
     test('if `away` status is removed delete the key from the object', () => {
-      const initialState = Immutable.Map([[1, { away: true }]]);
-      const action = deepFreeze({
-        type: EVENT_USER_STATUS_UPDATE,
-        user_id: 1,
-        away: false,
-      });
-      const expectedState = Immutable.Map([[1, {}]]);
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(Immutable.Map([[makeUserId(1), { away: true }]]), {
+          id: 0,
+          type: EVENT_USER_STATUS_UPDATE,
+          user_id: makeUserId(1),
+          away: false,
+        }),
+      ).toEqual(Immutable.Map([[1, {}]]));
     });
 
     test('if `status_text` is removed delete the key from the object', () => {
-      const initialState = Immutable.Map([[1, { status_text: 'Hello, world!' }]]);
-      const action = deepFreeze({
-        type: EVENT_USER_STATUS_UPDATE,
-        user_id: 1,
-        status_text: '',
-      });
-      const expectedState = Immutable.Map([[1, {}]]);
-
-      const actualState = userStatusReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        userStatusReducer(Immutable.Map([[makeUserId(1), { status_text: 'Hello, world!' }]]), {
+          id: 0,
+          type: EVENT_USER_STATUS_UPDATE,
+          user_id: makeUserId(1),
+          status_text: '',
+        }),
+      ).toEqual(Immutable.Map([[1, {}]]));
     });
   });
 });
