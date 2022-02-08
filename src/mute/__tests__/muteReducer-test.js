7c875fe0d2f68f5297e3cbd964145da6477a8206
Greg Price <greg@zulipchat.com>
2022-02-07 17:03:22 -0800

    mute tests [nfc]: Tighten up muteModel tests a lot
    Less boilerplate, so it's easier to see what the tests are actually
    doing, and also so there's less to edit to adapt to changes.

--- src/mute/__tests__/muteReducer-test.js
+++ src/mute/__tests__/muteReducer-test.js
@@ -5,58 +5,37 @@ import muteReducer from '../muteReducer';
 import { EVENT_MUTED_TOPICS } from '../../actionConstants';
 import * as eg from '../../__tests__/lib/exampleData';
 
+const initialState = deepFreeze([]);
+
 describe('muteReducer', () => {
   describe('REGISTER_COMPLETE', () => {
     test('when `mute` data is provided init state with it', () => {
-      const initialState = deepFreeze([]);
       const action = eg.mkActionRegisterComplete({ muted_topics: [['stream', 'topic']] });
-
-      const actualState = muteReducer(initialState, action);
-
-      expect(actualState).toEqual([['stream', 'topic']]);
+      expect(muteReducer(initialState, action)).toEqual([['stream', 'topic']]);
     });
 
     test('when no `mute` data is given reset state', () => {
-      const initialState = deepFreeze([['stream', 'topic']]);
+      const state = deepFreeze([['stream', 'topic']]);
       const action = eg.mkActionRegisterComplete({ muted_topics: [] });
-      const expectedState = [];
-
-      const actualState = muteReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(muteReducer(state, action)).toEqual(initialState);
     });
   });
 
   describe('ACCOUNT_SWITCH', () => {
     test('resets state to initial state', () => {
-      const initialState = deepFreeze([['stream', 'some_topic']]);
-
-      const action = eg.action.account_switch;
-
-      const expectedState = [];
-
-      const actualState = muteReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      const state = deepFreeze([['stream', 'some_topic']]);
+      expect(muteReducer(state, eg.action.account_switch)).toEqual(initialState);
     });
   });
 
   describe('EVENT_MUTED_TOPICS', () => {
     test('appends and test a new muted topic', () => {
-      const initialState = deepFreeze([]);
-
       const action = deepFreeze({
         type: EVENT_MUTED_TOPICS,
         id: -1,
         muted_topics: [['stream', 'topic']],
       });
-
-      const expectedState = [['stream', 'topic']];
-
-      const newState = muteReducer(initialState, action);
-
-      expect(newState).toEqual(expectedState);
-      expect(newState).not.toBe(initialState);
+      expect(muteReducer(initialState, action)).toEqual([['stream', 'topic']]);
     });
   });
 });
