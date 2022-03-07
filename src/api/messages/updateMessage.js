3c767c6eef39c0688395d402b4c31a61559debb1
Greg Price <greg@zulipchat.com>
2022-03-07 12:45:27 -0800

    api types: Add send_notification_to_*_thread params on updateMessage

--- src/api/messages/updateMessage.js
+++ src/api/messages/updateMessage.js
@@ -3,14 +3,27 @@ import type { ApiResponse, Auth } from '../transportTypes';
 import type { PropagateMode } from '../modelTypes';
 import { apiPatch } from '../apiFetch';
 
-/** See https://zulip.com/api/update-message */
+/**
+ * https://zulip.com/api/update-message
+ *
+ * NB the caller must manage old-server compatibility:
+ * `send_notification_to_old_thread` and `send_notification_to_new_thread`
+ * are available only starting with FL 9.
+ */
+// TODO(#4659): Once we pass the feature level to API methods, this one
+//   should encapsulate dropping send_notification_* when unsupported.
+// TODO(server-3.0): Simplify that away.
 export default async (
   auth: Auth,
   params: $ReadOnly<{|
     // TODO(server-2.0): Say "topic", not "subject"
     subject?: string,
+
     propagate_mode?: PropagateMode,
     content?: string,
+
+    send_notification_to_old_thread?: boolean,
+    send_notification_to_new_thread?: boolean,
   |}>,
   id: number,
 ): Promise<ApiResponse> => apiPatch(auth, `messages/${id}`, params);
