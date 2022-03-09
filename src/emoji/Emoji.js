d671898b6db2381a66972e5fde1c652a5de66502
Chris Bobbe <cbobbe@zulip.com>
2022-03-08 20:01:46 -0800

    Emoji: Set `color` on UnicodeEmoji so the emoji doesn't look faded

--- src/emoji/Emoji.js
+++ src/emoji/Emoji.js
@@ -1,11 +1,11 @@
 /* @flow strict-local */
-import React, { useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import type { Node } from 'react';
 import { Image } from 'react-native';
 import { createIconSet } from 'react-native-vector-icons';
 
 import type { EmojiType } from '../types';
-import { createStyleSheet } from '../styles';
+import { createStyleSheet, ThemeContext } from '../styles';
 import { useSelector } from '../react-redux';
 import { getAllImageEmojiByCode } from './emojiSelectors';
 import { codeToEmojiMap } from './data';
@@ -23,6 +23,7 @@ type Props = $ReadOnly<{|
 
 export default function Emoji(props: Props): Node {
   const { code, size = 20 } = props;
+  const { color } = useContext(ThemeContext);
   const imageEmoji = useSelector(state =>
     props.type === 'image' ? getAllImageEmojiByCode(state)[props.code] : undefined,
   );
@@ -33,5 +34,18 @@ export default function Emoji(props: Props): Node {
   if (imageEmoji) {
     return <Image style={componentStyles.image} source={{ uri: imageEmoji.source_url }} />;
   }
-  return <UnicodeEmoji name={code} size={size} />;
+  return (
+    <UnicodeEmoji
+      // Set `color` just to remove some transparency or darkening that's
+      // somehow getting applied, at least on Android, making emojis
+      // noticeably faded; not sure how. See a screenshot of the faded
+      // appearance at
+      //   https://github.com/zulip/zulip-mobile/pull/5277#issuecomment-1062504604
+      // and the doc for this property at
+      //   https://github.com/oblador/react-native-vector-icons#properties
+      color={color}
+      name={code}
+      size={size}
+    />
+  );
 }
