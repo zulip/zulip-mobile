7c2ea178f3ccdfc6e010d203be274a110b4c2b4d
Austin Riba <austin@zulip.com>
2022-03-31 16:12:25 -0700

    api: Accept more properties in createStream

--- src/api/streams/createStream.js
+++ src/api/streams/createStream.js
@@ -10,11 +10,15 @@ export default (
   // TODO(server-3.0): Send numeric user IDs (#3764), not emails.
   principals?: $ReadOnlyArray<string> = [],
   inviteOnly?: boolean = false,
+  is_web_public?: boolean = false,
+  history_public_to_subscribers?: boolean = false,
   announce?: boolean = false,
 ): Promise<ApiResponse> =>
   apiPost(auth, 'users/me/subscriptions', {
     subscriptions: JSON.stringify([{ name, description }]),
     principals: JSON.stringify(principals),
     invite_only: inviteOnly,
+    is_web_public,
+    history_public_to_subscribers,
     announce,
   });
