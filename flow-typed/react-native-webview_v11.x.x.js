00172a1f96aef6aa92f76989f4dd49b00b45c492
Chris Bobbe <cbobbe@zulip.com>
2022-04-07 17:50:07 -0700

    react-native-webview [nfc]: Remove defs for "UIManager"-related things
    I haven't found anything suggesting that these are supposed to be
    consumer-facing. The docs don't mention "UI manager"-anything:
      https://github.com/react-native-webview/react-native-webview/tree/5e73b2089/docs
    
    src/WebView.foo.tsx uses React Native's UIManager, e.g.,
      https://github.com/react-native-webview/react-native-webview/blob/5e73b2089/src/WebView.android.tsx
    But, looking at where those files use UIManager, they actually seem
    to be wrapping it so the consumer doesn't have to interact with it.
    
    This lets us get rid of some pretty shaky type definitions from a
    Flow translation of RN's TypeScript libdef @types/react-native,
    which is out of date with RN itself and possibly never quite matched
    the RN implementation.

--- flow-typed/react-native-webview_v11.x.x.js
+++ flow-typed/react-native-webview_v11.x.x.js
@@ -21,144 +21,6 @@ declare module 'react-native-webview/@@react-native' {
     zoomScale: number,
   |};
 
-  declare export type UIManagerStatic = {|
-    /**
-     * Capture an image of the screen, window or an individual view. The image
-     * will be stored in a temporary file that will only exist for as long as the
-     * app is running.
-     *
-     * The `view` argument can be the literal string `window` if you want to
-     * capture the entire window, or it can be a reference to a specific
-     * React Native component.
-     *
-     * The `options` argument may include:
-     * - width/height (number) - the width and height of the image to capture.
-     * - format (string) - either 'png' or 'jpeg'. Defaults to 'png'.
-     * - quality (number) - the quality when using jpeg. 0.0 - 1.0 (default).
-     *
-     * Returns a Promise<string> (tempFilePath)
-     * @platform ios
-     */
-    takeSnapshot: (
-      view?: 'window' | React$Element<React$ElementType> | number,
-      options?: {|
-        width?: number,
-        height?: number,
-        format?: 'png' | 'jpeg',
-        quality?: number,
-      |},
-    ) => Promise<string>,
-
-    /**
-     * Determines the location on screen, width, and height of the given view and
-     * returns the values via an async callback. If successful, the callback will
-     * be called with the following arguments:
-     *
-     *  - x
-     *  - y
-     *  - width
-     *  - height
-     *  - pageX
-     *  - pageY
-     *
-     * Note that these measurements are not available until after the rendering
-     * has been completed in native. If you need the measurements as soon as
-     * possible, consider using the [`onLayout`
-     * prop](docs/view.html#onlayout) instead.
-     *
-     * @deprecated Use `ref.measure` instead.
-     */
-    measure(node: number, callback: MeasureOnSuccessCallback): void,
-
-    /**
-     * Determines the location of the given view in the window and returns the
-     * values via an async callback. If the React root view is embedded in
-     * another native view, this will give you the absolute coordinates. If
-     * successful, the callback will be called with the following
-     * arguments:
-     *
-     *  - x
-     *  - y
-     *  - width
-     *  - height
-     *
-     * Note that these measurements are not available until after the rendering
-     * has been completed in native.
-     *
-     * @deprecated Use `ref.measureInWindow` instead.
-     */
-    measureInWindow(node: number, callback: MeasureInWindowOnSuccessCallback): void,
-
-    /**
-     * Like [`measure()`](#measure), but measures the view relative an ancestor,
-     * specified as `relativeToNativeNode`. This means that the returned x, y
-     * are relative to the origin x, y of the ancestor view.
-     *
-     * As always, to obtain a native node handle for a component, you can use
-     * `React.findNodeHandle(component)`.
-     *
-     * @deprecated Use `ref.measureLayout` instead.
-     */
-    measureLayout(
-      node: number,
-      relativeToNativeNode: number,
-      onFail: () => void,
-      onSuccess: MeasureLayoutOnSuccessCallback,
-    ): void,
-
-    /**
-     * Automatically animates views to their new positions when the
-     * next layout happens.
-     *
-     * A common way to use this API is to call it before calling `setState`.
-     *
-     * Note that in order to get this to work on **Android** you need to set the following flags via `UIManager`:
-     *
-     *     UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
-     */
-    setLayoutAnimationEnabledExperimental(value: boolean): void,
-
-    /**
-     * Used to display an Android PopupMenu. If a menu item is pressed, the success callback will
-     * be called with the following arguments:
-     *
-     *  - item - the menu item.
-     *  - index - index of the pressed item in array. Returns `undefined` if cancelled.
-     *
-     * To obtain a native node handle for a component, you can use
-     * `React.findNodeHandle(component)`.
-     *
-     * Note that this works only on Android
-     */
-    showPopupMenu(
-      node: number,
-      items: string[],
-      error: () => void,
-      success: (item: string, index: number | void) => void,
-    ): void,
-
-    getViewManagerConfig: (
-      name: string,
-    ) => {|
-      Commands: {|
-        [key: string]: number,
-      |},
-    |},
-
-    /**
-     * Used to call a native view method from JavaScript
-     *
-     * reactTag - Id of react view.
-     * commandID - Id of the native method that should be called.
-     * commandArgs - Args of the native method that we can pass from JS to native.
-     */
-    dispatchViewManagerCommand: (
-      reactTag: number | null,
-      commandID: number,
-      commandArgs?: Array<any>,
-    ) => void,
-  |};
-
   declare type Falsy = void | null | false;
   declare type RecursiveArray<T> = Array<T | RecursiveArray<T> | $ReadOnlyArray<T>>;
   /**
@@ -199,34 +61,15 @@ declare module 'react-native-webview' {
     ViewProps,
     StyleProp,
     ViewStyle,
-    UIManagerStatic,
     NativeScrollEvent,
   } from 'react-native-webview/@@react-native';
 
-  declare type WebViewCommands =
-    | 'goForward'
-    | 'goBack'
-    | 'reload'
-    | 'stopLoading'
-    | 'postMessage'
-    | 'injectJavaScript'
-    | 'loadUrl'
-    | 'requestFocus';
-  declare type AndroidWebViewCommands = 'clearHistory' | 'clearCache' | 'clearFormData';
-  declare type RNCWebViewUIManager<Commands: string> = {|
-    ...$Exact<UIManagerStatic>,
+  // Not represented:
+  //   RNCWebViewUIManagerAndroid
+  //   RNCWebViewUIManagerIOS
+  //   RNCWebViewUIManagerMacOS
+  //   RNCWebViewUIManagerWindows
 
-    getViewManagerConfig: (
-      name: string,
-    ) => {|
-      Commands: $ObjMapi<{| [k: Commands]: any |}, <key>(key) => number>,
-    |},
-  |};
-  declare export type RNCWebViewUIManagerAndroid = RNCWebViewUIManager<
-    WebViewCommands | AndroidWebViewCommands, >;
-  declare export type RNCWebViewUIManagerIOS = RNCWebViewUIManager<WebViewCommands>;
-  declare export type RNCWebViewUIManagerMacOS = RNCWebViewUIManager<WebViewCommands>;
-  declare export type RNCWebViewUIManagerWindows = RNCWebViewUIManager<WebViewCommands>;
   declare type WebViewState = 'IDLE' | 'LOADING' | 'ERROR';
   declare type BaseState = {|
     viewState: WebViewState,
