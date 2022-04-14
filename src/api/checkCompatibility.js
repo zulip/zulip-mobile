db1114b6da73d30b8f056204a27eab7ec7d81bac
Chris Bobbe <cbobbe@zulip.com>
2022-04-13 18:22:12 -0700

    api types: Mark checkCompatibility as returning proper Promise<Response>
    This is now appropriate, following RN v0.65, where React Native
    stopped clobbering the Response type with `any`.

--- src/api/checkCompatibility.js
+++ src/api/checkCompatibility.js
@@ -3,7 +3,7 @@ import userAgent from '../utils/userAgent';
 
 // check with server if current mobile app is compatible with latest backend
 // compatibility fails only if server responds with 400 (but not with 200 or 404)
-export default (): Promise<{ status: number, ... }> =>
+export default (): Promise<Response> =>
   fetch('https://zulip.com/compatibility', {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
