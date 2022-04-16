0c56883ef5afb63dad9dc03992d0c3123f1d21aa
Chris Bobbe <cbobbe@zulip.com>
2022-04-15 17:21:24 -0700

    api types: Sync updateStream's params with API doc at FL 121

--- src/api/streams/updateStream.js
+++ src/api/streams/updateStream.js
@@ -6,19 +6,42 @@ import { apiPatch } from '../apiFetch';
 /**
  * https://zulip.com/api/update-stream
  *
- * NB the caller must adapt for old-server compatibility; see comment.
+ * NB the caller must adapt for old-server compatibility; see comments.
  */
-// TODO(#4659): Once we pass the feature level to API methods, this one
-//   should encapsulate a switch at feature level 64.  See its call sites.
-// TODO(server-4.0): Simplify that away.
+// Current to FL 121; property ordering follows the doc.
 export default (
   auth: Auth,
   id: number,
   params: $ReadOnly<{|
+    // TODO(#4659): Once we pass the feature level to API methods, this one
+    //   should encapsulate a switch at FL 64 related to these two parameters.
+    //   See call sites.
     description?: $PropertyType<Stream, 'description'>,
     new_name?: $PropertyType<Stream, 'name'>,
 
     // controls the invite_only property
     is_private?: $PropertyType<Stream, 'invite_only'>,
+
+    // TODO(server-5.0): New in FL 98.
+    is_web_public?: $PropertyType<Stream, 'is_web_public'>,
+
+    // TODO(server-3.0): New in FL 1; for older servers, pass is_announcement_only.
+    stream_post_policy?: $PropertyType<Stream, 'stream_post_policy'>,
+
+    history_public_to_subscribers?: $PropertyType<Stream, 'history_public_to_subscribers'>,
+
+    // Doesn't take the same special values as Stream.is_announcement_only!
+    //   https://chat.zulip.org/#narrow/stream/412-api-documentation/topic/message_retention_days/near/1367895
+    // TODO(server-3.0): New in FL 17
+    message_retention_days?:
+      | number
+      | 'realm_default'
+      // TODO(server-5.0): Added in FL 91, replacing 'forever'.
+      | 'unlimited'
+      // TODO(server-5.0): Replaced in FL 91 by 'unlimited'.
+      | 'forever',
+
+    // TODO(server-3.0): Replaced in FL 1 by 'stream_post_policy'.
+    is_announcement_only?: $PropertyType<Stream, 'is_announcement_only'>,
   |}>,
 ): Promise<ApiResponse> => apiPatch(auth, `streams/${id}`, params);
