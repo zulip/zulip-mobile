7fee95bbdc8e70209b71f31c60176f9794b4d4a9
Chris Bobbe <cbobbe@zulip.com>
2022-04-18 13:39:28 -0700

    api types [nfc]: Express createStream's params by referring to Stream type

--- src/api/streams/createStream.js
+++ src/api/streams/createStream.js
@@ -1,5 +1,6 @@
 /* @flow strict-local */
 import type { ApiResponse, Auth } from '../transportTypes';
+import type { Stream } from '../modelTypes';
 import { apiPost } from '../apiFetch';
 
 /** See https://zulip.com/api/create-stream */
@@ -8,9 +9,9 @@ export default (
   streamAttributes: $ReadOnly<{|
     // Ordered by their appearance in the doc.
 
-    name: string,
-    description?: string,
-    invite_only?: boolean,
+    name: $PropertyType<Stream, 'name'>,
+    description?: $PropertyType<Stream, 'description'>,
+    invite_only?: $PropertyType<Stream, 'invite_only'>,
   |}>,
   options?: $ReadOnly<{|
     // TODO(server-3.0): Send numeric user IDs (#3764), not emails.
