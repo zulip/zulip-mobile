b97eddbbc88bf21068b796519ade3a60060fd6c4
Chris Bobbe <cbobbe@zulip.com>
2022-04-19 15:04:55 -0700

    streamsReducer tests [nfc]: Make more compact
    Well, sort of.

--- src/streams/__tests__/streamsReducer-test.js
+++ src/streams/__tests__/streamsReducer-test.js
@@ -10,13 +10,9 @@ import streamsReducer from '../streamsReducer';
 describe('streamsReducer', () => {
   describe('ACCOUNT_SWITCH', () => {
     test('resets state to initial state', () => {
-      const initialState = deepFreeze([eg.makeStream({ name: 'some_stream' })]);
-
-      const expectedState = [];
-
-      const actualState = streamsReducer(initialState, eg.action.account_switch);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        streamsReducer([eg.makeStream({ name: 'some_stream' })], eg.action.account_switch),
+      ).toEqual([]);
     });
   });
 
@@ -25,23 +21,20 @@ describe('streamsReducer', () => {
       const stream1 = eg.makeStream({ name: 'some stream', stream_id: 1 });
       const stream2 = eg.makeStream({ name: 'some other stream', stream_id: 2 });
 
-      const initialState = deepFreeze([]);
-
-      const action = deepFreeze({
-        type: EVENT,
-        event: {
-          id: 0,
-          type: EventTypes.stream,
-          op: 'create',
-          streams: [stream1, stream2],
-        },
-      });
-
-      const expectedState = [stream1, stream2];
-
-      const newState = streamsReducer(initialState, action);
-
-      expect(newState).toEqual(expectedState);
+      expect(
+        streamsReducer(
+          [],
+          deepFreeze({
+            type: EVENT,
+            event: {
+              id: 0,
+              type: EventTypes.stream,
+              op: 'create',
+              streams: [stream1, stream2],
+            },
+          }),
+        ),
+      ).toEqual([stream1, stream2]);
     });
 
     test('if stream already exist, do not add it', () => {
@@ -52,23 +45,20 @@ describe('streamsReducer', () => {
       });
       const stream2 = eg.makeStream({ name: 'some other stream', stream_id: 2 });
 
-      const initialState = deepFreeze([stream1]);
-
-      const action = deepFreeze({
-        type: EVENT,
-        event: {
-          id: 0,
-          type: EventTypes.stream,
-          op: 'create',
-          streams: [stream1, stream2],
-        },
-      });
-
-      const expectedState = [stream1, stream2];
-
-      const newState = streamsReducer(initialState, action);
-
-      expect(newState).toEqual(expectedState);
+      expect(
+        streamsReducer(
+          [stream1],
+          deepFreeze({
+            type: EVENT,
+            event: {
+              id: 0,
+              type: EventTypes.stream,
+              op: 'create',
+              streams: [stream1, stream2],
+            },
+          }),
+        ),
+      ).toEqual([stream1, stream2]);
     });
   });
 
@@ -90,46 +80,40 @@ describe('streamsReducer', () => {
         name: 'third stream',
       });
 
-      const initialState = deepFreeze([stream1, stream2, stream3]);
+      const newState = streamsReducer(
+        [stream1, stream2, stream3],
+        deepFreeze({
+          type: EVENT,
+          event: {
+            id: 0,
+            type: EventTypes.stream,
+            op: 'delete',
+            streams: [stream1, stream2],
+          },
+        }),
+      );
 
-      const action = deepFreeze({
-        type: EVENT,
-        event: {
-          id: 0,
-          type: EventTypes.stream,
-          op: 'delete',
-          streams: [stream1, stream2],
-        },
-      });
-
-      const expectedState = [stream3];
-
-      const newState = streamsReducer(initialState, action);
-
-      expect(newState).toEqual(expectedState);
+      expect(newState).toEqual([stream3]);
     });
 
     test('removes streams that exist, do nothing if not', () => {
       const stream1 = eg.makeStream({ name: 'some stream', stream_id: 1 });
       const stream2 = eg.makeStream({ name: 'some other stream', stream_id: 2 });
 
-      const initialState = deepFreeze([stream1]);
-
-      const action = deepFreeze({
-        type: EVENT,
-        event: {
-          id: 0,
-          type: EventTypes.stream,
-          op: 'delete',
-          streams: [stream1, stream2],
-        },
-      });
-
-      const expectedState = [];
-
-      const newState = streamsReducer(initialState, action);
-
-      expect(newState).toEqual(expectedState);
+      expect(
+        streamsReducer(
+          [stream1],
+          deepFreeze({
+            type: EVENT,
+            event: {
+              id: 0,
+              type: EventTypes.stream,
+              op: 'delete',
+              streams: [stream1, stream2],
+            },
+          }),
+        ),
+      ).toEqual([]);
     });
   });
 
@@ -139,26 +123,23 @@ describe('streamsReducer', () => {
       const stream67 = eg.makeStream({ stream_id: 67, name: 'design' });
       const stream53 = eg.makeStream({ stream_id: 53, name: 'mobile' });
 
-      const initialState = deepFreeze([stream123, stream67, stream53]);
-
-      const action = deepFreeze({
-        type: EVENT,
-        event: {
-          id: 2,
-          type: EventTypes.stream,
-          op: 'update',
-          stream_id: 123,
-          name: 'competition',
-          property: 'name',
-          value: 'real competition',
-        },
-      });
-
-      const expectedState = [{ ...stream123, name: 'real competition' }, stream67, stream53];
-
-      const actualState = streamsReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        streamsReducer(
+          [stream123, stream67, stream53],
+          deepFreeze({
+            type: EVENT,
+            event: {
+              id: 2,
+              type: EventTypes.stream,
+              op: 'update',
+              stream_id: 123,
+              name: 'competition',
+              property: 'name',
+              value: 'real competition',
+            },
+          }),
+        ),
+      ).toEqual([{ ...stream123, name: 'real competition' }, stream67, stream53]);
     });
 
     test('Change the description property', () => {
@@ -174,26 +155,23 @@ describe('streamsReducer', () => {
       });
       const stream53 = eg.makeStream({ stream_id: 53, name: 'mobile', description: 'android' });
 
-      const initialState = deepFreeze([stream123, stream67, stream53]);
-
-      const action = deepFreeze({
-        type: EVENT,
-        event: {
-          id: 2,
-          type: EventTypes.stream,
-          op: 'update',
-          stream_id: 53,
-          name: 'mobile',
-          property: 'description',
-          value: 'iOS + android',
-        },
-      });
-
-      const expectedState = [stream123, stream67, { ...stream53, description: 'iOS + android' }];
-
-      const actualState = streamsReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      expect(
+        streamsReducer(
+          [stream123, stream67, stream53],
+          deepFreeze({
+            type: EVENT,
+            event: {
+              id: 2,
+              type: EventTypes.stream,
+              op: 'update',
+              stream_id: 53,
+              name: 'mobile',
+              property: 'description',
+              value: 'iOS + android',
+            },
+          }),
+        ),
+      ).toEqual([stream123, stream67, { ...stream53, description: 'iOS + android' }]);
     });
   });
 });
