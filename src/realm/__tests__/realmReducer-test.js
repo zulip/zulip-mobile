5c1938f5ea45a9db3f353485ab881662d9e77557
Chris Bobbe <cbobbe@zulip.com>
2022-05-16 12:13:32 -0700

    realm reducer tests [nfc]: Remove unused conditional
    We're only testing one branch of this conditional, the one for
    recent servers.

--- src/realm/__tests__/realmReducer-test.js
+++ src/realm/__tests__/realmReducer-test.js
@@ -33,9 +33,9 @@ describe('realmReducer', () => {
         messageContentDeleteLimitSeconds: action.data.realm_message_content_delete_limit_seconds,
         messageContentEditLimitSeconds: action.data.realm_message_content_edit_limit_seconds,
         pushNotificationsEnabled: action.data.realm_push_notifications_enabled,
-        webPublicStreamsEnabled: action.data.server_web_public_streams_enabled ?? false,
-        createWebPublicStreamPolicy: action.data.realm_create_web_public_stream_policy ?? 6,
-        enableSpectatorAccess: action.data.realm_enable_spectator_access ?? false,
+        webPublicStreamsEnabled: action.data.server_web_public_streams_enabled,
+        createWebPublicStreamPolicy: action.data.realm_create_web_public_stream_policy,
+        enableSpectatorAccess: action.data.realm_enable_spectator_access,
 
         //
         // InitialDataRealmUser
