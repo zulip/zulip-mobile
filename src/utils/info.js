2886b8916e791d741d60e4647d29e877b1d6d421
Chris Bobbe <cbobbe@zulip.com>
2022-05-18 17:17:31 -0700

    info: Let `showErrorAlert` take a URL for a "Learn more" button.

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,10 +1,36 @@
 /* @flow strict-local */
+import invariant from 'invariant';
 import { Alert } from 'react-native';
 import Toast from 'react-native-simple-toast';
+
+import type { GlobalSettingsState } from '../types';
+import { openLinkWithUserPreference } from './openLink';
 
 export const showToast = (message: string) => {
   Toast.show(message);
 };
 
-export const showErrorAlert = (title: string, message?: string): void =>
-  Alert.alert(title, message, [{ text: 'OK', onPress: () => {} }], { cancelable: true });
+export const showErrorAlert = (
+  title: string,
+  message?: string,
+  learnMoreURL?: URL,
+
+  // Required if learnMoreURL passed (used to open the link)
+  globalSettings?: GlobalSettingsState,
+): void => {
+  // TODO: Translate button text
+
+  const buttons = [];
+  if (learnMoreURL) {
+    invariant(globalSettings !== undefined, 'learnMoreURL is passed; globalSettings should be too');
+    buttons.push({
+      text: 'Learn more',
+      onPress: () => {
+        openLinkWithUserPreference(learnMoreURL.toString(), globalSettings);
+      },
+    });
+  }
+  buttons.push({ text: 'OK', onPress: () => {} });
+
+  Alert.alert(title, message, buttons, { cancelable: true });
+};
