15832797ab19e030f0e95f72a3aab0b418d67892
Chris Bobbe <cbobbe@zulip.com>
2022-05-25 12:51:49 -0700

    EditStreamScreen [nfc]: Stop changing initialValues sent to EditStreamCard
    This is NFC because EditStreamCard has already been using a snapshot
    of initialValues that it makes on its first render. But since the
    prop is named `initialValues`, we shouldn't be passing something
    that changes to be different from the initial values.
    
    This will help with an upcoming change; see Greg's comment at
      https://github.com/zulip/zulip-mobile/pull/5389#discussion_r881187606

--- src/streams/EditStreamScreen.js
+++ src/streams/EditStreamScreen.js
@@ -1,5 +1,5 @@
 /* @flow strict-local */
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import type { Node } from 'react';
 
 import type { RouteProp } from '../react-navigation';
@@ -22,6 +22,13 @@ export default function EditStreamScreen(props: Props): Node {
   const dispatch = useDispatch();
   const stream = useSelector(state => getStreamForId(state, props.route.params.streamId));
 
+  // What we pass for EditStreamCard's `initialValues` should be constant.
+  const initialValues = useRef({
+    name: stream.name,
+    description: stream.description,
+    privacy: stream.invite_only ? 'private' : 'public',
+  }).current;
+
   const handleComplete = useCallback(
     (name, description, privacy) => {
       dispatch(
@@ -41,11 +48,7 @@ export default function EditStreamScreen(props: Props): Node {
       <EditStreamCard
         navigation={navigation}
         isNewStream={false}
-        initialValues={{
-          name: stream.name,
-          description: stream.description,
-          privacy: stream.invite_only ? 'private' : 'public',
-        }}
+        initialValues={initialValues}
         onComplete={handleComplete}
       />
     </Screen>
