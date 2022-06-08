dde18611470c7aefde1c599b2617e669f5b55f5a
Greg Price <greg@zulip.com>
2022-06-07 21:34:59 -0700

    deps: Upgrade react-native-safe-area-context and react-native-screens
    These need an upgrade in order to upgrade react-navigation to 6.x:
      https://reactnavigation.org/docs/upgrading-from-5.x/#minimum-requirements
    
    The react-native-safe-area-context upgrade is also needed as part of
    the fix (found by Chris) for a conspicuous layout issue, where the
    recently-added app bar on the "streams" screen gets messed up on iOS:
      https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/Regression.20in.20.22Streams.22.20screen/near/1412459
    
    Performed the upgrade with:
    
        $ yarn upgrade --latest react-native-safe-area-context react-native-screens
    
    followed by updating the react-native-safe-area-context type
    definitions like so:
    
        $ git commit -am 'wip partial upgrade'
        $ tools/tsflower unpack
            # … resolved a rebase conflict, fixed some new type errors …
        $ tools/tsflower pack
        $ git commit --amend
    
    Then in our own code, dropped enabling react-native-screens,
    because that's now done by default:
      https://github.com/software-mansion/react-native-screens/releases/tag/3.0.0

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -5,8 +5,6 @@ import { Platform, UIManager } from 'react-native';
 import 'react-native-url-polyfill/auto';
 // $FlowFixMe[untyped-import]
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
-// $FlowFixMe[untyped-import]
-import { enableScreens } from 'react-native-screens';
 
 import RootErrorBoundary from './RootErrorBoundary';
 import ZulipNavigationContainer from './nav/ZulipNavigationContainer';
@@ -21,8 +19,6 @@ import { initializeSentry } from './sentry';
 import ZulipSafeAreaProvider from './boot/ZulipSafeAreaProvider';
 
 initializeSentry();
-
-enableScreens();
 
 // $FlowFixMe[prop-missing]
 console.disableYellowBox = true; // eslint-disable-line
