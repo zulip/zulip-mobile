7b87bc1fc498bac02d31e0b6957e84c44896158b
Chris Bobbe <cbobbe@zulip.com>
2022-07-07 17:02:55 -0700

    StoreProvider [nfc]: Split useEffect into two useEffects
    With the `observeStore` effect first, since we want to make sure the
    observer is in place before `restore` finishes its work; see
      https://github.com/zulip/zulip-mobile/pull/5438#issuecomment-1232296713

--- src/boot/StoreProvider.js
+++ src/boot/StoreProvider.js
@@ -14,32 +14,36 @@ type Props = $ReadOnly<{|
 |}>;
 
 export default function StoreProvider(props: Props): Node {
+  useEffect(
+    () =>
+      observeStore(
+        store,
+        // onChange will fire when this value changes
+        state => {
+          const perAccountState = tryGetActiveAccountState(state);
+          if (!perAccountState) {
+            return undefined;
+          }
+          return getAccount(perAccountState).zulipVersion;
+        },
+        zulipVersion => {
+          // TODO(#5005): This is for the *active* account; that may not be
+          //   the one a given piece of code is working with!
+          //
+          // On fetch, we'll have called this already before entering the
+          // reducers, so this will be redundant.  But on switching accounts,
+          // this is the call that will make the change.
+          logging.setTagsFromServerVersion(zulipVersion);
+        },
+      ),
+    [],
+  );
+
   useEffect(() => {
     timing.start('Store hydration');
     restore(() => {
       timing.end('Store hydration');
     });
-
-    return observeStore(
-      store,
-      // onChange will fire when this value changes
-      state => {
-        const perAccountState = tryGetActiveAccountState(state);
-        if (!perAccountState) {
-          return undefined;
-        }
-        return getAccount(perAccountState).zulipVersion;
-      },
-      zulipVersion => {
-        // TODO(#5005): This is for the *active* account; that may not be
-        //   the one a given piece of code is working with!
-        //
-        // On fetch, we'll have called this already before entering the
-        // reducers, so this will be redundant.  But on switching accounts,
-        // this is the call that will make the change.
-        logging.setTagsFromServerVersion(zulipVersion);
-      },
-    );
   }, []);
 
   return <Provider store={store}>{props.children}</Provider>;
