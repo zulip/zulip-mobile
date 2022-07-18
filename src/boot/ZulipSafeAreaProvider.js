c40dae30046ed9b7be84c49de6d91f7fbba95e9d
Greg Price <greg@zulip.com>
2022-07-18 11:30:32 -0700

    safe-area: Get background color from theme, when hydrated
    At this stage, I believe the only effect of this change is one
    that's virtually imperceptible: when we finish hydrating and move
    from the loading screen to the inbox, this causes us to flash the
    theme background color (the color of the upcoming screen) instead of
    Zulip blue (the color of the screen we're leaving.)
    
    This will be more useful when we enable react-native-screens, which
    will cause this SafeAreaProvider background color to appear in more
    situations, where there's no screenful of Zulip blue and so it does
    stand out while the theme background color doesn't.

--- src/boot/ZulipSafeAreaProvider.js
+++ src/boot/ZulipSafeAreaProvider.js
@@ -3,21 +3,31 @@ import * as React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { BRAND_COLOR } from '../styles';
+import { useGlobalSelector } from '../react-redux';
+import { getGlobalSettings, getIsHydrated } from '../directSelectors';
+import { themeData } from '../styles/theme';
 
 type Props = {|
   +children: React.Node,
 |};
 
 export default function ZulipSafeAreaProvider(props: Props): React.Node {
-  return (
-    <SafeAreaProvider
-      style={{
-        // While waiting for the safe-area insets, this will
-        // show. Best for it not to be a white flicker.
-        backgroundColor: BRAND_COLOR,
-      }}
-    >
-      {props.children}
-    </SafeAreaProvider>
-  );
+  // This background color appears in at least one situation:
+  //
+  //  * At startup, just after the loading screen, it covers the whole
+  //    screen as a brief flash, just a frame or so.
+  //
+  // We can make this quirk virtually invisible by giving it the background
+  // color used across the app.
+  const backgroundColor = useGlobalSelector(state => {
+    if (!getIsHydrated(state)) {
+      // The only screen we'll be showing at this point is the loading
+      // screen.  Match that screen's background.
+      return BRAND_COLOR;
+    }
+
+    return themeData[getGlobalSettings(state).theme].backgroundColor;
+  });
+
+  return <SafeAreaProvider style={{ backgroundColor }}>{props.children}</SafeAreaProvider>;
 }
