44d641aa016d200a9520c3277924e1c9b819bfa8
Chris Bobbe <cbobbe@zulip.com>
2022-08-05 18:53:08 -0700

    cssEmojis [nfc]: Use availableUnicodeEmojiCodes instead of codeToEmojiMap
    They're the same codes.

--- src/webview/css/cssEmojis.js
+++ src/webview/css/cssEmojis.js
@@ -1,11 +1,14 @@
 /* @flow strict-local */
-import { codeToEmojiMap } from '../../emoji/data';
+import { codeToEmojiMap, availableUnicodeEmojiCodes } from '../../emoji/data';
 
 const codeToCss = (code: string): string =>
   `.emoji-${code}:before { content: '${codeToEmojiMap[code]}'; }`;
 
-const cssEmojis: string = Object.keys(codeToEmojiMap)
-  .map(key => codeToCss(key))
-  .join('\n');
+// TODO(#2956): Have caller pass available codes from server data instead.
+const chunks = [];
+for (const code of availableUnicodeEmojiCodes) {
+  chunks.push(codeToCss(code));
+}
+const cssEmojis: string = chunks.join('\n');
 
 export default cssEmojis;
