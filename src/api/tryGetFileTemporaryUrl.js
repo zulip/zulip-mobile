7b5f5f631c02beef47587cb8d713ac7b32df5274
Chris Bobbe <cbobbe@zulip.com>
2022-09-09 13:24:40 -0700

    api.tryGetFileTemporaryUrl [nfc]: Simplify jsdoc

--- src/api/tryGetFileTemporaryUrl.js
+++ src/api/tryGetFileTemporaryUrl.js
@@ -3,13 +3,7 @@ import type { Auth } from './apiTypes';
 import getFileTemporaryUrl from './messages/getFileTemporaryUrl';
 
 /**
- * Get the complete authless URL to a realm uploaded file, returns null if that
- * fails.
- *
- * See aslo jsdoc on getFileTemporaryUrl.
- *
- * @param href URL path to the resource.
- * @param auth Current user's auth information.
+ * Like getFileTemporaryUrl, but on error returns null instead of throwing.
  */
 export default async (href: string, auth: Auth): Promise<URL | null> => {
   try {
