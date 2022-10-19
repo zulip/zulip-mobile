868ba9c2ef343efb25957a3ffd47a1553af8aded
Greg Price <greg@zulip.com>
2022-10-19 10:03:50 -0700

    api [nfc]: Expand comments on kNotificationBotEmail

--- src/api/constants.js
+++ src/api/constants.js
@@ -22,5 +22,9 @@ export const kNoTopicTopic: string = '(no topic)';
  */
 // Copied from
 //   https://github.com/zulip/zulip/blob/f6f7e4c53/zproject/default_settings.py#L231 .
-// TODO: Make all servers give us a way to identify the notification bot.
+// The web app also makes the assumption that Notification Bot has this email:
+//   https://github.com/zulip/zulip/blob/eec294b25/static/js/read_receipts.js#L22-L26
+// TODO: Make all servers give us a way to identify the notification bot, or
+//   at least the messages it sends.  E.g., the mooted "message type" concept:
+//   https://chat.zulip.org/#narrow/stream/378-api-design/topic/message.20type.20field/near/1230581
 export const kNotificationBotEmail: string = 'notification-bot@zulip.com';
