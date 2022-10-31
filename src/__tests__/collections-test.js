1852c8b34bc39926177bd741fce09da29b17b9e1
Greg Price <greg@zulip.com>
2022-10-31 13:42:51 -0700

    collections: Add symmetricDiff
    Hooray for unit tests -- these actually caught a silly bug in my
    original draft.  I'd even done some manual testing of a new feature in
    a draft implementation built on this, and that just happened to avoid
    triggering the bug.

--- src/__tests__/collections-test.js
+++ src/__tests__/collections-test.js
@@ -1,6 +1,6 @@
 /* @flow strict-local */
 
-import { mapOrNull } from '../collections';
+import { mapOrNull, symmetricDiff } from '../collections';
 
 describe('mapOrNull', () => {
   test('behaves right in happy case', () => {
@@ -20,4 +20,20 @@ describe('mapOrNull', () => {
     });
     expect(log).toEqual([1, 2]);
   });
+});
+
+describe('symmetricDiff', () => {
+  for (const [desc, as, bs, aOnly, bOnly] of [
+    ['empty', [], [], [], []],
+    ['empty/inhabited', [], [1, 2], [], [1, 2]],
+    ['inhabited/empty', [1, 2], [], [1, 2], []],
+    ['equal', [1, 2], [1, 2], [], []],
+    ['extra a', [1, 2, 3, 4, 5, 6], [2, 5], [1, 3, 4, 6], []],
+    ['extra b', [2, 5], [1, 2, 3, 4, 5, 6], [], [1, 3, 4, 6]],
+    ['both extra in same place', [1, 3, 4, 5, 7, 8], [2, 3, 6, 7, 9], [1, 4, 5, 8], [2, 6, 9]],
+  ]) {
+    test(desc, () => {
+      expect(symmetricDiff(as, bs)).toEqual([aOnly, bOnly]);
+    });
+  }
 });
--- src/collections.js
+++ src/collections.js
@@ -2,7 +2,7 @@
  * Handy generic functions for operating on JS collections.
  *
  * Any function here is the sort of thing that perhaps should be in the
- * language's standard library.
+ * language's standard library, or is just a bit too algorithmical for that.
  *
  * See also
  *  * `jsBackport.js`, for things that actually are in newer versions of JS,
@@ -38,4 +38,37 @@ export function mapOrNull<T, U>(
     result.push(r);
   }
   return result;
+}
+
+/**
+ * Find items that are in one but not both of the given arrays.
+ *
+ * The two returned arrays consist of items in only the first array, and
+ * items in only the second array, respectively.
+ *
+ * Both input arrays must be sorted.  The output arrays will also be sorted.
+ *
+ * This function takes linear time, i.e., O(as.length + bs.length).
+ *
+ * (The implementation would work just as well for strings; it only needs
+ * `<`.  The type is narrower just because `number | string` would mean
+ * comparing numbers and strings to each other, which in JS is no good.)
+ */
+// prettier-ignore
+export function symmetricDiff(
+  as: $ReadOnlyArray<number>,
+  bs: $ReadOnlyArray<number>,
+): [number[], number[]] {
+  /* eslint-disable one-var */
+  const aOnly = [], bOnly = [];
+  let ia = 0, ib = 0;
+  while (ia < as.length && ib < bs.length) {
+    const a = as[ia], b = bs[ib];
+    if    (a === b) { ia++; ib++; }
+    else if (a < b) { aOnly.push(a); ia++; }
+    else            { bOnly.push(b); ib++; }
+  }
+  if      (ia < as.length) { aOnly.push(...as.slice(ia)); }
+  else if (ib < bs.length) { bOnly.push(...bs.slice(ib)); }
+  return [aOnly, bOnly];
 }
