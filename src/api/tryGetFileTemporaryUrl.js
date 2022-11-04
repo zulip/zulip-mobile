9eeddd6a413cd83579337dff197d62cff4c8cbd2
Chris Bobbe <cbobbe@zulip.com>
2022-11-04 16:36:33 -0700

    api.tryGetFileTemporaryUrl: Reject off-realm URLs

--- src/api/tryGetFileTemporaryUrl.js
+++ src/api/tryGetFileTemporaryUrl.js
@@ -1,17 +1,17 @@
 /* @flow strict-local */
 import type { Auth } from './apiTypes';
-import { tryParseUrl } from '../utils/url';
+import { isUrlOnRealm, tryParseUrl } from '../utils/url';
 import getFileTemporaryUrl from './messages/getFileTemporaryUrl';
 
 /**
  * Like getFileTemporaryUrl, but on error returns null instead of throwing.
  *
- * Validates that `href` parses to a URL object with auth.realm.
+ * Validates that `href` parses to an on-realm URL object with auth.realm.
  */
 export default async (href: string, auth: Auth): Promise<URL | null> => {
   // TODO: Have this function take this parsed form instead of raw string.
   const parsedUrl = tryParseUrl(href, auth.realm);
-  if (!parsedUrl) {
+  if (!parsedUrl || !isUrlOnRealm(parsedUrl, auth.realm)) {
     return null;
   }
   // TODO: Stop using unparsed `href` below this point.
