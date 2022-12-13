08284ba06d08c37a8bcc82d40d87a1ec697c0738
Greg Price <greg@zulip.com>
2022-12-13 12:13:54 -0800

    ios: Add option to use RN's "New Architecture"
    The bulk of this code only does anything if we enable New Arch.
    I'm not sure if some of the other bits might have some behavioral
    effect, though.
    
    This squashes together the following template changes upstream on the
    way to RN v0.68:
    
    facebook/react-native@73a04d145 Refactor the iOS app template to move setups to a helper class
    facebook/react-native@8ec0e6919 Add turbo module support in the default app template
    facebook/react-native@2e9a376c8 Add fabric option to the default app template.
    facebook/react-native@c0c543995 Rename the new architecture flag to RCT_NEW_ARCH_ENABLED
    facebook/react-native@1cd8f05ee Refactor app template setup util functions
    facebook/react-native@6a046fb9f Fix build issue for the new app template
    facebook/react-native@e330eee9c Add Other C++ flags when RCT_NEW_ARCH_ENABLE=1
    facebook/react-native@ccd170809 … Consider relative to pwd installation root …
    
    They use new APIs introduced to RN at the same time, some of which get
    refactored further by the later commits; the last one fixes a bug a
    previous one introduced in the Podfile.
    
    It also includes:
    
    * Fixing the type of `rootView` back to the more specific `RCTRootView *`,
      rather than `UIView *`.  We need the more specific type when setting
      its `.loadingView` property below.
    
    * Re-following Expo instructions for adding Expo to an existing RN app:
        https://docs.expo.dev/bare/installing-expo-modules/#configuration-for-ios
      This just requires the one hunk of that diff that changes the
      initializer for `rootView`.

--- ios/ZulipMobile/AppDelegate.mm
+++ ios/ZulipMobile/AppDelegate.mm
@@ -9,39 +9,46 @@
 #import <UserNotifications/UserNotifications.h>
 #import <RNCPushNotificationIOS.h>
 
-#ifdef FB_SONARKIT_ENABLED
-  #import <FlipperKit/FlipperClient.h>
-  #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-  #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-  #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-  #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-  #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <React/RCTAppSetupUtils.h>
 
-  static void InitializeFlipper(UIApplication *application) {
-    FlipperClient *client = [FlipperClient sharedClient];
-    SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-    [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-    [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-    [client addPlugin:[FlipperKitReactPlugin new]];
-    [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-    [client start];
-  }
+#if RCT_NEW_ARCH_ENABLED
+#import <React/CoreModulesPlugins.h>
+#import <React/RCTCxxBridgeDelegate.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+#import <React/RCTFabricSurfaceHostingProxyRootView.h>
+#import <React/RCTSurfacePresenter.h>
+#import <React/RCTSurfacePresenterBridgeAdapter.h>
+
+#import <react/config/ReactNativeConfig.h>
+
+@interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
+  RCTTurboModuleManager *_turboModuleManager;
+  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
+  std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
+  facebook::react::ContextContainer::Shared _contextContainer;
+}
+@end
 #endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  #ifdef FB_SONARKIT_ENABLED
-    InitializeFlipper(application);
-  #endif
+  RCTAppSetupPrepareApp(application);
 
   RCTSetLogThreshold(RCTLogLevelError);
 
   RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [self.reactDelegate createRootViewWithBridge:bridge
-                                                      moduleName:@"ZulipMobile"
-                                               initialProperties:nil];
+
+#if RCT_NEW_ARCH_ENABLED
+  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
+  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
+  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
+  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
+  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
+#endif
+
+  RCTRootView *rootView = [self.reactDelegate createRootViewWithBridge:bridge moduleName:@"ZulipMobile" initialProperties:nil];
 
   UIView* loadingView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
   loadingView.backgroundColor = [UIColor colorNamed:@"Brand"];
@@ -139,5 +146,43 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
     | UNNotificationPresentationOptionAlert
  );
 }
+
+#if RCT_NEW_ARCH_ENABLED
+
+#pragma mark - RCTCxxBridgeDelegate
+
+- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
+{
+  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
+                                                             delegate:self
+                                                            jsInvoker:bridge.jsCallInvoker];
+  return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
+}
+
+#pragma mark RCTTurboModuleManagerDelegate
+
+- (Class)getModuleClassFromName:(const char *)name
+{
+  return RCTCoreModulesClassProvider(name);
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
+{
+  return nullptr;
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                     initParams:(const facebook::react::ObjCTurboModule::InitParams &)params
+{
+  return nullptr;
+}
+
+- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
+{
+  return RCTAppSetupDefaultModuleFromClass(moduleClass);
+}
+
+#endif
 
 @end
