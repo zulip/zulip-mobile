692a881ba8e95fbd0d3474064313059e309977da
Chris Bobbe <cbobbe@zulip.com>
2022-12-13 18:10:19 -0800

    fetching tests [nfc]: Tighten code style a bit

--- src/chat/__tests__/fetchingReducer-test.js
+++ src/chat/__tests__/fetchingReducer-test.js
@@ -25,65 +25,43 @@ describe('fetchingReducer', () => {
 
   describe('MESSAGE_FETCH_START', () => {
     test('if messages are fetched before or after the corresponding flag is set', () => {
-      const prevState = deepFreeze({
-        [HOME_NARROW_STR]: { older: false, newer: false },
-      });
-
-      const action = deepFreeze({
-        type: MESSAGE_FETCH_START,
-        narrow: HOME_NARROW,
-        numBefore: 10,
-        numAfter: 10,
-      });
-
-      const expectedState = {
-        [HOME_NARROW_STR]: { older: true, newer: true },
-      };
-
-      const newState = fetchingReducer(prevState, action);
-
-      expect(newState).toEqual(expectedState);
+      const prevState = deepFreeze({ [HOME_NARROW_STR]: { older: false, newer: false } });
+      expect(
+        fetchingReducer(
+          prevState,
+          deepFreeze({
+            type: MESSAGE_FETCH_START,
+            narrow: HOME_NARROW,
+            numBefore: 10,
+            numAfter: 10,
+          }),
+        ),
+      ).toEqual({ [HOME_NARROW_STR]: { older: true, newer: true } });
     });
 
     test('if key for narrow does not exist, it is created and corresponding flags are set', () => {
-      const prevState = deepFreeze({
-        [HOME_NARROW_STR]: { older: false, newer: false },
-      });
-
       const narrow = streamNarrow(eg.stream.stream_id);
-      const action = deepFreeze({
-        type: MESSAGE_FETCH_START,
-        narrow,
-        numBefore: 10,
-        numAfter: 0,
-      });
 
-      const expectedState = {
+      const prevState = deepFreeze({ [HOME_NARROW_STR]: { older: false, newer: false } });
+      expect(
+        fetchingReducer(
+          prevState,
+          deepFreeze({ type: MESSAGE_FETCH_START, narrow, numBefore: 10, numAfter: 0 }),
+        ),
+      ).toEqual({
         [HOME_NARROW_STR]: { older: false, newer: false },
         [keyFromNarrow(narrow)]: { older: true, newer: false },
-      };
-
-      const newState = fetchingReducer(prevState, action);
-
-      expect(newState).toEqual(expectedState);
+      });
     });
 
     test('if fetching for a search narrow, ignore', () => {
-      const prevState = deepFreeze({
-        [HOME_NARROW_STR]: {
-          older: false,
-          newer: false,
-        },
-      });
-
-      const action = deepFreeze({
-        ...eg.action.message_fetch_start,
-        narrow: SEARCH_NARROW('some query'),
-      });
-
-      const newState = fetchingReducer(prevState, action);
-
-      expect(newState).toEqual(prevState);
+      const prevState = deepFreeze({ [HOME_NARROW_STR]: { older: false, newer: false } });
+      expect(
+        fetchingReducer(
+          deepFreeze(prevState),
+          deepFreeze({ ...eg.action.message_fetch_start, narrow: SEARCH_NARROW('some query') }),
+        ),
+      ).toEqual(prevState);
     });
   });
 
@@ -91,66 +69,36 @@ describe('fetchingReducer', () => {
     test('reverses the effect of MESSAGE_FETCH_START as much as possible', () => {
       // As of the addition of this test, that means setting
       // DEFAULT_FETCHING as the key.
-      const state0 = deepFreeze({});
-
-      const messageFetchStartAction = deepFreeze({
-        ...eg.action.message_fetch_start,
-        narrow: HOME_NARROW,
-      });
-
-      const state1 = fetchingReducer(state0, messageFetchStartAction);
-
-      const messageFetchErrorAction = deepFreeze({
-        type: MESSAGE_FETCH_ERROR,
-        narrow: HOME_NARROW,
-        error: new Error(),
-      });
-
-      const finalState = fetchingReducer(state1, messageFetchErrorAction);
-
-      const expectedState = {
-        [HOME_NARROW_STR]: DEFAULT_FETCHING,
-      };
-
-      expect(finalState).toEqual(expectedState);
+      expect(
+        [
+          deepFreeze({ ...eg.action.message_fetch_start, narrow: HOME_NARROW }),
+          deepFreeze({ type: MESSAGE_FETCH_ERROR, narrow: HOME_NARROW, error: new Error() }),
+        ].reduce(fetchingReducer, eg.baseReduxState.fetching),
+      ).toEqual({ [HOME_NARROW_STR]: DEFAULT_FETCHING });
     });
   });
 
   describe('MESSAGE_FETCH_COMPLETE', () => {
     test('sets corresponding fetching flags to false, if messages are received before or after', () => {
-      const prevState = deepFreeze({
-        [HOME_NARROW_STR]: { older: true, newer: true },
-      });
-
-      const action = {
-        ...eg.action.message_fetch_complete,
-        narrow: HOME_NARROW,
-        numBefore: 10,
-        numAfter: 0,
-      };
-
-      const expectedState = {
-        [HOME_NARROW_STR]: { older: false, newer: true },
-      };
-
-      const newState = fetchingReducer(prevState, action);
-
-      expect(newState).toEqual(expectedState);
+      const prevState = deepFreeze({ [HOME_NARROW_STR]: { older: true, newer: true } });
+      expect(
+        fetchingReducer(prevState, {
+          ...eg.action.message_fetch_complete,
+          narrow: HOME_NARROW,
+          numBefore: 10,
+          numAfter: 0,
+        }),
+      ).toEqual({ [HOME_NARROW_STR]: { older: false, newer: true } });
     });
   });
 
   test('if fetched messages are from a search narrow, ignore them', () => {
-    const prevState = deepFreeze({
-      [HOME_NARROW_STR]: { older: true, newer: true },
-    });
-
-    const action = deepFreeze({
-      ...eg.action.message_fetch_complete,
-      narrow: SEARCH_NARROW('some query'),
-    });
-
-    const newState = fetchingReducer(prevState, action);
-
-    expect(newState).toEqual(prevState);
+    const prevState = deepFreeze({ [HOME_NARROW_STR]: { older: true, newer: true } });
+    expect(
+      fetchingReducer(
+        prevState,
+        deepFreeze({ ...eg.action.message_fetch_complete, narrow: SEARCH_NARROW('some query') }),
+      ),
+    ).toEqual(prevState);
   });
 });
