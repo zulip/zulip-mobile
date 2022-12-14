418ca192f15fd3ff4d3e89fa4182973b05d084e7
Chris Bobbe <cbobbe@zulip.com>
2022-12-13 21:29:48 -0800

    outbox tests [nfc]: Tighten up code style a bit

--- src/outbox/__tests__/outboxReducer-test.js
+++ src/outbox/__tests__/outboxReducer-test.js
@@ -1,4 +1,4 @@
-// @flow strict-local
+/* @flow strict-local */
 import deepFreeze from 'deep-freeze';
 
 import outboxReducer from '../outboxReducer';
@@ -12,15 +12,9 @@ describe('outboxReducer', () => {
       const message1 = eg.streamOutbox({ content: 'New one' });
       const message2 = eg.streamOutbox({ content: 'Another one' });
       const message3 = eg.streamOutbox({ content: 'Message already sent', isSent: true });
-      const initialState = deepFreeze([message1, message2, message3]);
 
-      const action = eg.action.register_complete;
-
-      const expectedState = [message1, message2];
-
-      const actualState = outboxReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      const prevState = deepFreeze([message1, message2, message3]);
+      expect(outboxReducer(prevState, eg.action.register_complete)).toEqual([message1, message2]);
     });
   });
 
@@ -28,80 +22,61 @@ describe('outboxReducer', () => {
     test('add a new message to the outbox', () => {
       const message = eg.streamOutbox({ content: 'New one' });
 
-      const initialState = deepFreeze([]);
-
-      const action = deepFreeze({
-        type: MESSAGE_SEND_START,
-        outbox: message,
-      });
-
-      const expectedState = [message];
-
-      const actualState = outboxReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      const prevState = deepFreeze([]);
+      expect(
+        outboxReducer(prevState, deepFreeze({ type: MESSAGE_SEND_START, outbox: message })),
+      ).toEqual([message]);
     });
 
     test('do not add a message with a duplicate timestamp to the outbox', () => {
       const message1 = eg.streamOutbox({ content: 'hello', timestamp: 123 });
       const message2 = eg.streamOutbox({ content: 'hello twice', timestamp: 123 });
 
-      const initialState = deepFreeze([message1]);
-
-      const action = deepFreeze({
-        type: MESSAGE_SEND_START,
-        outbox: message2,
-      });
-
-      const actualState = outboxReducer(initialState, action);
-
-      expect(actualState).toBe(initialState);
+      const prevState = deepFreeze([message1]);
+      expect(
+        outboxReducer(prevState, deepFreeze({ type: MESSAGE_SEND_START, outbox: message2 })),
+      ).toBe(prevState);
     });
   });
 
   describe('EVENT_NEW_MESSAGE', () => {
     test('do not mutate state if a message is not removed', () => {
-      const initialState = deepFreeze([eg.streamOutbox({ timestamp: 546 })]);
-
       const message = eg.streamMessage({ timestamp: 123 });
 
-      const action = eg.mkActionEventNewMessage(message, {
-        local_message_id: message.timestamp,
-      });
-
-      const actualState = outboxReducer(initialState, action);
-      expect(actualState).toBe(initialState);
+      const prevState = deepFreeze([eg.streamOutbox({ timestamp: 546 })]);
+      expect(
+        outboxReducer(
+          prevState,
+          eg.mkActionEventNewMessage(message, { local_message_id: message.timestamp }),
+        ),
+      ).toBe(prevState);
     });
 
     test('remove message if local_message_id matches', () => {
       const message1 = eg.streamOutbox({ timestamp: 546 });
       const message2 = eg.streamOutbox({ timestamp: 150238512430 });
       const message3 = eg.streamOutbox({ timestamp: 150238594540 });
-      const initialState = deepFreeze([message1, message2, message3]);
 
-      const action = eg.mkActionEventNewMessage(eg.streamMessage(), {
-        local_message_id: 546,
-      });
-
-      const expectedState = [message2, message3];
-
-      const actualState = outboxReducer(initialState, action);
-
-      expect(actualState).toEqual(expectedState);
+      const prevState = deepFreeze([message1, message2, message3]);
+      expect(
+        outboxReducer(
+          prevState,
+          eg.mkActionEventNewMessage(eg.streamMessage(), { local_message_id: 546 }),
+        ),
+      ).toEqual([message2, message3]);
     });
 
     test("remove nothing if local_message_id doesn't match", () => {
       const message1 = eg.streamOutbox({ timestamp: 546 });
       const message2 = eg.streamOutbox({ timestamp: 150238512430 });
       const message3 = eg.streamOutbox({ timestamp: 150238594540 });
-      const initialState = deepFreeze([message1, message2, message3]);
 
-      const action = eg.mkActionEventNewMessage(eg.streamMessage(), {
-        local_message_id: 15023859,
-      });
-
-      const actualState = outboxReducer(initialState, action);
-      expect(actualState).toBe(initialState);
+      const prevState = deepFreeze([message1, message2, message3]);
+      const actualState = outboxReducer(
+        prevState,
+        eg.mkActionEventNewMessage(eg.streamMessage(), { local_message_id: 15023859 }),
+      );
+      expect(actualState).toBe(prevState);
     });
   });
 });
