7471f6e1e245a6aea1ef385af4479dd47ec2b918
Chris Bobbe <cbobbe@zulip.com>
2022-12-22 18:35:22 -0500

    NotificationsScreen: Offer link to system notification settings for Zulip

--- ios/ZulipMobile/ZLPConstants.swift
+++ ios/ZulipMobile/ZLPConstants.swift
@@ -15,6 +15,21 @@ class ZLPConstants: NSObject {
 
   @objc
   func constantsToExport() -> [String: Any]! {
-    return ["resourceURL": Bundle.main.resourceURL!.absoluteString]
+    var result: [String: Any] = [:]
+    result["resourceURL"] = Bundle.main.resourceURL!.absoluteString
+    result["UIApplication.openSettingsURLString"] = UIApplication.openSettingsURLString
+    if #available(iOS 16.0, *) {
+      // New name for the notification-settings URL
+      // https://developer.apple.com/documentation/uikit/uiapplication/4013180-opennotificationsettingsurlstrin/
+      // TODO(ios-16.0): Remove conditional and fallback for 15.4+.
+      result["UIApplication.openNotificationSettingsURLString"] = UIApplication.openNotificationSettingsURLString
+    }
+    if #available(iOS 15.4, *) {
+      // Old name for the notification-settings URL
+      // https://developer.apple.com/documentation/uikit/uiapplicationopennotificationsettingsurlstring
+      // TODO(ios-15.4): Remove conditional.
+      result["UIApplicationOpenNotificationSettingsURLString"] = UIApplicationOpenNotificationSettingsURLString
+    }
+    return result
   }
 }
--- src/settings/NotificationsScreen.js
+++ src/settings/NotificationsScreen.js
@@ -2,6 +2,8 @@
 
 import React, { useCallback } from 'react';
 import type { Node } from 'react';
+import { Platform, Linking, NativeModules } from 'react-native';
+import OpenNotification from 'react-native-open-notification';
 
 import type { RouteProp } from '../react-navigation';
 import type { AppNavigationProp } from '../nav/AppNavigator';
@@ -11,11 +13,38 @@ import SwitchRow from '../common/SwitchRow';
 import Screen from '../common/Screen';
 import * as api from '../api';
 import ServerPushSetupBanner from '../common/ServerPushSetupBanner';
+import NestedNavRow from '../common/NestedNavRow';
+
+const { ZLPConstants } = NativeModules;
 
 type Props = $ReadOnly<{|
   navigation: AppNavigationProp<'notifications'>,
   route: RouteProp<'notifications', void>,
 |}>;
+
+function openSystemNotificationSettings() {
+  if (Platform.OS === 'ios') {
+    Linking.openURL(
+      // Link directly to notification settings when iOS supports it
+      // (15.4+). Otherwise, link to the regular settings, and the user
+      // should get to notification settings with one tap from there.
+      ZLPConstants['UIApplication.openNotificationSettingsURLString'] // New name, iOS 16.0+
+        // TODO(ios-16.0): Remove use of old name
+        ?? ZLPConstants.UIApplicationOpenNotificationSettingsURLString // Old name, iOS 15.4+
+        // TODO(ios-15.4): Remove fallback.
+        ?? ZLPConstants['UIApplication.openSettingsURLString'],
+    );
+  } else {
+    // On iOS, react-native-open-notification doesn't support opening all
+    // the way to *notification* settings. It does support that on
+    // Android, so we use it here. The library is oddly named for one that
+    // opens notification settings; perhaps one day we'll replace it with
+    // our own code. But Greg points out that the implementation is small
+    // and reasonable:
+    //   https://github.com/zulip/zulip-mobile/pull/5627#discussion_r1058039648
+    OpenNotification.open();
+  }
+}
 
 /** (NB this is a per-account screen -- these are per-account settings.) */
 export default function NotificationsScreen(props: Props): Node {
@@ -23,6 +52,10 @@ export default function NotificationsScreen(props: Props): Node {
   const offlineNotification = useSelector(state => getSettings(state).offlineNotification);
   const onlineNotification = useSelector(state => getSettings(state).onlineNotification);
   const streamNotification = useSelector(state => getSettings(state).streamNotification);
+
+  const handleSystemSettingsPress = useCallback(() => {
+    openSystemNotificationSettings();
+  }, []);
 
   // TODO(#3999): It'd be good to show "working on it" UI feedback while a
   //   request is pending, after the user touches a switch.
@@ -54,6 +87,7 @@ export default function NotificationsScreen(props: Props): Node {
   return (
     <Screen title="Notifications">
       <ServerPushSetupBanner isDismissable={false} />
+      <NestedNavRow label="System settings for Zulip" onPress={handleSystemSettingsPress} />
       <SwitchRow
         label="Notifications when offline"
         value={offlineNotification}
