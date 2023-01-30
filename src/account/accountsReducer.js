d52fe78895f5d0ae4d6c18c105fbcbf263380a56
Chris Bobbe <cbobbe@zulip.com>
2023-01-30 12:08:16 -0800

    accounts [nfc]: Inline remaining reducer handler-helpers
    See the previous commit, where we did just one of these.

--- src/account/accountsReducer.js
+++ src/account/accountsReducer.js
@@ -27,57 +27,6 @@ const findAccount = (state: AccountsState, identity: Identity): number => {
   );
 };
 
-const loginSuccess = (state, action) => {
-  const { realm, email, apiKey } = action;
-  const accountIndex = findAccount(state, { realm, email });
-  if (accountIndex === -1) {
-    return [
-      {
-        realm,
-        email,
-        apiKey,
-        userId: null,
-        ackedPushToken: null,
-        zulipVersion: null,
-        zulipFeatureLevel: null,
-        lastDismissedServerPushSetupNotice: null,
-      },
-      ...state,
-    ];
-  }
-  return [
-    { ...state[accountIndex], apiKey, ackedPushToken: null },
-    ...state.slice(0, accountIndex),
-    ...state.slice(accountIndex + 1),
-  ];
-};
-
-const ackPushToken = (state, action) => {
-  const { pushToken: ackedPushToken, identity } = action;
-  const accountIndex = findAccount(state, identity);
-  if (accountIndex === -1) {
-    return state;
-  }
-  return [
-    ...state.slice(0, accountIndex),
-    { ...state[accountIndex], ackedPushToken },
-    ...state.slice(accountIndex + 1),
-  ];
-};
-
-const unackPushToken = (state, action) => {
-  const { identity } = action;
-  const accountIndex = findAccount(state, identity);
-  if (accountIndex === -1) {
-    return state;
-  }
-  return [
-    ...state.slice(0, accountIndex),
-    { ...state[accountIndex], ackedPushToken: null },
-    ...state.slice(accountIndex + 1),
-  ];
-};
-
 // eslint-disable-next-line default-param-last
 export default (state: AccountsState = initialState, action: Action): AccountsState => {
   switch (action.type) {
@@ -107,14 +56,56 @@ export default (state: AccountsState = initialState, action: Action): AccountsSt
         : [account, ...state.filter(a => a !== account)]; // put account first
     }
 
-    case LOGIN_SUCCESS:
-      return loginSuccess(state, action);
+    case LOGIN_SUCCESS: {
+      const { realm, email, apiKey } = action;
+      const accountIndex = findAccount(state, { realm, email });
+      if (accountIndex === -1) {
+        return [
+          {
+            realm,
+            email,
+            apiKey,
+            userId: null,
+            ackedPushToken: null,
+            zulipVersion: null,
+            zulipFeatureLevel: null,
+            lastDismissedServerPushSetupNotice: null,
+          },
+          ...state,
+        ];
+      }
+      return [
+        { ...state[accountIndex], apiKey, ackedPushToken: null },
+        ...state.slice(0, accountIndex),
+        ...state.slice(accountIndex + 1),
+      ];
+    }
 
-    case ACK_PUSH_TOKEN:
-      return ackPushToken(state, action);
+    case ACK_PUSH_TOKEN: {
+      const { pushToken: ackedPushToken, identity } = action;
+      const accountIndex = findAccount(state, identity);
+      if (accountIndex === -1) {
+        return state;
+      }
+      return [
+        ...state.slice(0, accountIndex),
+        { ...state[accountIndex], ackedPushToken },
+        ...state.slice(accountIndex + 1),
+      ];
+    }
 
-    case UNACK_PUSH_TOKEN:
-      return unackPushToken(state, action);
+    case UNACK_PUSH_TOKEN: {
+      const { identity } = action;
+      const accountIndex = findAccount(state, identity);
+      if (accountIndex === -1) {
+        return state;
+      }
+      return [
+        ...state.slice(0, accountIndex),
+        { ...state[accountIndex], ackedPushToken: null },
+        ...state.slice(accountIndex + 1),
+      ];
+    }
 
     case LOGOUT: {
       return [{ ...state[0], apiKey: '', ackedPushToken: null }, ...state.slice(1)];
