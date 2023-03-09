c62402d0551715049113042f44091e9d31897ae5
Chris Bobbe <cbobbe@zulip.com>
2023-03-08 21:32:56 -0800

    ZLPNotifications [nfc]: Remove trivial `constantsToExport`
    Since the module doesn't have any constants to export. If it wants
    to export any, we can always add the method back.
    
    Also remove `requiresMainQueueSetup`, which (according to the linked
    doc) was needed because we were implementing `constantsToExport`.
    
    On my iPhone 13 Pro running iOS 16.1, the feature supported by this
    module (alerting the user when Zulip notifications are disabled at
    the system level) still works as expected.

--- ios/ZulipMobile/ZLPNotifications.swift
+++ ios/ZulipMobile/ZLPNotifications.swift
@@ -4,15 +4,6 @@ import React.RCTBridgeModule
 
 @objc(ZLPNotifications)
 class ZLPNotifications: NSObject {
-  // For why we include this, see
-  //   https://reactnative.dev/docs/0.68/native-modules-ios#exporting-constants
-  @objc
-  static func requiresMainQueueSetup() -> Bool {
-    // Initialization may be done on any thread; we don't need access to
-    // UIKit.
-    return false
-  }
-
   /// Whether the app can receive remote notifications.
   // Ideally we could subscribe to changes in this value, but there
   // doesn't seem to be an API for that. The caller can poll, e.g., by
@@ -27,10 +18,5 @@ class ZLPNotifications: NSObject {
       .getNotificationSettings(completionHandler: { (settings) -> Void in
         resolve(settings.authorizationStatus == UNAuthorizationStatus.authorized)
       })
-  }
-
-  @objc
-  func constantsToExport() -> [String: Any]! {
-    return [:]
   }
 }
