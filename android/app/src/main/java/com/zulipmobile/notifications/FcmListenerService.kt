e40dd1ace516d34a1f15bbbed568b5a275244665
Chris Bobbe <cbobbe@zulip.com>
2023-03-28 12:55:46 -0700

    android [nfc]: Remove some unneeded auto-generated imports
    Odd that the auto-conversion to Kotlin added these; they aren't
    needed:
      https://github.com/zulip/zulip-mobile/pull/5695#discussion_r1149970734

--- android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.kt
+++ android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.kt
@@ -1,11 +1,9 @@
 package com.zulipmobile.notifications
 
-import com.zulipmobile.notifications.onReceived
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.ReactApplication
-import com.zulipmobile.notifications.NotificationsModule
 
 class FcmListenerService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
