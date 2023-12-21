17eb634d151f09745d107a21bbb36f07a7685180
Chris Bobbe <cbobbe@zulip.com>
2023-12-20 17:34:50 -0800

    test: Have our mock GetText `mock_` handle our special "{_}" message
    I don't think the "{_}" ever comes up when we run our tests -- this
    codebase is extremely light on UI tests -- but it's about to, and in
    such a way that it'll be nice if our mock handled it.

--- src/__tests__/lib/intl.js
+++ src/__tests__/lib/intl.js
@@ -3,4 +3,17 @@
 import type { GetText } from '../../types';
 
 // Our translation function, usually given the name _.
-export const mock_: GetText = m => (typeof m === 'object' ? m.text : m); // eslint-disable-line no-underscore-dangle
+// eslint-disable-next-line no-underscore-dangle
+export const mock_: GetText = m => {
+  if (typeof m === 'object') {
+    if (m.text === '{_}') {
+      // $FlowIgnore[incompatible-indexer]
+      /* $FlowIgnore[incompatible-cast]
+         We expect an `m.values` that corresponds to `m.text`. */
+      const values = (m.values: {| +_: string |});
+      return values._;
+    }
+    return m.text;
+  }
+  return m;
+};
