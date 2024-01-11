5aa041fdf290c48615e32838d70bd302fa788afa
Chris Bobbe <cbobbe@zulip.com>
2024-01-11 14:54:15 -0800

    types: Update GetText jsdoc for Hooks-based use
    And drop the reference to withGetText, which we shouldn't need in a
    Hooks-based world. Indeed that HOC isn't even used anymore; we'll
    remove it next.

--- src/types.js
+++ src/types.js
@@ -372,15 +372,8 @@ export type LocalizableReactText =
 /**
  * Usually called `_`, and invoked like `_('Message')` -> `'Nachricht'`.
  *
- * To use, put these two lines at the top of a React component's body:
- *
- *     static contextType = TranslationContext;
- *     context: GetText;
- *
- * and then in methods, say `const _ = this.context`.
- *
- * Alternatively, for when `context` is already in use: use `withGetText`
- * and then say `const { _ } = this.props`.
+ * To use:
+ *   const _ = React.useContext(TranslationContext);
  */
 export type GetText = {|
   (message: string, values?: {| +[string]: MessageFormatPrimitiveValue |}): string,
