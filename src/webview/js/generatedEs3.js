1222bc9fa0792797bc08604883c6c992a9d6ef28
Chris Bobbe <cbobbe@zulip.com>
2024-01-23 12:34:04 -0800

    deps: Run `yarn upgrade`
    Tested on Android (office device) and iOS (my iPhone). See the
    previous commit about some preparation that was needed; we upgraded
    the AGP.

--- src/webview/js/generatedEs3.js
+++ src/webview/js/generatedEs3.js
@@ -36,6 +36,20 @@ var compiledWebviewJs = (function (exports) {
     }
     return e;
   }
+  function _toPrimitive(t, r) {
+    if ("object" != typeof t || !t) return t;
+    var e = t[Symbol.toPrimitive];
+    if (void 0 !== e) {
+      var i = e.call(t, r || "default");
+      if ("object" != typeof i) return i;
+      throw new TypeError("@@toPrimitive must return a primitive value.");
+    }
+    return ("string" === r ? String : Number)(t);
+  }
+  function _toPropertyKey(t) {
+    var i = _toPrimitive(t, "string");
+    return "symbol" == typeof i ? i : String(i);
+  }
   function _defineProperty(obj, key, value) {
     key = _toPropertyKey(key);
     if (key in obj) {
@@ -49,20 +63,6 @@ var compiledWebviewJs = (function (exports) {
       obj[key] = value;
     }
     return obj;
-  }
-  function _toPrimitive(input, hint) {
-    if (typeof input !== "object" || input === null) return input;
-    var prim = input[Symbol.toPrimitive];
-    if (prim !== undefined) {
-      var res = prim.call(input, hint || "default");
-      if (typeof res !== "object") return res;
-      throw new TypeError("@@toPrimitive must return a primitive value.");
-    }
-    return (hint === "string" ? String : Number)(input);
-  }
-  function _toPropertyKey(arg) {
-    var key = _toPrimitive(arg, "string");
-    return typeof key === "symbol" ? key : String(key);
   }
 
   const makeUserId = id => id;
