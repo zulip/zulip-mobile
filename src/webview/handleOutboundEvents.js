ae4cf01b7481ffcd7f7ad813367f2a31b9234553
Chris Bobbe <cbobbe@zulip.com>
2024-04-08 18:49:03 -0700

    msglist: Catch invalid-URL error on link long-press
    Fixes: #5854

--- src/webview/handleOutboundEvents.js
+++ src/webview/handleOutboundEvents.js
@@ -6,7 +6,7 @@ import * as api from '../api';
 import config from '../config';
 import type { UserId } from '../types';
 import type { JSONableDict } from '../utils/jsonable';
-import { showErrorAlert, showToast } from '../utils/info';
+import { showConfirmationDialog, showErrorAlert, showToast } from '../utils/info';
 import { pmKeyRecipientsFromMessage } from '../utils/recipient';
 import { isUrlAnImage, tryParseUrl } from '../utils/url';
 import * as logging from '../utils/logging';
@@ -209,8 +209,23 @@ const handleLongPress = (args: {|
   const { _ } = props;
 
   if (href !== null) {
-    const url = new URL(href, props.backgroundData.auth.realm).toString();
-    Clipboard.setString(url);
+    const url = tryParseUrl(href, props.backgroundData.auth.realm);
+    if (url == null) {
+      showConfirmationDialog({
+        title: 'Copy invalid link',
+        message: {
+          text: 'This link appears to be invalid. Do you want to copy it anyway?\n\n{text}',
+          values: { text: href },
+        },
+        onPressConfirm: () => {
+          Clipboard.setString(href);
+          showToast(_('Text copied'));
+        },
+        _,
+      });
+      return;
+    }
+    Clipboard.setString(url.toString());
     showToast(_('Link copied'));
     return;
   }
